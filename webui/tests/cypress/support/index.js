33f068101c5daeff77e354b392411bc403614174
Hamid Zare <hamid@determined.ai>
2020-04-03 16:34:50 -0700

    test: stream Cypress browser logs to terminal and CI

--- webui/tests/cypress/support/index.js
+++ webui/tests/cypress/support/index.js
@@ -16,5 +16,29 @@
 import './commands';
 
 Cypress.Cookies.defaults({
-  whitelist: /auth/,
+  whitelist: /auth/
+})
+
+Cypress.on('window:before:load', (window) => {
+  Cypress.log({
+    name: 'console.log',
+    message: 'wrap on console.log',
+  });
+
+  // pass through cypress log so we can see log inside command execution order
+  window.console.log = (...args) => {
+    Cypress.log({
+      name: 'console.log',
+      message: args,
+    });
+  };
+});
+
+Cypress.on('log:added', (options) => {
+  if (options.instrument === 'command') {
+    // eslint-disable-next-line no-console
+    console.log(
+      `${(options.displayName || options.name || '').toUpperCase()} ${options.message}`,
+    );
+  }
 });
