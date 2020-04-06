74816135d090ab8c68647fe5b4b984d4714f28e9
Hamid Zare <hamid@determined.ai>
2020-04-06 16:37:49 -0700

    feat: refactor agents into using React hook pattern

--- webui/react/src/contexts/Agents.tsx
+++ webui/react/src/contexts/Agents.tsx
@@ -1,9 +1,14 @@
 import { generateContext } from 'contexts';
+import { RestApiState } from 'hooks/useRestApi';
 import { Agent } from 'types';
 
-const contextProvider = generateContext<Agent[]>({
-  initialState: [],
-  name: 'Agent',
-});
+const initialState = {
+  errorCount: 0,
+  hasLoaded: false,
+  isLoading: false,
+};
 
-export default contextProvider;
+export default generateContext<RestApiState<Agent[]>>({
+  initialState: initialState,
+  name: 'Agents',
+});
