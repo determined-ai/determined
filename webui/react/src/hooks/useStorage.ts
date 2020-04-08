e97696a1124e7558f974fc669610bb90b9f1a844
Hamid Zare <hamid@determined.ai>
2020-04-07 18:08:21 -0700

    feat: isolate user preferences in localStorage from each other

--- webui/react/src/hooks/useStorage.ts
+++ webui/react/src/hooks/useStorage.ts
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 
+import Auth from 'contexts/Auth';
 import { Storage, Store } from 'utils/storage';
 
 export const useStorage = (basePath: string, store: Store = window.localStorage): Storage => {
+  const auth = Auth.useStateContext();
+  const userNamespace = auth.user ? `u:${auth.user.username}` : '';
   const [ storage ] = useState(
-    new Storage({ basePath, store }),
+    new Storage({ basePath: `${userNamespace}/${basePath}`, store }),
   );
   return storage;
 };
