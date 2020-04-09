f792ab6ef874fb322cf76a09a224174aaea29c4b
Yoav Zimmerman <yoavz@determined.ai>
2020-04-09 11:17:39 -0700

    test: add unit tests for det.create_trial_instance

---
+++ tests/unit/test_native.py
@@ -0,0 +1,27 @@
+import tempfile
+from typing import Type
+
+import pytest
+
+import determined as det
+from determined_common import check
+from tests.unit.frameworks.fixtures import (
+    estimator_xor_model,
+    pytorch_xor_model,
+    tf_keras_xor_model,
+)
+
+
+@pytest.mark.parametrize(  # type: ignore
+    "trial_class",
+    [estimator_xor_model.XORTrial, pytorch_xor_model.XORTrial, tf_keras_xor_model.XORTrial],
+)
+def test_create_trial_instance(trial_class: Type[det.Trial]) -> None:
+    with tempfile.TemporaryDirectory() as td:
+        trial_instance = det.create_trial_instance(
+            trial_def=trial_class,
+            config={"hyperparameters": {"global_batch_size": det.Constant(16)}},
+            checkpoint_dir=td,
+        )
+
+        check.check_isinstance(trial_instance, det.Trial)
