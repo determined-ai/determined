a4b18220560b33639ef2a19e63b91b559305d9a2
Bradley Laney <bradlaney@determined.ai>
2020-04-13 14:23:20 -0400

    chore: specify application_name in postgres cnxs
    While debugging DET-2812 this came up as a "nice to have" sort of thing,
    mainly to filter hasura out easier. Also useful for anyone trying to
    admin Determined elsewhere.

--- scripts/hasura/run.sh
+++ scripts/hasura/run.sh
@@ -9,7 +9,7 @@ docker run -p 127.0.0.1:8081:8080 --rm \
   --network "$DOCKER_NETWORK" \
   --name hasura \
   -e HASURA_GRAPHQL_ADMIN_SECRET="$HASURA_SECRET" \
-  -e HASURA_GRAPHQL_DATABASE_URL=postgres://postgres:"$DB_PASSWORD"@"$DB_HOST":5432/determined \
+  -e HASURA_GRAPHQL_DATABASE_URL=postgres://postgres:"$DB_PASSWORD"@"$DB_HOST":5432/determined?application_name=hasura \
   -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
   -e HASURA_GRAPHQL_ENABLE_TELEMETRY=false \
   -e HASURA_GRAPHQL_CONSOLE_ASSETS_DIR=/srv/console-assets \
