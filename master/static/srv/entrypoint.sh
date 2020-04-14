0ea369ab5bc8e3cc5e4e00a934c1a71c6ad7ed8f
brian <brian@determined.ai>
2020-04-13 20:20:21 -0700

    chore: directly install the determined wheels in entrypoints

--- master/static/srv/entrypoint.sh
+++ master/static/srv/entrypoint.sh
@@ -6,6 +6,7 @@ set -x
 WORKING_DIR="/run/determined/workdir"
 STARTUP_HOOK="./startup-hook.sh"
 
-python3.6 -m pip install --user --upgrade --find-links /opt/determined/wheels determined
+python3.6 -m pip install --user /opt/determined/wheels/determined*.whl
+
 cd ${WORKING_DIR} && test -f "${STARTUP_HOOK}" && source "${STARTUP_HOOK}"
 exec python3.6 -m determined.exec.harness "$@"
