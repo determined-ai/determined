a5c42b8fe6b2cd5513369d60b8330f8008eaa2df
brian <brian@determined.ai>
2020-04-13 18:47:37 -0700

    feat: update environments with Horovod TF2 Keras gradient aggregation support

--- deploy/determined_deploy/gcp/constants.py
+++ deploy/determined_deploy/gcp/constants.py
@@ -2,7 +2,7 @@ class defaults:
 
     AGENT_INSTANCE_TYPE = "n1-standard-32"
     DB_PASSWORD = "postgres"
-    ENVIRONMENT_IMAGE = "pedl-environments-f0545f2"
+    ENVIRONMENT_IMAGE = "pedl-environments-5b275a7"
     GPU_NUM = 8
     GPU_TYPE = "nvidia-tesla-k80"
     HASURA_SECRET = "secret"
