a26fd9d42c5b225815ddfb06009e7afc8829ad1f
Bradley Laney <bradlaney@determined.ai>
2020-04-14 10:03:30 -0400

    chore: lock down database max_connection on the client side
    The default behavior of database/sql's pooling is to open an new
    physical connection to the database every time it needs one and
    none are available. This lead to DET-2812 and previous out of
    connections errors. Keeping the max_conns low results in more
    throughput, less contention and.. less errors.
    See wiki.postgresql.org/wiki/Number_Of_Database_Connections for more.

--- master/internal/db/setup.go
+++ master/internal/db/setup.go
@@ -7,6 +7,8 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+const maxOpenConns = 48
+
 const cnxTpl = "postgres://%s:%s@%s:%s/%s?sslmode=disable&application_name=determined-master"
 
 // Setup connects to the database and run any necessary migrations.
@@ -17,6 +19,9 @@ func Setup(opts *Config) (*PgDB, error) {
 	if err != nil {
 		return nil, errors.Wrapf(err, "connecting to database: %s", dbURL)
 	}
+
+	db.sql.SetMaxOpenConns(maxOpenConns)
+
 	log.Infof("running migrations from %v", opts.Migrations)
 	if err = db.Migrate(opts.Migrations); err != nil {
 		return nil, errors.Wrap(err, "running migrations")
