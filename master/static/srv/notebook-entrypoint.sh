8fb768d95426e59bf9105097e3eef2365440a8ec
Ryan <rb@determined.ai>
2020-04-17 17:02:01 -0600

    fix: add det command to PATH
    Add the det command to the PATH in the entrypoint scripts.
    
    It cannot be set with other environment variables in the master because
    the docker.Config object will not do environment variable expansion.
    
    The alternative would be to somehow, in the master, inspect the metadata
    for the docker container to get the PATH environment variable, then use
    that to set the final PATH, but then you would need to have the image
    present on the master for that to work.

--- master/static/srv/notebook-entrypoint.sh
+++ master/static/srv/notebook-entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export PATH="/run/determined/pythonuserbase/bin:$PATH"
+
 python3.6 -m pip install --user /opt/determined/wheels/determined*.whl
 
 jupyter lab --config /run/determined/workdir/jupyter-conf.py
