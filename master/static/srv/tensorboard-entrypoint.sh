50a502117db6ca9cccfa36e9f5724f6b7bd7f4b7
Ryan <rb@determined.ai>
2020-04-24 16:45:11 -0600

    fix: install determined before using it

--- master/static/srv/tensorboard-entrypoint.sh
+++ master/static/srv/tensorboard-entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 
 export PATH="/run/determined/pythonuserbase/bin:$PATH"
 
+python3.6 -m pip install --user /opt/determined/wheels/determined*.whl
+
 STARTUP_HOOK=${STARTUP_HOOK_SCRIPT:-startup-hook.sh}
 
 # Source `startup-hook.sh` before running tensorboard.
 source <(python3.6 -m determined.exec.prepare_env ${STARTUP_HOOK})
-
-python3.6 -m pip install --user /opt/determined/wheels/determined*.whl
 
 # S3 backed experiments must have AWS_REGION set in the environment.
 eval "$(python3.6 -m determined.exec.tensorboard s3)"
