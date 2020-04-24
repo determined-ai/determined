8d94b00a3f285db7836840f1cdfef15852722039
Jonathan Ben-tzur <yoni@determined.ai>
2020-04-24 11:52:37 -0700

    feat: reintroduce trial endpoints

---
+++ master/static/srv/get_trial_metrics.sql
@@ -0,0 +1,52 @@
+SELECT row_to_json(r1)
+FROM
+  (SELECT t.id,
+          t.experiment_id,
+          t.state,
+          t.start_time,
+          t.end_time,
+          t.hparams,
+          t.seed,
+          t.warm_start_checkpoint_id,
+
+     (SELECT coalesce(jsonb_agg(r2
+                                ORDER BY r2.id ASC), '[]'::JSONB)
+      FROM
+        (SELECT s.id,
+                s.trial_id,
+                s.state,
+                s.start_time,
+                s.end_time,
+                s.metrics,
+
+           (SELECT row_to_json(r3)
+            FROM
+              (SELECT c.id,
+                      c.trial_id,
+                      c.step_id,
+                      c.state,
+                      c.start_time,
+                      c.end_time,
+                      c.uuid,
+                      c.resources,
+                      c.labels
+               FROM checkpoints c
+               WHERE c.trial_id = t.id
+                 AND c.step_id = s.id ) r3) AS CHECKPOINT,
+
+           (SELECT row_to_json(r4)
+            FROM
+              (SELECT v.id,
+                      v.trial_id,
+                      v.step_id,
+                      v.state,
+                      v.start_time,
+                      v.end_time,
+                      v.metrics
+               FROM validations v
+               WHERE v.trial_id = t.id
+                 AND v.step_id = s.id ) r4) AS validation
+         FROM steps s
+         WHERE s.trial_id = t.id ) r2) AS steps
+   FROM trials t
+   WHERE t.id = $1 ) r1
