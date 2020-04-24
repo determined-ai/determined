111fcd3c687b09f6abaa302777841f22313f50f8
Hamid Zare <hamid@determined.ai>
2020-04-23 18:55:43 -0700

    test: add authentication tests.

--- webui/tests/cypress/integration/02-loginout.spec.ts
+++ webui/tests/cypress/integration/02-loginout.spec.ts
@@ -4,6 +4,8 @@ describe('Sign in/out', () => {
     cy.login();
   });
 
+  const elmTitleSelector = '#det-main-container div.text-2xl';
+
   it('should be logged in React side', () => {
     cy.visit('/det/dashboard');
     cy.checkLoggedIn('determined');
@@ -36,23 +38,46 @@ describe('Sign in/out', () => {
   });
 
   it('should be able to log in', () => {
-    // Logging out above should put us on the login page, so enter the login
-    // information directly.
-    cy.checkLoggedOut();
+    cy.logout();
     cy.visit('/det/login');
     const username = 'determined';
     // We directly set the value to avoid using the less reliable .type() method
     // from Cypress. We also trigger 'input' event to keep it closer to an actual typing
     // behavior this would help functions relying onInput.
-    // cy.get('input#input_username')
+    // cy.get('form input#basic_username')
     //   .invoke('val', username)
+    //   .trigger('change')
     //   .trigger('input');
 
     cy.get('input#basic_username')
-      .type(username, { delay: 50, force: true })
+      .type(username, { delay: 100 })
       .should('have.value', username);
 
     cy.get('button[type="submit"]').click();
     cy.checkLoggedIn('determined');
   });
+
+  it('visiting login while logged in should redirect away from login', () => {
+    cy.login();
+    cy.visit('/det/login');
+    cy.url().should('not.contain', '/det/login');
+  });
+
+  it('visting the logout page should logout the user', () => {
+    cy.login();
+    cy.visit('/det/logout');
+    cy.checkLoggedOut();
+  });
+
+  it('visting the logout page should land user in the login page', () => {
+    cy.visit('/det/logout');
+    cy.url().should('contain', '/det/login');
+  });
+
+  it('login page should redirect to the request url on elm', () => {
+    cy.login();
+    cy.visit('/det/login?redirect=/ui/experiments');
+    cy.get(elmTitleSelector).contains('Experiments');
+  });
+
 });
