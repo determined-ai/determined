3cfd7029e38d3b8688977e117fb99c5e9990a3d6
Shiyuan Zhu <shiyuan@determined.ai>
2020-04-28 14:29:35 -0700

    docs: make TF Keras Native and Trial examples use same model
    DET-2692 #Done.

--- examples/experimental/native_fashion_mnist_tf_keras/model_def.py
+++ examples/experimental/native_fashion_mnist_tf_keras/model_def.py
@@ -1,11 +1,13 @@
 """
-This example shows how to use Determined to implement a tf.keras-based CNN to
-perform image classification on the Fashion-MNIST dataset.
+This example shows how to use Determined to implement an image
+classification model for the Fashion-MNIST dataset using tf.keras.
 
-Based off: https://www.tensorflow.org/tutorials/keras/classification
+Based on: https://www.tensorflow.org/tutorials/keras/classification.
+
+After about 5 training epochs, accuracy should be around > 85%.
+This mimics theoriginal implementation. Continue training or increase
+the number of steps to increase accuracy.
 """
-import tempfile
-
 import tensorflow as tf
 from tensorflow import keras
 
@@ -14,18 +16,15 @@ from determined.keras import TFKerasTrial, TFKerasTrialContext, InputData
 import data
 
 
-class MNISTTrial(TFKerasTrial):
+class FashionMNISTTrial(TFKerasTrial):
     def __init__(self, context: TFKerasTrialContext) -> None:
         self.context = context
-
-        # Create a unique download directory for each rank so they don't overwrite each other.
-        self.download_directory = tempfile.mkdtemp()
 
     def build_model(self):
         model = keras.Sequential(
             [
                 keras.layers.Flatten(input_shape=(28, 28)),
-                keras.layers.Dense(128, activation="relu"),
+                keras.layers.Dense(self.context.get_hparam("dense1"), activation="relu"),
                 keras.layers.Dense(10),
             ]
         )
@@ -38,13 +37,13 @@ class MNISTTrial(TFKerasTrial):
         return model
 
     def build_training_data_loader(self) -> InputData:
-        (train_images, train_labels), (_, _) = data.load_data(self.download_directory)
+        train_images, train_labels = data.load_training_data()
         train_images = train_images / 255.0
 
         return train_images, train_labels
 
     def build_validation_data_loader(self) -> InputData:
-        (_, _), (test_images, test_labels) = data.load_data(self.download_directory)
+        test_images, test_labels = data.load_validation_data()
         test_images = test_images / 255.0
 
         return test_images, test_labels
