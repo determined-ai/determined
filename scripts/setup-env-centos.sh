e1aa8301fa73b4ff98fd4a29ce813f1afc4cdbc8
Danny Zhu <dzhu@determined.ai>
2020-05-01 20:07:30 -0700

    chore: update OS setup scripts (#395)
    - Update the scripts to use the newer `nvidia-toolkit-container` package
      rather than the deprecated `nvidia-docker2` one, since the rest of the
      system has just switched over.
    
    - Remove the use of `virtualenvwrapper` in favor of the
      Python-recommended `python -m venv` (except on CentOS, since it seems
      to be awkward to get the latter to work there).
    
    - Reorder each script into "install things from the network" followed by
      "do local setup things".
    
    - Make the scripts consistently handle the Docker service (they all
      enable it, to avoid somebody coming back after a reboot and being
      surprised that it's not running, and restart it, which is necessary to
      pick up GPU support after installing the toolkit).
    
    - Apply `shfmt`.

--- scripts/setup-env-centos.sh
+++ scripts/setup-env-centos.sh
@@ -1,30 +1,56 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e -x
 
+PKGS=(docker-ce golang krb5-devel nodejs nvidia-container-toolkit python36 python36-devel)
+
+# Read user choices.
+read -r -p "Do you have an NVIDIA GPU and want to enable Docker GPU support? [y/n] "
+if [[ $REPLY =~ ^[Yy].*$ ]]; then
+    PKGS+=(nvidia-container-toolkit)
+fi
+
+# Download things.
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -fsSL https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | \
-    sudo tee /etc/yum.repos.d/nvidia-docker.repo
+distribution=$(
+  . /etc/os-release
+  echo "$ID$VERSION_ID"
+)
+curl -fsSL https://nvidia.github.io/nvidia-docker/"$distribution"/nvidia-docker.repo |
+  sudo tee /etc/yum.repos.d/nvidia-docker.repo
 
 sudo rpm --import https://mirror.go-repo.io/centos/RPM-GPG-KEY-GO-REPO
 curl -fsSL https://mirror.go-repo.io/centos/go-repo.repo | sudo tee /etc/yum.repos.d/go-repo.repo
 
-sudo yum install -y \
-  docker-ce \
-  golang \
-  krb5-devel \
-  nodejs \
-  nvidia-docker2 \
-  python36 \
-  python36-devel
-sudo systemctl start docker
+sudo yum install -y "${PKGS[@]}"
 
 curl -fsSL https://bootstrap.pypa.io/get-pip.py | sudo python3.6
 
 sudo pip3 install virtualenvwrapper
 
-sudo usermod -aG docker $USER
-echo "source /usr/bin/virtualenvwrapper.sh" >> ~/.bashrc
+# Configure local things.
+sudo usermod -aG docker "$USER"
+sudo systemctl enable docker
+sudo systemctl restart docker
+
+echo -e "\x1b[1mConsider adding the following line to your shell startup script:\x1b[m"
+echo
+echo "    . /usr/bin/virtualenvwrapper.sh"
+
+set +x
+echo -e '
+\x1b[32;1mInstallation complete!\x1b[m
+
+You may want to run the following commands:
+
+- To ensure that Go-related tools installed by Determined can be found:
+
+    export PATH="$(go env GOPATH)"/bin:"$PATH"
+
+  This command should also be added to your shell initialization script.
+
+- To set up a Python virtualenv for use with Determined:
+
+    mkvirtualenv ~/.virtualenvs/determined'
--- scripts/setup-env-macos.sh
+++ scripts/setup-env-macos.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
 set -x
 
-# Quick install script for mac
-
-# Install homebrew
+# Install Homebrew.
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# Brew install all packages
+# Brew install all packages.
 brew upgrade
 
 brew install \
-	go \
-	libomp \
-	node \
-	git \
-	pyenv \
-	yarn
+  go \
+  node \
+  git \
+  pyenv \
+  yarn
 
 brew cask install \
-	docker
+  docker
 
-# Install Python 3.6 and virtualenv
-pyenv install 3.6.9
-python3 -m pip install virtualenv
+pyenv install 3.6.10
 
-# Set up go mono repo
-mkdir -p "$HOME/go"
-mkdir -p "$HOME/go/bin"
-mkdir -p "$HOME/go/pkg"
-mkdir -p "$HOME/go/src"
+# Set up Determined virtualenv.
+
+set +x
+echo -e '
+\x1b[32;1mInstallation complete!\x1b[m
+
+You may want to run the following commands:
+
+- To ensure that Go-related tools installed by Determined can be found:
+
+    export PATH="$(go env GOPATH)"/bin:"$PATH"
+
+  This command should also be added to your shell initialization script.
+
+- To set up a Python virtualenv for use with Determined:
+
+    "$(pyenv root)"/versions/3.6.10/bin/python -m venv ~/.virtualenvs/determined'
