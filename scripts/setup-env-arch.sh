dd911486d886e723586bff5b25eefb6324abd405
Danny Zhu <dzhu@determined.ai>
2020-05-01 20:07:30 -0700

    chore: update OS setup scripts (#395)
    - Update the scripts to use the newer `nvidia-toolkit-container` package
      rather than the deprecated `nvidia-docker2` one, since the rest of the
      system has just switched over.
    
    - Remove the use of `virtualenvwrapper` in favor of the
      Python-recommended `python -m venv` (except on CentOS, since it seems
      to be awkward to get the latter to work there).
    
    - Reorder each script into "install things from the network" followed by
      "do local setup things".
    
    - Make the scripts consistently handle the Docker service (they all
      enable it, to avoid somebody coming back after a reboot and being
      surprised that it's not running, and restart it, which is necessary to
      pick up GPU support after installing the toolkit).
    
    - Apply `shfmt`.

--- scripts/setup-env-arch.sh
+++ scripts/setup-env-arch.sh
@@ -1,47 +1,44 @@
 #!/bin/bash
 
-set -e
+set -e -x
 
-## Specify the script/command that can install packages from both AUR and official Arch repos.
+# A command that can install both AUR and official packages; edit this if you use a different tool.
 PKG_INSTALL_COMMAND=(yay -S --needed)
 
-PKGS=(docker go python36 python-virtualenvwrapper)
+PKGS=(docker go npm python36)
 
-if which "${PKG_INSTALL_COMMAND[0]}" > /dev/null; then
-  echo "Installing packages: ${PKGS[@]}..."
-else
-  >&2 echo "Problem finding the install script!"
+if ! command -v "${PKG_INSTALL_COMMAND[0]}" >/dev/null; then
+  echo "Problem finding the install command ${PKG_INSTALL_COMMAND[0]}!" >&2
   exit 1
 fi
 
-"${PKG_INSTALL_COMMAND[@]}" "${PKGS[@]}"
-sudo usermod -aG docker "$USER"
-
-## Install Nvidia support for Docker.
-read -p "Do you have an Nvidia GPU and want to install Nvidia GPU support (y/n)? " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  echo "Installing nvidia-docker and setting the default configuration."
-  "${PKG_INSTALL_COMMAND[@]}" nvidia-docker
-
-  ## Set Docker's default runtime to the Nvidia one.
-  if [ -f /etc/docker/daemon.json ]; then
-    sudo tee -a /etc/docker/daemon.json.backup > /dev/null < /etc/docker/daemon.json 
-  fi
-  sudo sh -c 'echo <<EOT > /etc/docker/daemon.json
-  {
-    "default-runtime": "nvidia",
-    "runtimes": {
-      "nvidia": {
-        "path": "/usr/bin/nvidia-container-runtime",
-        "runtimeArgs": []
-      }
-    }
-  }
-  EOT'
+# Read user choices.
+read -r -p "Do you have an NVIDIA GPU and want to enable Docker GPU support? [y/n] "
+if [[ $REPLY =~ ^[Yy].*$ ]]; then
+  PKGS+=(nvidia-container-toolkit)
 fi
 
-sudo systemctl enable --now docker
+# Download things.
+echo "Installing packages: ${PKGS[*]}..."
+"${PKG_INSTALL_COMMAND[@]}" "${PKGS[@]}"
 
-## Set up Determined virtualenv.
-mkdir -p ~/.virtualenvs
-python3.6 -m venv ~/.virtualenvs/det
+# Configure local things.
+sudo usermod -aG docker "$USER"
+sudo systemctl enable docker
+sudo systemctl restart docker
+
+set +x
+echo -e '
+\x1b[32;1mInstallation complete!\x1b[m
+
+You may want to run the following commands:
+
+- To ensure that Go-related tools installed by Determined can be found:
+
+    export PATH="$(go env GOPATH)"/bin:"$PATH"
+
+  This command should also be added to your shell initialization script.
+
+- To set up a Python virtualenv for use with Determined:
+
+    python3.6 -m venv ~/.virtualenvs/determined'
