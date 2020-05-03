dfce44af1fd690dd92e08e45e5d2c4931b09a5ff
brian <brian@determined.ai>
2020-05-03 07:59:45 -0700

    ci: download aws cluster logs for aws tests

--- .circleci/scripts/get_output_from_stack.py
+++ .circleci/scripts/get_output_from_stack.py
@@ -3,12 +3,12 @@ import argparse
 import boto3
 
 
-def get_address_from_stack(stack_name: str) -> str:
+def get_output_from_stack(stack_name: str, output_key: str) -> str:
     stack = boto3.resource("cloudformation").Stack(stack_name)
-    public_ip = list(filter(lambda d: d["OutputKey"] == "DeterminedAddress", stack.outputs))[0][
+    output_value = list(filter(lambda d: d["OutputKey"] == output_key, stack.outputs))[0][
         "OutputValue"
     ]  # type: str
-    return public_ip
+    return output_value
 
 
 def main() -> None:
@@ -16,8 +16,9 @@ def main() -> None:
     parser.add_argument(
         "stack_name", help="Name of CloudFormation stack to get master address from."
     )
+    parser.add_argument("output_key", help="CloudFormation stack output key")
     args = parser.parse_args()
-    print(get_address_from_stack(args.stack_name), end="", flush=True)
+    print(get_output_from_stack(args.stack_name, args.output_key), end="", flush=True)
 
 
 if __name__ == "__main__":
