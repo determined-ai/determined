5e86ef59762bd78eda60ca872441b416b4b08cd0
Hamid Zare <hamid@determined.ai>
2020-05-04 19:22:19 -0700

    chore: remove unused utility module

--- webui/react/src/utils/styled.ts
+++
@@ -1,8 +0,0 @@
-import { hasKey } from 'utils/types';
-/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-export const isPropTrue = (props: Record<string, any>, propName: string): boolean => {
-  if (hasKey(props, propName)) {
-    return !!props[propName];
-  }
-  return false;
-};
