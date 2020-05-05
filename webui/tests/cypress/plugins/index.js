03d8441df769ac711277ea8b41b636569b0493ac
Caleb Kang <caleb@determined.ai>
2020-05-05 13:24:48 -0600

    ci: reduce webui e2e-tests cypress logs to warnings and errors

--- webui/tests/cypress/plugins/index.js
+++ webui/tests/cypress/plugins/index.js
@@ -11,10 +11,25 @@ module.exports = on => {
   };
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  on('task', {
-    createExperiment: experiments.create,
-  });
+  on('task', { createExperiment: experiments.create });
   on('file:preprocessor', wp(options));
 
-  require('cypress-log-to-output').install(on);
+  require('cypress-log-to-output').install(on, (type, event) => {
+    /*
+     * return true or false from this plugin to control if the event
+     * is logged `type` is either `console` or `browser`
+     * if `type` is `browser`, `event` is an object of the type `LogEntry`:
+     * https://chromedevtools.github.io/devtools-protocol/tot/Log#type-LogEntry
+     * if `type` is `console`, `event` is an object of the type passed
+     * to `Runtime.consoleAPICalled`:
+     * https://chromedevtools.github.io/devtools-protocol/tot/Runtime#event-consoleAPICalled
+     */
+
+    if (event.level === 'error' || event.type === 'error' ||
+        event.level === 'warning' || event.type === 'warning') {
+      return true;
+    }
+
+    return false;
+  });
 };
