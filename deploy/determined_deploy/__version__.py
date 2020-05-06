2b9d13151cc48f9e0ba94fb52c85aca03b00f9d2
Jonathan Ben-tzur <yoni@determined.ai>
2020-05-06 15:43:41 -0700

    chore: remove version dep from python packages

--- deploy/determined_deploy/__version__.py
+++ deploy/determined_deploy/__version__.py
@@ -1,7 +1,1 @@
-def _get_version() -> str:
-    import pkg_resources
-
-    return pkg_resources.get_distribution("determined_deploy").version
-
-
-__version__ = _get_version()
+__version__ = "0.12.4.dev0"
