47aca439cc438edd02b4b20e33796609d2aa0cbb
Shiyuan Zhu <shiyuan@determined.ai>
2020-05-07 17:08:46 -0700

    refactor: refactor tests to go in the unified path of test mode

--- harness/tests/experiment/test_local.py
+++ harness/tests/experiment/test_local.py
@@ -5,10 +5,10 @@ from tests.experiment.fixtures import pytorch_xor_model
 
 
 def test_test_one_batch() -> None:
-    experimental.test_one_batch(
-        pathlib.Path(pytorch_xor_model.__file__).parent,
-        pytorch_xor_model.XORTrial,
-        config={
-            "hyperparameters": {"hidden_size": 2, "learning_rate": 0.5, "global_batch_size": 4}
-        },
-    )
+    with experimental._local_execution_manager(pathlib.Path(pytorch_xor_model.__file__).parent):
+        experimental.test_one_batch(
+            trial_class=pytorch_xor_model.XORTrial,
+            config={
+                "hyperparameters": {"hidden_size": 2, "learning_rate": 0.5, "global_batch_size": 4}
+            },
+        )
