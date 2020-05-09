4ef388ec56fa0f4473d32f1f6b695e0729d1d074
Danny Zhu <dzhu@determined.ai>
2020-05-08 19:13:47 -0700

    docs: make Sphinx default to not highlighting code blocks
    Sphinx defaults to applying Python highlighting if possible [1], since
    reST is so closely associated with Python. In our docs, though, we
    actually have a pretty broad mix, so we don't want to end up with Python
    highlighting where it doesn't apply.
    
    This changes the default to no highlighting and removes all explicit
    instances of "text" highlighting, which also means no highlighting.
    
    [1] https://www.sphinx-doc.org/en/master/usage/restructuredtext/directives.html#code-examples

--- docs/conf.py
+++ docs/conf.py
@@ -63,6 +63,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "examples", "requirement
 # The suffix of source filenames.
 source_suffix = {".rst": "restructuredtext", ".txt": "restructuredtext"}
 
+highlight_language = "none"
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
