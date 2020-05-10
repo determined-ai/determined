68b9aa1c1438110da84886738c9ee8dd2d56ce51
Jonathan Ben-tzur <yoni@determined.ai>
2020-05-10 00:33:17 -0700

    fix: format agent tools file

--- agent/tools.go
+++ agent/tools.go
@@ -4,6 +4,6 @@ package tools
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "golang.org/x/tools/cmd/goimports"
 	_ "github.com/goreleaser/goreleaser"
+	_ "golang.org/x/tools/cmd/goimports"
 )
