7014f7d945508aa388f3e184145910fb0c687d60
Jonathan Ben-tzur <yoni@determined.ai>
2020-05-10 22:22:33 -0700

    chore: remove hasura from checkpoint list

--- common/determined_common/api/metric.py
+++ common/determined_common/api/metric.py
@@ -2,7 +2,7 @@ from typing import Any
 
 
 def get_validation_metric(metric_name: str, validation: Any) -> Any:
-    if not validation or not validation.metrics:
+    if not validation or not validation["metrics"]:
         return None
 
-    return validation.metrics.get("validation_metrics", {}).get(metric_name)
+    return validation["metrics"].get("validation_metrics", {}).get(metric_name)
