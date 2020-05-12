f1b9a1d6f7d2359f28915d476c73dce2f592242e
sidneyw <sidney@determined.ai>
2020-05-12 17:40:25 -0400

    fix: checkpoint load uses trial to retrieve keras h5 checkpoints
    Keras models cannot be retieved from just the h5 checkpoint file
    reliably. Additionally, if the user supplies custom object to keras ie.
    loss functions, the original code will be necessary to rebuild the model
    from a checkpoint. Therefore, we instantiate the trial class, call
    build_model, and load the weights for keras models.

--- common/determined_common/experimental/checkpoint/_checkpoint.py
+++ common/determined_common/experimental/checkpoint/_checkpoint.py
@@ -2,7 +2,7 @@ import enum
 import json
 import pathlib
 import shutil
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, cast
 
 from determined_common import api, storage
 
@@ -161,8 +161,21 @@ class Checkpoint(object):
                 <https://www.tensorflow.org/versions/r1.15/api_docs/python/tf/saved_model/load_v2>`_.
         """
         checkpoint_dir = pathlib.Path(path)
+        # We used MLflow's MLmodel checkpoint format in the past for
+        # serializing pytorch models. We now use our own format that contains a
+        # metadata.json file. We are checking for checkpoint existence by
+        # looking for both checkpoint formats in the output directory.
+        # Eventually, we will remove support for MLmodel checkpoints.
+        if checkpoint_dir.joinpath("MLmodel").exists():
+            import determined_common.experimental.checkpoint._torch
 
-        checkpoint_type = Checkpoint.get_type(checkpoint_dir)
+            return determined_common.experimental.checkpoint._torch.load_model(
+                checkpoint_dir, **kwargs
+            )
+
+        metadata = Checkpoint.parse_metadata(checkpoint_dir)
+        checkpoint_type = Checkpoint.get_type(metadata)
+
         if checkpoint_type == ModelFramework.PYTORCH:
             import determined_common.experimental.checkpoint._torch
 
@@ -174,29 +187,28 @@ class Checkpoint(object):
             import determined_common.experimental.checkpoint._tf
 
             return determined_common.experimental.checkpoint._tf.load_model(
-                checkpoint_dir, tags=tags
+                checkpoint_dir, metadata, tags=tags
             )
 
         raise AssertionError("Unknown checkpoint format at {}".format(checkpoint_dir))
 
     @staticmethod
-    def get_type(directory: pathlib.Path) -> ModelFramework:
-        # We used MLflow's MLmodel checkpoint format in the past for
-        # serializing pytorch models.
-        if directory.joinpath("MLmodel").exists():
-            return ModelFramework.PYTORCH
-
+    def parse_metadata(directory: pathlib.Path) -> Dict[str, Any]:
         metadata_path = directory.joinpath("metadata.json")
         with metadata_path.open() as f:
             metadata = json.load(f)
 
+        return cast(Dict[str, Any], metadata)
+
+    @staticmethod
+    def get_type(metadata: Dict[str, Any]) -> ModelFramework:
         if "torch_version" in metadata:
             return ModelFramework.PYTORCH
 
         elif "tensorflow_version" in metadata:
             return ModelFramework.TENSORFLOW
 
-        raise AssertionError("Unknown checkpoint format at {}".format(directory))
+        raise AssertionError("Unknown checkpoint format")
 
     def __repr__(self) -> str:
         return "Checkpoint(uuid={})".format(self.uuid)
