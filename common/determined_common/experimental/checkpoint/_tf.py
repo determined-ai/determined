dec3786b5fbfb6796c6e0e42ba92cc60acdc8b04
sidneyw <sidney@determined.ai>
2020-05-12 17:40:25 -0400

    fix: checkpoint load uses trial to retrieve keras h5 checkpoints
    Keras models cannot be retieved from just the h5 checkpoint file
    reliably. Additionally, if the user supplies custom object to keras ie.
    loss functions, the original code will be necessary to rebuild the model
    from a checkpoint. Therefore, we instantiate the trial class, call
    build_model, and load the weights for keras models.

--- common/determined_common/experimental/checkpoint/_tf.py
+++ common/determined_common/experimental/checkpoint/_tf.py
@@ -1,45 +1,58 @@
 import pathlib
-from typing import List, Optional
+from typing import Any, Dict, List, Optional, cast
 
 import tensorflow as tf
 from tensorflow.python.training.tracking.tracking import AutoTrackable
 
+from determined.experimental._native import _local_trial_from_context
+from determined.keras import TFKerasTrial
 
-def load_model(ckpt_dir: pathlib.Path, tags: Optional[List[str]] = None) -> AutoTrackable:
-    saved_model_paths = list(ckpt_dir.glob("**/saved_model.pb"))
-    h5_paths = list(ckpt_dir.glob("**/*.h5"))
 
-    if not h5_paths and not saved_model_paths:
-        raise AssertionError(
-            "No checkpoint saved_model.pb or h5 files found at {}".format(ckpt_dir)
+def load_model(
+    ckpt_dir: pathlib.Path, metadata: Dict[str, Any], tags: Optional[List[str]] = None
+) -> AutoTrackable:
+    save_format = metadata.get("format", None)
+
+    if not save_format or cast(str, save_format) == "saved_model":
+        return load_saved_model(ckpt_dir, tags=tags)
+
+    elif save_format == "h5":
+        trial = _local_trial_from_context(
+            ckpt_dir.joinpath("code"),
+            config=metadata["experiment_config"],
+            hparams=metadata["hparams"],
         )
+
+        trial = cast(TFKerasTrial, trial)
+        model = trial.build_model()
+        model.load_weights(str(ckpt_dir.joinpath("determined-keras-model.h5")))
+        return model
+    else:
+        raise AssertionError("Unknown checkpoint format at {}".format(str(ckpt_dir)))
+
+
+def load_saved_model(ckpt_dir: pathlib.Path, tags: Optional[List[str]] = None) -> AutoTrackable:
+    saved_model_paths = list(ckpt_dir.glob("**/saved_model.pb"))
 
     # Tensorflow 1 favors saved_models for tf.estimators and h5 for tf.keras
     # models. Tensorflow is moving towards saved_model for both high level
     # APIs in tf.2. For this reason we favor the saved_model below but also
     # check for h5 models.
-    if saved_model_paths:
-        if len(saved_model_paths) > 1:
-            raise AssertionError(
-                "Checkpoint directory {} contains multiple \
-                nested saved_model.pb files: {}".format(
-                    ckpt_dir, saved_model_paths
-                )
+    if len(saved_model_paths) > 1:
+        raise AssertionError(
+            "Checkpoint directory {} contains multiple \
+            nested saved_model.pb files: {}".format(
+                ckpt_dir, saved_model_paths
             )
+        )
 
-        if tags is None:
-            print('No tags specified. Loading "serve" tag from saved_model.')
-            tags = ["serve"]
+    # Tensorflow uses tags to determine which metagraph to load. Most
+    # commonly, users will attempt to serve or otherwise use the model for
+    # inference. Therefore we default to the serve graph tag which disables
+    # operations that are only relevant for training.
+    if tags is None:
+        print('No tags specified. Loading "serve" tag from saved_model.')
+        tags = ["serve"]
 
-        saved_model_path = saved_model_paths[0]
-        return tf.compat.v1.saved_model.load_v2(str(saved_model_path.parent), tags)
-
-    else:
-        if len(h5_paths) > 1:
-            raise AssertionError(
-                "Checkpoint directory {} contains multiple \
-                nested .h5 files: {}".format(
-                    ckpt_dir, h5_paths
-                )
-            )
-        return tf.keras.models.load_model(h5_paths[0])
+    saved_model_path = saved_model_paths[0]
+    return tf.compat.v1.saved_model.load_v2(str(saved_model_path.parent), tags)
