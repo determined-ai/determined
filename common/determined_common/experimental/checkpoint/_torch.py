b87ae28c002ba52fc72671a23d52c43a7e103aac
sidneyw <sidney@determined.ai>
2020-05-13 16:35:55 -0400

    feat: checkpoint load uses trial to retrieve checkpoints
    This reverts commit f1330027379a2141c85a1e9c524b63dc00e270e1.

--- common/determined_common/experimental/checkpoint/_torch.py
+++ common/determined_common/experimental/checkpoint/_torch.py
@@ -1,28 +1,22 @@
 import pathlib
-import sys
-from typing import Any
+from typing import Any, Dict, cast
 
-import cloudpickle
 import torch
 
+from determined.experimental._native import _local_trial_from_context
+from determined.pytorch import PyTorchTrial
 
-def load_model(ckpt_dir: pathlib.Path, **kwargs: Any) -> torch.nn.Module:
-    code_path = ckpt_dir.joinpath("code")
 
-    # We used MLflow's MLmodel checkpoint format in the past. This format
-    # nested the checkpoint in data/. Currently, we have the checkpoint at the
-    # top level of the checkpoint directory.
-    potential_model_paths = [["model.pth"], ["data", "model.pth"]]
+def load_model(ckpt_dir: pathlib.Path, metadata: Dict[str, Any], **kwargs: Any) -> torch.nn.Module:
+    trial = _local_trial_from_context(
+        ckpt_dir.joinpath("code"),
+        config=metadata["experiment_config"],
+        hparams=metadata["hparams"],
+    )
 
-    for nested_path in potential_model_paths:
-        maybe_model = ckpt_dir.joinpath(*nested_path)
-        if maybe_model.exists():
-            break
+    trial = cast(PyTorchTrial, trial)
+    model = trial.build_model()
+    checkpoint = torch.load(ckpt_dir.joinpath("state_dict.pth"), map_location="cpu")  # type: ignore
+    model.load_state_dict(checkpoint["model_state_dict"])
 
-    if not maybe_model.exists():
-        raise AssertionError("checkpoint at {} doesn't include a model.pth file".format(ckpt_dir))
-
-    code_subdirs = [str(x) for x in code_path.iterdir() if x.is_dir()]
-    sys.path = [str(code_path)] + code_subdirs + sys.path
-
-    return torch.load(maybe_model, pickle_module=cloudpickle.pickle, **kwargs)  # type: ignore
+    return model
