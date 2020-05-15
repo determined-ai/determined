5fa952ee6579b4dfbfd69c48dece2d196b08b415
Caleb Kang <caleb@determined.ai>
2020-05-15 13:56:04 -0600

    feat: add user with password and user without password to tests

--- webui/tests/bin/createUserAndExperiments.py
+++ webui/tests/bin/createUserAndExperiments.py
@@ -4,8 +4,9 @@ import pathlib
 import pexpect
 import subprocess
 
-test_username = "userwithpw"
-test_password = "specialpw"
+USER_WITH_PASSWORD_USERNAME = "user-w-pw"
+USER_WITH_PASSWORD_PASSWORD = "special-pw"
+USER_WITHOUT_PASSWORD_USERNAME = "user-wo-pw"
 
 
 def wait_for_process(process):
@@ -21,28 +22,38 @@ def wait_for_process(process):
         process.wait()
 
 
-# Create a non-default user for testing purposes. The CLI reads a password using `getpass`, which
-# reads from `/dev/tty`; Pexpect is a portable way to simulate the TTY input. (On Linux, `echo |
-# setsid -w determined ...` would work (https://unix.stackexchange.com/a/68591),
-# but the `setsid` command does not seem to be generally available on macOS.)
-p = pexpect.spawn("det", ["-u", "admin", "user", "create", test_username])
-p.expect("Password.*:")
-p.sendline("")
-wait_for_process(p)
+def login_as(username, password=""):
+    p = pexpect.spawn("det", ["user", "login", username])
+    p.expect("Password.*:")
+    p.sendline(password)
+    wait_for_process(p)
 
-# Change the password for the newly create user
-p = pexpect.spawn("det", ["-u", "admin", "user", "change-password", test_username])
-p.expect("New password.*:")
-p.sendline(test_password)
-p.expect("Confirm password:")
-p.sendline(test_password)
-wait_for_process(p)
 
-# Log into CLI as the newly created user
-p = pexpect.spawn("det", ["user", "login", test_username])
-p.expect("Password.*:")
-p.sendline(test_password)
-wait_for_process(p)
+def create_user(username, password=""):
+    p = pexpect.spawn("det", ["-u", "admin", "user", "create", username])
+    p.expect(pexpect.EOF)
+    wait_for_process(p)
+
+    if password != "":
+        p = pexpect.spawn("det", ["-u", "admin", "user", "change-password", username])
+        p.expect("New password.*:")
+        p.sendline(password)
+        p.expect("Confirm password:")
+        p.sendline(password)
+        wait_for_process(p)
+
+
+# First login as admin to avoid having to authenticate downstream
+login_as("admin")
+
+# Create a non-default user without a password
+create_user(USER_WITHOUT_PASSWORD_USERNAME)
+
+# Create a non-default user with a password
+create_user(USER_WITH_PASSWORD_USERNAME, USER_WITH_PASSWORD_PASSWORD)
+
+# Login as non-default user with password
+login_as(USER_WITH_PASSWORD_USERNAME, USER_WITH_PASSWORD_PASSWORD)
 
 # Create experiments
 determined_root_dir = pathlib.Path(__file__).absolute().parents[3]
