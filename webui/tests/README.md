33cf89e5f6a1cdfaa0826f18a063291e68cc3ae5
Hamid Zare <hamid@determined.ai>
2020-05-21 16:32:36 -0700

    wip

---
+++ webui/tests/README.md
@@ -0,0 +1,3 @@
+# WebUI End-to-end Tests
+
+Use 127.0.0.1. cookie collision with localhost?
--- webui/tests/bin/e2e-tests.py
+++ webui/tests/bin/e2e-tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import logging
+import time
 import os
 import pathlib
 import signal
@@ -14,7 +15,9 @@ DOCKER_CYPRESS_IMAGE = "cypress/included:4.3.0"
 RESULTS_DIR_NAME = "results"
 logger = logging.getLogger("e2e-tests")
 
-root = subprocess.check_output(["git", "rev-parse", "--show-toplevel"], encoding="utf-8")[:-1]
+root = subprocess.check_output(
+    ["git", "rev-parse", "--show-toplevel"], encoding="utf-8"
+)[:-1]
 root_path = pathlib.Path(root)
 webui_dir = root_path.joinpath("webui")
 tests_dir = webui_dir.joinpath("tests")
@@ -26,8 +29,10 @@ def run(cmd: List[str], config) -> None:
     logger.info("+ %s", " ".join(cmd))
     return subprocess.check_call(cmd, env=config["env"])
 
+
 def run_forget(cmd: List[str], config) -> None:
-    return subprocess.Popen(cmd)
+    return subprocess.Popen(cmd, stdout=subprocess.DEVNULL)
+
 
 def run_ignore_failure(cmd: List[str], config):
     try:
@@ -46,9 +51,11 @@ def run_cluster_cmd(subcommand: List[str], detach: bool, config):
 
 def pre_e2e_tests(config):
     run_ignore_failure(["rm", "-r", str(tests_dir.joinpath(RESULTS_DIR_NAME))], config)
-    run(["docker", "pull", DOCKER_CYPRESS_IMAGE], config)
-    run_cluster_cmd( [ "start-db" ], False, config,)
-    cluster_process = run_cluster_cmd( [ "run" ], True, config,)
+    # run(["docker", "pull", DOCKER_CYPRESS_IMAGE], config)
+    run_cluster_cmd(["start-db"], False, config)
+    cluster_process = run_cluster_cmd(["run"], True, config)
+    print("waiting for cluster ready...")
+    time.sleep(6)  # FIXME add a ready check for master
     test_setup_path = tests_dir.joinpath("bin", "createUserAndExperiments.py")
     run(["python", str(test_setup_path)], config)
     print("cluster pid", cluster_process.pid)
@@ -61,14 +68,20 @@ def _cypress_container_name(config):
 
 def post_e2e_tests(config):
     clean_up_cypress(config)
+
     print("TODO kill cluster")
-    run_cluster_cmd(["stop-db"], config)
+    run_ignore_failure(["pkill", "determined"], config)
+    run_ignore_failure(["pkill", "run-server"], config)
+
+    run_cluster_cmd(["stop-db"], False, config)
 
 
 # _cypress_arguments generates an array of cypress arguments.
 def _cypress_arguments(cypress_configs, config, use_docker):
     base_url_config = f"baseUrl=http://{config['DET_MASTER']}"
-    timeout_config = f"defaultCommandTimeout={config['CYPRESS_DEFAULT_COMMAND_TIMEOUT']}"
+    timeout_config = (
+        f"defaultCommandTimeout={config['CYPRESS_DEFAULT_COMMAND_TIMEOUT']}"
+    )
     config_file_name = "cypress-docker.json" if use_docker else "cypress.json"
     args = [
         "--config-file",
@@ -87,20 +100,31 @@ def _cypress_arguments(cypress_configs, config, use_docker):
 
 
 def container_exists(name):
-    return any(filter(lambda container: container.name == name, client.containers.list(all=True)))
+    return any(
+        filter(
+            lambda container: container.name == name, client.containers.list(all=True)
+        )
+    )
 
 
-def clean_up_cypress(config):
-    cypress_name = _cypress_container_name(config)
-    if container_exists(cypress_name):
-        # ensure that the cypress container is stopped and removed
-        run(["docker", "container", "rm", "-f", cypress_name], config)
+# def clean_up_cypress(config):
+#     cypress_name = _cypress_container_name(config)
+#     if container_exists(cypress_name):
+#         # ensure that the cypress container is stopped and removed
+#         run(["docker", "container", "rm", "-f", cypress_name], config)
 
 
 def run_e2e_tests(config):
     cypress_arguments = _cypress_arguments([], config, False)
-    command = ["yarn", "--cwd", str(tests_dir), "run", "cypress", "run",
-            *cypress_arguments]
+    command = [
+        "yarn",
+        "--cwd",
+        str(tests_dir),
+        "run",
+        "cypress",
+        "run",
+        *cypress_arguments,
+    ]
 
     run(
         command, config,
@@ -170,7 +194,7 @@ def get_config(args):
     # config["INTEGRATIONS_RESOURCE_SUFFIX"] = "_webui_tests_" + config["INTEGRATIONS_HOST_PORT"]
     # config["INTEGRATIONS_NETWORK"] = "determined" + config["INTEGRATIONS_RESOURCE_SUFFIX"]
     # config["DET_DOCKER_MASTER_NODE"] = "localhost"
-    config["DET_MASTER"] = f"127.0.0.1:{args.integrations_host_port}"
+    config["DET_MASTER"] = f"localhost:{args.integrations_host_port}"
     config["CYPRESS_DEFAULT_COMMAND_TIMEOUT"] = args.cypress_default_command_timeout
     config["CYPRESS_ARGS"] = args.cypress_args
 
@@ -179,22 +203,24 @@ def get_config(args):
         value = os.environ.get(var)
         if value is not None:
             env[var] = value
-    # env["INTEGRATIONS_HOST_PORT"] = config["INTEGRATIONS_HOST_PORT"]
+    env["INTEGRATIONS_HOST_PORT"] = config["INTEGRATIONS_HOST_PORT"]
     # env["INTEGRATIONS_RESOURCE_SUFFIX"] = config["INTEGRATIONS_RESOURCE_SUFFIX"]
-    # env["DET_MASTER"] = "localhost:" + config["INTEGRATIONS_HOST_PORT"]
-    logging.basicConfig(level=(args.log_level or "INFO"), format=(args.log_format or "%(message)s"))
+    env["DET_MASTER"] = config["DET_MASTER"]
+    logging.basicConfig(
+        level=(args.log_level or "INFO"), format=(args.log_format or "%(message)s")
+    )
     config["env"] = env
     return config
 
 
 def main():
     operation_to_fn = {
-        "docker-run-e2e-tests": docker_run_e2e_tests,
+        # "docker-run-e2e-tests": docker_run_e2e_tests,
         "pre-e2e-tests": pre_e2e_tests,
         "run-e2e-tests": run_e2e_tests,
         "post-e2e-tests": post_e2e_tests,
         "e2e-tests": e2e_tests,
-        "docker-e2e-tests": docker_e2e_tests,
+        # "docker-e2e-tests": docker_e2e_tests,
     }
 
     parser = argparse.ArgumentParser(description="Manage e2e tests.")
