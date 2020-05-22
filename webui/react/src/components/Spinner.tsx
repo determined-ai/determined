133a0d3589f795f0385ba7ea377c25690763263a
Caleb Kang <caleb@determined.ai>
2020-05-22 00:35:31 -0600

    style: add additional style props to spinner to prep for log viewer

--- webui/react/src/components/Spinner.tsx
+++ webui/react/src/components/Spinner.tsx
@@ -5,15 +5,19 @@ import Icon from 'components/Icon';
 import css from './Spinner.module.scss';
 
 interface Props {
+  fillContainer?: boolean;
   fullPage?: boolean;
   opaque?: boolean;
+  shade?: boolean;
 }
 
 const Spinner: React.FC<Props> = (props: Props) => {
   const classes = [ css.base ];
 
+  if (props.fillContainer) classes.push(css.fillContainer);
   if (props.fullPage) classes.push(css.fullPage);
   if (props.opaque) classes.push(css.opaque);
+  if (props.shade) classes.push(css.shade);
 
   return (
     <div className={classes.join(' ')}>
