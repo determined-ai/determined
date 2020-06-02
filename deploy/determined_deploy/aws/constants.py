a5c22d06828ae6e7db9fda52df794237af638c2f
Shiyuan Zhu <shiyuan@determined.ai>
2020-06-02 15:42:07 -0700

    chore: update aws deployment tool with agent starting perioo

--- deploy/determined_deploy/aws/constants.py
+++ deploy/determined_deploy/aws/constants.py
@@ -34,6 +34,7 @@ class cloudformation:
     BOTO3_SESSION = "Boto3Session"
     AGENT_TAG_NAME = "AgentTagName"
     MAX_IDLE_AGENT_PERIOD = "MaxIdleAgentPeriod"
+    MAX_AGENT_STARTING_PERIOD = "MaxAgentStartingPeriod"
     MAX_DYNAMIC_AGENTS = "MaxDynamicAgents"
     LOG_GROUP = "LogGroup"
     REGION = "Region"
