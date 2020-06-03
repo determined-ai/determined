80e39d0559b365d40e97b96347e4ff01aed87124
Hamid Zare <hamidzr@users.noreply.github.com>
2020-06-03 12:28:48 -0700

    feat: add antd breadcrumb stories [DET-3002] (#582)

---
+++ webui/react/src/components/Breadcrumb.stories.tsx
@@ -0,0 +1,40 @@
+import { Breadcrumb, Space } from 'antd';
+import React from 'react';
+
+import Icon from 'components/Icon';
+import Link from 'components/Link';
+
+export default {
+  component: Breadcrumb,
+  title: 'Breadcrumb',
+};
+
+export const Default = (): React.ReactNode => (
+  <Breadcrumb>
+    <Breadcrumb.Item>
+      <Space align="center" size="small">
+        <Icon name="experiment" size="small" />
+        <Link path="/ui/experiments">Experiments</Link>
+      </Space>
+    </Breadcrumb.Item>
+    <Breadcrumb.Item>
+      <Link path="/ui/experiments/3">Experiment 3</Link>
+    </Breadcrumb.Item>
+  </Breadcrumb>
+);
+export const TrialDetail = (): React.ReactNode => (
+  <Breadcrumb>
+    <Breadcrumb.Item>
+      <Space align="center" size="small">
+        <Icon name="experiment" size="small" />
+        <Link path="/ui/experiments">Experiments</Link>
+      </Space>
+    </Breadcrumb.Item>
+    <Breadcrumb.Item>
+      <Link path="/ui/experiments/3"> Experiment 3</Link>
+    </Breadcrumb.Item>
+    <Breadcrumb.Item>
+      <Link path="/ui/experiments/3/trials/34">Trial 34</Link>
+    </Breadcrumb.Item>
+  </Breadcrumb>
+);
