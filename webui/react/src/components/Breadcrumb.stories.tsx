bcd5ccc2375a4f260b24438e85ccd7e76cb08932
Hamid Zare <hamid@determined.ai>
2020-06-03 10:09:25 -0700

    remove breadcrumb component.

--- webui/react/src/components/Breadcrumb.stories.tsx
+++ webui/react/src/components/Breadcrumb.stories.tsx
@@ -3,21 +3,12 @@ import React from 'react';
 
 import Icon from 'components/Icon';
 import Link from 'components/Link';
-import { appRoutes, RouteConfigItem } from 'routes';
-
-// import Breadcrumb from './Breadcrumb';
 
 export default {
   component: Breadcrumb,
   title: 'Breadcrumb',
 };
 
-// export const Default = (): React.ReactNode =>
-//   <Breadcrumb route={appRoutes.find(route => route.id === 'dashboard') as RouteConfigItem} />;
-// export const Experiments = (): React.ReactNode =>
-//   <Breadcrumb route={{ path: '/det/dashboard/experiments' } as RouteConfigItem} />;
-// export const ExperimentDetail = (): React.ReactNode =>
-//   <Breadcrumb route={{ path: '/det/dashboard/experiments/1' } as RouteConfigItem} />;
 export const Default = (): React.ReactNode => (
   <Breadcrumb>
     <Breadcrumb.Item>
