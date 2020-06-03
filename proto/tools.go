43e352c53e7a51bbc1365bad53d31ee0816a7030
Jonathan Ben-tzur <yoni@determined.ai>
2020-06-03 09:42:44 -0700

    chore: upgrade proto libraries

--- proto/tools.go
+++ proto/tools.go
@@ -4,8 +4,8 @@ package tools
 
 import (
 	_ "github.com/bufbuild/buf/cmd/buf"
-	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
 	_ "google.golang.org/grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
