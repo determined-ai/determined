a8c1bb2f84e101d20a732d963997b284d6e9c8bc
Yoni Ben-tzur <yoni@determined.ai>
2020-06-03 09:22:16 -0700

    feat: add master endpoint to new api (#627)

---
+++ master/internal/api_master.go
@@ -0,0 +1,27 @@
+package internal
+
+import (
+	"context"
+
+	structpb "github.com/golang/protobuf/ptypes/struct"
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/encoding/protojson"
+
+	"github.com/determined-ai/determined/proto/pkg/apiv1"
+)
+
+func (a *apiServer) GetMaster(
+	_ context.Context, _ *apiv1.GetMasterRequest) (*apiv1.GetMasterResponse, error) {
+	config, err := a.m.config.Printable()
+	if err != nil {
+		return nil, errors.Wrap(err, "error parsing master config")
+	}
+	configStruct := &structpb.Struct{}
+	err = protojson.Unmarshal(config, configStruct)
+	return &apiv1.GetMasterResponse{
+		Version:   a.m.Version,
+		MasterId:  a.m.MasterID,
+		ClusterId: a.m.ClusterID,
+		Config:    configStruct,
+	}, err
+}
