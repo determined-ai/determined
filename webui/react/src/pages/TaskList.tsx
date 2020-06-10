b2eafd514827db83c9594e01793a2bf17352eaee
Hamid Zare <hamid@determined.ai>
2020-06-09 17:22:42 -0700

    remove experiments from task list

--- webui/react/src/pages/TaskList.tsx
+++ webui/react/src/pages/TaskList.tsx
@@ -3,15 +3,13 @@ import React from 'react';
 import Page from 'components/Page';
 import TaskTable from 'components/TasksTable';
 import { Commands, Notebooks, Shells, Tensorboards } from 'contexts/Commands';
-import Experiments from 'contexts/Experiments';
-import { commandToTask, experimentToTask } from 'utils/types';
+import { commandToTask } from 'utils/types';
 
 const TaskList: React.FC = () => {
   const commands = Commands.useStateContext();
   const notebooks = Notebooks.useStateContext();
   const shells = Shells.useStateContext();
   const tensorboards = Tensorboards.useStateContext();
-  const experiments = Experiments.useStateContext();
 
   const genericCommands = [
     ...(commands.data || []),
@@ -21,7 +19,6 @@ const TaskList: React.FC = () => {
   ];
 
   const loadedTasks = [
-    ...(experiments.data || []).map(experimentToTask),
     ...genericCommands.map(commandToTask),
   ];
 
