4b26ce7b445e8c632baac7c1edf9c22f55e5aa7d
katport <57640100+katport@users.noreply.github.com>
2020-06-11 08:40:27 -0700

    fix: fix nightly tests file locations (#698)

--- e2e_tests/tests/nightly/test_capability.py
+++ e2e_tests/tests/nightly/test_capability.py
@@ -9,7 +9,7 @@ def test_nas_search() -> None:
     config = conf.load_config(conf.experimental_path("trial/nas_search/train_one_arch.yaml"))
     config = conf.set_max_steps(config, 2)
 
-    exp.run_basic_test_with_temp_config(config, conf.experimental_path("nas_search"), 1)
+    exp.run_basic_test_with_temp_config(config, conf.experimental_path("trial/nas_search"), 1)
 
 
 @pytest.mark.nightly  # type: ignore
@@ -17,7 +17,9 @@ def test_bert_glue() -> None:
     config = conf.load_config(conf.experimental_path("trial/bert_glue_pytorch/const.yaml"))
     config = conf.set_max_steps(config, 2)
 
-    exp.run_basic_test_with_temp_config(config, conf.experimental_path("bert_glue_pytorch/"), 1)
+    exp.run_basic_test_with_temp_config(
+        config, conf.experimental_path("trial/bert_glue_pytorch/"), 1
+    )
 
 
 @pytest.mark.nightly  # type: ignore
@@ -27,7 +29,7 @@ def test_faster_rcnn() -> None:
     config = conf.set_slots_per_trial(config, 1)
 
     exp.run_basic_test_with_temp_config(
-        config, conf.experimental_path("FasterRCNN_tp"), 1, max_wait_secs=4800
+        config, conf.experimental_path("trial/FasterRCNN_tp"), 1, max_wait_secs=4800
     )
 
 
@@ -36,7 +38,9 @@ def test_mnist_tp_to_estimator() -> None:
     config = conf.load_config(conf.experimental_path("trial/mnist_tp_to_estimator/const.yaml"))
     config = conf.set_max_steps(config, 2)
 
-    exp.run_basic_test_with_temp_config(config, conf.experimental_path("mnist_tp_to_estimator"), 1)
+    exp.run_basic_test_with_temp_config(
+        config, conf.experimental_path("trial/mnist_tp_to_estimator"), 1
+    )
 
 
 @pytest.mark.nightly  # type: ignore
@@ -44,7 +48,9 @@ def test_resnet50() -> None:
     config = conf.load_config(conf.experimental_path("trial/resnet50_tf_keras/const.yaml"))
     config = conf.set_max_steps(config, 2)
 
-    exp.run_basic_test_with_temp_config(config, conf.experimental_path("resnet50_tf_keras"), 1)
+    exp.run_basic_test_with_temp_config(
+        config, conf.experimental_path("trial/resnet50_tf_keras"), 1
+    )
 
 
 @pytest.mark.nightly  # type: ignore
@@ -53,7 +59,7 @@ def test_mnist_pytorch_multi_output() -> None:
     config = conf.set_max_steps(config, 2)
 
     exp.run_basic_test_with_temp_config(
-        config, conf.experimental_path("mnist_pytorch_multi_output"), 1
+        config, conf.experimental_path("trial/mnist_pytorch_multi_output"), 1
     )
 
 
@@ -63,5 +69,5 @@ def test_imagenet_nas() -> None:
     config = conf.set_max_steps(config, 2)
 
     exp.run_basic_test_with_temp_config(
-        config, conf.experimental_path("imagenet_nas_arch_pytorch"), 1
+        config, conf.experimental_path("trial/imagenet_nas_arch_pytorch"), 1
     )
--- e2e_tests/tests/nightly/test_convergence.py
+++ e2e_tests/tests/nightly/test_convergence.py
@@ -8,7 +8,7 @@ from tests import experiment as exp
 def test_cifar10_pytorch_accuracy() -> None:
     config = conf.load_config(conf.official_examples_path("trial/cifar10_cnn_pytorch/const.yaml"))
     experiment_id = exp.run_basic_test_with_temp_config(
-        config, conf.official_examples_path("cifar10_cnn_pytorch"), 1
+        config, conf.official_examples_path("trial/cifar10_cnn_pytorch"), 1
     )
 
     trials = exp.experiment_trials(experiment_id)
@@ -33,7 +33,7 @@ def test_cifar10_pytorch_accuracy() -> None:
 def test_mnist_tp_accuracy() -> None:
     config = conf.load_config(conf.official_examples_path("trial/mnist_tp/const.yaml"))
     experiment_id = exp.run_basic_test_with_temp_config(
-        config, conf.official_examples_path("mnist_tp"), 1
+        config, conf.official_examples_path("trial/mnist_tp"), 1
     )
 
     trials = exp.experiment_trials(experiment_id)
@@ -62,7 +62,7 @@ def test_mnist_tp_accuracy() -> None:
 def test_mnist_estimator_accuracy() -> None:
     config = conf.load_config(conf.official_examples_path("trial/mnist_estimator/const.yaml"))
     experiment_id = exp.run_basic_test_with_temp_config(
-        config, conf.official_examples_path("mnist_estimator"), 1
+        config, conf.official_examples_path("trial/mnist_estimator"), 1
     )
 
     trials = exp.experiment_trials(experiment_id)
@@ -87,7 +87,7 @@ def test_mnist_estimator_accuracy() -> None:
 def test_mnist_pytorch_accuracy() -> None:
     config = conf.load_config(conf.official_examples_path("trial/mnist_pytorch/const.yaml"))
     experiment_id = exp.run_basic_test_with_temp_config(
-        config, conf.official_examples_path("mnist_pytorch"), 1
+        config, conf.official_examples_path("trial/mnist_pytorch"), 1
     )
 
     trials = exp.experiment_trials(experiment_id)
@@ -115,7 +115,7 @@ def test_object_detection_accuracy() -> None:
     )
     config = conf.set_random_seed(config, 1590497309)
     experiment_id = exp.run_basic_test_with_temp_config(
-        config, conf.official_examples_path("object_detection_pytorch"), 1
+        config, conf.official_examples_path("trial/object_detection_pytorch"), 1
     )
 
     trials = exp.experiment_trials(experiment_id)
@@ -141,7 +141,7 @@ def test_iris_tf_keras() -> None:
     config = conf.load_config(conf.official_examples_path("trial/iris_tf_keras/const.yaml"))
     config = conf.set_random_seed(config, 1591280374)
     experiment_id = exp.run_basic_test_with_temp_config(
-        config, conf.official_examples_path("iris_tf_keras"), 1
+        config, conf.official_examples_path("trial/iris_tf_keras"), 1
     )
 
     trials = exp.experiment_trials(experiment_id)
@@ -169,7 +169,7 @@ def test_fashion_mnist_tf_keras() -> None:
     )
     config = conf.set_random_seed(config, 1591110586)
     experiment_id = exp.run_basic_test_with_temp_config(
-        config, conf.official_examples_path("fashion_mnist_tf_keras"), 1
+        config, conf.official_examples_path("trial/fashion_mnist_tf_keras"), 1
     )
 
     trials = exp.experiment_trials(experiment_id)
