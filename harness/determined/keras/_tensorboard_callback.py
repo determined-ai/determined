4a349a35541258c0e39fc7b733fa148e8cef9392
Neil Conway <neil@determined.ai>
2020-06-11 17:45:52 -0700

    docs: minor fixes for TensorBoard docs (#700)
    Also make a few tweaks elsewhere.

--- harness/determined/keras/_tensorboard_callback.py
+++ harness/determined/keras/_tensorboard_callback.py
@@ -6,14 +6,23 @@ from determined import tensorboard
 
 
 class TFKerasTensorBoard(tf.keras.callbacks.TensorBoard):  # type: ignore
+    """
+    This is a thin wrapper over the TensorBoard callback that ships with ``tf.keras``.  For more
+    information, see the :ref:`TensorBoard Guide <how-to-tensorboard>` or the upstream docs for
+    `tf.keras.callbacks.TensorBoard
+    <https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/TensorBoard>`__.
+
+    Note that if a ``log_dir`` argument is passed to the constructor, it will be ignored.
+    """
+
     def __init__(self, *args: Any, **kwargs: Any):
         log_dir = str(tensorboard.get_base_path({}).resolve())
         super().__init__(log_dir=log_dir, *args, **kwargs)
 
     def _write_logs(self, *args: Any) -> None:
         """
-        _write_logs calls the original write logs function from the keras
-        TensorBoard callback. After the logs are flushed to disk we close and
+        _write_logs calls the original _write_logs() function from the Keras
+        TensorBoard callback. After the logs are flushed to disk, we close and
         reopen the tf event writer so that it serializes the next set of logs
         to a new file. This allows the tensorboard manager to treat the
         written files as immutable and upload them to persistent storage
