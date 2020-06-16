aab299e43a86dc18f9b4ac2a6897ca3cec9d6053
hoang <hoang@determined.ai>
2020-06-16 16:27:12 -0700

    feat: add GBT TF Estimator example

--- e2e_tests/tests/nightly/test_capability.py
+++ e2e_tests/tests/nightly/test_capability.py
@@ -71,3 +71,11 @@ def test_imagenet_nas() -> None:
     exp.run_basic_test_with_temp_config(
         config, conf.experimental_path("trial/imagenet_nas_arch_pytorch"), 1
     )
+
+
+@pytest.mark.nightly  # type: ignore
+def test_gbt_estimator() -> None:
+    config = conf.load_config(conf.experimental_path("trial/gbt_estimator/const.yaml"))
+    config = conf.set_max_steps(config, 2)
+
+    exp.run_basic_test_with_temp_config(config, conf.experimental_path("trial/gbt_estimator"), 1)
