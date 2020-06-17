7391e599491bc17c631ba00d6a03235e0d46eaf1
Hamid Zare <hamid@determined.ai>
2020-06-17 11:39:54 -0700

    change the title of the wording.

--- webui/react/src/components/TaskActionDropdown.tsx
+++ webui/react/src/components/TaskActionDropdown.tsx
@@ -107,7 +107,7 @@ const TaskActionDropdown: React.FC<Props> = ({ task }: Props) => {
 
   if (menuItems.length === 0) {
     return (
-      <div className={css.base} title="No applicable action items" onClick={stopPropagation}>
+      <div className={css.base} title="No actions available" onClick={stopPropagation}>
         <button disabled>
           <Icon name="overflow-vertical" />
         </button>
