6823c5274d325b31977403b47355eaaf2b10b6bc
Caleb Kang <caleb@determined.ai>
2020-06-18 17:10:47 -0600

    refactor: abstract select filter

--- webui/react/src/components/UserSelectFilter.tsx
+++ webui/react/src/components/UserSelectFilter.tsx
@@ -6,8 +6,7 @@ import Auth from 'contexts/Auth';
 import Users from 'contexts/Users';
 import { User } from 'types';
 
-import Icon from './Icon';
-import css from './UserSelectFilter.module.scss';
+import SelectFilter, { ALL_VALUE } from './SelectFilter';
 
 const { Option } = Select;
 
@@ -16,18 +15,12 @@ interface Props {
   value?: SelectValue;
 }
 
-export const ALL_VALUE = 'all';
-
 const userToSelectOption = (user: User): React.ReactNode =>
   <Option key={user.id} value={user.username}>{user.username}</Option>;
 
 const UserSelectFilter: React.FC<Props> = ({ onChange, value }: Props) => {
   const auth = Auth.useStateContext();
   const users = Users.useStateContext();
-
-  const handleFilter = useCallback((search: string, option) => {
-    return option.props.children.indexOf(search) !== -1;
-  }, []);
 
   const handleSelect = useCallback((newValue: SelectValue) => {
     const singleValue = Array.isArray(newValue) ? newValue[0] : newValue;
@@ -54,20 +47,13 @@ const UserSelectFilter: React.FC<Props> = ({ onChange, value }: Props) => {
   }, [ auth.user, users.data ]);
 
   return (
-    <div className={css.base}>
-      <div className={css.label}>Users</div>
-      <Select
-        defaultValue={value || ALL_VALUE}
-        dropdownMatchSelectWidth={false}
-        filterOption={handleFilter}
-        optionFilterProp="children"
-        showSearch={true}
-        style={{ width: '10rem' }}
-        suffixIcon={<Icon name="arrow-down" size="tiny" />}
-        onSelect={handleSelect}>
-        {options}
-      </Select>
-    </div>
+    <SelectFilter
+      label="Users"
+      style={{ width: '10rem' }}
+      value={value || ALL_VALUE}
+      onSelect={handleSelect}>
+      {options}
+    </SelectFilter>
   );
 };
 
