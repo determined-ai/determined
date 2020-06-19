f06e1b99f522543b734a8640ba376dcac1fcfa81
Jonathan Ben-tzur <yoni@determined.ai>
2020-06-19 13:58:57 -0700

    clean up sort and filter

--- master/internal/api.go
+++ master/internal/api.go
@@ -1,10 +1,15 @@
 package internal
 
 import (
+	"fmt"
 	"reflect"
+	"sort"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+	pref "google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/pkg/errors"
 
@@ -17,12 +22,12 @@ type apiServer struct {
 	m *Master
 }
 
-func (a *apiServer) pagination(values interface{}, offset, limit int32) (*apiv1.Pagination, error) {
+func (a *apiServer) paginate(p **apiv1.Pagination, values interface{}, offset, limit int32) error {
 	rv := reflect.ValueOf(values)
-	if rv.Kind() != reflect.Slice {
-		return nil, errors.Errorf("error paginating non-slice type: %T", rv.Kind())
+	if rv.Elem().Kind() != reflect.Slice {
+		return errors.Errorf("error paginating non-slice type: %T", rv.Kind())
 	}
-	total := int32(rv.Len())
+	total := int32(rv.Elem().Len())
 	startIndex := offset
 	if offset < 0 {
 		startIndex = total + offset
@@ -31,16 +36,132 @@ func (a *apiServer) pagination(values interface{}, offset, limit int32) (*apiv1.
 	if limit == 0 || endIndex > total {
 		endIndex = total
 	}
-	err := grpc.ValidateRequest(
-		func() (bool, string) { return 0 <= startIndex && startIndex <= total, "offset out of bounds" },
-	)
-	return &apiv1.Pagination{
+	*p = &apiv1.Pagination{
 		Offset:     offset,
 		Limit:      limit,
 		StartIndex: startIndex,
 		EndIndex:   endIndex,
 		Total:      total,
-	}, err
+	}
+	rv.Elem().Set(rv.Elem().Slice(int(startIndex), int(endIndex)))
+	return grpc.ValidateRequest(
+		func() (bool, string) { return 0 <= startIndex && startIndex <= total, "offset out of bounds" },
+	)
+}
+
+func (a *apiServer) sort(
+	slice interface{}, order apiv1.OrderBy, keys ...interface{}) {
+	rv := reflect.ValueOf(slice)
+	sort.Slice(slice, func(i, j int) bool {
+		a1 := rv.Index(i).Interface().(proto.Message).ProtoReflect()
+		a2 := rv.Index(j).Interface().(proto.Message).ProtoReflect()
+		if order == apiv1.OrderBy_ORDER_BY_DESC {
+			a2, a1 = a1, a2
+		}
+		d1, d2 := a1.Descriptor(), a2.Descriptor()
+		for _, key := range keys {
+			key := reflect.ValueOf(key).Int()
+			if key == 0 {
+				continue
+			}
+			fn := pref.FieldNumber(key)
+			fd1, fd2 := d1.Fields().ByNumber(fn), d2.Fields().ByNumber(fn)
+			f1, f2 := a1.Get(fd1), a2.Get(fd2)
+
+			if fd1.Cardinality() == pref.Repeated {
+				panic(fmt.Sprintf("incomparable cardinality for field: %s", fd1.FullName()))
+			}
+			switch fd1.Kind() {
+			case pref.BoolKind:
+				v1, v2 := f1.Bool(), f2.Bool()
+				if v1 == v2 {
+					continue
+				}
+				return v1
+			case pref.EnumKind:
+				v1, v2 := f1.Enum(), f2.Enum()
+				if v1 == v2 {
+					continue
+				}
+				return v1 < v2
+			case pref.Int32Kind, pref.Sint32Kind, pref.Int64Kind,
+				pref.Sint64Kind, pref.Sfixed32Kind, pref.Sfixed64Kind:
+				v1, v2 := f1.Int(), f2.Int()
+				if v1 == v2 {
+					continue
+				}
+				return v1 < v2
+			case pref.Uint32Kind, pref.Uint64Kind, pref.Fixed32Kind, pref.Fixed64Kind:
+				v1, v2 := f1.Uint(), f2.Uint()
+				if v1 == v2 {
+					continue
+				}
+				return v1 < v2
+			case pref.FloatKind, pref.DoubleKind:
+				v1, v2 := f1.Float(), f2.Float()
+				if v1 == v2 {
+					continue
+				}
+				return v1 < v2
+			case pref.StringKind:
+				v1, v2 := f1.String(), f2.String()
+				if v1 == v2 {
+					continue
+				}
+				return v1 < v2
+			case pref.MessageKind:
+				v1, v2 := f1.Message().Interface(), f2.Message().Interface()
+				switch t1 := v1.(type) {
+				case *timestamppb.Timestamp:
+					t2 := v2.(*timestamppb.Timestamp)
+					if t1.Seconds == t2.Seconds {
+						if t1.Nanos == t2.Nanos {
+							continue
+						}
+						return t1.Nanos < t2.Nanos
+					}
+					return t1.Seconds < t2.Seconds
+				default:
+					panic(fmt.Sprintf("incomparable message type: %T", t1))
+				}
+			default:
+				panic(fmt.Sprintf("incomparable field type for %s: %s", fd1.FullName(), fd1.Kind()))
+			}
+		}
+		return false
+	})
+}
+
+func checkIn(v interface{}, values interface{}) func() bool {
+	rv := reflect.ValueOf(values)
+	return func() bool {
+		for i := 0; i < rv.Len(); i++ {
+			if v == rv.Index(i).Interface() {
+				return true
+			}
+		}
+		return rv.Len() == 0
+	}
+}
+
+func filterAll(filters ...func() bool) bool {
+	for _, filter := range filters {
+		if !filter() {
+			return false
+		}
+	}
+	return true
+}
+
+func (a *apiServer) filter(values interface{}, check func(int) bool) {
+	rv := reflect.ValueOf(values)
+	results := reflect.MakeSlice(rv.Type().Elem(), 0, 0)
+	for i := 0; i < rv.Elem().Len(); i++ {
+		if check(i) {
+			results = reflect.Append(results, rv.Elem().Index(i))
+		}
+	}
+	rv.Elem().Set(results)
 }
 
 func (a *apiServer) actorRequest(addr string, req actor.Message, v interface{}) error {
--- master/internal/api_experiment.go
+++ master/internal/api_experiment.go
@@ -2,97 +2,29 @@ package internal
 
 import (
 	"context"
-	"fmt"
-	"sort"
 	"strings"
 
 	"github.com/determined-ai/determined/proto/pkg/apiv1"
-	"github.com/determined-ai/determined/proto/pkg/experimentv1"
 )
 
 func (a *apiServer) GetExperiments(
 	_ context.Context, req *apiv1.GetExperimentsRequest) (*apiv1.GetExperimentsResponse, error) {
 	resp := &apiv1.GetExperimentsResponse{}
-	err := a.m.db.QueryProto("get_experiments", &resp.Experiments)
-	if err != nil {
+	if err := a.m.db.QueryProto("get_experiments", &resp.Experiments); err != nil {
 		return nil, err
 	}
-	var filtered []*experimentv1.Experiment
-	for _, exp := range resp.Experiments {
-		if filterExperiments(req, exp) {
-			filtered = append(filtered, exp)
-		}
-	}
-	resp.Experiments = filtered
-	sort.Slice(resp.Experiments, func(i, j int) bool {
-		a1, a2 := resp.Experiments[i], resp.Experiments[j]
-		if req.OrderBy == apiv1.OrderBy_ORDER_BY_DESC {
-			a1, a2 = a2, a1
-		}
-		switch req.SortBy {
-		case apiv1.GetExperimentsRequest_SORT_BY_ID, apiv1.GetExperimentsRequest_SORT_BY_UNSPECIFIED:
-			return a1.Id < a2.Id
-		case apiv1.GetExperimentsRequest_SORT_BY_DESCRIPTION:
-			return a1.Description < a2.Description
-		case apiv1.GetExperimentsRequest_SORT_BY_START_TIME:
-			return a1.StartTime.Seconds < a2.StartTime.Seconds
-		case apiv1.GetExperimentsRequest_SORT_BY_END_TIME:
-			switch {
-			case a1.EndTime == nil && a2.EndTime == nil:
-				return a1.Id < a2.Id
-			case a1.EndTime == nil:
-				return false
-			case a2.EndTime == nil:
-				return true
-			default:
-				return a1.EndTime.Seconds < a2.EndTime.Seconds
-			}
-		case apiv1.GetExperimentsRequest_SORT_BY_NUM_TRIALS:
-			return a1.NumTrials < a2.NumTrials
-		case apiv1.GetExperimentsRequest_SORT_BY_STATE:
-			return a1.State < a2.State
-		case apiv1.GetExperimentsRequest_SORT_BY_PROGRESS:
-			return a1.Progress < a2.Progress
-		case apiv1.GetExperimentsRequest_SORT_BY_USER:
-			return a1.Username < a2.Username
-		default:
-			panic(fmt.Sprintf("unknown sort type specified: %s", req.SortBy))
-		}
+	a.filter(&resp.Experiments, func(i int) bool {
+		v := resp.Experiments[i]
+		return filterAll(
+			func() bool { return req.Archived == nil || req.Archived.Value == v.Archived },
+			func() bool {
+				return strings.Contains(
+					strings.ToLower(v.Description), strings.ToLower(req.Description))
+			},
+			checkIn(v.State, req.States),
+			checkIn(v.Username, req.Users),
+		)
 	})
-	resp.Pagination, err = a.pagination(resp.Experiments, req.Offset, req.Limit)
-	if err != nil {
-		return nil, err
-	}
-	resp.Experiments = resp.Experiments[resp.Pagination.StartIndex:resp.Pagination.EndIndex]
-	return resp, nil
-}
-
-func filterExperiments(req *apiv1.GetExperimentsRequest, exp *experimentv1.Experiment) bool {
-	if req.Archived != nil && req.Archived.Value != exp.Archived {
-		return false
-	}
-	if len(req.States) > 0 {
-		found := false
-		for _, state := range req.States {
-			if exp.State == state {
-				found = true
-			}
-		}
-		if !found {
-			return false
-		}
-	}
-	if len(req.Users) > 0 {
-		found := false
-		for _, user := range req.Users {
-			if exp.Username == user {
-				found = true
-			}
-		}
-		if !found {
-			return false
-		}
-		return false
-	}
-	return strings.Contains(strings.ToLower(exp.Description), strings.ToLower(req.Description))
+	a.sort(resp.Experiments, req.OrderBy, req.SortBy, apiv1.GetExperimentsRequest_SORT_BY_ID)
+	return resp, a.paginate(&resp.Pagination, &resp.Experiments, req.Offset, req.Limit)
 }
