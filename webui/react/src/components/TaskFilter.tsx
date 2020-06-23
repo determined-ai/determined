ea4f4f18d9f3c97e592f47152bbe3f755c582240
Caleb Hoyoul Kang <caleb@determined.ai>
2020-06-23 17:15:33 -0600

    refactor: abstract task filters to be reusable (#748)

--- webui/react/src/components/TaskFilter.tsx
+++ webui/react/src/components/TaskFilter.tsx
@@ -2,145 +2,112 @@ import { Select } from 'antd';
 import { SelectValue } from 'antd/es/select';
 import React, { useCallback, useMemo } from 'react';
 
-import Icon from 'components/Icon';
 import { CommandState, CommandType, RecentTask, RunState, TaskType, User } from 'types';
 import { capitalize } from 'utils/string';
 import { isExperimentTask } from 'utils/task';
-import { commandStateToLabel, runStateToLabel } from 'utils/types';
 
 import IconFilterButtons from './IconFilterButtons';
+import SelectFilter, { ALL_VALUE } from './SelectFilter';
+import StateSelectFilter from './StateSelectFilter';
 import css from './TaskFilter.module.scss';
+import UserSelectFilter from './UserSelectFilter';
 
-const { Option, OptGroup } = Select;
+const { Option } = Select;
 
-export const ALL_VALUE = 'all';
+export { ALL_VALUE };
 
-export interface TaskFilters {
+export interface TaskFilters<T extends TaskType = TaskType> {
   limit: number;
   states: string[];
   username?: string;
-  types: Record<TaskType, boolean>;
+  types: Record<T, boolean>;
 }
 
 interface Props {
   filters: TaskFilters;
   onChange: (filters: TaskFilters) => void;
-  authUser?: User;
-  users: User[];
+  showExperiments?: boolean;
+  showLimit?: boolean;
 }
+
+const defaultProps = {
+  showExperiments: true,
+  showLimit: true,
+};
 
 const limitOptions: number[] = [ 10, 25, 50 ];
 
-const taskTypeConfig = [
-  { id: 'Experiment' },
+const commandConfig = [
   { id: CommandType.Notebook },
   { id: CommandType.Tensorboard },
   { id: CommandType.Shell },
   { id: CommandType.Command },
 ];
+const experimentConfig = [ { id: 'Experiment' } ];
 
-const selectIcon = <Icon name="arrow-down" size="tiny" />;
+const TaskFilter: React.FC<Props> = ({
+  filters, onChange, showExperiments, showLimit,
+}: Props) => {
+  const allTypesOff = !Object.values(filters.types).reduce((acc, type) => (acc || type), false);
+  const showCommandStates = allTypesOff ||
+    filters.types.COMMAND || filters.types.NOTEBOOK ||
+    filters.types.SHELL || filters.types.TENSORBOARD;
+  const showExperimentStates = showExperiments && (allTypesOff || filters.types.Experiment);
 
-const TaskFilter: React.FC<Props> = ({ authUser, filters, onChange, users }: Props) => {
   const handleTypeClick = useCallback((id: string) => {
     const idAsType = id as TaskType;
     const types = { ...filters.types, [idAsType]: !filters.types[idAsType] };
     onChange({ ...filters, types });
   }, [ filters, onChange ]);
 
-  const handleStateSelect = useCallback((value: SelectValue): void => {
+  const handleStateChange = useCallback((value: SelectValue): void => {
     if (typeof value !== 'string') return;
     onChange({ ...filters, states: [ value ] });
   }, [ filters, onChange ]);
 
-  const handleUserFilter = useCallback((search: string, option) => {
-    return option.props.children.indexOf(search) !== -1;
-  }, []);
-
-  const handleUserSelect = useCallback((value: SelectValue) => {
+  const handleUserChange = useCallback((value: SelectValue) => {
     const username = value === ALL_VALUE ? undefined : value as string;
     onChange({ ...filters, username });
   }, [ filters, onChange ]);
 
-  const handleLimitSelect = useCallback((limit: number): void => {
-    onChange({ ...filters, limit });
+  const handleLimitSelect = useCallback((limit: SelectValue): void => {
+    onChange({ ...filters, limit: limit as number });
   }, [ filters, onChange ]);
 
   const filterTypeConfig = useMemo(() => {
+    const taskTypeConfig = [
+      ...(showExperiments ? experimentConfig : []),
+      ...commandConfig,
+    ];
     return taskTypeConfig.map(config => ({
       active: filters.types[config.id as TaskType],
       icon: config.id.toLocaleLowerCase(),
       id: config.id,
       label: capitalize(config.id),
     }));
-  }, [ filters.types ]);
-
-  const userToSelectOption = (user: User): React.ReactNode =>
-    <Option key={user.id} value={user.username}>{user.username}</Option>;
-
-  const userOptions = (): React.ReactNode[] => {
-    const options: React.ReactNode[] = [ <Option key={ALL_VALUE} value={ALL_VALUE}>All</Option> ];
-    if (authUser) {
-      options.push(userToSelectOption(authUser));
-    }
-    const restOfOptions = users
-      .filter(u => (!authUser || u.id !== authUser.id))
-      .sort((a, b) => a.username.localeCompare(b.username, 'en'))
-      .map(userToSelectOption);
-    options.push(...restOfOptions);
-
-    return options;
-  };
+  }, [ filters.types, showExperiments ]);
 
   return (
     <div className={css.base}>
       <IconFilterButtons buttons={filterTypeConfig} onClick={handleTypeClick} />
-      <div className={css.filter}>
-        <div className={css.label}>State</div>
-        <Select
-          defaultValue={filters.states[0]}
-          dropdownMatchSelectWidth={false}
-          suffixIcon={selectIcon}
-          onSelect={handleStateSelect}>
-          <Option key={ALL_VALUE} value={ALL_VALUE}>All</Option>
-          <OptGroup key="expGroup" label="Experiment States">
-            {Object.values(RunState).map((value) => (
-              <Option key={value} value={value}>{runStateToLabel[value]}</Option>
-            ))}
-          </OptGroup>
-          <OptGroup key="cmdGroup" label="Command States">
-            {Object.values(CommandState).map((value) => (
-              <Option key={value} value={value}>{commandStateToLabel[value]}</Option>
-            ))}
-          </OptGroup>
-        </Select>
-      </div>
-      <div className={css.filter}>
-        <div className={css.label}>Users</div>
-        <Select
-          defaultValue={filters.username || ALL_VALUE}
-          dropdownMatchSelectWidth={false}
-          filterOption={handleUserFilter}
-          optionFilterProp="children"
-          showSearch={true}
-          style={{ width: '10rem' }}
-          suffixIcon={selectIcon}
-          onSelect={handleUserSelect}>
-          {userOptions()}
-        </Select>
-      </div>
-      <div className={css.filter}>
-        <div className={css.label}>Limit</div>
-        <Select
-          defaultValue={filters.limit}
-          suffixIcon={selectIcon}
-          onSelect={handleLimitSelect}>
-          {limitOptions.map(limit => <Option key={limit} value={limit}>{limit}</Option>)}
-        </Select>
-      </div>
+      <StateSelectFilter
+        showCommandStates={showCommandStates}
+        showExperimentStates={showExperimentStates}
+        value={filters.states}
+        onChange={handleStateChange} />
+      <UserSelectFilter value={filters.username} onChange={handleUserChange} />
+      {showLimit && <SelectFilter
+        label="Limit"
+        showSearch={false}
+        value={filters.limit}
+        onSelect={handleLimitSelect}>
+        {limitOptions.map(limit => <Option key={limit} value={limit}>{limit}</Option>)}
+      </SelectFilter>}
     </div>
   );
 };
+
+TaskFilter.defaultProps = defaultProps;
 
 export default TaskFilter;
 
---
+++ webui/react/src/components/UserSelectFilter.tsx
@@ -0,0 +1,56 @@
+import { Select } from 'antd';
+import { SelectValue } from 'antd/es/select';
+import React, { useCallback, useMemo } from 'react';
+
+import Auth from 'contexts/Auth';
+import Users from 'contexts/Users';
+import { User } from 'types';
+
+import SelectFilter, { ALL_VALUE } from './SelectFilter';
+
+const { Option } = Select;
+
+interface Props {
+  onChange: (value: SelectValue) => void;
+  value?: SelectValue;
+}
+
+const userToSelectOption = (user: User): React.ReactNode =>
+  <Option key={user.id} value={user.username}>{user.username}</Option>;
+
+const UserSelectFilter: React.FC<Props> = ({ onChange, value }: Props) => {
+  const auth = Auth.useStateContext();
+  const users = Users.useStateContext();
+
+  const handleSelect = useCallback((newValue: SelectValue) => {
+    const singleValue = Array.isArray(newValue) ? newValue[0] : newValue;
+    onChange(singleValue);
+  }, [ onChange ]);
+
+  const options = useMemo(() => {
+    const authUser = auth.user;
+    const list: React.ReactNode[] = [ <Option key={ALL_VALUE} value={ALL_VALUE}>All</Option> ];
+
+    if (authUser) {
+      list.push(<Option key={authUser.id} value={authUser.username}>{authUser.username}</Option>);
+    }
+
+    if (users.data) {
+      const allOtherUsers = users.data
+        .filter(user => (!authUser || user.id !== authUser.id))
+        .sort((a, b) => a.username.localeCompare(b.username, 'en'))
+        .map(userToSelectOption);
+      list.push(...allOtherUsers);
+    }
+
+    return list;
+  }, [ auth.user, users.data ]);
+
+  return (
+    <SelectFilter label="Users" value={value || ALL_VALUE} onSelect={handleSelect}>
+      {options}
+    </SelectFilter>
+  );
+};
+
+export default UserSelectFilter;
