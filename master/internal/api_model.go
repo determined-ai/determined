192ad5489a21bb6386abd2dfd3cb4a4addaa23a3
sidneyw <sidney@determined.ai>
2020-06-23 12:18:23 -0400

    wip: working patch

--- master/internal/api_model.go
+++ master/internal/api_model.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/pkg/errors"
@@ -41,4 +42,44 @@ func (a *apiServer) PostModel(
 
 	return &apiv1.PostModelResponse{Model: m},
 		errors.Wrapf(err, "error fetching model %s from database", req.Model.Name)
+}
+
+func (a *apiServer) PatchModel(
+	_ context.Context, req *apiv1.PatchModelRequest) (*apiv1.PatchModelResponse, error) {
+
+	existingModel := &modelv1.Model{}
+	switch err := a.m.db.QueryProto("get_model", existingModel, req.Model.Name); err {
+	case db.ErrNotFound:
+		return nil, status.Errorf(
+			codes.NotFound, "model %s not found", req.Model.Name)
+	default:
+		fmt.Printf("req = %+v\n", req)
+
+		desiredModel := &modelv1.Model{}
+		paths := req.UpdateMask.GetPaths()
+		for _, v := range paths {
+			fmt.Printf("v = %+v\n", v)
+			if v == "model.description" {
+				desiredModel.Description = req.Model.Description
+			} else {
+				desiredModel.Description = existingModel.Description
+			}
+			if v == "model.metadata" {
+				fmt.Println("UPDATE Meta")
+				desiredModel.Metadata = req.Model.Metadata
+			} else {
+				desiredModel.Metadata = existingModel.Metadata
+			}
+		}
+
+		respModel := &modelv1.Model{}
+		b, err := protojson.Marshal(desiredModel.Metadata)
+		if err != nil {
+			return nil, errors.Wrap(err, "error marshaling model.Metadata")
+		}
+
+		err = a.m.db.QueryProto("update_model", respModel, req.Model.Name, desiredModel.Description, b, time.Now())
+		return &apiv1.PatchModelResponse{Model: respModel},
+			errors.Wrapf(err, "error fetching model %s from database", req.Model.Name)
+	}
 }
