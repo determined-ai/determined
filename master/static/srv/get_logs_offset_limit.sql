ddeeddde164d4b7b0fa0815c19b834decd47d9db
Hamid Zare <hamidzr@users.noreply.github.com>
2020-06-25 13:29:57 -0700

    feat: paginated CLI trial logs [DET-3442] (#779)

---
+++ master/static/srv/get_logs_offset_limit.sql
@@ -0,0 +1,9 @@
+SELECT
+    trial_logs.id as id,
+    trials.state as state,
+    encode(message, 'escape') as message
+FROM trial_logs JOIN trials
+ON trial_logs.trial_id = trials.id
+WHERE trials.id = $1 AND trial_logs.id > $2
+ORDER BY trial_logs.id ASC
+LIMIT $3
