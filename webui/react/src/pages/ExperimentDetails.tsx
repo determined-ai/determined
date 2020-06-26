c6eae584a5be3a242ed751ddf25674c36522c4d4
Hamid Zare <hamid@determined.ai>
2020-06-26 11:13:46 -0700

    bring in breadcrumbs

--- webui/react/src/pages/ExperimentDetails.tsx
+++ webui/react/src/pages/ExperimentDetails.tsx
@@ -1,6 +1,9 @@
+import { Breadcrumb, Space } from 'antd';
 import React from 'react';
 import { useParams } from 'react-router';
 
+import Icon from 'components/Icon';
+import Link from 'components/Link';
 import Page from 'components/Page';
 
 interface Params {
@@ -10,7 +13,19 @@ interface Params {
 const ExperimentDetails: React.FC = () => {
   const { experimentId } = useParams<Params>();
   return (
-    <Page title={`Experiment ${experimentId}`} />
+    <Page title={`Experiment ${experimentId}`}>
+      <Breadcrumb>
+        <Breadcrumb.Item>
+          <Space align="center" size="small">
+            <Icon name="experiment" size="small" />
+            <Link path="/det/experiments">Experiments</Link>
+          </Space>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>
+          <span>3</span>
+        </Breadcrumb.Item>
+      </Breadcrumb>
+    </Page>
   );
 };
 
