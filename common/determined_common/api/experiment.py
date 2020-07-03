176789c6ce1df769afcea47ed2250a7b688751da
naren-determined <naren@determined.ai>
2020-07-02 19:46:58 -0700

    revert PR to latest commit

--- common/determined_common/api/experiment.py
+++ common/determined_common/api/experiment.py
@@ -93,6 +93,7 @@ def make_test_experiment_config(config: Dict[str, Any]) -> Dict[str, Any]:
     config.setdefault(
         "data_layer", {"type": "shared_fs", "container_storage_path": "/tmp/determined"}
     )
+
     return config_test
 
 
