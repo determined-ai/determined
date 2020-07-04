e2a5910d135b229667cd47dac5c647dc7ac5dc78
liam <liam@determined.ai>
2020-07-03 23:24:55 -0400

    feat: add gaea nas

---
+++ examples/experimental/trial/gaea_nas/utils.py
@@ -0,0 +1,38 @@
+import torchvision.transforms as transforms
+
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+def data_transforms_cifar10():
+    CIFAR_MEAN = [0.49139968, 0.48215827, 0.44653124]
+    CIFAR_STD = [0.24703233, 0.24348505, 0.26158768]
+
+    train_transform = transforms.Compose(
+        [
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+        ]
+    )
+    valid_transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize(CIFAR_MEAN, CIFAR_STD),]
+    )
+    return train_transform, valid_transform
+
+def accuracy(output, target, topk=(1,)):
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
