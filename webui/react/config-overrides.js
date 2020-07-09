2b01057224e43eff529302c7e69dfd69f3f3af59
Hamid Zare <hamid@determined.ai>
2020-07-09 10:12:14 -0700

    address newly found eslint checks

--- webui/react/config-overrides.js
+++ webui/react/config-overrides.js
@@ -23,7 +23,7 @@ module.exports = override(
   }),
 
   // Add LESS loader support for antd.
-  addLessLoader({ javascriptEnabled: true }),
+  addLessLoader({ lessOptions: { javascriptEnabled: true } }),
 
   // Replace momentjs to Day.js to reduce antd package size.
   addWebpackPlugin(new AntdDayjsWebpackPlugin()),
