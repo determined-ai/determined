b8e798757a9bed3fbedc9af53cedd0547d874b84
Armand McQueen <armandmcqueen@users.noreply.github.com>
2020-07-13 11:05:05 -0700

    chore: upgrade agent VM image to newer kernel version
    Upgrade the agent VM image to one built on top of a newer version of Ubuntu 16.04. This was motivated by a desire to mount a Lustre filesystem onto agent VMs, but the Lustre client requires a kernel version that is newer than what our base Ubuntu 16.04 image had.
    
    The Docker contents did not change, but the change to the Environments repo changed the hash in the tag name, so we upgrade all references to the old hash to make the codebase consistent/easy to understand/easy to search.

--- master/pkg/model/defaults.go
+++ master/pkg/model/defaults.go
@@ -96,8 +96,8 @@ func DefaultExperimentConfig() ExperimentConfig {
 		BatchesPerStep: 100,
 		Environment: Environment{
 			Image: RuntimeItem{
-				CPU: "determinedai/environments:py-3.6.9-pytorch-1.4-tf-1.14-cpu-0c9e956",
-				GPU: "determinedai/environments:cuda-10.0-pytorch-1.4-tf-1.14-gpu-0c9e956",
+				CPU: "determinedai/environments:py-3.6.9-pytorch-1.4-tf-1.14-cpu-9c8c0bd",
+				GPU: "determinedai/environments:cuda-10.0-pytorch-1.4-tf-1.14-gpu-9c8c0bd",
 			},
 		},
 		Reproducibility: ReproducibilityConfig{
