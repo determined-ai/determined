70085dc9a46892ad9bdc7be636444f3ee53d210c
aaron276h <aaron@determined.ai>
2020-07-17 14:22:08 -0400

    feat: forward k8 events to task logs

---
+++ master/internal/kubernetes/events.go
@@ -0,0 +1,79 @@
+package kubernetes
+
+import (
+	"github.com/pkg/errors"
+
+	k8sV1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sClient "k8s.io/client-go/kubernetes"
+
+	"github.com/determined-ai/determined/master/pkg/actor"
+)
+
+// messages that are sent to the event listener.
+type (
+	startEventListener struct{}
+)
+
+// messages that are sent by the event listener.
+type (
+	podEventUpdate struct {
+		event *k8sV1.Event
+	}
+)
+
+type eventListener struct {
+	clientSet   *k8sClient.Clientset
+	namespace   string
+	podsHandler *actor.Ref
+}
+
+func newEventListener(
+	clientSet *k8sClient.Clientset,
+	namespace string,
+	podsHandler *actor.Ref,
+) *eventListener {
+	return &eventListener{
+		clientSet:   clientSet,
+		namespace:   namespace,
+		podsHandler: podsHandler,
+	}
+}
+
+func (e *eventListener) Receive(ctx *actor.Context) error {
+	switch msg := ctx.Message().(type) {
+	case actor.PreStart:
+		ctx.Tell(ctx.Self(), startEventListener{})
+
+	case startEventListener:
+		if err := e.startEventListener(ctx); err != nil {
+			return err
+		}
+
+	case actor.PostStop:
+
+	default:
+		ctx.Log().Errorf("unexpected message %T", msg)
+		return actor.ErrUnexpectedMessage(ctx)
+	}
+
+	return nil
+}
+
+func (e *eventListener) startEventListener(ctx *actor.Context) error {
+	watch, err := e.clientSet.CoreV1().Events(e.namespace).Watch(metaV1.ListOptions{})
+	if err != nil {
+		return errors.Wrap(err, "error initializing event watch")
+	}
+
+	ctx.Log().Info("event listener is starting")
+	for event := range watch.ResultChan() {
+		newEvent := event.Object.(*k8sV1.Event)
+		ctx.Tell(e.podsHandler, podEventUpdate{event: newEvent})
+	}
+
+	ctx.Log().Warn("event listener stopped unexpectedly")
+	ctx.Tell(ctx.Self(), startEventListener{})
+
+	return nil
+}
