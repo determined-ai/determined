6108f58960de0504de7f3ac43c7936ccdf82c849
Caleb Kang <caleb@determined.ai>
2020-07-21 13:23:08 -0600

    feat: add trials table to experiment details

--- webui/react/src/pages/ExperimentDetails.tsx
+++ webui/react/src/pages/ExperimentDetails.tsx
@@ -1,25 +1,31 @@
-import { Alert, Breadcrumb, Modal, Space } from 'antd';
+import { Alert, Breadcrumb, Button, Modal, Space, Table } from 'antd';
+import { ColumnsType } from 'antd/lib/table';
 import yaml from 'js-yaml';
 import React, { useCallback, useEffect, useState } from 'react';
 import MonacoEditor from 'react-monaco-editor';
 import { useParams } from 'react-router';
 
+import CheckpointModal from 'components/CheckpointModal';
 import ExperimentActions from 'components/ExperimentActions';
 import ExperimentInfoBox from 'components/ExperimentInfoBox';
 import Icon from 'components/Icon';
+import { makeClickHandler } from 'components/Link';
 import Link from 'components/Link';
 import Message from 'components/Message';
 import Page from 'components/Page';
 import Section from 'components/Section';
 import Spinner from 'components/Spinner';
+import { stateRenderer } from 'components/Table';
 import handleError, { ErrorType } from 'ErrorHandler';
 import usePolling from 'hooks/usePolling';
 import { useRestApiSimple } from 'hooks/useRestApi';
 import { routeAll } from 'routes';
 import { forkExperiment, getExperimentDetails, isNotFound } from 'services/api';
 import { ExperimentDetailsParams } from 'services/types';
-import { ExperimentDetails } from 'types';
+import { CheckpointDetail, ExperimentDetails, TrialSummary } from 'types';
 import { clone } from 'utils/data';
+import { alphanumericSorter, numericSorter, runStateSorter } from 'utils/data';
+import { humanReadableFloat } from 'utils/string';
 
 import css from './ExperimentDetails.module.scss';
 
@@ -28,27 +34,28 @@ interface Params {
 }
 
 const ExperimentDetailsComp: React.FC = () => {
-  const { experimentId: experimentIdParam } = useParams<Params>();
-  const experimentId = parseInt(experimentIdParam);
-  const [ experiment, setExpRequestParams ] =
-    useRestApiSimple<ExperimentDetailsParams, ExperimentDetails>(
-      getExperimentDetails,
-      { id: experimentId },
-    );
-  const pollExperimentDetails = useCallback(
-    () => setExpRequestParams({ id: experimentId }),
-    [ setExpRequestParams, experimentId ],
-  );
+  const { experimentId } = useParams<Params>();
+  const id = parseInt(experimentId);
+  const [ experimentResponse, setExpRequestParams ] =
+    useRestApiSimple<ExperimentDetailsParams, ExperimentDetails>(getExperimentDetails, { id });
+  const experiment = experimentResponse.data;
+  const [ activeCheckpoint, setActiveCheckpoint ] = useState<CheckpointDetail>();
+  const [ showCheckpoint, setShowCheckpoint ] = useState(false);
+
+  const pollExperimentDetails = useCallback(() => {
+    setExpRequestParams({ id });
+  }, [ id, setExpRequestParams ]);
+
   usePolling(pollExperimentDetails);
   const [ forkValue, setForkValue ] = useState<string>('Loading');
   const [ forkModalState, setForkModalState ] = useState({ visible: false });
   const [ forkError, setForkError ] = useState<string>();
 
   useEffect(() => {
-    if (experiment.data && experiment.data.config) {
+    if (experiment && experiment.config) {
       try {
         const prefix = 'Fork of ';
-        const rawConfig = clone(experiment.data.configRaw);
+        const rawConfig = clone(experiment.configRaw);
         rawConfig.description = prefix + rawConfig.description;
         setForkValue(yaml.safeDump(rawConfig));
       } catch (e) {
@@ -60,7 +67,7 @@ const ExperimentDetailsComp: React.FC = () => {
         setForkValue('failed to load experiment config');
       }
     }
-  }, [ experiment.data ]);
+  }, [ experiment ]);
 
   const showForkModal = useCallback((): void => {
     setForkModalState(state => ({ ...state, visible: true }));
@@ -71,30 +78,29 @@ const ExperimentDetailsComp: React.FC = () => {
     setForkError(undefined);
   }, []);
 
-  if (isNaN(experimentId)) {
-    return (
-      <Page hideTitle title="Not Found">
-        <Message>Bad experiment ID {experimentIdParam}</Message>
-      </Page>
-    );
-  }
+  const handleTableRow = useCallback((record: TrialSummary) => ({
+    onClick: makeClickHandler(record.url as string),
+  }), []);
 
-  if (experiment.error !== undefined) {
-    const message = isNotFound(experiment.error) ? `Experiment ${experimentId} not found.`
-      : `Failed to fetch experiment ${experimentId}.`;
+  let message = '';
+  if (isNaN(id)) message = `Bad experiment ID ${experimentId}`;
+  if (experimentResponse.error) {
+    message = isNotFound(experimentResponse.error) ?
+      `Experiment ${experimentId} not found.` :
+      `Failed to fetch experiment ${experimentId}.`;
+  }
+  if (message) {
     return (
       <Page hideTitle title="Not Found">
         <Message>{message}</Message>
       </Page>
     );
-  } else if (!experiment.data) {
+  } else if (!experiment) {
     return <Spinner fillContainer />;
   }
 
   const monacoOpts = {
-    minimap: {
-      enabled: false,
-    },
+    minimap: { enabled: false },
     selectOnLineNumbers: true,
   };
 
@@ -120,8 +126,58 @@ const ExperimentDetailsComp: React.FC = () => {
     setForkModalState(state => ({ ...state, visible: false }));
   };
 
+  const handleCheckpointShow = (event: React.MouseEvent, checkpoint: CheckpointDetail) => {
+    event.stopPropagation();
+    setActiveCheckpoint(checkpoint);
+    setShowCheckpoint(true);
+  };
+  const handleCheckpointDismiss = () => setShowCheckpoint(false);
+
+  const columns: ColumnsType<TrialSummary> = [
+    {
+      dataIndex: 'id',
+      sorter: (a: TrialSummary, b: TrialSummary): number => alphanumericSorter(a.id, b.id),
+      title: 'ID',
+    },
+    {
+      render: (_: string, record: TrialSummary): React.ReactNode => {
+        if (experiment.config && record.bestAvailableCheckpoint) {
+          const checkpoint: CheckpointDetail = {
+            ...record.bestAvailableCheckpoint,
+            batch: record.numBatchTally,
+            experimentId: id,
+            trialId: record.id,
+          };
+          return <Button onClick={e => handleCheckpointShow(e, checkpoint)}>
+            {record.numBatchTally}
+          </Button>;
+        }
+        return record.numBatchTally;
+      },
+      sorter: (a: TrialSummary, b: TrialSummary): number =>{
+        return alphanumericSorter(a.numBatchTally, b.numBatchTally);
+      },
+      title: 'Batches',
+    },
+    {
+      dataIndex: 'bestValidationMetric',
+      render: (_: string, record: TrialSummary): React.ReactNode => {
+        return record.bestValidationMetric ? humanReadableFloat(record.bestValidationMetric) : null;
+      },
+      sorter: (a: TrialSummary, b: TrialSummary): number => {
+        return numericSorter(a.bestValidationMetric, b.bestValidationMetric);
+      },
+      title: `Metric (${experiment.config.searcher.metric})`,
+    },
+    {
+      render: stateRenderer,
+      sorter: (a: TrialSummary, b: TrialSummary): number => runStateSorter(a.state, b.state),
+      title: 'State',
+    },
+  ];
+
   return (
-    <Page title={`Experiment ${experiment.data?.config.description}`}>
+    <Page title={`Experiment ${experiment?.config.description}`}>
       <Breadcrumb>
         <Breadcrumb.Item>
           <Space align="center" size="small">
@@ -133,11 +189,11 @@ const ExperimentDetailsComp: React.FC = () => {
           <span>{experimentId}</span>
         </Breadcrumb.Item>
       </Breadcrumb>
-      <ExperimentActions experiment={experiment.data} onClick={{ Fork: showForkModal }}
+      <ExperimentActions
+        experiment={experiment}
+        onClick={{ Fork: showForkModal }}
         onSettled={pollExperimentDetails} />
-      <ExperimentInfoBox experiment={experiment.data} />
-      <Section title="Chart" />
-      <Section title="Trials" />
+      <ExperimentInfoBox experiment={experiment} />
       <Modal
         bodyStyle={{
           padding: 0,
@@ -164,6 +220,21 @@ const ExperimentDetailsComp: React.FC = () => {
           <Alert className={css.error} message={forkError} type="error" />
         }
       </Modal>
+      <Section title="Chart" />
+      <Section title="Trials">
+        <Table
+          columns={columns}
+          dataSource={experiment?.trials}
+          loading={!experimentResponse.hasLoaded}
+          rowKey="id"
+          size="small"
+          onRow={handleTableRow} />
+      </Section>
+      {activeCheckpoint && <CheckpointModal
+        checkpoint={activeCheckpoint}
+        config={experiment.config}
+        show={showCheckpoint}
+        onHide={handleCheckpointDismiss} />}
     </Page>
   );
 };
