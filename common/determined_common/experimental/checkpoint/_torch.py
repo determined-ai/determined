22b8aed4403ee093e4061e13cfa99ffe1a3a9f69
David Hershey <david@determined.ai>
2020-07-22 17:09:01 -0400

    fix: restore previous checkpoint export functionality

--- common/determined_common/experimental/checkpoint/_torch.py
+++ common/determined_common/experimental/checkpoint/_torch.py
@@ -10,7 +10,7 @@ from determined.pytorch import PyTorchTrial, PyTorchTrialContext
 def load_model(
     ckpt_dir: pathlib.Path, metadata: Dict[str, Any], **kwargs: Any
 ) -> Union[PyTorchTrial, torch.nn.Module]:
-    checkpoint = torch.load(ckpt_dir.joinpath("state_dict.pth"), map_location="cpu")  # type: ignore
+    checkpoint = torch.load(ckpt_dir.joinpath("state_dict.pth"), **kwargs)  # type: ignore
 
     trial_cls, trial_context = experimental._load_trial_on_local(
         ckpt_dir.joinpath("code"),
@@ -20,13 +20,16 @@ def load_model(
 
     trial_context = cast(PyTorchTrialContext, trial_context)
     trial = cast(PyTorchTrial, trial_cls(trial_context))
-
     if "model_state_dict" in checkpoint:
         # Backward compatible with older checkpoint format.
         model = trial.build_model()
         model.load_state_dict(checkpoint["model_state_dict"])
         return model
     else:
-        for idx, model in enumerate(trial_context.models):
-            model.load_state_dict(checkpoint["models_state_dict"][idx])
-        return trial
+        model = trial.build_model()
+        model.load_state_dict(checkpoint["models_state_dict"][0])
+        return model
+        # TODO: Fix for multiple model case
+        # for idx, model in enumerate(trial_context.models):
+        #     model.load_state_dict(checkpoint["models_state_dict"][idx])
+        # return trial
