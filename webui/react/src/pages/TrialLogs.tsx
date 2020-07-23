bcf0f6dd47813acca5ef22d8b57188b97f16a084
Hamid Zare <hamid@determined.ai>
2020-07-23 15:16:21 -0700

    fix: log download link in react dev

--- webui/react/src/pages/TrialLogs.tsx
+++ webui/react/src/pages/TrialLogs.tsx
@@ -21,7 +21,7 @@ const TrialLogs: React.FC = () => {
   const { trialId } = useParams<Params>();
   const id = parseInt(trialId);
   const title = `Trial ${id} Logs`;
-  const downloadUrl = `${serverAddress()}/trials/${id}/logs?format=raw`;
+  const downloadUrl = `${serverAddress(true)}/trials/${id}/logs?format=raw`;
   const setUI = UI.useActionContext();
   const logsRef = useRef<LogViewerHandles>(null);
   const [ oldestFetchedId, setOldestFetchedId ] = useState(Number.MAX_SAFE_INTEGER);
