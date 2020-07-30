51a8b51aec87243f1b03eb1a84bcf6740da8d1cf
Bradley Laney <bradlaney@determined.ai>
2020-07-30 14:32:34 -0400

    chore: remove steps from the CLI [DET-3625] (#940)
    This change removes any references to the concept "step" in the CLI and replaces them with references to the number of batches complete at each workload. It also adds a database migration to backfill the values for `num_batches` and `total_batches_processed` for steps belonging to experiments trained in older versions of Determined.

---
+++ master/static/migrations/20200729211811_extend-steps-table.up.sql
@@ -0,0 +1,26 @@
+ALTER TABLE public.steps ADD COLUMN total_batches_processed integer NULL;
+
+WITH legacy_num_batches AS (
+    SELECT
+        id experiment_id,
+        (e.config->>'batches_per_step')::int num_batches
+    FROM public.experiments e
+)
+-- first backfill num_batches for each step with the value of batches_per_step, if missing.
+UPDATE public.steps AS s
+SET num_batches = (
+    SELECT num_batches
+    FROM legacy_num_batches b
+    JOIN public.trials t ON t.experiment_id = b.experiment_id
+    WHERE t.id = s.trial_id
+)
+WHERE s.num_batches IS NULL;
+
+-- then backfill total_batches_processed using the value of num_batches we just backfilled.
+UPDATE public.steps AS s
+SET total_batches_processed = (
+    SELECT coalesce(sum(ss.num_batches), 0)
+    FROM public.steps ss
+    WHERE ss.id < s.id
+)
+WHERE s.total_batches_processed IS NULL;
