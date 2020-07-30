63163a6fc0bc079d1c2881a14467d025ca8bbf6f
Bradley Laney <bradlaney@determined.ai>
2020-07-30 08:33:27 -0400

    simplify migration sql

--- master/static/migrations/20200729211811_extend-steps-table.up.sql
+++ master/static/migrations/20200729211811_extend-steps-table.up.sql
@@ -9,24 +9,18 @@ WITH legacy_num_batches AS (
 -- first backfill num_batches for each step with the value of batches_per_step, if missing.
 UPDATE public.steps AS s
 SET num_batches = (
-    CASE WHEN num_batches is NULL THEN (
-        SELECT num_batches
-        FROM legacy_num_batches b
-        JOIN public.trials t ON t.experiment_id = b.experiment_id
-        WHERE t.id = s.trial_id
-    )
-    ELSE s.num_batches
-    END
-);
+    SELECT num_batches
+    FROM legacy_num_batches b
+    JOIN public.trials t ON t.experiment_id = b.experiment_id
+    WHERE t.id = s.trial_id
+)
+WHERE s.num_batches IS NULL;
 
 -- then backfill total_batches_processed using the value of num_batches we just backfilled.
 UPDATE public.steps AS s
-SET total_batches_processed = ( 
-    CASE WHEN s.total_batches_processed is NULL THEN (
-        SELECT coalesce(sum(ss.num_batches), 0)
-        FROM public.steps ss
-        WHERE ss.id < s.id
-    )
-    ELSE s.total_batches_processed
-    END
-);
+SET total_batches_processed = (
+    SELECT coalesce(sum(ss.num_batches), 0)
+    FROM public.steps ss
+    WHERE ss.id < s.id
+)
+WHERE s.total_batches_processed IS NULL;
