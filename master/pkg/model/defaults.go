bc43f4d2f9ac80c97835bcccf98ababef1bd0937
Neil Conway <neil@determined.ai>
2020-07-29 17:20:38 -0700

    chore: update cloud images and default task envs.
    This pulls in the recent update to the Nvidia drivers in the public
    cloud images, moving from driver version 410.104 to 450.51.05.
    
    Taskenvs should not actually be modified by this change, but since the
    build machinery pushed a new version of the taskenvs, update to use the
    latest taskenv versions just to be safe.

--- master/pkg/model/defaults.go
+++ master/pkg/model/defaults.go
@@ -96,8 +96,8 @@ func DefaultExperimentConfig() ExperimentConfig {
 		BatchesPerStep: 100,
 		Environment: Environment{
 			Image: RuntimeItem{
-				CPU: "determinedai/environments:py-3.6.9-pytorch-1.4-tf-1.15-cpu-23863bb",
-				GPU: "determinedai/environments:cuda-10.0-pytorch-1.4-tf-1.15-gpu-23863bb",
+				CPU: "determinedai/environments:py-3.6.9-pytorch-1.4-tf-1.15-cpu-aaa3750",
+				GPU: "determinedai/environments:cuda-10.0-pytorch-1.4-tf-1.15-gpu-aaa3750",
 			},
 		},
 		Reproducibility: ReproducibilityConfig{
