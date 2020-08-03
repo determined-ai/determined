624a1180303cffdfba1aebf0f324fac7e265bba8
Caleb Kang <caleb@determined.ai>
2020-08-03 16:24:55 -0600

    chore: update based on PR review

--- webui/react/src/pages/ExperimentList.table.tsx
+++ webui/react/src/pages/ExperimentList.table.tsx
@@ -29,10 +29,6 @@ export const columns: ColumnType<ExperimentItem>[] = [
     title: 'Duration',
   },
   // TODO bring in actual trial counts once available.
-  // {
-  //   render: (): number => Math.floor(Math.random() * 100),
-  //   title: 'Trials',
-  // },
   {
     render: stateRenderer,
     sorter: (a: ExperimentItem, b: ExperimentItem): number => runStateSorter(a.state, b.state),
