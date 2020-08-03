c94143d9b0d9568a17daeacf89c3a2ae33a3f0a1
aaron276h <aaron@determined.ai>
2020-08-03 19:39:44 -0400

    test: add e2e test using pod spec

--- e2e_tests/tests/config.py
+++ e2e_tests/tests/config.py
@@ -141,3 +141,10 @@ def set_perform_initial_validation(config: Dict[Any, Any], init_val: bool) -> Di
     config = config.copy()
     config["perform_initial_validation"] = init_val
     return config
+
+
+def set_pod_spec(config: Dict[Any, Any], pod_spec: Dict[Any, Any]) -> Dict[Any, Any]:
+    config = config.copy()
+    config.setdefault("kubernetes", {})
+    config["kubernetes"]["pod_spec"] = pod_spec
+    return config
