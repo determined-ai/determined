7cb3b2535b6711ff2dc7598290fabfc4a7a88440
aaron276h <aaron@determined.ai>
2020-08-03 17:34:06 -0400

    test: add CI for k8s [DET-3652, DET-3653, DET-3654, DET-3655] (#975)
    Adding support for running full suite of e2e GPU tests and parallel tests on GKE as part of our CI. Added support for running automatically on merge for master branch, and on approval for all other branches.

--- e2e_tests/tests/experiment/test_tf_keras.py
+++ e2e_tests/tests/experiment/test_tf_keras.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 import pytest
 
 from tests import config as conf
@@ -117,7 +119,9 @@ def test_tf_keras_mnist_data_layer_lfs(tf2: bool) -> None:
 @pytest.mark.e2e_gpu  # type: ignore
 @pytest.mark.parametrize("tf2", [False])  # type: ignore
 @pytest.mark.parametrize("storage_type", ["s3"])  # type: ignore
-def test_tf_keras_mnist_data_layer_s3(tf2: bool, storage_type: str) -> None:
+def test_tf_keras_mnist_data_layer_s3(
+    tf2: bool, storage_type: str, secrets: Dict[str, str]
+) -> None:
     run_tf_keras_mnist_data_layer_test(tf2, storage_type)
 
 
@@ -138,7 +142,9 @@ def run_tf_keras_mnist_data_layer_test(tf2: bool, storage_type: str) -> None:
 @pytest.mark.parallel  # type: ignore
 @pytest.mark.parametrize("tf2", [False])  # type: ignore
 @pytest.mark.parametrize("storage_type", ["lfs", "s3"])  # type: ignore
-def test_tf_keras_mnist_data_layer_parallel(tf2: bool, storage_type: str) -> None:
+def test_tf_keras_mnist_data_layer_parallel(
+    tf2: bool, storage_type: str, secrets: Dict[str, str]
+) -> None:
     config = conf.load_config(conf.experimental_path("trial/data_layer_mnist_tf_keras/const.yaml"))
     config = conf.set_max_steps(config, 2)
     config = conf.set_slots_per_trial(config, 8)
