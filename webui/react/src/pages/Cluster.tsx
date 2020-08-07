bdee966b3e41f020ecb3eb644c26952381199433
Caleb Hoyoul Kang <caleb@determined.ai>
2020-08-07 10:09:43 -0600

    fix: filter out disabled slots for reporting (#1030)

--- webui/react/src/pages/Cluster.tsx
+++ webui/react/src/pages/Cluster.tsx
@@ -5,20 +5,20 @@ import Message from 'components/Message';
 import Page from 'components/Page';
 import ResourceChart from 'components/ResourceChart';
 import Spinner from 'components/Spinner';
-import AgentsCtx from 'contexts/Agents';
+import Agents from 'contexts/Agents';
 import { Resource, ResourceType } from 'types';
 import { categorize } from 'utils/data';
 
 const Cluster: React.FC = () => {
-  const agents = AgentsCtx.useStateContext();
+  const agents = Agents.useStateContext();
 
   const availableResources = useMemo(() => {
     if (!agents.data) return {};
     const resourceList = agents.data
       .map(agent => agent.resources)
-      .flat();
+      .flat()
+      .filter(resource => resource.enabled);
     return categorize(resourceList, (res: Resource) => res.type);
-
   }, [ agents ]);
 
   const availableResourceTypes = Object.keys(availableResources);
@@ -28,18 +28,14 @@ const Cluster: React.FC = () => {
   if (!agents.data) {
     unhappyView = <Spinner />;
   } else if (agents.data.length === 0) {
-    unhappyView = (<Message>
-      No agents connected.
-    </Message>);
+    unhappyView = <Message>No agents connected.</Message>;
   } else if (availableResourceTypes.length === 0) {
-    unhappyView = (<Message>
-      No slots available.
-    </Message>);
+    unhappyView = <Message>No slots available.</Message>;
   }
 
   return (
     <Page id="cluster" title="Cluster">
-      {unhappyView ? unhappyView :
+      {unhappyView ||
         <Grid minItemWidth={50}>
           {Object.values(ResourceType).map((resourceType, idx) => (
             <ResourceChart key={idx}
