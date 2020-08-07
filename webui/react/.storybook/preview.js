bf500b646959ed067903c337f4794d9336303c63
Hamid Zare <hamid@determined.ai>
2020-08-06 23:09:42 -0700

    test: add Loki storybook visual testing

--- webui/react/.storybook/preview.js
+++ webui/react/.storybook/preview.js
@@ -2,6 +2,7 @@ import 'styles/index.scss';
 import 'styles/storybook.scss';
 
 import { addDecorator } from "@storybook/react"
+import 'loki/configure-react';
 
 import ThemeDecorator from "storybook/ThemeDecorator"
 
