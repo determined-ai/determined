8f4e0456ce77c597c36b030c783873b58cc587f0
Caleb Kang <caleb@determined.ai>
2020-08-13 01:06:04 -0600

    chore: add basic trial detail table column definition

---
+++ webui/react/src/pages/TrialDetails.table.tsx
@@ -0,0 +1,28 @@
+import { ColumnType } from 'antd/lib/table';
+import React from 'react';
+
+import { Renderer, stateRenderer } from 'components/Table';
+import { Step } from 'types';
+import { alphanumericSorter, runStateSorter } from 'utils/data';
+
+const batchRender: Renderer<Step> = (_, record) => (
+  <>{record.numBatches + record.priorBatchesProcessed}</>
+);
+
+export const columns: ColumnType<Step>[] = [
+  {
+    dataIndex: 'id',
+    sorter: (a: Step, b: Step): number => alphanumericSorter(a.id, b.id),
+    title: 'ID',
+  },
+  {
+    render: batchRender,
+    sorter: (a: Step, b: Step): number => alphanumericSorter(a.numBatches, b.numBatches),
+    title: 'Batches',
+  },
+  {
+    render: stateRenderer,
+    sorter: (a: Step, b: Step): number => runStateSorter(a.state, b.state),
+    title: 'State',
+  },
+];
