ae271c428a3d072cc30ea5ddf17467f23166b6bc
Ryan <rb@determined.ai>
2020-08-18 16:29:52 -0600

    chore: add a shell-entrypoint.sh (#1102)
    Add shell-entrypoint.sh which configures an interactive session with the
    proper Determined python packages pre-installed and the proper PATH in
    the same way that notebook-entrypoint.sh does.
    
    Also configure ssh to set PermitUserEnvironment to yes, so that we can
    use that mechansim to inject environment variables into the shell
    sessions via the sshd server.
    
    Also set PermitUserRC to yes and AllowEnv to * so that users can easily
    customize their shell environment via some of the normal ssh mechanisms.

--- e2e_tests/tests/command/test_shell.py
+++ e2e_tests/tests/command/test_shell.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import pytest
 
+import determined as det
 from tests import command as cmd
 
 
@@ -9,11 +10,12 @@ from tests import command as cmd
 @pytest.mark.e2e_gpu  # type: ignore
 def test_start_and_write_to_shell(tmp_path: Path) -> None:
     with cmd.interactive_command("shell", "start") as shell:
-        shell.stdin.write(b"echo hello world\n")
+        # Call our cli to ensure that PATH and PYTHONUSERBASE are properly set.
+        shell.stdin.write(b"det --version\n")
         shell.stdin.close()
 
         for line in shell.stdout:
-            if "hello world" in line:
+            if str(det.__version__) in line:
                 break
         else:
-            pytest.fail("Did not find expected input 'hello world' in shell stdout.")
+            pytest.fail(f"Did not find expected input {det.__version__} in shell stdout.")
