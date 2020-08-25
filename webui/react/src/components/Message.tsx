116c9c7b8f0c27ce1eb4b611126ef5fd298ecf82
Caleb Kang <caleb@determined.ai>
2020-08-25 17:33:18 -0600

    style: add empty message types and apply to dashboard

--- webui/react/src/components/Message.tsx
+++ webui/react/src/components/Message.tsx
@@ -1,18 +1,30 @@
+import { Empty } from 'antd';
 import React from 'react';
 
 import warningImage from 'assets/warning.svg';
 
 import css from './Message.module.scss';
 
+export enum MessageType {
+  Empty = 'empty',
+  Warning = 'warning',
+}
+
 interface Props {
   message?: string;
   title: string;
+  type?: MessageType;
 }
 
-const Message: React.FC<Props> = ({ message, title }: Props) => {
+const Message: React.FC<Props> = ({
+  message,
+  title,
+  type = MessageType.Warning,
+}: Props) => {
   return (
     <div className={css.base}>
-      <img src={warningImage} />
+      {type === MessageType.Empty && Empty.PRESENTED_IMAGE_SIMPLE}
+      {type === MessageType.Warning && <img src={warningImage} />}
       <div className={css.title}>{title}</div>
       {message && <span>{message}</span>}
     </div>
