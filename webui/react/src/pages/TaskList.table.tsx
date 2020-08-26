6bdf0978cd8acfa7f06ff2ea9e5a55185cb27f98
Caleb Kang <caleb@determined.ai>
2020-08-25 21:02:30 -0600

    feat: save task list table sorting to user preferences

--- webui/react/src/pages/TaskList.table.tsx
+++ webui/react/src/pages/TaskList.table.tsx
@@ -12,40 +12,45 @@ import {
 export const columns: ColumnType<CommandTask>[] = [
   {
     dataIndex: 'id',
+    key: 'id',
     render: taskIdRenderer,
     sorter: (a: CommandTask, b: CommandTask): number => alphanumericSorter(a.id, b.id),
     title: 'Short ID',
   },
   {
+    key: 'type',
     render: taskTypeRenderer,
     sorter: (a: CommandTask, b: CommandTask): number => alphanumericSorter(a.type, b.type),
     title: 'Type',
   },
   {
     dataIndex: 'name',
+    key: 'name',
     sorter: (a: CommandTask, b: CommandTask): number => alphanumericSorter(a.name, b.name),
     title: 'Name',
   },
   {
-    defaultSortOrder: 'descend',
+    key: 'startTime',
     render: (_: number, record: CommandTask): React.ReactNode =>
       relativeTimeRenderer(new Date(record.startTime)),
     sorter: (a: CommandTask, b: CommandTask): number => stringTimeSorter(a.startTime, b.startTime),
     title: 'Start Time',
   },
   {
+    key: 'state',
     render: stateRenderer,
     sorter: (a: CommandTask, b: CommandTask): number => commandStateSorter(a.state, b.state),
     title: 'State',
   },
   {
+    key: 'sources',
     sorter: (a: CommandTask, b: CommandTask): number => commandSourceSorter(a.misc, b.misc),
     title: 'Sources',
   },
   {
+    key: 'user',
     render: userRenderer,
-    sorter: (a: CommandTask, b: CommandTask): number =>
-      alphanumericSorter(a.username, b.username),
+    sorter: (a: CommandTask, b: CommandTask): number => alphanumericSorter(a.username, b.username),
     title: 'User',
   },
   {
