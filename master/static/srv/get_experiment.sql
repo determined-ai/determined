379bcc75464ab85c4e2b3dd3b51a8f676dc973cc
Bradley Laney <bradlaney@determined.ai>
2020-08-27 15:56:49 -0400

    feat: add GET /experiment/$ID

---
+++ master/static/srv/get_experiment.sql
@@ -0,0 +1,15 @@
+SELECT
+    e.id AS id,
+    e.config->>'description' AS description,
+    e.config->'labels' AS labels,
+    e.start_time AS start_time,
+    e.end_time AS end_time,
+    'STATE_' || e.state AS state,
+    (SELECT COUNT(*) FROM trials t WHERE e.id = t.experiment_id) AS num_trials,
+    e.archived AS archived,
+    COALESCE(e.progress, 0) AS progress,
+    u.username AS username
+FROM
+    experiments e
+JOIN users u ON e.owner_id = u.id
+WHERE e.id = $1;
