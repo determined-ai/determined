a3bed63006b717e26e5ef4b0e8cbd032d4c5629f
Caleb Kang <caleb@determined.ai>
2020-09-02 00:46:23 -0600

    chore: connect default sorter to api sorting and add numTrials column

--- webui/react/src/pages/ExperimentList.table.tsx
+++ webui/react/src/pages/ExperimentList.table.tsx
@@ -6,61 +6,59 @@ import {
   expermentDurationRenderer, relativeTimeRenderer, stateRenderer, userRenderer,
 } from 'components/Table';
 import { ExperimentX } from 'types';
-import { alphanumericSorter, runStateSorter, stringTimeSorter } from 'utils/data';
-import { getDuration } from 'utils/time';
 
 export const columns: ColumnType<ExperimentX>[] = [
   {
     dataIndex: 'id',
     key: 'id',
-    sorter: (a: ExperimentX, b: ExperimentX): number => alphanumericSorter(a.id, b.id),
+    sorter: true,
     title: 'ID',
   },
   {
     dataIndex: 'name',
     key: 'name',
-    sorter: (a: ExperimentX, b: ExperimentX): number => alphanumericSorter(a.name, b.name),
+    sorter: true,
     title: 'Name',
   },
   {
     key: 'startTime',
     render: (_: number, record: ExperimentX): React.ReactNode =>
       relativeTimeRenderer(new Date(record.startTime)),
-    sorter: (a: ExperimentX, b: ExperimentX): number =>
-      stringTimeSorter(a.startTime, b.startTime),
+    sorter: true,
     title: 'Start Time',
   },
   {
     key: 'duration',
     render: expermentDurationRenderer,
-    sorter: (a: ExperimentX, b: ExperimentX): number => getDuration(a) - getDuration(b),
     title: 'Duration',
   },
-  // TODO bring in actual trial counts once available.
+  {
+    dataIndex: 'numTrials',
+    key: 'numTrials',
+    sorter: true,
+    title: 'Trials',
+  },
   {
     key: 'state',
     render: stateRenderer,
-    sorter: (a: ExperimentX, b: ExperimentX): number => runStateSorter(a.state, b.state),
+    sorter: true,
     title: 'State',
   },
   {
     key: 'progress',
     render: experimentProgressRenderer,
-    sorter: (a: ExperimentX, b: ExperimentX): number => (a.progress || 0) - (b.progress || 0),
+    sorter: true,
     title: 'Progress',
   },
   {
     key: 'archived',
     render: experimentArchivedRenderer,
-    sorter: (a: ExperimentX, b: ExperimentX): number =>
-      (a.archived === b.archived) ? 0 : (a.archived ? 1 : -1),
     title: 'Archived',
   },
   {
     key: 'user',
     render: userRenderer,
-    sorter: (a: ExperimentX, b: ExperimentX): number =>
-      alphanumericSorter(a.username, b.username),
+    sorter: true,
     title: 'User',
   },
   {
