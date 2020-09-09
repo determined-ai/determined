9ccb2228302c50facc9488a8669e381c1b3b02b3
Hamid Zare <hamid@determined.ai>
2020-09-09 11:23:51 -0700

    convert it to a WITH statement

--- master/static/srv/proto_get_trial.sql
+++ master/static/srv/proto_get_trial.sql
@@ -1,3 +1,70 @@
+WITH extras AS (
+    WITH const AS (
+        SELECT config->'searcher'->>'metric' AS metric_name,
+            (
+                SELECT CASE
+                        WHEN coalesce(
+                            (
+                                config->'searcher'->>'smaller_is_better'
+                            )::boolean,
+                            true
+                        ) THEN -1 -- so we can order by DESC to get the highest value
+                        ELSE 1
+                    END
+            ) AS sign,
+            t.id AS trial_id
+        FROM experiments e
+            INNER JOIN trials t ON t.experiment_id = e.id
+        WHERE t.id = 20
+    ),
+    w_validations AS (
+        SELECT v.trial_id,
+            v.step_id,
+            v.end_time,
+            v.state,
+            (
+                v.metrics->'validation_metrics'->>(const.metric_name)
+            )::float8 AS searcher_metric,
+            (
+                (
+                    v.metrics->'validation_metrics'->>(const.metric_name)
+                )::float8 * const.sign
+            ) AS searcher_metric_value
+        FROM validations v
+            JOIN const ON const.trial_id = v.trial_id
+        WHERE v.state = 'COMPLETED'
+            AND (
+                v.metrics->'validation_metrics'->>(const.metric_name)
+            ) IS NOT NULL
+    ),
+    best_validation AS (
+        SELECT v.*
+        FROM w_validations v
+        ORDER BY v.searcher_metric_value DESC
+        LIMIT 1
+    ), latest_validation AS (
+        SELECT v.*
+        FROM w_validations v
+        ORDER BY end_time DESC
+        LIMIT 1
+    ), best_checkpoint AS (
+        SELECT c.*
+        FROM checkpoints c
+            JOIN w_validations v ON c.step_id = v.step_id
+        WHERE c.trial_id = 20
+        ORDER BY v.searcher_metric_value DESC
+        LIMIT 1
+    )
+    SELECT bv.searcher_metric AS best_validation,
+        lv.searcher_metric AS latest_validation,
+        bc.id AS best_checkpoint,
+        t.id
+    FROM trials t
+        INNER JOIN best_validation bv ON bv.trial_id = t.id
+        INNER JOIN latest_validation lv ON lv.trial_id = t.id
+        INNER JOIN best_checkpoint bc ON bc.trial_id = t.id
+    LIMIT 1
+)
 SELECT t.id,
     t.experiment_id,
     'STATE_' || t.state AS state,
@@ -16,71 +83,5 @@ SELECT t.id,
     extras.best_validation,
     extras.latest_validation
 FROM trials t
-    JOIN (
-        WITH const AS (
-            SELECT config->'searcher'->>'metric' AS metric_name,
-                (
-                    SELECT CASE
-                            WHEN coalesce(
-                                (
-                                    config->'searcher'->>'smaller_is_better'
-                                )::boolean,
-                                true
-                            ) THEN -1 -- so we can order by DESC to get the highest value
-                            ELSE 1
-                        END
-                ) AS sign,
-                t.id AS trial_id
-            FROM experiments e
-                INNER JOIN trials t ON t.experiment_id = e.id
-            WHERE t.id = 20
-        ),
-        w_validations AS (
-            SELECT v.trial_id,
-                v.step_id,
-                v.end_time,
-                v.state,
-                (
-                    v.metrics->'validation_metrics'->>(const.metric_name)
-                )::float8 AS searcher_metric,
-                (
-                    (
-                        v.metrics->'validation_metrics'->>(const.metric_name)
-                    )::float8 * const.sign
-                ) AS searcher_metric_value
-            FROM validations v
-                JOIN const ON const.trial_id = v.trial_id
-            WHERE v.state = 'COMPLETED'
-                AND (
-                    v.metrics->'validation_metrics'->>(const.metric_name)
-                ) IS NOT NULL
-        ),
-        best_validation AS (
-            SELECT v.*
-            FROM w_validations v
-            ORDER BY v.searcher_metric_value DESC
-            LIMIT 1
-        ), latest_validation AS (
-            SELECT v.*
-            FROM w_validations v
-            ORDER BY end_time DESC
-            LIMIT 1
-        ), best_checkpoint AS (
-            SELECT c.*
-            FROM checkpoints c
-                JOIN w_validations v ON c.step_id = v.step_id
-            WHERE c.trial_id = 20
-            ORDER BY v.searcher_metric_value DESC
-            LIMIT 1
-        )
-        SELECT bv.searcher_metric AS best_validation,
-            lv.searcher_metric AS latest_validation,
-            bc.id AS best_checkpoint,
-            t.id
-        FROM trials t
-            INNER JOIN best_validation bv ON bv.trial_id = t.id
-            INNER JOIN latest_validation lv ON lv.trial_id = t.id
-            INNER JOIN best_checkpoint bc ON bc.trial_id = t.id
-        LIMIT 1
-    ) extras ON t.id = extras.id
+    JOIN extras ON t.id = extras.id
 WHERE t.id = $1
