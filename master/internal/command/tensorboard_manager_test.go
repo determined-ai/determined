81de46a5adcbd8c3d5aaafc63444f1de8ae65519
Eric <31023784+eecsliu@users.noreply.github.com>
2020-09-11 15:36:09 -0700

    feat: allow tensorboard to accept arbitrary arguments (#1271)
    * fix: adding --purge_orphaned_data config option to prevent data removal
    
    * allowing tensorboard to accept arbitrary args

---
+++ master/internal/command/tensorboard_manager_test.go
@@ -0,0 +1,36 @@
+package command
+
+import (
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+func TestRefineArgs(t *testing.T) {
+	args := []string{
+		"first",
+		"-second",
+		"--third",
+		"-fourth-one",
+		"-----fifth",
+		"h",
+		"-h",
+		"--h",
+		"------h",
+	}
+
+	expected := []string{
+		"--first",
+		"--second",
+		"--third",
+		"--fourth-one",
+		"--fifth",
+		"-h",
+		"-h",
+		"-h",
+		"-h",
+	}
+	refineArgs(args)
+
+	assert.DeepEqual(t, args, expected)
+}
