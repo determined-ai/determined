3b82dbabff3510edf3d49fdb90f1185a28240a70
aaron276h <aaron@determined.ai>
2020-09-22 08:21:42 -0400

    test: remove nightly tensorpack tests

--- e2e_tests/tests/nightly/test_capability.py
+++ e2e_tests/tests/nightly/test_capability.py
@@ -23,27 +23,6 @@ def test_bert_glue() -> None:
 
 
 @pytest.mark.nightly  # type: ignore
-def test_faster_rcnn() -> None:
-    config = conf.load_config(conf.experimental_path("trial/FasterRCNN_tp/16-gpus.yaml"))
-    config = conf.set_max_length(config, {"batches": 128})
-    config = conf.set_slots_per_trial(config, 1)
-
-    exp.run_basic_test_with_temp_config(
-        config, conf.experimental_path("trial/FasterRCNN_tp"), 1, max_wait_secs=4800
-    )
-
-
-@pytest.mark.nightly  # type: ignore
-def test_mnist_tp_to_estimator() -> None:
-    config = conf.load_config(conf.experimental_path("trial/mnist_tp_to_estimator/const.yaml"))
-    config = conf.set_max_length(config, {"batches": 32})
-
-    exp.run_basic_test_with_temp_config(
-        config, conf.experimental_path("trial/mnist_tp_to_estimator"), 1
-    )
-
-
-@pytest.mark.nightly  # type: ignore
 def test_resnet50() -> None:
     config = conf.load_config(conf.experimental_path("trial/resnet50_tf_keras/const.yaml"))
     config = conf.set_max_length(config, {"batches": 200})
