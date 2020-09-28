24a3b26b8ed43407252c8e4da17d5040619d7721
Hamid Zare <hamid@determined.ai>
2020-09-28 14:20:00 -0700

    add post experiment Swagger config

--- proto/scripts/swagger.py
+++ proto/scripts/swagger.py
@@ -1,6 +1,22 @@
 import json
 import os
 import sys
+import collections
+
+
+def merge_dict(d1, d2):
+    """
+    Modifies d1 in-place to contain values from d2.  If any value
+    in d1 is a dictionary (or dict-like), *and* the corresponding
+    value in d2 is also a dictionary, then merge them in-place.
+    """
+
+    for k, v2 in d2.items():
+        v1 = d1.get(k) # returns None if v1 has no value for this key
+        if (isinstance(v1, collections.Mapping) and isinstance(v2, collections.Mapping)):
+            merge_dict(v1, v2)
+        else:
+            d1[k] = v2
 
 
 def capitalize(s: str) -> str:
@@ -57,6 +73,10 @@ def clean(fn: str) -> None:
         # Clean up titles.
         if "title" not in value:
             value["title"] = "".join(capitalize(k) for k in key.split(sep="v1"))
+
+    with open("swagger-patch.json", "r") as diff_f:
+        diff = json.load(diff_f)
+        merge_dict(spec, diff)
 
     with open(fn, "w") as fp:
         json.dump(spec, fp)
