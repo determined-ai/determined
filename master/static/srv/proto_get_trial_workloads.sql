5f233ed43f73f681f38e2dfdc3db97d6fe6450fd
Hamid Zare <hamid@determined.ai>
2020-09-29 15:16:23 -0700

    WIP get step information and use UNION

--- master/static/srv/proto_get_trial_workloads.sql
+++ master/static/srv/proto_get_trial_workloads.sql
@@ -1,21 +1,31 @@
 WITH validations_vt AS (
-  SELECT row_to_json(r1) AS validation
+  SELECT row_to_json(r1) AS validation,
+    num_batches,
+    prior_batches_processed
   FROM (
       SELECT 'STATE_' || v.state as state,
         v.start_time,
         v.end_time,
+        s.num_batches,
+        s.prior_batches_processed,
         v.metrics->'num_inputs' as num_inputs,
         v.metrics->'validation_metrics' as metrics
       FROM validations v
+        INNER JOIN steps s ON v.trial_id = s.trial_id
+        AND v.step_id = s.id
       WHERE v.trial_id = 20
     ) AS r1
 ),
 trainings_vt AS (
-  SELECT row_to_json(r1) AS training
+  SELECT row_to_json(r1) AS training,
+    num_batches,
+    prior_batches_processed
   FROM (
       SELECT s.start_time,
         s.end_time,
         'STATE_' || s.state as state,
+        s.num_batches,
+        s.prior_batches_processed,
         s.metrics->'avg_metrics' as metrics,
         s.metrics->'num_inputs' as num_inputs
       FROM steps s
@@ -23,18 +33,30 @@ trainings_vt AS (
     ) AS r1
 ),
 checkpoints_vt AS (
-  SELECT row_to_json(r1) AS checkpoint
+  SELECT row_to_json(r1) AS checkpoint,
+    num_batches,
+    prior_batches_processed
   FROM (
       SELECT 'STATE_' || c.state as state,
         c.start_time,
         c.end_time,
         c.uuid,
+        s.num_batches,
+        s.prior_batches_processed,
         c.resources
       FROM checkpoints c
+        INNER JOIN steps s ON c.trial_id = s.trial_id
+        AND c.step_id = s.id
       WHERE c.trial_id = 20
     ) AS r1
 )
-SELECT *
+SELECT t.training as workload,
+  t.num_batches,
+  t.prior_batches_processed
 FROM trainings_vt t
-  FULL JOIN checkpoints_vt c ON false
-  FULL JOIN validations_vt v ON false
+UNION ALL
+SELECT *
+FROM validations_vt v
+UNION ALL
+SELECT *
+FROM checkpoints_vt c
