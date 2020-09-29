9f493d83401aaa59a4600590ce1d94ed91293bb0
Hamid Zare <hamid@determined.ai>
2020-09-29 14:59:15 -0700

    WIP workloads

--- master/static/srv/proto_get_trial_workloads.sql
+++ master/static/srv/proto_get_trial_workloads.sql
@@ -1,47 +1,40 @@
-SELECT s.id,
-  s.num_batches,
-  s.prior_batches_processed,
-  (
-    SELECT row_to_json(r1)
-    FROM (
-        SELECT 'STATE_' || v.state as state,
-          v.start_time,
-          v.end_time,
-          v.metrics->'num_inputs' as num_inputs,
-          v.metrics->'validation_metrics' as metrics
-        FROM validations v
-        WHERE v.trial_id = s.trial_id
-          AND v.step_id = s.id
-      ) r1
-  ) AS validation,
-  (
-    SELECT row_to_json(r2)
-    FROM (
-        SELECT 'STATE_' || c.state as state,
-          c.start_time,
-          c.end_time,
-          c.uuid,
-          c.resources
-        FROM checkpoints c
-          LEFT JOIN validations v ON c.trial_id = v.trial_id
-          AND c.step_id = v.step_id
-        WHERE c.trial_id = s.trial_id
-          AND c.step_id = s.id
-      ) r2
-  ) AS checkpoint,
-  (
-    SELECT row_to_json(r3)
-    FROM (
-        SELECT s2.start_time,
-          s2.end_time,
-          'STATE_' || s2.state as state,
-          s2.metrics->'avg_metrics' as metrics,
-          s2.metrics->'num_inputs' as num_inputs
-        FROM steps s2
-        WHERE s2.trial_id = s.trial_id
-          AND s2.id = s.id
-      ) r3
-  ) AS training
-from steps s
-WHERE s.trial_id = $1
-ORDER BY s.id ASC
+WITH validations_vt AS (
+  SELECT row_to_json(r1) AS validation
+  FROM (
+      SELECT 'STATE_' || v.state as state,
+        v.start_time,
+        v.end_time,
+        v.metrics->'num_inputs' as num_inputs,
+        v.metrics->'validation_metrics' as metrics
+      FROM validations v
+      WHERE v.trial_id = 20
+    ) AS r1
+),
+trainings_vt AS (
+  SELECT row_to_json(r1) AS training
+  FROM (
+      SELECT s.start_time,
+        s.end_time,
+        'STATE_' || s.state as state,
+        s.metrics->'avg_metrics' as metrics,
+        s.metrics->'num_inputs' as num_inputs
+      FROM steps s
+      WHERE s.trial_id = 20
+    ) AS r1
+),
+checkpoints_vt AS (
+  SELECT row_to_json(r1) AS checkpoint
+  FROM (
+      SELECT 'STATE_' || c.state as state,
+        c.start_time,
+        c.end_time,
+        c.uuid,
+        c.resources
+      FROM checkpoints c
+      WHERE c.trial_id = 20
+    ) AS r1
+)
+SELECT *
+FROM trainings_vt t
+  FULL JOIN checkpoints_vt c ON false
+  FULL JOIN validations_vt v ON false
