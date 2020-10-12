689b06f90aacf44a0918fefd2552dc8d43495cf8
Aaron Harlap <aaron@determined.ai>
2020-10-12 10:35:34 -0400

    chore: update horovod version  (#1413)

--- e2e_tests/tests/fixtures/tf_keras_grad_aggregation/grad_aggregation_test.py
+++ e2e_tests/tests/fixtures/tf_keras_grad_aggregation/grad_aggregation_test.py
@@ -1,4 +1,6 @@
 import argparse
+import math
+from distutils.version import LooseVersion
 from typing import Any
 
 import tensorflow as tf
@@ -11,16 +13,26 @@ except ModuleNotFoundError:
     pass
 
 
+_PRE_TF_2_4_0 = LooseVersion(tf.__version__) < LooseVersion("2.4.0")
+
+
 class CustomOptimizer(optimizer_v2.OptimizerV2):
     def get_config(self) -> Any:
         config = super(CustomOptimizer, self).get_config()
         return config
+
+    def _create_slots(self, var_list: Any) -> None:
+        pass
+
+    def _resource_apply_dense(self, grad: Any, var: Any, apply_state: Any = None) -> Any:
+        return var.assign_add(grad)
 
 
 def check_tf_1(aggregation_frequency: int, average_aggregated_gradients: bool) -> None:
     config = tf.compat.v1.ConfigProto()
     config.gpu_options.allow_growth = True
     config.gpu_options.visible_device_list = str(hvd.local_rank())
+
     K.set_session(tf.Session(config=config))
     session = tf.compat.v1.keras.backend.get_session(op_input_list=())
 
@@ -29,16 +41,26 @@ def check_tf_1(aggregation_frequency: int, average_aggregated_gradients: bool) -
         aggregation_frequency=aggregation_frequency,
         average_aggregated_gradients=average_aggregated_gradients,
     )
+    iterations = hvd_optimizer.iterations
+    session.run(iterations.initializer)
 
-    constant_multiplier = 4.0
-    grads = [tf.constant([hvd.rank() * constant_multiplier])]
-    op = hvd_optimizer._allreduce(grads)
+    grads = [tf.constant([float(hvd.rank())])]
+    variables = [tf.Variable([0.0])]
+    session.run(variables[0].initializer)
+
+    allreduce_op = hvd_optimizer._allreduce(grads)
+    grads_and_vars = [(allreduce_op[0], variables[0])]
+    apply_grads_op = hvd_optimizer.apply_gradients(grads_and_vars)
+
     for idx in range(10):
-        value = session.run(op)[0][0]
-        expected_value = compute_expected_value(
-            idx, aggregation_frequency, constant_multiplier, average_aggregated_gradients, False
+        _ = session.run(apply_grads_op)
+
+        expected_value = compute_expected_variable_value(
+            idx, aggregation_frequency, average_aggregated_gradients
         )
-        assert expected_value == value
+
+        assert idx + 1 == session.run(hvd_optimizer.iterations)
+        assert expected_value == session.run(variables[0].read_value())
 
 
 def check_tf_2(aggregation_frequency: int, average_aggregated_gradients: bool) -> None:
@@ -53,45 +75,48 @@ def check_tf_2(aggregation_frequency: int, average_aggregated_gradients: bool) -
         aggregation_frequency=aggregation_frequency,
         average_aggregated_gradients=average_aggregated_gradients,
     )
+    _ = hvd_optimizer.iterations
 
-    constant_multiplier = 4.0
-    grads_and_vars = [(tf.constant([hvd.rank() * constant_multiplier]), None)]
+    gradients = [tf.constant([float(hvd.rank())])]
+    variables = [tf.Variable([0.0])]
     for idx in range(10):
-        grads = hvd_optimizer._aggregate_gradients(grads_and_vars)
-        value = grads[0][0].numpy()
-        expected_value = compute_expected_value(
-            idx, aggregation_frequency, constant_multiplier, average_aggregated_gradients, True
+        if _PRE_TF_2_4_0:
+            # In TF < 2.4 `_aggregate_gradients()` is called outside of `apply_gradients()`.
+            updated_gradients = hvd_optimizer._aggregate_gradients(zip(gradients, variables))
+            hvd_optimizer.apply_gradients(
+                zip(updated_gradients, variables), experimental_aggregate_gradients=False
+            )
+        else:
+            hvd_optimizer.apply_gradients(zip(gradients, variables))
+
+        updated_variable_value = variables[0][0].numpy()
+        expected_value = compute_expected_variable_value(
+            idx, aggregation_frequency, average_aggregated_gradients
         )
-        assert expected_value == value
+
+        assert expected_value == updated_variable_value
+        assert idx + 1 == hvd_optimizer.iterations.numpy()
 
 
-def compute_expected_value(
+def compute_expected_variable_value(
     batch_id: int,
     aggregation_frequency: int,
-    multiplier: float,
     average_aggregated_gradient: bool,
-    tf2: bool,
 ) -> float:
     """
-    Compute the expected value based on how we are aggregating gradients.
+    Computes the expected current value of variables based on how we are aggregating gradients.
     """
-    gradients_aggregated = (batch_id + 1) % aggregation_frequency == 0
-    if gradients_aggregated:
-        all_reduced_grads = 0.0
-        for _ in range(aggregation_frequency):
-            grads_for_batch = 0.0
-            for rank in range(hvd.size()):
-                grads_for_batch += rank * multiplier
-            if average_aggregated_gradient:
-                grads_for_batch /= float(aggregation_frequency)
-            all_reduced_grads += grads_for_batch / float(hvd.size())
-        return all_reduced_grads
-    else:
-        non_aggregated_grads = hvd.rank() * multiplier
-        if tf2:
-            # In Tf2 we return the sum of the locally aggregated gradients.
-            non_aggregated_grads *= (batch_id + 1) % aggregation_frequency
-        return non_aggregated_grads
+    aggregations_completed = math.floor((batch_id + 1) / aggregation_frequency)
+    sum_per_aggregation = 0.0
+    for _ in range(aggregation_frequency):
+        grads_for_batch = 0.0
+        for rank in range(hvd.size()):
+            grads_for_batch += rank
+        if average_aggregated_gradient:
+            grads_for_batch /= float(aggregation_frequency)
+        sum_per_aggregation += grads_for_batch / float(hvd.size())
+
+    return aggregations_completed * sum_per_aggregation
 
 
 def main() -> None:
