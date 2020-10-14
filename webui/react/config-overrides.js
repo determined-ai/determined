84c351380bfae87b664b45bddbb99ca5637ee7c3
David Wang <dsw@determined.ai>
2020-10-14 15:06:18 -0700

    chore: bump version: 0.13.6rc0 -> 0.13.6rc1

--- webui/react/config-overrides.js
+++ webui/react/config-overrides.js
@@ -51,7 +51,7 @@ const webpackConfig = override(
   // Webapp version is hardcoded but handled by `bumpversion`
   addWebpackPlugin(
     new webpack.DefinePlugin({
-      'process.env.VERSION': '"0.13.6rc0"',
+      'process.env.VERSION': '"0.13.6rc1"',
       'process.env.IS_DEV': JSON.stringify(process.env.NODE_ENV === 'development'),
     })
   ),
