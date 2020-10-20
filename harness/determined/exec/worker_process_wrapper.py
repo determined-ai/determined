d5cf919619b2ff5ce6fd32ac33851cb77bc14d0e
Bradley Laney <bradlaney@determined.ai>
2020-10-20 19:20:54 -0400

    feedback

--- harness/determined/exec/worker_process_wrapper.py
+++ harness/determined/exec/worker_process_wrapper.py
@@ -3,46 +3,26 @@ worker_process_wrapper.py is the entrypoint for Horovod worker processes.
 It exists to redirect stdout/stderr to the docker logging without needing
 to package a shell script.
 """
-import itertools
 import os
 import subprocess
 import sys
 import threading
-from typing import List, TextIO
+from typing import BinaryIO, List
 
 from determined import constants
 
 
-def forward_stream(src_stream: TextIO, dst_stream: TextIO, rank: str) -> None:
-    for line in iter(src_stream.readline, None):
-        if line is None:
-            break
-        if not isinstance(line, str):
-            line = line.decode("utf-8")
-        if not line:
-            break
-        line = "[rank={rank}] {line}".format(
-            line=line,
-            rank=rank,
-        )
-        os.write(dst_stream.fileno(), line.encode("utf-8"))
+def forward_stream(src_stream: BinaryIO, dst_stream: BinaryIO, rank: str) -> None:
+    for line in iter(src_stream.readline, b""):
+        line = f"[rank={rank}] ".encode() + line
+        os.write(dst_stream.fileno(), line)
 
 
-def run_all(fwds: List[threading.Thread]) -> None:
-    for fwd in fwds:
-        fwd.start()
-
-    done = {}
-    for fwd in itertools.cycle(fwds):
-        if fwd.name in done:
-            continue
-
-        fwd.join(timeout=1)
-        if not fwd.isAlive():
-            done[fwd.name] = True
-
-        if len(done) == len(fwds):
-            return
+def run_all(ts: List[threading.Thread]) -> None:
+    for t in ts:
+        t.start()
+    for t in ts:
+        t.join()
 
 
 def main() -> None:
@@ -57,15 +37,15 @@ def main() -> None:
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
-    with open(constants.CONTAINER_STDOUT, "w") as cstdout:
-        with open(constants.CONTAINER_STDERR, "w") as cstderr:
-            with proc:
-                run_all(
-                    [
-                        threading.Thread(target=forward_stream, args=(proc.stdout, cstdout, rank)),
-                        threading.Thread(target=forward_stream, args=(proc.stderr, cstderr, rank)),
-                    ]
-                )
+    with open(constants.CONTAINER_STDOUT, "w") as cstdout, open(
+        constants.CONTAINER_STDERR, "w"
+    ) as cstderr, proc:
+        run_all(
+            [
+                threading.Thread(target=forward_stream, args=(proc.stdout, cstdout, rank)),
+                threading.Thread(target=forward_stream, args=(proc.stderr, cstderr, rank)),
+            ]
+        )
 
 
 if __name__ == "__main__":
