86e4685cc27d08752aee9715af16f13527659c91
Hamid Zare <hamid@determined.ai>
2020-10-26 14:02:58 -0700

    remove react settings and format

--- webui/tests/.eslintrc.js
+++ webui/tests/.eslintrc.js
@@ -1,14 +1,19 @@
 const reactEslint = require('../react/.eslintrc');
 
-const UNSUPPORTED_RULES = ['^react*'];
+const UNSUPPORTED_RULES = [ '^react*' ];
 const rules = Object.keys(reactEslint.rules).reduce((acc, cur) => {
   if (UNSUPPORTED_RULES.find(search => RegExp(search).test(cur))) return acc;
   acc[cur] = reactEslint.rules[cur];
   return acc;
 }, {});
+delete reactEslint.settings;
 
 module.exports = {
   ...reactEslint,
+  env: {
+    ...reactEslint.env,
+    'cypress/globals': true,
+  },
   extends: [
     'plugin:cypress/recommended',
     'eslint:recommended',
@@ -16,14 +21,10 @@ module.exports = {
 
   ],
   plugins: [
-    'cypress', 
+    'cypress',
     'import',
     'sort-keys-fix',
   ],
-  env: {
-    ...reactEslint.env,
-    'cypress/globals': true,
-  },
   rules: {
     ...rules,
     // disable until https://github.com/cypress-io/eslint-plugin-cypress/issues/43 is resolved.
