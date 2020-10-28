fb509776175c1844d769148db207fd43031b51eb
Alessandro Pizzini <alessandro.pizzini@gmail.com>
2020-10-28 07:50:35 +0100

    fix: webui experiment list goes to first page when changing filters [DET-4377] (#1508)

--- webui/react/src/utils/step.ts
+++ webui/react/src/utils/step.ts
@@ -1,5 +1,5 @@
 import { CheckpointState, Step } from '../types';
 
-export const hasCheckpoint = (step: Step) => {
+export const hasCheckpoint = (step: Step): boolean => {
   return !!step.checkpoint && step.checkpoint.state !== CheckpointState.Deleted;
 };
