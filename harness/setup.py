8dd54fbc37b1091d18a70733b9f17b0776bf4dba
Ryan <rb@determined.ai>
2020-10-30 12:22:41 -0600

    fix: make pip happy again

--- harness/setup.py
+++ harness/setup.py
@@ -17,7 +17,8 @@ setup(
         "cloudpickle==0.5.3",
         "determined-common==0.13.8.dev0",
         "dill>=0.2.9",
-        "h5py>=2.9.0",
+        # TF 2.2 has strict h5py requirements, which we expose here.
+        "h5py>=2.10.0,<2.11.0",
         "matplotlib",
         "packaging",
         "numpy>=1.16.2",
