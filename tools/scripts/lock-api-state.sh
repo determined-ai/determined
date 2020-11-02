fedefa25ae1cafb2b0eacb21641dde208340b6c4
Hamid Zare <hamid@determined.ai>
2020-11-02 15:59:10 -0800

    simplify dirty file check and add it as part of gen-buf-image target

--- tools/scripts/lock-api-state.sh
+++ tools/scripts/lock-api-state.sh
@@ -4,17 +4,8 @@
 BUF_IMAGE=buf.image.bin
 PROJECT_ROOT=$(pwd)
 
-if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
-    echo "git: there are dirty files."
-    exit 1
-elif [[ $(git status --porcelain 2>/dev/null| grep "^??") ]]; then
-    echo "git: there are untracked files."
-    exit 1
-fi
-
 ## lock in current protobuf state
 cd proto
-make check
 make gen-buf-image
 git add $BUF_IMAGE
 git commit -m "chore: lock api state for backward compatibility check" || echo "buf image is already up to date"
