c9f42e561f5c0fc482d171c0434adb7d8e8d8be7
Hamid Zare <hamid@determined.ai>
2020-11-04 12:21:25 -0800

    update comments

--- tools/scripts/lock-api-state.sh
+++ tools/scripts/lock-api-state.sh
@@ -2,8 +2,9 @@
 
 ## lock in current protobuf state
 
-# make gen-buf-iamge ensures that it starts with a clean git state
+# make gen-buf-image ensures that it starts with a clean git state
 make -C proto gen-buf-image
+# check to see if gen-buf-image resulted in any file changes or not
 if [[ -z "$(git status --porcelain)" ]]; then
     echo "buf image is already up to date"
     exit 0
