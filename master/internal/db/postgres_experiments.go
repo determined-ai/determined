266e5f576df3c3db3362788b98d9489a0abdcf0f
Sean Mackrory <mackrory@determined.ai>
2020-11-05 11:30:23 -0700

    Further simplification of DB code

--- master/internal/db/postgres_experiments.go
+++ master/internal/db/postgres_experiments.go
@@ -48,7 +48,8 @@ func (db *PgDB) MetricNames(experimentID int, sStartTime time.Time, vStartTime t
 		EndTime time.Time `db:"end_time"`
 	}
 
-	rows, err := db.sql.Queryx(`
+	var rows []namesWrapper
+	err = db.queryRows(`
 SELECT 
   jsonb_object_keys(s.metrics->'avg_metrics') AS name,
   max(s.end_time) AS end_time
@@ -56,28 +57,19 @@ FROM trials t
 INNER JOIN steps s ON t.id=s.trial_id
 WHERE t.experiment_id=$1
   AND s.end_time > $2
-GROUP BY name;`, experimentID, sStartTime)
+GROUP BY name;`, &rows, experimentID, sStartTime)
 	if err != nil {
 		return nil, nil, sEndTime, vEndTime, errors.Wrapf(err,
 			"error querying training metric names for experiment %d", experimentID)
 	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var row namesWrapper
-		err = rows.StructScan(&row)
-		if err != nil {
-			return nil, nil, sEndTime, vEndTime, errors.Wrapf(err,
-				"error scanning training metric names for experiment %d",
-				experimentID)
-		}
+	for _, row := range rows {
 		training = append(training, row.Name)
 		if row.EndTime.After(sEndTime) {
 			sEndTime = row.EndTime
 		}
 	}
 
-	rows, err = db.sql.Queryx(`
+	err = db.queryRows(`
 SELECT
   jsonb_object_keys(v.metrics->'validation_metrics') AS name,
   max(v.end_time) AS end_time
@@ -91,16 +83,7 @@ GROUP BY name;`, experimentID, vStartTime)
 		return nil, nil, sEndTime, vEndTime, errors.Wrapf(err,
 			"error querying validation metric names for experiment %d", experimentID)
 	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var row namesWrapper
-		err = rows.StructScan(&row)
-		if err != nil {
-			return nil, nil, sEndTime, vEndTime, errors.Wrapf(err,
-				"error scanning validation metric names for experiment %d",
-				experimentID)
-		}
+	for _, row := range rows {
 		validation = append(validation, row.Name)
 		if row.EndTime.After(sEndTime) {
 			sEndTime = row.EndTime
@@ -119,7 +102,8 @@ type batchesWrapper struct {
 // metric was recorded.
 func (db *PgDB) TrainingMetricBatches(experimentID int, metricName string, startTime time.Time) (
 	batches []int32, endTime time.Time, err error) {
-	query := `
+	var rows []*batchesWrapper
+	err = db.queryRows(`
 SELECT (s.prior_batches_processed + num_batches) AS batches_processed,
   max(s.end_time) as end_time
 FROM trials t INNER JOIN steps s ON t.id=s.trial_id
@@ -127,10 +111,7 @@ WHERE t.experiment_id=$1
   AND s.state = 'COMPLETED'
   AND s.metrics->'avg_metrics' ? $2
   AND s.end_time > $3
-GROUP BY batches_processed;`
-
-	var rows []*batchesWrapper
-	err = db.queryRows(query, &rows, experimentID, metricName, startTime)
+GROUP BY batches_processed;`, &rows, experimentID, metricName, startTime)
 	if err != nil {
 		return nil, endTime, errors.Wrapf(err, "error querying DB for training metric batches")
 	}
@@ -148,7 +129,8 @@ GROUP BY batches_processed;`
 // validation metric was recorded.
 func (db *PgDB) ValidationMetricBatches(experimentID int, metricName string, startTime time.Time) (
 	batches []int32, endTime time.Time, err error) {
-	query := `
+	var rows []*batchesWrapper
+	err = db.queryRows(`
 SELECT (s.prior_batches_processed + num_batches) AS batches_processed,
   max(v.end_time) as end_time
 FROM trials t INNER JOIN steps s ON t.id=s.trial_id
@@ -157,10 +139,7 @@ WHERE t.experiment_id=$1
   AND v.state = 'COMPLETED'
   AND v.metrics->'validation_metrics' ? $2
   AND v.end_time > $3
-GROUP BY batches_processed;`
-
-	var rows []*batchesWrapper
-	err = db.queryRows(query, &rows, experimentID, metricName, startTime)
+GROUP BY batches_processed;`, &rows, experimentID, metricName, startTime)
 	if err != nil {
 		return nil, endTime, errors.Wrapf(err, "error querying DB for validation metric batches")
 	}
