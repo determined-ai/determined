6a37a5f9f1b95b47ecd13d1a20dd84e71a88b69d
Ryan <rb@determined.ai>
2020-11-16 15:27:42 -0700

    chore: bumpenvs for NCCL update (#1593)
    Based on environments commit 0e2ce4d98e0c0670be4329423a136f1260e9ba08.

--- master/pkg/model/defaults.go
+++ master/pkg/model/defaults.go
@@ -25,8 +25,8 @@ const (
 
 // Default task environment docker image names.
 const (
-	defaultCPUImage = "determinedai/environments:py-3.6.9-pytorch-1.4-tf-1.15-cpu-1def2ee"
-	defaultGPUImage = "determinedai/environments:cuda-10.0-pytorch-1.4-tf-1.15-gpu-1def2ee"
+	defaultCPUImage = "determinedai/environments:py-3.6.9-pytorch-1.4-tf-1.15-cpu-0e2ce4d"
+	defaultGPUImage = "determinedai/environments:cuda-10.0-pytorch-1.4-tf-1.15-gpu-0e2ce4d"
 )
 
 // DefaultExperimentConfig returns a new default experiment config.
