82032ec067db486a1bb9bfb03b995f117985138a
Sean Mackrory <mackrory@determined.ai>
2020-11-17 13:22:28 -0700

    Updating e2e tests

--- e2e_tests/tests/config.py
+++ e2e_tests/tests/config.py
@@ -13,8 +13,8 @@ MAX_TRIAL_BUILD_SECS = 90
 
 TF1_CPU_IMAGE = "determinedai/environments:py-3.6.9-pytorch-1.4-tf-1.15-cpu-0e2ce4d"
 TF2_CPU_IMAGE = "determinedai/environments:py-3.6.9-pytorch-1.4-tf-2.2-cpu-0e2ce4d"
-TF1_GPU_IMAGE = "determinedai/environments:cuda-10.0-pytorch-1.4-tf-1.15-gpu-0e2ce4d"
-TF2_GPU_IMAGE = "determinedai/environments:cuda-10.1-pytorch-1.4-tf-2.2-gpu-0e2ce4d"
+TF1_GPU_IMAGE = "mackrorysd/determined:cuda11-tf1"
+TF2_GPU_IMAGE = "mackrorysd/determined:cuda11"
 
 
 def fixtures_path(path: str) -> str:
