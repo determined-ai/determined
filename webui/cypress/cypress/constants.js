c2e651f35425dea71c53a0a4e784e5c331dc02d3
Caleb Kang <caleb@determined.ai>
2020-11-18 16:42:59 -0700

    chore: remove cypress from project

--- webui/cypress/cypress/constants.js
+++
@@ -1,6 +0,0 @@
-export const API_PATH = '/api/v1';
-export const ACCOUNT_USERNAME = 'user-w-pw';
-export const ACCOUNT_PASSWORD = 'special-pw';
-export const USERNAME_WITHOUT_PASSWORD = 'user-wo-pw';
-export const DEFAULT_WAIT_TIME = 5000;
-export const STORAGE_KEY_AUTH = 'global/auth-token';
