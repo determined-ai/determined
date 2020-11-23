a77657d0e5696ad965ea668dad98e2f87c6be669
naren-determined <65423210+naren-determined@users.noreply.github.com>
2020-11-23 12:47:45 -0800

    fix: unets_tf_keras example data download (#1635)

--- examples/computer_vision/unets_tf_keras/startup-hook.sh
+++ examples/computer_vision/unets_tf_keras/startup-hook.sh
@@ -1,4 +1,3 @@
 pip install git+https://github.com/tensorflow/examples.git
 pip install -q -U tfds-nightly
-python -m tensorflow_datasets.scripts.download_and_prepare --register_checksums=True --datasets='oxford_iiit_pet:3.*.*' --data_dir='/tmp/data'
 
