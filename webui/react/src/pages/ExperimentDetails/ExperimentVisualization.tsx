f4ac37340b358e7a66630b2489ae3f11e40e0d6d
Caleb Kang <caleb@determined.ai>
2020-12-03 17:39:32 -0700

    feat: add metric select filter component

--- webui/react/src/pages/ExperimentDetails/ExperimentVisualization.tsx
+++ webui/react/src/pages/ExperimentDetails/ExperimentVisualization.tsx
@@ -70,6 +70,7 @@ const ExperimentVisualization: React.FC<Props> = ({
 
   // Stream available metrics
   useEffect(() => {
+    console.log('consuming metrics stream', experiment.id);
     consumeStream<V1MetricNamesResponse>(
       detApi.StreamingInternal.determinedMetricNames(
         experiment.id,
