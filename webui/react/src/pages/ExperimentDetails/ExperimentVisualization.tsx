9dca902956995f53f42b725bed192d74d1063116
Caleb Kang <caleb@determined.ai>
2020-12-07 11:29:16 -0700

    feat: add batches component filter

--- webui/react/src/pages/ExperimentDetails/ExperimentVisualization.tsx
+++ webui/react/src/pages/ExperimentDetails/ExperimentVisualization.tsx
@@ -1,16 +1,18 @@
-import { Col, Row } from 'antd';
+import { Col, Row, Select } from 'antd';
 import React, { useCallback, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import Link from 'components/Link';
 import MetricSelectFilter from 'components/MetricSelectFilter';
-import Spinner from 'components/Spinner';
+import SelectFilter from 'components/SelectFilter';
 import { V1MetricBatchesResponse, V1MetricNamesResponse } from 'services/api-ts-sdk';
 import { detApi } from 'services/apiConfig';
 import { consumeStream } from 'services/utils';
 import { ExperimentDetails, MetricName, MetricType } from 'types';
 
 import css from './ExperimentVisualization.module.scss';
+
+const { Option } = Select;
 
 export enum VisualizationType {
   HpParallelCoord = 'hp-parallel-coord',
@@ -46,6 +48,9 @@ const ExperimentVisualization: React.FC<Props> = ({
   const [ validationMetrics, setValidationMetrics ] = useState<string[]>([]);
   const [ selectedMetric, setSelectedMetric ] = useState<MetricName>();
   const [ searchMetric, setSearchMetric ] = useState<string>();
+  const [ batches, setBatches ] = useState<number[]>([]);
+  const [ selectedBatch, setSelectedBatch ] = useState<number>();
+  const [ canceler ] = useState(new AbortController());
 
   const metrics = [
     ...(validationMetrics || []).map(name => ({ name, type: MetricType.Validation })),
@@ -65,16 +70,42 @@ const ExperimentVisualization: React.FC<Props> = ({
 
   // Stream available metrics
   useEffect(() => {
-    console.log('consuming metrics stream', experiment.id);
     consumeStream<V1MetricNamesResponse>(
-      detApi.StreamingInternal.determinedMetricNames(experiment.id),
+      detApi.StreamingInternal.determinedMetricNames(experiment.id, { signal: canceler.signal }),
       event => {
         setSearchMetric(event.searcherMetric);
         setTrainingMetrics(event.trainingMetrics || []);
         setValidationMetrics(event.validationMetrics || []);
       },
     );
-  }, [ experiment.id ]);
+
+    return canceler.abort;
+  }, [ canceler, experiment.id ]);
+
+  // Stream available batches
+  useEffect(() => {
+    if (!selectedMetric) return;
+
+    const batchesCanceler = new AbortController();
+    const metricTypeParam = selectedMetric?.type === MetricType.Training
+      ? 'METRIC_TYPE_TRAINING' : 'METRIC_TYPE_VALIDATION';
+    consumeStream<V1MetricBatchesResponse>(
+      detApi.StreamingInternal.determinedMetricBatches(
+        experiment.id,
+        selectedMetric?.name,
+        metricTypeParam,
+        { signal: batchesCanceler.signal },
+      ),
+      event => {
+        if (event.batches) {
+          setBatches(event.batches);
+          if (event.batches.length !== 0) setSelectedBatch(event.batches[0]);
+        }
+      },
+    );
+
+    return () => batchesCanceler.abort();
+  }, [ experiment.id, selectedMetric ]);
 
   // Set the default metric of interest
   useEffect(() => {
@@ -101,17 +132,23 @@ const ExperimentVisualization: React.FC<Props> = ({
               ))}
             </div>
             <div className={css.divider} />
+            <header>Filters</header>
             <div className={css.filters}>
-              <header>Filters</header>
-              <div>
-                <MetricSelectFilter
-                  defaultMetricNames={metrics}
-                  metricNames={metrics}
-                  multiple={false}
-                  value={selectedMetric}
-                  width={'100%'}
-                  onChange={handleMetricChange} />
-              </div>
+              <MetricSelectFilter
+                defaultMetricNames={metrics}
+                metricNames={metrics}
+                multiple={false}
+                value={selectedMetric}
+                verticalLayout={true}
+                width={'100%'}
+                onChange={handleMetricChange} />
+              <SelectFilter
+                label="Batches"
+                style={{ width: '100%' }}
+                value={selectedBatch}
+                verticalLayout={true}>
+                {batches.map(batch => <Option key={batch} value={batch}>{batch}</Option>)}
+              </SelectFilter>
             </div>
           </div>
         </Col>
