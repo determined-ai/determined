1bc0d4f422acc63715ae5b2780c22c06853627b1
liam <liam@determined.ai>
2020-12-09 17:01:45 -0800

    address comments

--- examples/computer_vision/detr_coco_pytorch/data_utils.py
+++ examples/computer_vision/detr_coco_pytorch/data_utils.py
@@ -12,8 +12,7 @@ def download_file(url, output_dir):
         r.raise_for_status()
         with open(local_filename, "wb") as f:
             for chunk in r.iter_content(chunk_size=8192):
-                # If you have chunk encoded response uncomment if
-                # and set chunk_size parameter to None.
+                # If you have chunk encoded response uncomment the line below and set chunk_size parameter to None.
                 # if chunk:
                 f.write(chunk)
     return local_filename
