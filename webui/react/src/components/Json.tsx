a96b474a483f2231de1e9844bf9e06c6c7eb961f
Hamid Zare <hamid@determined.ai>
2020-12-14 11:41:47 -0800

    update json viewer

--- webui/react/src/components/Json.tsx
+++ webui/react/src/components/Json.tsx
@@ -1,8 +1,9 @@
-import { List } from 'antd';
 import React from 'react';
 
 import { RawJson } from 'types';
 import { isObject } from 'utils/data';
+
+import css from './Json.module.scss';
 
 interface Props {
   json: RawJson;
@@ -10,17 +11,20 @@ interface Props {
 
 // TODO can be reused in TrialInfoBox.
 
+const row = (label: string, value: RawJson): React.ReactNode => {
+  const textValue = isObject(value) ? JSON.stringify(value, null, 2) : value.toString();
+  return <li className={css.item} key={label}>
+    <span className={css.label}>{label}:</span>
+    <span className={css.value}>{textValue}</span>
+  </li>;
+};
+
 const Json: React.FC<Props> = ({ json }: Props) => {
 
   return (
-    <List
-      dataSource={Object.entries(json)}
-      renderItem={([ label, value ]) => {
-        const textValue = isObject(value) ? JSON.stringify(value, null, 2) : value.toString();
-        return <List.Item>{label}: {textValue}</List.Item>;
-      }}
-      size="small"
-    />
+    <ul className={css.base}>
+      {Object.entries(json).map(([ label, value ]) => row(label, value))}
+    </ul>
   );
 };
 
