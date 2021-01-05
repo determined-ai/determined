b9a940845fc13f498cc7b50c7324edd137390089
Ryan <rb@determined.ai>
2021-01-05 16:37:16 -0700

    chore: hack tensorboard support to include custom metrics (#1750)
    Our Tensorboard support is still based on the totally outdated Callback
    class (BatchMetricWriter is the only remaining implementation of
    Callback).
    
    The bug was that PyTorch custom reducers for training metrics did not
    show up in Tensorboard at all.  This is because they are only set in
    avg_metrics, and the Callback interface never used to pass avg_metrics.
    
    Since there is only one implementation of Callback, we can just change
    pass batch_metrics and avg_metrics to allow BatchMetricWriter to detect
    these custom reduced metrics.  It would be preferable to break the
    Tensorboard logic out of the WorkloadManager, but this hack is easy
    enough to land in time for the feature release of PyTorch custom
    reducers.

--- harness/determined/tensorboard/metric_writers/callback.py
+++ harness/determined/tensorboard/metric_writers/callback.py
@@ -1,5 +1,5 @@
 import abc
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, Union
 
 import numpy as np
 
@@ -33,14 +33,25 @@ class BatchMetricWriter(callback.Callback):
         step_id: int,
         num_batches: int,
         total_batches_processed: int,
-        metrics: List[Dict[str, Any]],
+        metrics: Dict[str, Any],
     ) -> None:
         if step_id <= 0:
             raise AssertionError(f"Expected step_id to be a positive int, but it is {step_id}")
-        for batch_idx, batch_metrics in enumerate(metrics):
+        metrics_seen = set()
+
+        # Log all batch metrics.
+        for batch_idx, batch_metrics in enumerate(metrics["batch_metrics"]):
             batches_seen = total_batches_processed + batch_idx
             for name, value in batch_metrics.items():
                 self._maybe_write_metric(name, value, batches_seen)
+                metrics_seen.add(name)
+
+        # Log avg metrics which were calculated by a custom reducer and are not in batch metrics.
+        batches_seen = total_batches_processed + num_batches
+        for name, value in metrics["avg_metrics"].items():
+            if name in metrics_seen:
+                continue
+            self._maybe_write_metric(name, value, batches_seen)
 
         self.writer.reset()
 
