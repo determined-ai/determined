cf23e5636bec5b6eff610c893505513906206dab
Hamid Zare <hamid@determined.ai>
2021-01-11 14:29:42 -0800

    add respool details modal

--- webui/react/src/pages/HGICluster.tsx
+++ webui/react/src/pages/HGICluster.tsx
@@ -1,15 +1,16 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 import Grid, { GridMode } from 'components/Grid';
 import Message from 'components/Message';
 import OverviewStats from 'components/OverviewStats';
 import Page from 'components/Page';
 import ResourcePoolCard from 'components/ResourcePoolCard';
+import ResourcePoolDetails from 'components/ResourcePoolDetails';
 import ResponsiveTable from 'components/ResponsiveTable';
 import Section from 'components/Section';
 import SlotAllocationBar from 'components/SlotAllocationBar';
 import Spinner, { Indicator } from 'components/Spinner';
-import { defaultRowClassName, getPaginationConfig } from 'components/Table';
+import { defaultRowClassName, getPaginationConfig, isAlternativeAction } from 'components/Table';
 import Agents from 'contexts/Agents';
 import ClusterOverview from 'contexts/ClusterOverview';
 import { columns } from 'pages/HGICluster.table';
@@ -25,6 +26,7 @@ const resourcePools = getResourcePools();
 const HGICluster: React.FC = () => {
   const agents = Agents.useStateContext();
   const overview = ClusterOverview.useStateContext();
+  const [ detailVisible, setDetailVisible ] = useState(false);
 
   const availableResources = useMemo(() => {
     if (!agents.data) return {};
@@ -50,6 +52,21 @@ const HGICluster: React.FC = () => {
   }, [ ]);
 
   const slotContainerStates = getSlotContainerStates(agents.data || []);
+
+  const toggleModal = useCallback(
+    () => {
+      setDetailVisible((cur: boolean) => !cur);
+    },
+    [],
+  );
+
+  const handleTableRow = useCallback((record: ResourcePool) => {
+    const handleClick = (event: React.MouseEvent) => {
+      if (isAlternativeAction(event)) return;
+      toggleModal();
+    };
+    return { onAuxClick: handleClick, onClick: handleClick };
+  }, []);
 
   if (!agents.data) {
     return <Spinner />;
@@ -103,13 +120,14 @@ const HGICluster: React.FC = () => {
             spinning: agents.isLoading, // TODO replace with resource pools
           }}
           pagination={getPaginationConfig(resourcePools.length, 10)} // TODO config page size
-          rowClassName={defaultRowClassName({ clickable: false })}
+          rowClassName={defaultRowClassName({ clickable: true })}
           rowKey="batchNum"
           scroll={{ x: 1000 }}
           showSorterTooltip={false}
           size="small"
-          // onChange={handleTableChange}
+          onRow={handleTableRow}
         />
+        <ResourcePoolDetails finally={toggleModal} rpIndex={0} visible={detailVisible} />
       </Section>
     </Page>
   );
