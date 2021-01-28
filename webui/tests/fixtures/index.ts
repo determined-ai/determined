f3af0045f625aaa361e22db0a906ad37a42a1a47
Hamid Zare <hamidzare.vu@gmail.com>
2021-01-27 22:29:26 -0800

    Revert "wip mock responses"
    This reverts commit e6779e5ff8072863eb1e9f3843ef9a5a4def451d.

--- webui/tests/fixtures/index.ts
+++
@@ -1,8 +0,0 @@
-import getTrialDetails from './getTrialDetails.json'
-
-export default {
-  getTrialDetails: {
-    url: 'http://localhost:8080/api/v1/trials/2',
-    responses: getTrialDetails,
-  },
-};
