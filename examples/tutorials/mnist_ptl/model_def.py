c6a4358910494e04910ea6cc7b8236d83c94dabb
Hamid Zare <hamid@determined.ai>
2021-02-01 14:59:48 -0800

    restructure example files.

--- examples/tutorials/mnist_ptl/model_def.py
+++ examples/tutorials/mnist_ptl/model_def.py
@@ -15,7 +15,6 @@ import torch
 
 from determined.pytorch import DataLoader, PyTorchTrial, PyTorchTrialContext
 
-import data
 import ptl
 import pytorch_lightning as pl
 from determined.experimental.pytorch_lightning import PTLAdapter
@@ -25,29 +24,3 @@ TorchData = Union[Dict[str, torch.Tensor], Sequence[torch.Tensor], torch.Tensor]
 class MNistTrial(PTLAdapter): # match PyTorchTrial API
     def __init__(self, context: PyTorchTrialContext) -> None:
         super().__init__(context, ptl.LightningMNISTClassifier, data_module=ptl.MNISTDataModule)
-
-        # Create a unique download directory for each rank so they don't overwrite each other.
-        # self.download_directory = f"/tmp/data-rank{self.context.distributed.get_rank()}"
-        # self.data_downloaded = False
-
-    # def build_training_data_loader(self) -> DataLoader:
-    #     if not self.data_downloaded:
-    #         self.download_directory = data.download_dataset(
-    #             download_directory=self.download_directory,
-    #             data_config=self.context.get_data_config(),
-    #         )
-    #         self.data_downloaded = True
-
-    #     train_data = data.get_dataset(self.download_directory, train=True)
-    #     return DataLoader(train_data, batch_size=self.context.get_per_slot_batch_size())
-
-    # def build_validation_data_loader(self) -> DataLoader:
-    #     if not self.data_downloaded:
-    #         self.download_directory = data.download_dataset(
-    #             download_directory=self.download_directory,
-    #             data_config=self.context.get_data_config(),
-    #         )
-    #         self.data_downloaded = True
-
-    #     validation_data = data.get_dataset(self.download_directory, train=False)
-    #     return DataLoader(validation_data, batch_size=self.context.get_per_slot_batch_size())
