9372678a9d6b22d874ae3b00ae0e54e7de33138b
Bradley Laney <bradlaney@determined.ai>
2021-02-03 16:54:54 -0500

    fixes

--- master/static/migrations/20210203150406_change-limit-behavior.up.sql
+++ master/static/migrations/20210203150406_change-limit-behavior.up.sql
@@ -3,14 +3,14 @@ CREATE OR REPLACE FUNCTION public.page_info(total bigint, "offset" int, "limit" 
     LANGUAGE sql IMMUTABLE
 AS $$
 WITH start_index AS (
-    SELECT (CASE WHEN "offset" < 0
-                 THEN total + "offset"
+    SELECT (CASE WHEN "offset" < -total OR "offset" > total THEN total
+                 WHEN "offset" < 0 THEN total + "offset"
                  ELSE "offset" END) AS start_index
 ), end_index AS (
-    SELECT (CASE WHEN (SELECT start_index FROM start_index) + "limit" > total THEN total
-                 WHEN "limit" = 0 THEN least(100::bigint + (SELECT start_index FROM start_index), total)
+    SELECT (CASE WHEN "limit" = -2 THEN (SELECT start_index FROM start_index)
                  WHEN "limit" = -1 THEN total
-                 WHEN "limit" = -2 THEN (SELECT start_index FROM start_index)
+                 WHEN "limit" = 0 THEN least(100::bigint + (SELECT start_index FROM start_index), total)
+                 WHEN (SELECT start_index FROM start_index) + "limit" > total THEN total
                  ELSE (SELECT start_index FROM start_index) + "limit" END) AS end_index
 ), page_info AS (
     SELECT
