ef702dc42e8ba8a0b87f0e9ad156e949fb5a004d
Caleb Hoyoul Kang <caleb@determined.ai>
2021-02-03 17:09:24 -0700

    fix: correct the comparison function when numbers are fractions [DET-4969] (#1924)
    (cherry picked from commit c796178b4958579bbf4bf2fe8354e01887737d61)

--- webui/react/src/utils/data.ts
+++ webui/react/src/utils/data.ts
@@ -119,9 +119,14 @@ export const metricNameSorter = (a: MetricName, b: MetricName): number => {
 };
 
 export const numericSorter = (a?: number, b?: number, reverseOrder = false): number => {
-  if (a != null && b != null) return reverseOrder ? b - a : a - b;
+  if (a != null && b != null) {
+    const diff = reverseOrder ? b - a : a - b;
+    if (diff < 0) return -1;
+    if (diff > 0) return 1;
+    return 0;
+  }
   if (a != null && b == null) return reverseOrder ? -1 : 1;
-  if (b == null && b != null) return reverseOrder ? 1 : -1;
+  if (a == null && b != null) return reverseOrder ? 1 : -1;
   return 0;
 };
 
