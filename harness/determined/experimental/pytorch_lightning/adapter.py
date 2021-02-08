00f817aa2e11375382dd76e93f7ff63dfa421e4d
Hamid Zare <hamid@determined.ai>
2021-02-08 14:00:45 -0800

    format for flake8 compaints

--- harness/determined/experimental/pytorch_lightning/adapter.py
+++ harness/determined/experimental/pytorch_lightning/adapter.py
@@ -1,11 +1,10 @@
 import sys
 from inspect import signature
-from typing import Any, Callable, Dict, NewType, Sequence, Union
+from typing import Any, Callable, Dict, Sequence, Union
 
 import pytorch_lightning as ptl
 import torch
 
-from determined import monkey_patch
 from determined.pytorch import PyTorchTrial, PyTorchTrialContext
 
 TorchData = Union[Dict[str, torch.Tensor], Sequence[torch.Tensor], torch.Tensor]
@@ -31,7 +30,8 @@ def check_compat(lm: ptl.LightningModule):
 class PTLAdapter(PyTorchTrial):
     context: PyTorchTrialContext
 
-    # QUESTION: take uninstantiated lightning and datamodule so we isntatiate it instead? less code for the user but might be better if the user sees this?
+    # QUESTION: take uninstantiated lightning and datamodule so we isntatiate it instead? less code
+    # for the user but might be better if the user sees this?
     def __init__(self, context: PyTorchTrialContext, lightning_module: ptl.LightningModule):
         super().__init__(context)
         check_compat(lightning_module)
@@ -47,8 +47,10 @@ class PTLAdapter(PyTorchTrial):
         LM optimizer
         - Single optimizer.
         - List or Tuple - List of optimizers.
-        - Two lists - The first list has multiple optimizers, the second a list of LR schedulers (or lr_dict).
-        - Dictionary, with an ‘optimizer’ key, and (optionally) a ‘lr_scheduler’ key whose value is a single LR scheduler or lr_dict.
+        - Two lists - The first list has multiple optimizers, the second a list of LR schedulers
+        (or lr_dict).
+        - Dictionary, with an ‘optimizer’ key, and (optionally) a ‘lr_scheduler’ key whose value is
+        a single LR scheduler or lr_dict.
         - Tuple of dictionaries as described, with an optional ‘frequency’ key.
         - None - Fit will run without any optimizer.
         """
