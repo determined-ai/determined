cbf814f3d4816e2a285a1bad9df5b3b58127df37
David Wang <dsw@determined.ai>
2021-02-09 12:45:37 -0800

    chore: bump version: 0.14.1rc1 -> 0.14.1rc2

--- common/setup.py
+++ common/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="determined-common",
-    version="0.14.1rc1",
+    version="0.14.1rc2",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
