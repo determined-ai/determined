0de14ec571412d5c3eaeec0adb673225153c932d
Caleb Kang <caleb@determined.ai>
2021-02-22 15:22:54 -0700

    test: add 0 to numeric sorting tests

--- webui/react/src/utils/data.test.ts
+++ webui/react/src/utils/data.test.ts
@@ -224,6 +224,10 @@ describe('data utility', () => {
   describe('sorters', () => {
     it('numericSorter', () => {
       const tests = [
+        { input: [ 0, 1 ], output: -1 },
+        { input: [ 1, 0 ], output: 1 },
+        { input: [ 0, -0.123 ], output: 1 },
+        { input: [ -0.123, 0 ], output: -1 },
         { input: [ 100, 10 ], output: 1 },
         { input: [ -100, -10 ], output: -1 },
         { input: [ 1, 10 ], output: -1 },
@@ -233,8 +237,11 @@ describe('data utility', () => {
         { input: [ 1.23e2, -123 ], output: 1 },
         { input: [ -0.999, 9e-3 ], output: -1 },
         { input: [ 0.123, undefined ], output: 1 },
+        { input: [ 0, undefined ], output: 1 },
         { input: [ undefined, -0.123 ], output: -1 },
+        { input: [ undefined, 0 ], output: -1 },
         { input: [ undefined, undefined ], output: 0 },
+        { input: [ 0, 0 ], output: 0 },
         { input: [ 1e7, 1e7 ], output: 0 },
         { input: [ 1e-5, 1e-5 ], output: 0 },
         { input: [ 10, 10 ], output: 0 },
