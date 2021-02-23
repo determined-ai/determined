5b93f8a16f26e8318bf1bb1d0bc69c1e61b90ba2
Caleb Kang <caleb@determined.ai>
2021-02-23 16:12:59 -0700

    refactor: separate out sorting utility functions

--- webui/react/src/utils/data.test.ts
+++ webui/react/src/utils/data.test.ts
@@ -15,7 +15,6 @@ import {
   isPrimitive,
   isSet,
   isSyncFunction,
-  numericSorter,
   setPathList,
 } from './data';
 
@@ -217,41 +216,6 @@ describe('data utility', () => {
             : test.type === group.type;
           expect(group.fn(test.value)).toBe(result);
         });
-      });
-    });
-  });
-
-  describe('sorters', () => {
-    it('numericSorter', () => {
-      const tests = [
-        { input: [ 0, 1 ], output: -1 },
-        { input: [ 1, 0 ], output: 1 },
-        { input: [ 0, -0.123 ], output: 1 },
-        { input: [ -0.123, 0 ], output: -1 },
-        { input: [ 100, 10 ], output: 1 },
-        { input: [ -100, -10 ], output: -1 },
-        { input: [ 1, 10 ], output: -1 },
-        { input: [ -1, -10 ], output: 1 },
-        { input: [ 0.01, 0.1 ], output: -1 },
-        { input: [ -0.01, -0.1 ], output: 1 },
-        { input: [ 1.23e2, -123 ], output: 1 },
-        { input: [ -0.999, 9e-3 ], output: -1 },
-        { input: [ 0.123, undefined ], output: 1 },
-        { input: [ 0, undefined ], output: 1 },
-        { input: [ undefined, -0.123 ], output: -1 },
-        { input: [ undefined, 0 ], output: -1 },
-        { input: [ undefined, undefined ], output: 0 },
-        { input: [ 0, 0 ], output: 0 },
-        { input: [ 1e7, 1e7 ], output: 0 },
-        { input: [ 1e-5, 1e-5 ], output: 0 },
-        { input: [ 10, 10 ], output: 0 },
-        { input: [ -0.123, -0.123 ], output: 0 },
-      ];
-      tests.forEach(test => {
-        const result = numericSorter(test.input[0], test.input[1], false);
-        const reverseResult = numericSorter(test.input[0], test.input[1], true);
-        expect(result).toStrictEqual(test.output);
-        expect(reverseResult).toStrictEqual(test.output === 0 ? 0 : test.output * -1);
       });
     });
   });
---
+++ webui/react/src/utils/sort.ts
@@ -0,0 +1,93 @@
+import { CommandState, MetricName, MetricType, Primitive, RunState, State } from 'types';
+
+const runStateSortValues: Record<RunState, number> = {
+  [RunState.Active]: 0,
+  [RunState.Paused]: 1,
+  [RunState.StoppingError]: 2,
+  [RunState.Errored]: 3,
+  [RunState.StoppingCompleted]: 4,
+  [RunState.Completed]: 5,
+  [RunState.StoppingCanceled]: 6,
+  [RunState.Canceled]: 7,
+  [RunState.Deleted]: 7,
+  [RunState.Unspecified]: 8,
+};
+
+const commandStateSortValues: Record<CommandState, number> = {
+  [CommandState.Pending]: 0,
+  [CommandState.Assigned]: 1,
+  [CommandState.Pulling]: 2,
+  [CommandState.Starting]: 3,
+  [CommandState.Running]: 4,
+  [CommandState.Terminating]: 5,
+  [CommandState.Terminated]: 6,
+};
+
+export const alphanumericSorter = (a: string | number, b: string | number): number => {
+  return a.toString().localeCompare(b.toString(), 'en', { numeric: true });
+};
+
+export const booleanSorter = (a: boolean, b: boolean): number => {
+  // True values first.
+  return (a === b) ? 0 : (a ? -1 : 1);
+};
+
+export const commandStateSorter = (a: CommandState, b: CommandState): number => {
+  return commandStateSortValues[a] - commandStateSortValues[b];
+};
+
+/*
+ * Sort the metric names by having the validation metrics come first followed by training metrics.
+ * Within each type of metric, sort in the order they appear in the `MetricNames` array.
+ * Within the respective type of metrics, `MetricNames` is currently sorted alphanumerically.
+ */
+export const metricNameSorter = (a: MetricName, b: MetricName): number => {
+  const isAValidation = a.type === MetricType.Validation;
+  const isBValidation = b.type === MetricType.Validation;
+  if (isAValidation && !isBValidation) return -1;
+  if (isBValidation && !isAValidation) return 1;
+  return alphanumericSorter(a.name, b.name);
+};
+
+export const numericSorter = (a?: number, b?: number, reverseOrder = false): number => {
+  if (a != null && b != null) {
+    const diff = reverseOrder ? b - a : a - b;
+    if (diff < 0) return -1;
+    if (diff > 0) return 1;
+    return 0;
+  }
+  if (a != null && b == null) return reverseOrder ? -1 : 1;
+  if (a == null && b != null) return reverseOrder ? 1 : -1;
+  return 0;
+};
+
+export const primitiveSorter = (a: Primitive, b: Primitive): number => {
+  if (typeof a === 'boolean' && typeof b === 'boolean') {
+    return booleanSorter(a, b);
+  } else if (typeof a === 'number' && typeof b === 'number') {
+    return numericSorter(a, b);
+  } else if (typeof a === 'string' && typeof b === 'string') {
+    return alphanumericSorter(a, b);
+  }
+  return 0;
+};
+
+export const runStateSorter = (a: RunState, b: RunState): number => {
+  return runStateSortValues[a] - runStateSortValues[b];
+};
+
+export const stringTimeSorter = (a: string, b: string): number => {
+  const aTime = new Date(a).getTime();
+  const bTime = new Date(b).getTime();
+  return aTime - bTime;
+};
+
+export const taskStateSorter = (a: State, b: State): number => {
+  // FIXME this is O(n) we can do it in constant time.
+  // What is the right typescript way of doing it?
+  const aValue = Object.values(RunState).includes(a as RunState) ?
+    runStateSortValues[a as RunState] : commandStateSortValues[a as CommandState];
+  const bValue = Object.values(RunState).includes(b as RunState) ?
+    runStateSortValues[b as RunState] : commandStateSortValues[b as CommandState];
+  return aValue - bValue;
+};
