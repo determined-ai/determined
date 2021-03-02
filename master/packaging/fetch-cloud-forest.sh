a79b082c28b31c1a380c50c2478e1167ac601aba
Sean Mackrory <mackrory@determined.ai>
2021-03-02 07:31:55 -0700

    Stop polluting go.mod and go.sum

--- master/packaging/fetch-cloud-forest.sh
+++ master/packaging/fetch-cloud-forest.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Install native version to ensure it's on PATH
-go install github.com/ryanbressler/CloudForest/growforest
+GO111MODULE=off go install github.com/ryanbressler/CloudForest/growforest
 
 # Ensure the Linux binary is installed regardless of OS
-GOOS=linux go install github.com/ryanbressler/CloudForest/growforest
+GO111MODULE=off GOOS=linux go install github.com/ryanbressler/CloudForest/growforest
 
 # Find the Linux binary for packaging and output the path
 if [ "$(go env GOOS)" == "linux" ]; then
