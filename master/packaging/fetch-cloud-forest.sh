8d471015533050688b1682f4bb06d96219a4c948
Sean Mackrory <mackrory@determined.ai>
2021-03-02 08:47:51 -0700

    using tools.go

--- master/packaging/fetch-cloud-forest.sh
+++ master/packaging/fetch-cloud-forest.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Install native version to ensure it's on PATH
-GO111MODULE=off go install github.com/ryanbressler/CloudForest/growforest
+go install github.com/ryanbressler/CloudForest/growforest
 
 # Ensure the Linux binary is installed regardless of OS
-GO111MODULE=off GOOS=linux go install github.com/ryanbressler/CloudForest/growforest
+GOOS=linux go install github.com/ryanbressler/CloudForest/growforest
 
 # Find the Linux binary for packaging and output the path
 if [ "$(go env GOOS)" == "linux" ]; then
