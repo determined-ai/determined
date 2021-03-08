3a80ca7e98be93dccbbb6bd1a05319078f8e7ae0
Hamid Zare <hamid@determined.ai>
2021-03-08 12:04:42 -0800

    update startup hook py package version

--- examples/gan/_gan_mnist_pl/startup-hook.sh
+++ examples/gan/_gan_mnist_pl/startup-hook.sh
@@ -1,3 +1,3 @@
 # TODO: remove this file before merging
 
-python3 -m pip install -q --user --force-reinstall determined_common-0.14.3.dev0-py3-none-any.whl determined-0.14.3.dev0-py3-none-any.whl
+python3 -m pip install -q --user --force-reinstall determined_common-0.14.4.dev0-py3-none-any.whl determined-0.14.4.dev0-py3-none-any.whl
