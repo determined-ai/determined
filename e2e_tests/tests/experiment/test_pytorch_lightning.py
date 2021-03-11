a1ee28241468e78ef0abd692f997f3535053ecb5
Hamid Zare <hamid@determined.ai>
2021-03-11 12:06:13 -0800

    make fmt

--- e2e_tests/tests/experiment/test_pytorch_lightning.py
+++ e2e_tests/tests/experiment/test_pytorch_lightning.py
@@ -4,15 +4,14 @@ from determined.experimental import Determined
 from tests import config as conf
 from tests import experiment as exp
 
+
 @pytest.mark.parallel  # type: ignore
 def test_pl_mnist() -> None:
     exp_dir = "_mnist_pl"
     config = conf.load_config(conf.cv_examples_path(exp_dir + "/const.yaml"))
     config = conf.set_max_length(config, {"batches": 200})
 
-    experiment_id = exp.run_basic_test_with_temp_config(
-        config, conf.cv_examples_path(exp_dir), 1
-    )
+    experiment_id = exp.run_basic_test_with_temp_config(config, conf.cv_examples_path(exp_dir), 1)
     trials = exp.experiment_trials(experiment_id)
     (
         Determined(conf.make_master_url())
@@ -21,15 +20,14 @@ def test_pl_mnist() -> None:
         .load(map_location="cpu")
     )
 
+
 @pytest.mark.parallel  # type: ignore
 def test_pl_mnist_gan() -> None:
     exp_dir = "_gan_mnist_pl"
     config = conf.load_config(conf.gan_examples_path(exp_dir + "/const.yaml"))
     config = conf.set_max_length(config, {"batches": 200})
 
-    experiment_id = exp.run_basic_test_with_temp_config(
-        config, conf.gan_examples_path(exp_dir), 1
-    )
+    experiment_id = exp.run_basic_test_with_temp_config(config, conf.gan_examples_path(exp_dir), 1)
     trials = exp.experiment_trials(experiment_id)
     (
         Determined(conf.make_master_url())
