811dcdb753417c3d679ca92895fbb59da8f25aa2
Caleb Kang <caleb@determined.ai>
2021-03-17 19:14:37 -0600

    feat: dynamically hide/show radio group labels if there is room

--- webui/react/src/components/RadioGroup.tsx
+++ webui/react/src/components/RadioGroup.tsx
@@ -1,8 +1,11 @@
-import { Radio } from 'antd';
+import { Radio, Tooltip } from 'antd';
 import { RadioChangeEvent } from 'antd/lib/radio';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { throttle } from 'throttle-debounce';
 
 import Icon, { IconSize } from 'components/Icon';
+import useResize from 'hooks/useResize';
+import { ConditionalWrapper } from 'utils/react';
 
 import css from './RadioGroup.module.scss';
 
@@ -20,9 +23,29 @@ export interface RadioGroupOption {
   label?: string;
 }
 
+interface SizeInfo {
+  baseHeight: number;
+  baseWidth: number;
+  parentWidth: number;
+}
+
+const RESIZE_THROTTLE_TIME = 500;
+const PARENT_WIDTH_BUFFER = 16;
+const HEIGHT_LIMIT = 50;
+
 const RadioGroup: React.FC<Props> = ({ className, defaultOptionId, onChange, options }: Props) => {
+  const baseRef = useRef<HTMLDivElement>(null);
+  const originalWidth = useRef<number>();
+  const resize = useResize();
   const [ selected, setSelected ] = useState<string | undefined>(() => defaultOptionId);
+  const [ showLabels, setShowLabels ] = useState(true);
+  const [ sizes, setSizes ] = useState<SizeInfo>({ baseHeight: 0, baseWidth: 0, parentWidth: 0 });
   const classes = [ css.base ];
+
+  const hasIconsAndLabels = useMemo(() => {
+    if (options.length === 0) return false;
+    return options.reduce((acc, option) => acc || (!!option.icon && !!option.label), false);
+  }, [ options ]);
 
   if (className) classes.push(className);
 
@@ -32,13 +55,66 @@ const RadioGroup: React.FC<Props> = ({ className, defaultOptionId, onChange, opt
     if (onChange) onChange(id);
   }, [ onChange ]);
 
+  /*
+   * Dynamically check to see if labels can be shown along with the icons,
+   * if there's room to show both and both are available.
+   */
+  useEffect(() => {
+    if (!hasIconsAndLabels || !baseRef.current) return;
+    if (sizes.baseWidth === 0 || sizes.parentWidth === 0) return;
+
+    setShowLabels(prev => {
+      if (!originalWidth.current) return prev;
+      if (prev && sizes.baseHeight > HEIGHT_LIMIT) return false;
+      if (!prev && originalWidth.current < sizes.parentWidth) return true;
+      return prev;
+    });
+  }, [ hasIconsAndLabels, sizes ]);
+
+  /*
+   * Update parent and component sizes upon resize of the window,
+   * at a throttled rate.
+   */
+  useEffect(() => {
+    const throttleFunc = throttle(RESIZE_THROTTLE_TIME, () => {
+      if (!hasIconsAndLabels || !baseRef.current) return;
+      const parent = baseRef.current.parentElement;
+      if (!parent) return;
+
+      const parentRect = parent.getBoundingClientRect();
+      if (!parentRect) return;
+
+      const baseRect = baseRef.current.getBoundingClientRect();
+      if (!originalWidth.current) originalWidth.current = baseRect.width;
+
+      setSizes({
+        baseHeight: baseRect.height,
+        baseWidth: baseRect.width,
+        parentWidth: parentRect.width - PARENT_WIDTH_BUFFER,
+      });
+    });
+
+    throttleFunc();
+  }, [ hasIconsAndLabels, resize ]);
+
   return (
-    <Radio.Group className={classes.join(' ')} value={selected} onChange={handleChange}>
+    <Radio.Group
+      className={classes.join(' ')}
+      ref={baseRef}
+      value={selected}
+      onChange={handleChange}>
       {options.map(option => (
-        <Radio.Button className={css.option} key={option.id} value={option.id}>
-          {option.icon && <Icon name={option.icon} size={option.iconSize} title={option.label} />}
-          {option.label && <span>{option.label}</span>}
-        </Radio.Button>
+        <ConditionalWrapper
+          condition={!showLabels}
+          key={option.id}
+          wrapper={children => (
+            <Tooltip placement="top" title={option.label}>{children}</Tooltip>
+          )}>
+          <Radio.Button className={css.option} value={option.id}>
+            {option.icon && <Icon name={option.icon} size={option.iconSize} title={option.label} />}
+            {option.label && showLabels && <span className={css.label}>{option.label}</span>}
+          </Radio.Button>
+        </ConditionalWrapper>
       ))}
     </Radio.Group>
   );
