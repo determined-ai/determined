9d5dff71a26645b7af57887eaad075b0ed61600e
Shiyuan <shiyuan@determined.ai>
2021-03-31 10:05:04 -0700

    chore: add license information to Pytorch Lightning examples (#2147)
    (cherry picked from commit 957ffd6b876ed5f3e46c38707ce075b96c631266)

--- examples/gan/gan_mnist_pl/data.py
+++ examples/gan/gan_mnist_pl/data.py
@@ -1,3 +1,20 @@
+# The class MNISTDataModule is modified from the Pytorch Lightning example:
+# https://colab.research.google.com/github/PytorchLightning/pytorch-lightning/
+# blob/master/notebooks/01-mnist-hello-world.ipynb#scrollTo=4DNItffri95Q
+#
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
 import urllib.parse
 import requests
