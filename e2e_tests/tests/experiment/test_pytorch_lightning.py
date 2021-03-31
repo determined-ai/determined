5fcd59ec042a990a0f893c584881e1df843c1d99
Shiyuan Zhu <shiyuan@determined.ai>
2021-03-31 13:01:48 -0700

    fix: e2e test for pytorch lightning examples

--- e2e_tests/tests/experiment/test_pytorch_lightning.py
+++ e2e_tests/tests/experiment/test_pytorch_lightning.py
@@ -24,7 +24,7 @@ def test_pl_mnist_gan() -> None:
 
 @pytest.mark.e2e_gpu  # type: ignore
 @pytest.mark.parametrize("api_style", ["apex", "auto"])  # type: ignore
-def test_pytorch_const_with_amp(api_style: str) -> None:
+def test_pl_const_with_amp(api_style: str) -> None:
     dir_name = "pytorch_lightning_amp"
     config = conf.load_config(conf.fixtures_path(dir_name + "/" + api_style + "_amp.yaml"))
     config = conf.set_max_length(config, {"batches": 200})
