e47243675c660cbd571c78333e7ceece5f1db447
David Wang <dsw@determined.ai>
2021-04-01 18:26:15 -0700

    chore: bump version: 0.14.6rc4 -> 0.14.6

--- harness/setup.py
+++ harness/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="determined",
-    version="0.14.6rc4",
+    version="0.14.6",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
