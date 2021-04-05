486eec30b5e33f8e5fe761d06d426874dc011b10
Caleb Kang <caleb@determined.ai>
2021-04-05 16:05:57 -0600

    chore: add percent related utility function and unit tests

--- webui/react/src/utils/number.ts
+++ webui/react/src/utils/number.ts
@@ -1,7 +1,24 @@
+import { isString } from './data';
+
+const PERCENT_REGEX = /^(\d+\.?\d*|\.\d+)%$/;
+
+export const isPercent = (data: unknown): boolean => {
+  if (!isString(data)) return false;
+  return PERCENT_REGEX.test(data);
+};
+
 export const percent = (n: number, decimals = 1): number => {
   const normalized = n < 0 ? 0 : (n > 1 ? 1 : (n || 0));
   const factor = Math.pow(10, decimals);
   return Math.round(normalized * 100 * factor) / factor;
+};
+
+export const percentToFloat = (percent: unknown): number => {
+  if (isPercent(percent)) {
+    const matches = (percent as string).match(PERCENT_REGEX);
+    if (matches?.length === 2) return parseFloat(matches[1]) / 100;
+  }
+  return 1.0;
 };
 
 export const roundToPrecision = (n: number, precision = 6): number => {
