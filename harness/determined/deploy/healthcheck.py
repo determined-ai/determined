1eb27f21665f7bfe22548b9302c102e86f6ab41f
Ilia Glazkov <ilia@determined.ai>
2021-04-06 11:19:16 -0700

    review fixes.

--- harness/determined/deploy/healthcheck.py
+++ harness/determined/deploy/healthcheck.py
@@ -6,23 +6,28 @@ from determined.common import api
 
 from .errors import MasterTimeoutExpired
 
+DEFAULT_TIMEOUT = 100
+
 
 def _make_master_url(master_host: str, master_port: int, suffix: str = "") -> str:
     return "http://{}:{}/{}".format(master_host, master_port, suffix)
 
 
-def wait_for_master(master_host: str, master_port: int = 8080, timeout: int = 100) -> None:
+def wait_for_master(
+    master_host: str, master_port: int = 8080, timeout: int = DEFAULT_TIMEOUT
+) -> None:
     master_url = _make_master_url(master_host, master_port)
 
     return wait_for_master_url(master_url, timeout)
 
 
-def wait_for_master_url(master_url: str, timeout: int = 100) -> None:
+def wait_for_master_url(master_url: str, timeout: int = DEFAULT_TIMEOUT) -> None:
     POLL_INTERVAL = 2
     polling = False
-    try:
+    start_time = time.time()
 
-        for _ in range(int(timeout / POLL_INTERVAL)):
+    try:
+        while time.time() - start_time < timeout:
             try:
                 r = api.get(master_url, "info", authenticated=False)
                 if r.status_code == requests.codes.ok:
@@ -31,7 +36,8 @@ def wait_for_master_url(master_url: str, timeout: int = 100) -> None:
                 pass
             if not polling:
                 polling = True
-                print("Waiting for Master instance to be available..", end="", flush=True)
+                # Third dot will be added two lines later.
+                print("Waiting for master instance to be available..", end="", flush=True)
             time.sleep(POLL_INTERVAL)
             print(".", end="", flush=True)
 
