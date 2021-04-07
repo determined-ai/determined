98c4f2fc48b680bab192b93007159d2644d67be8
Caleb Kang <caleb@determined.ai>
2021-04-07 16:19:48 -0600

    chore: prevent multiple listeners from being added if useKeyTracker accidentally gets used multiple times

--- webui/react/src/hooks/useKeyTracker.ts
+++ webui/react/src/hooks/useKeyTracker.ts
@@ -1,6 +1,6 @@
 import { EventEmitter } from 'events';
 
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect } from 'react';
 
 export enum KeyEvent {
   KeyUp = 'KeyUp'
@@ -8,23 +8,28 @@ export enum KeyEvent {
 
 export const keyEmitter = new EventEmitter();
 
-const useKeyTracker = (): void => {
-  const emitter = useRef(keyEmitter);
+let listenerCount = 0;
 
+const useKeyTracker = (): void => {
   const handleKeyUp = useCallback((e: KeyboardEvent) => {
     if (e.target) {
       const element = e.target as Element;
       if ([ 'input', 'textarea' ].includes(element.tagName.toLowerCase())) return;
       if (element.getAttribute('contenteditable')) return;
     }
-    emitter.current.emit(KeyEvent.KeyUp, e);
+    keyEmitter.emit(KeyEvent.KeyUp, e);
   }, []);
 
   useEffect(() => {
+    if (listenerCount !== 0) return;
+
+    listenerCount++;
     document.body.addEventListener('keyup', handleKeyUp);
 
     return () => {
+      if (listenerCount === 0) return;
       document.body.removeEventListener('keyup', handleKeyUp);
+      listenerCount--;
     };
   }, [ handleKeyUp ]);
 };
