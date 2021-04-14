cdaff4f923d1e4c187a861592c6487edc267cbb1
Justin Chen <justin@determined.ai>
2021-04-14 09:52:37 -0700

    chore: bump version: 0.15.0.dev0 -> 0.15.0rc0

--- common/setup.py
+++ common/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="determined-common",
-    version="0.15.0.dev0",
+    version="0.15.0rc0",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
@@ -14,7 +14,7 @@ setup(
     python_requires=">=3.5",
     package_data={"determined.common": ["py.typed"]},
     install_requires=[
-        "determined==0.15.0.dev0",
+        "determined==0.15.0rc0",
     ],
     zip_safe=False,
 )
