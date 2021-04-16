7dae75d2807ee107b2312b37889d77844716f83c
Armand McQueen <armandmcqueen@gmail.com>
2021-04-15 20:36:35 -0400

    Remove startup hook change

--- examples/nlp/albert_squad_pytorch/startup-hook.sh
+++ examples/nlp/albert_squad_pytorch/startup-hook.sh
@@ -2,4 +2,3 @@
 pip install sentencepiece==0.1.91
 pip install transformers==3.1.0
 pip install -e git+git://github.com/LiyuanLucasLiu/RAdam.git@baf4f65445c00d686d4098841b3ca1f62a886326#egg=radam
-pip install pynvml
