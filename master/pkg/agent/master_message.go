e08d7cfe2ae188155b13d339fdbe1268369be957
Bradley Laney <bradlaney@determined.ai>
2021-04-21 13:15:25 -0500

    fix: gracefully handle Docker binding published ports to ipv4 and ipv6 for host

--- master/pkg/agent/master_message.go
+++ master/pkg/agent/master_message.go
@@ -83,23 +83,31 @@ func (c ContainerStarted) Addresses() []container.Address {
 		for _, network := range networks {
 			ipAddresses = append(ipAddresses, network.IPAddress)
 		}
+		// uniqueBindings is used to filter out bindings to 0.0.0.0 and ::
+		// that are coalesced to the same container.Address.
+		uniqueBindings := map[string]bool{}
 		for port, bindings := range info.NetworkSettings.Ports {
 			for _, binding := range bindings {
 				for _, ip := range ipAddresses {
 					hostIP := binding.HostIP
-					if hostIP == "" || hostIP == "0.0.0.0" {
+					if hostIP == "" || hostIP == "0.0.0.0" || hostIP == "::" {
 						hostIP = proxy
 					}
 					hostPort, err := strconv.Atoi(binding.HostPort)
 					if err != nil {
 						panic(errors.Wrapf(err, "unexpected host port: %s", binding.HostPort))
 					}
-					addresses = append(addresses, container.Address{
+
+					cAddress := container.Address{
 						ContainerIP:   ip,
 						ContainerPort: port.Int(),
 						HostIP:        hostIP,
 						HostPort:      hostPort,
-					})
+					}
+					if !uniqueBindings[cAddress.String()] {
+						addresses = append(addresses, cAddress)
+						uniqueBindings[cAddress.String()] = true
+					}
 				}
 			}
 		}
