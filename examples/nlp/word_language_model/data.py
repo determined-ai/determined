4b88f617d5206e4ac09158b7b989de5f69ef9a6a
Rehan S. Durrani <rehan@determined.ai>
2021-04-28 19:48:21 -0700

    Lay groundwork for dataset

--- examples/nlp/word_language_model/data.py
+++ examples/nlp/word_language_model/data.py
@@ -4,6 +4,7 @@ import torch
 from pathlib import Path
 import urllib.request
 import zipfile
+import torch
 
 def load_dataset(path):
     data_dir = path / 'wikitext-2'
@@ -35,13 +36,13 @@ class Dictionary(object):
 class Corpus(object):
     def __init__(self, path):
         self.dictionary = Dictionary()
-        self.train = self.tokenize(os.path.join(path, 'train.txt'))
-        self.valid = self.tokenize(os.path.join(path, 'valid.txt'))
-        self.test = self.tokenize(os.path.join(path, 'test.txt'))
+        self.train = self.tokenize(path / 'train.txt')
+        self.val = self.tokenize(path / 'valid.txt')
+        self.test = self.tokenize(path / 'test.txt')
 
     def tokenize(self, path):
         """Tokenizes a text file."""
-        assert os.path.exists(path)
+        assert path.exists()
         # Add words to the dictionary
         with open(path, 'r', encoding="utf8") as f:
             for line in f:
@@ -61,3 +62,24 @@ class Corpus(object):
             ids = torch.cat(idss)
 
         return ids
+
+class WikiTextDataset(torch.utils.data.IterableDataset):
+    def __init__(self, path, mode, batch_size=20):
+        assert mode in ['train', 'val'], "Data mode for WikiTextDataset must be one of ['train', 'val']"
+        load_dataset(path)
+        data_path = path / 'wikitext-2'
+        self.corpus = Corpus(data_path)
+        self.batch_size = batch_size
+        self.ntokens = len(self.corpus.dictionary)
+        self.process_data(mode)
+
+    def process_data(self, mode):
+        self.data = self.corpus.train if mode == 'train' else self.corpus.val
+        nbatch = self.data.size(0) // self.batch_size
+        # Trim off any extra elements that wouldn't cleanly fit (remainders).
+        self.data = self.data.narrow(0, 0, nbatch * self.batch_size)
+        # Evenly divide the data across the bsz batches.
+        self.data = self.data.view(bsz, -1).t().contiguous()
+    
+    def __iter__(self):
+        pass
