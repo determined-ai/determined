57fd3342db51c82c1d539f34dbd070074e79dc57
Caleb Hoyoul Kang <caleb@determined.ai>
2021-04-29 10:04:23 -0600

    chore: apply react strict mode and upgrade to React 17 [DET-5325] (#2279)

--- webui/react/src/index.tsx
+++ webui/react/src/index.tsx
@@ -13,7 +13,14 @@ import * as serviceWorker from './serviceWorker';
 import 'prototypes';
 import 'dev';
 
-ReactDOM.render(<Router history={history}><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <Router history={history}>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
 
 /*
  * If you want your app to work offline and load faster, you can change
