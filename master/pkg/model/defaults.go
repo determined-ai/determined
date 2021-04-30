b6d1bef107c0414625f225b90f584e83e0798dcf
Ilia Glazkov <ilia@determined.ai>
2021-04-30 12:45:01 -0700

    chore: update environment images to 0.12.0. (#2304)
    - Update to env images 0.12.0, which fixes tf1 gpu support.
    - Replace cuda10.2 + tf2.4 with cuda11 + tf2.4 images.
    
    (cherry picked from commit bbb500731cce2805c7aa28a1d16bde44594089e5)

--- master/pkg/model/defaults.go
+++ master/pkg/model/defaults.go
@@ -25,8 +25,8 @@ const (
 
 // Default task environment docker image names.
 const (
-	defaultCPUImage = "determinedai/environments:py-3.7-pytorch-1.7-tf-1.15-cpu-da9ba40"
-	defaultGPUImage = "determinedai/environments:cuda-10.2-pytorch-1.7-tf-1.15-gpu-da9ba40"
+	defaultCPUImage = "determinedai/environments:py-3.7-pytorch-1.7-tf-1.15-cpu-1c26118"
+	defaultGPUImage = "determinedai/environments:cuda-10.2-pytorch-1.7-tf-1.15-gpu-1c26118"
 )
 
 // DefaultExperimentConfig returns a new default experiment config.
