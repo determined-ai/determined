0dccbb6d07973ea87526cc9524cc9d01f8f66202
Bradley Laney <bradlaney@determined.ai>
2021-05-04 13:25:40 -0400

    Apply suggestions from code review
    Co-authored-by: Shiyuan <shiyuan@determined.ai>

--- master/internal/db/database.go
+++ master/internal/db/database.go
@@ -8,7 +8,7 @@ import (
 var ErrNotFound = errors.New("not found")
 
 // ErrTooMany is returned if too many rows are affected.
-var ErrTooMany = errors.New("too many")
+var ErrTooManyRowsAffected = errors.New("too many rows are affected")
 
 // ErrDuplicateRecord is returned when trying to create a row that already exists.
 var ErrDuplicateRecord = errors.New("row already exists")
