d520250958368b172ea259f6cbd019d3ada18b0a
Ryan <rb@determined.ai>
2021-05-05 11:47:41 -0600

    use expconf in pkg/model

--- master/pkg/model/experiment.go
+++ master/pkg/model/experiment.go
@@ -10,6 +10,7 @@ import (
 	"github.com/pkg/errors"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
+	"github.com/determined-ai/determined/master/pkg/schemas/expconf"
 	"github.com/determined-ai/determined/master/version"
 	"github.com/determined-ai/determined/proto/pkg/apiv1"
 	"github.com/determined-ai/determined/proto/pkg/logv1"
@@ -186,10 +187,10 @@ var CheckpointReverseTransitions = reverseTransitions(CheckpointTransitions)
 
 // Experiment represents a row from the `experiments` table.
 type Experiment struct {
-	ID             int              `db:"id"`
-	State          State            `db:"state"`
-	Config         ExperimentConfig `db:"config"`
-	OriginalConfig string           `db:"original_config"`
+	ID             int                      `db:"id"`
+	State          State                    `db:"state"`
+	Config         expconf.ExperimentConfig `db:"config"`
+	OriginalConfig string                   `db:"original_config"`
 	// The model definition is stored as a .tar.gz file (raw bytes).
 	ModelDefinitionBytes []byte     `db:"model_definition"`
 	StartTime            time.Time  `db:"start_time"`
@@ -205,16 +206,16 @@ type Experiment struct {
 
 // ExperimentDescriptor is a minimal description of an experiment.
 type ExperimentDescriptor struct {
-	ID       int              `json:"id"`
-	Archived bool             `json:"archived"`
-	Config   ExperimentConfig `json:"config"`
-	Labels   []string         `json:"labels"`
+	ID       int                      `json:"id"`
+	Archived bool                     `json:"archived"`
+	Config   expconf.ExperimentConfig `json:"config"`
+	Labels   []string                 `json:"labels"`
 }
 
 // NewExperiment creates a new experiment struct in the paused state.  Note
 // that the experiment ID will not be set.
 func NewExperiment(
-	config ExperimentConfig,
+	config expconf.ExperimentConfig,
 	originalConfig string,
 	modelDefinitionBytes []byte,
 	parentID *int,
