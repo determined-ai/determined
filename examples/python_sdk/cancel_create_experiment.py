32e5fe17eedc41e411c778b86a170cbc64368262
Mohammad Hossain <mohammad@determined.ai>
2021-05-05 15:50:30 -0700

    determined_client as a separate package

--- examples/python_sdk/cancel_create_experiment.py
+++ examples/python_sdk/cancel_create_experiment.py
@@ -1,12 +1,11 @@
 from determined.common.experimental.determined import Determined
 
-def cancel_experiment() -> None:
-    det = Determined(master='https://gcloud.determined.ai', user='determined')
+def cancel_experiment(det: object) -> None:
     exp1 = det.get_experiment(experiment_id=1)    
     print("Cancelling experimet id: {}".format(exp1.id))
     exp1.cancel()
 
-def create_experiment_wait() -> None:
+def create_experiment_wait(det: object) -> None:
     exp2 = det.create_experiment(exp_config='../tutorials/mnist_pytorch/const.yaml',
                                     model_dir='../tutorials/mnist_pytorch')
     exp2.activate()
@@ -14,9 +13,9 @@ def create_experiment_wait() -> None:
     print("Experiment {} {} is complete".format(exp2.config['description'], exp2.id))
 
 def main():
-    cancel_experiment()
-    create_experiment_wait()
+    det = Determined(master='http://localhost:8080', user='determined')
+    cancel_experiment(det)
+    create_experiment_wait(det)
 
 if __name__ == '__main__':
     main()
-
