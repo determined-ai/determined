a2f531c66c328b136984c8825d9bed6f7ece3508
Justin Chen <justin@determined.ai>
2021-05-12 14:29:48 -0700

    chore: bump version: 0.15.4rc0 -> 0.15.4rc1

--- harness/setup.py
+++ harness/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="determined",
-    version="0.15.4rc0",
+    version="0.15.4rc1",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
