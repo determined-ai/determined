ece7b265a346745f04c0042b999963af1e73b46e
Anda Zhou <anda@determined.ai>
2021-05-12 15:43:14 -0700

    add util test

--- harness/tests/test_util.py
+++ harness/tests/test_util.py
@@ -1,5 +1,7 @@
 from determined.common.util import sizeof_fmt
-from determined.util import _dict_to_list, _list_to_dict
+from determined.util import _dict_to_list, _list_to_dict, sizeof_dict
+import random
+import string
 
 
 def test_list_to_dict() -> None:
@@ -15,3 +17,14 @@ def test_dict_to_list() -> None:
 def test_sizeof_fmt() -> None:
     assert sizeof_fmt(1024) == "1.0KB"
     assert sizeof_fmt(36) == "36.0B"
+
+
+def test_sizeof_dict() -> None:
+    rand_values = [random.randint(0, 100) for i in range(10)]
+    key = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
+    dict = {
+        key: rand_values
+    }
+
+    dict_size = len("{") + len("'") + len(str(key)) + len("'") + len(": ") + len(str(rand_values)) + len("}")
+    assert sizeof_dict(dict) == dict_size
