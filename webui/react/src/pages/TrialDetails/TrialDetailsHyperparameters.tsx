f16f6f98b24776c0c00cace660bb93ae08f94f9f
Alessandro Pizzini <alessandro.pizzini@gmail.com>
2021-05-13 11:54:10 +0200

    table

--- webui/react/src/pages/TrialDetails/TrialDetailsHyperparameters.tsx
+++ webui/react/src/pages/TrialDetails/TrialDetailsHyperparameters.tsx
@@ -1,27 +1,53 @@
-import { List } from 'antd';
-import React from 'react';
+import { ColumnType } from 'antd/es/table';
+import React, { useMemo } from 'react';
 
-import Section from 'components/Section';
+import ResponsiveTable from 'components/ResponsiveTable';
 import { ExperimentBase, TrialDetails } from 'types';
 import { isObject } from 'utils/data';
+import { alphanumericSorter } from 'utils/sort';
 
 export interface Props {
   experiment: ExperimentBase;
   trial: TrialDetails;
 }
 
+interface HyperParameter {
+  hyperparameter: string,
+  value: string,
+}
+
 const TrialDetailsHyperparameters: React.FC<Props> = ({ trial }: Props) => {
+  const columns: ColumnType<HyperParameter>[] = useMemo(() => [
+    {
+      dataIndex: 'hyperparameter',
+      defaultSortOrder: 'ascend',
+      key: 'hyperparameter',
+      sorter: (a: HyperParameter, b: HyperParameter) =>
+        alphanumericSorter(a.hyperparameter, b.hyperparameter),
+      title: 'Hyperparameter',
+    },
+    {
+      dataIndex: 'value',
+      key: 'value',
+      title: 'Value',
+    },
+  ], []);
+  const dataSource: HyperParameter[] = useMemo(() => {
+    return Object.entries(trial.hparams).map(([ hyperparameter, value ]) => {
+      return {
+        hyperparameter,
+        key: hyperparameter,
+        value: isObject(value) ? JSON.stringify(value, null, 2) : value.toString(),
+      };
+    });
+  }, [ trial.hparams ]);
+
   return (
-    <Section bodyBorder bodyNoPadding>
-      <List
-        dataSource={Object.entries(trial.hparams)}
-        renderItem={([ label, value ]) => {
-          const textValue = isObject(value) ? JSON.stringify(value, null, 2) : value.toString();
-          return <List.Item>{label}: {textValue}</List.Item>;
-        }}
-        size="small"
-      />
-    </Section>
+    <ResponsiveTable
+      columns={columns}
+      dataSource={dataSource}
+      pagination={false}
+    />
   );
 };
 
