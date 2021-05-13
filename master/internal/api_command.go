150db7ee2cf2a89f73829b3dafc20c118570d8d9
Shiyuan <shiyuan@determined.ai>
2021-05-13 12:49:14 -0700

    feat: add a preview parameter to the notebook launch API (#2359)

--- master/internal/api_command.go
+++ master/internal/api_command.go
@@ -1,17 +1,18 @@
 package internal
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 
+	"github.com/ghodss/yaml"
+	pstruct "github.com/golang/protobuf/ptypes/struct"
 	"github.com/pkg/errors"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/encoding/protojson"
-
-	pstruct "github.com/golang/protobuf/ptypes/struct"
 
 	"github.com/determined-ai/determined/master/internal/api"
 	"github.com/determined-ai/determined/master/internal/command"
@@ -19,6 +20,8 @@ import (
 	"github.com/determined-ai/determined/master/internal/sproto"
 	"github.com/determined-ai/determined/master/pkg/actor"
 	"github.com/determined-ai/determined/master/pkg/model"
+	"github.com/determined-ai/determined/master/pkg/protoutils"
+	"github.com/determined-ai/determined/master/pkg/tasks"
 	"github.com/determined-ai/determined/proto/pkg/apiv1"
 	"github.com/determined-ai/determined/proto/pkg/commandv1"
 	"github.com/determined-ai/determined/proto/pkg/utilv1"
@@ -31,36 +34,122 @@ type protoCommandParams struct {
 	Config       *pstruct.Struct
 	Files        []*utilv1.File
 	Data         []byte
+	MustZeroSlot bool
+	Preview      bool
 }
 
-// prepareLaunchParams prepares command launch parameters.
-func (a *apiServer) prepareLaunchParams(ctx context.Context, req *protoCommandParams) (
-	*command.CommandParams, *model.User, error,
-) {
-	user, _, err := grpcutil.GetUser(ctx, a.m.db)
-	if err != nil {
-		return nil, nil, status.Errorf(codes.Internal, "failed to get the user: %s", err)
+func (a *apiServer) makeFullCommandSpec(
+	configBytes []byte, templateName *string, mustBeZeroSlot bool,
+) (*model.CommandConfig, *tasks.TaskSpec, error) {
+	resources := model.ParseJustResources(configBytes)
+	taskSpec := a.m.makeTaskSpec(resources.ResourcePool, resources.Slots)
+	config := command.DefaultConfig(&taskSpec.TaskContainerDefaults)
+	if templateName != nil && *templateName != "" {
+		template, err := a.m.db.TemplateByName(*templateName)
+		if err != nil {
+			return nil, nil, errors.Wrapf(err, "failed to find template: %s", *templateName)
+		}
+		if err := yaml.Unmarshal(template.Config, &config); err != nil {
+			return nil, nil, errors.Wrapf(err, "failed to unmarshal template: %s", *templateName)
+		}
 	}
 
-	cmdParams := command.CommandParams{UserFiles: filesToArchive(req.Files)}
-	if req.TemplateName != "" {
-		cmdParams.Template = &req.TemplateName
+	if len(configBytes) != 0 {
+		dec := json.NewDecoder(bytes.NewBuffer(configBytes))
+		dec.DisallowUnknownFields()
+
+		if err := dec.Decode(&config); err != nil {
+			return nil, nil, errors.Wrapf(
+				err,
+				"unable to parse the config in the parameters: %s",
+				string(configBytes),
+			)
+		}
 	}
+
+	// mustBeZeroSlot indicates that this type of command may never use more than
+	// zero slots (as of Jan 2021, this is only Tensorboards). This is important
+	// when building up the config, so that we can route the command to the
+	// correct default resource pool. If the user didn't explicitly set the 'slots' field,
+	// the DefaultConfig will set slots=1. We need to correct that before attempting to fill
+	// in the default resource pool as otherwise we will mistakenly route this CPU task to the
+	// default GPU pool.
+	if mustBeZeroSlot {
+		config.Resources.Slots = 0
+	}
+
+	if err := sproto.ValidateRP(a.m.system, config.Resources.ResourcePool); err != nil {
+		return nil, nil, errors.Wrapf(
+			err, "resource pool does not exist: %s", config.Resources.ResourcePool,
+		)
+	}
+
+	// If the resource pool isn't set, fill in the default at creation time.
+	if config.Resources.ResourcePool == "" {
+		if config.Resources.Slots == 0 {
+			config.Resources.ResourcePool = sproto.GetDefaultCPUResourcePool(a.m.system)
+		} else {
+			config.Resources.ResourcePool = sproto.GetDefaultGPUResourcePool(a.m.system)
+		}
+	}
+
+	return &config, &taskSpec, nil
+}
+
+// prepareLaunchParams prepares launch parameters for Commands, Notebooks, Shells, and TensorBoards.
+func (a *apiServer) prepareLaunchParams(ctx context.Context, req *protoCommandParams) (
+	*command.CommandParams, error,
+) {
+	params := command.CommandParams{}
+	var err error
+
+	// Must get the user and the agent user group
+	params.User, _, err = grpcutil.GetUser(ctx, a.m.db)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "failed to get the user: %s", err)
+	}
+	params.AgentUserGroup, err = a.m.db.AgentUserGroup(params.User.ID)
+	if err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			"cannot find user and group information for user %s: %s",
+			params.User.Username,
+			err,
+		)
+	}
+	if params.AgentUserGroup == nil {
+		params.AgentUserGroup = &a.m.config.Security.DefaultTask
+	}
+
+	// Get the full configuration.
+	var configBytes []byte
 	if req.Config != nil {
-		configBytes, err := protojson.Marshal(req.Config)
+		configBytes, err = protojson.Marshal(req.Config)
 		if err != nil {
-			return nil, nil, errors.Wrapf(err, "failed to parse config: %s", configBytes)
+			return nil, status.Errorf(
+				codes.Internal, "failed to parse config %s: %s", configBytes, err)
 		}
-		cmdParams.ConfigBytes = configBytes
 	}
-	if len(req.Data) != 0 {
+
+	params.FullConfig, params.TaskSpec, err = a.makeFullCommandSpec(
+		configBytes, &req.TemplateName, req.MustZeroSlot)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to make command spec: %s", err)
+	}
+
+	if len(req.Files) > 0 {
+		params.UserFiles = filesToArchive(req.Files)
+	}
+
+	if len(req.Data) > 0 {
 		var data map[string]interface{}
-		if err := json.Unmarshal(req.Data, &data); err != nil {
-			return nil, nil, errors.Wrapf(err, "failed to parse data: %s", req.Data)
+		if err = json.Unmarshal(req.Data, &data); err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to parse data %s: %s", req.Data, err)
 		}
-		cmdParams.Data = data
+		params.Data = data
 	}
-	return &cmdParams, user, nil
+
+	return &params, nil
 }
 
 func (a *apiServer) GetCommands(
@@ -87,7 +176,7 @@ func (a *apiServer) KillCommand(
 func (a *apiServer) LaunchCommand(
 	ctx context.Context, req *apiv1.LaunchCommandRequest,
 ) (*apiv1.LaunchCommandResponse, error) {
-	cmdParams, user, err := a.prepareLaunchParams(ctx, &protoCommandParams{
+	params, err := a.prepareLaunchParams(ctx, &protoCommandParams{
 		TemplateName: req.TemplateName,
 		Config:       req.Config,
 		Files:        req.Files,
@@ -97,10 +186,7 @@ func (a *apiServer) LaunchCommand(
 		return nil, err
 	}
 
-	commandLaunchReq := command.CommandLaunchRequest{
-		CommandParams: cmdParams,
-		User:          user,
-	}
+	commandLaunchReq := command.CommandLaunchRequest{CommandParams: params}
 	commandIDFut := a.m.system.AskAt(commandsAddr, commandLaunchReq)
 	if err = api.ProcessActorResponseError(&commandIDFut); err != nil {
 		return nil, err
@@ -114,5 +200,6 @@ func (a *apiServer) LaunchCommand(
 
 	return &apiv1.LaunchCommandResponse{
 		Command: command.Get().(*commandv1.Command),
+		Config:  protoutils.ToStruct(*params.FullConfig),
 	}, nil
 }
