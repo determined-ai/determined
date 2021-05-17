4f7f4d764111dfd2534601397ced68bd1cc980cd
Rehan Sohail Durrani <rehan@determined.ai>
2021-05-17 15:00:13 -0700

    feat: Add PyTorch Word language Modeling example to Determined's Example [DET-5226] (#2352)
    * Add model.py and begin data.py
    
    * Lay groundwork for dataset
    
    * feat: Add data.py (including dataset and batch sampler) for wlm.
    
    * feat: Add model_def for word language model.
    
    * feat: Add const.yaml experiment config.
    
    * chore: Debugging input batch size
    
    * feat: Add distributed yaml file, as well as other models
    
    * chore: Reformat const.yaml
    
    * chore: replace previous lr scheduler with reduce on plateau scheduler.
    
    * feat: Add README.md
    
    * chore: Add type annotations.
    
    * feat: Add capability and distributed tests.
    
    * chore: Update README's and tar files.
    
    * chore: resolve first pass PR comments, add release notes.
    
    * chore: Only test transformer and rnn.
    
    * chore: move hyperparameters to .yaml file.
    
    * chore: fix host mount path.
    
    * chore: Add eval batch size to .yaml files.
    
    * chore: format docs/examples.txt
    
    * chore: Add placeholder image for README.md
    
    * feat: Add validation loss + perplexity, remove buggy RNN_TANH
    
    * chore: fix cut off table
    
    * chore: replace rnn tests with lstm
    
    merge conflicts

---
+++ examples/nlp/word_language_model/README.md
@@ -0,0 +1,41 @@
+# PyTorch Word-Level Language Modeling Example
+
+This example shows how to build the RNN and Transformer word-level language modeling models
+on the WikiText-2 dataset using Determined's PyTorch API. This example is adapted 
+from this [PyTorch Word-Level Language Modeling example](https://github.com/pytorch/examples/tree/master/word_language_model).
+
+## Files
+* **model.py**: This file defines both models - the RNN and the Transformer.
+* **data.py**: The data loading and preparation code for the model. Unless `use_cache: False` is specified in the experiment config file, this code will attempt to load a cached version of the preprocessed data. If the cached version is unavailable, it will download and preprocess the data, and cache it for future use.
+* **model_def.py**: The core code for training. This includes building and compiling the model.
+
+### Configuration Files
+* **const.yaml**: Train the model on a single GPU with constant hyperparameter values.
+* **distributed.yaml**: Same as `const.yaml`, but trains the model with 8 GPUs (distributed training).
+
+## Data
+The data used for this example is downloaded from [Salesforce Einstein](https://www.salesforce.com/products/einstein/ai-research/the-wikitext-dependency-language-modeling-dataset/) at the start of each run. This is the same source as the original example.
+
+## To Run
+If you have not yet installed Determined, installation instructions can be found
+under `docs/install-admin.html` or at https://docs.determined.ai/latest/index.html
+
+Run the following command: `det -m <master host:port> experiment create -f 
+const.yaml .`. The other configurations can be run by specifying the appropriate 
+configuration file in place of `const.yaml`.
+
+### Specifying the Model
+This example defines 3 models - a Transformer, a recurrent neural network with LSTM cells,
+and a recurrent neural network with GRU cells. We do not include the recurrent neural network with the `tanh` and `ReLU` activations due to numerical instability.
+
+To specify which model to use, replace the `model_cls` attribute in the appropriate .yaml file with one of the following:
+* `Transformer`
+* `LSTM`
+* `GRU`
+
+## Results
+Note: The purpose of this table is to show the word language modeling models running in Determined 
+for a set number of epochs, demonstrating the acceleration of model training time 
+achieved with Determined’s distributed training.
+
+![Single GPU vs. Distributed Training with Determined AI](validation_loss_table.png)
---
+++ examples/nlp/word_language_model/model_def.py
@@ -0,0 +1,146 @@
+"""
+This example is to show how to use an the PyTorch Word Language Modeling example with Determined.
+The flags and configurations can be found under const.yaml for single GPU training, and distributed.yaml
+for distributed training across 8 GPUs. For more information
+regarding the optional flags view the original script linked below.
+This implementation is based on:
+https://github.com/pytorch/examples/tree/master/word_language_model
+"""
+from pathlib import Path
+from typing import Dict, Sequence, Union
+
+import torch
+import torch.nn as nn
+from determined.pytorch import (
+    DataLoader,
+    LRScheduler,
+    PyTorchTrial,
+    PyTorchTrialContext,
+)
+
+import data
+from model import RNNModel, TransformerModel
+
+TorchData = Union[Dict[str, torch.Tensor], Sequence[torch.Tensor], torch.Tensor]
+
+
+class WordLanguageModelTrial(PyTorchTrial):
+    def __init__(self, context: PyTorchTrialContext):
+        self.context = context
+        data_config = self.context.get_data_config()
+        hparams = self.context.get_hparams()
+        using_bind_mount = data_config["use_bind_mount"]
+        use_cache = data_config["use_cache"]
+        self.eval_batch_size = hparams["eval_batch_size"]
+
+        download_directory = (
+            Path(data_config["bind_mount_path"]) if using_bind_mount else Path("/data")
+        ) / f"data-rank{self.context.distributed.get_rank()}"
+
+        self.corpus = data.load_and_cache_dataset(download_directory, use_cache)
+        self.model_cls = hparams["model_cls"]
+        emsize = hparams["word_embeddings_size"]
+        num_hidden = hparams["num_hidden"]
+        num_layers = hparams["num_layers"]
+        dropout = hparams["dropout"]
+        self.bptt = hparams["bptt"]
+
+        if self.model_cls.lower() == "transformer":
+            num_heads = hparams["num_heads"]
+            self.model = TransformerModel(
+                self.corpus.ntokens, emsize, num_heads, num_hidden, num_layers, dropout
+            )
+        else:
+            tied = hparams["tied"]
+            self.model = RNNModel(
+                self.model_cls,
+                self.corpus.ntokens,
+                emsize,
+                num_hidden,
+                num_layers,
+                dropout,
+                tied,
+            )
+
+        self.model = self.context.wrap_model(self.model)
+        self.criterion = nn.NLLLoss()
+
+        lr = hparams["lr"]
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=lr)
+        self.optimizer = self.context.wrap_optimizer(optimizer)
+
+        self.lr_scheduler = self.context.wrap_lr_scheduler(
+            torch.optim.lr_scheduler.ReduceLROnPlateau(
+                self.optimizer,
+                factor=0.25,
+                patience=0,
+                threshold=0.001,
+                threshold_mode="abs",
+                verbose=True,
+            ),
+            LRScheduler.StepMode.MANUAL_STEP,
+        )
+
+    def build_training_data_loader(self) -> DataLoader:
+        train_dataset = data.WikiTextDataset(
+            self.corpus,
+            batch_size=self.context.get_per_slot_batch_size(),
+        )
+        batch_samp = data.BatchSamp(train_dataset, self.bptt)
+        return DataLoader(train_dataset, batch_sampler=batch_samp)
+
+    def build_validation_data_loader(self) -> DataLoader:
+        val_dataset = data.WikiTextDataset(
+            self.corpus,
+            batch_size=self.eval_batch_size,
+            valid=True,
+        )
+        self.val_data_len = len(val_dataset) - 1
+        batch_samp = data.BatchSamp(val_dataset, self.bptt)
+        return DataLoader(val_dataset, batch_sampler=batch_samp)
+
+    def train_batch(
+        self, batch: TorchData, epoch_idx: int, batch_idx: int
+    ) -> Dict[str, Union[torch.Tensor, float]]:
+        if batch_idx == 0 and self.model_cls.lower() != "transformer":
+            self.hidden = self.model.init_hidden(self.context.get_per_slot_batch_size())
+        inputs = batch[:-1]
+        labels = batch[1:].view(-1)
+        if self.model_cls.lower() == "transformer":
+            output = self.model(inputs)
+            output = output.view(-1, self.corpus.ntokens)
+        else:
+            self.hidden = self.model.repackage_hidden(self.hidden)
+            output, self.hidden = self.model(inputs, self.hidden)
+        loss = self.criterion(output, labels)
+
+        self.context.backward(loss)
+        self.context.step_optimizer(
+            self.optimizer,
+            clip_grads=lambda params: torch.nn.utils.clip_grad_norm_(
+                params, self.context.get_hparam("max_grad_norm")
+            ),
+        )
+        return {"loss": loss, "lr": float(self.optimizer.param_groups[0]["lr"])}
+
+    def evaluate_full_dataset(self, data_loader: DataLoader) -> Dict[str, torch.Tensor]:
+        validation_loss = 0.0
+        if self.model_cls.lower() != "transformer":
+            self.hidden = self.model.init_hidden(self.eval_batch_size)
+        for batch in data_loader:
+            batch = self.context.to_device(batch)
+            if self.model_cls.lower() == "transformer":
+                output = self.model(batch[:-1])
+                output = output.view(-1, self.corpus.ntokens)
+            else:
+                output, self.hidden = self.model(batch[:-1], self.hidden)
+                self.hidden = self.model.repackage_hidden(self.hidden)
+            validation_loss += (
+                len(batch[:-1]) * self.criterion(output, batch[1:].view(-1)).item()
+            )
+
+        validation_loss /= len(data_loader.dataset) - 1
+        self.lr_scheduler.step(validation_loss)
+        if self.model_cls.lower() != "transformer":
+            self.hidden = self.model.init_hidden(self.context.get_per_slot_batch_size())
+        return {"validation_loss": validation_loss}
