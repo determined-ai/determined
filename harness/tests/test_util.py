1a59e929e03da76d69abb9e75e52698837852fa0
Anda Zhou <anda@determined.ai>
2021-05-17 15:44:41 -0700

    import error

--- harness/tests/test_util.py
+++ harness/tests/test_util.py
@@ -1,7 +1,5 @@
 from determined.common.util import sizeof_fmt
-from determined.util import _dict_to_list, _list_to_dict, sizeof_dict
-import random
-import string
+from determined.util import _dict_to_list, _list_to_dict
 
 
 def test_list_to_dict() -> None:
