396f4928212158a1df3be96ce813c13f808362b8
Neil Conway <neil@determined.ai>
2021-05-26 12:00:04 -0400

    chore: bump version: 0.15.6.dev0 -> 0.15.6rc0

--- harness/setup.py
+++ harness/setup.py
@@ -5,7 +5,7 @@ packages += [f"determined._swagger.{pkg}" for pkg in find_packages(where="determ
 
 setup(
     name="determined",
-    version="0.15.6.dev0",
+    version="0.15.6rc0",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
