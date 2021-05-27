7c17856475c56e817fa2cffe154a235798abf9f6
Rehan Sohail Durrani <rehan@determined.ai>
2021-05-27 16:56:59 -0700

    chore: catch ruamel.yaml Duplicate Key Errors and format for users [DET-5542] (#2450)
    If `ruamel.yaml`'s `safe_load` is called directly on a faulty yaml file, it gives the user an error with a traceback including our internal apis. This PR adds a utility method that calls `ruamel.yaml`'s `safe_load`, but also catches any errors it may raise, and formats them neatly for the user.

--- harness/determined/cli/command.py
+++ harness/determined/cli/command.py
@@ -8,7 +8,7 @@ from typing import IO, Any, Dict, Iterable, List, Optional, Tuple
 from termcolor import colored
 
 from determined.cli import render
-from determined.common import api, context, yaml
+from determined.common import api, context, util, yaml
 from determined.common.api.authentication import authentication_required
 
 CONFIG_DESC = """
@@ -200,7 +200,7 @@ def parse_config(
     config = {}  # type: Dict[str, Any]
     if config_file:
         with config_file:
-            config = yaml.safe_load(config_file)
+            config = util.safe_load_yaml_with_exceptions(config_file)
 
     for config_arg in overrides:
         if "=" not in config_arg:
