b2d3d7cd1a979e1b762f3e515d1d45e0c8669eec
Caleb Kang <caleb@determined.ai>
2021-05-27 16:19:36 -0600

    chore: update dashboard and task filters to new task filters definition

--- webui/react/src/components/TaskFilter.tsx
+++ webui/react/src/components/TaskFilter.tsx
@@ -37,15 +37,13 @@ const TaskFilter: TaskFilterFC = <T extends TaskType = TaskType>({
   showExperiments = true,
   showLimit = true,
 }: Props<T>) => {
-  const allTypesOff = !Object.values(filters.types).reduce((acc, type) => (acc || type), false);
-  const showCommandStates = allTypesOff ||
-    filters.types[CommandType.Command as T] || filters.types[CommandType.Notebook as T] ||
-    filters.types[CommandType.Shell as T] || filters.types[CommandType.Tensorboard as T];
-  const showExperimentStates = showExperiments && (allTypesOff || filters.types['Experiment' as T]);
-
   const handleTypeClick = useCallback((id: string) => {
-    const types = { ...filters.types, [id as T]: !filters.types[id as T] };
-    onChange({ ...filters, types });
+    const typeId = id as T;
+    const types = filters.types ? [ ...filters.types ] : [];
+    const index = types.indexOf(typeId);
+    if (index === -1) types.push(typeId);
+    else types.splice(index, 1);
+    onChange({ ...filters, types: types.length === 0 ? undefined : types });
   }, [ filters, onChange ]);
 
   const handleStateChange = useCallback((value: SelectValue): void => {
@@ -54,8 +52,8 @@ const TaskFilter: TaskFilterFC = <T extends TaskType = TaskType>({
   }, [ filters, onChange ]);
 
   const handleUserChange = useCallback((value: SelectValue) => {
-    const username = value === ALL_VALUE ? undefined : value as string;
-    onChange({ ...filters, username });
+    const users = value === ALL_VALUE ? undefined : [ value as string ];
+    onChange({ ...filters, users });
   }, [ filters, onChange ]);
 
   const handleLimitSelect = useCallback((limit: SelectValue): void => {
@@ -68,7 +66,7 @@ const TaskFilter: TaskFilterFC = <T extends TaskType = TaskType>({
       ...commandConfig,
     ];
     return taskTypeConfig.map(config => ({
-      active: filters.types[config.id as T],
+      active: Array.isArray(filters.types) && filters.types.includes(config.id as T),
       icon: config.id.toLocaleLowerCase(),
       id: config.id,
       label: capitalize(config.id),
@@ -79,11 +77,11 @@ const TaskFilter: TaskFilterFC = <T extends TaskType = TaskType>({
     <ResponsiveFilters hasFiltersApplied={false}>
       <IconFilterButtons buttons={filterTypeConfig} onClick={handleTypeClick} />
       <StateSelectFilter
-        showCommandStates={showCommandStates}
-        showExperimentStates={showExperimentStates}
+        showCommandStates={true}
+        showExperimentStates={showExperiments}
         value={filters.states}
         onChange={handleStateChange} />
-      <UserSelectFilter value={filters.username} onChange={handleUserChange} />
+      <UserSelectFilter value={filters.users} onChange={handleUserChange} />
       {showLimit && <SelectFilter
         label="Limit"
         showSearch={false}
