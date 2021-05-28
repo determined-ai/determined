c52c6165f6782e4dd17197688d75fbbf237a551d
Bradley Laney <bradlaney@determined.ai>
2021-05-28 17:42:17 -0400

    chore: move run increment to allocation not termination [DET-5559, DET-5450] (#2462)

--- master/internal/trial.go
+++ master/internal/trial.go
@@ -378,6 +378,13 @@ func (t *trial) Receive(ctx *actor.Context) error {
 				name = fmt.Sprintf("Trial (Experiment %d)", t.experiment.ID)
 			}
 
+			// TODO(brad): When we support tracking runs for more than trials, this logic should
+			// likely be generalized by moving it rather than duplicating it for all task types.
+			t.RunID++
+			if err := t.db.AddTrialRun(t.id, t.RunID); err != nil {
+				return errors.Wrap(err, "failed to save trial run")
+			}
+
 			t.task = &sproto.AllocateRequest{
 				ID:             sproto.NewTaskID(),
 				Name:           name,
@@ -618,12 +625,6 @@ func (t *trial) processAllocated(
 
 	if err = saveWorkload(t.db, w); err != nil {
 		ctx.Log().WithError(err).Error("failed to save workload to the database after allocation")
-	}
-
-	// TODO(brad): When we support tracking runs for more than trials, this logic should
-	// likely be generalized by moving it rather than duplicating it for all task types.
-	if err = t.db.AddTrialRun(t.id, t.RunID); err != nil {
-		ctx.Log().WithError(err).Error("failed to save trial run")
 	}
 
 	ctx.Log().Infof("starting trial container: %v", w)
@@ -1138,8 +1139,6 @@ func (t *trial) terminated(ctx *actor.Context) {
 	if err := t.db.CompleteTrialRun(t.id, t.RunID); err != nil {
 		ctx.Log().WithError(err).Error("failed to mark trial run completed")
 	}
-
-	t.RunID++
 
 	if t.task != nil {
 		if err := t.db.DeleteTaskSessionByTaskID(string(t.task.ID)); err != nil {
