b9e88ae8581caf2b13350f1b2b31ac92563cda9c
liam <liam@determined.ai>
2021-06-07 16:31:41 -0700

    chore: bump transformers version for model-hub

--- model_hub/setup.py
+++ model_hub/setup.py
@@ -19,8 +19,8 @@ setup(
     install_requires=[
         "attrdict",
         "determined>=0.13.11",  # We require custom reducers for PyTorchTrial.
-        "transformers==4.4.2",
-        "datasets==1.5.0",
+        "transformers==4.6.1",
+        "datasets==1.7.0",
     ],
     zip_safe=False,
 )
