1797640478ebcdd23ff1b936a062f3a8a5f28a2c
Sean Mackrory <mackrory@determined.ai>
2021-06-07 13:40:05 -0600

    chore: update urllib3 (#2504)

--- harness/setup.py
+++ harness/setup.py
@@ -37,13 +37,7 @@ setup(
         "pathspec>=0.6.0",
         "simplejson",
         "termcolor>=1.1.0",
-        # boto3 1.14.11+ has consistent urllib3 requirements which we have to manually resolve.
-        "boto3>=1.14.11",
-        # requests<2.22.0 requires urllib3<1.25, which is incompatible with boto3>=1.14.11
-        "requests>=2.22.0",
-        # botocore>1.19.0 has stricter urllib3 requirements than boto3, and pip will not reliably
-        # resolve it until the --use-feature=2020-resolver behavior in pip 20.3, so we list it here.
-        "urllib3>=1.25.4,<1.26",
+        "boto3",
         # CLI:
         "argcomplete>=1.9.4",
         "gitpython>=3.1.3",
