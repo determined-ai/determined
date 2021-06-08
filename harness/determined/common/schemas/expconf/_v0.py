5da2c200fe8a1763a24da317a00b4246b9a5fddc
azhou-determined <anda@determined.ai>
2021-06-08 14:51:24 -0700

    lint

--- harness/determined/common/schemas/expconf/_v0.py
+++ harness/determined/common/schemas/expconf/_v0.py
@@ -241,9 +241,13 @@ class EnvironmentImageV0(schemas.SchemaBase):
 
     def runtime_defaults(self) -> None:
         if self.cpu is None:
-            self.cpu = "determinedai/environments:py-3.7-pytorch-1.7-lightning-1.2-tf-2.4-cpu-da845fc"
+            self.cpu = (
+                "determinedai/environments:py-3.7-pytorch-1.7-lightning-1.2-tf-2.4-cpu-da845fc"
+            )
         if self.gpu is None:
-            self.gpu = "determinedai/environments:cuda-11.0-pytorch-1.7-lightning-1.2-tf-2.4-gpu-da845fc"
+            self.gpu = (
+                "determinedai/environments:cuda-11.0-pytorch-1.7-lightning-1.2-tf-2.4-gpu-da845fc"
+            )
 
 
 class EnvironmentVariablesV0(schemas.SchemaBase):
