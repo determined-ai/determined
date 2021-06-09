2c7c5ee88737653ef5411537b9de3c6eed611b44
Alessandro Pizzini <alessandro.pizzini@gmail.com>
2021-06-09 20:11:33 +0200

    chore: revamp experiment and trial pages header [DET-5406] (#2456)

---
+++ webui/react/src/components/PageHeaderFoldable.tsx
@@ -0,0 +1,103 @@
+import { Button, Dropdown, Menu, Tooltip } from 'antd';
+import React, { CSSProperties, useState } from 'react';
+
+import Icon from 'components/Icon';
+
+import css from './PageHeaderFoldable.module.scss';
+
+export interface Option {
+  icon?: React.ReactNode,
+  isLoading?: boolean,
+  key: string;
+  label: string;
+  onClick?: () => void;
+  tooltip?: string;
+}
+
+interface Props {
+  foldableContent?: React.ReactNode,
+  leftContent: React.ReactNode,
+  options?: Option[],
+  style?: CSSProperties;
+}
+
+const renderOptionLabel =
+  (option: Option): React.ReactNode => {
+    return option.tooltip
+      ? <Tooltip title={option.tooltip}><span>{option.label}</span></Tooltip>
+      : <span>{option.label}</span>;
+  };
+
+const PageHeaderFoldable: React.FC<Props> = (
+  { foldableContent, leftContent, options, style }: Props,
+) => {
+  const [ isExpanded, setIsExpanded ] = useState(false);
+
+  const dropdownClasses = [ css.optionsDropdown ];
+  let dropdownOptions = null;
+  if (options && options.length > 0) {
+    if (options.length === 1) dropdownClasses.push(css.optionsDropdownOneChild);
+    if (options.length === 2) dropdownClasses.push(css.optionsDropdownTwoChild);
+    dropdownOptions = (
+      <Menu>
+        {options.map(opt => (
+          <Menu.Item
+            className={css.optionsDropdownItem}
+            disabled={!opt.onClick}
+            key={opt.key}
+            onClick={opt.onClick}
+          >{renderOptionLabel(opt)}</Menu.Item>
+        ))}
+      </Menu>
+    );
+  }
+
+  return (
+    <>
+      <div className={css.base} style={style}>
+
+        <div className={css.left}>
+          {leftContent}
+        </div>
+
+        {foldableContent && (
+          <div className={css.toggle} onClick={() => setIsExpanded(!isExpanded)}>
+            <Icon name={isExpanded ? 'arrow-up' : 'arrow-down'} />
+          </div>
+        )}
+
+        <div className={css.options}>
+          {options && options.slice(0, 2).map((opt, i) => (
+            <Button
+              className={css.optionsMainButton}
+              disabled={!opt.onClick}
+              ghost={i !== 0}
+              icon={opt?.icon}
+              key={opt.key}
+              loading={opt.isLoading}
+              onClick={opt.onClick}
+            >{renderOptionLabel(opt)}</Button>
+          ))}
+          {dropdownOptions && (
+            <Dropdown arrow overlay={dropdownOptions} placement="bottomRight">
+              <Button
+                className={dropdownClasses.join(' ')}
+                ghost={true}
+                icon={<Icon name="overflow-vertical" />}
+              />
+            </Dropdown>
+          )}
+        </div>
+
+        {foldableContent && isExpanded && (
+          <div className={css.bottom}>
+            {foldableContent}
+          </div>
+        )}
+
+      </div>
+    </>
+  );
+};
+
+export default PageHeaderFoldable;
