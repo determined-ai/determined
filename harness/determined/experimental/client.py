e0c8decc55c540429ad4a10c6b2ce71b2647f606
Ryan <rb@determined.ai>
2021-06-09 17:55:59 -0600

    docs: extend docs for the client module (#2556)
    (cherry picked from commit 251e7093b60a92633b684586ac7a566379442f15)

--- harness/determined/experimental/client.py
+++ harness/determined/experimental/client.py
@@ -1,3 +1,48 @@
+"""
+The ``client`` module exposes man of the same capabilities as the ``det`` CLI tool directly to
+Python code with an object-oriented interface.
+
+As a simple example, let's walk through the most basic workflow for creating an experiment,
+waiting for it to complete, and finding the top-performing checkpoint.
+
+The first step is to import the ``client`` module and possibly to call
+:func:`~determined.experimental.client.login`:
+
+.. code:: python
+
+   from determined.experimental import client
+
+   # We will assume that you have called `det user login`, so this is unnecessary:
+   # client.login(master=..., user=..., password=...)
+
+The next step is to call :func:`~determined.experimental.client.create_experiment`:
+
+.. code:: python
+
+   # config can be a path to a config file or a python dict of the config.
+   exp = client.create_experiment(config="my_config.yaml", model_dir=".")
+   print(f"started experiment {exp.id}")
+
+The returned object will be an :class:`~determined.experimental.client.ExperimentReference`
+which has methods for controlling the lifetime of the experiment running on the cluster.
+In this example, we will just wait for the experiment to complete.
+
+.. code:: python
+
+   exit_status = exp.wait()
+   print(f"experiment completed with status {exit_status}")
+
+Now that the experiment has completed, you can grab the top-performing checkpoint from training:
+
+.. code:: python
+
+   best_checkpoint = exp.top_checkpoint()
+   print(f"best checkpoint was {best_checkpoint.uuid}")
+
+
+See :ref:`use-trained-models` for more ideas on what to do next.
+"""
+
 import functools
 import pathlib
 from typing import Any, Callable, Dict, List, Optional, Union
