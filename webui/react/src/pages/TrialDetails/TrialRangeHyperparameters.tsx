c201726458ddee4c4f964684fbca2a87f618d6c0
Emily Bonar <emily@determined.ai>
2021-06-10 14:57:54 -0500

    Laying out groundwork

--- webui/react/src/pages/TrialDetails/TrialRangeHyperparameters.tsx
+++ webui/react/src/pages/TrialDetails/TrialRangeHyperparameters.tsx
@@ -8,25 +8,54 @@ export interface Props {
 }
 
 interface HyperParameter {
-  hyperparameter: string,
+  name: string,
   value: ExperimentHyperParam,
 }
 
 const TrialRangeHyperparameters: React.FC<Props> = ({ experiment }: Props) => {
   const dataSource: HyperParameter[] = useMemo(() => {
-    return Object.entries(experiment.config.hyperparameters).map(([ hyperparameter, value ]) => {
+    return Object.entries(experiment.config.hyperparameters).map(([ name, value ]) => {
       return {
-        hyperparameter,
+        name,
         value,
       };
     });
   }, [ experiment.config.hyperparameters ]);
 
   return (
-    <div>
-      {dataSource.map(hp => <div key={hp.hyperparameter}>{JSON.stringify(hp.value)}</div>)}
+    <div style={{ display: 'flex', gap: 20 }}>
+      {dataSource.map(hp => <div key={hp.name}>
+        <HyperparameterRange name={hp.name} value={hp.value} />
+      </div>)}
     </div>
   );
+};
+
+const HyperparameterRange:React.FC<HyperParameter> = ({ name, value }: HyperParameter) => {
+  return <div style={{ display: 'flex', flexDirection: 'column' }}>{name}<div
+    style={{
+      alignSelf: 'center',
+      backgroundColor: 'lightgray',
+      borderRadius: 5,
+      display: 'flex',
+      flexDirection: 'column',
+      height: 200,
+      justifyContent: 'space-between',
+      width: 10,
+    }}>
+    {value.vals?.map(op =>
+      <div
+        key={op.toString()}
+        style={{
+          backgroundColor: 'blue',
+          borderRadius: '100%',
+          height: 12,
+          left: -12/8,
+          position: 'relative',
+          width: 12,
+        }}
+      />)}
+  </div></div>;
 };
 
 export default TrialRangeHyperparameters;
