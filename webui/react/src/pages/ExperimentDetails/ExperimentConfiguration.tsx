b81a246582e032be4732f4c78759293725c75356
Alessandro Pizzini <alessandro.pizzini@gmail.com>
2021-06-14 19:32:34 +0200

    remove un-needed styling

--- webui/react/src/pages/ExperimentDetails/ExperimentConfiguration.tsx
+++ webui/react/src/pages/ExperimentDetails/ExperimentConfiguration.tsx
@@ -5,8 +5,6 @@ import MonacoEditor from 'react-monaco-editor';
 import Section from 'components/Section';
 import { ExperimentBase } from 'types';
 
-import css from './ExperimentConfiguration.module.scss';
-
 interface Props {
   experiment: ExperimentBase;
 }
@@ -14,20 +12,18 @@ interface Props {
 const ExperimentConfiguration: React.FC<Props> = ({ experiment }: Props) => {
   return (
     <Section bodyBorder>
-      <div className={css.base}>
-        <MonacoEditor
-          height="60vh"
-          language="yaml"
-          options={{
-            minimap: { enabled: false },
-            occurrencesHighlight: false,
-            readOnly: true,
-            scrollBeyondLastLine: false,
-            selectOnLineNumbers: true,
-          }}
-          theme="vs-light"
-          value={yaml.dump(experiment.configRaw)} />
-      </div>
+      <MonacoEditor
+        height="60vh"
+        language="yaml"
+        options={{
+          minimap: { enabled: false },
+          occurrencesHighlight: false,
+          readOnly: true,
+          scrollBeyondLastLine: false,
+          selectOnLineNumbers: true,
+        }}
+        theme="vs-light"
+        value={yaml.dump(experiment.configRaw)} />
     </Section>
   );
 };
