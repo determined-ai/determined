e9e65499dcfcbd7dc77980ecabb0b3e4d43e67a6
azhou-determined <83614683+azhou-determined@users.noreply.github.com>
2021-06-16 15:21:57 -0700

    support tensorflow profiling plugin (#2577)

--- harness/determined/tensorboard/base.py
+++ harness/determined/tensorboard/base.py
@@ -1,16 +1,18 @@
 import abc
-import logging
 import pathlib
 import time
 from typing import List
 
+from determined.tensorboard import util
+
 
 class TensorboardManager(metaclass=abc.ABCMeta):
     """
-    TensorboardManager stores tfevent logs to a supported storage backend. The
-    trial will generate tfevent files during training. The tfevent files must
-    be written to the same base_path as the base_path used in the contructor of
-    this class.
+    TensorboardManager stores tensorboard logs (tfevent files, .gz zipped archives,
+    and .pb protobuf graph and model definition files) to a supported storage backend. The
+    trial will generate tfevent files during training. If a profiling callback is used,
+    .pb and .gz files will also be generated. These files must be written to the same
+    base_path as the base_path used in the constructor of this class.
 
     Each supported persistent storage backend must define a subclass which
     implements the sync method.
@@ -25,29 +27,23 @@ class TensorboardManager(metaclass=abc.ABCMeta):
         self.sync_path = sync_path
         self.last_sync = 0.0
 
-    def list_tfevents(self, since: float) -> List[pathlib.Path]:
+    def list_tb_files(self, since: float) -> List[pathlib.Path]:
         """
-        list_tfevents returns tfevent file names located in the base_path directory that have been
-        modified since a certain time.
+        list_files returns Tensorboard-relevant file names located in the base_path directory
+        and all sub-directories that have been modified since a certain time.
 
-        If many tfevent files have been created, the syscall to stat on each of them can be quite
+        If many files have been created, the syscall to stat on each of them can be quite
         expensive, taking on the order of 1ms for every 100 files. Each file gets stat'd every call
         to this function, which can be a bottleneck late in training or in local training when many
-        tfevent files exist but few or none need to be resynced.
+        files exist but few or none need to be re-synced.
         """
 
-        if not self.base_path.exists():
-            logging.warning(
-                f"{self.base_path} directory does not exist. "
-                "Trial does not include the correct callback for TensorBoard"
-            )
-            return []
-
-        return [f for f in self.base_path.glob("**/*tfevents*") if f.stat().st_mtime > since]
+        tb_files = util.find_tb_files(self.base_path)
+        return list(filter(lambda file: file.stat().st_mtime > since, tb_files))
 
     def to_sync(self) -> List[pathlib.Path]:
         sync_start = time.time()
-        sync_paths = self.list_tfevents(self.last_sync)
+        sync_paths = self.list_tb_files(self.last_sync)
         self.last_sync = sync_start
 
         return sync_paths
