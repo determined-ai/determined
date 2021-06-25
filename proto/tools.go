5f98e0b718abaca711cb092075edc9941f48af04
Bradley Laney <bradlaney@determined.ai>
2021-06-25 14:17:40 -0400

    chore: add tooling to document echo-based apis (#2529)
    This change adds swag (https://github.com/swaggo/swag) to doc echo APIs. This is useful because some APIs cannot be expressed within the constraints of grpc<->grpc-gateway land, e.g. endpoints that return CSV, or generally endpoints that accept or return MIME types other than application/json.

--- proto/tools.go
+++ proto/tools.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/bufbuild/buf/cmd/buf"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "github.com/swaggo/swag/cmd/swag"
 	_ "google.golang.org/grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
