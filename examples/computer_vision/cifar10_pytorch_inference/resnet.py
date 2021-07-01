a06f817f7e4f214b2407f4bbbec59b11ba1c4970
Angela Jiang <ahj@determined.ai>
2021-07-01 10:09:30 -0700

    Remove need to add state_dicts dir

--- examples/computer_vision/cifar10_pytorch_inference/resnet.py
+++ examples/computer_vision/cifar10_pytorch_inference/resnet.py
@@ -264,7 +264,7 @@ def _resnet(arch, block, layers, pretrained, progress, device, **kwargs):
     if pretrained:
         script_dir = os.path.dirname(__file__)
         state_dict = torch.load(
-            script_dir + "/state_dicts/" + arch + ".pt", map_location=device
+            script_dir + "/" + arch + ".pt", map_location=device
         )
         model.load_state_dict(state_dict)
     return model
--- examples/computer_vision/cifar10_pytorch_inference/startup-hook.sh
+++ examples/computer_vision/cifar10_pytorch_inference/startup-hook.sh
@@ -1,2 +1,2 @@
 pip install gdown
-gdown https://drive.google.com/uc?id=1JTchzEFqtjbAVWXyNa5BkYPi12_CoHlS -O state_dicts/resnet18.pt
+gdown https://drive.google.com/uc?id=1JTchzEFqtjbAVWXyNa5BkYPi12_CoHlS -O resnet18.pt
