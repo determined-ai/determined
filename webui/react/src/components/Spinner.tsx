7397a9d0d49200da554c7745690ccbc4c6a0c41f
Alessandro Pizzini <alessandro.pizzini@gmail.com>
2021-07-01 08:31:52 +0200

    chore: make experiment description editable via webui [DET-4398] (#2634)

--- webui/react/src/components/Spinner.tsx
+++ webui/react/src/components/Spinner.tsx
@@ -2,16 +2,22 @@ import { Spin } from 'antd';
 import { SpinProps, SpinState } from 'antd/es/spin';
 import React, { PropsWithChildren } from 'react';
 
-import Icon from 'components/Icon';
+import Icon, { IconSize } from 'components/Icon';
 
 import css from './Spinner.module.scss';
 
 interface Props extends SpinProps, SpinState {}
 
-export const IndicatorUnpositioned: React.FC = () => {
+interface IndicatorUnpositionedProps {
+  size?: IconSize;
+}
+
+export const IndicatorUnpositioned: React.FC<IndicatorUnpositionedProps> =(
+  { size = 'large' }: IndicatorUnpositionedProps,
+) => {
   const classes = [ css.spin ];
   return <div className={classes.join(' ')}>
-    <Icon name="spinner" size="large" />
+    <Icon name="spinner" size={size} />
   </div>;
 };
 
