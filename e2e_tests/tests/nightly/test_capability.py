90a80e2ee127686a98eb31b445bd1cc75dd83700
katport <57640100+katport@users.noreply.github.com>
2021-07-07 13:36:36 -0700

    feat: Add Efficientdet example (#1733)

--- e2e_tests/tests/nightly/test_capability.py
+++ e2e_tests/tests/nightly/test_capability.py
@@ -46,6 +46,14 @@ def test_detr_coco_pytorch_const() -> None:
 
 
 @pytest.mark.nightly  # type: ignore
+def test_efficientdet_coco_pytorch_const() -> None:
+    config = conf.load_config(conf.cv_examples_path("efficientdet_pytorch/const_fake.yaml"))
+    config = conf.set_max_length(config, {"batches": 200})
+
+    exp.run_basic_test_with_temp_config(config, conf.cv_examples_path("efficientdet_pytorch"), 1)
+
+
+@pytest.mark.nightly  # type: ignore
 def test_deformabledetr_coco_pytorch_const() -> None:
     config = conf.load_config(conf.cv_examples_path("deformabledetr_coco_pytorch/const_fake.yaml"))
     config = conf.set_max_length(config, {"batches": 200})
