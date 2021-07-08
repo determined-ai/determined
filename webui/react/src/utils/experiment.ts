22886b5a4c631ce6f90e1d0d8738bcb670b019a3
Caleb Kang <caleb@determined.ai>
2021-07-08 13:25:00 -1000

    refactor: update decoder to support nested hp

--- webui/react/src/utils/experiment.ts
+++ webui/react/src/utils/experiment.ts
@@ -1,4 +1,23 @@
-import { ExperimentBase, ExperimentSearcherName } from 'types';
+import {
+  ExperimentBase, ExperimentSearcherName, Hyperparameter,
+  Hyperparameters, HyperparametersFlattened,
+} from 'types';
+
+export const flattenHyperParams = (
+  hyperparams: Hyperparameters,
+  keys: string[] = [],
+): HyperparametersFlattened => {
+  return Object.keys(hyperparams).reduce((acc, key) => {
+    const hp = hyperparams[key];
+    const keyPath = [ ...keys, key ].join('.');
+    if (hp.type) {
+      acc[keyPath] = hp as Hyperparameter;
+    } else {
+      acc = { ...acc, ...flattenHyperParams(hp as Hyperparameters, [ ...keys, key ]) };
+    }
+    return acc;
+  }, {} as HyperparametersFlattened);
+};
 
 export const isSingleTrialExperiment = (experiment: ExperimentBase): boolean => {
   return experiment?.config.searcher.name === ExperimentSearcherName.Single
