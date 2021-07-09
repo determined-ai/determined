69935d054c650632d9222111136cdc6403ec341c
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2021-07-09 12:03:31 -0700

    chore: improve dynamic server address support (#2690)
    - add a check for server address validity before accepting the address through omnibar
    - unify post server address change process

--- webui/react/src/dev.ts
+++ webui/react/src/dev.ts
@@ -1,16 +1,27 @@
 /* Tools and tweaks for dev environments */
 import { globalStorage } from 'globalStorage';
+import { paths, routeToReactUrl, serverAddress } from 'routes/utils';
 import * as Api from 'services/api';
 import { updateDetApi } from 'services/apiConfig';
+
+const onServerAddressChange = () => {
+  updateDetApi({ basePath: serverAddress() });
+  routeToReactUrl(paths.logout());
+};
 
 export const setServerAddress = (address: string): void => {
   const serverAddress = address.replace(/\/\s*$/, '');
   globalStorage.serverAddress = serverAddress;
-  updateDetApi({ basePath: serverAddress });
+  onServerAddressChange();
+};
+
+export const resetServerAddress = (): void => {
+  globalStorage.removeServerAddress();
+  onServerAddressChange();
 };
 
 window.dev = {
-  resetServerAddress: () => globalStorage.removeServerAddress(),
+  resetServerAddress,
   setServerAddress,
 };
 
