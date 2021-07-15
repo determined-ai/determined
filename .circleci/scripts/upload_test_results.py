ee84fd627613d491c0c2cc76e30e99a584e94520
Anda <anda@determined.ai>
2021-07-15 04:03:51 -0700

    access key

--- .circleci/scripts/upload_test_results.py
+++ .circleci/scripts/upload_test_results.py
@@ -4,27 +4,28 @@ import tempfile
 import requests
 
 
-def upload_results(directory, job_id) -> None:
+def upload_results(directory, job_id, access_key) -> None:
     with tempfile.TemporaryFile(suffix=".tar.gz") as temp_archive:
         with tarfile.open(fileobj=temp_archive, mode="w:gz") as tar_archive:
             tar_archive.add(directory, recursive=True)
+
         # Flush write data and reset pointer
         temp_archive.flush()
         temp_archive.seek(0)
         response = requests.post("http://34.215.54.95/upload",
+                                 headers={"x-api-key": access_key},
                                  files={"report": temp_archive},
                                  data={"job_id": job_id})
         print(response)
-
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Parse test results path")
     parser.add_argument("filepath", help="Test results filepath")
     parser.add_argument("job_id", help="CircleCI job id")
+    parser.add_argument("access_key", help="Determined CI API key")
     args = parser.parse_args()
-    print(args)
-    upload_results(args.filepath, args.job_id)
+    upload_results(args.filepath, args.job_id, args.access_key)
 
 
 if __name__ == "__main__":
