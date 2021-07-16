fe4a056435dbc61bc7caa443db95d5643b0832f9
Bradley Laney <bradlaney@determined.ai>
2021-07-16 14:04:08 -0400

    feat: asycn DELETE experiment

---
+++ master/static/migrations/20210716133048_add-deleting-states.down.sql
@@ -0,0 +1,13 @@
+ALTER TYPE public.experiment_state RENAME TO _experiment_state;
+CREATE TYPE public.experiment_state AS ENUM (
+    'ACTIVE',
+    'CANCELED',
+    'COMPLETED',
+    'ERROR',
+    'PAUSED',
+    'STOPPING_CANCELED',
+    'STOPPING_COMPLETED',
+    'STOPPING_ERROR'
+);
+ALTER TABLE public.experiments ALTER COLUMN state TYPE experiment_state USING state::text::experiment_state;
+DROP TYPE _experiment_state;
