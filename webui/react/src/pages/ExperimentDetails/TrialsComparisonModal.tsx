d5106d2c1ecabf86b64ccc643cc2679a7470103b
Emily Bonar <emily@determined.ai>
2021-07-22 11:22:20 -0500

    Set up basic info

--- webui/react/src/pages/ExperimentDetails/TrialsComparisonModal.tsx
+++ webui/react/src/pages/ExperimentDetails/TrialsComparisonModal.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 
 import { ExperimentBase, TrialItem } from 'types';
 
+import css from './TrialsComparisonModal.module.scss';
+
 interface ModalProps {
   experiment: ExperimentBase;
   trials: TrialItem[];
@@ -23,7 +25,42 @@ const TrialsComparisonModal: React.FC<ModalProps> =
 };
 
 const TrialsComparisonTable: React.FC<TableProps> = ({ trials }: TableProps) => {
-  return <div>{trials.map(trial => trial.id)}</div>;
+  return (
+    <div className={css.tableContainer}>
+      <div className={css.headerRow}><div />{trials.map(trial => trial.id)}</div>
+      <div className={css.row}><h3>State</h3>{trials.map(trial => trial.state)}</div>
+      <div className={css.row}><h3>Start Time</h3>{trials.map(trial => trial.startTime)}</div>
+      <div className={css.row}>
+        <h3>Training Time</h3>
+        {trials.map(trial => 'Need Workloads' + trial.id)}
+      </div>
+      <div className={css.row}>
+        <h3>Validation Time</h3>
+        {trials.map(trial => 'Need Workloads' + trial.id)}
+      </div>
+      <div className={css.row}>
+        <h3>Checkpoint Time</h3>
+        {trials.map(trial => 'Need Workloads' + trial.id)}
+      </div>
+      <div className={css.row}>
+        <h3>Batches Processed</h3>
+        {trials.map(trial => trial.totalBatchesProcessed)}
+      </div>
+      <div className={css.row}>
+        <h3>Best Checkpoint</h3>
+        {trials.map(trial => trial.bestAvailableCheckpoint)}
+      </div>
+      <div className={css.row}>
+        <h3>Total Checkpoint Size</h3>
+        {trials.map(trial => 'Need Workloads'+ trial.id)}
+      </div>
+      <div className={css.headerRow}><h2>Metrics</h2></div>
+      {trials.map(trial => 'Need Workloads'+ trial.id)}
+      <div className={css.headerRow}><h2>Hyperparameters</h2></div>
+      {Object.keys(trials.first().hyperparameters).map(hp =>
+        trials.map(trial => trial.hyperparameters[hp]))}
+    </div>
+  );
 };
 
 export default TrialsComparisonModal;
