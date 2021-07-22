20b72bcbec98c7e1375ee84a20a56c0ecba19a9d
Emily Bonar <emily@determined.ai>
2021-07-22 13:41:42 -0500

    Fetch trial details

--- webui/react/src/pages/ExperimentDetails/TrialsComparisonModal.tsx
+++ webui/react/src/pages/ExperimentDetails/TrialsComparisonModal.tsx
@@ -1,12 +1,15 @@
 import Modal from 'antd/lib/modal/Modal';
-import React from 'react';
+import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react';
 
-import { ExperimentBase, TrialItem } from 'types';
+import { getTrialDetails } from 'services/api';
+import { ApiState } from 'services/types';
+import { isAborted } from 'services/utils';
+import { TrialDetails, TrialItem } from 'types';
 
 import css from './TrialsComparisonModal.module.scss';
 
 interface ModalProps {
-  experiment: ExperimentBase;
   trials: TrialItem[];
   visible: boolean;
 }
@@ -16,15 +19,56 @@ interface TableProps {
 }
 
 const TrialsComparisonModal: React.FC<ModalProps> =
-({ experiment, trials, visible }: ModalProps) => {
+({ trials, visible }: ModalProps) => {
   return (
-    <Modal title={`Experiment ${experiment.id} Trial Comparison`} visible={visible}>
+    <Modal title={`Experiment ${trials.first().experimentId} Trial Comparison`} visible={visible}>
       <TrialsComparisonTable trials={trials} />
     </Modal>
   );
 };
 
 const TrialsComparisonTable: React.FC<TableProps> = ({ trials }: TableProps) => {
+  const [ trialsDetails, setTrialsDetails ] = useState<Record<string, ApiState<TrialDetails>>>({});
+  const [ source ] = useState(axios.CancelToken.source());
+  const [ canceler ] = useState(new AbortController());
+
+  const fetchTrialDetails = useCallback(async (trialId) => {
+    try {
+      const response = await getTrialDetails({ id: trialId }, { signal: canceler.signal });
+      setTrialsDetails(prev => (
+        { ...prev, [trialId]: { ...prev[trialId], data: response, isLoading: false } }
+      ));
+    } catch (e) {
+      if (!trialsDetails[trialId].error && !isAborted(e)) {
+        setTrialsDetails(prev => ({ ...prev, [trialId]: { ...prev[trialId], error: e } }));
+      }
+    }
+  }, [ canceler.signal, trialsDetails ]);
+
+  useEffect(() => {
+    return () => {
+      source.cancel();
+      canceler.abort();
+    };
+  }, [ canceler, source ]);
+
+  useEffect(() => {
+    const trialIds = trials.map(trial => {
+      setTrialsDetails(prev =>
+        ({
+          ...prev,
+          [trial.id]: {
+            data: undefined,
+            error: undefined,
+            isLoading: true,
+            source,
+          },
+        }));
+      return trial.id;
+    });
+    trialIds.forEach(trialId => fetchTrialDetails(trialId));
+  }, [ fetchTrialDetails, source, trials ]);
+
   return (
     <div className={css.tableContainer}>
       <div className={css.headerRow}><div />{trials.map(trial => trial.id)}</div>
