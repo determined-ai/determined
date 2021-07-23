259b27a1bad9823d9b2c327fe6c3a553fcaf87e9
Caleb Kang <caleb@determined.ai>
2021-07-22 17:52:20 -1000

    refactor: update create experiment to not have a static component

--- webui/react/src/hooks/useCreateExperimentModal.tsx
+++ webui/react/src/hooks/useCreateExperimentModal.tsx
@@ -1,11 +1,23 @@
-import React, { useCallback, useState } from 'react';
+import { Alert, Form, FormInstance, Input, Modal, ModalFuncProps } from 'antd';
+import { ModalFunc } from 'antd/es/modal/confirm';
+import yaml from 'js-yaml';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
-import CreateExperimentModal, { CreateExperimentType } from 'components/CreateExperimentModal';
+import Icon from 'components/Icon';
+import Spinner from 'components/Spinner';
 import { paths, routeToReactUrl } from 'routes/utils';
 import { createExperiment } from 'services/api';
 import { ExperimentBase, RawJson, TrialDetails, TrialHyperparameters } from 'types';
 import { clone } from 'utils/data';
 import { trialHParamsToExperimentHParams, upgradeConfig } from 'utils/types';
+
+import css from './useCreateExperimentModal.module.scss';
+import usePrevious from './usePrevious';
+
+export enum CreateExperimentType {
+  Fork = 'Fork',
+  ContinueTrial = 'Continue Trial',
+}
 
 interface ShowProps {
   experiment: ExperimentBase;
@@ -15,18 +27,32 @@ interface ShowProps {
 
 interface ModalState {
   config: RawJson;
+  configError?: string;
+  configString: string;
   error?: string;
   experiment?: ExperimentBase;
-  title: string;
+  isAdvancedMode: boolean;
   trial?: TrialDetails;
   type: CreateExperimentType;
   visible: boolean;
 }
 
 interface ModalHooks {
-  createModal: () => JSX.Element | null;
   showModal: (props: ShowProps) => void;
 }
+
+const getExperimentName = (config: RawJson) => {
+  return config.name || '';
+};
+
+const getMaxLengthType = (config: RawJson) => {
+  return (Object.keys(config.searcher?.max_length || {}) || [])[0];
+};
+
+const getMaxLengthValue = (config: RawJson) => {
+  const value = (Object.keys(config.searcher?.max_length || {}) || [])[1];
+  return value ? parseInt(value) : undefined;
+};
 
 const trialContinueConfig = (
   experimentConfig: RawJson,
@@ -46,50 +72,82 @@ const trialContinueConfig = (
   };
 };
 
+const MonacoEditor = React.lazy(() => import('react-monaco-editor'));
+
 const useCreateExperimentModal = (): ModalHooks => {
-  const [ state, setState ] = useState<ModalState>({
+  const modalRef = useRef<ReturnType<ModalFunc>>();
+  const formRef = useRef<FormInstance>(null);
+  const [ modalState, setModalState ] = useState<ModalState>({
     config: {},
-    title: '',
+    configString: '',
+    isAdvancedMode: false,
     type: CreateExperimentType.Fork,
     visible: false,
   });
-  const [ error, setError ] = useState<string>();
+  const prevIsAdvancedMode = usePrevious(modalState.isAdvancedMode, false);
 
   const showModal = useCallback(({ experiment, trial, type }: ShowProps) => {
-    const isContinueTrial = type === CreateExperimentType.ContinueTrial;
+    const isFork = type === CreateExperimentType.Fork;
 
-    let title = '';
     let config = clone(experiment.configRaw);
-
-    if (isContinueTrial && trial) {
-      title = `Continue Trial ${trial.id}`;
+    if (!isFork && trial) {
       config = trialContinueConfig(config, trial.hyperparameters, trial.id);
       config.description = `Continuation of trial ${trial.id}, experiment ${experiment.id}` +
         (config.description ? ` (${config.description})` : '');
-    } else if (!isContinueTrial) {
-      title = `Fork Experiment ${experiment.id}`;
+    } else if (!isFork) {
       if (config.description) config.description = `Fork of ${config.description}`;
     }
-
     upgradeConfig(config);
-    setState({ config, experiment, title, trial, type, visible: true });
+
+    setModalState({
+      config,
+      configString: yaml.dump(config),
+      experiment,
+      isAdvancedMode: false,
+      trial,
+      type,
+      visible: true,
+    });
   }, []);
 
-  const handleCancel = useCallback(() => {
-    setState(prev => ({ ...prev, visible: false }));
+  const closeModal = useCallback(() => {
+    if (!modalRef.current) return;
+    modalRef.current.destroy();
+    modalRef.current = undefined;
   }, []);
 
-  const handleSubmit = useCallback(async (newConfig: string) => {
-    const isContinueTrial = state.type === CreateExperimentType.ContinueTrial;
-    if (!state.experiment || (isContinueTrial && !state.trial)) return;
+  const getConfigFromForm = useCallback((config: RawJson) => {
+    if (!formRef.current) return yaml.dump(config);
+
+    const formValues = formRef.current.getFieldsValue();
+    const newConfig = clone(config);
+
+    if (formValues.name) {
+      newConfig.name = formValues.name;
+    }
+    if (formValues.maxLength) {
+      const maxLengthType = getMaxLengthType(newConfig);
+      newConfig.searcher.max_length = { [maxLengthType]: parseInt(formValues.maxLength) };
+    }
+
+    return yaml.dump(newConfig);
+  }, []);
+
+  const submitExperiment = useCallback(async (newConfig: string) => {
+    const isContinueTrial = modalState.type === CreateExperimentType.ContinueTrial;
+    if (!modalState.experiment || (isContinueTrial && !modalState.trial)) return;
 
     try {
       const { id: newExperimentId } = await createExperiment({
         experimentConfig: newConfig,
-        parentId: state.experiment.id,
+        parentId: modalState.experiment.id,
       });
-      setState(prev => ({ ...prev, visible: false }));
-      routeToReactUrl(paths.experimentDetails(newExperimentId));
+
+      closeModal();
+
+      // Route to reload path to forcibly remount experiment page.
+      const newPath = paths.experimentDetails(newExperimentId);
+      routeToReactUrl(paths.reload(newPath));
     } catch (e) {
       let errorMessage = 'Unable to continue trial with the provided config.';
       if (e.name === 'YAMLException') {
@@ -100,26 +158,181 @@ const useCreateExperimentModal = (): ModalHooks => {
         const errorJSON = await e.json();
         errorMessage = errorJSON.error?.error;
       }
-      setError(errorMessage);
+
+      setModalState(prev => ({ ...prev, error: errorMessage }));
     }
-  }, [ state ]);
+  }, [ closeModal, modalState ]);
 
-  const createModal = useCallback(() => {
-    if (!state) return null;
+  const handleCancel = useCallback((close) => {
+    if (!modalRef.current) return;
+
+    if (close.triggerCancel) {
+      closeModal();
+    } else {
+      setModalState(prev => {
+        if (prev.isAdvancedMode && formRef.current) {
+          try {
+            const newConfig = (yaml.load(prev.configString) || {}) as RawJson;
+            const isFork = prev.type === CreateExperimentType.Fork;
+
+            formRef.current.setFields([
+              { name: 'name', value: getExperimentName(newConfig) },
+              {
+                name: 'maxLength',
+                value: !isFork ? getMaxLengthValue(newConfig) : undefined,
+              },
+            ]);
+
+            formRef.current.validateFields();
+          } catch (e) {}
+        }
+
+        return {
+          ...prev,
+          configError: !prev.isAdvancedMode ? undefined : prev.configError,
+          isAdvancedMode: !prev.isAdvancedMode,
+        };
+      });
+    }
+  }, [ closeModal ]);
+
+  const handleOk = useCallback(async () => {
+    if (!formRef.current || !modalRef.current) return Promise.reject();
+    if (!!modalState.error || !!modalState.configError) return Promise.reject();
+
+    try {
+      let configString;
+      if (!modalState.isAdvancedMode) {
+        await formRef.current.validateFields();
+        configString = getConfigFromForm(modalState.config);
+      } else {
+        configString = modalState.configString;
+      }
+      await submitExperiment(configString);
+    } catch (e) {}
+
+    return Promise.reject();
+  }, [ getConfigFromForm, modalState, submitExperiment ]);
+
+  const handleEditorChange = useCallback((newConfigString: string) => {
+    // Update config string upon each keystroke change.
+    setModalState(prev => {
+      let configError = undefined;
+
+      // Validate the yaml syntax by attempting to load it.
+      try {
+        yaml.load(newConfigString);
+      } catch (e) {
+        configError = e.message;
+      }
+
+      return { ...prev, configError, configString: newConfigString };
+    });
+  }, []);
+
+  const generateModalContent = useCallback((state: ModalState): React.ReactNode => {
+    const { config, configError, configString, error, type } = state;
+    const isFork = type === CreateExperimentType.Fork;
     return (
-      <CreateExperimentModal
-        config={state.config}
-        error={error}
-        title={state.title}
-        type={state.type}
-        visible={state.visible}
-        onCancel={handleCancel}
-        onOk={handleSubmit}
-      />
+      <>
+        {error && <Alert className={css.error} message={error} type="error" />}
+        {configError && state.isAdvancedMode && (
+          <Alert className={css.error} message={configError} type="error" />
+        )}
+        {state.isAdvancedMode && (
+          <React.Suspense
+            fallback={<div className={css.loading}><Spinner tip="Loading text editor..." /></div>}>
+            <MonacoEditor
+              height="40vh"
+              language="yaml"
+              options={{
+                minimap: { enabled: false },
+                scrollBeyondLastLine: false,
+                selectOnLineNumbers: true,
+              }}
+              theme="vs-light"
+              value={configString}
+              onChange={handleEditorChange}
+            />
+          </React.Suspense>
+        )}
+        <Form
+          className={css.form}
+          hidden={state.isAdvancedMode}
+          initialValues={{
+            maxLength: !isFork ? getMaxLengthValue(config) : undefined,
+            name: getExperimentName(config),
+          }}
+          labelCol={{ span: 8 }}
+          name="basic"
+          ref={formRef}>
+          <Form.Item
+            label="Experiment name"
+            name="name"
+            rules={[ { message: 'Please provide a new experiment name.', required: true } ]}>
+            <Input />
+          </Form.Item>
+          {!isFork && (
+            <Form.Item
+              label={`Max ${getMaxLengthType(config)}`}
+              name="maxLength"
+              rules={[ { message: 'Please provide a max length.', required: true } ]}>
+              <Input type="number" />
+            </Form.Item>
+          )}
+        </Form>
+      </>
     );
-  }, [ error, handleCancel, handleSubmit, state ]);
+  }, [ handleEditorChange ]);
 
-  return { createModal, showModal };
+  const generateModalProps = useCallback((state: ModalState): Partial<ModalFuncProps> => {
+    const { experiment, trial, type } = state;
+    const isFork = type === CreateExperimentType.Fork;
+    if (!experiment || (!isFork && !trial)) return {};
+
+    const titleLabel = (!isFork && trial)
+      ? `Continue Trial ${trial.id}` : `Fork Experiment ${experiment.id}`;
+    const modalProps = {
+      bodyStyle: { padding: 0 },
+      cancelText: state.isAdvancedMode ? 'Show Simple Config' : 'Show Full Config',
+      className: css.base,
+      closable: true,
+      content: generateModalContent(state),
+      icon: null,
+      okText: type,
+      onCancel: handleCancel,
+      onOk: handleOk,
+      style: { minWidth: 600 },
+      title: (
+        <div className={css.title}>
+          <Icon name="fork" /> {titleLabel}
+        </div>
+      ),
+    };
+
+    return modalProps;
+  }, [ generateModalContent, handleCancel, handleOk ]);
+
+  // Detect modal state change and update.
+  useEffect(() => {
+    if (!modalState.visible) return;
+
+    const modalProps = generateModalProps(modalState);
+    if (modalRef.current) {
+      modalRef.current.update(prev => ({ ...prev, ...modalProps }));
+    } else {
+      modalRef.current = Modal.confirm(modalProps);
+    }
+  }, [ generateModalProps, modalState ]);
+
+  // Update the config from form when switching to advanced mode.
+  useEffect(() => {
+    if (modalState.isAdvancedMode !== prevIsAdvancedMode && modalState.isAdvancedMode) {
+      setModalState(prev => ({ ...prev, configString: getConfigFromForm(prev.config) }));
+    }
+  }, [ getConfigFromForm, modalState.isAdvancedMode, prevIsAdvancedMode ]);
+
+  return { showModal };
 };
 
 export default useCreateExperimentModal;
