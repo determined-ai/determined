02c2e834f6981310e5b3c6eb36ec361151c709dc
Caleb Kang <caleb@determined.ai>
2021-08-16 15:52:25 -0600

    chore: update storybook for table batch

--- webui/react/src/components/TableBatch.stories.tsx
+++ webui/react/src/components/TableBatch.stories.tsx
@@ -1,5 +1,4 @@
 import { number, withKnobs } from '@storybook/addon-knobs';
-import { Button } from 'antd';
 import React from 'react';
 
 import TableBatch from './TableBatch';
@@ -11,18 +10,22 @@ export default {
   title: 'TableBatch',
 };
 
+const batchOptions = [
+  { label: 'Batch Operation 1', value: 'Action1' },
+  { label: 'Batch Operation 2', value: 'Action2' },
+  { label: 'Batch Operation 3', value: 'Action3' },
+];
+
 export const Default = (): React.ReactNode => (
-  <TableBatch selectedRowCount={1}>
-    <Button danger type="primary">Batch Operation 1</Button>
-    <Button>Batch Operation 2</Button>
-    <Button>Batch Operation 3</Button>
-  </TableBatch>
+  <TableBatch
+    actions={batchOptions}
+    selectedRowCount={1}
+  />
 );
 
 export const Custom = (): React.ReactNode => (
-  <TableBatch selectedRowCount={number('selectedRowCount', 1)}>
-    <Button danger type="primary">Batch Operation 1</Button>
-    <Button>Batch Operation 2</Button>
-    <Button>Batch Operation 3</Button>
-  </TableBatch>
+  <TableBatch
+    actions={batchOptions}
+    selectedRowCount={number('selectedRowCount', 1)}
+  />
 );
