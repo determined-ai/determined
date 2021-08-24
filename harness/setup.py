88e26e66f3da10cb2867bba9e3d3883e51af6c8a
Danny Zhu <dzhu@hpe.com>
2021-08-24 11:42:42 -0600

    chore: bump version: 0.16.4rc0 -> 0.16.4

--- harness/setup.py
+++ harness/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="determined",
-    version="0.16.4rc0",
+    version="0.16.4",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
