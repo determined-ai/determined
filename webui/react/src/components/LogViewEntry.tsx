6b457f9ff1b4742783c21db56af7474cec8e6e36
Caleb Kang <caleb@determined.ai>
2021-08-24 09:57:20 -0600

    chore: migrate level component into entry component

--- webui/react/src/components/LogViewEntry.tsx
+++ webui/react/src/components/LogViewEntry.tsx
@@ -1,10 +1,12 @@
+import { Tooltip } from 'antd';
 import React from 'react';
 
 import { LogLevel } from 'types';
 import { ansiToHtml } from 'utils/dom';
+import { capitalize } from 'utils/string';
 
+import Icon from './Icon';
 import css from './LogViewerEntry.module.scss';
-import LogViewerLevel from './LogViewerLevel';
 
 interface Prop {
   formattedTime: string;
@@ -14,14 +16,23 @@ interface Prop {
   timeStyle?: React.CSSProperties;
 }
 
+export const ICON_WIDTH = 26;
+
 const LogViewerEntry: React.FC<Prop> = ({ level, message, style, formattedTime, timeStyle }) => {
+  const logLevel = level ? level : LogLevel.Info;
+  const levelClasses = [ css.level, logLevel ];
   const messageClasses = [ css.message ];
 
-  if (level) messageClasses.push(css[level]);
+  if (logLevel) messageClasses.push(css[logLevel]);
 
   return (
     <div className={css.base} style={style}>
-      <LogViewerLevel logLevel={level} />
+      <Tooltip placement="top" title={`Level: ${capitalize(logLevel)}`}>
+        <div className={levelClasses.join(' ')} style={{ width: ICON_WIDTH }}>
+          <div className={css.levelLabel}>&lt;[{logLevel}]&gt;</div>
+          <Icon name={logLevel} size="small" />
+        </div>
+      </Tooltip>
       <div className={css.time} style={timeStyle}>{formattedTime}</div>
       <div
         className={messageClasses.join(' ')}
