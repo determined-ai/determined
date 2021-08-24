96e398104e98dc758d68e6809f44ec1538646733
Rehan S. Durrani <rehan@determined.ai>
2021-08-23 17:13:15 -0700

    finish initial implementation of det deploy gke with schedulers, continue dogfooding

--- harness/determined/deploy/gke/cli.py
+++ harness/determined/deploy/gke/cli.py
@@ -1,14 +1,26 @@
 import argparse
 import json
+from multiprocessing.sharedctypes import Value
 from pathlib import Path
 import subprocess
-from typing import Callable
+from typing import Callable, Dict, Union
 import determined
+import sys
+from termcolor import cprint
 
-from determined.common.declarative_argparse import Arg, ArgGroup, Cmd
+from determined.common.declarative_argparse import Arg, ArgGroup, Cmd, Group
 from determined.deploy.gke.constants import defaults
 from determined.common.util import safe_load_yaml_with_exceptions
 from determined.common import yaml
+
+
+def make_spec(task_container_defaults: dict, key: str) -> dict:
+    pod_spec = task_container_defaults.get(key)
+    if not pod_spec:
+        pod_spec = {"apiVersion": "v1", "kind": "Pod"}
+    if not pod_spec.get("spec"):
+        pod_spec["spec"] = {}
+    return pod_spec
 
 
 def validate_accelerator_type() -> Callable:
@@ -47,16 +59,22 @@ def validate_location(isZone: bool = True) -> Callable:
     return validate
 
 
-def validate_machine_type(args: argparse.Namespace, zone: str, key: str = "master") -> None:
-    machine_type = args.master_machine_type if key == "master" else args.agent_machine_type
+def validate_machine_type(machine_type: str, zone: str) -> None:
     try:
         subprocess.check_call(
-            ["gcloud", "compute", "machine-types", "describe", machine_type, "--zone", zone],
+            [
+                "gcloud",
+                "compute",
+                "machine-types",
+                "describe",
+                machine_type,
+                "--zone",
+                zone,
+            ],
             stdout=subprocess.DEVNULL,
         )
     except:
-        raise argparse.ArgumentError(
-            machine_type,
+        raise ValueError(
             "The specified machine type {} was not found in the specified zone {}.".format(
                 machine_type, zone
             ),
@@ -78,8 +96,7 @@ def validate_accelerator_zone(args: argparse.Namespace, zone: str) -> None:
             stdout=subprocess.DEVNULL,
         )
     except:
-        raise argparse.ArgumentError(
-            args.gpu_type,
+        raise ValueError(
             "The specified accelerator type {} was not found in the specified zone {}.".format(
                 args.gpu_type, zone
             ),
@@ -88,17 +105,31 @@ def validate_accelerator_zone(args: argparse.Namespace, zone: str) -> None:
 
 def validate_args(args: argparse.Namespace) -> None:
     if args.master_machine_type != "n1-standard-16":
-        validate_machine_type(args, args.zone)
+        validate_machine_type(args.master_machine_type, args.zone)
     if args.agent_machine_type != "n1-standard-32":
-        validate_machine_type(args, args.zone, key="agent")
+        validate_machine_type(args.agent_machine_type, args.zone)
     validate_accelerator_zone(args, args.zone)
     if args.multi_node_pool:
         if args.cpu_node_pool_name is None:
-            raise argparse.ArgumentError(
-                args.cpu_node_pool_name,
+            raise ValueError(
                 "--cpu-node-pool-name must be specified if using multiple node pools "
                 + "(--multi-node-pool flag is set).",
             )
+    if args.gpu_coscheduler and args.preemption:
+        raise ValueError(
+            "--gpu-coscheduler and --preemptive-scheduler are mutually exclusive and cannot both be"
+            " specified",
+        )
+    if args.cpu_coscheduler and not args.gpu_coscheduler:
+        raise ValueError(
+            "To enable coscheduling on the CPU Node Pool, coscheduling must be enabled on the GPU"
+            " Node Pool. --cpu-coscheduler cannot be specified if --gpu-coscheduler isn't as well."
+        )
+    if args.cpu_coscheduler and not args.multi_node_pool:
+        raise ValueError(
+            "To enable coscheduling on the CPU Node Pool, multiple node pools must be used."
+            + " --cpu-coscheduler cannot be specified if --multi-node-pool isn't as well."
+        )
 
 
 def create_cluster(args: argparse.Namespace) -> None:
@@ -136,10 +167,17 @@ def create_nodepools(region: str, args: argparse.Namespace) -> None:
         "type={},count={}".format(args.gpu_type, args.gpus_per_node),
         "--zone",
         region,
-        "--num-nodes=0",
-        "--enable-autoscaling",
-        "--min-nodes=0",
-        "--max-nodes={}".format(args.max_gpu_nodes),
+    ]
+    if args.gpu_coscheduler or args.preemption:
+        cmd += ["--num-nodes={}".format(args.max_gpu_nodes)]
+    else:
+        cmd += [
+            "--num-nodes=0",
+            "--enable-autoscaling",
+            "--min-nodes=0",
+            "--max-nodes={}".format(args.max_gpu_nodes),
+        ]
+    cmd += [
         "--machine-type={}".format(args.agent_machine_type),
         "--scopes=storage-full,cloud-platform",
         "--node-labels=accelerator_type=gpu",
@@ -165,10 +203,17 @@ def create_nodepools(region: str, args: argparse.Namespace) -> None:
             args.cluster_name,
             "--zone",
             region,
-            "--num-nodes=0",
-            "--enable-autoscaling",
-            "--min-nodes=0",
-            "--max-nodes={}".format(args.max_cpu_nodes),
+        ]
+        if args.cpu_coscheduler:
+            cmd += ["--num-nodes={}".format(args.max_cpu_nodes)]
+        else:
+            cmd += [
+                "--num-nodes=0",
+                "--enable-autoscaling",
+                "--min-nodes=0",
+                "--max-nodes={}".format(args.max_cpu_nodes),
+            ]
+        cmd += [
             "--machine-type={}".format(args.agent_machine_type),
             "--scopes=storage-full,cloud-platform",
             "--node-labels=accelerator_type=cpu",
@@ -186,6 +231,10 @@ def configure_helm(args: argparse.Namespace) -> None:
     elif "dev" in helm_chart["appVersion"]:
         # Preserve user overridden appVersion in helm chart unless it includes dev in the version.
         helm_chart["appVersion"] = determined.__version__
+    if args.gpu_coscheduler:
+        helm_chart["defaultScheduler"] = "coscheduler"
+    elif args.preemption:
+        helm_chart["defaultScheduler"] = "preemption"
     with (helm_dir / "Chart.yaml").open("w") as f:
         yaml.round_trip_dump(helm_chart, f)
     with (helm_dir / "values.yaml").open() as f:
@@ -203,56 +252,98 @@ def configure_helm(args: argparse.Namespace) -> None:
     helm_values["checkpointStorage"] = checkpointStorage
     helm_values["maxSlotsPerPod"] = args.gpus_per_node
     if args.multi_node_pool:
-        helm_values["taskContainerDefaults"] = {
-            "gpuPodSpec": {
-                "apiVersion": "v1",
-                "kind": "Pod",
-                "nodeSelector": {"accelerator_type": "gpu"},
-                "spec": {
-                    "tolerations": [
-                        {
-                            "key": "gpuAvailable",
-                            "operator": "Equal",
-                            "value": "True",
-                            "effect": "NoSchedule",
-                        }
-                    ]
-                },
-            },
-            "cpuPodSpec": {
-                "apiVersion": "v1",
-                "kind": "Pod",
-                "nodeSelector": {"accelerator_type": "cpu"},
-                "spec": {
-                    "tolerations": [
-                        {
-                            "key": "gpuAvailable",
-                            "operator": "Equal",
-                            "value": "False",
-                            "effect": "NoSchedule",
-                        }
-                    ]
-                },
+        gpu_pod_spec = make_spec(helm_values["taskContainerDefaults"], "gpuPodSpec")
+        gpu_pod_spec["spec"]["tolerations"] = [
+            {
+                "key": "gpuAvailable",
+                "operator": "Equal",
+                "value": "True",
+                "effect": "NoSchedule",
             }
-        }
+        ]
+        gpu_pod_spec["spec"]["nodeSelector"] = {"accelerator_type": "gpu"}
+        helm_values["taskContainerDefaults"]["gpuPodSpec"] = gpu_pod_spec
+        cpu_pod_spec = make_spec(helm_values["taskContainerDefaults"], "cpuPodSpec")
+        cpu_pod_spec["spec"]["tolerations"] = [
+            {
+                "key": "gpuAvailable",
+                "operator": "Equal",
+                "value": "False",
+                "effect": "NoSchedule",
+            }
+        ]
+        cpu_pod_spec["spec"]["nodeSelector"] = {"accelerator_type": "cpu"}
+        if args.cpu_coscheduler:
+            cpu_pod_spec["spec"]["schedulerName"] = "coscheduler"
+        helm_values["taskContainerDefaults"]["cpuPodSpec"] = cpu_pod_spec
     with (helm_dir / "values.yaml").open("w") as f:
         yaml.round_trip_dump(helm_values, f)
 
 
 def handle_up(args: argparse.Namespace) -> None:
-    validate_args(args)
+    try:
+        validate_args(args)
+    except Exception as e:
+        exc_str = "Argument Error: {}".format(e)
+        cprint(exc_str, "red")
+        cprint("Failed to create gke cluster", "red")
+        sys.exit(1)
+    if args.gpu_coscheduler or args.preemption:
+        cprint(
+            (
+                "Autoscaling is not supported with the lightweight coscheduling plugin or with the"
+                + " preemptive priority-based scheduler, and so a GPU node pool with {} nodes will"
+                + " be statically allocated. This can be changed by using the default Kubernetes"
+                + "  scheduler, or specifying a different value for --max-gpu-nodes."
+            ).format(args.max_gpu_nodes),
+            "yellow",
+        )
+    if args.cpu_coscheduler:
+        cprint(
+            (
+                "Autoscaling is not supported with the lightweight coscheduling plugin and so a CPU"
+                + " node pool with {} nodes will be statically allocated. This can be changed by"
+                + "  using the default Kubernetes scheduler, or specifying a different value for"
+                + " --max-cpu-nodes."
+            ).format(args.max_cpu_nodes),
+            "yellow",
+        )
     create_cluster(args)
     configure_helm(args)
+    if args.cpu_coscheduler:
+        cprint(
+            (
+                "The Priority-Based Gang Scheduler has been enabled for the CPU Agent Pool. To use"
+                + " this feature, please specify the following in your experiment config for"
+                + " commands or CPU Experiments:"
+            ),
+            "yellow",
+        )
+        exp_config = {
+            "environment": {
+                "podspec": {
+                    "metadata": {
+                        "labels": {
+                            "pod-group.scheduling.sigs.k8s.io/name": "<unique task name>",
+                            "pod-group.scheduling.sigs.k8s.io/min-available": "<# of Nodes required>",
+                        }
+                    }
+                }
+            }
+        }
+        cprint(yaml.dump(exp_config), "blue")
+        cprint("Replacing the text in the angle brackets (<>).", "yellow")
     cmd = ["helm", "install", "determined-gke", args.helm_dir]
     subprocess.check_call(cmd)
 
 
 def handle_down(args: argparse.Namespace) -> None:
-    print(
+    cprint(
         (
             "Setting kubectl config to cluster {}. Please make sure to run\n`kubectl config "
             + "set-cluster <other_cluster_name>`\nto interact with other deployed clusters."
-        ).format(args.cluster_name)
+        ).format(args.cluster_name),
+        "yellow",
     )
     cmd = ["kubectl", "config", "set-cluster", args.cluster_name]
     subprocess.check_call(cmd)
@@ -306,7 +397,8 @@ args_description = Cmd(
                             type=str,
                             default=None,
                             required=True,
-                            help="a unique name for the GCS bucket that will store your checkpoints",
+                            help="a unique name for the GCS bucket that will store your"
+                            " checkpoints",
                         ),
                     ],
                 ),
@@ -335,7 +427,12 @@ args_description = Cmd(
                             required=False,
                             help="directory containing Helm Chart, values.yaml and templates.",
                         ),
-                        Arg("--det-version", type=str, default=None, help=argparse.SUPPRESS),
+                        Arg(
+                            "--det-version",
+                            type=str,
+                            default=None,
+                            help=argparse.SUPPRESS,
+                        ),
                         Arg(
                             "--no-make-bucket",
                             required=False,
@@ -383,7 +480,39 @@ args_description = Cmd(
                         Arg(
                             "--multi-node-pool",
                             required=False,
-                            help="flag that indicates multiple node pools should be used - 1 for CPU only tasks and one for GPU tasks",
+                            help="flag that indicates multiple node pools should be used - one"
+                            " for CPU only tasks and one for GPU tasks",
+                            action="store_true",
+                        ),
+                        Arg(
+                            "--cpu-coscheduler",
+                            required=False,
+                            help="Enables the lightweight coscheduling plugin for Kubernetes that"
+                            " provides priority-based gang scheduling for the CPU Agent Nodepool."
+                            "If this argument is set, cluster autoscaling is disabled, and"
+                            " --max-cpu-nodes nodes are statically allocated for the CPU Agent Node"
+                            " pool at creation time.",
+                            action="store_true",
+                        ),
+                        Arg(
+                            "--gpu-coscheduler",
+                            "--coscheduler",
+                            required=False,
+                            help="Enables the lightweight coscheduling plugin for Kubernetes that"
+                            " provides priority-based gang scheduling for the GPU Agent Nodepool."
+                            "If this argument is set, cluster autoscaling is disabled, and"
+                            " --max-gpu-nodes nodes are statically allocated for the GPU Agent Node"
+                            " pool at creation time.",
+                            action="store_true",
+                        ),
+                        Arg(
+                            "--preemption",
+                            "--preemptive-scheduler",
+                            required=False,
+                            help="Enables the priority-based scheduler with preemption on the GPU"
+                            " Agent Nodepool. If this argument is set, cluster autoscaling is"
+                            " disabled, and --max-gpu-nodes nodes are statically allocated for the "
+                            " GPU Agent Node pool at creation time.",
                             action="store_true",
                         ),
                     ],
