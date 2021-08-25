10972980ab26d191b0bafe0e63b86161164166a7
Emily Bonar <emily.bonar@hpe.com>
2021-08-25 10:26:15 -0500

    Switch to InfoBox

--- webui/react/src/components/InfoBox.tsx
+++ webui/react/src/components/InfoBox.tsx
@@ -3,27 +3,35 @@ import React from 'react';
 import css from './InfoBox.module.scss';
 
 export interface InfoRow {
-  content?: React.ReactNode;
+  content?: React.ReactNode | React.ReactNode[];
   label: string;
 }
 
 interface Props {
+  header?: React.ReactNode;
   rows: InfoRow[];
 }
 
 export const renderRow = ({ label, content }: InfoRow): React.ReactNode => {
-  if (!content) return null;
+  if (content == null) return null;
   return (
     <div className={css.info} key={label}>
       <div className={css.label}>{label}</div>
-      <div className={css.content}>{content}</div>
+      {Array.isArray(content) ?
+        <div className={css.contentList}>
+          {content.map((item, idx) => <div className={css.content} key={idx}>{item}</div>)}
+        </div> :
+        <div className={css.content}>{content}</div>
+      }
+
     </div>
   );
 };
 
-const InfoBox: React.FC<Props> = ({ rows }: Props) => {
+const InfoBox: React.FC<Props> = ({ header, rows }: Props) => {
   return (
     <div className={css.base}>
+      {header != null ? <div className={css.header}>{header}</div>: null}
       {rows.map(renderRow)}
     </div>
   );
