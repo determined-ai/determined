ff01fea674c7308154972ae2ac7cbac83787b54d
Emily Bonar <emily.bonar@hpe.com>
2021-08-26 11:49:11 -0500

    Set up basic table

--- webui/react/src/pages/ModelRegistry.tsx
+++ webui/react/src/pages/ModelRegistry.tsx
@@ -1,10 +1,13 @@
-import React, { useCallback, useState } from 'react';
+import { ColumnsType } from 'antd/lib/table';
+import React, { useCallback, useMemo, useState } from 'react';
 
 import Page from 'components/Page';
+import ResponsiveTable from 'components/ResponsiveTable';
 import Section from 'components/Section';
 import handleError, { ErrorType } from 'ErrorHandler';
 import usePolling from 'hooks/usePolling';
 import { getModels } from 'services/api';
+import { V1GetModelsRequestSortBy } from 'services/api-ts-sdk';
 import { ModelItem } from 'types';
 import { isEqual } from 'utils/data';
 
@@ -25,12 +28,44 @@ const ModelRegistry: React.FC = () => {
 
   usePolling(fetchModels);
 
+  const columns = useMemo(() => {
+    const tableColumns: ColumnsType<ModelItem> = [
+      { dataIndex: 'id', sorter: true, title: 'ID' },
+      {
+        dataIndex: 'name',
+        key: V1GetModelsRequestSortBy.NAME,
+        sorter: true,
+        title: 'Model name',
+      },
+      {
+        dataIndex: 'description',
+        key: V1GetModelsRequestSortBy.DESCRIPTION,
+        sorter: true,
+        title: 'Description',
+      },
+      { dataIndex: 'versions', title: 'Versions' },
+      {
+        dataIndex: 'lastUpdatedTime',
+        key: V1GetModelsRequestSortBy.LASTUPDATEDTIME,
+        sorter: true,
+        title: 'Last updated',
+      },
+      { dataIndex: 'labels', title: 'Labels' },
+    ];
+
+    return tableColumns;
+  }, []);
+
   return (
     <Page docTitle="Model Registry" id="models">
       <Section title="Model Registry">
-        <div />
+        <ResponsiveTable
+          columns={columns}
+          dataSource={models}
+          showSorterTooltip={false} />
       </Section>
-    </Page>);
+    </Page>
+  );
 };
 
 export default ModelRegistry;
