1876c04a3741a55bccf1c01bb624708f20c1faa6
Ilia Glazkov <ilia@hpe.com>
2021-09-02 16:16:10 -0700

    ci: `using_k8s` should use `det master config -o json`. (#2908)

--- e2e_tests/tests/conftest.py
+++ e2e_tests/tests/conftest.py
@@ -141,6 +141,8 @@ def using_k8s(request: SubRequest) -> bool:
         config.make_master_url(),
         "master",
         "config",
+        "-o",
+        "json",
     ]
 
     output = subprocess.check_output(command, universal_newlines=True, stderr=subprocess.PIPE)
