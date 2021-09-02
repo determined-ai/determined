0170c33cccff7aa280fa28ece11dc4ba71c7c605
Bradley Laney <bradley.laney@hpe.com>
2021-09-02 10:38:38 -0400

    chore: remove old harness profiler

--- harness/determined/layers/__init__.py
+++ harness/determined/layers/__init__.py
@@ -1,3 +1,2 @@
-from determined.layers._harness_profiler import HarnessProfiler
 from determined.layers._worker_process import WorkerProcessContext
 from determined.layers._workload_sequencer import WorkloadSequencer, make_compatibility_workloads
