6cba7fd2ca87598e6093685beee033fd61a3efc2
Hamid Zare <hamidzare.vu@gmail.com>
2021-09-08 15:08:11 -0700

    set up api handler signatures

--- master/internal/api_job.go
+++ master/internal/api_job.go
@@ -6,28 +6,30 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	"github.com/determined-ai/determined/master/internal/sproto"
 	"github.com/determined-ai/determined/proto/pkg/apiv1"
 )
 
+var notImplementedError = status.Error(codes.Unimplemented, "API not implemented")
+
+// GetJobs TODO.
 func (a *apiServer) GetJobs(
 	_ context.Context, req *apiv1.GetJobsRequest,
 ) (resp *apiv1.GetJobsResponse, err error) {
-	switch {
-	case sproto.UseAgentRM(a.m.system):
-		err = a.actorRequest(sproto.JobsAddr, req, &resp)
-	case sproto.UseK8sRM(a.m.system):
-		err = a.actorRequest(sproto.PodsAddr, req, &resp)
-	default:
-		err = status.Error(codes.NotFound, "cannot find jobs or pods actor")
-	}
-	if err != nil {
-		return nil, err
-	}
-	a.filter(&resp.Jobs, func(i int) bool {
-		v := resp.Jobs[i]
-		return req.Label == "" || v.Label == req.Label
-	})
-	a.sort(resp.Jobs, req.OrderBy, req.SortBy, apiv1.GetJobsRequest_SORT_BY_ID)
-	return resp, a.paginate(&resp.Pagination, &resp.Jobs, req.Offset, req.Limit)
+	// a.sort(resp.Jobs, req.OrderBy, req.SortBy, apiv1.GetJobsRequest_SORT_BY_QUEUE_POSITION)
+	// resp, a.paginate(&resp.Pagination, &resp.Jobs, req.Pagination.Offset, req.Pagination.Limit)
+	return nil, notImplementedError
+}
+
+// GetJobQueueStats TODO.
+func (a *apiServer) GetJobQueueStats(
+	_ context.Context, req *apiv1.GetJobQueueStatsRequest,
+) (resp *apiv1.GetJobQueueStatsResponse, err error) {
+	return nil, notImplementedError
+}
+
+// UpdateJobQueue TODO.
+func (a *apiServer) UpdateJobQueue(
+	_ context.Context, req *apiv1.UpdateJobQueueRequest,
+) (resp *apiv1.UpdateJobQueueResponse, err error) {
+	return nil, notImplementedError
 }
