b92e4797702337c0aebd5dd746e7bb67f2783844
Hamid Zare <hamidzare.vu@gmail.com>
2021-09-09 12:18:59 -0700

    wip add comments

--- master/internal/resourcemanagers/scheduler.go
+++ master/internal/resourcemanagers/scheduler.go
@@ -9,9 +9,9 @@ import (
 
 // Scheduler schedules tasks on agents.  Its only function Schedule is called
 // to determine which pending requests can be fulfilled and which scheduled tasks
-// can be terminated.  Schedule is expected to ba called every time there is a change
+// can be terminated. Schedule is expected to ba called every time there is a change
 // to the cluster status, for example, new agents being connected, devices being disabled,
-// and etc,.   Schedule should avoid unnecessary shuffling tasks on agents to avoid
+// and etc,. Schedule should avoid unnecessary shuffling tasks on agents to avoid
 // the overhead of restarting a preempted task.
 type Scheduler interface {
 	Schedule(rp *ResourcePool) ([]*sproto.AllocateRequest, []*actor.Ref)
