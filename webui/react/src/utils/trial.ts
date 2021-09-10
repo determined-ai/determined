38c1f078291f0692edca9c86afd65b31750e5d0d
Caleb Kang <caleb@determined.ai>
2021-09-10 16:13:05 -0600

    chore: change metric values to pass in raw numbers and unprocessed

--- webui/react/src/utils/trial.ts
+++ webui/react/src/utils/trial.ts
@@ -16,7 +16,7 @@ export const extractMetricNames = (workloads: WorkloadWrapper[]): MetricName[] =
       return acc;
     }, new Set<string>()) as Set<string>; // this "as" shouldn't be needed.
 
-  const trainingMetrics: MetricName[]= Array.from(trainingNames).map(name => ({
+  const trainingMetrics: MetricName[] = Array.from(trainingNames).map(name => ({
     name,
     type: MetricType.Training,
   }));
@@ -30,7 +30,7 @@ export const extractMetricNames = (workloads: WorkloadWrapper[]): MetricName[] =
       return acc;
     }, new Set<string>()) as Set<string>; // this "as" shouldn't be needed.
 
-  const validationMetrics: MetricName[]= Array.from(validationNames).map(name => ({
+  const validationMetrics: MetricName[] = Array.from(validationNames).map(name => ({
     name,
     type: MetricType.Validation,
   }));
@@ -45,10 +45,9 @@ export const extractMetricValue = (
   wl: WorkloadWrapper,
   metricName: MetricName,
 ): number | undefined => {
-  const source = (metricName.type === MetricType.Training
-    ? wl.training?.metrics : wl.validation?.metrics) || {};
-  if (isNumber(source[metricName.name])) return source[metricName.name];
-  return undefined;
+  const isTrainingMetric = metricName.type === MetricType.Training;
+  const source = (isTrainingMetric ? wl.training?.metrics : wl.validation?.metrics) ?? {};
+  return source[metricName.name];
 };
 
 export const metricNameFromValue = (metricValue: string): MetricName | undefined => {
