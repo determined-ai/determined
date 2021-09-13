c08ec13a8dc3afe3d70625bd4683fd7573aff171
Caleb Kang <caleb@determined.ai>
2021-09-13 15:24:18 -0600

    chore: add unit test

--- webui/react/src/utils/experiment.test.ts
+++ webui/react/src/utils/experiment.test.ts
@@ -1,8 +1,10 @@
 import {
-  ExperimentBase, ExperimentSearcherName, Hyperparameters, UnknownRecord,
+  ExperimentBase, ExperimentSearcherName, Hyperparameters, TrialHyperparameters, UnknownRecord,
 } from 'types';
 
-import { flattenHyperparameters, isSingleTrialExperiment } from './experiment';
+import {
+  flattenHyperparameters, isSingleTrialExperiment, trialHParamsToExperimentHParams,
+} from './experiment';
 
 describe('flattenHyperparameters', () => {
   const tests: UnknownRecord[] = [
@@ -60,6 +62,41 @@ describe('isSingleTrialExperiment', () => {
   it('should detect single trial experiment from config', () => {
     tests.forEach(test => {
       expect(isSingleTrialExperiment(test.input as ExperimentBase)).toStrictEqual(test.output);
+    });
+  });
+});
+
+describe('trialHParamsToExperimentHParams', () => {
+  const tests: UnknownRecord[] = [
+    {
+      input: {
+        'arch.n_filters1': 62,
+        'arch.n_filters2': 35,
+        'dropout1': 0.5532344404035913,
+        'dropout2': 0.762201718697237,
+        'global_batch_size': 64,
+        'learning_rate': 0.016050683206198273,
+      },
+      output: {
+        arch: {
+          n_filters1: { type: 'const', val: 62 },
+          n_filters2: { type: 'const', val: 35 },
+        },
+        dropout1: { type: 'const', val: 0.5532344404035913 },
+        dropout2: { type: 'const', val: 0.762201718697237 },
+        global_batch_size: { type: 'const', val: 64 },
+        learning_rate: { type: 'const', val: 0.016050683206198273 },
+      },
+    },
+    {
+      input: { 'a.b.c': 5 },
+      output: { a: { b: { c: { type: 'const', val: 5 } } } },
+    },
+  ];
+  it('should convert trial hyperparameters to experiment config hyperparameters', () => {
+    tests.forEach(test => {
+      expect(trialHParamsToExperimentHParams(test.input as TrialHyperparameters))
+        .toStrictEqual(test.output);
     });
   });
 });
