1db3b2efa992b70b5e73e8069946361991502ca2
Anda <anda@determined.ai>
2021-09-17 16:21:04 -0700

    pr comment changes

--- harness/determined/common/storage/boto3_credential_manager.py
+++ harness/determined/common/storage/boto3_credential_manager.py
@@ -1,8 +1,8 @@
 import logging
 import os
 import threading
-from datetime import timedelta
-from typing import Any, Callable, Optional
+import time
+from typing import Any
 
 import boto3
 from botocore import session
@@ -12,7 +12,6 @@ from botocore.credentials import (
     Credentials,
     ReadOnlyCredentials,
     SharedCredentialProvider,
-    _local_now,
 )
 from botocore.session import get_session
 
@@ -28,30 +27,39 @@ class RefreshedCredentialProvider(CredentialProvider):  # type: ignore
 
     def __init__(self, credential_provider: SharedCredentialProvider, check_every: int = 2) -> None:
         super().__init__()
-        self.check_every = timedelta(minutes=check_every)
+        self.check_every = check_every
         self.credential_provider = credential_provider
-        self.last_loaded = self._credentials_modified_time()
 
-    def load(self) -> Optional[Credentials]:
-        credentials = self._load_credentials()
-        return credentials and RefreshedSharedCredentials(
-            refresh_using=self._load_credentials,
-            refresh_needed=self._reload_needed,
-            check_every=self.check_every,
-            access_key=credentials.access_key,
-            secret_key=credentials.secret_key,
-            token=credentials.token,
+    def load(self) -> Credentials:
+        return RefreshableSharedCredentials(
+            credentials_provider=self.credential_provider, check_every=self.check_every
         )
 
-    def _reload_needed(self) -> bool:
-        return self._credentials_modified_time() > self.last_loaded
 
-    def _load_credentials(self) -> Credentials:
-        self.last_loaded = self._credentials_modified_time()
-        return self.credential_provider.load()
+class RefreshableSharedCredentials(Credentials):  # type: ignore
+    def __init__(
+        self,
+        check_every: int,
+        credentials_provider: SharedCredentialProvider,
+    ):
+        self._credentials_provider = credentials_provider
+        self._check_every = check_every
+        self._lock = threading.Lock()
+        self._check_time = time.time() + check_every
+        self._load_and_set_credentials()
+
+    def _load_and_set_credentials(self) -> None:
+        credentials = self._credentials_provider.load()
+        self._last_loaded = self._credentials_modified_time()
+        self.access_key = credentials.access_key
+        self.secret_key = credentials.secret_key
+        self.token = credentials.token
+        self._frozen_credentials = ReadOnlyCredentials(
+            credentials.access_key, credentials.secret_key, credentials.token
+        )
 
     def _credentials_file(self) -> Any:
-        path = self.credential_provider._creds_filename
+        path = self._credentials_provider._creds_filename
         path = os.path.expandvars(path)
         path = os.path.expanduser(path)
         return path
@@ -60,44 +68,22 @@ class RefreshedCredentialProvider(CredentialProvider):  # type: ignore
         credentials_file = self._credentials_file()
         return os.stat(credentials_file).st_mtime
 
-
-class RefreshedSharedCredentials(Credentials):  # type: ignore
-    def __init__(
-        self,
-        access_key: str,
-        secret_key: str,
-        token: str,
-        check_every: timedelta,
-        refresh_using: Callable,
-        refresh_needed: Callable,
-    ):
-        self._refresh_using = refresh_using
-        self._refresh_needed = refresh_needed
-        self._access_key = ensure_unicode(access_key)
-        self._secret_key = ensure_unicode(secret_key)
-        self._token = token
-        self._check_every = check_every
-        self._lock = threading.Lock()
-        self._check_time = self._next_check_time()
-        self._frozen_credentials = ReadOnlyCredentials(access_key, secret_key, token)
+    def _refresh_needed(self) -> bool:
+        return self._credentials_modified_time() > self._last_loaded
 
     def _refresh(self) -> None:
-        now = _local_now()
+        now = time.time()
+        # Check before acquiring lock to prevent excessive locking
         if now < self._check_time:
             return
         with self._lock:
+            # Real time check after acquiring lockg
             if now < self._check_time:
                 return
-            self._check_time = self._next_check_time()
+            self._check_time = now + self._check_every
             if self._refresh_needed():
                 logging.info("credential file changes detected, refreshing credentials")
-                credentials = self._refresh_using()
-                self.access_key = credentials.access_key
-                self.secret_key = credentials.secret_key
-                self.token = credentials.token
-
-    def _next_check_time(self) -> Any:
-        return _local_now() + self._check_every
+                self._load_and_set_credentials()
 
     def get_frozen_credentials(self) -> ReadOnlyCredentials:
         self._refresh()
@@ -135,7 +121,7 @@ def register_credential_provider(session: session, provider_name: str) -> None:
     credential_resolver = session.get_component("credential_provider")
     credential_provider = credential_resolver.get_provider(provider_name)
     managed_credential_provider = RefreshedCredentialProvider(
-        credential_provider=credential_provider
+        check_every=2, credential_provider=credential_provider
     )
     credential_resolver.insert_before(
         name=provider_name, credential_provider=managed_credential_provider
