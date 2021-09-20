8129aef706f592bf1c948333bc3dc43e741f9e2d
Bradley Laney <bradley.laney@hpe.com>
2021-09-20 09:05:04 -0400

    eh maybe no cascade

--- master/static/migrations/20210917133742_drop-workload-start-time.up.sql
+++ master/static/migrations/20210917133742_drop-workload-start-time.up.sql
@@ -1,24 +1,30 @@
-ALTER TABLE public.raw_steps
-    DROP COLUMN start_time CASCADE; -- Cascade to the view destroys it.
+DROP VIEW steps;
 
 ALTER TABLE public.raw_steps
-    DROP COLUMN id CASCADE; -- Should be nothing to cascade to at this point but :shrug:
+    DROP COLUMN start_time;
+
+ALTER TABLE public.raw_steps
+    DROP COLUMN id;
 
 ALTER TABLE public.raw_steps
     ADD COLUMN id SERIAL;
 
-CREATE OR REPLACE VIEW steps AS -- But 'OR REPLACE' anyway, just in case :shrug:
+CREATE VIEW steps AS
     SELECT * FROM raw_steps WHERE NOT archived;
+
+DROP VIEW validations;
 
 ALTER TABLE public.raw_validations
     DROP COLUMN start_time CASCADE;
 
-CREATE OR REPLACE VIEW validations AS
+CREATE VIEW validations AS
     SELECT * FROM raw_validations WHERE NOT archived;
 
-ALTER TABLE public.raw_checkpoints
-    DROP COLUMN start_time CASCADE;
+DROP VIEW checkpoints;
 
-CREATE OR REPLACE VIEW checkpoints AS
+ALTER TABLE public.raw_checkpoints
+    DROP COLUMN start_time;
+
+CREATE VIEW checkpoints AS
     SELECT * FROM raw_checkpoints WHERE NOT archived;
 
