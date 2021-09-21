50aab3bbc56d99fc05f97aba29c3381f44397557
Hamid Zare <hamid@determined.ai>
2021-09-21 09:51:46 -0700

    fix sql typos

--- master/static/migrations/20210917164119_job-queue-jid.up.sql
+++ master/static/migrations/20210917164119_job-queue-jid.up.sql
@@ -5,24 +5,24 @@ experiments => jobs
 NTbCS => jobs (if they did persist in DB)
 */
 
-CREATE TYPE public.task_type AS ENUM (
+CREATE TYPE public.job_type AS ENUM (
     'EXPERIMENT',
     'NOTEBOOK',
     'SHELL',
     'COMMAND',
-    'TENSORBOARD',
+    'TENSORBOARD'
 );
 
 CREATE TABLE public.jobs (
     job_id text NOT NULL UNIQUE,
-    task_type task_type NOT NULL,
+    job_type job_type NOT NULL
 );
 
 
 -- TODO existing tasks that had jobs? do we care
 ALTER TABLE public.tasks
-    ADD COLUMN job_id text NOT NULL REFERENCES public.jobs(job_id),
+    ADD COLUMN job_id text NOT NULL REFERENCES public.jobs(job_id);
 
 -- FIXME maybe we allow nulls for old experiments?
 ALTER TABLE public.experiments
-    ADD COLUMN job_id text NOT NULL REFERENCES public.jobs(job_id),
+    ADD COLUMN job_id text NOT NULL REFERENCES public.jobs(job_id);
