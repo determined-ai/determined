75164906fb6e300c6594588995bb08e421ced504
Emily Bonar <emily.bonar@hpe.com>
2021-09-27 09:44:49 -0500

    Outline RegisterModelVersionModal

---
+++ webui/react/src/components/RegisterModelVersionModal.tsx
@@ -0,0 +1,57 @@
+import { Input, Modal, Select } from 'antd';
+import React, { useCallback, useMemo, useState } from 'react';
+
+import EditableTagList from './TagList';
+
+const { Option } = Select;
+
+const RegisterModelVersionModal: React.FC = () => {
+  const [ model, setModel ] = useState<string>();
+  const [ versionName, setVersionName ] = useState('');
+  const [ modelDescription, setModelDescription ] = useState('');
+  const [ tags, setTags ] = useState<string[]>([]);
+
+  const updateModel = useCallback((value) => {
+    setModel(value);
+  }, []);
+
+  const updateVersionName = useCallback((value) => {
+    setVersionName(value);
+  }, []);
+
+  const updateModelDescription = useCallback((value) => {
+    setModelDescription(value);
+  }, []);
+
+  const modelOptions = useMemo(() => {
+    return [ '' ];
+  }, []);
+
+  return (
+    <Modal
+      okButtonProps={{ disabled: versionName === '' }}
+      okText="Add Model Version"
+      title="Register Model">
+      <p>Save this checkpoint to the Model Registry</p>
+      <h2>Select Model</h2>
+      <Select placeholder="Select a model..." onChange={updateModel}>
+        {modelOptions.map(option => (
+          <Option key={option} value={option}>
+            {option === ''?
+              'New Model' :
+              option}
+          </Option>))}
+      </Select>
+      <h2>Version Name</h2>
+      <Input value={versionName} onChange={updateVersionName} />
+      <h2>Description <span>(optional)</span></h2>
+      <Input.TextArea value={modelDescription} onChange={updateModelDescription} />
+      <h2>Metadata <span>(optional)</span></h2>
+      <p>Placeholder</p>
+      <h2>Tags <span>(optional)</span></h2>
+      <EditableTagList tags={tags} onChange={setTags} />
+    </Modal>
+  );
+};
+
+export default RegisterModelVersionModal;
