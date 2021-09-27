9e4b2c7c8ab7321d67a867194b8eaf7b13fcbbd2
Emily Bonar <emily.bonar@hpe.com>
2021-09-27 09:30:52 -0500

    Outline NewModelModal

---
+++ webui/react/src/components/NewModelModal.tsx
@@ -0,0 +1,34 @@
+import { Input, Modal } from 'antd';
+import React, { useCallback, useState } from 'react';
+
+import EditableTagList from './TagList';
+
+const NewModelModal: React.FC = () => {
+  const [ modelName, setModelName ] = useState('');
+  const [ modelDescription, setModelDescription ] = useState('');
+  const [ tags, setTags ] = useState<string[]>([]);
+
+  const updateModelName = useCallback((value) => {
+    setModelName(value);
+  }, []);
+
+  const updateModelDescription = useCallback((value) => {
+    setModelDescription(value);
+  }, []);
+
+  return (
+    <Modal okButtonProps={{ disabled: modelName === '' }} okText="Create Model" title="New Model">
+      <p>Create a registered model to organize important checkpoints.</p>
+      <h2>Model name</h2>
+      <Input value={modelName} onChange={updateModelName} />
+      <h2>Description <span>(optional)</span></h2>
+      <Input.TextArea value={modelDescription} onChange={updateModelDescription} />
+      <h2>Metadata <span>(optional)</span></h2>
+      <p>Placeholder</p>
+      <h2>Tags <span>(optional)</span></h2>
+      <EditableTagList tags={tags} onChange={setTags} />
+    </Modal>
+  );
+};
+
+export default NewModelModal;
