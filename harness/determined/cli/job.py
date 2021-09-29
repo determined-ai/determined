42949c29cde4d9a8ddaf3563a1492ffa2f28c06c
Hamid Zare <hamid@determined.ai>
2021-09-29 15:14:02 -0700

    type comments

--- harness/determined/cli/job.py
+++ harness/determined/cli/job.py
@@ -3,12 +3,18 @@ from typing import Any, List
 from argparse import Namespace
 from determined.common.declarative_argparse import Arg, Cmd
 import determined.common.api.swagger as swg
-import yaml
+# import determined.common.api.swagger_client.models as models
 
 @swg.auth_required
 def list(_: Namespace) -> None:
-    api_response = swg.job_api.determined_get_jobs(resource_pools=["default"]) # TODO type info for (optional) input and output
-    print(yaml.dump(api_response))
+    # type info shows for some language servers but not all since the generated code
+    # is using legacy docstring types
+    api_response = swg.job_api.determined_get_jobs(resource_pools=["default"])
+    # models.V1GetJobsResponse(api_response)
+    # models.V1Job
+    print(api_response.jobs)
+    # print(yaml.dump(api_response.jobs))
+
 
 args_description = [
     Cmd("j|ob", None, "manage job", [
