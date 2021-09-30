a74b9133fb9ead6d270f1b95aeadd6ce8fc6f62f
Hamid Zare <hamid@determined.ai>
2021-09-29 23:49:27 -0700

    improve job list

--- harness/determined/cli/job.py
+++ harness/determined/cli/job.py
@@ -1,7 +1,10 @@
 from __future__ import print_function
-from typing import Any, List
+from typing import Any, List, Dict, Union
 from argparse import Namespace
 from determined.common.declarative_argparse import Arg, Cmd
+from determined.cli import  render
+import yaml
+import json
 import determined.common.api.fapi as fapi
 # from determined.common.api.fastapi_client.models import V1LoginRequest
 
@@ -14,10 +17,22 @@ import determined.common.api.fapi as fapi
 #     api_response = swg.job_api.determined_get_jobs(resource_pools=["default"])
 #     print(api_response.jobs)
 
+
 @fapi.auth_required
-def list(_: Namespace) -> None:
-    api_response = fapi.sync_apis.jobs_api.determined_get_jobs(resource_pools=["default"])
-    print(api_response.jobs)
+def list(args: Namespace) -> None:
+    response = fapi.sync_apis.jobs_api.determined_get_jobs(resource_pools=[args.resource_pool])
+    if response.jobs is None: # TODO mark the proto as required.
+        return
+    jobs_json = fapi.to_json(response.jobs)
+    if args.output == 'yaml':
+        print(yaml.safe_dump(jobs_json, default_flow_style=False))
+    elif args.output == 'json':
+        print(json.dumps(jobs_json, indent=4, default=str))
+    elif ['csv', 'table'].count(args.output) > 0:
+        # render.tabulate_or_csv # TODO maybe add support for csv or tabular format. ref exp list
+        raise NotImplementedError(f"Output not implemented, adopt a cat to unlock: {args.output}")
+    else:
+        raise ValueError(f"Bad output format: {args.output}")
 
 
 args_description = [
@@ -25,6 +40,8 @@ args_description = [
         Cmd("list", list, "list jobs", [
             Arg("-o", "--output", type=str, default="yaml",
                 help="Output format, one of json|yaml"),
+            Arg("-rp", "--resource-pool", type=str, default="default",
+                help=""),
         ]),
     ])
 ]  # type: List[Any]
--- harness/determined/common/api/fapi.py
+++ harness/determined/common/api/fapi.py
@@ -1,10 +1,12 @@
 from determined.common.api.authentication import Authentication, cli_auth
 import functools
-from typing import Any, Callable, Awaitable
+from typing import Any, Callable, Awaitable, List, Dict
 import argparse
 from determined.common.api.fastapi_client import SyncApis, ApiClient, AsyncApis
 from determined.common.api.fastapi_client.api_client import Send
 from httpx import Request, Response
+from pydantic import BaseModel # FIXME this doesn't get resolved in my IDE's language server
+import json
 # from determined.common.api.fastapi_client.api_client import ApiClient, AsyncApis, SyncApis
 # from determined.common.api.fastapi_client.models import Pet
 
@@ -26,23 +28,22 @@ def add_token(token: str):
         return send(req)
     return f
 
+def to_dict(o: BaseModel):
+    rv = o
+    if isinstance(o, List):
+        return [to_dict(i) for i in o]
+    elif hasattr(o, 'dict'):
+        rv = o.dict() # type: Dict[str, Any]
+        if isinstance(o, dict):
+            for k, v in o.items():
+                rv[k] = to_dict(v)
+    return rv
 
-# resp = sync_apis.cluster_api.determined_get_master()
-# print(resp)
-
-
-# fa.models.V1GetMasterResponse.update_forward_refs()
-
-# pet_1 = sync_apis.pet_api.get_pet_by_id(pet_id=1)
-# assert isinstance(pet_1, Pet)
-
-# resp = sync_apis.cluster_api.determined_get_master_config()
-# print(resp)
-
-# async def get_pet_2() -> Pet:
-#     pet_2 = await async_apis.pet_api.get_pet_by_id(pet_id=2)
-#     assert isinstance(pet_2, Pet)
-#     return pet_2
+def to_json(o: BaseModel):
+    if isinstance(o, List):
+        return [to_json(i) for i in o]
+    assert hasattr(o, 'json')
+    return json.loads(o.json())
 
 def auth_required(func: Callable[[argparse.Namespace], Any]) -> Callable[..., Any]:
     """
