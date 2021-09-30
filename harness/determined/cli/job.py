3e844cdac2a81a9582988d74db2a9abc7bcd745d
Hamid Zare <hamid@determined.ai>
2021-09-29 22:33:03 -0700

    separate swagger example

--- harness/determined/cli/job.py
+++ harness/determined/cli/job.py
@@ -2,21 +2,21 @@ from __future__ import print_function
 from typing import Any, List
 from argparse import Namespace
 from determined.common.declarative_argparse import Arg, Cmd
-# import determined.common.api.swagger as swg
 import determined.common.api.fapi as fapi
 # from determined.common.api.fastapi_client.models import V1LoginRequest
 
+# import determined.common.api.swagger as swg
 # import determined.common.api.swagger_client.models as models
-
 # @swg.auth_required
+# def list(_: Namespace) -> None:
+#     # type info shows for some language servers but not all since the generated code
+#     # is using legacy docstring types
+#     api_response = swg.job_api.determined_get_jobs(resource_pools=["default"])
+#     print(api_response.jobs)
+
 @fapi.auth_required
 def list(_: Namespace) -> None:
-    # type info shows for some language servers but not all since the generated code
-    # is using legacy docstring types
-    # api_response = swg.job_api.determined_get_jobs(resource_pools=["default"])
     api_response = fapi.sync_apis.jobs_api.determined_get_jobs(resource_pools=["default"])
-    # models.V1GetJobsResponse(api_response)
-    # models.V1Job
     print(api_response.jobs)
 
 
@@ -24,7 +24,7 @@ args_description = [
     Cmd("j|ob", None, "manage job", [
         Cmd("list", list, "list jobs", [
             Arg("-o", "--output", type=str, default="yaml",
-                help="Output format, one of json|yaml")
+                help="Output format, one of json|yaml"),
         ]),
     ])
 ]  # type: List[Any]
