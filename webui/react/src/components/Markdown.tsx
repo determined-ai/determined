340c4b6c2aba72e258592b61c7611cef2e21ce7a
Emily Bonar <emily.bonar@hpe.com>
2021-10-01 16:46:18 -0500

    Actually hook up the onChange

--- webui/react/src/components/Markdown.tsx
+++ webui/react/src/components/Markdown.tsx
@@ -12,9 +12,10 @@ interface Props {
   editing?: boolean;
   height?: string;
   markdown: string;
+  onChange?: (editedMarkdown: string) => void;
 }
 
-const Markdown: React.FC<Props> = ({ editing=false, height='80vh', markdown }: Props) => {
+const Markdown: React.FC<Props> = ({ editing=false, height='80vh', markdown, onChange }: Props) => {
   return (
     editing ?<Tabs>
       <TabPane key="1" tab="Edit">
@@ -28,7 +29,8 @@ const Markdown: React.FC<Props> = ({ editing=false, height='80vh', markdown }: P
               wordWrap: 'on',
               wrappingIndent: 'indent',
             }}
-            width="100%" />
+            width="100%"
+            onChange={onChange} />
         </React.Suspense>
       </TabPane>
       <TabPane key="2" tab="Preview">
