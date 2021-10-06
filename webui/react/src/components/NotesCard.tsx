89d41621d803e631661477a598281646b8c8c2d4
Emily Bonar <emily.bonar@hpe.com>
2021-10-06 09:38:37 -0500

    Styling changes

--- webui/react/src/components/NotesCard.tsx
+++ webui/react/src/components/NotesCard.tsx
@@ -30,6 +30,7 @@ const NotesCard: React.FC<Props> = ({ notes, onSave, style }: Props) => {
 
   return (
     <Card
+      bodyStyle={{ paddingTop: 'var(--theme-sizes-layout-large)' }}
       extra={(editingNotes ?
         <div style={{ display: 'flex', gap: 4 }}>
           <Button onClick={cancelEdit}>Cancel</Button>
