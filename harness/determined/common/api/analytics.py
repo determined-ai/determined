3a9688cd41ffed0e0feeb2c89639281ea4db0f4b
naren-determined <naren.krishna@hpe.com>
2021-10-07 09:02:58 -0700

    silent fail correct

--- harness/determined/common/api/analytics.py
+++ harness/determined/common/api/analytics.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from typing import Any
 
@@ -7,9 +8,8 @@ import analytics
 def send_analytics(tracking_key: str) -> None:
     if os.environ.get("DET_SEGMENT_ENABLED"):
         analytics.write_key = os.environ.get("DET_SEGMENT_API_KEY")
-        analytics.on_error = on_error
         analytics.track(os.environ.get("DET_CLUSTER_ID"), tracking_key)
 
 
 def on_error(error: Any, items: Any) -> None:
-    pass
+    logging.warning(f"Analytics tracking received error: {error}")
