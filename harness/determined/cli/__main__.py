da494dda2c7f82b6de1fed76e2f7582e6ccb221b
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2021-10-06 17:41:33 -0700

    reset changes to cli/__main

--- harness/determined/cli/__main__.py
+++ harness/determined/cli/__main__.py
@@ -1,8 +1,4 @@
-import sys
-from determined.cli.cli import main, all_args_description, make_parser
+from determined.cli.cli import main
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == 'deploy':
-        from determined.deploy.cli import args_description as deploy_args_description
-        all_args_description.append(deploy_args_description)
-    main(make_parser(all_args_description))
+    main()
