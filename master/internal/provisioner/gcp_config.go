6b4ff18793b21c0a45fa680a34a19f18482bd44c
naren-determined <naren.krishna@hpe.com>
2021-10-07 15:37:02 -0700

    chore: environment bump analytics-python (#3057)
    * environment bump analytics-python

--- master/internal/provisioner/gcp_config.go
+++ master/internal/provisioner/gcp_config.go
@@ -53,7 +53,7 @@ type GCPClusterConfig struct {
 func DefaultGCPClusterConfig() *GCPClusterConfig {
 	return &GCPClusterConfig{
 		BootDiskSize:        200,
-		BootDiskSourceImage: "projects/determined-ai/global/images/det-environments-e8af732",
+		BootDiskSourceImage: "projects/determined-ai/global/images/det-environments-f286b87",
 		LabelKey:            "managed-by",
 		InstanceType: gceInstanceType{
 			MachineType: "n1-standard-32",
