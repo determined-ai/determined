3e8c94c23f77533e17d4ce5a6b6e8884361f2603
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2021-10-13 13:17:51 -0700

    feat: jq - experiment job summary & job stats api (#3063)
    * remove doc symlink
    
    * job summary
    
    * just return nil if job is not found
    
    * add terminal state check
    
    * wip carrying jobid
    
    * wip getting jobid
    
    * make job_id part of base experiment
    
    * move job_id out of JobSummary at api level
    
    * attach job_id to list endpoints for commands
    
    * fmt master and update generated api code
    
    * mark jobid as required in api annotations
    
    * remove unused sql
    
    * add job queue stats implemention
    
    * update generated code
    
    * tests add jobId to mock experiment model
    
    * remove orderedset remnants

--- master/test/testutils/mocks.go
+++ master/test/testutils/mocks.go
@@ -56,6 +56,7 @@ func ExperimentModel(opts ...ExperimentModelOption) *model.Experiment {
 
 	e := &model.Experiment{
 		State:                model.ActiveState,
+		JobID:                model.NewJobID(),
 		Config:               config,
 		StartTime:            time.Now(),
 		OwnerID:              &defaultDeterminedUID,
