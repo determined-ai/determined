4aba5422002afc061d45dfae0599679593cd5c56
Ilia Glazkov <ilia@hpe.com>
2021-10-14 14:35:24 -0700

    circleci 2

--- model_hub/model_hub/huggingface/_trial.py
+++ model_hub/model_hub/huggingface/_trial.py
@@ -1,6 +1,6 @@
 import dataclasses
 import logging
-from typing import Any, Dict, List, Optional, Tuple, Union, cast
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import attrdict
 import datasets as hf_datasets
@@ -172,7 +172,6 @@ def default_load_dataset(
             data_config.dataset_name, data_config.dataset_config_name
         )
         assert hasattr(datasets, "keys"), "Expected a dictionary of datasets."
-        datasets = cast(Union[hf_datasets.DatasetDict, hf_datasets.IterableDatasetDict], datasets)
 
         if "validation" not in datasets.keys():
             assert (
