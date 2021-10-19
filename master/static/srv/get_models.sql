34fdb3a24dc310409f60e2b03479eadd51c5ff67
Nick Doiron <ndoiron@mapmeld.com>
2021-10-19 15:59:49 -0400

    left join users

--- master/static/srv/get_models.sql
+++ master/static/srv/get_models.sql
@@ -1,5 +1,6 @@
-SELECT m.id, m.name, m.description, m.metadata, m.creation_time, m.last_updated_time, m.labels, m.readme, u.username, m.archived, COUNT(mv.version) as num_versions FROM models as m LEFT JOIN model_versions as mv ON mv.model_id = m.id
-JOIN users as u ON u.id = m.user_id
+SELECT m.id, m.name, m.description, m.metadata, m.creation_time, m.last_updated_time, m.labels, m.readme, u.username, m.archived, COUNT(mv.version) as num_versions FROM models as m
+LEFT JOIN model_versions as mv ON mv.model_id = m.id
+LEFT JOIN users as u ON u.id = m.user_id
 WHERE ($1 = '' OR m.archived = $1::BOOL)
 AND ($2 = '' OR (u.username IN (SELECT unnest(string_to_array($2, ',')))))
 AND ($3 = '' OR (m.labels <@ string_to_array($3, ',')))
