5204d7d8f2b966ef05262f616c2fe7bc7927191c
Emily Bonar <emily.bonar@hpe.com>
2021-10-25 11:32:25 -0500

    Fix lint

--- webui/react/src/services/api.ts
+++ webui/react/src/services/api.ts
@@ -3,8 +3,8 @@ import * as Config from 'services/apiConfig';
 import {
   CommandIdParams, CreateExperimentParams, EmptyParams, ExperimentDetailsParams, ExperimentIdParams,
   GetCommandsParams, GetExperimentParams, GetExperimentsParams, GetJupyterLabsParams,
-  GetResourceAllocationAggregatedParams, GetShellsParams, GetTemplatesParams, GetTensorBoardsParams,
-  GetTrialsParams, LaunchJupyterLabParams, LaunchTensorBoardParams, LoginResponse, LogsParams,
+  GetResourceAllocationAggregatedParams, GetShellsParams, GetTemplatesParams, GetTensorboardsParams,
+  GetTrialsParams, LaunchJupyterLabParams, LaunchTensorboardParams, LoginResponse,
   PatchExperimentParams, SingleEntityParams, TaskLogsParams, TrialDetailsParams,
 } from 'services/types';
 import { generateApi, generateDetApi } from 'services/utils';
