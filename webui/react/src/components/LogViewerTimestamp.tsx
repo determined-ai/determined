ec43527e3cb604da8a13773ae859010906666b01
Emily Bonar <emily.bonar@hpe.com>
2021-10-27 15:39:49 -0500

    Remove import

--- webui/react/src/components/LogViewerTimestamp.tsx
+++ webui/react/src/components/LogViewerTimestamp.tsx
@@ -2,7 +2,7 @@ import { Button, notification, Space, Tooltip } from 'antd';
 import dayjs, { Dayjs } from 'dayjs';
 import queryString from 'query-string';
 import React, {
-  Reducer, useCallback, useEffect, useLayoutEffect, useMemo, useReducer, useRef, useState,
+  Reducer, useCallback, useEffect, useLayoutEffect, useReducer, useRef, useState,
 } from 'react';
 import { useLocation } from 'react-router-dom';
 import {
