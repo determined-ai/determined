5dff6c1a65eda7ea99c9577bbd4a53ebd91ba4a0
Caleb Kang <caleb@determined.ai>
2021-11-04 01:05:51 -0600

    chore: apply core log viewer to master and trial logs

--- webui/react/src/pages/MasterLogs.tsx
+++ webui/react/src/pages/MasterLogs.tsx
@@ -1,26 +1,45 @@
 import React, { useCallback } from 'react';
 
-import LogViewerCore, { FetchOptions, OffsetType } from 'components/LogViewerCore';
+import LogViewerCore, { FetchConfig, FetchType } from 'components/LogViewerCore';
+import Page from 'components/Page';
 import { detApi } from 'services/apiConfig';
 import { jsonToMasterLog } from 'services/decoder';
 
+import css from './MasterLogs.module.scss';
+
 const MasterLogs: React.FC = () => {
-  const handleFetch = useCallback((options: FetchOptions) => {
+  const handleFetch = useCallback((config: FetchConfig, type: FetchType) => {
+    const options = { follow: false, limit: config.limit, offset: 0 };
+
+    if (type === FetchType.Initial) {
+      if (config.isNewestFirst) options.offset = -config.limit;
+    } else if (type === FetchType.Newer) {
+      options.offset = config.offsetLog?.id ?? 0;
+    } else if (type === FetchType.Older) {
+      options.offset = Math.max(0, (config.offsetLog?.id ?? 0) - config.limit);
+    } else if (type === FetchType.Stream) {
+      options.offset = -1;
+      options.follow = true;
+      options.limit = 0;
+    }
+
     return detApi.StreamingCluster.determinedMasterLogs(
-      options.offset ?? 0,
+      options.offset,
       options.limit,
       options.follow,
-      { signal: options.canceler.signal },
+      { signal: config.canceler.signal },
     );
   }, []);
 
   return (
-    <LogViewerCore
-      decoder={jsonToMasterLog}
-      title="Master Logs"
-      type={OffsetType.Id}
-      onFetch={handleFetch}
-    />
+    <Page bodyNoPadding id="master-logs">
+      <LogViewerCore
+        decoder={jsonToMasterLog}
+        sortKey="id"
+        title={<div className={css.title}>Master Logs</div>}
+        onFetch={handleFetch}
+      />
+    </Page>
   );
 };
 
