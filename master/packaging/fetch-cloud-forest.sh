6d2f40ebd48a567d7dc3a52f00ca1cf240412efb
Bradley Laney <bradlaney@determined.ai>
2021-11-05 18:25:18 -0400

    fix: update fetch-cloud-forest.sh to be compatible with old versions of bash (#3115)

--- master/packaging/fetch-cloud-forest.sh
+++ master/packaging/fetch-cloud-forest.sh
@@ -4,7 +4,10 @@
 go install github.com/ryanbressler/CloudForest/growforest
 
 # Load the whole go env.
-source <(go env)
+# Not just process substitution because some versions of MacOS ship with older versions of bash, for
+# which `source` only works with regular files.
+# See https://lists.gnu.org/archive/html/bug-bash/2006-01/msg00018.html for detail.
+source /dev/stdin <<<"$(go env)"
 
 # Find the appropriate binary for packaging and output the path.
 if [ "$GOOS" == "$GOHOSTOS" ] && [ "$GOARCH" == "$GOHOSTARCH" ]; then
