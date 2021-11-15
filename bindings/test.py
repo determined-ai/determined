0709f63fc28d26f3f3b7f59cddc91bc1ab592b41
Hamid Zare <hamidzare.vu@gmail.com>
2021-11-15 13:44:55 -0500

    make it no gen

--- bindings/test.py
+++
@@ -1,8 +0,0 @@
-import determined as det
-from determined.experimental import client
-import b
-
-if __name__ == "__main__":
-    client.login()
-    session = client._determined._session
-    print(b.get_Determined_GetMasterConfig(session).to_json())
