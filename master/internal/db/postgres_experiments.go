determined-ai/determined

commit 048f67a4dbb9800c380ee2a836aae520cc6c7ab3

feat: jq api report experiment username and progress (#3185)

master/internal/db/postgres_experiments.go
@@ -1156,9 +1156,11 @@ func (db *PgDB) ExperimentByID(id int) (*model.Experiment, error) {
 	var experiment model.Experiment
 
 	if err := db.query(`
-SELECT id, state, config, model_definition, start_time, end_time, archived,
-	   git_remote, git_commit, git_committer, git_commit_date, owner_id, job_id
-FROM experiments
+SELECT e.id, state, config, model_definition, start_time, end_time, archived,
+	   git_remote, git_commit, git_committer, git_commit_date, owner_id, notes,
+		 job_id, u.username as username
+FROM experiments e
+JOIN users u ON (e.owner_id = u.id)
 WHERE id = $1`, &experiment, id); err != nil {
 		return nil, err
 	}
@@ -1192,9 +1194,11 @@ func (db *PgDB) ExperimentWithoutConfigByID(id int) (*model.Experiment, error) {
 	var experiment model.Experiment
 
 	if err := db.query(`
-SELECT id, state, model_definition, start_time, end_time, archived,
-       git_remote, git_commit, git_committer, git_commit_date, owner_id, job_id
-FROM experiments
+SELECT e.id, state, model_definition, start_time, end_time, archived,
+       git_remote, git_commit, git_committer, git_commit_date, owner_id, notes,
+			 job_id, u.username as username
+FROM experiments e
+JOIN users u ON e.owner_id = u.id
 WHERE id = $1`, &experiment, id); err != nil {
 		return nil, err
 	}
@@ -1227,9 +1231,11 @@ FROM experiments e, trials t  WHERE t.id = $1 AND e.id = t.experiment_id`,
 // NonTerminalExperiments finds all experiments in the database whose states are not terminal.
 func (db *PgDB) NonTerminalExperiments() ([]*model.Experiment, error) {
 	rows, err := db.sql.Queryx(`
-SELECT id, state, config, model_definition, start_time, end_time, archived,
-       git_remote, git_commit, git_committer, git_commit_date, owner_id, job_id
-FROM experiments
+SELECT e.id, state, config, model_definition, start_time, end_time, archived,
+       git_remote, git_commit, git_committer, git_commit_date, owner_id, job_id,
+			 u.username as username
+FROM experiments e
+JOIN users u ON e.owner_id = u.id
 WHERE state IN ('ACTIVE', 'PAUSED', 'STOPPING_CANCELED', 'STOPPING_COMPLETED', 'STOPPING_ERROR')`)
 	if err == sql.ErrNoRows {
 		return nil, errors.WithStack(ErrNotFound)

master/internal/core_experiment.go
@@ -492,6 +492,7 @@ func (m *Master) postExperiment(c echo.Context) (interface{}, error) {
 	}
 
 	dbExp.OwnerID = &user.ID
+	dbExp.Username = user.Username
 	e, err := newExperiment(m, dbExp, taskSpec)
 	if err != nil {
 		return nil, errors.Wrap(err, "starting experiment")

master/internal/experiment.go
@@ -614,7 +614,8 @@ func (e *experiment) toV1Job() *jobv1.Job {
 		ResourcePool:   e.Config.Resources().ResourcePool(),
 		SubmissionTime: timestamppb.New(e.StartTime),
 		Weight:         e.Config.Resources().Weight(),
-		Username:       fmt.Sprintf("%d-userid", e.OwnerID),
+		Username:       e.Username,
+		Progress:       float32(e.searcher.Progress()),
 		Name:           e.Config.Name().String(),
 	}
 

master/pkg/model/experiment.go
@@ -283,6 +283,7 @@ type Experiment struct {
 	GitCommitter         *string    `db:"git_committer"`
 	GitCommitDate        *time.Time `db:"git_commit_date"`
 	OwnerID              *UserID    `db:"owner_id"`
+	Username             string     `db:"username"`
 }
 
 // ExperimentDescriptor is a minimal description of an experiment.

proto/src/determined/experiment/v1/experiment.proto
@@ -50,6 +50,7 @@ message Experiment {
         "num_trials",
         "job_id",
         "username",
+        "job_id",
         "searcher_type"
       ]
     }

proto/src/determined/job/v1/job.proto
@@ -91,6 +91,8 @@ message Job {
   int32 allocated_slots = 12;
   // Job name.
   string name = 13;
+  // Job's progress from 0 to 1.
+  float progress = 14;
 }
 
 // Describes a message to control jobs in a queue.

webui/react/src/services/api-ts-sdk/api.ts
@@ -2669,6 +2669,12 @@ export interface V1Job {
      * @memberof V1Job
      */
     name: string;
+    /**
+     * Job's progress from 0 to 1.
+     * @type {number}
+     * @memberof V1Job
+     */
+    progress?: number;
 }
 
 /**

commit cb050fd51c5b859175d28260b3b580b09dbfa2b7

fix: fix an issue with an ambiguous sql col name: id
was introduced in 048f67a4dbb9800c380ee2a836aae520cc6c7ab3

master/internal/db/postgres_experiments.go
@@ -1161,7 +1161,7 @@ SELECT e.id, state, config, model_definition, start_time, end_time, archived,
 		 job_id, u.username as username
 FROM experiments e
 JOIN users u ON (e.owner_id = u.id)
-WHERE id = $1`, &experiment, id); err != nil {
+WHERE e.id = $1`, &experiment, id); err != nil {
 		return nil, err
 	}
 
@@ -1199,7 +1199,7 @@ SELECT e.id, state, model_definition, start_time, end_time, archived,
 			 job_id, u.username as username
 FROM experiments e
 JOIN users u ON e.owner_id = u.id
-WHERE id = $1`, &experiment, id); err != nil {
+WHERE e.id = $1`, &experiment, id); err != nil {
 		return nil, err
 	}
 