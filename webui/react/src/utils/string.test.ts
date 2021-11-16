6ef6765576405fc34146b879772dc51526c54f74
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2021-11-16 16:21:16 -0500

    chore: update string tests organization (#3192)

--- webui/react/src/utils/string.test.ts
+++ webui/react/src/utils/string.test.ts
@@ -1,33 +1,6 @@
 import * as string from './string';
 
 describe('Routing Utilities', () => {
-  describe('capitalizeWord', () => {
-    it('should make first character uppercase and the rest lower case', () => {
-      const tests = [
-        { input: 'hello world', output: 'Hello world' },
-        { input: 'hello World', output: 'Hello world' },
-        { input: '^@hello', output: '^@hello' },
-      ];
-
-      tests.forEach(test => {
-        expect(string.capitalizeWord(test.input)).toBe(test.output);
-      });
-    });
-  });
-
-  describe('capitalize', () => {
-    it('should capitalize all words in a string', () => {
-      const tests = [
-        { input: '123 abc Def ghi', output: '123 Abc Def Ghi' },
-        { input: '^@hello', output: '^@hello' },
-      ];
-
-      tests.forEach(test => {
-        expect(string.capitalize(test.input)).toBe(test.output);
-      });
-    });
-  });
-
   describe('toHtmlId', () => {
     it('should replace spaces with -', () => {
       expect(string.toHtmlId('Hello World')).toBe('hello-world');
@@ -39,6 +12,33 @@ describe('Routing Utilities', () => {
 
     it('should generate lowercase ids', () => {
       expect(string.toHtmlId('HellO')).toBe('hello');
+    });
+  });
+});
+
+describe('capitalizeWord', () => {
+  it('should make first character uppercase and the rest lower case', () => {
+    const tests = [
+      { input: 'hello world', output: 'Hello world' },
+      { input: 'hello World', output: 'Hello world' },
+      { input: '^@hello', output: '^@hello' },
+    ];
+
+    tests.forEach(test => {
+      expect(string.capitalizeWord(test.input)).toBe(test.output);
+    });
+  });
+});
+
+describe('capitalize', () => {
+  it('should capitalize all words in a string', () => {
+    const tests = [
+      { input: '123 abc Def ghi', output: '123 Abc Def Ghi' },
+      { input: '^@hello', output: '^@hello' },
+    ];
+
+    tests.forEach(test => {
+      expect(string.capitalize(test.input)).toBe(test.output);
     });
   });
 });
