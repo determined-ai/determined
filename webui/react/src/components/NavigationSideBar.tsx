07e23ede4880fd51aa1306fb6a32c85dd44bdffd
Emily Bonar <emily.bonar@hpe.com>
2021-11-23 11:03:49 -0600

    Reorder where Model Registry appears in navbar

--- webui/react/src/components/NavigationSideBar.tsx
+++ webui/react/src/components/NavigationSideBar.tsx
@@ -54,8 +54,8 @@ const menuConfig = {
   top: [
     { icon: 'dashboard', label: 'Dashboard', path: paths.dashboard() },
     { icon: 'experiment', label: 'Experiments', path: paths.experimentList() },
-    { icon: 'tasks', label: 'Tasks', path: paths.taskList() },
     { icon: 'model', label: 'Model Registry', path: paths.modelList() },
+    { icon: 'tasks', label: 'Tasks', path: paths.taskList() },
     { icon: 'cluster', label: 'Cluster', path: paths.cluster() },
     { icon: 'logs', label: 'Master Logs', path: paths.masterLogs() },
   ],
