cb68c368ce0fd1e92136dcb4ba89a9f5bae79170
Anda <anda@determined.ai>
2021-12-01 11:28:06 -0800

    only prometheus endpoint

--- agent/packaging/entrypoint.sh
+++ agent/packaging/entrypoint.sh
@@ -3,10 +3,4 @@
 chmod +x /usr/local/determined/container_startup_script
 /usr/local/determined/container_startup_script
 
-docker run -d \
-  --gpus=all \
-  --publish=9400:9400 \
-  --name=dgcm-exporter \
-  nvidia/dcgm-exporter:2.0.13-2.1.2-ubuntu18.04
-
 /usr/bin/determined-agent "$@"
