7803c74bd03d3603471b4811a36b6010ff3cbe61
Sean Mackrory <mackrory@determined.ai>
2021-12-10 12:47:25 -0700

    chore: bump version: 0.17.5-dev0 -> 0.17.6-dev0

--- model_hub/setup.py
+++ model_hub/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="model-hub",
-    version="0.17.5-dev0",
+    version="0.17.6-dev0",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
