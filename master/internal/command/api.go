28f36ad730da416fb830d8d7492c7c7453f4196f
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2021-12-17 13:43:23 -0800

    chore: reset unintentional changes

--- master/internal/command/api.go
+++ master/internal/command/api.go
@@ -4,7 +4,6 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/pkg/errors"
 
-	"github.com/determined-ai/determined/master/internal/config"
 	"github.com/determined-ai/determined/master/internal/db"
 	"github.com/determined-ai/determined/master/pkg/actor"
 	"github.com/determined-ai/determined/master/pkg/actor/api"
@@ -19,22 +18,17 @@ func RegisterAPIHandler(
 	system *actor.System,
 	echo *echo.Echo,
 	db *db.PgDB,
-	mConfig *config.Config,
 	middleware ...echo.MiddlewareFunc,
 ) {
-	system.ActorOf(actor.Addr("commands"),
-		&commandManager{db: db, mConfig: mConfig})
+	system.ActorOf(actor.Addr("commands"), &commandManager{db: db})
 	echo.Any("/commands*", api.Route(system, nil), middleware...)
 
-	system.ActorOf(actor.Addr("notebooks"),
-		&notebookManager{db: db, mConfig: mConfig})
+	system.ActorOf(actor.Addr("notebooks"), &notebookManager{db: db})
 	echo.Any("/notebooks*", api.Route(system, nil), middleware...)
 
-	system.ActorOf(actor.Addr("shells"),
-		&shellManager{db: db, mConfig: mConfig})
+	system.ActorOf(actor.Addr("shells"), &shellManager{db: db})
 	echo.Any("/shells*", api.Route(system, nil), middleware...)
 
-	system.ActorOf(actor.Addr("tensorboard"),
-		&tensorboardManager{db: db, mConfig: mConfig})
+	system.ActorOf(actor.Addr("tensorboard"), &tensorboardManager{db: db})
 	echo.Any("/tensorboard*", api.Route(system, nil), middleware...)
 }
--- master/internal/trial.go
+++ master/internal/trial.go
@@ -218,7 +218,7 @@ func (t *trial) maybeAllocateTask(ctx *actor.Context) error {
 			SingleAgent: false,
 		},
 
-		Preemptible:  true, // TODO should this read from config files?
+		Preemptible:  true,
 		DoRendezvous: true,
 	}, t.db, t.rm))
 	return nil
