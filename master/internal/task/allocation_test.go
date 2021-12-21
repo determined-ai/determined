30dcbcb7983013e0f0902b44cb77eff00b077794
Nick Doiron <ndoiron@mapmeld.com>
2021-12-21 14:23:09 -0500

    unit tests

--- master/internal/task/allocation_test.go
+++ master/internal/task/allocation_test.go
@@ -100,6 +100,18 @@ func TestAllocation(t *testing.T) {
 						Devices: []device.Device{},
 					},
 				}
+				db.On("UpdateAllocationState", a.req.AllocationID, model.AllocationStateAssigned,
+					false).Return(nil)
+				db.On("UpdateAllocationState", a.req.AllocationID, model.AllocationStatePulling,
+					false).Return(nil)
+				db.On("UpdateAllocationState", a.req.AllocationID, model.AllocationStateStarting,
+					false).Return(nil)
+				db.On("UpdateAllocationState", a.req.AllocationID,
+					model.AllocationStateRunning, false).Return(nil)
+				db.On("UpdateAllocationState", a.req.AllocationID, model.AllocationStateTerminating,
+					false).Return(nil)
+				db.On("UpdateAllocationState", a.req.AllocationID, model.AllocationStateTerminated,
+					false).Return(nil)
 				require.NoError(t, system.Ask(self, containerStateChanged).Error())
 				containerStateChanged.Container.State = cproto.Pulling
 				require.NoError(t, system.Ask(self, containerStateChanged).Error())
