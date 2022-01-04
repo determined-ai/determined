d85c14545c23374e45fdff6998357230debc35ea
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-01-04 14:58:48 -0800

    test job utils

---
+++ webui/react/src/utils/job.test.ts
@@ -0,0 +1,20 @@
+import { CommandType, JobType } from 'types';
+
+import * as utils from './job';
+
+describe('Job Utilities', () => {
+  describe('jobTypeIconName', () => {
+    it('should support experiment and command types', () => {
+      expect(utils.jobTypeIconName(JobType.EXPERIMENT)).toEqual('experiment');
+      expect(utils.jobTypeIconName(JobType.NOTEBOOK)).toEqual(CommandType.JupyterLab);
+    });
+  });
+  describe('jobTypeToCommandType', () => {
+    it('should convert notebook to jupyterlab', () => {
+      expect(utils.jobTypeToCommandType(JobType.NOTEBOOK)).toEqual(CommandType.JupyterLab);
+    });
+    it('should return undefined for non command types', () => {
+      expect(utils.jobTypeToCommandType(JobType.EXPERIMENT)).toBeUndefined();
+    });
+  });
+});
