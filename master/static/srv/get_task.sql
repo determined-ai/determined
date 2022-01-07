3954350503dc11cea1c31376760a081b48fd65e9
Nick Doiron <ndoiron@mapmeld.com>
2022-01-07 11:43:02 -0800

    edits

--- master/static/srv/get_task.sql
+++ master/static/srv/get_task.sql
@@ -1,5 +1,10 @@
-SELECT tasks.task_id, state, is_ready
+WITH allo AS (
+  SELECT task_id, state, is_ready
+  FROM allocations
+  WHERE task_id = $1
+)
+SELECT tasks.task_id, json_build_array(allo) AS allocations
 FROM tasks
-  LEFT JOIN allocations
-  ON allocations.task_id = tasks.task_id
+LEFT JOIN allo ON tasks.task_id = allo.task_id
 WHERE tasks.task_id = $1
+GROUP BY tasks.task_id, allo.*;
