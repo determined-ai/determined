b0f5458c1c8cba9806a8eb660ca77aa5490f2651
Ilia Glazkov <ilia@hpe.com>
2022-01-11 09:54:10 -0800

    chore: bump env images for security. (#3415)

--- harness/determined/deploy/gcp/constants.py
+++ harness/determined/deploy/gcp/constants.py
@@ -3,7 +3,7 @@ class defaults:
     AUX_AGENT_INSTANCE_TYPE = "n1-standard-4"
     COMPUTE_AGENT_INSTANCE_TYPE = "n1-standard-32"
     DB_PASSWORD = "postgres"
-    ENVIRONMENT_IMAGE = "det-environments-eb6c7d1"
+    ENVIRONMENT_IMAGE = "det-environments-08f9c9b"
     GPU_NUM = 8
     GPU_TYPE = "nvidia-tesla-k80"
     MASTER_INSTANCE_TYPE = "n1-standard-2"
