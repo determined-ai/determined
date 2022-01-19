349b17413825f0ef64dda28f8a44aacf4077e4c5
Nick Doiron <ndoiron@mapmeld.com>
2022-01-19 11:32:04 -0800

    allocations array is empty when null

--- master/static/srv/get_task.sql
+++ master/static/srv/get_task.sql
@@ -1,11 +1,8 @@
-WITH allo AS (
+SELECT tasks.task_id, (SELECT coalesce(jsonb_agg(allo), '[]'::jsonb) FROM (
   SELECT allocation_id, task_id, state, is_ready, start_time, end_time
   FROM allocations
-  WHERE task_id = $1
+  WHERE task_id = tasks.task_id
   ORDER BY end_time DESC NULLS FIRST
-)
-SELECT tasks.task_id, json_build_array(allo) AS allocations
+) allo) AS allocations
 FROM tasks
-LEFT JOIN allo ON tasks.task_id = allo.task_id
-WHERE tasks.task_id = $1
-GROUP BY tasks.task_id, allo.*;
+WHERE tasks.task_id = $1;
