ff3151375c2cb7e64bba0dd69dc7aa718eb55cd2
Nick Doiron <ndoiron@mapmeld.com>
2022-01-19 15:36:09 -0500

    fix: Allow allocations array to be empty without null value (#3465)
    * allocations array is empty when null
    
    * jsonb_agg and ORDER BY

--- master/static/srv/get_task.sql
+++ master/static/srv/get_task.sql
@@ -1,11 +1,7 @@
-WITH allo AS (
+SELECT tasks.task_id, (SELECT coalesce(jsonb_agg(allo ORDER BY end_time DESC NULLS FIRST), '[]'::jsonb) FROM (
   SELECT allocation_id, task_id, state, is_ready, start_time, end_time
   FROM allocations
-  WHERE task_id = $1
-  ORDER BY end_time DESC NULLS FIRST
-)
-SELECT tasks.task_id, json_build_array(allo) AS allocations
+  WHERE allocations.task_id = tasks.task_id
+) allo) AS allocations
 FROM tasks
-LEFT JOIN allo ON tasks.task_id = allo.task_id
-WHERE tasks.task_id = $1
-GROUP BY tasks.task_id, allo.*;
+WHERE tasks.task_id = $1;
