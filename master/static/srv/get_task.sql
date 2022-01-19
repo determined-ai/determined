f6bcec1372817a0fe6cceda422b330548795a092
Nick Doiron <ndoiron@mapmeld.com>
2022-01-19 11:52:13 -0800

    jsonb_agg and ORDER BY

--- master/static/srv/get_task.sql
+++ master/static/srv/get_task.sql
@@ -1,8 +1,7 @@
-SELECT tasks.task_id, (SELECT coalesce(jsonb_agg(allo), '[]'::jsonb) FROM (
+SELECT tasks.task_id, (SELECT coalesce(jsonb_agg(allo ORDER BY end_time DESC NULLS FIRST), '[]'::jsonb) FROM (
   SELECT allocation_id, task_id, state, is_ready, start_time, end_time
   FROM allocations
-  WHERE task_id = tasks.task_id
-  ORDER BY end_time DESC NULLS FIRST
+  WHERE allocations.task_id = tasks.task_id
 ) allo) AS allocations
 FROM tasks
 WHERE tasks.task_id = $1;
