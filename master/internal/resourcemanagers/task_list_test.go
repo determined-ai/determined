1dc572eb2236fa28c4623c7c56908ab430be1880
Ryan <rb@hpe.com>
2022-01-19 13:56:41 -0700

    build: make -C master doesn't call make fmt
    Also fix a file that wasn't formatted as a result of the accidental make
    fmt.

--- master/internal/resourcemanagers/task_list_test.go
+++ master/internal/resourcemanagers/task_list_test.go
@@ -4,8 +4,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/determined-ai/determined/master/pkg/actor"
 	"gotest.tools/assert"
+
+	"github.com/determined-ai/determined/master/pkg/actor"
 )
 
 func TestAllocateRequestComparator(t *testing.T) {
