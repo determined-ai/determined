709c25363bbe514f57e425c542ee74ddca915e4b
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-01-21 12:08:56 -0800

    Revert "Calculate last column"
    This reverts commit b25bc38cbf21084f713b410f484a56bc83d757e1.

--- webui/tests/tests/StepImplementation.ts
+++ webui/tests/tests/StepImplementation.ts
@@ -410,15 +410,13 @@ export default class StepImplementation {
 
   @Step('<action> experiment row <row>')
   public async modifyExperiment(action: string, row: string) {
-    const lastCol = (await t.$('th').elements()).length;
-    await t.click(t.tableCell({ row: parseInt(row) + 1, col: lastCol }));
+    await t.click(t.tableCell({ row: parseInt(row) + 1, col: 16 }));
     await t.click(t.text(action, t.within(t.$('.ant-dropdown'))));
   }
 
   @Step('Open TensorBoard from experiment row <row>')
   public async openExperimentInTensorBoard(row: string) {
-    const lastCol = (await t.$('th').elements()).length;
-    await t.click(t.tableCell({ row: parseInt(row) + 1, col: lastCol }));
+    await t.click(t.tableCell({ row: parseInt(row) + 1, col: 16 }));
     await clickAndCloseTab(t.text('View in TensorBoard', t.within(t.$('.ant-dropdown'))));
   }
 
