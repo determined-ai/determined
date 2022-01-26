86f4e768c67ae9c53dec74c328b7aef003e40f35
Danny Zhu <dzhu@hpe.com>
2022-01-25 18:32:18 -0700

    feat: make notebook timeout more configurable
    Previously, we could only set the idle timeout for notebooks to start
    counting if there were no kernels; that requires users to manually shut
    down kernels to take advantage of the feature. This change allows the
    notebook idle timeout to be based on either the presence of connections
    to kernels or the last activity on any kernel or terminal (as well as
    the method from before).

--- master/pkg/model/command_config.go
+++ master/pkg/model/command_config.go
@@ -4,13 +4,26 @@ import (
 	"github.com/determined-ai/determined/master/pkg/check"
 )
 
+const (
+	// NotebookIdleTypeKernelsOrTerminals indicates that a notebook should be considered active if any
+	// kernels or terminals are open.
+	NotebookIdleTypeKernelsOrTerminals = "kernels_or_terminals"
+	// NotebookIdleTypeKernelConnections indicates that a notebook should be considered active if any
+	// connections to kernels are open.
+	NotebookIdleTypeKernelConnections = "kernel_connections"
+	// NotebookIdleTypeActivity indicates that a notebook should be considered active if any kernel is
+	// running a command or any terminal is inputting or outputting data.
+	NotebookIdleTypeActivity = "activity"
+)
+
 // DefaultConfig is the default configuration used by all
 // commands (e.g., commands, notebooks, shells) if a request
 // does not specify any configuration options.
 func DefaultConfig(taskContainerDefaults *TaskContainerDefaultsConfig) CommandConfig {
 	out := CommandConfig{
-		Resources:   DefaultResourcesConfig(taskContainerDefaults),
-		Environment: DefaultEnvConfig(taskContainerDefaults),
+		Resources:        DefaultResourcesConfig(taskContainerDefaults),
+		Environment:      DefaultEnvConfig(taskContainerDefaults),
+		NotebookIdleType: NotebookIdleTypeKernelsOrTerminals,
 	}
 
 	if taskContainerDefaults != nil {
@@ -24,14 +37,15 @@ func DefaultConfig(taskContainerDefaults *TaskContainerDefaultsConfig) CommandCo
 // CommandConfig holds the necessary configurations to launch a command task in
 // the cluster.
 type CommandConfig struct {
-	Description     string           `json:"description"`
-	BindMounts      BindMountsConfig `json:"bind_mounts"`
-	Environment     Environment      `json:"environment"`
-	Resources       ResourcesConfig  `json:"resources"`
-	Entrypoint      []string         `json:"entrypoint"`
-	TensorBoardArgs []string         `json:"tensorboard_args,omitempty"`
-	IdleTimeout     *Duration        `json:"idle_timeout"`
-	WorkDir         *string          `json:"work_dir"`
+	Description      string           `json:"description"`
+	BindMounts       BindMountsConfig `json:"bind_mounts"`
+	Environment      Environment      `json:"environment"`
+	Resources        ResourcesConfig  `json:"resources"`
+	Entrypoint       []string         `json:"entrypoint"`
+	TensorBoardArgs  []string         `json:"tensorboard_args,omitempty"`
+	IdleTimeout      *Duration        `json:"idle_timeout"`
+	NotebookIdleType string           `json:"notebook_idle_type"`
+	WorkDir          *string          `json:"work_dir"`
 }
 
 // Validate implements the check.Validatable interface.
@@ -39,5 +53,14 @@ func (c *CommandConfig) Validate() []error {
 	return []error{
 		check.GreaterThanOrEqualTo(c.Resources.Slots, 0, "resources.slots must be >= 0"),
 		check.GreaterThan(len(c.Entrypoint), 0, "entrypoint must be non-empty"),
+		check.Contains(
+			c.NotebookIdleType,
+			[]interface{}{
+				NotebookIdleTypeKernelsOrTerminals,
+				NotebookIdleTypeKernelConnections,
+				NotebookIdleTypeActivity,
+			},
+			"invalid notebook idle type",
+		),
 	}
 }
