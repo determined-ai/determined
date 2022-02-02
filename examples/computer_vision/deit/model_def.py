46e87ab49492af3646147b88c1cd25e90ea00c42
gt2345 <gt2345@columbia.edu>
2022-02-02 11:08:06 -0800

    remove un used code

--- examples/computer_vision/deit/model_def.py
+++ examples/computer_vision/deit/model_def.py
@@ -30,49 +30,8 @@ from losses import DistillationLoss
 
 from typing import Any, Dict, Sequence, Tuple, Union, cast
 
-torch.set_grad_enabled(False);
 
 TorchData = Union[Dict[str, torch.Tensor], Sequence[torch.Tensor], torch.Tensor]
-
-def unpickle(file):
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo)
-    return dict
-
-def load_databatch(data_folder, idx, img_size=32):
-    data_file = os.path.join(data_folder, 'train_data_batch_')
-
-    d = unpickle(data_file + str(idx))
-    x = d['data']
-    y = d['labels']
-    mean_image = d['mean']
-
-    x = x/np.float32(255)
-    mean_image = mean_image/np.float32(255)
-
-    # Labels are indexed from 1, shift it so that indexes start at 0
-    y = [i-1 for i in y]
-    data_size = x.shape[0]
-
-    x -= mean_image
-
-    img_size2 = img_size * img_size
-
-    x = np.dstack((x[:, :img_size2], x[:, img_size2:2*img_size2], x[:, 2*img_size2:]))
-    x = x.reshape((x.shape[0], img_size, img_size, 3)).transpose(0, 3, 1, 2)
-
-    # create mirrored images
-    X_train = x[0:data_size, :, :, :]
-    Y_train = y[0:data_size]
-    X_train_flip = X_train[:, :, :, ::-1]
-    Y_train_flip = Y_train
-    X_train = np.concatenate((X_train, X_train_flip), axis=0)
-    Y_train = np.concatenate((Y_train, Y_train_flip), axis=0)
-
-    return dict(
-        X_train=X_train,
-        Y_train=Y_train.astype('int32'),
-        mean=mean_image)
 
 
 def is_dist_avail_and_initialized():
@@ -119,9 +78,6 @@ class DeitTrial(PyTorchTrial):
         )
         self.model = self.context.wrap_model(model)
 
-        n_parameters = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
-        print('number of params:', n_parameters)
-
         model_without_ddp = self.model
 
         optimizer = create_optimizer(self.args, model_without_ddp)
@@ -131,8 +87,6 @@ class DeitTrial(PyTorchTrial):
         self.criterion = DistillationLoss(
             criterion, None, 'none', 0.5, 1.0
         )
-
-    
         
     def train_batch(self, batch: TorchData, epoch_idx: int, batch_idx: int):
         samples, targets = batch
@@ -150,9 +104,8 @@ class DeitTrial(PyTorchTrial):
         self.model.eval()
         images, target = batch
 
-        with torch.cuda.amp.autocast():
-            output = self.model(images)
-            loss = self.criterion(output, target)
+        output = self.model(images)
+        loss = self.criterion(output, target)
 
         acc1, acc5 = accuracy(output, target, topk=(1, 5))
         return {"loss": loss, 'top1': acc1[0], 'top5': acc5[0]}
