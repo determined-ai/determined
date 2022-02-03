015e48129913bf210bd34dc8bc10cf8c1a900362
Bradley Laney <bradley.laney@hpe.com>
2022-02-03 11:22:00 -0500

    chore: write tests to ensure models code handles unaligned checkpoints

--- master/internal/db/postgres_helpers_intg_test.go
+++ master/internal/db/postgres_helpers_intg_test.go
@@ -4,7 +4,9 @@
 package db
 
 import (
+	"context"
 	"crypto/rand"
+	"encoding/json"
 	"fmt"
 	"testing"
 	"time"
@@ -12,19 +14,22 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/structpb"
 	"gopkg.in/guregu/null.v3"
 
 	"github.com/determined-ai/determined/master/pkg/model"
 	"github.com/determined-ai/determined/master/pkg/ptrs"
 	"github.com/determined-ai/determined/master/pkg/schemas"
 	"github.com/determined-ai/determined/master/pkg/schemas/expconf"
+	"github.com/determined-ai/determined/proto/pkg/modelv1"
+	"github.com/determined-ai/determined/proto/pkg/trialv1"
 )
 
 func pprintedExpect(expected, got interface{}) string {
 	return fmt.Sprintf("expected \n\t%s\ngot\n\t%s", spew.Sdump(expected), spew.Sdump(got))
 }
 
-func requireMockUser(t *testing.T, db *PgDB) *model.User {
+func requireMockUser(t *testing.T, db *PgDB) model.User {
 	user := model.User{
 		Username:     uuid.NewString(),
 		PasswordHash: null.NewString("", false),
@@ -32,12 +37,12 @@ func requireMockUser(t *testing.T, db *PgDB) *model.User {
 	}
 	err := db.AddUser(&user, nil)
 	require.NoError(t, err, "failed to add user")
-	return &user
+	return user
 }
 
 const defaultSearcherMetric = "okness"
 
-func requireMockExperiment(t *testing.T, db *PgDB, user *model.User) *model.Experiment {
+func requireMockExperiment(t *testing.T, db *PgDB, user model.User) *model.Experiment {
 	cfg := schemas.WithDefaults(expconf.ExperimentConfigV0{
 		RawCheckpointStorage: &expconf.CheckpointStorageConfigV0{
 			RawSharedFSConfig: &expconf.SharedFSConfigV0{
@@ -93,4 +98,63 @@ func requireMockTrial(t *testing.T, db *PgDB, exp *model.Experiment) *model.Tria
 	err := db.AddTrial(&tr)
 	require.NoError(t, err, "failed to add trial")
 	return &tr
+}
+
+func requireMockModel(t *testing.T, db *PgDB, user model.User) *modelv1.Model {
+	now := time.Now()
+	m := model.Model{
+		Name:            uuid.NewString(),
+		Description:     "",
+		Metadata:        map[string]interface{}{},
+		CreationTime:    now,
+		LastUpdatedTime: now,
+		Labels:          []string{},
+		Username:        user.Username,
+	}
+	b, err := json.Marshal(m.Metadata)
+	require.NoError(t, err)
+
+	var pmdl modelv1.Model
+	err = db.QueryProto("insert_model", &pmdl, m.Name, m.Description, b, m.Labels, "", user.ID)
+	require.NoError(t, err)
+	return &pmdl
+}
+
+func requireMockMetrics(
+	t *testing.T, db *PgDB, tr *model.Trial, latestBatch int, metricValue float64,
+) *trialv1.TrialMetrics {
+	m := trialv1.TrialMetrics{
+		TrialId:     int32(tr.ID),
+		LatestBatch: int32(latestBatch),
+		Metrics: &structpb.Struct{
+			Fields: map[string]*structpb.Value{
+				defaultSearcherMetric: {
+					Kind: &structpb.Value_NumberValue{
+						NumberValue: metricValue,
+					},
+				},
+			},
+		},
+		BatchMetrics: []*structpb.Struct{},
+	}
+	err := db.AddValidationMetrics(context.TODO(), &m)
+	require.NoError(t, err)
+	return &m
+}
+
+func requireMockCheckpoint(
+	t *testing.T, db *PgDB, tr *model.Trial, latestBatch int,
+) *trialv1.CheckpointMetadata {
+	ckpt := trialv1.CheckpointMetadata{
+		TrialId:           int32(tr.ID),
+		Uuid:              uuid.NewString(),
+		Resources:         map[string]int64{"ok": 1.0},
+		Framework:         "some framework",
+		Format:            "some format",
+		DeterminedVersion: "1.0.0",
+		LatestBatch:       int32(latestBatch),
+	}
+	err := db.AddCheckpointMetadata(context.TODO(), &ckpt)
+	require.NoError(t, err)
+	return &ckpt
 }
