daaef88757781992cf3063f35398ab7936e794dd
Anda <anda@determined.ai>
2022-02-02 16:17:38 -0800

    chore: bump version: 0.17.8-dev0 -> 0.17.9-dev0

--- webui/react/craco.config.js
+++ webui/react/craco.config.js
@@ -76,7 +76,7 @@ module.exports = {
       new DefinePlugin({
         'process.env.IS_DEV': JSON.stringify(IS_DEV),
         'process.env.SERVER_ADDRESS': JSON.stringify(process.env.SERVER_ADDRESS),
-        'process.env.VERSION': '"0.17.8-dev0"',
+        'process.env.VERSION': '"0.17.9-dev0"',
       }),
       /*
        * Add theme override support for antd. For more options:
