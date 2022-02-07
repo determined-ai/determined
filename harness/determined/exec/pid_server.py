bdb7b3edb1c5f27a1a92fdc008ed338e935e1b95
liam <liam@determined.ai>
2022-02-07 12:59:38 -0800

    chore: add additional flags to pid_server

--- harness/determined/exec/pid_server.py
+++ harness/determined/exec/pid_server.py
@@ -1,6 +1,7 @@
 import argparse
 import signal
 import sys
+import time
 from typing import Optional
 
 from determined import ipc
@@ -22,6 +23,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-x", "--on-fail", dest="on_fail", action="store", default="SIGTERM")
     parser.add_argument("-e", "--on-exit", dest="on_exit", action="store", default="WAIT")
+    parser.add_argument("--grace-period", dest="grace_period", type=int, default=3)
+    parser.add_argument(
+        "--return-one-on-worker-error", dest="return_one_on_worker_error", action="store_true"
+    )
     parser.add_argument("addr")
     parser.add_argument("num_workers", type=int)
     parser.add_argument("cmd")
@@ -32,11 +37,14 @@ if __name__ == "__main__":
     on_exit = read_action("--on-exit", args.on_exit)
     addr = ipc.read_pid_server_addr(args.addr)
 
+    start_time = time.time()
     with ipc.PIDServer(addr, args.num_workers) as pid_server:
         sys.exit(
             pid_server.run_subprocess(
                 cmd=[args.cmd] + args.cmd_args,
                 on_fail=on_fail,
                 on_exit=on_exit,
+                grace_period=args.grace_period,
+                return_one_on_worker_error=args.return_one_on_worker_error,
             ),
         )
