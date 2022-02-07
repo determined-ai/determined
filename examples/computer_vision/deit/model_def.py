ab2d8bcd4441b6a22d5b50a1840dc76fecf24c2c
gt2345 <gt2345@columbia.edu>
2022-02-07 14:38:30 -0800

    change yaml and model_def

--- examples/computer_vision/deit/model_def.py
+++ examples/computer_vision/deit/model_def.py
@@ -57,7 +57,6 @@ class DeitTrial(PyTorchTrial):
     def __init__(self, context: PyTorchTrialContext):
 
         self.context = context
-        self.context.experimental.use_amp()
 
         # Create a unique download directory for each rank so they don't overwrite each
         # other when doing distributed training.
@@ -67,6 +66,8 @@ class DeitTrial(PyTorchTrial):
         self.args.data_path = self.download_directory
 
         _, nb_classes = build_dataset(is_train=True, args=self.args)
+        
+        self.context.experimental.use_amp()
 
         model = create_model(
             'deit_base_patch16_224',
@@ -78,21 +79,22 @@ class DeitTrial(PyTorchTrial):
         )
         self.model = self.context.wrap_model(model)
 
+        n_parameters = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+        print('number of params:', n_parameters)
+
         model_without_ddp = self.model
 
         optimizer = create_optimizer(self.args, model_without_ddp)
         self.optimizer = self.context.wrap_optimizer(optimizer)
 
-        criterion = torch.nn.CrossEntropyLoss()
-        self.criterion = DistillationLoss(
-            criterion, None, 'none', 0.5, 1.0
-        )
+        self.criterion = torch.nn.CrossEntropyLoss()
+
         
     def train_batch(self, batch: TorchData, epoch_idx: int, batch_idx: int):
         samples, targets = batch
 
         outputs = self.model(samples)
-        loss = self.criterion(samples, outputs, targets)
+        loss = self.criterion(outputs, targets)
 
         is_second_order = hasattr(self.optimizer, 'is_second_order') and self.optimizer.is_second_order
 
@@ -101,24 +103,19 @@ class DeitTrial(PyTorchTrial):
         return {"loss": loss.item()}
 
     def evaluate_batch(self, batch: TorchData):
-        self.model.eval()
         images, target = batch
 
         output = self.model(images)
         loss = self.criterion(output, target)
 
         acc1, acc5 = accuracy(output, target, topk=(1, 5))
-        return {"loss": loss, 'top1': acc1[0], 'top5': acc5[0]}
+        return {"loss": loss, 'top1': acc1, 'top5': acc5}
 
 
     def build_training_data_loader(self):
         dataset_train, self.args.nb_classes = build_dataset(is_train=True, args=self.args)
-        num_tasks = get_world_size()
-        global_rank = get_rank()
-        sampler_train = torch.utils.data.DistributedSampler(
-                dataset_train, num_replicas=num_tasks, rank=global_rank, shuffle=True)
         return DataLoader(
-            dataset_train, sampler=sampler_train,
+            dataset_train, 
             batch_size=self.context.get_per_slot_batch_size(),
             num_workers=self.context.get_hparam("workers"),
             pin_memory=True,
@@ -128,12 +125,8 @@ class DeitTrial(PyTorchTrial):
 
     def build_validation_data_loader(self):
         dataset_val, _ = build_dataset(is_train=False, args=self.args)
-        num_tasks = get_world_size()
-        global_rank = get_rank()
-        sampler_val = torch.utils.data.DistributedSampler(
-                dataset_val, num_replicas=num_tasks, rank=global_rank, shuffle=False)
         return DataLoader(
-            dataset_val, sampler=sampler_val,
+            dataset_val, 
             batch_size=int(1.5 * self.context.get_per_slot_batch_size()),
             num_workers=self.context.get_hparam("workers"),
             pin_memory=True,
