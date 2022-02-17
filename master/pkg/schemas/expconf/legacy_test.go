e8ed6a2f7d11443e19800d8840dd958639ee5fa7
azhou-determined <83614683+azhou-determined@users.noreply.github.com>
2022-02-17 12:38:37 -0800

    chore: update PTL adapter to support latest PTL [DET-6421] (#3603)

--- master/pkg/schemas/expconf/legacy_test.go
+++ master/pkg/schemas/expconf/legacy_test.go
@@ -257,8 +257,8 @@ func TestLegacyConfig(t *testing.T) {
                     gpu: []
                   force_pull_image: false
                   image:
-                    cpu: determinedai/environments:py-3.7-pytorch-1.7-tf-1.15-cpu-9f0cb26
-                    gpu: determinedai/environments:cuda-10.2-pytorch-1.7-tf-1.15-gpu-9f0cb26
+                    cpu: determinedai/environments:py-3.7-pytorch-1.7-tf-1.15-cpu-4a7d07f
+                    gpu: determinedai/environments:cuda-10.2-pytorch-1.7-tf-1.15-gpu-4a7d07f
                   pod_spec:
                     apiVersion: v1
                     kind: Pod
