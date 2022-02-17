9819edc989f47dc49e3cbd7443b7acf2ae05bb4d
Anda <anda@determined.ai>
2022-02-16 18:19:14 -0800

    bumpenvs string replace

--- master/internal/resourcemanagers/provisioner/gcp_config.go
+++ master/internal/resourcemanagers/provisioner/gcp_config.go
@@ -53,7 +53,7 @@ type GCPClusterConfig struct {
 func DefaultGCPClusterConfig() *GCPClusterConfig {
 	return &GCPClusterConfig{
 		BootDiskSize:        200,
-		BootDiskSourceImage: "projects/determined-ai/global/images/det-environments-9f0cb26",
+		BootDiskSourceImage: "projects/determined-ai/global/images/det-environments-4a7d07f",
 		LabelKey:            "managed-by",
 		InstanceType: gceInstanceType{
 			MachineType: "n1-standard-32",
