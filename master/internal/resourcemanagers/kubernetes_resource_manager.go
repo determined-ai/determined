bdd8e184ccb8fa44967c2af3b16bda15bf3568e4
Eric Liu <eric.liu@hpe.com>
2022-02-28 16:27:01 -0800

    fixing rebase

--- master/internal/resourcemanagers/kubernetes_resource_manager.go
+++ master/internal/resourcemanagers/kubernetes_resource_manager.go
@@ -322,7 +322,7 @@ func (k *kubernetesResourceManager) receiveJobQueueMsg(ctx *actor.Context) error
 			if it.value().Group == msg.Handler {
 				taskActor := it.value().TaskActor
 				if id, ok := k.addrToContainerID[taskActor]; ok {
-					ctx.Tell(k.agent.handler, kubernetes.ChangePriority{PodID: id})
+					ctx.Tell(k.podsActor, kubernetes.ChangePriority{PodID: id})
 					delete(k.addrToContainerID, taskActor)
 				}
 			}
@@ -388,7 +388,7 @@ func (k *kubernetesResourceManager) assignResources(
 		})
 
 		k.addrToContainerID[req.TaskActor] = container.id
-		ctx.Tell(k.agent.handler, kubernetes.SetPodOrder{
+		ctx.Tell(k.podsActor, kubernetes.SetPodOrder{
 			QPosition: -1,
 			PodID:     container.id,
 		})
