7fbea74fedbebf32d62505876b1c2fa2a5fcd681
John Kim <john.kim@hpe.com>
2022-03-01 18:15:21 -0800

    fetch users if necessary

--- webui/react/src/components/Avatar.tsx
+++ webui/react/src/components/Avatar.tsx
@@ -2,6 +2,7 @@ import { Tooltip } from 'antd';
 import React, { useEffect, useState } from 'react';
 
 import { useStore } from 'contexts/Store';
+import { useFetchUsers } from 'hooks/useFetch';
 import { hex2hsl, hsl2str } from 'utils/color';
 import md5 from 'utils/md5';
 import { getDisplayName } from 'utils/user';
@@ -35,15 +36,19 @@ const getColor = (name = ''): string => {
 const Avatar: React.FC<Props> = ({ hideTooltip, id, name, large }: Props) => {
   const [ value, setValue ] = useState('');
   const { users } = useStore();
+  const fetchUsers = useFetchUsers(new AbortController());
 
   useEffect(() => {
     if (!name && id) {
+      if (!users.length) {
+        fetchUsers();
+      }
       const user = users.find(user => user.username === id);
       setValue(getDisplayName(user));
     } else if (name) {
       setValue(name);
     }
-  }, [ id, name, users ]);
+  }, [ fetchUsers, id, name, users ]);
 
   const style = { backgroundColor: getColor(value) };
   const classes = [ css.base ];
