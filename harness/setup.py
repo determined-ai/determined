fea84524b7a779e2ab74b42feeff952431e570ca
Sean Mackrory <mackrory@determined.ai>
2022-03-04 15:27:31 -0700

    chore: bump version: 0.17.10-rc3 -> 0.17.10

--- harness/setup.py
+++ harness/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="determined",
-    version="0.17.10-rc3",
+    version="0.17.10",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
