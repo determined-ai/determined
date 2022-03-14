54b5394de90946626be0617904a9e962605a65c7
Emily Bonar <emily.bonar@hpe.com>
2022-03-14 12:47:33 -0500

    chore: bump version: 0.17.11-rc1 -> 0.17.11

--- model_hub/setup.py
+++ model_hub/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="model-hub",
-    version="0.17.11-rc1",
+    version="0.17.11",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
