774458e3428bbd1622b52e87309b46d56bda3d13
Nikita <nikita.rajaneesh@hpe.com>
2022-03-15 15:54:03 -0500

    pr changes

--- master/internal/mocks/db.go
+++ master/internal/mocks/db.go
@@ -1833,21 +1833,7 @@ func (_m *DB) TrialStatus(trialID int) (model.State, *time.Time, error) {
 	return r0, r1, r2
 }
 
-// UpdateAllocationState provides a mock function with given fields: allocation
-func (_m *DB) UpdateAllocationState(allocation model.Allocation) error {
-	ret := _m.Called(allocation)
-
-	var r0 error
-	if rf, ok := ret.Get(0).(func(model.Allocation) error); ok {
-		r0 = rf(allocation)
-	} else {
-		r0 = ret.Error(0)
-	}
-
-	return r0
-}
-
-// UpdateAllocationState provides a mock function with given fields: allocation
+// UpdateAllocationStartTime provides a mock function with given fields: allocation
 func (_m *DB) UpdateAllocationStartTime(allocation model.Allocation) error {
 	ret := _m.Called(allocation)
 
@@ -1861,7 +1847,19 @@ func (_m *DB) UpdateAllocationStartTime(allocation model.Allocation) error {
 	return r0
 }
 
+// UpdateAllocationState provides a mock function with given fields: allocation
+func (_m *DB) UpdateAllocationState(allocation model.Allocation) error {
+	ret := _m.Called(allocation)
 
+	var r0 error
+	if rf, ok := ret.Get(0).(func(model.Allocation) error); ok {
+		r0 = rf(allocation)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
 
 // UpdateCheckpointMetadata provides a mock function with given fields: checkpoint
 func (_m *DB) UpdateCheckpointMetadata(checkpoint *model.Checkpoint) error {
