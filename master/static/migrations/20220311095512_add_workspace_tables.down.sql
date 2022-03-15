2b507444293650c98aa6f96da51d79b1c2224a3b
Nick Doiron <nick.doiron@hpe.com>
2022-03-14 23:13:43 -0500

    sql changes only

--- master/static/migrations/20220311095512_add_workspace_tables.down.sql
+++ master/static/migrations/20220311095512_add_workspace_tables.down.sql
@@ -7,4 +7,5 @@ ALTER TABLE experiments DROP COLUMN project_id;
 DROP INDEX IF EXISTS ix_experiments_project_id;
 DROP INDEX IF EXISTS ix_projects_workspace_id;
 DROP INDEX IF EXISTS ix_projects_user_id;
+DROP INDEX IF EXISTS ix_project_models_project_id;
 DROP INDEX IF EXISTS ix_workspaces_user_id;
