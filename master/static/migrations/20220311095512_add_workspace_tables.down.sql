12a767bd43985a569f96b3bbdfa2a526d00941bc
Nick Doiron <nick.doiron@hpe.com>
2022-03-14 22:58:06 -0500

    indexes and default uncategorized obj

--- master/static/migrations/20220311095512_add_workspace_tables.down.sql
+++ master/static/migrations/20220311095512_add_workspace_tables.down.sql
@@ -3,3 +3,8 @@ DROP TABLE projects;
 DROP TABLE notes;
 DROP TABLE project_models;
 ALTER TABLE experiments DROP COLUMN project_id;
+
+DROP INDEX IF EXISTS ix_experiments_project_id;
+DROP INDEX IF EXISTS ix_projects_workspace_id;
+DROP INDEX IF EXISTS ix_projects_user_id;
+DROP INDEX IF EXISTS ix_workspaces_user_id;
--- master/static/migrations/20220311095512_add_workspace_tables.up.sql
+++ master/static/migrations/20220311095512_add_workspace_tables.up.sql
@@ -1,7 +1,9 @@
+-- Tables
 CREATE TABLE workspaces (
   id SERIAL PRIMARY KEY,
   name TEXT UNIQUE NOT NULL,
   archived BOOLEAN,
+  created_at timestamp with time zone NOT NULL DEFAULT NOW(),
   user_id INT REFERENCES users(id)
 );
 CREATE TABLE projects (
@@ -9,17 +11,37 @@ CREATE TABLE projects (
   name TEXT UNIQUE NOT NULL,
   description TEXT,
   archived BOOLEAN,
+  created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+  notes JSONB,
   workspace_id INT REFERENCES workspaces(id),
   user_id INT REFERENCES users(id)
-);
-CREATE TABLE notes (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  contents TEXT,
-  project_id INT REFERENCES projects(id)
 );
 CREATE TABLE project_models (
   name TEXT,
   checkpoint_id INT
 );
 ALTER TABLE experiments ADD COLUMN project_id INT REFERENCES projects(id) NULL;
+
+-- Indexes
+CREATE INDEX ix_experiments_project_id ON public.experiments USING btree (project_id);
+CREATE INDEX ix_projects_workspace_id ON public.projects USING btree (workspace_id);
+CREATE INDEX ix_projects_user_id ON public.projects USING btree (user_id);
+CREATE INDEX ix_workspaces_user_id ON public.workspaces USING btree (user_id);
+
+-- Default workspace and project objects
+WITH admin AS (
+  SELECT id FROM users WHERE username = 'admin' LIMIT 1
+),
+worker AS (
+  INSERT INTO workspaces (name, archived, user_id)
+  SELECT 'Uncategorized', false, admin.id
+  FROM admin
+  RETURNING workspaces.id
+),
+newp AS (
+  INSERT INTO projects (name, description, archived, workspace_id, user_id)
+  SELECT 'Uncategorized', '', false, worker.id, admin.id
+  FROM admin, worker
+  RETURNING projects.id
+)
+UPDATE experiments SET project_id = (SELECT id FROM newp);
