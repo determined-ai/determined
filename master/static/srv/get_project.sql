ac12794f4432cbbdcb79a7fd9a254b77cb33671c
Nick Doiron <nick.doiron@hpe.com>
2022-03-14 23:39:27 -0500

    experiment data on projects without join/group

--- master/static/srv/get_project.sql
+++ master/static/srv/get_project.sql
@@ -1,10 +1,14 @@
+WITH pe AS (
+  SELECT state, start_time
+  FROM experiments
+  WHERE experiments.project_id = $1
+)
 SELECT p.id, p.name, p.workspace_id, p.description, p.archived, p.notes,
   COUNT(pe) AS num_experiments,
   SUM(case when pe.state = 'ACTIVE' then 1 else 0 end) AS num_active_experiments,
   MAX(pe.start_time) AS last_experiment_started_at,
   u.username
-FROM projects as p
+FROM pe, projects as p
   LEFT JOIN users as u ON u.id = p.user_id
-  LEFT JOIN experiments pe ON pe.project_id = p.id
 WHERE p.id = $1
-GROUP BY p.id, pe.project_id, u.username;
+GROUP BY p.id, u.username;
