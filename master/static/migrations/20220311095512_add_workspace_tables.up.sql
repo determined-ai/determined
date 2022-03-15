cf3a8cbcf3592fc0bd9f85284ec9816a50d47615
Nick Doiron <nick.doiron@hpe.com>
2022-03-14 23:08:48 -0500

    sql changes

--- master/static/migrations/20220311095512_add_workspace_tables.up.sql
+++ master/static/migrations/20220311095512_add_workspace_tables.up.sql
@@ -4,21 +4,26 @@ CREATE TABLE workspaces (
   name TEXT UNIQUE NOT NULL,
   archived BOOLEAN,
   created_at timestamp with time zone NOT NULL DEFAULT NOW(),
-  user_id INT REFERENCES users(id)
+  user_id INT REFERENCES users(id),
+  immutable BOOLEAN DEFAULT FALSE
 );
 CREATE TABLE projects (
   id SERIAL PRIMARY KEY,
-  name TEXT UNIQUE NOT NULL,
+  name TEXT NOT NULL,
   description TEXT,
   archived BOOLEAN,
   created_at timestamp with time zone NOT NULL DEFAULT NOW(),
   notes JSONB,
   workspace_id INT REFERENCES workspaces(id),
-  user_id INT REFERENCES users(id)
+  user_id INT REFERENCES users(id),
+  immutable BOOLEAN DEFAULT FALSE,
+  UNIQUE (name, workspace_id)
 );
 CREATE TABLE project_models (
   name TEXT,
-  checkpoint_id INT
+  checkpoint_id INT,
+  created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+  project_id INT REFERENCES projects(id)
 );
 ALTER TABLE experiments ADD COLUMN project_id INT REFERENCES projects(id) NULL;
 
@@ -26,6 +31,7 @@ ALTER TABLE experiments ADD COLUMN project_id INT REFERENCES projects(id) NULL;
 CREATE INDEX ix_experiments_project_id ON public.experiments USING btree (project_id);
 CREATE INDEX ix_projects_workspace_id ON public.projects USING btree (workspace_id);
 CREATE INDEX ix_projects_user_id ON public.projects USING btree (user_id);
+CREATE INDEX ix_project_models_project_id ON public.project_models USING btree (project_id);
 CREATE INDEX ix_workspaces_user_id ON public.workspaces USING btree (user_id);
 
 -- Default workspace and project objects
@@ -33,14 +39,14 @@ WITH admin AS (
   SELECT id FROM users WHERE username = 'admin' LIMIT 1
 ),
 worker AS (
-  INSERT INTO workspaces (name, archived, user_id)
-  SELECT 'Uncategorized', false, admin.id
+  INSERT INTO workspaces (name, archived, user_id, immutable)
+  SELECT 'Uncategorized', false, admin.id, true
   FROM admin
   RETURNING workspaces.id
 ),
 newp AS (
-  INSERT INTO projects (name, description, archived, workspace_id, user_id)
-  SELECT 'Uncategorized', '', false, worker.id, admin.id
+  INSERT INTO projects (name, description, archived, workspace_id, user_id, immutable)
+  SELECT 'Uncategorized', '', false, worker.id, admin.id, true
   FROM admin, worker
   RETURNING projects.id
 )
