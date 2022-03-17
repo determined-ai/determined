6bdbadde7a700c60c726b620aa3e51e649b78ac6
John Kim <john.kim@hpe.com>
2022-03-17 10:03:01 -0700

    User and UserFields are redundant

--- webui/react/src/utils/user.ts
+++ webui/react/src/utils/user.ts
@@ -1,10 +1,5 @@
 import { DetailedUser, User } from 'types';
 
-interface UserFields {
-  displayName: string;
-  username: string;
-}
-
-export function getDisplayName (user: DetailedUser | User | UserFields | undefined): string {
+export function getDisplayName (user: DetailedUser | User | undefined): string {
   return user?.displayName || user?.username || 'Unavailable';
 }
