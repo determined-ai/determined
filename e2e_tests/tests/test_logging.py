37bbc6a492ac053fd45371028769ae7b886c0fab
Bradley Laney <bradley.laney@hpe.com>
2022-03-21 13:33:36 -0400

    disable log flake

--- e2e_tests/tests/test_logging.py
+++ e2e_tests/tests/test_logging.py
@@ -46,19 +46,7 @@ def test_trial_logs() -> None:
     "task_type,task_config,log_regex",
     [
         # TODO(DET-6712): Investigate intermittent slowness with K8s command logs.
-        (
-            command.TaskTypeCommand,
-            {
-                "entrypoint": [
-                    "echo",
-                    "hello",
-                    "&&",
-                    "sleep",
-                    "10",
-                ]
-            },
-            re.compile("^.*hello.*$"),
-        ),
+        # (command.TaskTypeCommand, {"entrypoint": ["echo", "hello"]}, re.compile("^.*hello.*$")),
         (command.TaskTypeNotebook, {}, re.compile("^.*Jupyter Server .* is running.*$")),
         (command.TaskTypeShell, {}, re.compile("^.*Server listening on.*$")),
         (command.TaskTypeTensorBoard, {}, re.compile("^.*TensorBoard .* at .*$")),
