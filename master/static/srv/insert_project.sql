bdfa92792377933d7ea116bbf3b4ecb437a7f64b
Nick Doiron <nick.doiron@hpe.com>
2022-03-22 21:19:43 -0500

    more complete responses from sql queries

--- master/static/srv/insert_project.sql
+++ master/static/srv/insert_project.sql
@@ -3,6 +3,6 @@ WITH p AS (
   VALUES ($1, $2, $3, $4)
   RETURNING id, name, description, archived, workspace_id, user_id
 )
-SELECT p.id, p.name, p.description, p.archived, u.username
+SELECT p.id, p.name, p.description, p.archived, p.workspace_id, u.username
 FROM p
 JOIN users u on u.id = p.user_id;
