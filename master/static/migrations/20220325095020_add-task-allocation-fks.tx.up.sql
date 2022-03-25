158f0f39b4b1483028029ceeaa3b849d7046079d
Ilia Glazkov <ilia@hpe.com>
2022-03-25 15:20:59 -0700

    style

--- master/static/migrations/20220325095020_add-task-allocation-fks.tx.up.sql
+++ master/static/migrations/20220325095020_add-task-allocation-fks.tx.up.sql
@@ -8,6 +8,7 @@ FOREIGN KEY (task_id) REFERENCES tasks (task_id);
 -- Since we clear out allocation_sessions on master init anyway,
 -- clear them out here to avoid conflicts.
 DELETE FROM allocation_sessions;
+
 ALTER TABLE allocation_sessions
 ADD CONSTRAINT allocation_id_fkey
 FOREIGN KEY (allocation_id) REFERENCES allocations (allocation_id);
