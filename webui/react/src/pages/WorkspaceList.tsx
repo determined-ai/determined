9ad6af390f1d97f9ff6da766b26419affe463b4b
Emily <15078396+EmilyBonar@users.noreply.github.com>
2022-03-28 09:03:53 -0500

    feat: add workspace and project routes (#3805)

---
+++ webui/react/src/pages/WorkspaceList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Page from 'components/Page';
+import Section from 'components/Section';
+
+const WorkspaceList: React.FC = () => {
+  return (
+    <Page docTitle="Workspaces" id="workspaces">
+      <Section title="Workspaces" />
+    </Page>
+  );
+};
+
+export default WorkspaceList;
