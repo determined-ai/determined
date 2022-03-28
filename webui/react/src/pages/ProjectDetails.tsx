7ff19deb70ce71b61f9c03b4988f5b874a1fdb49
Emily <15078396+EmilyBonar@users.noreply.github.com>
2022-03-28 09:03:53 -0500

    feat: add workspace and project routes (#3805)

---
+++ webui/react/src/pages/ProjectDetails.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Page from 'components/Page';
+import Section from 'components/Section';
+
+const ProjectDetails: React.FC = () => {
+  return (
+    <Page docTitle="Project Details" id="projectDetails">
+      <Section title="Project Details" />
+    </Page>
+  );
+};
+
+export default ProjectDetails;
