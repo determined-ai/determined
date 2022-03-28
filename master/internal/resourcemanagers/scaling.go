95ea7a29fea3bc564a46a67260965b5bbcc14878
nrajanee <nikita.rajaneesh@hpe.com>
2022-03-28 15:06:33 -0500

    fix: use max slots provisioning agents [DET-5725]  (#3788)
    * include max_slots in scaling.go
    
    * fix scaling_test.go
    
    * fix test
    
    * fix lint check
    
    * Update master/internal/resourcemanagers/scaling.go
    
    Co-authored-by: Ilia Glazkov <ioooga@gmail.com>
    
    * fix comment
    
    Co-authored-by: Ilia Glazkov <ioooga@gmail.com>

--- master/internal/resourcemanagers/scaling.go
+++ master/internal/resourcemanagers/scaling.go
@@ -1,13 +1,18 @@
 package resourcemanagers
 
+import (
+	"github.com/determined-ai/determined/master/pkg/actor"
+)
+
 // calculateDesiredNewAgentNum calculates the new instances based on pending tasks and
 // slots per instance.
 func calculateDesiredNewAgentNum(
-	taskList *taskList, slotsPerAgent int, maxZeroSlotTasksPerAgent int,
+	taskList *taskList, groups map[*actor.Ref]*group, slotsPerAgent int, maxZeroSlotTasksPerAgent int,
 ) int {
 	slotSum := 0
 	allTasks := 0
 	zeroSlotTasks := 0
+	groupSlotsNeeded := make(map[*group]int)
 	for it := taskList.iterator(); it.next(); {
 		// TODO(DET-4035): This code is duplicated from the fitting functions in the
 		//    scheduler. To determine is a task is schedulable, we would ideally interface
@@ -21,12 +26,23 @@ func calculateDesiredNewAgentNum(
 			allTasks++
 		case slotsPerAgent == 0:
 			continue
-		case it.value().SlotsNeeded <= slotsPerAgent:
-			slotSum += it.value().SlotsNeeded
+		case it.value().SlotsNeeded <= slotsPerAgent, it.value().SlotsNeeded%slotsPerAgent == 0:
+			if groups != nil {
+				group := groups[it.value().Group]
+				groupSlotsNeeded[group] += it.value().SlotsNeeded
+			} else {
+				slotSum += it.value().SlotsNeeded
+			}
 			allTasks++
-		case it.value().SlotsNeeded%slotsPerAgent == 0:
-			slotSum += it.value().SlotsNeeded
-			allTasks++
+		}
+	}
+
+	for g, groupSlotSum := range groupSlotsNeeded {
+		maxSlots := g.maxSlots
+		if maxSlots != nil {
+			slotSum += min(*maxSlots, groupSlotSum)
+		} else {
+			slotSum += groupSlotSum
 		}
 	}
 
