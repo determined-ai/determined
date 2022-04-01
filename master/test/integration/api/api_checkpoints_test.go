4e16a7b3c8f85a29b4afcd9b31c127dbd0d25873
Maksim Kouznetsov <maksim.kouznetsov@hpe.com>
2022-04-01 10:40:36 -0700

    checkpoint with and without validation

--- master/test/integration/api/api_checkpoints_test.go
+++ master/test/integration/api/api_checkpoints_test.go
@@ -8,6 +8,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/determined-ai/determined/proto/pkg/checkpointv1"
+
 	"github.com/determined-ai/determined/master/internal/db"
 	"github.com/determined-ai/determined/master/test/testutils"
 	"github.com/determined-ai/determined/proto/pkg/trialv1"
@@ -31,47 +33,93 @@ func TestGetCheckpoint(t *testing.T) {
 func testGetCheckpoint(
 	t *testing.T, creds context.Context, cl apiv1.DeterminedClient, db *db.PgDB,
 ) {
-	t.Run("a", func(t *testing.T) {
-		experiment := model.ExperimentModel()
-		err := db.AddExperiment(experiment)
-		assert.NilError(t, err, "failed to insert experiment")
+	type testCase struct {
+		name     string
+		validate bool
+	}
 
-		trial := model.TrialModel(
-			experiment.ID, experiment.JobID, model.WithTrialState(model.ActiveState))
-		err = db.AddTrial(trial)
-		assert.NilError(t, err, "failed to insert trial")
+	testCases := []testCase{
+		{
+			name:     "checkpoint with validation",
+			validate: true,
+		},
+		{
+			name:     "checkpoint without validation",
+			validate: false,
+		},
+	}
 
-		checkpointUuid := uuid.NewString()
-		latestBatch := int32(10)
-		checkpointMeta := trialv1.CheckpointMetadata{
-			TrialId:           int32(trial.ID),
-			TrialRunId:        int32(0),
-			Uuid:              checkpointUuid,
-			Resources:         map[string]int64{"ok": 1.0},
-			Framework:         "some framework",
-			Format:            "some format",
-			DeterminedVersion: "1.0.0",
-			LatestBatch:       latestBatch,
-		}
-		err = db.AddCheckpointMetadata(context.Background(), &checkpointMeta)
-		t.Logf("checkpoint meta=%v", checkpointMeta)
-		assert.NilError(t, err, "failed to add checkpoint meta")
+	runTestCase := func(t *testing.T, tc testCase, id int) {
+		t.Run(tc.name, func(t *testing.T) {
+			experiment := model.ExperimentModel()
+			err := db.AddExperiment(experiment)
+			assert.NilError(t, err, "failed to insert experiment")
 
-		ctx, _ := context.WithTimeout(creds, 10*time.Second)
-		req := apiv1.GetCheckpointRequest{CheckpointUuid: checkpointUuid}
+			trial := model.TrialModel(
+				experiment.ID, experiment.JobID, model.WithTrialState(model.ActiveState))
+			err = db.AddTrial(trial)
+			assert.NilError(t, err, "failed to insert trial")
 
-		ckptResp, err := cl.GetCheckpoint(ctx, &req)
-		assert.NilError(t, err, "failed to get checkpoint from api")
-		ckptCl := *ckptResp.Checkpoint
-		fields := ckptCl.ExperimentConfig.GetFields()
-		entrypoint := fields["entrypoint"].GetStringValue()
-		assert.Equal(t, ckptCl.Uuid, checkpointUuid)
-		t.Logf("Entrypoint: %s", entrypoint)
-		// assert.Equal(t, ckptCl.ExperimentConfig.Entrypoint.ModelDef, "model_def:SomeTrialClass")
-		assert.Equal(t, ckptCl.ExperimentId, int32(experiment.ID))
-		assert.Equal(t, ckptCl.TrialId, int32(trial.ID))
-		t.Logf("Checkpoint from api: %v", ckptCl)
-		t.Logf("Uuid=%s", ckptCl.Uuid)
-		t.Logf("ExperimentConfig=%v", ckptCl.ExperimentConfig)
-	})
+			latestBatch := int32(10)
+			if tc.validate {
+				trialMetrics := trialv1.TrialMetrics{
+					TrialId:     int32(trial.ID),
+					TrialRunId:  int32(0),
+					LatestBatch: latestBatch,
+					Metrics:     nil,
+				}
+
+				err = db.AddValidationMetrics(context.Background(), &trialMetrics)
+				assert.NilError(t, err, "failed to add validation metrics")
+			}
+
+			checkpointUuid := uuid.NewString()
+			checkpointMeta := trialv1.CheckpointMetadata{
+				TrialId:           int32(trial.ID),
+				TrialRunId:        int32(0),
+				Uuid:              checkpointUuid,
+				Resources:         map[string]int64{"ok": 1.0},
+				Framework:         "some framework",
+				Format:            "some format",
+				DeterminedVersion: "1.0.0",
+				LatestBatch:       latestBatch,
+			}
+			err = db.AddCheckpointMetadata(context.Background(), &checkpointMeta)
+
+			// TODO remove
+			t.Logf("checkpoint meta=%v", checkpointMeta)
+
+			assert.NilError(t, err, "failed to add checkpoint meta")
+
+			ctx, _ := context.WithTimeout(creds, 10*time.Second)
+			req := apiv1.GetCheckpointRequest{CheckpointUuid: checkpointUuid}
+
+			ckptResp, err := cl.GetCheckpoint(ctx, &req)
+			assert.NilError(t, err, "failed to get checkpoint from api")
+			ckptCl := *ckptResp.Checkpoint
+			assert.Equal(t, ckptCl.Uuid, checkpointUuid)
+
+			entrypoint := ckptCl.ExperimentConfig.GetFields()["entrypoint"].GetStringValue()
+			assert.Equal(t, entrypoint, "model_def:SomeTrialClass")
+
+			assert.Equal(t, ckptCl.ExperimentId, int32(experiment.ID))
+			assert.Equal(t, ckptCl.TrialId, int32(trial.ID))
+			assert.Equal(t, ckptCl.Framework, "some framework")
+			assert.Equal(t, ckptCl.Format, "some format")
+
+			if tc.validate {
+				assert.Equal(t, ckptCl.ValidationState, checkpointv1.State_STATE_COMPLETED)
+			} else {
+				assert.Equal(t, ckptCl.ValidationState, checkpointv1.State_STATE_UNSPECIFIED)
+			}
+			assert.Equal(t, ckptCl.State, checkpointv1.State_STATE_COMPLETED)
+			t.Logf("Checkpoint from api: %v", ckptCl)
+			t.Logf("Uuid=%s", ckptCl.Uuid)
+			t.Logf("ExperimentConfig=%v", ckptCl.ExperimentConfig)
+		})
+	}
+
+	for idx, tc := range testCases {
+		runTestCase(t, tc, idx)
+	}
 }
