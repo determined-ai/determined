82e59f17d0319983e4a0a9637ce4494e27e272e2
Nikita <nikita.rajaneesh@hpe.com>
2022-04-05 13:28:18 -0500

    support_bundle

--- harness/determined/cli/support_bundle.py
+++ harness/determined/cli/support_bundle.py
@@ -3,44 +3,63 @@ from determined.common import api
 from determined.common.declarative_argparse import Arg, Cmd
 from determined.common.api import authentication, bindings
 import tarfile
+import json
 
 @authentication.required
 def output_logs(args: Namespace): 
-    output_dir_tar = tarfile.open(f'{args.output_dir}.tar.gz')
+    output_dir_tar = tarfile.open(f'{args.output_dir}.tar.gz', 'w:gz')
 
-    err = write_trial_logs(args.trial_id, output_dir_tar)
-    if err != nil:
-        raise.Error("Unable to write trial logs to a json file")
+    trial_logs_file = write_trial_logs(args, args.trial_id, output_dir_tar)
+    output_dir_tar.add(f'{trial_logs_file}.json')
     
-    err = write_master_logs(args.trial_id, output_dir_tar)
-     if err != nil:
-        raise.Error("Unable to write master logs to a json file")
-   
-    err = write_api_logs(args.trial_id, output_dir_tar)
-     if err != nil:
-        raise.Error("Unable to write api logs to a json file")
+    master_logs_file = write_master_logs(args.trial_id, output_dir_tar)
+    output_dir_tar.add(f'{master_logs_file}.json')
 
-def write_trial_logs(trial_id, output_dir):  #difference between this trial_logs and api call? There doesn't seem to be any. 
-    trial_logs = api.trial_logs(args.master, trial_id)
-     create_file_in_dir(trial_logs,'trial_logs', output_dir)
+    api_trail_file, api_experiment_file = write_api_call(args.trial_id, output_dir_tar)
+    output_dir_tar.add(f'{api_trail_file}.json')
+    output_dir_tar.add(f'{api_experiment_file}.json')
 
-def write_master_logs(trial_id, output_dir):
+    output_dir_tar.close()
+
+    return f'{args.output_dir}.tar.gz'
+
+    
+
+def write_trial_logs(args,trial_id, output_dir):  #difference between this trial_logs and api call? There doesn't seem to be any. 
+    trial_logs = api.task_logs(args.master, trial_id)
+    # use trial logs to get the experiment id 
+    file_name = 'trial_logs'
+    create_json_file_in_dir(trial_logs,file_name, output_dir)
+    return file_name
+
+def write_master_logs(args, trial_id, output_dir):
     response = api.get(
                 args.master, "logs"
             )
-    create_file_in_dir(response,'master_logs', output_dir)
+    file_name = 'master_logs'
+    create_json_file_in_dir(response,file_name, output_dir)
+    return file_name
 
-
-def write_api_logs(trial_id, output_dir): 
+def write_api_call(trial_id, output_dir): 
     #r = bindings.get_GetExperiment(sess, experimentId=exp_id).experiment -> how would I get experiment id associated with trial? From task table in database?
-    return None
+    bindings.get_GetExperiment()
+    file_name1 = 'api_experiment_call'
+    file_name2 = 'api_trial_call'
+
+    trial_obj = bindings.get_GetTrial(trialId=trial_id)
+    experiment_id = trial_obj.experimentId
+    exp_obj = bindings.get_GetExperiment(experimentId=experiment_id)
+
+    trial_obj.to_json(), exp_obj.to_json()
+    create_json_file_in_dir(exp_obj.to_json(), file_name1, output_dir)
+    create_json_file_in_dir(trial_obj.to_json(), file_name2, output_dir)
+    return file_name1, file_name2
 
 def create_json_file_in_dir(content, file_name, output_dir): 
     with open('{file_name}.json', 'w') as f: 
         json.dump(content, f)
-        
-    outpur_dir.add(f'{file_name}.json')
     
+    f.close()
 
 args_description = [
     Cmd(
