96947e5a84bd963411fa648a83faa4a26aa028c3
Emily Bonar <emily.bonar@hpe.com>
2022-04-07 14:11:12 -0500

    Workspace Details Header

--- webui/react/src/pages/WorkspaceDetails.tsx
+++ webui/react/src/pages/WorkspaceDetails.tsx
@@ -1,9 +1,15 @@
-import { Button } from 'antd';
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
+import Message, { MessageType } from 'components/Message';
 import Page from 'components/Page';
-import useModalProjectCreate from 'hooks/useModal/useModalProjectCreate';
+import Spinner from 'components/Spinner';
+import usePolling from 'hooks/usePolling';
+import { getWorkspace, getWorkspaceProjects, isNotFound } from 'services/api';
+import { Project, Workspace } from 'types';
+import handleError from 'utils/error';
+
+import WorkspaceDetailsHeader from './WorkspaceDetails/WorkspaceDetailsHeader';
 
 interface Params {
   workspaceId: string;
@@ -11,17 +17,59 @@ interface Params {
 
 const WorkspaceDetails: React.FC = () => {
   const { workspaceId } = useParams<Params>();
-  const { modalOpen } = useModalProjectCreate({ workspaceId: parseInt(workspaceId) });
+  const [ workspace, setWorkspace ] = useState<Workspace>();
+  const [ projects, setProjects ] = useState<Project[]>([]);
+  const [ pageError, setPageError ] = useState<Error>();
+  const [ isLoading, setIsLoading ] = useState(true);
+  const [ canceler ] = useState(new AbortController());
 
-  const handleProjectCreateClick = useCallback(() => {
-    modalOpen();
-  }, [ modalOpen ]);
+  const id = parseInt(workspaceId);
+
+  const fetchWorkspace = useCallback(async () => {
+    try {
+      const response = await getWorkspace({ id });
+      setWorkspace(response);
+    } catch (e) {
+      if (!pageError) setPageError(e as Error);
+    }
+  }, [ id, pageError ]);
+
+  const fetchProjects = useCallback(async () => {
+    try {
+      const response = await getWorkspaceProjects({ id });
+      setProjects(response.projects);
+    } catch (e) {
+      handleError(e, { publicSubject: 'Unable to fetch projects.' });
+    } finally {
+      setIsLoading(false);
+    }
+  }, [ id ]);
+
+  const fetchAll = useCallback(async () => {
+    await Promise.allSettled([ fetchWorkspace(), fetchProjects() ]);
+  }, [ fetchWorkspace, fetchProjects ]);
+
+  usePolling(fetchAll);
+
+  useEffect(() => {
+    return () => canceler.abort();
+  }, [ canceler ]);
+
+  if (isNaN(id)) {
+    return <Message title={`Invalid Workspace ID ${workspaceId}`} />;
+  } else if (pageError) {
+    const message = isNotFound(pageError) ?
+      `Unable to find Workspace ${workspaceId}` :
+      `Unable to fetch Workspace ${workspaceId}`;
+    return <Message title={message} type={MessageType.Warning} />;
+  } else if (!workspace) {
+    return <Spinner tip={`Loading workspace ${workspaceId} details...`} />;
+  }
 
   return (
     <Page
+      headerComponent={<WorkspaceDetailsHeader workspace={workspace} />}
       id="workspaceDetails"
-      options={<Button onClick={handleProjectCreateClick}>New Project</Button>}
-      title="Workspace Details"
     />
   );
 };
---
+++ webui/react/src/pages/WorkspaceDetails/WorkspaceDetailsHeader.tsx
@@ -0,0 +1,72 @@
+import { DownOutlined } from '@ant-design/icons';
+import { Button, Dropdown, Menu, Space } from 'antd';
+import React, { useCallback, useMemo, useState } from 'react';
+
+import InlineEditor from 'components/InlineEditor';
+import useModalProjectCreate from 'hooks/useModal/useModalProjectCreate';
+import { Workspace } from 'types';
+import handleError from 'utils/error';
+
+import css from './WorkspaceDetailsHeader.module.scss';
+
+interface Props {
+  workspace: Workspace;
+}
+
+const WorkspaceDetailsHeader: React.FC<Props> = ({ workspace }: Props) => {
+  const { modalOpen } = useModalProjectCreate({ workspaceId: workspace.id });
+
+  const handleProjectCreateClick = useCallback(() => {
+    modalOpen();
+  }, [ modalOpen ]);
+
+  const handleArchiveClick = useCallback(() => {
+    if (workspace.archived) {
+      try {
+        // unarchive workspace
+      } catch (e) {
+        handleError(e, { publicSubject: 'Unable to unarchive workspace.' });
+      }
+    } else {
+      try {
+        // archive workspace
+      } catch (e) {
+        handleError(e, { publicSubject: 'Unable to archive workspace.' });
+      }
+    }
+  }, [ workspace.archived ]);
+
+  const handleDeleteClick = useCallback(() => {
+    // bring up delete workspace modal
+  }, []);
+
+  const ActionMenu = useMemo(() => {
+    return (
+      <Menu>
+        <Menu.Item onClick={handleArchiveClick}>
+          {workspace.archived ? 'Unarchive' : 'Archive'}
+        </Menu.Item>
+        <Menu.Item danger onClick={handleDeleteClick}>Delete...</Menu.Item>
+      </Menu>
+    );
+  }, [ handleArchiveClick, handleDeleteClick, workspace.archived ]);
+
+  return (
+    <div className={css.base}>
+      <Space align="center">
+        <div className={css.icon}>{workspace.name[0]}</div>
+        <h1 className={css.name}>
+          <InlineEditor disabled={workspace.immutable} maxLength={80} value={workspace.name} />
+        </h1>
+        {!workspace.immutable && (
+          <Dropdown arrow overlay={ActionMenu} trigger={[ 'click' ]}>
+            <DownOutlined style={{ fontSize: 12 }} />
+          </Dropdown>
+        )}
+      </Space>
+      <Button onClick={handleProjectCreateClick}>New Project</Button>
+    </div>
+  );
+};
+
+export default WorkspaceDetailsHeader;
