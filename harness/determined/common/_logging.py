fe08f163805d096076e1a8653d6df04ee395dee6
Tim Tucker <varlogtim@gmail.com>
2022-04-07 10:15:17 -0400

    Update _logging.py

--- harness/determined/common/_logging.py
+++ harness/determined/common/_logging.py
@@ -11,6 +11,6 @@ def set_logger(debug_enabled: bool) -> None:
 
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.DEBUG if debug_enabled else logging.INFO)
-    formatter = logging.Formatter("%(levelname)s [%(process)s]: %(message)s")
+    formatter = logging.Formatter("%(levelname)s [%(process)s]: %(name)s: %(message)s")
     handler.setFormatter(formatter)
     root.addHandler(handler)
