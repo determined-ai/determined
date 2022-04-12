39bfc146b02f35ce13aff58a73d41936b4365e40
Nikita <nikita.rajaneesh@hpe.com>
2022-04-12 09:42:10 -0700

    add e2e test

--- e2e_tests/tests/trial/trial.py
+++ e2e_tests/tests/trial/trial.py
@@ -9,9 +9,15 @@ import time
 
 def create_exp_get_trial_id():
     exp_id = exp.create_experiment(conf.cv_examples_path("cifar10_pytorch/const.yaml"),conf.cv_examples_path("cifar10_pytorch"))
-    exp.wait_for_experiment_state(exp_id, determinedexperimentv1State.STATE_COMPLETED) # waiting for completion because can't be exactly sure at which point trials get added. 
+    exp.wait_for_experiment_state(exp_id, determinedexperimentv1State.STATE_ACTIVE) 
+    session = test_session()
+    trials = bindings.get_GetExperimentTrials(session, experimentId=exp_id).trials    
+    while(len(trials) <= 0):
+        print(f'no of trials: {len(trials)}')
+        time.sleep(5 * 60) # wait for 5 minutes to check again whether trials were added 
+        trials = bindings.get_GetExperimentTrials(session, experimentId=exp_id).trials    
+
     assert len(trials) > 0
-    trials = bindings.get_GetExperimentTrials(test_session(), experimentId=exp_id).trials    
     trial_example = trials[0]
     trial_id = trial_example.id
     return trial_id
