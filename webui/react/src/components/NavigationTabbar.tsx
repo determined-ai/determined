9e0e497f2aac89dc0693475fc5f166b405c68633
Julian Glover <julianglover@Julians-MacBook-Pro.local>
2022-04-15 12:35:30 -0500

    chore: add key attribute to avatar ActionCard action

--- webui/react/src/components/NavigationTabbar.tsx
+++ webui/react/src/components/NavigationTabbar.tsx
@@ -87,7 +87,7 @@ const NavigationTabbar: React.FC = () => {
         actions={[
           {
             render: () => {
-              return <AvatarCard className={css.user} user={auth.user} />;
+              return <AvatarCard key='Avatar' className={css.user} user={auth.user} />;
             },
           },
           {
