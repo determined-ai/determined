6920540251b38851e50ec3c628ba5be28e8f4051
Corey Staten <corey.staten@hpe.com>
2022-04-15 12:12:14 -0400

    Bumpenvs to use new environments.

--- harness/determined/common/schemas/expconf/_v0.py
+++ harness/determined/common/schemas/expconf/_v0.py
@@ -259,14 +259,14 @@ class EnvironmentImageV0(schemas.SchemaBase):
     def runtime_defaults(self) -> None:
         if self.cpu is None:
             self.cpu = (
-                "determinedai/environments:py-3.8-pytorch-1.10-lightning-1.5-tf-2.8-cpu-83dbcaa"
+                "determinedai/environments:py-3.8-pytorch-1.10-lightning-1.5-tf-2.8-cpu-ed66d8a"
             )
         if self.rocm is None:
-            self.rocm = "determinedai/environments:rocm-4.2-pytorch-1.9-tf-2.5-rocm-83dbcaa"
+            self.rocm = "determinedai/environments:rocm-4.2-pytorch-1.9-tf-2.5-rocm-ed66d8a"
 
         if self.cuda is None:
             self.cuda = (
-                "determinedai/environments:cuda-11.3-pytorch-1.10-lightning-1.5-tf-2.8-gpu-83dbcaa"
+                "determinedai/environments:cuda-11.3-pytorch-1.10-lightning-1.5-tf-2.8-gpu-ed66d8a"
             )
 
 
