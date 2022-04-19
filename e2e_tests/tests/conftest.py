b566deaa588177ebe251833e0f44fb8d2d5fe8d3
Nikita <nikita.rajaneesh@hpe.com>
2022-04-19 13:20:48 -0700

    lint

--- e2e_tests/tests/conftest.py
+++ e2e_tests/tests/conftest.py
@@ -12,9 +12,11 @@ from botocore import exceptions as boto_exc
 
 from tests import config
 from tests.experiment import profile_test
-#from tests.nightly.compute_stats import compare_stats
 
 from .cluster_log_manager import ClusterLogManager
+
+# from tests.nightly.compute_stats import compare_stats
+
 
 _INTEG_MARKERS = {
     "tensorflow1_cpu",
