295fa9ed43329c3b0ebc5019fb1a3abb9e676176
Anda <anda@determined.ai>
2022-04-25 12:42:03 -0700

    initialize torch dist after single slot

--- harness/determined/launch/torch_distributed.py
+++ harness/determined/launch/torch_distributed.py
@@ -50,14 +50,14 @@ def main(override_args: List[str], script: List[str]) -> int:
     info = det.get_cluster_info()
     assert info is not None, "must be run on-cluster"
 
-    os.environ["USE_TORCH_DISTRIBUTED"] = "True"
-
     single_slot = len(info.container_addrs) == 1 and len(info.slot_ids) == 1
 
     # Detect single-slot trials and skip distributed launch
     if single_slot:
         return subprocess.Popen(script).wait()
 
+    os.environ["USE_TORCH_DISTRIBUTED"] = "True"
+    
     chief_ip = info.container_addrs[0]
     os.environ["DET_CHIEF_IP"] = chief_ip
 
