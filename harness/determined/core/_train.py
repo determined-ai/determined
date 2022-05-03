3f5c710581441eddff7c6e859404057068916006
Maksim Kouznetsov <maksim.kouznetsov@hpe.com>
2022-05-02 18:23:28 -0700

    update buf.image.bin

--- harness/determined/core/_train.py
+++ harness/determined/core/_train.py
@@ -227,7 +227,8 @@ class DummyTrainContext(TrainContext):
         serializable_metrics = self._get_serializable_metrics(metrics)
         metrics = {k: metrics[k] for k in serializable_metrics}
         logger.info(
-            f"report_validation_metrics(steps_completed={steps_completed}, metrics={metrics})"
+            f"report_validation_metrics(steps_completed={steps_completed},"
+            f" metrics={metrics})"
         )
 
     def report_early_exit(self, reason: EarlyExitReason) -> None:
