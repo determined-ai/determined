bf722610cda79218e02c8587e16ecca83312c093
Daniel R Hunter <daniel.robert.hunter@hpe.com>
2022-05-04 13:34:58 -0400

    Print losses with every preview

--- examples/gan/pix2pix_tf_keras/fit.py
+++ examples/gan/pix2pix_tf_keras/fit.py
@@ -30,16 +30,21 @@ def fit(train_ds, test_ds, steps, preview=0):
     start = time.time()
 
     for step, data in train_ds.repeat().take(steps).enumerate():
-        if preview and ((step) % preview == 0):
-            if step != 0:
-                print(f"Time taken for {preview} steps: {time.time()-start:.2f} sec\n")
-            start = time.time()
-            generate_images(pix2pix.generator, example_input, example_target)
-            print(f"Step: {step}")
         # Training step
-        pix2pix.train_step(data)
+        losses = pix2pix.train_step(data)
         if (step + 1) % 10 == 0:
             print(".", end="", flush=True)
+        if preview and ((step + 1) % preview == 0):
+            if step != 0:
+                print(f"Time taken for {preview} steps: {time.time()-start:.2f} sec\n")
+                print("g_gan_loss: ", losses["g_gan_loss"])
+                print("g_l1_loss:  ", losses["g_l1_loss"])
+                print("g_loss:     ", losses["g_loss"])
+                print("d_loss:     ", losses["d_loss"])
+                print("total_loss: ", losses["total_loss"])
+                generate_images(pix2pix.generator, example_input, example_target)
+            print(f"Step: {step + 1}")
+            start = time.time()
 
 
 def main():
