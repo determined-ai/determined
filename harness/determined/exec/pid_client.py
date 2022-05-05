5fab384177b4cd54f9d2aa1adca76c45c077496e
azhou-determined <83614683+azhou-determined@users.noreply.github.com>
2022-05-05 09:36:36 -0700

    fix: wrap torch.distributed launch in pid server/client  (#4077)
    * wrap torch.distributed.run in pid server/client

--- harness/determined/exec/pid_client.py
+++ harness/determined/exec/pid_client.py
@@ -6,11 +6,10 @@ from determined import ipc
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("addr")
-    parser.add_argument("cmd")
-    parser.add_argument("cmd_args", nargs="*")
+    parser.add_argument("cmd", nargs=argparse.REMAINDER)
     args = parser.parse_args()
 
     addr = ipc.read_pid_server_addr(args.addr)
 
     with ipc.PIDClient(addr) as pid_client:
-        sys.exit(pid_client.run_subprocess([args.cmd] + args.cmd_args))
+        sys.exit(pid_client.run_subprocess(args.cmd))
