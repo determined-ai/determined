ef75c09dae91eeb187cc25e6ca078e59e7e9b632
Nick Doiron <nick.doiron@hpe.com>
2022-05-06 09:53:40 -0700

    delete dependencies of experiment instead of moving

--- master/static/srv/delete_project.sql
+++ master/static/srv/delete_project.sql
@@ -12,8 +12,40 @@ proj AS (
   )
 ),
 exper AS (
-  UPDATE experiments SET project_id = 1
+  SELECT id FROM experiments
   WHERE project_id IN (SELECT id FROM proj)
+),
+o_trials AS (
+  SELECT id, task_id FROM trials
+  WHERE experiment_id IN (SELECT id FROM exper)
+),
+del_steps AS (
+  DELETE FROM raw_steps
+  WHERE trial_id IN (SELECT id FROM o_trials)
+),
+del_trials AS (
+  DELETE FROM trials
+  WHERE experiment_id IN (SELECT id FROM exper)
+),
+o_allocations AS (
+  SELECT allocation_id FROM allocations
+  WHERE task_id IN (SELECT task_id FROM o_trials)
+),
+del_task_stats AS (
+  DELETE FROM task_stats
+  WHERE allocation_id IN (SELECT allocation_id FROM o_allocations)
+),
+del_allocations AS (
+  DELETE FROM allocations
+  WHERE allocation_id IN (SELECT allocation_id FROM o_allocations)
+),
+del_tasks AS (
+  DELETE FROM tasks
+  WHERE task_id IN (SELECT task_id FROM o_trials)
+),
+del_exper AS (
+  DELETE FROM experiments
+  WHERE id IN (SELECT id FROM exper)
 )
 DELETE FROM projects
 WHERE id IN (SELECT id FROM proj)
