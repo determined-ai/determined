3366c2e92912ac54fc43ff6f914a53ea92da78f0
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-05-12 08:16:28 -0700

    bring in shared-types and utils from core.
    reviewed in https://github.com/determined-ai/determined/pull/4112
    
    Squashed commit of the following:
    
    commit 20cbf1d3c9978b6ff35ce66d987e8a700c687887
    Author: Hamid Zare <12127420+hamidzr@users.noreply.github.com>
    Date:   Tue May 10 15:12:45 2022 -0700
    
        bring in utils

---
+++ utils/color.test.ts
@@ -0,0 +1,138 @@
+import { GLASBEY } from 'shared/constants/colors';
+
+import * as utils from './color';
+
+describe('Color Utilities', () => {
+  const colors = [
+    {
+      hex: '#000000',
+      hsl: { h: 0, l: 0, s: 0 },
+      hslStr: 'hsl(0, 0%, 0%)',
+      rgb: { b: 0, g: 0, r: 0 },
+      rgbStr: 'rgb(0, 0, 0)',
+    },
+    {
+      hex: '#999999',
+      hsl: { h: 0, l: 60, s: 0 },
+      hslStr: 'hsl(0, 0%, 60%)',
+      rgb: { b: 153, g: 153, r: 153 },
+      rgbStr: 'rgb(153, 153, 153)',
+    },
+    {
+      hex: '#ffffff',
+      hsl: { h: 0, l: 100, s: 0 },
+      hslStr: 'hsl(0, 0%, 100%)',
+      rgb: { b: 255, g: 255, r: 255 },
+      rgbStr: 'rgb(255, 255, 255)',
+    },
+    {
+      hex: '#ff0000',
+      hsl: { h: 0, l: 50, s: 100 },
+      hslStr: 'hsl(0, 100%, 50%)',
+      rgb: { b: 0, g: 0, r: 255 },
+      rgbStr: 'rgb(255, 0, 0)',
+    },
+    {
+      hex: '#00ff00',
+      hsl: { h: 120, l: 50, s: 100 },
+      hslStr: 'hsl(120, 100%, 50%)',
+      rgb: { b: 0, g: 255, r: 0 },
+      rgbStr: 'rgb(0, 255, 0)',
+    },
+    {
+      hex: '#0000ff',
+      hsl: { h: 240, l: 50, s: 100 },
+      hslStr: 'hsl(240, 100%, 50%)',
+      rgb: { b: 255, g: 0, r: 0 },
+      rgbStr: 'rgb(0, 0, 255)',
+    },
+  ];
+
+  describe('glasbeyColor', () => {
+    const firstColor = 'rgb(0, 155, 222)';
+    const lastColor = 'rgb(142, 190, 88)';
+
+    it('should match first glasbey color', () => {
+      expect(utils.glasbeyColor(0)).toBe(firstColor);
+    });
+
+    it('should match last glasbey color', () => {
+      expect(utils.glasbeyColor(GLASBEY.length - 1)).toBe(lastColor);
+    });
+
+    it('should wrap around the list of glasbey colors', () => {
+      expect(utils.glasbeyColor(GLASBEY.length)).toBe(firstColor);
+    });
+  });
+
+  describe('hex2hsl', () => {
+    it('should convert all hex colors to hsl', () => {
+      colors.forEach(color => {
+        expect(utils.hex2hsl(color.hex)).toEqual(color.hsl);
+      });
+    });
+  });
+
+  describe('hex2rgb', () => {
+    it('should convert all hex colors to rgb', () => {
+      colors.forEach(color => {
+        expect(utils.hex2rgb(color.hex)).toEqual(color.rgb);
+      });
+    });
+  });
+
+  describe('hsl2str', () => {
+    it('should convert all hsl colors to hsl string', () => {
+      colors.forEach(color => {
+        expect(utils.hsl2str(color.hsl)).toEqual(color.hslStr);
+      });
+    });
+  });
+
+  describe('rgba2str', () => {
+    it('should convert all rgb colors to rgb string', () => {
+      colors.forEach(color => {
+        expect(utils.rgba2str(color.rgb)).toEqual(color.rgbStr);
+      });
+    });
+
+    it('should convert all rgba colors to rgba string', () => {
+      expect(utils.rgba2str({ a: 0.5, b: 50, g: 50, r: 50 })).toBe('rgba(50, 50, 50, 0.5)');
+      expect(utils.rgba2str({ a: 1, b: 0, g: 128, r: 255 })).toBe('rgba(255, 128, 0, 1)');
+    });
+  });
+
+  describe('rgbaFromGradient', () => {
+    it('should interpolate grey', () => {
+      const black = { b: 0, g: 0, r: 0 };
+      const white = { b: 255, g: 255, r: 255 };
+      const grey = { b: 128, g: 128, r: 128 };
+      expect(utils.rgbaFromGradient(black, white, 0.5)).toEqual(grey);
+      expect(utils.rgbaFromGradient(white, black, 0.5)).toEqual(grey);
+    });
+
+    it('should interpolate alpha', () => {
+      const black = { a: 1.0, b: 0, g: 0, r: 0 };
+      const white = { a: 0.0, b: 255, g: 255, r: 255 };
+      const grey = { a: 0.5, b: 128, g: 128, r: 128 };
+      expect(utils.rgbaFromGradient(black, white, 0.5)).toEqual(grey);
+      expect(utils.rgbaFromGradient(white, black, 0.5)).toEqual(grey);
+    });
+  });
+
+  describe('str2rgba', () => {
+    it('should convert all hex colors to rgba', () => {
+      colors.forEach(color => {
+        expect(utils.str2rgba(color.hex)).toEqual(color.rgb);
+      });
+    });
+
+    it('should convert all rgba string colors to rgba', () => {
+      expect(utils.str2rgba('rgba(255, 128, 64, 0.5)')).toEqual({ a: 0.5, b: 64, g: 128, r: 255 });
+    });
+
+    it('should handle invalid rgba string colors', () => {
+      expect(utils.str2rgba('rgba(1000, 1000, 1000, 10')).toEqual({ a: 0.0, b: 0, g: 0, r: 0 });
+    });
+  });
+});
---
+++ utils/string.ts
@@ -0,0 +1,83 @@
+import prettyBytes from 'pretty-bytes';
+
+const LETTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const CHARACTERS = `0123456789${LETTERS}`;
+
+export const DEFAULT_ALPHA_NUMERIC_LENGTH = 8;
+
+export const camelCaseToSentence = (text: string): string => {
+  const result = text.trim().replace(/([A-Z])/g, ' $1');
+  return result.charAt(0).toUpperCase() + result.slice(1);
+};
+
+export const sentenceToCamelCase = (text: string): string => {
+  const result = text.trim().split(' ').map((word, idx) => (
+    idx === 0 ? word.toLowerCase() : capitalizeWord(word)
+  ));
+  return result.join('');
+};
+
+export const capitalize = (str: string): string => {
+  return str.split(/\s+/).map(part => capitalizeWord(part)).join(' ');
+};
+
+export const capitalizeWord = (str: string): string => {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
+
+export const floatToPercent = (num: number, precision = 2): string => {
+  if (isNaN(num)) return 'NaN';
+  if (num === Infinity) return 'Infinity';
+  if (num === -Infinity) return '-Infinity';
+  return (num * 100).toFixed(precision) + '%';
+};
+
+export const generateAlphaNumeric = (
+  length = DEFAULT_ALPHA_NUMERIC_LENGTH,
+  chars = CHARACTERS,
+): string => {
+  let result = '';
+  for (let i = length; i > 0; --i) {
+    result += chars[ Math.floor(Math.random() * chars.length) ];
+  }
+  return result;
+};
+
+export const generateUUID = (): string => {
+  return [
+    generateAlphaNumeric(8),
+    generateAlphaNumeric(4),
+    generateAlphaNumeric(4),
+    generateAlphaNumeric(4),
+    generateAlphaNumeric(12),
+  ].join('-');
+};
+
+export const generateLetters = (length = DEFAULT_ALPHA_NUMERIC_LENGTH): string => {
+  return generateAlphaNumeric(length, LETTERS);
+};
+
+export const humanReadableBytes = (bytes: number): string => {
+  return prettyBytes(bytes);
+};
+
+export const listToStr = (list: (string | undefined)[], glue = ' '): string => {
+  return list.filter(item => !!item).join(glue);
+};
+
+export const toHtmlId = (str: string): string => {
+  return str
+    .replace(/[\s_]/gi, '-')
+    .replace(/[^a-z0-9-]/gi, '')
+    .toLowerCase();
+};
+
+export const truncate = (str: string, maxLength = 20, suffix = '...'): string => {
+  if (maxLength < suffix.length + 1) {
+    maxLength = suffix.length + 1;
+  }
+  if (str.length <= maxLength) {
+    return str;
+  }
+  return str.slice(0, maxLength - suffix.length) + suffix;
+};
