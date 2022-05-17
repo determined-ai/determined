3ad8c90a6ebd00dfc5d9d3d276914157c0792fe6
Maksim Kouznetsov <maksim.kouznetsov@hpe.com>
2022-05-17 16:12:58 -0700

    restore

--- harness/determined/tensorboard/util.py
+++ harness/determined/tensorboard/util.py
@@ -16,9 +16,11 @@ def find_tb_files(base_dir: pathlib.Path) -> List[pathlib.Path]:
     Recursively searches through base_dir and subdirectories to find files
     needed by Tensorboard, currently matching by filenames and extensions.
     This method is used to sync files generated during training to persistent storage.
+
     :param base_dir: starting directory path
     :return: list of filepaths within base_dir that are relevant Tensorboard files
     """
+
     if not base_dir.exists():
         logging.warning(f"{base_dir} directory does not exist.")
         return []
