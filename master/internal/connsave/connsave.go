c4092caeb1379adc826d1a61b7a193b5941f14cc
Eric Liu <eric.liu@hpe.com>
2022-05-24 15:13:43 -0700

    rename & relint

--- master/internal/connsave/connsave.go
+++ master/internal/connsave/connsave.go
@@ -7,10 +7,12 @@ import (
 
 type connKey struct{}
 
+// SaveConn saves net.Conn into the context for future retrieval.
 func SaveConn(ctx context.Context, c net.Conn) context.Context {
 	return context.WithValue(ctx, connKey{}, c)
 }
 
+// GetConn retrieves net.Conn from the context.
 func GetConn(ctx context.Context) net.Conn {
 	return ctx.Value(connKey{}).(net.Conn)
 }
