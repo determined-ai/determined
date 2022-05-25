47ea74822217f7b9b172403ba587b567d3d7a49e
Nikita <nikita.rajaneesh@hpe.com>
2022-05-25 13:13:00 +0530

    fix migrations

--- master/static/migrations/20220512160917_update-job-type-jobs.tx.down.sql
+++ master/static/migrations/20220512160917_update-job-type-jobs.tx.down.sql
@@ -6,7 +6,5 @@ CREATE TYPE public.job_type AS ENUM (
     'COMMAND',
     'TENSORBOARD'
 );
-ALTER TABLE public.jobs 
-    ALTER COLUMN job_type TYPE public.job_type
-    USING (job_type::job_type);
-DROP TYPE public._job_type;
+ALTER TABLE public.jobs ALTER COLUMN job_type TYPE public.job_type USING (job_type::text::job_type);
+DROP TYPE _job_type;
