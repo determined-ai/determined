d02dc72e2a62388ef2dcdfc31b17eeab2ab6fcb5
julian-determined-ai <julian.glover@hpe.com>
2022-05-26 11:44:43 -0500

    chore: test file space addition

--- webui/react/src/components/ThemeToggle.test.tsx
+++ webui/react/src/components/ThemeToggle.test.tsx
@@ -1,12 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
 import React from 'react';
 import { Router } from 'react-router-dom';
 
 import StoreProvider from 'contexts/Store';
 import history from 'shared/routes/history';
-
 
 import ThemeToggle from './ThemeToggle';
 
@@ -22,8 +20,9 @@ const ThemeToggleContainer: React.FC = () => {
 };
 
 const SYSTEM_MODE = 'System Mode';
-const LIGHT_MODE = 'Light Mode'
-const DARK_MODE = 'Dark Mode'
+const LIGHT_MODE = 'Light Mode';
+const DARK_MODE = 'Dark Mode';
+
 const setup = () => render(<ThemeToggleContainer />);
 
 describe('ThemeToggle', () => {
@@ -34,19 +33,19 @@ describe('ThemeToggle', () => {
 
   it('Light Mode is activated after System Mode', async () => {
     await setup();
-    userEvent.click(screen.getByText(SYSTEM_MODE))
+    userEvent.click(screen.getByText(SYSTEM_MODE));
     expect(await screen.findByText(LIGHT_MODE)).toBeInTheDocument();
   });
 
   it('Dark Mode is activated after Light Mode', async () => {
     await setup();
-    userEvent.click(screen.getByText(LIGHT_MODE))
+    userEvent.click(screen.getByText(LIGHT_MODE));
     expect(await screen.findByText(DARK_MODE)).toBeInTheDocument();
   });
 
   it('System Mode is activated after Dark Mode', async () => {
     await setup();
-    userEvent.click(screen.getByText(DARK_MODE))
+    userEvent.click(screen.getByText(DARK_MODE));
     expect(await screen.findByText(SYSTEM_MODE)).toBeInTheDocument();
   });
 
