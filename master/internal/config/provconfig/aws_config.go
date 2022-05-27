dbae9ba0cc4cfb656016f96c82f4dcd8d6468136
Ryan <rb@hpe.com>
2022-05-27 11:46:42 -0600

    apply bumpenvs update

--- master/internal/config/provconfig/aws_config.go
+++ master/internal/config/provconfig/aws_config.go
@@ -49,16 +49,16 @@ type AWSClusterConfig struct {
 }
 
 var defaultAWSImageID = map[string]string{
-	"ap-northeast-1": "ami-0e51573e00fb73fad",
-	"ap-northeast-2": "ami-0fd06d3bdb9e89fba",
-	"ap-southeast-1": "ami-0e284df9fbbc17157",
-	"ap-southeast-2": "ami-0102c15eb8e9d1056",
-	"us-east-2":      "ami-0f54a7ddadc097262",
-	"us-east-1":      "ami-0b02aaf6e4087c8fd",
-	"us-west-2":      "ami-091a8a0157d603e0f",
-	"eu-central-1":   "ami-05f4c27d26768d7bb",
-	"eu-west-2":      "ami-080c06cec1af6118e",
-	"eu-west-1":      "ami-03a1febbba7c934e7",
+	"ap-northeast-1": "ami-01f8604bbcc0c29a5",
+	"ap-northeast-2": "ami-0023427de584a10cc",
+	"ap-southeast-1": "ami-013837086f63264ce",
+	"ap-southeast-2": "ami-0aa534ed0641f1fcf",
+	"us-east-2":      "ami-0cf270fdbecdf69b9",
+	"us-east-1":      "ami-0f2974f1a8588e274",
+	"us-west-2":      "ami-0d98e6e0d954414fc",
+	"eu-central-1":   "ami-0c91840793cffac21",
+	"eu-west-2":      "ami-0792502919ac00e0d",
+	"eu-west-1":      "ami-0f54a840ac094c1e8",
 }
 
 var defaultAWSClusterConfig = AWSClusterConfig{
