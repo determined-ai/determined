c02035625b53a442430c8c1567b9ed5bd41c2dee
Nick Doiron <nick.doiron@hpe.com>
2022-05-31 05:12:47 +0300

    py lint, list comprehension

--- e2e_tests/tests/cluster/test_workspace_org.py
+++ e2e_tests/tests/cluster/test_workspace_org.py
@@ -83,21 +83,15 @@ def test_workspace_org() -> None:
         ).workspace
         test_workspaces.append(workspace2)
         list_test_1 = bindings.get_GetWorkspaces(sess).workspaces
-        assert ["Uncategorized", "_TestPatched", "_TestWS"] == list(
-            map(lambda w: w.name, list_test_1)
-        )
+        assert ["Uncategorized", "_TestPatched", "_TestWS"] == [w.name for w in list_test_1]
         list_test_2 = bindings.get_GetWorkspaces(
             sess, orderBy=bindings.v1OrderBy.ORDER_BY_DESC
         ).workspaces
-        assert ["_TestWS", "_TestPatched", "Uncategorized"] == list(
-            map(lambda w: w.name, list_test_2)
-        )
+        assert ["_TestWS", "_TestPatched", "Uncategorized"] == [w.name for w in list_test_2]
         list_test_3 = bindings.get_GetWorkspaces(
             sess, sortBy=bindings.v1GetWorkspacesRequestSortBy.SORT_BY_NAME
         ).workspaces
-        assert ["_TestPatched", "_TestWS", "Uncategorized"] == list(
-            map(lambda w: w.name, list_test_3)
-        )
+        assert ["_TestPatched", "_TestWS", "Uncategorized"] == [w.name for w in list_test_3]
 
         # Test pinned workspaces.
         pinned = bindings.get_GetWorkspaces(
@@ -211,23 +205,17 @@ def test_workspace_org() -> None:
         ).project
         test_projects += [p1, p2]
         list_test_4 = bindings.get_GetWorkspaceProjects(sess, id=made_workspace.id).projects
-        assert ["_TestPatchedProject", "_TestPRJ", "_TestEarly"] == list(
-            map(lambda w: w.name, list_test_4)
-        )
+        assert ["_TestPatchedProject", "_TestPRJ", "_TestEarly"] == [p.name for p in list_test_4]
         list_test_5 = bindings.get_GetWorkspaceProjects(
             sess, id=made_workspace.id, orderBy=bindings.v1OrderBy.ORDER_BY_DESC
         ).projects
-        assert ["_TestEarly", "_TestPRJ", "_TestPatchedProject"] == list(
-            map(lambda w: w.name, list_test_5)
-        )
+        assert ["_TestEarly", "_TestPRJ", "_TestPatchedProject"] == [p.name for p in list_test_5]
         list_test_6 = bindings.get_GetWorkspaceProjects(
             sess,
             id=made_workspace.id,
             sortBy=bindings.v1GetWorkspaceProjectsRequestSortBy.SORT_BY_NAME,
         ).projects
-        assert ["_TestEarly", "_TestPatchedProject", "_TestPRJ"] == list(
-            map(lambda w: w.name, list_test_6)
-        )
+        assert ["_TestEarly", "_TestPatchedProject", "_TestPRJ"] == [p.name for p in list_test_6]
 
         # Move a project to another workspace
         bindings.post_MoveProject(
