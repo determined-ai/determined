33b238853ae81e633b1b696adfe009414ebee211
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-06-06 09:46:22 -0700

    test: link circleci to run integration tests with core repo (#8)
    add option to the tester to select which integration tests to run

--- bin/test.py
+++ bin/test.py
@@ -1,17 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import subprocess
-import typing
-import sys
+import typing as t
 import pathlib
+import argparse
+
+parser = argparse.ArgumentParser()
 
 SAAS_REPO='https://github.com/determined-ai/saas'
 CORE_REPO='https://github.com/determined-ai/determined'
 SM_DIR = 'src/shared'
 
-users = [
-    {'name': 'saas', 'repo': SAAS_REPO, 'web_dir': 'web'},
-    {'name': 'core', 'repo': CORE_REPO, 'web_dir': 'webui/react'},
-]
+repos = {
+    'saas': {
+        'repo': SAAS_REPO,
+        'web_dir': 'web',
+    },
+    'core': {
+        'repo': CORE_REPO,
+        'web_dir': 'webui/react',
+    },
+}
 
 
 # Python program to print
@@ -19,7 +27,7 @@ users = [
 def print_purple(skk): print("\033[93m {}\033[00m" .format(skk))
 
 
-def run(command, cwd: typing.Optional[pathlib.Path] = None):
+def run(command, cwd: t.Optional[pathlib.Path] = None):
     print_purple(f'{command}')
     subprocess.run(command, cwd=cwd, check=True, shell=True)
 
@@ -47,24 +55,32 @@ def get_current_hash():
     return get_output('git rev-parse HEAD')
 
 
-def setup_user(user, sm_hash: str):
-    clone_dir = '/tmp' / pathlib.Path(user['name'])
+def setup_user(user, name: str, sm_hash: str):
+    clone_dir = '/tmp' / pathlib.Path(name)
     web_dir = clone_dir / user['web_dir']
     run(f'rm -rf {clone_dir}; git clone {user["repo"]} {clone_dir} --recurse-submodules')
     run(f'git checkout master && git pull', cwd=clone_dir)
     run(f'git checkout {sm_hash}', cwd=web_dir/SM_DIR)
 
 
-def test(sm_hash: str):
-    for user in users:
-        setup_user(user, sm_hash)
-        clone_dir = '/tmp' / pathlib.Path(user['name'])
+def test(sm_hash: str, repo_names: t.List[str]):
+    for name in repo_names:
+        if name not in repos:
+            raise ValueError(f'unknown repo name: {name}')
+        user = repos[name]
+        setup_user(user, name, sm_hash)
+        clone_dir = '/tmp' / pathlib.Path(name)
         web_dir = clone_dir / user['web_dir']
         for target in ['get-deps', 'check', 'build', 'test']:
             run(f'make {target}', cwd=web_dir)
 
 
 if __name__ == '__main__':
+    parser.add_argument('--git-hash', help='desired shared-web githash to test')
+    parser.add_argument('--repos',
+                        help=f'repos to test. available: {", ".join(repos.keys())}',
+                        nargs='+')
+    args = parser.parse_args()
     # get current git hash from cli args as first argument through sys.args if one is provided
-    sm_hash = sys.argv[1] if len(sys.argv) > 1 else get_current_hash()
-    test(sm_hash)
+    sm_hash = args.git_hash if args.git_hash else get_current_hash()
+    test(sm_hash, args.repos or repos.keys())
