determined-ai/determined

commit f103ab8189d4ed8d2fd2e9f7b2b122b8d8b57faa

chore: stop hardcoding python3.6 [DET-4929] [DET-4539] (#1967)
chore: stop hardcoding python3.6

Support setting the DET_PYTHON_EXECUTABLE as an escape hatch for very
strange custom environments, but since it seems unlikely many users
ever need it, document it through error messages rather than in docs.

Notably, installing python3.6 from the deadsnakes PPA used to result
in such an environment, but we have used deadsnakes in our images
for a long time.

Also support startup-hook.sh in notebooks and shells while we're at it.

master/static/srv/entrypoint.sh
@@ -29,6 +29,15 @@ set -x
 WORKING_DIR="/run/determined/workdir"
 STARTUP_HOOK="startup-hook.sh"
 export PATH="/run/determined/pythonuserbase/bin:$PATH"
+if [ -z "$DET_PYTHON_EXECUTABLE" ] ; then
+    export DET_PYTHON_EXECUTABLE="python3"
+fi
+if ! /bin/which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
+    echo "error: unable to find python3 as \"$DET_PYTHON_EXECUTABLE\"" >&2
+    echo "please install python3 or set the environment variable DET_PYTHON_EXECUTABLE=/path/to/python3" >&2
+    exit 1
+fi
+
 
 # If HOME is not explicitly set for a container, libcontainer (Docker) will
 # try to guess it by reading /etc/password directly, which will not work with
@@ -41,7 +50,7 @@ if [ "$HOME" = "/" ] ; then
     export HOME
 fi
 
-python3.6 -m pip install -q --user /opt/determined/wheels/determined*.whl
+"$DET_PYTHON_EXECUTABLE" -m pip install -q --user /opt/determined/wheels/determined*.whl
 
 cd ${WORKING_DIR} && test -f "${STARTUP_HOOK}" && source "${STARTUP_HOOK}"
-exec python3.6 -m determined.exec.harness "$@"
+exec "$DET_PYTHON_EXECUTABLE" -m determined.exec.harness "$@"

master/static/srv/gc-checkpoints-entrypoint.sh
@@ -3,7 +3,16 @@
 set -e
 
 export PATH="/run/determined/pythonuserbase/bin:$PATH"
+if [ -z "$DET_PYTHON_EXECUTABLE" ] ; then
+    export DET_PYTHON_EXECUTABLE="python3"
+fi
+if ! /bin/which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
+    echo "error: unable to find python3 as \"$DET_PYTHON_EXECUTABLE\"" >&2
+    echo "please install python3 or set the environment variable DET_PYTHON_EXECUTABLE=/path/to/python3" >&2
+    exit 1
+fi
 
-python3.6 -m pip install -q --user /opt/determined/wheels/determined*.whl
 
-exec python3.6 -m determined.exec.gc_checkpoints "$@"
+"$DET_PYTHON_EXECUTABLE" -m pip install -q --user /opt/determined/wheels/determined*.whl
+
+exec "$DET_PYTHON_EXECUTABLE" -m determined.exec.gc_checkpoints "$@"

master/static/srv/notebook-entrypoint.sh
@@ -2,7 +2,17 @@
 
 set -e
 
+WORKING_DIR="/run/determined/workdir"
+STARTUP_HOOK="startup-hook.sh"
 export PATH="/run/determined/pythonuserbase/bin:$PATH"
+if [ -z "$DET_PYTHON_EXECUTABLE" ] ; then
+    export DET_PYTHON_EXECUTABLE="python3"
+fi
+if ! /bin/which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
+    echo "error: unable to find python3 as \"$DET_PYTHON_EXECUTABLE\"" >&2
+    echo "please install python3 or set the environment variable DET_PYTHON_EXECUTABLE=/path/to/python3" >&2
+    exit 1
+fi
 
 # If HOME is not explicitly set for a container, libcontainer (Docker) will
 # try to guess it by reading /etc/password directly, which will not work with
@@ -15,6 +25,7 @@ if [ "$HOME" = "/" ] ; then
     export HOME
 fi
 
-python3.6 -m pip install -q --user /opt/determined/wheels/determined*.whl
+"$DET_PYTHON_EXECUTABLE" -m pip install -q --user /opt/determined/wheels/determined*.whl
 
-jupyter lab --config /run/determined/workdir/jupyter-conf.py --port=${NOTEBOOK_PORT}
+pushd ${WORKING_DIR} && test -f "${STARTUP_HOOK}" && source "${STARTUP_HOOK}" && popd
+exec jupyter lab --config /run/determined/workdir/jupyter-conf.py --port=${NOTEBOOK_PORT}

master/static/srv/shell-entrypoint.sh
@@ -2,13 +2,25 @@
 
 set -e
 
+WORKING_DIR="/run/determined/workdir"
+STARTUP_HOOK="startup-hook.sh"
 export PATH="/run/determined/pythonuserbase/bin:$PATH"
+if [ -z "$DET_PYTHON_EXECUTABLE" ] ; then
+    export DET_PYTHON_EXECUTABLE="python3"
+fi
+if ! /bin/which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
+    echo "error: unable to find python3 as \"$DET_PYTHON_EXECUTABLE\"" >&2
+    echo "please install python3 or set the environment variable DET_PYTHON_EXECUTABLE=/path/to/python3" >&2
+    exit 1
+fi
 
 # Unlike trial and notebook entrypoints, the HOME directory does not need to be
 # modified in this entrypoint because the HOME in the user's ssh session is set
 # by sshd at a later time.
 
-python3.6 -m pip install -q --user /opt/determined/wheels/determined*.whl
+"$DET_PYTHON_EXECUTABLE" -m pip install -q --user /opt/determined/wheels/determined*.whl
+
+pushd ${WORKING_DIR} && test -f "${STARTUP_HOOK}" && source "${STARTUP_HOOK}" && popd
 
 # Prepend each key in authorized_keys with a set of environment="KEY=VALUE"
 # options to inject the entire docker environment into the eventual ssh

master/static/srv/tensorboard-entrypoint.sh
@@ -6,9 +6,17 @@ set -x
 WORKING_DIR="/run/determined/workdir"
 STARTUP_HOOK="startup-hook.sh"
 export PATH="/run/determined/pythonuserbase/bin:$PATH"
+if [ -z "$DET_PYTHON_EXECUTABLE" ] ; then
+    export DET_PYTHON_EXECUTABLE="python3"
+fi
+if ! /bin/which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
+    echo "error: unable to find python3 as \"$DET_PYTHON_EXECUTABLE\"" >&2
+    echo "please install python3 or set the environment variable DET_PYTHON_EXECUTABLE=/path/to/python3" >&2
+    exit 1
+fi
 
-python3.6 -m pip install -q --user /opt/determined/wheels/determined*.whl
+"$DET_PYTHON_EXECUTABLE" -m pip install -q --user /opt/determined/wheels/determined*.whl
 
 cd ${WORKING_DIR} && test -f "${STARTUP_HOOK}" && source "${STARTUP_HOOK}"
 
-python3.6 -m determined.exec.tensorboard "$@"
+exec "$DET_PYTHON_EXECUTABLE" -m determined.exec.tensorboard "$@"

.circleci/config.yml
@@ -200,7 +200,7 @@ commands:
       - run:
           name: Setup venv
           command: |
-            python3.6 -m venv /tmp/venv
+            python3 -m venv /tmp/venv
             echo "export PATH=/tmp/venv/bin:\"${PATH}\"" >> $BASH_ENV
             /tmp/venv/bin/python -m pip install --upgrade pip\<20 wheel setuptools
 

e2e_tests/tests/command/test_notebook.py
@@ -1,4 +1,5 @@
 import re
+from typing import List
 
 import pytest
 
@@ -8,9 +9,11 @@ from tests import command as cmd
 @pytest.mark.slow  # type: ignore
 @pytest.mark.e2e_cpu  # type: ignore
 def test_basic_notebook_start_and_kill() -> None:
+    lines = []  # type: List[str]
     with cmd.interactive_command("notebook", "start") as notebook:
         for line in notebook.stdout:
             if re.search("Jupyter Notebook .*is running at", line) is not None:
                 return
+            lines.append(line)
 
-    raise AssertionError()
+    raise ValueError(lines)

e2e_tests/tests/fixtures/dockerfiles/py3.6-custom-workdir.Dockerfile
@@ -1,4 +1,0 @@
-FROM python:3.6.9-stretch
-MAINTAINER Determined AI <hello@determined.ai>
-
-WORKDIR /custom/workdir

commit 9f525a77076ec2bd05ccd1399633dc23dce4999e

fix: don't hardcode /bin/which in entrypoints
This was my bug, introduced in #1967.

I have no idea why I wrote it that way.

master/static/srv/entrypoint.sh
@@ -10,7 +10,7 @@ export PATH="/run/determined/pythonuserbase/bin:$PATH"
 if [ -z "$DET_PYTHON_EXECUTABLE" ] ; then
     export DET_PYTHON_EXECUTABLE="python3"
 fi
-if ! /bin/which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
+if ! which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
     echo "error: unable to find python3 as \"$DET_PYTHON_EXECUTABLE\"" >&2
     echo "please install python3 or set the environment variable DET_PYTHON_EXECUTABLE=/path/to/python3" >&2
     exit 1

master/static/srv/gc-checkpoints-entrypoint.sh
@@ -9,7 +9,7 @@ export PATH="/run/determined/pythonuserbase/bin:$PATH"
 if [ -z "$DET_PYTHON_EXECUTABLE" ] ; then
     export DET_PYTHON_EXECUTABLE="python3"
 fi
-if ! /bin/which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
+if ! which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
     echo "error: unable to find python3 as \"$DET_PYTHON_EXECUTABLE\"" >&2
     echo "please install python3 or set the environment variable DET_PYTHON_EXECUTABLE=/path/to/python3" >&2
     exit 1

master/static/srv/notebook-entrypoint.sh
@@ -10,7 +10,7 @@ export PATH="/run/determined/pythonuserbase/bin:$PATH"
 if [ -z "$DET_PYTHON_EXECUTABLE" ] ; then
     export DET_PYTHON_EXECUTABLE="python3"
 fi
-if ! /bin/which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
+if ! which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
     echo "error: unable to find python3 as \"$DET_PYTHON_EXECUTABLE\"" >&2
     echo "please install python3 or set the environment variable DET_PYTHON_EXECUTABLE=/path/to/python3" >&2
     exit 1

master/static/srv/shell-entrypoint.sh
@@ -10,7 +10,7 @@ export PATH="/run/determined/pythonuserbase/bin:$PATH"
 if [ -z "$DET_PYTHON_EXECUTABLE" ] ; then
     export DET_PYTHON_EXECUTABLE="python3"
 fi
-if ! /bin/which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
+if ! which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
     echo "error: unable to find python3 as \"$DET_PYTHON_EXECUTABLE\"" >&2
     echo "please install python3 or set the environment variable DET_PYTHON_EXECUTABLE=/path/to/python3" >&2
     exit 1

master/static/srv/tensorboard-entrypoint.sh
@@ -10,7 +10,7 @@ export PATH="/run/determined/pythonuserbase/bin:$PATH"
 if [ -z "$DET_PYTHON_EXECUTABLE" ] ; then
     export DET_PYTHON_EXECUTABLE="python3"
 fi
-if ! /bin/which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
+if ! which "$DET_PYTHON_EXECUTABLE" >/dev/null 2>&1 ; then
     echo "error: unable to find python3 as \"$DET_PYTHON_EXECUTABLE\"" >&2
     echo "please install python3 or set the environment variable DET_PYTHON_EXECUTABLE=/path/to/python3" >&2
     exit 1