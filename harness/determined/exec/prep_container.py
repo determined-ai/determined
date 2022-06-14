5d345c5a436a359c0f7e6cc6c5df630b94ba32a3
Daniel R Hunter <daniel.robert.hunter@hpe.com>
2022-06-14 16:53:02 -0400

    Configure logging in prep_container.py (affects logging in gpu.py)

--- harness/determined/exec/prep_container.py
+++ harness/determined/exec/prep_container.py
@@ -233,7 +233,18 @@ if __name__ == "__main__":
     parser.add_argument("--resources", action="store_true")
     parser.add_argument("--rendezvous", action="store_true")
     parser.add_argument("--proxy", action="store_true")
+    parser.add_argument(
+        "--log-level",
+        default=os.getenv("DET_LOG_LEVEL", "INFO"),
+        choices=["DEBUG", "INFO", "WARNING", "ERROR"],
+        help="Set the logging level",
+    )
     args = parser.parse_args()
+
+    logging.basicConfig(
+        level=args.log_level, format=det.LOG_FORMAT,
+    )
+    logging.debug("running prep_container")
 
     # Avoid reading det.get_cluster_info(), which might (wrongly) set a singleton to None.
     info = det.ClusterInfo._from_file()
