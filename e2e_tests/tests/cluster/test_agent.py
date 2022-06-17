86c51b7c902272a4774ab7c22eabe6bfa1ac915e
Eric Liu <eric.liu@hpe.com>
2022-06-16 17:04:34 -0700

    fix e2e test permissions

--- e2e_tests/tests/cluster/test_agent.py
+++ e2e_tests/tests/cluster/test_agent.py
@@ -6,6 +6,8 @@ import determined
 from determined.common import api
 from tests import config as conf
 
+from .test_users import ADMIN_CREDENTIALS, logged_in_user
+
 
 # TODO: This should be marked as a cross-version test, but it can't actually be at the time of
 # writing, since older agent versions don't report their versions.
@@ -14,6 +16,7 @@ def test_agent_version() -> None:
     # DET_AGENT_VERSION is available and specifies the agent version in cross-version tests; for
     # other tests, this evaluates to the current version.
     target_version = os.environ.get("DET_AGENT_VERSION") or determined.__version__
-    agents = api.get(conf.make_master_url(), "agents").json()
+    with logged_in_user(ADMIN_CREDENTIALS):
+        agents = api.get(conf.make_master_url(), "agents").json()
 
     assert all(agent["version"] == target_version for agent in agents.values())
