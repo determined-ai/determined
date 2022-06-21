3d6f9222e1911e4f67deb4de05c70808d3dfe0d5
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-06-21 14:43:28 -0700

    wip styles

--- webui/react/craco.config.js
+++ webui/react/craco.config.js
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+const AntDesignThemePlugin = require('antd-theme-webpack-plugin');
 const { DefinePlugin } = require('webpack');
 
 const config = require('./src/shared/configs/craco.config');
@@ -7,6 +10,20 @@ const webpackEnvPlugin = new DefinePlugin({
   'process.env.IS_DEV': JSON.stringify(config.isDev),
   'process.env.SERVER_ADDRESS': JSON.stringify(process.env.SERVER_ADDRESS),
   'process.env.VERSION': '"0.18.3-dev0"',
+});
+
+/**
+ * Add theme override support for antd. For more options:
+ * https://github.com/mzohaibqc/antd-theme-webpack-plugin
+*/
+const antdPlugin = new AntDesignThemePlugin({
+  antDir: path.join(__dirname, './node_modules/antd'),
+  indexFileName: 'index.html',
+  mainLessFile: path.join(__dirname, './src/shared/styles/index.less'),
+  publicPath: process.env.PUBLIC_URL,
+  stylesDir: path.join(__dirname, './src/shared/styles'),
+  themeVariables: [ '@primary-color' ],
+  varFile: path.join(__dirname, './src/shared/styles/variables.less'),
 });
 
 module.exports = {
@@ -28,6 +45,7 @@ module.exports = {
     plugins: [
       ...config.webpack.plugins,
       webpackEnvPlugin,
+      antdPlugin,
     ],
   },
 };
