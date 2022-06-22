c524c4cd7369bd27179b036dd57d804568233d3f
Corey Staten <corey.staten@hpe.com>
2022-06-22 19:50:53 -0400

    chore: remove outdated mmdetection example

--- examples/computer_vision/mmdetection_pytorch/startup-hook.sh
+++
@@ -1,7 +0,0 @@
-apt-get update
-apt-get install unzip
-
-wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
-unzip annotations_trainval2017.zip 
-mv annotations/instances_train2017.json /tmp
-mv annotations/instances_val2017.json /tmp
