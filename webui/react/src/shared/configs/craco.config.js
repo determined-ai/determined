fbca6fdc4fa6b1ea5dd873e3b10e394d41e62e18
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-06-23 13:16:39 -0700

    bring in shared-web changes via a cp
    dc2b38fc3c03724d9222093884312d45bf2aca6d

---
+++ webui/react/src/shared/configs/craco.config.js
@@ -0,0 +1,99 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require('path');
+
+const { when } = require('@craco/craco');
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
+const CracoLessPlugin = require('craco-less');
+const CracoSassResoucesPlugin = require('craco-sass-resources-loader');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+const IS_DEV = process.env.DET_NODE_ENV === 'development';
+
+module.exports = {
+  babel: {
+    plugins: [
+      [
+        'import', {
+          libraryDirectory: 'es',
+          libraryName: 'antd',
+          style: true,
+        },
+      ],
+    ],
+  },
+  eslint: { enable: false },
+  /** custom development flag */
+  isDev: IS_DEV,
+  plugins: [
+    {
+      options: { lessLoaderOptions: { lessOptions: { javascriptEnabled: true } } },
+      plugin: CracoLessPlugin,
+    },
+    {
+      options: { resources: path.join(__dirname, '../styles/global.scss') },
+      plugin: CracoSassResoucesPlugin,
+    },
+  ],
+  webpack: {
+    // Skip webpack v4 optimizations when development environment to lower build time.
+    ...when(
+      IS_DEV,
+      () => ({
+        configure: {
+          optimization: {
+            concatenateModules: false,
+            flagIncludedChunks: false,
+            mergeDuplicateChunks: false,
+            minimize: false,
+            namedChunks: false,
+            namedModules: false,
+            occurrenceOrder: false,
+            providedExports: false,
+            removeAvailableModules: false,
+            removeEmptyChunks: false,
+            sideEffects: false,
+            usedExports: false,
+          },
+        },
+      }),
+      {},
+    ),
+    plugins: [
+      /*
+       * Replace momentjs with dayjs to reduce antd package size.
+       * https://github.com/ant-design/antd-dayjs-webpack-plugin
+       */
+      new AntdDayjsWebpackPlugin({
+        plugins: [
+          'isSameOrBefore',
+          'isSameOrAfter',
+          'advancedFormat',
+          'customParseFormat',
+          'weekday',
+          'weekYear',
+          'weekOfYear',
+          'isMoment',
+          'localeData',
+          'localizedFormat',
+        ],
+        replaceMoment: true,
+      }),
+      /* Available MonacoWebpackPlugin options are documented at:
+       * https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+       */
+      new MonacoWebpackPlugin({
+        features: [
+          'codelens',
+          'colorDetector',
+          'find',
+          'parameterHints',
+          'quickOutline',
+          'suggest',
+          'wordHighlighter',
+        ],
+        languages: [ 'markdown', 'yaml' ],
+      }),
+    ],
+  },
+};
