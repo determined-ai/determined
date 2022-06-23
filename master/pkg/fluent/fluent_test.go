e6d2f7a8440931b510e98381f436326945f115d6
Nicholas Blaskey <nick.blaskey@hpe.com>
2022-06-23 11:30:07 -0400

    Docs

---
+++ master/pkg/fluent/fluent_test.go
@@ -0,0 +1,45 @@
+package fluent
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/determined-ai/determined/master/pkg/model"
+	"github.com/determined-ai/determined/master/pkg/ptrs"
+)
+
+func TestAdditionalOutputConfig(t *testing.T) {
+	additionalFluentOutputs := `
+[OUTPUT]
+  Name file
+  File test.log
+  Match *
+  Path /run/determined/fluent
+`
+	loggingConfigs := []model.LoggingConfig{
+		model.LoggingConfig{
+			DefaultLoggingConfig:    &model.DefaultLoggingConfig{},
+			AdditionalFluentOutputs: ptrs.Ptr(additionalFluentOutputs),
+		},
+		model.LoggingConfig{
+			ElasticLoggingConfig: &model.ElasticLoggingConfig{
+				Host: "test",
+				Port: 9200,
+				Security: model.ElasticSecurityConfig{
+					Username: ptrs.Ptr("username"),
+					Password: ptrs.Ptr("password"),
+				},
+			},
+			AdditionalFluentOutputs: ptrs.Ptr(additionalFluentOutputs),
+		},
+	}
+
+	for _, loggingConfig := range loggingConfigs {
+		c := &strings.Builder{}
+		makeOutputConfig(c, nil, "127.0.0.1", 8080, loggingConfig, model.TLSClientConfig{})
+		require.Contains(t, c.String(), additionalFluentOutputs,
+			"outputs did not contain additional fluent options")
+	}
+}
