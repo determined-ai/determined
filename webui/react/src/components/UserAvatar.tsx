a9212ecd76a4fd54befd673ea64bd2070c3d8deb
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-06-29 16:51:50 -0700

    chore: remove name prop from UserAvatar.
    that's displayed and supported in Avatar stories

--- webui/react/src/components/UserAvatar.tsx
+++ webui/react/src/components/UserAvatar.tsx
@@ -6,27 +6,23 @@ import Avatar, { Props } from 'shared/components/Avatar';
 import { getDisplayName } from 'utils/user';
 
 const UserAvatar: React.FC<{
-  name?: string;
   userId?: number;
-} & Omit<Props, 'displayName'>> = ({ name, userId, ...rest }) => {
+} & Omit<Props, 'displayName'>> = ({ userId, ...rest }) => {
 
   const [ displayName, setDisplayName ] = useState('');
   const { users } = useStore();
   const fetchUsers = useFetchUsers(new AbortController());
 
   useEffect(() => {
-    if (!name && userId) {
-      if (!users.length) {
-        fetchUsers();
-      }
-      const user = users.find(user => user.id === userId);
-      setDisplayName(getDisplayName(user));
-    } else if (name) {
-      setDisplayName(name);
+    if (!userId) return;
+    if (!users.length) {
+      fetchUsers();
     }
-  }, [ fetchUsers, userId, name, users ]);
+    const user = users.find(user => user.id === userId);
+    setDisplayName(getDisplayName(user));
+  }, [ fetchUsers, userId, users ]);
 
-  return <Avatar {...rest} displayName={name || displayName} />;
+  return <Avatar {...rest} displayName={displayName} />;
 };
 
 export default UserAvatar;
