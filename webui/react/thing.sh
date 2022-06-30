dba079b269fde8a7c0f849b94a647f3178d8431e
Trent Watson <trentwatson1@gmail.com>
2022-06-30 08:40:18 -0700

    test-

--- webui/react/thing.sh
+++ webui/react/thing.sh
@@ -1,10 +1,9 @@
-#!/bin/sh  
-while true  
-:Q
+#!/bin/zsh  
+
 for i in 1 2 3 4 5 6 7 8 9 10 11 12
 do
   touch "tempor$i"
-  gac 'no-op'
+  git add . && git commit -m "test-$1"
   sleep 300000
 done
 
