bbd576588395cbc30c6305d35169daa9afff72cf
julian-determined-ai <julian.glover@hpe.com>
2022-07-11 13:27:47 -0500

    fixed validaiton params and added non functional training and h parms

--- webui/react/src/services/api-ts-sdk/api.ts
+++ webui/react/src/services/api-ts-sdk/api.ts
@@ -5032,6 +5032,12 @@ export interface V1QueryFilters {
      * @memberof V1QueryFilters
      */
     trainingMetrics?: Array<V1NumberRangeFilter>;
+    /**
+     * 
+     * @type {Array<V1NumberRangeFilter>}
+     * @memberof V1QueryFilters
+     */
+    hparams?: Array<V1NumberRangeFilter>;
 }
 
 /**
