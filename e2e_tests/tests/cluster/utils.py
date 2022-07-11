3e25234f32b8f635bdaeb2e0e7db773d78612fdc
Nikita <nikita.rajaneesh@hpe.com>
2022-07-11 16:45:30 -0500

    fix port

--- e2e_tests/tests/cluster/utils.py
+++ e2e_tests/tests/cluster/utils.py
@@ -30,7 +30,9 @@ def get_master_port(loaded_config: dict) -> str:
     for d in loaded_config["stages"]:
         for k in d.keys():
             if k == "master":
-                return str(d["master"]["config_file"]["port"])
+                if "port" in d["master"]["config_file"]:
+                    return str(d["master"]["config_file"]["port"])
+                    
     return "8080"  # default value if not explicit in config file
 
 
