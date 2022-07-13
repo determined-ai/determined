e310a6d257a41abcef3e33f816f30e4e53fb3cd8
Caleb Kang <caleb@determined.ai>
2022-07-13 12:23:21 -0600

    chore: keep settings tab in stealth mode

--- webui/react/src/pages/Settings.tsx
+++ webui/react/src/pages/Settings.tsx
@@ -1,34 +1,37 @@
-import { Tabs } from 'antd';
-import React, { useCallback, useState } from 'react';
-import { useHistory, useParams } from 'react-router';
+// import { Tabs } from 'antd';
+import React from 'react';
+// import React, { useCallback, useState } from 'react';
+// import { useHistory, useParams } from 'react-router';
 
 import Page from 'components/Page';
 import SettingsAccount from 'pages/Settings/SettingsAccount';
-import { paths } from 'routes/utils';
+// import { paths } from 'routes/utils';
 
-const { TabPane } = Tabs;
+// import css from './Settings.module.scss';
 
-enum TabType {
-  Account = 'account',
-  UserManagement = 'user-management',
-}
+// const { TabPane } = Tabs;
+//
+// enum TabType {
+//   Account = 'account',
+//   UserManagement = 'user-management',
+// }
 
-interface Params {
-  tab?: TabType;
-}
+// interface Params {
+//   tab?: TabType;
+// }
 
-const DEFAULT_TAB_KEY = TabType.Account;
+// const DEFAULT_TAB_KEY = TabType.Account;
 
 const Settings: React.FC = () => {
-  const { tab } = useParams<Params>();
-  const [ tabKey, setTabKey ] = useState<TabType>(tab || DEFAULT_TAB_KEY);
-  const basePath = paths.settings();
-  const history = useHistory();
+  // const { tab } = useParams<Params>();
+  // const [ tabKey, setTabKey ] = useState<TabType>(tab || DEFAULT_TAB_KEY);
+  // const basePath = paths.settings();
+  // const history = useHistory();
 
-  const handleTabChange = useCallback(key => {
-    setTabKey(key);
-    history.replace(key === DEFAULT_TAB_KEY ? basePath : `${basePath}/${key}`);
-  }, [ basePath, history ]);
+  // const handleTabChange = useCallback(key => {
+  //   setTabKey(key);
+  //   history.replace(key === DEFAULT_TAB_KEY ? basePath : `${basePath}/${key}`);
+  // }, [ basePath, history ]);
 
   return (
     <Page
@@ -36,14 +39,14 @@ const Settings: React.FC = () => {
       id="cluster"
       stickyHeader
       title="Settings">
-      <Tabs className="no-padding" defaultActiveKey={tabKey} onChange={handleTabChange}>
-        <TabPane key="account" tab="Account">
-          <SettingsAccount />
-        </TabPane>
-        <TabPane key="userManagement" tab="User Management">
-          User Management
-        </TabPane>
-      </Tabs>
+      {/*<Tabs className="no-padding" defaultActiveKey={tabKey} onChange={handleTabChange}>*/}
+      {/*  <TabPane key="account" tab="Account">*/}
+      <SettingsAccount />
+      {/*  </TabPane>*/}
+      {/*  <TabPane key="userManagement" tab="User Management">*/}
+      {/*    User Management*/}
+      {/*  </TabPane>*/}
+      {/*</Tabs>*/}
     </Page>
   );
 };
