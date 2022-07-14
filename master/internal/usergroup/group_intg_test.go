135094a1724946bb78669b2da2ac1b8cd3f4e11f
Max Russell <max.russell@hpe.com>
2022-07-13 17:59:07 -0700

    Moved user group code into master/internal/usergroup

--- master/internal/usergroup/group_intg_test.go
+++ master/internal/usergroup/group_intg_test.go
@@ -1,7 +1,7 @@
 //go:build integration
 // +build integration
 
-package db
+package usergroup
 
 import (
 	"context"
@@ -10,24 +10,25 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/determined-ai/determined/master/internal/db"
 	"github.com/determined-ai/determined/master/pkg/model"
 )
 
 func TestUserGroups(t *testing.T) {
 	ctx := context.Background()
-	pgDB := MustResolveTestPostgres(t)
-	MustMigrateTestPostgres(t, pgDB, migrationsFromDB)
+	pgDB := db.MustResolveTestPostgres(t)
+	db.MustMigrateTestPostgres(t, pgDB, pathToMigrations)
 
-	t.Cleanup(func() { cleanUp(ctx, t, pgDB) })
+	t.Cleanup(func() { cleanUp(ctx, t) })
 	setUp(ctx, t, pgDB)
 
 	t.Run("group creation", func(t *testing.T) {
-		_, err := pgDB.AddGroup(ctx, testGroup)
+		_, err := AddGroup(ctx, testGroup)
 		require.NoError(t, err, "failed to create group")
 	})
 
 	t.Run("search groups", func(t *testing.T) {
-		groups, err := pgDB.SearchGroups(ctx, 0)
+		groups, err := SearchGroups(ctx, 0)
 		require.NoError(t, err, "failed to search for groups")
 
 		index := groupsContain(groups, testGroup.ID)
@@ -37,7 +38,7 @@ func TestUserGroups(t *testing.T) {
 	})
 
 	t.Run("find group by id", func(t *testing.T) {
-		foundGroup, err := pgDB.GroupByID(ctx, testGroup.ID)
+		foundGroup, err := GroupByID(ctx, testGroup.ID)
 		require.NoError(t, err, "failed to find group by id")
 		require.Equal(t, testGroup.Name, foundGroup.Name, "Expected found group to have the same name as the one we created")
 	})
@@ -46,25 +47,25 @@ func TestUserGroups(t *testing.T) {
 		// Put it back the way it was when we're done
 		defer func(name string) {
 			testGroup.Name = name
-			err := pgDB.UpdateGroup(ctx, testGroup)
+			err := UpdateGroup(ctx, testGroup)
 			require.NoError(t, err, "failed to put things back how they were after testing UpdateGroup")
 		}(testGroup.Name)
 
 		newName := "kljhadsflkgjhjklsfhgasdhj"
 		testGroup.Name = newName
-		err := pgDB.UpdateGroup(ctx, testGroup)
+		err := UpdateGroup(ctx, testGroup)
 		require.NoError(t, err, "failed to update group")
 
-		foundGroup, err := pgDB.GroupByID(ctx, testGroup.ID)
+		foundGroup, err := GroupByID(ctx, testGroup.ID)
 		require.NoError(t, err, "failed to find group by id")
 		require.Equal(t, newName, foundGroup.Name, "Expected found group to have the new name")
 	})
 
 	t.Run("add users to group", func(t *testing.T) {
-		err := pgDB.AddUsersToGroup(ctx, testGroup.ID, testUser.ID)
+		err := AddUsersToGroup(ctx, testGroup.ID, testUser.ID)
 		require.NoError(t, err, "failed to add users to group")
 
-		users, err := pgDB.GetUsersInGroup(ctx, testGroup.ID)
+		users, err := GetUsersInGroup(ctx, testGroup.ID)
 		require.NoError(t, err, "failed to search for users that belong to group")
 
 		index := usersContain(users, testUser.ID)
@@ -72,7 +73,7 @@ func TestUserGroups(t *testing.T) {
 	})
 
 	t.Run("search groups by user membership", func(t *testing.T) {
-		groups, err := pgDB.SearchGroups(ctx, testUser.ID)
+		groups, err := SearchGroups(ctx, testUser.ID)
 		require.NoError(t, err, "failed to search for groups that user blongs to")
 
 		index := groupsContain(groups, testGroup.ID)
@@ -80,30 +81,30 @@ func TestUserGroups(t *testing.T) {
 	})
 
 	t.Run("remove users from group", func(t *testing.T) {
-		err := pgDB.RemoveUsersFromGroup(ctx, testGroup.ID, -500)
-		require.Equal(t, ErrNotFound, err, "failed to return ErrNotFound when removing non-existent users from group")
+		err := RemoveUsersFromGroup(ctx, testGroup.ID, -500)
+		require.Equal(t, db.ErrNotFound, err, "failed to return ErrNotFound when removing non-existent users from group")
 
-		err = pgDB.RemoveUsersFromGroup(ctx, testGroup.ID, testGroup.UserID, -500)
-		require.Equal(t, ErrNotFound, err, "failed to return ErrNotFound when trying to remove a mix of users in a group and not")
+		err = RemoveUsersFromGroup(ctx, testGroup.ID, testGroup.UserID, -500)
+		require.Equal(t, db.ErrNotFound, err, "failed to return ErrNotFound when trying to remove a mix of users in a group and not")
 
-		err = pgDB.RemoveUsersFromGroup(ctx, testGroup.ID, testUser.ID)
+		err = RemoveUsersFromGroup(ctx, testGroup.ID, testUser.ID)
 		require.NoError(t, err, "failed to remove users from group")
 
-		users, err := pgDB.GetUsersInGroup(ctx, testGroup.ID)
+		users, err := GetUsersInGroup(ctx, testGroup.ID)
 		require.NoError(t, err, "failed to look for users in group")
 
 		i := usersContain(users, testUser.ID)
 		require.Equal(t, -1, i, "User found in group after removing them from it")
 
-		err = pgDB.RemoveUsersFromGroup(ctx, testGroup.ID, testUser.ID)
-		require.Equal(t, ErrNotFound, err, "failed to return ErrNotFound when trying to remove users from group they're not in")
+		err = RemoveUsersFromGroup(ctx, testGroup.ID, testUser.ID)
+		require.Equal(t, db.ErrNotFound, err, "failed to return ErrNotFound when trying to remove users from group they're not in")
 	})
 
 	t.Run("partial success on adding users to a group results in tx rollback and ErrNotFound", func(t *testing.T) {
-		err := pgDB.AddUsersToGroup(ctx, testGroup.ID, testUser.ID, 125674576, 12934728, 0, -15)
-		require.Equal(t, ErrNotFound, err, "didn't return ErrNotFound when adding non-existent users to a group")
+		err := AddUsersToGroup(ctx, testGroup.ID, testUser.ID, 125674576, 12934728, 0, -15)
+		require.Equal(t, db.ErrNotFound, err, "didn't return ErrNotFound when adding non-existent users to a group")
 
-		users, err := pgDB.GetUsersInGroup(ctx, testGroup.ID)
+		users, err := GetUsersInGroup(ctx, testGroup.ID)
 		require.NoError(t, err, "failed to search for users that belong to group")
 
 		index := usersContain(users, testUser.ID)
@@ -111,13 +112,13 @@ func TestUserGroups(t *testing.T) {
 	})
 
 	t.Run("AddUsersToGroup fails with ErrNotFound when attempting to add users to a non-existent group", func(t *testing.T) {
-		err := pgDB.AddUsersToGroup(ctx, -500, testUser.ID)
-		require.Equal(t, ErrNotFound, err, "didn't return ErrNotFound when trying to add users to a non-existent group")
+		err := AddUsersToGroup(ctx, -500, testUser.ID)
+		require.Equal(t, db.ErrNotFound, err, "didn't return ErrNotFound when trying to add users to a non-existent group")
 	})
 
 	t.Run("Deleting a group that doesn't exist results in ErrNotFound", func(t *testing.T) {
-		err := pgDB.DeleteGroup(ctx, -500)
-		require.Equal(t, ErrNotFound, err, "didn't return ErrNotFound when trying to delete a non-existent group")
+		err := DeleteGroup(ctx, -500)
+		require.Equal(t, db.ErrNotFound, err, "didn't return ErrNotFound when trying to delete a non-existent group")
 	})
 
 	t.Run("Deleting a group that has users should work", func(t *testing.T) {
@@ -125,34 +126,34 @@ func TestUserGroups(t *testing.T) {
 		tmpGroup.ID++
 		tmpGroup.Name += tmpGroup.Name
 
-		_, err := pgDB.AddGroup(ctx, tmpGroup)
+		_, err := AddGroup(ctx, tmpGroup)
 		require.NoError(t, err, "failed to create group")
 
-		err = pgDB.AddUsersToGroup(ctx, tmpGroup.ID, testUser.ID)
+		err = AddUsersToGroup(ctx, tmpGroup.ID, testUser.ID)
 		require.NoError(t, err, "failed to add user to group")
 
-		err = pgDB.DeleteGroup(ctx, tmpGroup.ID)
+		err = DeleteGroup(ctx, tmpGroup.ID)
 		require.NoError(t, err, "errored when deleting group")
 
-		_, err = pgDB.GroupByID(ctx, tmpGroup.ID)
-		require.Equal(t, ErrNotFound, err, "deleted group should not be found, and ErrNotFound returned")
+		_, err = GroupByID(ctx, tmpGroup.ID)
+		require.Equal(t, db.ErrNotFound, err, "deleted group should not be found, and ErrNotFound returned")
 	})
 
 	t.Run("AddGroup returns ErrDuplicateRecord when creating a group that already exists", func(t *testing.T) {
-		_, err := pgDB.AddGroup(ctx, testGroupStatic)
-		require.Equal(t, ErrDuplicateRecord, err, "didn't return ErrDuplicateRecord")
+		_, err := AddGroup(ctx, testGroupStatic)
+		require.Equal(t, db.ErrDuplicateRecord, err, "didn't return ErrDuplicateRecord")
 	})
 
 	t.Run("AddUsersToGroup returns ErrDuplicateRecord when adding users to a group they're already in", func(t *testing.T) {
-		err := pgDB.AddUsersToGroup(ctx, testGroupStatic.ID, testUser.ID)
-		require.Equal(t, ErrDuplicateRecord, err, "should have returned ErrDuplicateRecord")
+		err := AddUsersToGroup(ctx, testGroupStatic.ID, testUser.ID)
+		require.Equal(t, db.ErrDuplicateRecord, err, "should have returned ErrDuplicateRecord")
 	})
 
 	t.Run("Static test group should exist at the end and test user should be in it", func(t *testing.T) {
-		_, err := pgDB.GroupByID(ctx, testGroupStatic.ID)
+		_, err := GroupByID(ctx, testGroupStatic.ID)
 		require.NoError(t, err, "errored while getting static test group")
 
-		users, err := pgDB.GetUsersInGroup(ctx, testGroupStatic.ID)
+		users, err := GetUsersInGroup(ctx, testGroupStatic.ID)
 		require.NoError(t, err, "failed to search for users that belong to static group")
 
 		index := usersContain(users, testUser.ID)
@@ -161,11 +162,11 @@ func TestUserGroups(t *testing.T) {
 }
 
 var (
-	testGroup = model.Group{
+	testGroup = Group{
 		ID:   9001,
 		Name: "kljhadsflkgjhjklsfhg",
 	}
-	testGroupStatic = model.Group{
+	testGroupStatic = Group{
 		ID:   10001,
 		Name: "dsjkfkljjkasdfasdky",
 	}
@@ -177,8 +178,45 @@ var (
 	}
 )
 
+const (
+	pathToMigrations = "file://../../static/migrations"
+)
+
+func setUp(ctx context.Context, t *testing.T, pgDB *db.PgDB) {
+	_, err := pgDB.AddUser(&testUser, nil)
+	require.NoError(t, err, "failure creating user in setup")
+
+	_, err = AddGroup(ctx, testGroupStatic)
+	require.NoError(t, err, "failure creating static test group")
+
+	err = AddUsersToGroup(ctx, testGroupStatic.ID, testUser.ID)
+	require.NoError(t, err, "failure adding user to static group")
+}
+
+func cleanUp(ctx context.Context, t *testing.T) {
+	err := RemoveUsersFromGroup(ctx, testGroup.ID, testUser.ID)
+	if err != nil {
+		t.Logf("Error cleaning up group membership: %v", err)
+	}
+
+	err = DeleteGroup(ctx, testGroup.ID)
+	if err != nil {
+		t.Logf("Error cleaning up group: %v", err)
+	}
+
+	err = DeleteGroup(ctx, testGroupStatic.ID)
+	if err != nil {
+		t.Logf("Error cleaning up static group: %v", err)
+	}
+
+	err = deleteUser(ctx, testUser.ID)
+	if err != nil {
+		t.Logf("Error cleaning up user: %v\n", err)
+	}
+}
+
 // groupsContains returns -1 if group id was not found, else returns the index
-func groupsContain(groups []model.Group, id int) int {
+func groupsContain(groups []Group, id int) int {
 	if len(groups) < 1 {
 		return -1
 	}
@@ -208,39 +246,6 @@ func usersContain(users []model.User, id model.UserID) int {
 }
 
 func deleteUser(ctx context.Context, id model.UserID) error {
-	_, err := Bun().NewDelete().Table("users").Where("id = ?", id).Exec(ctx)
+	_, err := db.Bun().NewDelete().Table("users").Where("id = ?", id).Exec(ctx)
 	return err
-}
-
-func setUp(ctx context.Context, t *testing.T, pgDB *PgDB) {
-	_, err := pgDB.AddUser(&testUser, nil)
-	require.NoError(t, err, "failure creating user in setup")
-
-	_, err = pgDB.AddGroup(ctx, testGroupStatic)
-	require.NoError(t, err, "failure creating static test group")
-
-	err = pgDB.AddUsersToGroup(ctx, testGroupStatic.ID, testUser.ID)
-	require.NoError(t, err, "failure adding user to static group")
-}
-
-func cleanUp(ctx context.Context, t *testing.T, pgDB *PgDB) {
-	err := pgDB.RemoveUsersFromGroup(ctx, testGroup.ID, testUser.ID)
-	if err != nil {
-		t.Logf("Error cleaning up group membership: %v", err)
-	}
-
-	err = pgDB.DeleteGroup(ctx, testGroup.ID)
-	if err != nil {
-		t.Logf("Error cleaning up group: %v", err)
-	}
-
-	err = pgDB.DeleteGroup(ctx, testGroupStatic.ID)
-	if err != nil {
-		t.Logf("Error cleaning up static group: %v", err)
-	}
-
-	err = deleteUser(ctx, testUser.ID)
-	if err != nil {
-		t.Logf("Error cleaning up user: %v\n", err)
-	}
 }
--- master/internal/usergroup/postgres_groups.go
+++ master/internal/usergroup/postgres_groups.go
@@ -1,4 +1,4 @@
-package db
+package usergroup
 
 import (
 	"context"
@@ -9,20 +9,21 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/uptrace/bun"
 
+	"github.com/determined-ai/determined/master/internal/db"
 	"github.com/determined-ai/determined/master/pkg/model"
 )
 
 // AddGroup adds a group to the database. Returns ErrDuplicateRow if a
 // group already exists with the same name or ID.
-func (db *PgDB) AddGroup(ctx context.Context, group model.Group) (model.Group, error) {
-	_, err := Bun().NewInsert().Model(&group).Exec(ctx)
+func AddGroup(ctx context.Context, group Group) (Group, error) {
+	_, err := db.Bun().NewInsert().Model(&group).Exec(ctx)
 	return group, matchSentinelError(err)
 }
 
 // GroupByID looks for a group by id. Returns ErrNotFound if the group isn't found.
-func (db *PgDB) GroupByID(ctx context.Context, gid int) (model.Group, error) {
-	var g model.Group
-	err := Bun().NewSelect().Model(&g).Where("id = ?", gid).Scan(ctx)
+func GroupByID(ctx context.Context, gid int) (Group, error) {
+	var g Group
+	err := db.Bun().NewSelect().Model(&g).Where("id = ?", gid).Scan(ctx)
 
 	return g, matchSentinelError(err)
 }
@@ -31,10 +32,10 @@ func (db *PgDB) GroupByID(ctx context.Context, gid int) (model.Group, error) {
 // in that if a value < 1 is passed in, the parameter is ignored. SearchGroups
 // does not return an error if no groups are found, as that is considered a
 // successful search.
-func (db *PgDB) SearchGroups(ctx context.Context,
-	userBelongsTo model.UserID) ([]model.Group, error) {
-	var groups []model.Group
-	query := Bun().NewSelect().Model(&groups)
+func SearchGroups(ctx context.Context,
+	userBelongsTo model.UserID) ([]Group, error) {
+	var groups []Group
+	query := db.Bun().NewSelect().Model(&groups)
 
 	if userBelongsTo > 0 {
 		query = query.
@@ -49,8 +50,8 @@ func (db *PgDB) SearchGroups(ctx context.Context,
 
 // DeleteGroup deletes a group from the database. Returns ErrNotFound if the
 // group doesn't exist.
-func (db *PgDB) DeleteGroup(ctx context.Context, gid int) error {
-	tx, err := Bun().BeginTx(ctx, nil)
+func DeleteGroup(ctx context.Context, gid int) error {
+	tx, err := db.Bun().BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
@@ -71,7 +72,7 @@ func (db *PgDB) DeleteGroup(ctx context.Context, gid int) error {
 		return err
 	}
 
-	res, err := tx.NewDelete().Model(&model.Group{ID: gid}).WherePK().Exec(ctx)
+	res, err := tx.NewDelete().Model(&Group{ID: gid}).WherePK().Exec(ctx)
 	if foundErr := mustHaveAffectedRows(res, err); foundErr != nil {
 		return matchSentinelError(foundErr)
 	}
@@ -86,8 +87,8 @@ func (db *PgDB) DeleteGroup(ctx context.Context, gid int) error {
 
 // UpdateGroup updates a group in the database. Returns ErrNotFound if the
 // group isn't found.
-func (db *PgDB) UpdateGroup(ctx context.Context, group model.Group) error {
-	res, err := Bun().NewUpdate().Model(&group).WherePK().Exec(ctx)
+func UpdateGroup(ctx context.Context, group Group) error {
+	res, err := db.Bun().NewUpdate().Model(&group).WherePK().Exec(ctx)
 
 	return matchSentinelError(mustHaveAffectedRows(res, err))
 }
@@ -95,20 +96,20 @@ func (db *PgDB) UpdateGroup(ctx context.Context, group model.Group) error {
 // AddUsersToGroup adds users to a group by creating GroupMembership rows.
 // Returns ErrNotFound if the group isn't found or ErrDuplicateRow if one
 // of the users is already in the group.
-func (db *PgDB) AddUsersToGroup(ctx context.Context, gid int, uids ...model.UserID) error {
+func AddUsersToGroup(ctx context.Context, gid int, uids ...model.UserID) error {
 	if len(uids) < 1 {
 		return nil
 	}
 
-	groupMem := make([]model.GroupMembership, 0, len(uids))
+	groupMem := make([]GroupMembership, 0, len(uids))
 	for _, uid := range uids {
-		groupMem = append(groupMem, model.GroupMembership{
+		groupMem = append(groupMem, GroupMembership{
 			UserID:  uid,
 			GroupID: gid,
 		})
 	}
 
-	tx, err := Bun().BeginTx(ctx, nil)
+	tx, err := db.Bun().BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
@@ -134,12 +135,12 @@ func (db *PgDB) AddUsersToGroup(ctx context.Context, gid int, uids ...model.User
 // RemoveUsersFromGroup removes users from a group. Removes nothing and
 // returns ErrNotFound if the group or one of the users' membership rows
 // aren't found.
-func (db *PgDB) RemoveUsersFromGroup(ctx context.Context, gid int, uids ...model.UserID) error {
+func RemoveUsersFromGroup(ctx context.Context, gid int, uids ...model.UserID) error {
 	if len(uids) < 1 {
 		return nil
 	}
 
-	tx, err := Bun().BeginTx(ctx, nil)
+	tx, err := db.Bun().BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
@@ -168,9 +169,9 @@ func (db *PgDB) RemoveUsersFromGroup(ctx context.Context, gid int, uids ...model
 // GetUsersInGroup searches for users that belong to a group and returns them.
 // Does not return ErrNotFound if none are found, as that is considered a
 // successful search.
-func (db *PgDB) GetUsersInGroup(ctx context.Context, gid int) ([]model.User, error) {
+func GetUsersInGroup(ctx context.Context, gid int) ([]model.User, error) {
 	var users []model.User
-	err := Bun().NewSelect().Table("users").Model(&users).
+	err := db.Bun().NewSelect().Table("users").Model(&users).
 		Join("INNER JOIN user_group_membership AS ugm ON users.id=ugm.user_id").
 		Where("ugm.group_id = ?", gid).
 		Scan(ctx)
@@ -180,14 +181,14 @@ func (db *PgDB) GetUsersInGroup(ctx context.Context, gid int) ([]model.User, err
 
 func matchSentinelError(err error) error {
 	if errors.Is(err, sql.ErrNoRows) {
-		return ErrNotFound
+		return db.ErrNotFound
 	}
 
 	switch pgErrCode(err) {
-	case foreignKeyViolation:
-		return ErrNotFound
-	case uniqueViolation:
-		return ErrDuplicateRecord
+	case db.CodeForeignKeyViolation:
+		return db.ErrNotFound
+	case db.CodeUniqueViolation:
+		return db.ErrDuplicateRecord
 	}
 
 	return err
@@ -202,7 +203,7 @@ func mustHaveAffectedRows(result sql.Result, err error) error {
 			return affectedErr
 		}
 		if rowsAffected == 0 {
-			return ErrNotFound
+			return db.ErrNotFound
 		}
 	}
 
