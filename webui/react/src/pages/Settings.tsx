2d5eb0c7d1358cdb03274f37a494d1f609ce0ef3
Caleb Kang <caleb@determined.ai>
2022-07-19 11:47:14 -0600

    chore: support enabling rbac settings via query params

--- webui/react/src/pages/Settings.tsx
+++ webui/react/src/pages/Settings.tsx
@@ -1,6 +1,8 @@
 import { Tabs } from 'antd';
+import queryString from 'query-string';
 import React, { useCallback, useState } from 'react';
 import { useHistory, useParams } from 'react-router';
+import { useLocation } from 'react-router-dom';
 
 import Page from 'components/Page';
 import SettingsAccount from 'pages/Settings/SettingsAccount';
@@ -21,20 +23,27 @@ interface Params {
   tab?: TabType;
 }
 
+interface Queries {
+  rbac?: boolean;
+}
+
 const DEFAULT_TAB_KEY = TabType.Account;
 
 const SettingsContent: React.FC<ContentProps> = ({ enableRBAC = false }) => {
   const { tab } = useParams<Params>();
+  const location = useLocation();
   const [ tabKey, setTabKey ] = useState<TabType>(tab || DEFAULT_TAB_KEY);
   const basePath = paths.settings();
   const history = useHistory();
+
+  const { rbac } = queryString.parse(location.search);
 
   const handleTabChange = useCallback(key => {
     setTabKey(key);
     history.replace(key === DEFAULT_TAB_KEY ? basePath : `${basePath}/${key}`);
   }, [ basePath, history ]);
 
-  return enableRBAC ? (
+  return rbac ? (
     <Tabs className="no-padding" defaultActiveKey={tabKey} onChange={handleTabChange}>
       <TabPane key="account" tab="Account">
         <SettingsAccount />
