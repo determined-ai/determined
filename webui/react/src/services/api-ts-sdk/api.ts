ec3a5dbe8faf0dae010adbc0b9d9f3b66c23ee8a
nrajanee <nikita.rajaneesh@hpe.com>
2022-07-19 18:41:47 -0500

    feat: add custom search method and event queue [DET-7716] (#4560)
    Co-authored-by: Maksim Kouznetsov <maksim.kouznetsov@hpe.com>

--- webui/react/src/services/api-ts-sdk/api.ts
+++ webui/react/src/services/api-ts-sdk/api.ts
@@ -1545,7 +1545,7 @@ export interface V1CreateExperimentResponse {
  */
 export interface V1CreateTrialOperation {
     /**
-     * 
+     * The key refers to which part of the model this hyperparameters are for.
      * @type {{ [key: string]: V1Hyperparameter; }}
      * @memberof V1CreateTrialOperation
      */
@@ -2816,7 +2816,7 @@ export interface V1GetSearcherEventsResponse {
      * @type {Array<V1SearcherEvent>}
      * @memberof V1GetSearcherEventsResponse
      */
-    searcherEvent?: Array<V1SearcherEvent>;
+    searcherEvents?: Array<V1SearcherEvent>;
     /**
      * The last event that triggered client which sent the appropriate events to master which sent them successfully to the experiment actor.
      * @type {V1SearcherEvent}
@@ -3332,11 +3332,11 @@ export interface V1IdleNotebookResponse {
  */
 export interface V1InitialOperations {
     /**
-     * 
-     * @type {number}
+     * cannot have an empty class.
+     * @type {string}
      * @memberof V1InitialOperations
      */
-    id?: number;
+    holder?: string;
 }
 
 /**
@@ -6118,6 +6118,12 @@ export enum V1SchedulerType {
 export interface V1SearcherEvent {
     /**
      * 
+     * @type {number}
+     * @memberof V1SearcherEvent
+     */
+    id?: number;
+    /**
+     * 
      * @type {V1InitialOperations}
      * @memberof V1SearcherEvent
      */
@@ -6742,12 +6748,6 @@ export interface V1Tensorboard {
 export interface V1TrialClosed {
     /**
      * 
-     * @type {number}
-     * @memberof V1TrialClosed
-     */
-    id?: number;
-    /**
-     * 
      * @type {string}
      * @memberof V1TrialClosed
      */
@@ -6760,12 +6760,6 @@ export interface V1TrialClosed {
  * @interface V1TrialCreated
  */
 export interface V1TrialCreated {
-    /**
-     * 
-     * @type {number}
-     * @memberof V1TrialCreated
-     */
-    id?: number;
     /**
      * 
      * @type {string}
@@ -6806,12 +6800,6 @@ export enum V1TrialEarlyExitExitedReason {
  * @interface V1TrialExitedEarly
  */
 export interface V1TrialExitedEarly {
-    /**
-     * 
-     * @type {number}
-     * @memberof V1TrialExitedEarly
-     */
-    id?: number;
     /**
      * 
      * @type {string}
@@ -7306,12 +7294,6 @@ export interface V1ValidateAfterOperation {
  * @interface V1ValidationCompleted
  */
 export interface V1ValidationCompleted {
-    /**
-     * 
-     * @type {number}
-     * @memberof V1ValidationCompleted
-     */
-    id?: number;
     /**
      * 
      * @type {string}
