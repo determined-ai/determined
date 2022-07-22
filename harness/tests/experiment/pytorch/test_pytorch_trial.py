7720b86af0b63255521b5c5699435b3197543464
Garrett Goon <garrett.goon@hpe.com>
2022-07-22 14:32:36 -0400

    comment cleanup

--- harness/tests/experiment/pytorch/test_pytorch_trial.py
+++ harness/tests/experiment/pytorch/test_pytorch_trial.py
@@ -418,7 +418,7 @@ class TestPyTorchTrial:
             "trial_startups": 1,
             "validation_steps_started": 1,
             "validation_steps_ended": 1,
-            # Note: trial_shutdowns, checkpoints_ended, and checkpoints_uploaded cannot be
+            # Note: checkpoints_ended, checkpoints_uploaded, and trial_shutdowns, cannot be
             # persisted, as they are all updated after checkpointing.
             "checkpoints_ended": 0,
             "checkpoints_uploaded": 0,
