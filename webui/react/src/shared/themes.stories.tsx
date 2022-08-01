91c24ccf34c7dfc4578fa116a0f83f2bf56394a0
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-08-01 18:20:44 -0400

    add a story for antdbutton

---
+++ webui/react/src/shared/themes.stories.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd';
+import React from 'react';
+
+export default { title: 'Theme' };
+
+export const Default = (): React.ReactNode => (
+  <Button> Hello World </Button>
+);
+
+export const ButtonDisabled = (): React.ReactNode => (
+  <Button disabled> Hello World </Button>
+);
