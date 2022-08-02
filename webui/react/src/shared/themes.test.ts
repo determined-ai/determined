db8a85db3ffe74e81df3aa6eb588d0e4fa3c6c17
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-08-02 11:24:24 -0400

    add more test pairs

--- webui/react/src/shared/themes.test.ts
+++ webui/react/src/shared/themes.test.ts
@@ -8,19 +8,59 @@ const supportedThemes = {
   lightHpe: themes.themeLightHpe,
 };
 
+type ThemeVarPair = (keyof themes.Theme)[];
+const genPairs = (name: string): ThemeVarPair[] => {
+  const base = [
+    [ `${name}`, `${name}On` ],
+    [ `${name}`, `${name}Border` ],
+  ] as ThemeVarPair[];
+
+  // same intentsity pairs.
+  const strong = [ ...base.map(([ a, b ]) => [ `${a}Strong`, `${b}Strong` ]) ] as ThemeVarPair[];
+  const weak = [ ...base.map(([ a, b ]) => [ `${a}Weak`, `${b}Weak` ]) ] as ThemeVarPair[];
+
+  // stronger color on top of weaker color.
+  const strongOnDefault = [ ...base.map(([ a, b ]) => [ `${a}`, `${b}Strong` ]) ] as ThemeVarPair[];
+  const defaultOnWeak = [ ...base.map(([ a, b ]) => [ `${a}Weak`, `${b}` ]) ] as ThemeVarPair[];
+
+  // weak colors on top of strong colors.
+  // const weakOnDefault = [ ...base.map(([ a, b ]) => [ `${a}`, `${b}Weak` ]) ] as ThemeVarPair[];
+  // const defaultOnStrong = [ ...base.map(([ a, b ]) =>
+  //   [ `${a}Strong`, `${b}` ]) ] as ThemeVarPair[];
+
+  // CHECK: which of these pairs make sens to test?
+  return [ ...base,
+    ...strong,
+    ...weak,
+    ...strongOnDefault,
+    ...defaultOnWeak,
+    // ...weakOnDefault,
+    // ...defaultOnStrong,
+  ];
+};
+
 /** pars of theme colors used next to eachother */
-const pairs: (keyof themes.Theme)[][] = [
+const pairs: ThemeVarPair[] = [
   [ 'ixActive', 'ixOnActive' ],
   [ 'ixActive', 'ixBorderActive' ],
   [ 'ixInactive', 'ixOnInactive' ],
   [ 'ixInactive', 'ixBorderInactive' ],
+  [ 'background', 'backgroundOn' ],
 ];
 
 describe('themes', () => {
+  beforeAll(() => {
+    const newPairs = [ 'surface', 'stage', 'float' ]
+      .map(genPairs)
+      .reduce((acc, cur) => acc.concat(cur), []);
+    pairs.push(...newPairs);
+
+  });
+
   it('should have sufficient distance between adjacent colors', () => {
     /** defines the required minimum distance between at least one of the rgba values. */
-    const TOLERANCE = 8;
-    const violators: string[] = [];
+    const TOLERANCE = 4.1; // TODO: raise the bar.
+    const violators: (string|number)[][] = [];
     Object.entries(supportedThemes).forEach(([ name, theme ]) => {
       pairs.forEach(([ k1, k2 ]) => {
         expect(theme[k1]).toBeDefined();
@@ -33,10 +73,14 @@ describe('themes', () => {
         const c2CL = rgb2lab(c2);
         const distance = labDistance(c1CL, c2CL);
         if (distance < TOLERANCE) {
-          violators.push(`Theme: ${name} - ${k1} ${k2}. Distance: ${distance}`);
+          violators.push([ `Theme: ${name} - ${k1} ${k2}`, distance ]);
         }
       });
     });
-    expect(violators).toEqual([]);
+    // sort violators by distance.
+    const reports = violators
+      .sort((a, b) => (a[1] as number) - (b[1] as number))
+      .map(([ name, distance ]) => `${name} - ${distance}`);
+    expect(reports).toEqual([]);
   });
 });
