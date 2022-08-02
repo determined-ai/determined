5d50c8bfecfd51051cec695344e2700e353185ff
Caleb Kang <caleb@determined.ai>
2022-08-02 14:10:19 -0600

    chore: remove unused functions and minor code cleanup

--- webui/react/src/utils/wait.ts
+++ webui/react/src/utils/wait.ts
@@ -4,22 +4,12 @@ import { windowOpenFeatures } from 'shared/utils/routes';
 import { Command, CommandState, CommandTask, CommandType } from 'types';
 import { isCommandTask } from 'utils/task';
 
-// createWsUrl: Given an event url create the corresponding ws url.
-export function createWsUrl(eventUrl: string): string {
-  const isFullUrl = /^https?:\/\//i;
-
-  if (isFullUrl.test(eventUrl)) {
-    return eventUrl.replace(/^http/, 'ws');
-  } else {
-    // Remove the preceding slash if it is an absolute path.
-    eventUrl = eventUrl.replace(/^\//, '');
-    let url = window.location.protocol.replace(/^http/, 'ws');
-    url += '//' + window.location.host + '/' + eventUrl;
-    return url;
-  }
+export interface WaitStatus {
+  isReady: boolean;
+  state: CommandState
 }
 
-const commandToEventUrl = (command: Command | CommandTask): string => {
+export const commandToEventUrl = (command: Command | CommandTask): string => {
   const kind = isCommandTask(command) ? command.type : command.kind;
   let path = '';
   switch (kind) {
@@ -34,17 +24,6 @@ const commandToEventUrl = (command: Command | CommandTask): string => {
   return path;
 };
 
-export const waitPageUrl = (command: Command | CommandTask): string => {
-  const url = commandToEventUrl(command);
-  if (!url || !command.serviceAddress)
-    throw new Error('command cannot be opened');
-  const kind = isCommandTask(command) ? command.type : command.kind;
-
-  const waitPath = `${process.env.PUBLIC_URL}/wait/${kind.toLowerCase()}/${command.id}`;
-  const waitParams = `?eventUrl=${url}&serviceAddr=${command.serviceAddress}`;
-  return waitPath + waitParams;
-};
-
 export const openCommand = (command: CommandTask): void => {
   window.open(
     process.env.PUBLIC_URL + paths.interactive(command),
@@ -53,7 +32,14 @@ export const openCommand = (command: CommandTask): void => {
   );
 };
 
-export interface WaitStatus {
-  isReady: boolean;
-  state: CommandState
-}
+export const CANNOT_OPEN_COMMAND_ERROR = 'Command cannot be opened.';
+
+export const waitPageUrl = (command: Command | CommandTask): string => {
+  const url = commandToEventUrl(command);
+  if (!url || !command.serviceAddress) throw new Error(CANNOT_OPEN_COMMAND_ERROR);
+
+  const kind = isCommandTask(command) ? command.type : command.kind;
+  const waitPath = `${process.env.PUBLIC_URL}/wait/${kind.toLowerCase()}/${command.id}`;
+  const waitParams = `?eventUrl=${url}&serviceAddr=${command.serviceAddress}`;
+  return waitPath + waitParams;
+};
