2187e04f9e9c038f4ec79505d0566b240c39caa5
Thiago Dallacqua <thiago.menezes-dallacqua@hpe.com>
2022-08-02 10:35:52 -0300

    fix: remove the default theme from initialization

--- webui/react/src/contexts/Store.tsx
+++ webui/react/src/contexts/Store.tsx
@@ -4,7 +4,6 @@ import { globalStorage } from 'globalStorage';
 import { DarkLight, Mode, Theme } from 'shared/themes';
 import { clone, isEqual } from 'shared/utils/data';
 import { percent } from 'shared/utils/number';
-import themes from 'themes';
 import {
   Agent, Auth, BrandingType, ClusterOverview, ClusterOverviewResource,
   DetailedUser, DeterminedInfo, PoolOverview, ResourcePool, ResourceType, Workspace,
@@ -139,7 +138,7 @@ const initClusterOverview: ClusterOverview = {
   [ResourceType.UNSPECIFIED]: clone(initResourceTally),
 };
 const initInfo = {
-  branding: BrandingType.Determined,
+  branding: '' as BrandingType,
   checked: false,
   clusterId: '',
   clusterName: '',
@@ -155,7 +154,7 @@ const initUI = {
   omnibar: { isShowing: false },
   showChrome: true,
   showSpinner: false,
-  theme: themes[BrandingType.Determined][DarkLight.Light],
+  theme: {} as Theme,
 };
 const initState: State = {
   activeExperiments: 0,
