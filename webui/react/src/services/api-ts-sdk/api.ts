f43fc049705f21f9eb3e0765fa2ac08d26c2120a
Maksim <maksim.kouznetsov@hpe.com>
2022-08-05 14:05:49 -0700

    fix: DET-7973 client progress (#4712)

--- webui/react/src/services/api-ts-sdk/api.ts
+++ webui/react/src/services/api-ts-sdk/api.ts
@@ -2118,10 +2118,10 @@ export interface V1Experiment {
 export interface V1ExperimentInactive {
     /**
      * 
-     * @type {string}
+     * @type {Determinedexperimentv1State}
      * @memberof V1ExperimentInactive
      */
-    experimentState?: string;
+    experimentState: Determinedexperimentv1State;
 }
 
 /**
@@ -7492,7 +7492,7 @@ export interface V1TrialClosed {
      * @type {string}
      * @memberof V1TrialClosed
      */
-    requestId?: string;
+    requestId: string;
 }
 
 /**
@@ -7506,7 +7506,7 @@ export interface V1TrialCreated {
      * @type {string}
      * @memberof V1TrialCreated
      */
-    requestId?: string;
+    requestId: string;
 }
 
 /**
@@ -7545,13 +7545,13 @@ export interface V1TrialExitedEarly {
      * @type {string}
      * @memberof V1TrialExitedEarly
      */
-    requestId?: string;
+    requestId: string;
     /**
      * The reason for the exit.
      * @type {V1TrialExitedEarlyExitedReason}
      * @memberof V1TrialExitedEarly
      */
-    exitedReason?: V1TrialExitedEarlyExitedReason;
+    exitedReason: V1TrialExitedEarlyExitedReason;
 }
 
 /**
@@ -7749,13 +7749,13 @@ export interface V1TrialProgress {
      * @type {string}
      * @memberof V1TrialProgress
      */
-    requestId?: string;
+    requestId: string;
     /**
      * partial_units represent partial epochs, batches or records where the Unit is implied.
      * @type {number}
      * @memberof V1TrialProgress
      */
-    partialUnits?: number;
+    partialUnits: number;
 }
 
 /**
@@ -8125,19 +8125,19 @@ export interface V1ValidationCompleted {
      * @type {string}
      * @memberof V1ValidationCompleted
      */
-    requestId?: string;
+    requestId: string;
     /**
      * 
      * @type {number}
      * @memberof V1ValidationCompleted
      */
-    metric?: number;
+    metric: number;
     /**
      * length from ValidateAfterOperation.
      * @type {string}
      * @memberof V1ValidationCompleted
      */
-    validateAfterLength?: string;
+    validateAfterLength: string;
 }
 
 /**
