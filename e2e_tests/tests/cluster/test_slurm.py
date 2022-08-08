4a29f69ad2596cdbd5f4a5bfde3cb3f5a75a5481
CharlesTran1 <69864849+CharlesTran1@users.noreply.github.com>
2022-08-08 12:59:42 -0700

    fix: e2e_test test_slurm.py test_node_not_available fails on CPU based cluster (Mosaic) due to different Error output (FOUNDENG-132)  (#364)
    [FOUNDENG-132]
    Added extra error logs to account for differences with clusters with no GPUs. Also updated test_docker_login to include checks for error logs that are due to docker download rate limitations.

--- e2e_tests/tests/cluster/test_slurm.py
+++ e2e_tests/tests/cluster/test_slurm.py
@@ -1,7 +1,32 @@
+from typing import List
+
 import pytest
 
+from determined.common.api.bindings import determinedexperimentv1State
 from tests import config as conf
 from tests import experiment as exp
+
+
+def run_failure_test_multiple(config_file: str, model_def_file: str, errors: List[str]) -> int:
+    # Creates an experiment meant to fail and checks array of error messages
+    # If one of the errors are present, then the assertion passes
+    experiment_id = exp.create_experiment(
+        config_file,
+        model_def_file,
+    )
+    exp.wait_for_experiment_state(experiment_id, determinedexperimentv1State.STATE_ERROR)
+    trials = exp.experiment_trials(experiment_id)
+    for t in trials:
+        trial = t.trial
+        if trial.state != determinedexperimentv1State.STATE_ERROR:
+            continue
+
+        logs = exp.trial_logs(trial.id)
+        totalAssertion = False
+        for e in errors:
+            totalAssertion = totalAssertion or any(e in line for line in logs)
+        assert totalAssertion
+    return experiment_id
 
 
 @pytest.mark.e2e_slurm
@@ -36,10 +61,13 @@ def test_node_not_available() -> None:
     # Verifies that the error message includes the SBATCH options of the failed submission.
     # Only casablanca displays the SBATCH options. Horizon does not upon failure
     # The line: "SBATCH options:" is not present on horizon's output
-    exp.run_failure_test(
+    error1 = "Batch job submission failed: Requested node configuration is not available"
+    error2 = "CPU count per node can not be satisfied"
+    errors = [error1, error2]
+    run_failure_test_multiple(
         conf.fixtures_path("failures/slurm-requested-node-not-available.yaml"),
         conf.fixtures_path("failures/"),
-        "Batch job submission failed: Requested node configuration is not available",
+        errors,
     )
 
 
@@ -59,21 +87,25 @@ def test_bad_slurm_option() -> None:
 def test_docker_login() -> None:
     # Creates an experiment that references a valid docker image,
     # but it fails to download due to the lack of a docker login.
-    # As of writing, FOUNDENG-87 is still in progress. The error will instead be:
-    # FATAL:   Unable to handle docker://ilumb/mylolcow uri:
-    # failed to get checksum for docker://ilumb/mylolcow:
-    # loading registries configuration: reading registries.conf.d:
-    # lstat /root/.config/containers/registries.conf.d: permission denied
-    exp.run_failure_test(
+    # There are two potential errors that may occur. This is due
+    # to docker download rate limitations. The error that occurs
+    # depends on the amount of docker downloads on a given day.
+    # The Docker error is split into two errors due to slightly
+    # different potential outputs from the launcher.
+
+    errorPermission = (
+        "latest in docker.io/ilumb/mylolcow: errors: "
+        + "denied: requested access to the resource is denied"
+    )
+    errorDocker = "lstat /root/.config/containers/registries.conf.d: permission denied"
+    errors = [errorPermission, errorDocker]
+    run_failure_test_multiple(
         conf.fixtures_path("failures/docker-login-failure.yaml"),
         conf.fixtures_path("failures/"),
-        "FATAL:   Unable to pull docker://ilumb/mylolcow: conveyor failed to get: "
-        + "Error reading manifest latest in docker.io/ilumb/mylolcow: errors: "
-        + "denied: requested access to the resource is denied",
+        errors,
     )
 
 
-# Not possible right now due to incomplete state of circleci runner
 # A devcluster needs to be run with the master host entered incorrectly.
 @pytest.mark.e2e_slurm_misconfigured
 def test_master_host() -> None:
