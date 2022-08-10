12de267feb9ba7a85f127b4265bd821025c1db42
Daniel R Hunter <daniel.robert.hunter@hpe.com>
2022-08-10 15:11:26 -0400

    isort and black on pytorch_onevar_model.py

--- harness/tests/experiment/fixtures/pytorch_onevar_model.py
+++ harness/tests/experiment/fixtures/pytorch_onevar_model.py
@@ -34,7 +34,7 @@ Finally, we can calculate the updated weight (w') in terms of w0:
 TODO(DET-1597): migrate the all pytorch XOR trial unit tests to variations of the OneVarTrial.
 """
 
-from typing import Any, Dict, List, Optional, Tuple, Iterable
+from typing import Any, Dict, Iterable, List, Optional, Tuple
 
 import numpy as np
 import torch
@@ -160,17 +160,19 @@ class OneVarTrial(pytorch.PyTorchTrial):
 
     @staticmethod
     def check_batch_metrics(
-            metrics: Dict[str, Any],
-            batch_idx: int,
-            metric_keyname_pairs: Iterable[Tuple[str, str]] =
-            (("loss", "loss_exp"), ("w_after", "w_exp")),
-            **kwargs,
+        metrics: Dict[str, Any],
+        batch_idx: int,
+        metric_keyname_pairs: Iterable[Tuple[str, str]] = (
+            ("loss", "loss_exp"),
+            ("w_after", "w_exp"),
+        ),
+        **kwargs,
     ) -> None:
         """A check to be applied to the output of every train_batch in a test."""
         for k_a, k_b in metric_keyname_pairs:
             m_a, m_b = metrics[k_a], metrics[k_b]
-            assert (
-                np.allclose(m_a, m_b, **kwargs)
+            assert np.allclose(
+                m_a, m_b, **kwargs
             ), f"Metrics {k_a}={m_a} and {k_b}={m_b} do not match at batch {batch_idx}"
 
     def evaluate_batch(self, batch: pytorch.TorchData) -> Dict[str, Any]:
