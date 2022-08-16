d652667066565a03f239826188f6f8b2853f0082
aciborowska <35503877+aciborowska@users.noreply.github.com>
2022-08-16 15:47:58 -0700

    fix: show learning curve (#4795)

--- master/internal/api_experiment.go
+++ master/internal/api_experiment.go
@@ -1410,6 +1410,8 @@ func (a *apiServer) topTrials(experimentID int, maxTrials int, s expconf.Searche
 		ranking = ByMetricOfInterest
 	case expconf.GridConfig:
 		ranking = ByMetricOfInterest
+	case expconf.CustomConfig:
+		ranking = ByMetricOfInterest
 	case expconf.AsyncHalvingConfig:
 		ranking = ByTrainingLength
 	case expconf.AdaptiveASHAConfig:
@@ -1423,8 +1425,6 @@ func (a *apiServer) topTrials(experimentID int, maxTrials int, s expconf.Searche
 		ranking = ByTrainingLength
 	case expconf.SyncHalvingConfig:
 		ranking = ByTrainingLength
-	case expconf.CustomConfig:
-		return nil, errors.New("experiments with custom searcher are not supported for trial sampling")
 	default:
 		return nil, errors.New("unable to detect a searcher algorithm for trial sampling")
 	}
