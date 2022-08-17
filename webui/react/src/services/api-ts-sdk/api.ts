69f5dfb9db4b42f5a62b7cce2cf57edbcfc49f52
Ryan <rb@hpe.com>
2022-08-17 15:02:24 -0600

    chore: fix ListValue types in swagger spec (#4801)
    Protobuf google.protobuf.ListValue types come through swagger as:
    
        {"type":"array", "items":{"type":"object"} }
    
    which would convert to `Sequence[Dict[str, Any]]`, which is a pretty
    strange field type for v1PatchModel.labels.
    
    This PR adds tweaks via the proto/patches/api.json to force the right
    type in the swagger.

--- webui/react/src/services/api-ts-sdk/api.ts
+++ webui/react/src/services/api-ts-sdk/api.ts
@@ -4404,10 +4404,10 @@ export interface V1PatchExperiment {
     description?: string;
     /**
      * Labels attached to the experiment.
-     * @type {Array<any>}
+     * @type {Array<string>}
      * @memberof V1PatchExperiment
      */
-    labels?: Array<any>;
+    labels?: Array<string>;
     /**
      * The experiment name.
      * @type {string}
@@ -4462,10 +4462,10 @@ export interface V1PatchModel {
     metadata?: any;
     /**
      * An updated label list for the model.
-     * @type {Array<any>}
+     * @type {Array<string>}
      * @memberof V1PatchModel
      */
-    labels?: Array<any>;
+    labels?: Array<string>;
     /**
      * Updated notes associated with this model.
      * @type {string}
@@ -4520,10 +4520,10 @@ export interface V1PatchModelVersion {
     comment?: string;
     /**
      * An updated label list for the model version.
-     * @type {Array<any>}
+     * @type {Array<string>}
      * @memberof V1PatchModelVersion
      */
-    labels?: Array<any>;
+    labels?: Array<string>;
     /**
      * Updated text notes for the model version.
      * @type {string}
