ada554049429b94b099e7fb323195c938ce2a79b
Nicholas Blaskey <nick.blaskey@hpe.com>
2022-08-17 18:31:15 -0400

    Capitilization in sql

--- master/static/migrations/20220817105249_proto-time-helper.tx.up.sql
+++ master/static/migrations/20220817105249_proto-time-helper.tx.up.sql
@@ -5,8 +5,8 @@ CREATE OR REPLACE FUNCTION public.proto_time(ts timestamptz)
 AS $$
     SELECT json_build_object(
         -- Seconds since epoch
-        'seconds',  floor(extract(EPOCH from ts))::BIGINT, 
+        'seconds',  floor(extract(epoch FROM ts))::bigint, 
         -- Fractional part in nanos since epoch
-        'nanos',    ((extract(EPOCH from ts) - floor(extract(EPOCH from ts))) * 1000000000)::INT
+        'nanos',    ((extract(epoch FROM ts) - floor(extract(EPOCH FROM ts))) * 1000000000)::int
     )
 $$;
