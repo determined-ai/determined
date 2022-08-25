882445ecaeba9eb29b157941630919e0bbf6dbfa
Maksim <maksim.kouznetsov@hpe.com>
2022-08-25 09:14:58 -0700

    feat: Save operations (#4861)

--- harness/determined/searcher/core_search_runner.py
+++ harness/determined/searcher/core_search_runner.py
@@ -1,10 +1,12 @@
 import logging
-from typing import Any, Dict, Optional
+import pickle
+from typing import Any, Dict, List, Optional, Tuple
 
 import determined as det
 from determined.experimental import client
 from determined.searcher.search_method import SearchMethod
 from determined.searcher.search_runner import SearchRunner
+from harness.determined.searcher.search_method import Operation
 
 
 class CoreSearchRunner(SearchRunner):
@@ -22,19 +24,25 @@ class CoreSearchRunner(SearchRunner):
     ) -> None:
         logging.info("CoreSearchRunner.run")
 
+        operations: Optional[List[Operation]] = None
         if self.latest_checkpoint is not None:
-            experiment_id = self.load_state(self.latest_checkpoint)
+            experiment_id, operations = self.load_state(self.latest_checkpoint)
         else:
             exp = client.create_experiment(exp_config, context_dir)
             experiment_id = exp.id
             self.search_method.searcher_state.last_event_id = None
 
-        self.run_experiment(experiment_id)
+        self.run_experiment(experiment_id, operations)
 
-    def load_state(self, storage_id: str) -> int:
+    def load_state(self, storage_id: str) -> Tuple[int, List[Operation]]:
         with self.context.checkpoint.restore_path(storage_id) as path:
-            return self.search_method.load(path)
+            experiment_id = self.search_method.load(path)
+            with path.joinpath("ops").open("rb") as ops_file:
+                operations = pickle.load(ops_file)
+            return experiment_id, operations
 
-    def save_state(self, experiment_id: int) -> None:
+    def save_state(self, experiment_id: int, operations: List[Operation]) -> None:
         with self.context.checkpoint.store_path() as (path, storage_id):
             self.search_method.save(path, experiment_id=experiment_id)
+            with path.joinpath("ops").open("wb") as ops_file:
+                pickle.dump(operations, ops_file)
