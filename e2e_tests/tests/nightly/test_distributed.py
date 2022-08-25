7b4d1b149fdfe44b11026c5bda4d3649641dd577
aciborowska <aga.ciborowska@gmail.com>
2022-08-25 15:15:07 -0700

    fmt

--- e2e_tests/tests/nightly/test_distributed.py
+++ e2e_tests/tests/nightly/test_distributed.py
@@ -235,7 +235,11 @@ def test_deepspeed_dcgan() -> None:
 @pytest.mark.deepspeed
 @pytest.mark.gpu_required
 def test_deepspeed_cpu_offloading() -> None:
-    config = conf.load_config(conf.deepspeed_examples_path("cifar10_cpu_offloading/zero_stages_3_offload.yaml"))
+    config = conf.load_config(
+        conf.deepspeed_examples_path("cifar10_cpu_offloading/zero_stages_3_offload.yaml")
+    )
     config = conf.set_max_length(config, {"batches": 100})
 
-    exp.run_basic_test_with_temp_config(config, conf.deepspeed_examples_path("cifar10_cpu_offloading"), 1)
+    exp.run_basic_test_with_temp_config(
+        config, conf.deepspeed_examples_path("cifar10_cpu_offloading"), 1
+    )
