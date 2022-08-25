2d83035dde6c7eae21f23466231ba47628e52b9f
Bradley Laney <bradley.laney@hpe.com>
2022-08-25 15:55:35 -0400

    fix: downgrade fluentbit to fix tls.vhost issues

--- master/pkg/aproto/defaults.go
+++ master/pkg/aproto/defaults.go
@@ -1,6 +1,8 @@
 package aproto
 
 const (
-	// FluentImage is docker image id to use for the managed Fluent Bit daemon.
-	FluentImage = "fluent/fluent-bit:1.9.3"
+	// FluentImage is docker image id to use for the managed Fluent Bit daemon. Fluent Bit did a
+	// rewrite of their TLS stack that introduced a bug (unconfirmed with them) with `tls.vhost`
+	// in 1.7, so take care before upgrading this past that (1.9.3 did not work).
+	FluentImage = "fluent/fluent-bit:1.6"
 )
