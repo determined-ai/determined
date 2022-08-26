64d3666fe7c71d3c5e22081ea671384a0d5929d7
drh-determined-ai <daniel.robert.hunter@hpe.com>
2022-08-26 19:10:09 +0000

    Revert back to Apex opt_level O2 because patching torch functions causes bugs in following tests

--- harness/tests/experiment/fixtures/pytorch_onevar_model.py
+++ harness/tests/experiment/fixtures/pytorch_onevar_model.py
@@ -276,7 +276,7 @@ class OneVarApexAMPTrial(OneVarAMPBaseTrial):
         self.model, self.optimizer = self.context.configure_apex_amp(
             models=self.model,
             optimizers=self.opt,
-            opt_level="O1",
+            opt_level="O2",
         )
 
     def train_batch(
