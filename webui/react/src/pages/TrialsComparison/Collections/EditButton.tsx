7ced30ef170e8a50144aba56f1cfeab2e5022b27
Keita Nonaka <keita.nonaka@hpe.com>
2022-08-30 15:04:10 -0700

    chore: add input and same width for edit modal (#4910)
    * chore: adjust width of modal
    
    * chore: add input in edit modal

--- webui/react/src/pages/TrialsComparison/Collections/EditButton.tsx
+++ webui/react/src/pages/TrialsComparison/Collections/EditButton.tsx
@@ -1,30 +1,35 @@
-import { Button, Modal, notification } from 'antd';
+import { Button, Form, Input, Modal, notification } from 'antd';
 import yaml from 'js-yaml';
 import React, { useCallback, useState } from 'react';
 
 import MonacoEditor from 'components/MonacoEditor';
-import Spinner from 'shared/components/Spinner';
 
 import { TrialFilters } from './filters';
 
 interface Props {
   collectionName: string;
   filters: TrialFilters;
-  saveCollection: () => void;
+  saveCollection: (name: string) => void;
+}
+
+interface FormInputs {
+  collectionName: string;
 }
 
 const EditButton: React.FC<Props> = ({ saveCollection, filters, collectionName }) => {
+  const [ form ] = Form.useForm<FormInputs>();
   const [ isModalVisible, setIsModalVisible ] = useState<boolean>(false);
 
   const onShowModal = useCallback(() => setIsModalVisible(true), []);
 
   const onHideModal = useCallback(() => setIsModalVisible(false), []);
 
-  const onSubmit = useCallback(() => {
-    saveCollection();
+  const onSubmit = useCallback(async () => {
+    const values = await form.validateFields();
+    saveCollection(values.collectionName);
     notification.success({ message: 'Collection is sucessfully saved!' });
     onHideModal();
-  }, [ onHideModal, saveCollection ]);
+  }, [ form, onHideModal, saveCollection ]);
 
   return (
     <>
@@ -32,10 +37,21 @@ const EditButton: React.FC<Props> = ({ saveCollection, filters, collectionName }
       <Modal
         title={`Edit Collection: ${collectionName}`}
         visible={isModalVisible}
+        width={'clamp(280px, 416px, calc(100vw - 16px))'}
         onCancel={onHideModal}
         onOk={onSubmit}>
-        <React.Suspense
-          fallback={<div><Spinner tip="Loading text editor..." /></div>}>
+        <Form autoComplete="off" form={form} layout="vertical">
+          <Form.Item
+            initialValue={collectionName}
+            name="collectionName"
+            rules={[ { message: 'Collection name is required ', required: true } ]}>
+            <Input
+              allowClear
+              bordered={true}
+              placeholder="enter collection name"
+              onPressEnter={onSubmit}
+            />
+          </Form.Item>
           <MonacoEditor
             height="40vh"
             language="yaml"
@@ -46,7 +62,7 @@ const EditButton: React.FC<Props> = ({ saveCollection, filters, collectionName }
             }}
             value={yaml.dump(filters)}
           />
-        </React.Suspense>
+        </Form>
       </Modal>
     </>
   );
