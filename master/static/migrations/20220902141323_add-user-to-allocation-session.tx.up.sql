84ed1f564c5466e4a07b8763c55f7be4404fdb51
Nicholas Blaskey <nick.blaskey@hpe.com>
2022-09-02 15:11:42 -0400

    fmt

--- master/static/migrations/20220902141323_add-user-to-allocation-session.tx.up.sql
+++ master/static/migrations/20220902141323_add-user-to-allocation-session.tx.up.sql
@@ -1,9 +1,9 @@
 ALTER TABLE public.allocation_sessions
-	ADD COLUMN owner_id int REFERENCES users(id);
+    ADD COLUMN owner_id int REFERENCES users(id);
 
 -- Add owner_id for in progress trials.
 UPDATE public.allocation_sessions allocation_sessions
-	SET owner_id = experiments.owner_id
+    SET owner_id = experiments.owner_id
     FROM public.experiments experiments
     INNER JOIN public.trials trials ON experiments.id = trials.experiment_id
     INNER JOIN public.allocations allocations ON trials.task_id = allocations.task_id
