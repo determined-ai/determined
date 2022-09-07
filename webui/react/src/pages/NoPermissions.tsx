aa4b4440396d899e4b130688c2d04312a748d577
julian-determined-ai <julian.glover@hpe.com>
2022-09-07 11:13:15 -0500

    chore: update copy regarding admin contact

--- webui/react/src/pages/NoPermissions.tsx
+++ webui/react/src/pages/NoPermissions.tsx
@@ -8,11 +8,6 @@ import css from './NoPermissions.module.scss';
 
 const NoPermissions: React.FC = () => {
 
-  const { users } = useStore();
-
-  const systemAdmin = users.find((user) => user.isAdmin);
-  // TODO: Implement querying for the system admin
-
   const storeDispatch = useStoreDispatch();
   useEffect(() => storeDispatch({ type: StoreAction.HideUIChrome }), [ storeDispatch ]);
 
@@ -26,7 +21,7 @@ const NoPermissions: React.FC = () => {
         <h6>You don&lsquo;t have access to a workspace</h6>
         <p className={css.message}>
           In order to access and use Determined you need to be added to a workspace.
-          Contact {systemAdmin?.username } to request to be added to a workspace.
+          Contact your admin to request to be added to a workspace.
         </p>
       </div>
     </Page>
