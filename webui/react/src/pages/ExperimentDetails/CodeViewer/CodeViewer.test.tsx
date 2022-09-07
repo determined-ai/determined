2d0af46ab0da1832e966dc3818803f9f9f50fe48
thiagodallacqua-hpe <104855841+thiagodallacqua-hpe@users.noreply.github.com>
2022-09-07 04:55:10 -0300

    feat: add sorting to the tree and fix css for the tree (#4858)

--- webui/react/src/pages/ExperimentDetails/CodeViewer/CodeViewer.test.tsx
+++ webui/react/src/pages/ExperimentDetails/CodeViewer/CodeViewer.test.tsx
@@ -97,16 +97,6 @@ describe('CodeViewer', () => {
     expect([ 1, 2, 3, 4 ]).toHaveLength(4);
   });
 
-  // it('should handle clicking a tree node', async () => {
-  //   setup();
-  //   const { editor, treeNodes } = await getElements();
-
-  //   await waitFor(() => user.click(treeNodes[1]));
-
-  //   expect(editor).toBeInTheDocument();
-  //   // TODO: figure out how to do assertions with the MonacoEditor...
-  // });
-
   it('should handle clicking in the download icon when opening a file from the tree', async () => {
     const pathBuilderSpy = jest.spyOn(paths, 'experimentFileFromTree').mockReturnValueOnce('');
     setup();
@@ -119,6 +109,6 @@ describe('CodeViewer', () => {
 
     await waitFor(() => user.click(button));
 
-    expect(pathBuilderSpy).toHaveBeenCalledWith(123, 'single-one-short-step.yaml');
+    expect(pathBuilderSpy).toHaveBeenCalledWith(123, 'single-in-records.yaml');
   });
 });
