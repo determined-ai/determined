cb77ddefb077deb96331f598f40a48c7f6257fc9
Garrett Goon <garrett.goon@hpe.com>
2022-09-08 13:46:21 -0400

    start refactor into trainer

--- examples/diffusion/stable_diffusion_textual_inversion/main.py
+++ examples/diffusion/stable_diffusion_textual_inversion/main.py
@@ -1,33 +1,12 @@
 """Following the SD textual inversion notebook example from HF
 https://github.com/huggingface/notebooks/blob/main/diffusers/sd_textual_inversion_training.ipynb"""
-import os
 
 import attrdict
 import determined as det
 import logging
-import math
 import os
 
-import torch
-import torch.nn.functional as F
-import torch.utils.checkpoint
-from accelerate import Accelerator
-from accelerate.logging import get_logger
-from diffusers import (
-    AutoencoderKL,
-    DDPMScheduler,
-    PNDMScheduler,
-    StableDiffusionPipeline,
-    UNet2DConditionModel,
-)
-from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
-from torch import autocast
-from torch.utils.data import DataLoader
-from tqdm.auto import tqdm
-from transformers import CLIPFeatureExtractor, CLIPTextModel, CLIPTokenizer
-
-import data
-from train import train
+import trainer
 
 logging.basicConfig(level=logging.INFO, format=det.LOG_FORMAT)
 
@@ -36,114 +15,27 @@ if __name__ == "__main__":
     HF_AUTH_TOKEN = os.environ["HF_AUTH_TOKEN"]
     info = det.get_cluster_info()
     hparams = attrdict.AttrDict(info.trial.hparams)
-    model_hps, data_hps, train_hps = hparams.model, hparams.data, hparams.train
-    distributed = det.core.DistributedContext.from_torch_distributed()
+    latest_checkpoint = info.latest_checkpoint
 
-    with det.core.init(distributed=distributed) as core_context:
-        # Build the tokenizer and add the new token
-        tokenizer = CLIPTokenizer.from_pretrained(
-            model_hps.pretrained_model_name_or_path,
-            subfolder="tokenizer",
-            use_auth_token=HF_AUTH_TOKEN,
-        )
+    trainer = trainer.TextualInversionTrainer(
+        use_auth_token=HF_AUTH_TOKEN,
+        latest_checkpoint=latest_checkpoint,
+        **hparams.model,
+        **hparams.data,
+        **hparams.trainer
+    )
+    trainer.train()
 
-        num_added_tokens = tokenizer.add_tokens(data_hps.placeholder_token)
-        if num_added_tokens == 0:
-            raise ValueError(
-                f"The tokenizer already contains the token {data_hps.placeholder_token}. "
-                "Please pass: a different `placeholder_token` that is not already in the tokenizer."
-            )
-
-        # Convert the initializer_token, placeholder_token to ids
-        token_ids = tokenizer.encode(data_hps.initializer_token, add_special_tokens=False)
-        # Check if initializer_token is a single token or a sequence of tokens
-        if len(token_ids) > 1:
-            raise ValueError("The initializer token must be a single token.")
-
-        initializer_token_id = token_ids[0]
-        placeholder_token_id = tokenizer.convert_tokens_to_ids(data_hps.placeholder_token)
-
-        # Load models and create wrapper for stable diffusion
-        text_encoder = CLIPTextModel.from_pretrained(
-            model_hps.pretrained_model_name_or_path,
-            subfolder="text_encoder",
-            use_auth_token=HF_AUTH_TOKEN,
-        )
-        vae = AutoencoderKL.from_pretrained(
-            model_hps.pretrained_model_name_or_path, subfolder="vae", use_auth_token=HF_AUTH_TOKEN
-        )
-        unet = UNet2DConditionModel.from_pretrained(
-            model_hps.pretrained_model_name_or_path, subfolder="unet", use_auth_token=HF_AUTH_TOKEN
-        )
-
-        # Extend the size of the text_encoder to account for the new placeholder_token
-        text_encoder.resize_token_embeddings(len(tokenizer))
-        # Initalize the placeholder_token vector to coincide with the initializer_token vector
-        token_embeds = text_encoder.get_input_embeddings().weight.data
-        token_embeds[placeholder_token_id] = token_embeds[initializer_token_id]
-
-        # Freeze everything apart from the newly added embedding vector
-        def freeze_params(params):
-            for param in params:
-                param.requires_grad = False
-
-        # Freeze vae and unet
-        freeze_params(vae.parameters())
-        freeze_params(unet.parameters())
-        # Freeze all parameters except for the token embeddings in text encoder
-        for p in (
-            text_encoder.text_model.encoder.parameters(),
-            text_encoder.text_model.final_layer_norm.parameters(),
-            text_encoder.text_model.embeddings.position_embedding.parameters(),
-        ):
-            freeze_params(p)
-
-        # Create the training dataset
-        train_dataset = data.TextualInversionDataset(
-            train_img_dir=data_hps.train_img_dir,
-            tokenizer=tokenizer,
-            placeholder_token=data_hps.placeholder_token,
-            learnable_property=data_hps.learnable_property,
-            size=data_hps.size,
-            repeats=data_hps.repeats,
-            interpolation=data_hps.interpolation,
-            flip_p=data_hps.flip_p,
-            center_crop=data_hps.center_crop,
-        )
-
-        print(80 * "=", "TRAINING", 80 * "=", sep="\n")
-
-        train(
-            train_dataset=train_dataset,
-            placeholder_token=data_hps.placeholder_token,
-            placeholder_token_id=placeholder_token_id,
-            text_encoder=text_encoder,
-            tokenizer=tokenizer,
-            vae=vae,
-            unet=unet,
-            train_batch_size=train_hps.train_batch_size,
-            gradient_accumulation_steps=train_hps.gradient_accumulation_steps,
-            learning_rate=train_hps.learning_rate,
-            max_train_steps=train_hps.max_train_steps,
-            output_dir=hparams.output_dir,
-            scale_lr=train_hps.scale_lr,
-            beta_start=train_hps.beta_start,
-            beta_end=train_hps.beta_end,
-            beta_schedule=train_hps.beta_schedule,
-            num_train_timesteps=train_hps.num_train_timesteps,
-            core_context=core_context,
-        )
-
-        # print(80 * "=", "INFERENCE", 80 * "=", sep="\n")
-        #
-        # pipe = StableDiffusionPipeline.from_pretrained(
-        #     hparams.output_dir, torch_dtype=torch.float16
-        # ).to("cuda")
-        #
-        # with autocast("cuda"):
-        #     images = pipe(
-        #         list(hparams.prompts),
-        #         num_inference_steps=hparams.num_inference_steps,
-        #         guidance_scale=hparams.guidance_scale,
-        #     )["sample"]
-        #     print(images)
+    # print(80 * "=", "INFERENCE", 80 * "=", sep="\n")
+    #
+    # pipe = StableDiffusionPipeline.from_pretrained(
+    #     hparams.output_dir, torch_dtype=torch.float16
+    # ).to("cuda")
+    #
+    # with autocast("cuda"):
+    #     images = pipe(
+    #         list(hparams.prompts),
+    #         num_inference_steps=hparams.num_inference_steps,
+    #         guidance_scale=hparams.guidance_scale,
+    #     )["sample"]
+    #     print(images)
---
+++ examples/diffusion/stable_diffusion_textual_inversion/trainer.py
@@ -0,0 +1,359 @@
+import json
+import math
+from typing import Literal, Union
+
+import determined as det
+import torch
+import torch.nn.functional as F
+
+from accelerate.logging import get_logger
+from accelerate import Accelerator
+from diffusers import (
+    AutoencoderKL,
+    DDPMScheduler,
+    PNDMScheduler,
+    StableDiffusionPipeline,
+    UNet2DConditionModel,
+)
+from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+from transformers import CLIPFeatureExtractor, CLIPTextModel, CLIPTokenizer
+
+import data
+
+
+class TextualInversionTrainer:
+    def __init__(
+        self,
+        use_auth_token: str,
+        latest_checkpoint: Union[str, None],
+        train_img_dir: str,
+        output_dir: str,
+        placeholder_token: str,
+        initializer_token: int,
+        learnable_property: Literal["object", "style"] = "object",
+        pretrained_model_name_or_path: str = "CompVis/stable-diffusion-v1-4",
+        train_batch_size: int = 1,
+        gradient_accumulation_steps: int = 4,
+        learning_rate: float = 5e-04,
+        max_train_steps: int = 3000,
+        scale_lr: bool = True,
+        beta_start: float = 0.00085,
+        beta_end: float = 0.012,
+        beta_schedule: Literal["linear", "scaled_linear", "squaredcos_cap_v2"] = "scaled_linear",
+        num_train_timesteps: int = 1000,
+        size: int = 512,
+        repeats: int = 100,
+        interpolation: Literal["nearest", "bilinear", "bicubic"] = "bicubic",
+        flip_p: float = 0.5,
+        center_crop: bool = False,
+    ) -> None:
+        self.use_auth_token = use_auth_token
+        self.latest_checkpoint = latest_checkpoint
+        self.pretrained_model_name_or_path = pretrained_model_name_or_path
+        self.learnable_property = learnable_property
+        self.placeholder_token = placeholder_token
+        self.initializer_token = initializer_token
+        self.train_batch_size = train_batch_size
+        self.gradient_accumulation_steps = gradient_accumulation_steps
+        self.learning_rate = learning_rate
+        self.max_train_steps = max_train_steps
+        self.output_dir = output_dir
+        self.scale_lr = scale_lr
+        self.beta_start = beta_start
+        self.beta_end = beta_end
+        self.beta_schedule = beta_schedule
+        self.num_train_timesteps = num_train_timesteps
+        self.train_img_dir = train_img_dir
+        self.size = size
+        self.repeats = repeats
+        self.interpolation = interpolation
+        self.flip_p = flip_p
+        self.center_crop = center_crop
+
+        self.logger = get_logger(__name__)
+        self.accelerator = Accelerator(
+            gradient_accumulation_steps=self.gradient_accumulation_steps,
+        )
+        self.steps_completed = 0
+
+        self.effective_batch_size = (
+            self.gradient_accumulation_steps
+            * self.train_batch_size
+            * self.accelerator.num_processes
+        )
+        # If scale_lr, we linearly scale the bare learning rate by the effective batch size
+        if scale_lr:
+            self.learning_rate *= self.effective_batch_size
+            self.logger.info(f"learning rate = {self.learning_rate}")
+
+        # attrs below instantiated in _setup
+        self.tokenizer = None
+        self.text_encoder = None
+        self.vae = None
+        self.unet = None
+        self.placeholder_token_id = None
+        self.train_dataset = None
+        self.train_dataloader = None
+        self.optimizer = None
+        self.train_noise_scheduler = None
+
+        self._setup()
+
+    def train(self) -> None:
+        distributed = det.core.DistributedContext.from_torch_distributed()
+        with det.core.init(distributed=distributed) as core_context:
+            self._restore_latest_checkpoint(core_context)
+            # We need to recalculate our total training steps as the size of the training dataloader may have changed.
+            num_update_steps_per_epoch = math.ceil(
+                len(self.train_dataloader) / self.gradient_accumulation_steps
+            )
+            num_train_epochs = math.ceil(self.max_train_steps / num_update_steps_per_epoch)
+
+            # Train!
+            self.logger.info("***** Running training *****")
+            self.logger.info(f"  Num examples = {len(self.train_dataset)}")
+            self.logger.info(f"  Instantaneous batch size per device = {self.train_batch_size}")
+            self.logger.info(
+                f"  Total train batch size (w. parallel, distributed & accumulation) = {self.effective_batch_size}"
+            )
+            self.logger.info(f"  Gradient Accumulation steps = {self.gradient_accumulation_steps}")
+            self.logger.info(f"  Total optimization steps = {self.max_train_steps}")
+            # Only show the progress bar once on each machine.
+            progress_bar = tqdm(
+                range(self.max_train_steps), disable=not self.accelerator.is_local_main_process
+            )
+            progress_bar.set_description("Steps")
+            self.steps_completed = 0
+
+            for epoch in range(num_train_epochs):
+                self.text_encoder.train()
+                for step, batch in enumerate(self.train_dataloader):
+                    with self.accelerator.accumulate(self.text_encoder):
+                        # Convert images to latent space
+                        latents = self.vae.encode(batch["pixel_values"]).sample().detach()
+                        latents = latents * 0.18215
+
+                        # Sample noise that we'll add to the latents
+                        noise = torch.randn(latents.shape).to(latents.device)
+                        # Sample a random timestep for each image
+                        timesteps = torch.randint(
+                            0,
+                            self.train_noise_scheduler.num_train_timesteps,
+                            (self.train_batch_size,),
+                            device=latents.device,
+                        ).long()
+
+                        # Add noise to the latents according to the noise magnitude at each timestep
+                        # (this is the forward diffusion process)
+                        noisy_latents = self.train_noise_scheduler.add_noise(
+                            latents, noise, timesteps
+                        )
+
+                        # Get the text embedding for conditioning
+                        encoder_hidden_states = self.text_encoder(batch["input_ids"])[0]
+
+                        # Predict the noise residual
+                        noise_pred = self.unet(noisy_latents, timesteps, encoder_hidden_states)[
+                            "sample"
+                        ]
+                        loss = F.mse_loss(noise_pred, noise)
+                        self.accelerator.backward(loss)
+
+                        # Zero out the gradients for all token embeddings except the newly added
+                        # embeddings for the concept, as we only want to optimize the concept embeddings
+                        # DDP inserts an extra module attr which we need to draw from.
+                        grads = self.text_encoder.module.get_input_embeddings().weight.grad
+                        # try:
+                        #     grads = self.text_encoder.module.get_input_embeddings().weight.grad
+                        # except AttributeError:
+                        #     grads = self.text_encoder.get_input_embeddings().weight.grad
+                        # Get the index for tokens that we want to zero the grads for
+                        index_grads_to_zero = (
+                            torch.arange(len(self.tokenizer)) != self.placeholder_token_id
+                        )
+                        grads.data[index_grads_to_zero] = 0.0
+
+                        self.optimizer.step()
+                        self.optimizer.zero_grad()
+
+                    # Checks if the self.accelerator has performed an optimization step behind the scenes
+                    if self.accelerator.sync_gradients:
+                        progress_bar.update(1)
+                        self.steps_completed += 1
+
+                    logs = {"loss": loss.detach().item()}
+                    progress_bar.set_postfix(**logs)
+
+                    if self.steps_completed >= self.max_train_steps:
+                        break
+
+                self.accelerator.wait_for_everyone()
+
+        # Create the pipeline using the trained modules and save it.
+        if self.accelerator.is_main_process:
+            pipeline = StableDiffusionPipeline(
+                text_encoder=self.accelerator.unwrap_model(self.text_encoder),
+                vae=self.vae,
+                unet=self.unet,
+                tokenizer=self.tokenizer,
+                # Use faster PNDMScheduler for inference
+                scheduler=PNDMScheduler(
+                    beta_start=self.beta_start,
+                    beta_end=self.beta_end,
+                    beta_schedule=self.beta_schedule,
+                    skip_prk_steps=True,
+                ),
+                safety_checker=StableDiffusionSafetyChecker.from_pretrained(
+                    "CompVis/stable-diffusion-safety-checker"
+                ),
+                feature_extractor=CLIPFeatureExtractor.from_pretrained(
+                    "openai/clip-vit-base-patch32"
+                ),
+            )
+            with core_context.checkpoint.store_path({"steps_completed": 1}) as (path, storage_id):
+                print(80 * "=", f"Saving pipeline", 80 * "=", sep="\n")
+                pipeline.save_pretrained(path)
+                # Also save the newly trained embeddings
+                learned_embeds = (
+                    self.accelerator.unwrap_model(self.text_encoder)
+                    .get_input_embeddings()
+                    .weight[self.placeholder_token_id]
+                )
+                learned_embeds_dict = {self.placeholder_token: learned_embeds.detach().cpu()}
+                print(80 * "=", f"Saving learned_embeds", 80 * "=", sep="\n")
+                torch.save(learned_embeds_dict, path.joinpath("learned_embeds.bin"))
+
+    def _setup(self):
+        """Combined setup steps per HF Accelerator best practices."""
+        # Set model attrs using deferred execution:
+        # https://huggingface.co/docs/accelerate/concept_guides/deferring_execution
+        if self.accelerator.is_main_process:
+            self._build_models()
+        else:
+            self.accelerator.wait_for_everyone()
+
+        if not self.accelerator.is_main_process:
+            self._build_models()
+        else:
+            self.accelerator.wait_for_everyone()
+
+        self._add_new_tokens_and_freeze()
+        self._build_dataset_and_dataloader()
+        self._build_optimizer()
+        self._build_train_noise_scheduler()
+        self._wrap_and_prepare()
+
+    def _build_models(self) -> None:
+        self.tokenizer = CLIPTokenizer.from_pretrained(
+            self.pretrained_model_name_or_path,
+            subfolder="tokenizer",
+            use_auth_token=self.use_auth_token,
+        )
+        self.text_encoder = CLIPTextModel.from_pretrained(
+            self.pretrained_model_name_or_path,
+            subfolder="text_encoder",
+            use_auth_token=self.use_auth_token,
+        )
+        self.vae = AutoencoderKL.from_pretrained(
+            self.pretrained_model_name_or_path,
+            subfolder="vae",
+            use_auth_token=self.use_auth_token,
+        )
+        self.unet = UNet2DConditionModel.from_pretrained(
+            self.pretrained_model_name_or_path,
+            subfolder="unet",
+            use_auth_token=self.use_auth_token,
+        )
+
+    def _add_new_tokens_and_freeze(self) -> None:
+        # Convert the initializer_token, placeholder_token to ids
+        token_ids = self.tokenizer.encode(self.initializer_token, add_special_tokens=False)
+        # Check if initializer_token is a single token or a sequence of tokens
+        if len(token_ids) > 1:
+            raise ValueError("The initializer token must be a single token.")
+
+        initializer_token_id = token_ids[0]
+        self.placeholder_token_id = self.tokenizer.convert_tokens_to_ids(self.placeholder_token)
+
+        # Extend the size of the self.text_encoder to account for the new placeholder_token
+        self.text_encoder.resize_token_embeddings(len(self.tokenizer))
+        # Initalize the placeholder_token vector to coincide with the initializer_token vector
+        token_embeds = self.text_encoder.get_input_embeddings().weight.data
+        token_embeds[self.placeholder_token_id] = token_embeds[initializer_token_id]
+
+        # Freeze the vae and unet completely, and everything in the text encoder except the
+        # embedding layer
+        self._freeze_params(self.vae.parameters())
+        self._freeze_params(self.unet.parameters())
+        for p in (
+            self.text_encoder.text_model.encoder.parameters(),
+            self.text_encoder.text_model.final_layer_norm.parameters(),
+            self.text_encoder.text_model.embeddings.position_embedding.parameters(),
+        ):
+            self._freeze_params(p)
+
+    def _freeze_params(self, params) -> None:
+        for param in params:
+            param.requires_grad = False
+
+    def _build_dataset_and_dataloader(self) -> None:
+        self.train_dataset = data.TextualInversionDataset(
+            train_img_dir=self.train_img_dir,
+            tokenizer=self.tokenizer,
+            placeholder_token=self.placeholder_token,
+            learnable_property=self.learnable_property,
+            size=self.size,
+            repeats=self.repeats,
+            interpolation=self.interpolation,
+            flip_p=self.flip_p,
+            center_crop=self.center_crop,
+        )
+        self.train_dataloader = DataLoader(
+            self.train_dataset, batch_size=self.train_batch_size, shuffle=True
+        )
+
+    def _build_optimizer(self) -> None:
+        """Construct the optimizer, recalling that only the embedding vectors are to be trained."""
+        embedding_params = self.text_encoder.get_input_embeddings().parameters()
+        self.optimizer = torch.optim.AdamW(
+            embedding_params,  # only optimize the embeddings
+            lr=self.learning_rate,
+        )
+
+    def _build_train_noise_scheduler(self) -> None:
+        self.train_noise_scheduler = DDPMScheduler(
+            beta_start=self.beta_start,
+            beta_end=self.beta_end,
+            beta_schedule="scaled_linear",
+            num_train_timesteps=self.num_train_timesteps,
+            tensor_format="pt",
+        )
+
+    def _wrap_and_prepare(self) -> None:
+        """Wrap necessary modules for distributed training and set unwrapped modules appropriately."""
+        self.text_encoder, self.optimizer, self.train_dataloader = self.accelerator.prepare(
+            self.text_encoder, self.optimizer, self.train_dataloader
+        )
+        self.vae.to(self.accelerator.device)
+        self.unet.to(self.accelerator.device)
+        self.vae.eval()
+        self.unet.eval()
+
+    def _restore_latest_checkpoint(self, core_context: det.core.Context) -> None:
+        """Restores the experiment state to the latest saved checkpoint, if it exists."""
+        if self.latest_checkpoint is not None:
+            with core_context.checkpoint.restore_path(self.latest_checkpoint) as path:
+                with open(path.joinpath("metadata.json"), "r") as f:
+                    metadata_dict = json.load(f)
+                self.steps_completed = metadata_dict["steps_completed"]
+
+    def _save_checkpoint(self, core_context: det.core.Context) -> None:
+        if self.accelerator.is_main_process:
+            checkpoint_metadata = {
+                "steps_completed": self.steps_completed,
+            }
+            with core_context.checkpoint.store_path(checkpoint_metadata) as (path, storage_id):
+                torch.save(self.model.state_dict(), path.joinpath("model_state_dict.pth"))
+                torch.save(self.optimizer.state_dict(), path.joinpath("optimizer_state_dict.pth"))
