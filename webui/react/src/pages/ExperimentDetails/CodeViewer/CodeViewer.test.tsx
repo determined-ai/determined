120153fb50267cada81e58582dc6c52d35f088cd
Thiago Dallacqua <thiago.menezes-dallacqua@hpe.com>
2022-09-09 14:31:32 -0300

    fix: update test to fit last implementation

--- webui/react/src/pages/ExperimentDetails/CodeViewer/CodeViewer.test.tsx
+++ webui/react/src/pages/ExperimentDetails/CodeViewer/CodeViewer.test.tsx
@@ -74,7 +74,7 @@ jest.mock('components/MonacoEditor', () => ({
 
 jest.mock('hooks/useSettings', () => {
   const actualModule = jest.requireActual('hooks/useSettings');
-  const useSettings = jest.fn(() => ({ settings: { fileName: '' }, updateSettings: jest.fn() }));
+  const useSettings = jest.fn(() => ({ settings: { fileName: '', filepath: '' }, updateSettings: jest.fn() }));
 
   return {
     __esModule: true,
