63f0eb78debb5a2fe638d110122ee9ad2888b44f
aciborowska <35503877+aciborowska@users.noreply.github.com>
2022-09-08 21:56:01 -0400

    test: fault tolerance for core api searcher [DET-8272] (#4972)

--- harness/determined/searcher/core_search_runner.py
+++ harness/determined/searcher/core_search_runner.py
@@ -1,12 +1,12 @@
 import logging
+import os
 import pickle
 from typing import Any, Dict, List, Optional, Tuple
 
 import determined as det
 from determined.experimental import client
-from determined.searcher.search_method import SearchMethod
+from determined.searcher.search_method import Operation, SearchMethod
 from determined.searcher.search_runner import SearchRunner
-from harness.determined.searcher.search_method import Operation
 
 
 class CoreSearchRunner(SearchRunner):
@@ -14,6 +14,7 @@ class CoreSearchRunner(SearchRunner):
         super().__init__(search_method)
         self.context = context
         info = det.get_cluster_info()
+
         assert info is not None, "CoreSearchRunner only runs on-cluster"
         self.latest_checkpoint = info.latest_checkpoint
 
@@ -21,18 +22,28 @@ class CoreSearchRunner(SearchRunner):
         self,
         exp_config: Dict[str, Any],
         context_dir: Optional[str] = None,
-    ) -> None:
+    ) -> int:
         logging.info("CoreSearchRunner.run")
 
         operations: Optional[List[Operation]] = None
+
+        if context_dir is None:
+            context_dir = os.getcwd()
+
         if self.latest_checkpoint is not None:
             experiment_id, operations = self.load_state(self.latest_checkpoint)
+            logging.info(f"Resuming HP searcher for experiment {experiment_id}")
         else:
+            logging.info("No latest checkpoint. Starting new experiment.")
             exp = client.create_experiment(exp_config, context_dir)
-            experiment_id = exp.id
+            self.search_method.searcher_state.experiment_id = exp.id
             self.search_method.searcher_state.last_event_id = 0
+            self.save_state(exp.id, [])
+            experiment_id = exp.id
 
         self.run_experiment(experiment_id, operations)
+
+        return experiment_id
 
     def load_state(self, storage_id: str) -> Tuple[int, List[Operation]]:
         with self.context.checkpoint.restore_path(storage_id) as path:
@@ -42,7 +53,9 @@ class CoreSearchRunner(SearchRunner):
             return experiment_id, operations
 
     def save_state(self, experiment_id: int, operations: List[Operation]) -> None:
-        with self.context.checkpoint.store_path() as (path, storage_id):
+        steps_completed = self.search_method.searcher_state.last_event_id
+        metadata = {"steps_completed": steps_completed}
+        with self.context.checkpoint.store_path(metadata) as (path, storage_id):
             self.search_method.save(path, experiment_id=experiment_id)
             with path.joinpath("ops").open("wb") as ops_file:
                 pickle.dump(operations, ops_file)
