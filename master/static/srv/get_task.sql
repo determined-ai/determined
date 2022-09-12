c21bd942f392d2ca55e73401169df17c610d6c3e
gt2345 <gt2345@columbia.edu>
2022-09-12 14:56:01 -0500

    use task id in sql

--- master/static/srv/get_task.sql
+++ master/static/srv/get_task.sql
@@ -1,7 +1,7 @@
 SELECT tasks.task_id, (SELECT coalesce(jsonb_agg(allo ORDER BY end_time DESC NULLS FIRST), '[]'::jsonb) FROM (
   SELECT allocation_id, task_id, 'STATE_' || CASE WHEN (task_type = 'TENSORBOARD' AND state = 'PENDING') THEN 
     CASE WHEN EXISTS (
-      select 1 from raw_steps, raw_validations, trials, tasks where trials.task_id=tasks.task_id and (raw_steps.trial_id=trials.id or raw_validations.trial_id=trials.id) 
+      select 1 from raw_steps, raw_validations, trials, tasks where trials.task_id=$1 and (raw_steps.trial_id=trials.id or raw_validations.trial_id=trials.id) 
     ) THEN 'PENDING' ELSE 'WAITING' END 
    ELSE state::text END AS state , is_ready, start_time, end_time
   FROM allocations
