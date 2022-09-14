85cf4a3c4e59f64a3bf30eafeb925f7febd2f4fe
gt2345 <gt2345@columbia.edu>
2022-09-14 15:47:38 -0500

    reset postgres tasks file

--- master/internal/db/postgres_tasks.go
+++ master/internal/db/postgres_tasks.go
@@ -15,6 +15,8 @@ import (
 	"github.com/o1egl/paseto"
 	"github.com/pkg/errors"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/determined-ai/determined/master/pkg/model"
 )
 
@@ -163,7 +165,7 @@ func (db *PgDB) StartAllocationSession(
 	}
 
 	query := `
-INSERT INTO allocation_sessions (allocation_id, owner_id) VALUES
+INSERT INTO allocation_sessions (allocation_id, owner_id) VALUES 
 	(:allocation_id, :owner_id) RETURNING id`
 	if err := db.namedGet(&taskSession.ID, query, *taskSession); err != nil {
 		return "", err
@@ -175,6 +177,30 @@ INSERT INTO allocation_sessions (allocation_id, owner_id) VALUES
 		return "", errors.Wrap(err, "failed to generate task authentication token")
 	}
 	return token, nil
+}
+
+// AllocationSessionByToken returns a task session given an authentication token.
+func (db *PgDB) AllocationSessionByToken(token string) (*model.AllocationSession, error) {
+	v2 := paseto.NewV2()
+
+	var session model.AllocationSession
+	err := v2.Verify(token, db.tokenKeys.PublicKey, &session, nil)
+	if err != nil {
+		log.WithError(err).Debug("failed to verify allocation_session token")
+		return nil, ErrNotFound
+	}
+
+	query := `SELECT * FROM allocation_sessions WHERE id=$1`
+	if err := db.query(query, &session, session.ID); errors.Cause(err) == ErrNotFound {
+		log.WithField("allocation_sessions.id", session.ID).Debug("allocation_session not found")
+		return nil, ErrNotFound
+	} else if err != nil {
+		log.WithError(err).WithField("allocation_sessions.id", session.ID).
+			Debug("failed to lookup allocation_session")
+		return nil, err
+	}
+
+	return &session, nil
 }
 
 // DeleteAllocationSession deletes the task session with the given AllocationID.
