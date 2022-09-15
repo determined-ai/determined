ae0e6510f696f0faa8c1a467c65731d99036ec3c
Nicholas Blaskey <nick.blaskey@hpe.com>
2022-09-15 12:16:04 -0400

    sql fmt

--- master/static/migrations/20220914110745_add-users-to-personal-groups.tx.up.sql
+++ master/static/migrations/20220914110745_add-users-to-personal-groups.tx.up.sql
@@ -1,6 +1,6 @@
 INSERT INTO groups(group_name, user_id)
-SELECT username || 'DeterminedPersonalGroup' as group_name, id AS user_id FROM users;
+SELECT username || 'DeterminedPersonalGroup' AS group_name, id AS user_id FROM users;
 
 INSERT INTO user_group_membership(user_id, group_id)
-SELECT user_id AS user_id, id as group_id FROM groups
+SELECT user_id AS user_id, id AS group_id FROM groups
     WHERE group_name LIKE '%DeterminedPersonalGroup';
