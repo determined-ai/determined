b2f5859684cc43491282f03dcd80463bca94f3e4
Emily Bonar <emily.bonar@hpe.com>
2022-09-16 16:24:45 -0400

    Small changes

--- webui/react/src/components/Dropdown.stories.tsx
+++ webui/react/src/components/Dropdown.stories.tsx
@@ -1,3 +1,4 @@
+import { ComponentStory, Meta } from '@storybook/react';
 import { Button, Menu } from 'antd';
 import React from 'react';
 
@@ -5,9 +6,8 @@ import Dropdown, { Placement } from './Dropdown';
 
 export default {
   component: Dropdown,
-  parameters: { layout: 'centered' },
   title: 'Determined/Dropdowns/Dropdown',
-};
+} as Meta<typeof Dropdown>;
 
 const content = (
   <Menu
@@ -15,93 +15,10 @@ const content = (
   />
 );
 
-export const Default = (): React.ReactNode => (
-  <Dropdown content={content}>
+export const Default: ComponentStory<typeof Dropdown> = (args) => (
+  <Dropdown {...args} content={content}>
     <Button>Default Dropdown</Button>
   </Dropdown>
 );
-export const Placements = (): React.ReactNode => {
-  return (
-    <table>
-      <tbody>
-        <tr>
-          <td />
-          <td>
-            <Dropdown content={content} placement={Placement.BottomLeft}>
-              <Button>Top Left</Button>
-            </Dropdown>
-          </td>
-          <td align="center">
-            <Dropdown content={content} placement={Placement.Top}>
-              <Button>Top</Button>
-            </Dropdown>
-          </td>
-          <td align="right">
-            <Dropdown content={content} placement={Placement.TopRight}>
-              <Button>Top Right</Button>
-            </Dropdown>
-          </td>
-          <td />
-        </tr>
-        <tr>
-          <td>
-            <Dropdown content={content} placement={Placement.LeftTop}>
-              <Button>Left Top</Button>
-            </Dropdown>
-          </td>
-          <td colSpan={3} />
-          <td align="right">
-            <Dropdown content={content} placement={Placement.RightTop}>
-              <Button>Right Top</Button>
-            </Dropdown>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <Dropdown content={content} placement={Placement.Left}>
-              <Button>Left</Button>
-            </Dropdown>
-          </td>
-          <td colSpan={3} />
-          <td align="right">
-            <Dropdown content={content} placement={Placement.Right}>
-              <Button>Right</Button>
-            </Dropdown>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <Dropdown content={content} placement={Placement.LeftBottom}>
-              <Button>Left Bottom</Button>
-            </Dropdown>
-          </td>
-          <td colSpan={3} />
-          <td align="right">
-            <Dropdown content={content} placement={Placement.RightBottom}>
-              <Button>Right Bottom</Button>
-            </Dropdown>
-          </td>
-        </tr>
-        <tr>
-          <td />
-          <td>
-            <Dropdown content={content} placement={Placement.BottomLeft}>
-              <Button>Bottom Left</Button>
-            </Dropdown>
-          </td>
-          <td align="center">
-            <Dropdown content={content} placement={Placement.Bottom}>
-              <Button>Bottom</Button>
-            </Dropdown>
-          </td>
-          <td align="right">
-            <Dropdown content={content} placement={Placement.BottomRight}>
-              <Button>Bottom Right</Button>
-            </Dropdown>
-          </td>
-          <td />
-        </tr>
-      </tbody>
-    </table>
-  );
-};
+
+Default.args = { placement: Placement.BottomLeft, showArrow: true };
--- webui/react/src/shared/themes.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from 'antd';
-import React from 'react';
-
-export default { title: 'Shared/Theme' };
-
-export const Default = (): React.ReactNode => <Button>Hello World</Button>;
-
-export const ButtonDisabled = (): React.ReactNode => <Button disabled>Hello World</Button>;
