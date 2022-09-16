9776e4b051f6f8b7dbc4dd932224a323c4b4b2c6
Daniel R Hunter <daniel.robert.hunter@hpe.com>
2022-09-16 18:16:39 -0400

    Revert "Make the change to _should_update_scaler more conservative"
    This reverts commit 7251b1f868e4dea329d76312b904b117feb06c4b.

--- harness/determined/pytorch/_pytorch_trial.py
+++ harness/determined/pytorch/_pytorch_trial.py
@@ -374,9 +374,7 @@ class PyTorchTrialController(det.TrialController):
     def _should_update_scaler(self) -> bool:
         if not self.context._scaler or not self.context.experimental._auto_amp:
             return False
-        if self.context.distributed.size > 1 or self.context._aggregation_frequency > 1:
-            return self.context._should_communicate_and_update()  # type: ignore
-        return True
+        return self.context._should_communicate_and_update()  # type: ignore
 
     def _train_for_step(
         self, step_id: int, num_batches: int, total_batches_processed: int
