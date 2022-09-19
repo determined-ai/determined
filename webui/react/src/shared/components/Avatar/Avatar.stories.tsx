261c12a3bbf80e2ef8c4bbebc4497f7ef04e4154
John Kim <john.kim@hpe.com>
2022-09-19 16:29:42 -0700

    add noColor knob to story

--- webui/react/src/shared/components/Avatar/Avatar.stories.tsx
+++ webui/react/src/shared/components/Avatar/Avatar.stories.tsx
@@ -1,4 +1,4 @@
-import { select, text, withKnobs } from '@storybook/addon-knobs';
+import { boolean, select, text, withKnobs } from '@storybook/addon-knobs';
 import React from 'react';
 
 import { DarkLight } from 'shared/themes';
@@ -7,11 +7,11 @@ import Avatar from './Avatar';
 
 export default {
   component: Avatar,
-  decorators: [withKnobs],
+  decorators: [ withKnobs ],
   title: 'Avatar',
 };
 
-const DARK_LIGHT_OPTIONS = [DarkLight.Dark, DarkLight.Light];
+const DARK_LIGHT_OPTIONS = [ DarkLight.Dark, DarkLight.Light ];
 
 export const Default = (): React.ReactNode => (
   <Avatar darkLight={DarkLight.Light} displayName="Anonymous" />
@@ -21,5 +21,6 @@ export const Custom = (): React.ReactNode => (
   <Avatar
     darkLight={select('Theme', DARK_LIGHT_OPTIONS, DarkLight.Light)}
     displayName={text('Name', 'Martin Luther King')}
+    noColor={boolean('No color mode', false)}
   />
 );
