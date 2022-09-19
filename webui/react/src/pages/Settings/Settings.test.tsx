3f41806bcda500247d802365e23ad1191a15e11e
gt2345 <gt2345@columbia.edu>
2022-09-19 12:52:19 -0500

    fix test

--- webui/react/src/pages/Settings/Settings.test.tsx
+++ webui/react/src/pages/Settings/Settings.test.tsx
@@ -1,19 +1,43 @@
 import { render, screen } from '@testing-library/react';
-import React from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { Router } from 'react-router-dom';
 
-import StoreProvider from 'contexts/Store';
+import StoreProvider, { StoreAction, useStoreDispatch } from 'contexts/Store';
 import history from 'shared/routes/history';
+import { DetailedUser } from 'types';
 
 import Settings, { TabType } from './Settings';
+
+const DISPLAY_NAME = 'Test Name';
+const USERNAME = 'test_username1';
+
+const Container: React.FC = () => {
+  const storeDispatch = useStoreDispatch();
+
+  const currentUser: DetailedUser = useMemo(() => ({
+    displayName: DISPLAY_NAME,
+    id: 1,
+    isActive: true,
+    isAdmin: true,
+    username: USERNAME,
+  }), []);
+
+  const loadUser = useCallback(() => {
+    storeDispatch({ type: StoreAction.SetCurrentUser, value: currentUser });
+  }, [ storeDispatch, currentUser ]);
+
+  useEffect(() => loadUser(), [ loadUser ]);
+
+  return <Settings />;
+};
 
 const setup = () => {
   return render(
     <StoreProvider>
       <HelmetProvider>
         <Router history={history}>
-          <Settings />
+          <Container />
         </Router>
       </HelmetProvider>
     </StoreProvider>,
