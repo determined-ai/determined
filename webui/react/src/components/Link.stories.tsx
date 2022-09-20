a18553a190134648482326589a5c0a524209e307
Emily Bonar <emily.bonar@hpe.com>
2022-09-20 16:11:50 -0400

    Fix links

--- webui/react/src/components/Link.stories.tsx
+++ webui/react/src/components/Link.stories.tsx
@@ -7,22 +7,22 @@ export default {
   title: 'Determined/Link',
 };
 
-export const Default = (): React.ReactNode => <Link path="test">Plain Link</Link>;
+export const Default = (): React.ReactNode => <Link path="">Plain Link</Link>;
 
 export const Popout = (): React.ReactNode => (
-  <Link path="test" popout>
+  <Link path="" popout>
     Plain Link
   </Link>
 );
 
 export const Disabled = (): React.ReactNode => (
-  <Link disabled path="test">
+  <Link disabled path="">
     Disabled Plain Link
   </Link>
 );
 
 export const DisabledButton = (): React.ReactNode => (
-  <Link disabled isButton path="test">
+  <Link disabled isButton path="">
     Disabled Button Link
   </Link>
 );
