190f5db4d23712cf4edb334ad61b80af0cfd6ba6
Daniel R Hunter <daniel.robert.hunter@hpe.com>
2022-09-22 15:28:37 -0400

    Fix extra import

--- harness/tests/experiment/pytorch/test_pytorch_trial.py
+++ harness/tests/experiment/pytorch/test_pytorch_trial.py
@@ -8,7 +8,6 @@ import pytest
 import torch
 
 import determined as det
-import tests.experiment.fixtures.pytorch_onevar_model
 from determined import pytorch, workload
 from tests.experiment import utils  # noqa: I100
 from tests.experiment.fixtures import pytorch_onevar_model, pytorch_xor_model
@@ -324,7 +323,7 @@ class TestPyTorchTrial:
             validation_metrics[tag] = vm
 
         controller = utils.make_trial_controller_from_trial_implementation(
-            trial_class=tests.experiment.fixtures.pytorch_onevar_model.OneVarTrialGradClipping,
+            trial_class=pytorch_onevar_model.OneVarTrialGradClipping,
             hparams=self.hparams,
             workloads=make_workloads("original"),
             trial_seed=self.trial_seed,
