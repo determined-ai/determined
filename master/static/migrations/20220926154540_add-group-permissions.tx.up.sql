73d08deecbc3655324e98a80bbf1c6ab95757001
Eric Liu <eric.liu@hpe.com>
2022-09-27 04:42:11 -0700

    fix sql syntax err

--- master/static/migrations/20220926154540_add-group-permissions.tx.up.sql
+++ master/static/migrations/20220926154540_add-group-permissions.tx.up.sql
@@ -5,5 +5,5 @@ INSERT INTO permissions(id, name, global_only) VALUES
 INSERT INTO permission_assignments(permission_id, role_id)
 SELECT id AS permission_id, 1 FROM permissions WHERE name IN (
     'create group',
-    'delete group',
+    'delete group'
 );
