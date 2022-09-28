61bfd73ff63b9ef2b9cdf278065e77f50fff037e
Eric Liu <eric.liu@hpe.com>
2022-09-28 12:27:49 -0700

    linting

--- master/internal/rbac/authz_iface.go
+++ master/internal/rbac/authz_iface.go
@@ -8,6 +8,7 @@ import (
 	"github.com/determined-ai/determined/proto/pkg/rbacv1"
 )
 
+// RBACAuthZ describes authz methods for RBAC.
 type RBACAuthZ interface {
 	// GET /api/v1/permissions/summary
 	// GetPermissionsSummary()
@@ -18,32 +19,35 @@ type RBACAuthZ interface {
 	// POST /api/v1/roles/search/by-ids
 	CanGetRoles(curUser model.User) error
 
-	// FilterRolesQuery filters a role search to show only what a user
-	// can access.
+	// FilterRolesQuery filters a role search to show only what a user can access.
 	// POST /api/v1/roles/search
 	// ListRoles
 	FilterRolesQuery(curUser model.User, query *bun.SelectQuery) (*bun.SelectQuery, error)
 
-	// CanGetUserRoles checks
+	// CanGetUserRoles checks if a user can get another user's assigned roles.
 	// GET /api/v1/roles/search/by-user/{user_id}
 	// GetRolesAssignedToUser()
-	CanGetUserRoles(curUser model.User, userId int) error
+	CanGetUserRoles(curUser model.User, userID int) error
 
+	// CanGetGroupRoles checks if a user can get the roles assigned to a group.
 	// GET /api/v1/roles/search/by-group/{group_id}
 	// GetRolesAssignedToGroup()
-	CanGetGroupRoles(curUser model.User, groupId int) error
+	CanGetGroupRoles(curUser model.User, groupID int) error
 
+	// CanSearchScope checks if a user can search a scope for roles.
 	// POST /api/v1/roles/search/by-assignability
 	// SearchRolesAssignableToScope()
-	CanSearchScope(curUser model.User, workspaceId int) error
+	CanSearchScope(curUser model.User, workspaceID int) error
 
+	// CanAssignRoles checks if a user has the assign roles permission
 	// POST /api/v1/roles/add-assignments
 	// AssignRoles()
 	// AssignWorkspaceAdminToUserTx()
 	CanAssignRoles(curUser model.User, groupRoleAssignments []*rbacv1.GroupRoleAssignment,
 		userRoleAssignments []*rbacv1.UserRoleAssignment) error
 
-	// POST /api/v1/roles/remove-assignments"
+	// CanRemoveRoles checks if a user has the assign roles permission
+	// POST /api/v1/roles/remove-assignments
 	// RemoveAssignments
 	CanRemoveRoles(curUser model.User, groupRoleAssignments []*rbacv1.GroupRoleAssignment,
 		userRoleAssignments []*rbacv1.UserRoleAssignment) error
