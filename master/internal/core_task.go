674bc79f685c6a0bddaa45362dd644d6b259b770
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2022-10-04 15:12:20 -0400

    removed broken endpoint

--- master/internal/core_task.go
+++ master/internal/core_task.go
@@ -3,25 +3,12 @@ package internal
 import (
 	"github.com/labstack/echo/v4"
 
-	"github.com/determined-ai/determined/master/internal/api"
+	//"github.com/determined-ai/determined/master/internal/api"
 	"github.com/determined-ai/determined/master/internal/sproto"
-	"github.com/determined-ai/determined/master/pkg/model"
+	//"github.com/determined-ai/determined/master/pkg/model"
 )
 
 // TODO now
 func (m *Master) getTasks(c echo.Context) (interface{}, error) {
 	return m.rm.GetAllocationSummaries(m.system, sproto.GetAllocationSummaries{})
-}
-
-// TODO now
-func (m *Master) getTask(c echo.Context) (interface{}, error) {
-	args := struct {
-		AllocationID string `path:"allocation_id"`
-	}{}
-	if err := api.BindArgs(&args, c); err != nil {
-		return nil, err
-	}
-	return m.rm.GetAllocationSummary(m.system, sproto.GetAllocationSummary{
-		ID: model.AllocationID(args.AllocationID),
-	})
 }
