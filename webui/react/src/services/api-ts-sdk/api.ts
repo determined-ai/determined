b851933bcdfed3d08faf4cbac165a1503baf406c
Emily Bonar <emily.bonar@hpe.com>
2022-10-05 12:02:21 -0400

    Regenerate api

--- webui/react/src/services/api-ts-sdk/api.ts
+++ webui/react/src/services/api-ts-sdk/api.ts
@@ -9495,6 +9495,12 @@ export interface V1Workspace {
      * @memberof V1Workspace
      */
     errorMessage: string;
+    /**
+     * Optional agent host uid and gid override.
+     * @type {V1AgentUserGroup}
+     * @memberof V1Workspace
+     */
+    agentUserGroup?: V1AgentUserGroup;
 }
 
 /**
