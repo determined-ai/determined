30496154694ef1a9e3647e4b76de6af07934716b
phillip-gaisford <98362331+phillip-gaisford@users.noreply.github.com>
2022-10-05 11:50:42 -0400

    chore: remove refs to pbs/slurm in environment (#465)

--- master/pkg/tasks/dispatcher_task.go
+++ master/pkg/tasks/dispatcher_task.go
@@ -167,7 +167,6 @@ func (t *TaskSpec) ToDispatcherManifest(
 
 	var slurmArgs []string
 	slurmArgs = append(slurmArgs, t.TaskContainerDefaults.Slurm...)
-	slurmArgs = append(slurmArgs, t.Environment.Slurm()...)
 	logrus.Debugf("Custom slurm arguments: %s", slurmArgs)
 	customParams["slurmArgs"] = slurmArgs
 	errList := model.ValidateSlurm(slurmArgs)
@@ -178,7 +177,6 @@ func (t *TaskSpec) ToDispatcherManifest(
 
 	var pbsArgs []string
 	pbsArgs = append(pbsArgs, t.TaskContainerDefaults.Pbs...)
-	pbsArgs = append(pbsArgs, t.Environment.Pbs()...)
 	logrus.Debugf("Custom pbs arguments: %s", pbsArgs)
 	customParams["pbsArgs"] = pbsArgs
 
