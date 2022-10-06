ccbfec48da38ab69873979b441b647d5ac0d9556
Nick Doiron <nick.doiron@hpe.com>
2022-10-06 17:45:45 -0500

    hooks/useFeature

--- webui/react/src/pages/Settings/GroupManagement.tsx
+++ webui/react/src/pages/Settings/GroupManagement.tsx
@@ -7,6 +7,7 @@ import InteractiveTable, {
 } from 'components/InteractiveTable';
 import Page from 'components/Page';
 import { defaultRowClassName, getFullPaginationConfig } from 'components/Table';
+import useFeature from 'hooks/useFeature';
 import { useFetchKnownRoles } from 'hooks/useFetch';
 import useModalCreateGroup from 'hooks/useModal/UserSettings/useModalCreateGroup';
 import useModalDeleteGroup from 'hooks/useModal/UserSettings/useModalDeleteGroup';
@@ -147,6 +148,7 @@ const GroupManagement: React.FC = () => {
     fetchUsers();
   }, [settings.tableLimit, settings.tableOffset, fetchGroups, fetchUsers]);
 
+  const rbacEnabled = useFeature().isOn('rbac');
   useEffect(() => {
     if (rbacEnabled) {
       fetchKnownRoles();
