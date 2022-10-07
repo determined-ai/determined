bbb5a405ad70dd2b545107bc257efd1caa66454a
Keita Nonaka <keita.nonaka@hpe.com>
2022-10-07 11:46:03 -0700

    chore: use `argparse`

--- pre-commit/web_lint_check.py
+++ pre-commit/web_lint_check.py
@@ -1,25 +1,28 @@
-import subprocess
+import argparse
 import multiprocessing
-import sys
+import os
+import subprocess
 
 
 def web_lint_check():
-    if len(sys.argv) <= 2:
-        print("At least 2 arguments are required")
-        print("usage: python web_lint_check.py [js | css | misc] [<file_paths>]")
-        exit(2)
+    parser = argparse.ArgumentParser(description="Lint Check for Web")
+    parser.add_argument(
+        "target", help="Either js, css, or misc", choices=["js", "css", "misc"]
+    )
+    parser.add_argument("file_paths", help="1 or more file paths", nargs="*")
+    args = parser.parse_args()
+    DIR = "webui/react/"
 
-    dir = "webui/react/"
-    target: str = sys.argv[1]
+    target: str = args.target
     file_paths: str = " ".join(
-        [file_path.replace(dir, "") for file_path in sys.argv[2:]]
+        [os.path.relpath(file_path, DIR) for file_path in args.file_paths]
     )
     nproc: int = multiprocessing.cpu_count()
     run_command: list[str] = [
         "make",
         f"-j{nproc}",
         "-C",
-        dir,
+        DIR,
         "prettier",
         f"PRE_ARGS=-- -c {file_paths}",
     ]
@@ -31,9 +34,6 @@ def web_lint_check():
         run_command += ["stylelint", f"ST_ARGS={file_paths}"]
     elif target == "misc":
         run_command += ["check-package-lock"]
-    else:
-        print(f"{target} is not found")
-        exit(2)
 
     returncode: int = subprocess.call(run_command)
     exit(returncode)
