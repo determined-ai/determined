3ed5744405d152df716343467a68a05c157ca350
Garrett Goon <garrett.goon@hpe.com>
2022-10-07 19:46:08 -0400

    upgrade to diffuser 0.4.0

--- examples/diffusion/textual_inversion_stable_diffusion/startup-hook.sh
+++ examples/diffusion/textual_inversion_stable_diffusion/startup-hook.sh
@@ -1,2 +1,2 @@
-pip install diffusers["training"]==0.3.0 transformers==4.21.3 ftfy==6.1.1 setuptools==59.5.0 accelerate==0.12.0
+pip install diffusers["training"]==0.4.0 transformers==4.21.3 ftfy==6.1.1 setuptools==59.5.0 accelerate==0.12.0
 
