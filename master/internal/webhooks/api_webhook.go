10f5d7bae0fe7fed92b0868d7dcf7e15b56e382c
julian-determined-ai <103522725+julian-determined-ai@users.noreply.github.com>
2022-10-10 15:20:29 -0500

    feat: Add CAN_EDIT_WEBHOOKS permission to pre-canned admin role [WEB-218] (#5200)

--- master/internal/webhooks/api_webhook.go
+++ master/internal/webhooks/api_webhook.go
@@ -3,16 +3,39 @@ package webhooks
 import (
 	"context"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/determined-ai/determined/master/internal/grpcutil"
+
 	"github.com/determined-ai/determined/proto/pkg/apiv1"
 )
 
 // WebhooksAPIServer is an embedded api server struct.
 type WebhooksAPIServer struct{}
 
+// AuthorizeRequest checks if the user has CanEditWebhooks permissions.
+func AuthorizeRequest(ctx context.Context) error {
+	curUser, _, err := grpcutil.GetUser(ctx)
+	if err != nil {
+		return status.Errorf(codes.Internal, "failed to get the user: %s", err)
+	}
+	authErr := AuthZProvider.Get().
+		CanEditWebhooks(curUser)
+	if authErr != nil {
+		return status.Error(codes.PermissionDenied, authErr.Error())
+	}
+	return nil
+}
+
 // GetWebhooks returns all Webhooks.
 func (a *WebhooksAPIServer) GetWebhooks(
 	ctx context.Context, req *apiv1.GetWebhooksRequest,
 ) (*apiv1.GetWebhooksResponse, error) {
+	err := AuthorizeRequest(ctx)
+	if err != nil {
+		return nil, err
+	}
 	webhooks, err := GetWebhooks(ctx)
 	if err != nil {
 		return nil, err
@@ -24,6 +47,10 @@ func (a *WebhooksAPIServer) GetWebhooks(
 func (a *WebhooksAPIServer) PostWebhook(
 	ctx context.Context, req *apiv1.PostWebhookRequest,
 ) (*apiv1.PostWebhookResponse, error) {
+	err := AuthorizeRequest(ctx)
+	if err != nil {
+		return nil, err
+	}
 	w := WebhookFromProto(req.Webhook)
 	if err := AddWebhook(ctx, &w); err != nil {
 		return nil, err
@@ -35,6 +62,10 @@ func (a *WebhooksAPIServer) PostWebhook(
 func (a *WebhooksAPIServer) DeleteWebhook(
 	ctx context.Context, req *apiv1.DeleteWebhookRequest,
 ) (*apiv1.DeleteWebhookResponse, error) {
+	err := AuthorizeRequest(ctx)
+	if err != nil {
+		return nil, err
+	}
 	if err := DeleteWebhook(ctx, WebhookID(req.Id)); err != nil {
 		return nil, err
 	}
@@ -45,5 +76,9 @@ func (a *WebhooksAPIServer) DeleteWebhook(
 func (a *WebhooksAPIServer) TestWebhook(
 	ctx context.Context, req *apiv1.TestWebhookRequest,
 ) (*apiv1.TestWebhookResponse, error) {
+	err := AuthorizeRequest(ctx)
+	if err != nil {
+		return nil, err
+	}
 	panic("unimplemented")
 }
