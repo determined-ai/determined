2e04338163a43b90420d668c53222c59fc2fe729
Keita Nonaka <keita.nonaka@hpe.com>
2022-10-11 12:58:02 -0700

    test: fix errors

--- webui/react/src/routes/utils.test.ts
+++ webui/react/src/routes/utils.test.ts
@@ -2,20 +2,20 @@ import { reactHostAddress } from 'shared/utils/routes';
 
 import { findReactRoute } from './utils';
 
-const initPublicUrl = process.env.PUBLIC_URL;
+const initEnv = process.env;
 
 describe('Routing Utilities', () => {
   afterEach(() => {
-    process.env.PUBLIC_URL = initPublicUrl;
+    process.env = initEnv;
   });
   beforeEach(() => {
-    process.env.PUBLIC_URL = initPublicUrl;
+    process.env = initEnv;
   });
 
   describe('findReactRoute', () => {
     it('should match PUBLIC_URL/experiments/1 and PUBLIC_URL/experiments/xyz', () => {
       const subdirectory = '/det';
-      process.env.PUBLIC_URL = subdirectory;
+      process.env = { ...process.env, PUBLIC_URL: subdirectory };
 
       expect(findReactRoute(subdirectory + '/experiments/1')).toBeDefined();
       expect(findReactRoute(subdirectory + '/experiments/1/xyz')).toBeDefined();
@@ -23,7 +23,7 @@ describe('Routing Utilities', () => {
 
     it('should not match without PUBLIC_URL if PUBLIC_URL is set otherwise', () => {
       const subdirectory = '/det';
-      process.env.PUBLIC_URL = subdirectory;
+      process.env = { ...process.env, PUBLIC_URL: subdirectory };
 
       // negative cases
       expect(findReactRoute('det/experiments/1')).toBeUndefined();
@@ -34,7 +34,7 @@ describe('Routing Utilities', () => {
 
     it('should match full urls: HOST/PUBLIC_URL/experiments/1', () => {
       const subdirectory = '/det';
-      process.env.PUBLIC_URL = subdirectory;
+      process.env = { ...process.env, PUBLIC_URL: subdirectory };
       const prefix = reactHostAddress() + subdirectory;
 
       expect(findReactRoute(prefix + '/experiments/1')).toBeDefined();
@@ -43,7 +43,7 @@ describe('Routing Utilities', () => {
 
     it('should not match full urls from other hosts', () => {
       const subdirectory = '/det';
-      process.env.PUBLIC_URL = subdirectory;
+      process.env = { ...process.env, PUBLIC_URL: subdirectory };
       const prefix = 'http://letermined.com' + subdirectory;
 
       expect(findReactRoute(prefix + '/experiments/1')).toBeUndefined();
@@ -52,7 +52,7 @@ describe('Routing Utilities', () => {
 
     it('should not match full urls missing PUBLIC_URL', () => {
       const subdirectory = '/det';
-      process.env.PUBLIC_URL = subdirectory;
+      process.env = { ...process.env, PUBLIC_URL: subdirectory };
 
       expect(findReactRoute(reactHostAddress() + '/experiments/1')).toBeUndefined();
       expect(findReactRoute(reactHostAddress() + '/experiments/1/xyz')).toBeUndefined();
@@ -62,12 +62,12 @@ describe('Routing Utilities', () => {
   describe('reactHostAddress', () => {
     it('should be independent of PUBLIC_URL', () => {
       const init = process.env.PUBLIC_URL;
-      process.env.PUBLIC_URL = 'a';
+      process.env = { ...process.env, PUBLIC_URL: 'a' };
       const a = reactHostAddress();
-      process.env.PUBLIC_URL = 'b';
+      process.env = { ...process.env, PUBLIC_URL: 'b' };
       const b = reactHostAddress();
       expect(a).toEqual(b);
-      process.env.PUBLIC_URL = init;
+      process.env = { ...process.env, PUBLIC_URL: init };
     });
   });
 });
