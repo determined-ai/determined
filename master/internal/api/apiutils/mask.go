6835725c5da8181c291d95e19cde5da63d66eac4
Max Russell <max.russell@hpe.com>
2022-10-12 16:00:53 -0700

    fix: add support to FieldMask for specified ancestors and add tests

--- master/internal/api/apiutils/mask.go
+++ master/internal/api/apiutils/mask.go
@@ -1,6 +1,8 @@
 package apiutils
 
 import (
+	"strings"
+
 	field_mask "google.golang.org/genproto/protobuf/field_mask"
 )
 
@@ -18,29 +20,43 @@ func NewFieldMask(m *field_mask.FieldMask) FieldMask {
 	if m == nil {
 		return FieldMask{}
 	}
-	
+
 	m.Normalize()
-	
-	paths := m.Paths()
+
+	paths := m.GetPaths()
 	fields := make(map[string]bool, len(paths))
-	for _, f := paths {
+	for _, f := range paths {
 		fields[f] = true
 	}
 
-	m.IsValid()
-
 	return FieldMask{
-		m: m,
+		m:        m,
 		fieldSet: fields,
 	}
 }
 
-// FieldInSet answers whether the passed-in field is in the FieldMask. FieldInSet respects the 
+// FieldInSet answers whether the passed-in field is in the FieldMask. FieldInSet respects the
 // FieldMask convention of treating empty FieldMasks as containing every field.
 func (f *FieldMask) FieldInSet(field string) bool {
 	if len(f.fieldSet) == 0 {
 		return true
 	}
 
-	return f.fieldSet[field]
+	if f.fieldSet[field] {
+		return true
+	}
+
+	// If the fieldmask contains a.b and a user inquires about field a.b.c, the answer is yes.
+	fields := strings.Split(field, ".")
+	if len(fields) > 1 {
+		// Continually re-slice the slice of fields to check if an ancestor of field was specified.
+		for fields = fields[:len(fields)-1]; len(fields) > 0; fields = fields[:len(fields)-1] {
+			path := strings.Join(fields, ".")
+			if f.fieldSet[path] {
+				return true
+			}
+		}
+	}
+
+	return false
 }
