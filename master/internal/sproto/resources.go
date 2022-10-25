3ce6cd777e844a779c9fab9f3199ca73c56c8dca
CanmingCobble <107056780+CanmingCobble@users.noreply.github.com>
2022-10-25 14:50:22 -0500

    chore: HPC job ID is logged in experiment log. (#5315)

--- master/internal/sproto/resources.go
+++ master/internal/sproto/resources.go
@@ -73,6 +73,8 @@ type ResourcesStarted struct {
 	Addresses []cproto.Address
 	// NativeResourcesID is the native Docker hex container ID of the Determined container.
 	NativeResourcesID string
+	// HPCJobID is the Slurm/PBS job ID.
+	HPCJobID string
 }
 
 // FromContainerStarted converts an aproto.ContainerStarted message to ResourcesStarted.
