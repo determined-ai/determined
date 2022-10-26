29a6b4ed262db365652a33f2ba4c4a63a5e0dc45
Nikita <nikita.rajaneesh@hpe.com>
2022-10-26 16:00:15 -0500

    use determined obj instead of client

--- e2e_tests/tests/cluster/test_users.py
+++ e2e_tests/tests/cluster/test_users.py
@@ -16,7 +16,7 @@ from pexpect import spawn
 
 from determined.common import api, constants, yaml
 from determined.common.api import authentication, bindings, certs, errors
-from determined.experimental import client
+from determined.experimental import client, Determined
 from tests import command
 from tests import config as conf
 from tests import experiment as exp
@@ -215,8 +215,8 @@ def test_post_user_api(clean_auth: None, login_admin: None) -> None:
 def test_create_user_sdk(clean_auth: None, login_admin: None): 
     username =  get_random_string()
     password = get_random_string()
-    client.login(master=conf.make_master_url())
-    user = client.create_user(username=username, admin= False, password=password)
+    det_obj = Determined(master=conf.make_master_url())
+    user = det_obj.create_user(username=username, admin= False, password=password)
     assert user.user_id is not None and user.username == username
 
 @pytest.mark.e2e_cpu
@@ -262,7 +262,7 @@ def test_logout(clean_auth: None, login_admin: None) -> None:
     child.close()
     assert child.status == 0
 
-    # Change Determined passwordback to "".
+    # Change Determined password back to "".
     change_user_password(constants.DEFAULT_DETERMINED_USER, "")
     # Clean up.
 
@@ -295,8 +295,8 @@ def test_activate_deactivate(clean_auth: None, login_admin: None) -> None:
 
     # SDK testing for activating and deactivating. 
     log_in_user(ADMIN_CREDENTIALS)
-    client.login(master=conf.make_master_url())
-    user = client.get_user_by_name(user_name=creds.username)
+    det_obj = Determined(master=conf.make_master_url())
+    user = det_obj.get_user_by_name(user_name=creds.username)
     assert user.deactivate().user.active == False
     assert user.activate().user.active == True
     
@@ -324,8 +324,8 @@ def test_change_password(clean_auth: None, login_admin: None) -> None:
     assert log_in_user(authentication.Credentials(creds.username, newPassword)) == 0
     
     newPasswordSdk = get_random_string()
-    client.login(master=conf.make_master_url())
-    user = client.get_user_by_name(user_name=creds.username)
+    det_obj = Determined(master=conf.make_master_url())
+    user = det_obj.get_user_by_name(user_name=creds.username)
     assert user.change_password(new_password=newPasswordSdk, is_hashed=False)
     assert log_in_user(authentication.Credentials(creds.username, newPasswordSdk)) == 0
 
@@ -766,8 +766,8 @@ def create_linked_user(uid: int, user: str, gid: int, group: str) -> authenticat
 
 def create_linked_user_sdk(uid: int, agent_user: str, gid: int, group: str): 
     creds = create_test_user(False)
-    client.login(master=conf.make_master_url())
-    user = client.get_user_by_name(user_name=creds.username)
+    det_obj = Determined(master=conf.make_master_url())
+    user = det_obj.get_user_by_name(user_name=creds.username)
     resp = user.link_with_agent(agent_gid=gid, agent_uid=uid, agent_group=group, agent_user=agent_user)
     return creds
 
