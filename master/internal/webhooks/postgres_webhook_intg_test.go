e90ca36afc7ebe9877ca66d7149b0b60f32bbd56
julian-determined-ai <julian.glover@hpe.com>
2022-10-26 13:08:18 -0500

    fix typo in test files

--- master/internal/webhooks/postgres_webhook_intg_test.go
+++ master/internal/webhooks/postgres_webhook_intg_test.go
@@ -76,7 +76,7 @@ func TestReportExperimentStateChanged(t *testing.T) {
 	defer cancel()
 
 	pgDB := db.MustResolveTestPostgres(t)
-	db.MustMigrateTestPostgres(t, pgDB, db.MigrationsFromDB))
+	db.MustMigrateTestPostgres(t, pgDB, db.MigrationsFromDB)
 	clearWebhooksTables(ctx, t)
 
 	singletonShipper = &shipper{wake: make(chan<- struct{})} // mock shipper
@@ -275,7 +275,7 @@ func mockWebhook() *Webhook {
 func TestDequeueEvents(t *testing.T) {
 	ctx := context.Background()
 	pgDB := db.MustResolveTestPostgres(t)
-	db.MustMigrateTestPostgres(t, pgDB, db.MigrationsFromDB))
+	db.MustMigrateTestPostgres(t, pgDB, db.MigrationsFromDB)
 	clearWebhooksTables(ctx, t)
 
 	singletonShipper = &shipper{wake: make(chan<- struct{})} // mock shipper
