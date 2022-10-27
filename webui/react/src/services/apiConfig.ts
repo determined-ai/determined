01028af1aa1fba0e121f97bd835a6dffa187c118
Nikita <nikita.rajaneesh@hpe.com>
2022-10-27 15:17:43 -0500

    fix react

--- webui/react/src/services/apiConfig.ts
+++ webui/react/src/services/apiConfig.ts
@@ -157,7 +157,7 @@ export const patchUser: DetApi<
 > = {
   name: 'patchUser',
   postProcess: (response) => decoder.mapV1User(response.user),
-  request: (params) => detApi.Users.patchUser(params.userId, params.userParams),
+  request: (params) => detApi.Users.patchUser(params.userId, { user: params.userParams, userId: params.userId }),
 };
 
 export const getUserSetting: DetApi<
