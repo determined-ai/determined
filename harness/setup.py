12adc475211469e541e4525b2c216966bb765d91
Emily Bonar <emily.bonar@hpe.com>
2022-10-28 15:25:53 -0400

    chore: bump version: 0.19.6-rc6 -> 0.19.6

--- harness/setup.py
+++ harness/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="determined",
-    version="0.19.6-rc6",
+    version="0.19.6",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
