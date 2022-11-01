b6d361c6de8596bfe4fd698045d23191dd45fda4
gt2345 <gt2345@columbia.edu>
2022-11-01 13:52:29 -0500

    fmt

--- webui/react/src/components/ExperimentIcons/Active.tsx
+++ webui/react/src/components/ExperimentIcons/Active.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 import css from './Active.module.scss';
 
-const Active: React.FC = (() => {
-    return <div className={css.base}><div className={css.dots} /></div>;
-});
+const Active: React.FC = () => {
+  return (
+    <div className={css.base}>
+      <div className={css.dots} />
+    </div>
+  );
+};
 
 export default Active;
--- webui/react/src/components/ExperimentIcons/Queue.tsx
+++ webui/react/src/components/ExperimentIcons/Queue.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import css from './Queue.module.scss';
 
-const Queue: React.FC = (() => {
-    return (
-      <div className={css.base}>
-        <div className={css.spinner}>
-          <div className={css.inner_spinner} />
-        </div>
-      </div>);
-});
+const Queue: React.FC = () => {
+  return (
+    <div className={css.base}>
+      <div className={css.spinner}>
+        <div className={css.inner_spinner} />
+      </div>
+    </div>
+  );
+};
 
 export default Queue;
