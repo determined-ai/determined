a3b883f846ea0506d30078a3e8e8c55dc9a5e939
gt2345 <gt2345@columbia.edu>
2022-11-01 13:52:29 -0500

    fmt

--- webui/react/src/components/ExperimentIcons/Spinner.tsx
+++ webui/react/src/components/ExperimentIcons/Spinner.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 import css from './Spinner.module.scss';
 
 interface Props {
-    type: 'bowtie' | 'half'
+  type: 'bowtie' | 'half';
 }
 
-const Spinner: React.FC<Props> = (({ type }) => {
-    const classnames = [css.spinner, css[`spinner__${type}`]];
-    return <div className={css.base}><div className={classnames.join(' ')} /></div>;
-});
+const Spinner: React.FC<Props> = ({ type }) => {
+  const classnames = [css.spinner, css[`spinner__${type}`]];
+  return (
+    <div className={css.base}>
+      <div className={classnames.join(' ')} />
+    </div>
+  );
+};
 
 export default Spinner;
--- webui/react/src/components/ExperimentIcons/index.tsx
+++ webui/react/src/components/ExperimentIcons/index.tsx
@@ -17,42 +17,45 @@ import Queue from './Queue';
 import Spinner from './Spinner';
 
 interface Props {
-    state: RunState
+  state: RunState;
 }
 
 const ExperimentIcons: React.FC<Props> = ({ state }) => {
-    const { ui } = useUI();
-    const isDark = useMemo (() => ui.darkLight === DarkLight.Dark, [ui]);
-    const icon = useMemo(() => {
-        switch (state) {
-            case RunState.Queued:
-                return <Queue />;
-            case RunState.Starting:
-            case RunState.Pulling:
-                return <Spinner type="bowtie" />;
-            case RunState.Running:
-                return <Spinner type="half" />;
-            case RunState.Paused:
-                return <img src={isDark ? pause_dark : pause_light} />;
-            case RunState.Completed:
-                return <img src={complete} />;
-            case RunState.Error:
-            case RunState.Deleted:
-            case RunState.Deleting:
-            case RunState.DeleteFailed:
-                return <img src={error} />;
-            case RunState.Canceled:
-            case RunState.StoppingCanceled:
-            case RunState.StoppingCompleted:
-            case RunState.StoppingError:
-            case RunState.StoppingKilled:
-                return <img src={isDark ? cancle_dark : cancle_light} />;
-            default:
-                return <Active />;
-        }
-    }, [state, isDark]);
-    return <Tooltip title={runStateToLabel[state] || '123123'}><span>{icon}</span></Tooltip>;
-
+  const { ui } = useUI();
+  const isDark = useMemo(() => ui.darkLight === DarkLight.Dark, [ui]);
+  const icon = useMemo(() => {
+    switch (state) {
+      case RunState.Queued:
+        return <Queue />;
+      case RunState.Starting:
+      case RunState.Pulling:
+        return <Spinner type="bowtie" />;
+      case RunState.Running:
+        return <Spinner type="half" />;
+      case RunState.Paused:
+        return <img src={isDark ? pause_dark : pause_light} />;
+      case RunState.Completed:
+        return <img src={complete} />;
+      case RunState.Error:
+      case RunState.Deleted:
+      case RunState.Deleting:
+      case RunState.DeleteFailed:
+        return <img src={error} />;
+      case RunState.Canceled:
+      case RunState.StoppingCanceled:
+      case RunState.StoppingCompleted:
+      case RunState.StoppingError:
+      case RunState.StoppingKilled:
+        return <img src={isDark ? cancle_dark : cancle_light} />;
+      default:
+        return <Active />;
+    }
+  }, [state, isDark]);
+  return (
+    <Tooltip title={runStateToLabel[state]}>
+      <span>{icon}</span>
+    </Tooltip>
+  );
 };
 
 export default ExperimentIcons;
