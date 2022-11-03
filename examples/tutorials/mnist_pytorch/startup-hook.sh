a21b6d60fd1d4a3f21ea656413b6226ec6c90f5b
Maksim Kouznetsov <maksim.kouznetsov@hpe.com>
2022-11-03 16:53:02 -0700

    test: try pytorch 1.12

--- examples/tutorials/mnist_pytorch/startup-hook.sh
+++ examples/tutorials/mnist_pytorch/startup-hook.sh
@@ -1,3 +1,1 @@
 pip install torch==1.12.0+cpu torchvision==0.13.0+cpu torchaudio==0.12.0+cpu --extra-index-url https://download.pytorch.org/whl/cpu
-pip install pytorch_lightning==1.7.7 torchmetrics==0.9.3
-
