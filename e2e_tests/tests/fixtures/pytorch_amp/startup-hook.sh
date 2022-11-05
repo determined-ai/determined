61a94ed451a7658966ec4a63be99b2572358499e
Maksim Kouznetsov <maksim.kouznetsov@hpe.com>
2022-11-04 17:00:28 -0700

    only upgrade pt and tv and ta but not ptl

--- e2e_tests/tests/fixtures/pytorch_amp/startup-hook.sh
+++ e2e_tests/tests/fixtures/pytorch_amp/startup-hook.sh
@@ -1,2 +1,1 @@
 pip install torch==1.12.0+cpu torchvision==0.13.0+cpu torchaudio==0.12.0+cpu --extra-index-url https://download.pytorch.org/whl/cpu
-pip install pytorch_lightning==1.5.10 torchmetrics==0.5.1
---
+++ examples/gan/gan_mnist_pytorch/startup-hook.sh
@@ -0,0 +1,1 @@
+pip install torch==1.12.0+cpu torchvision==0.13.0+cpu torchaudio==0.12.0+cpu --extra-index-url https://download.pytorch.org/whl/cpu
