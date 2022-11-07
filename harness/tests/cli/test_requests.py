c154c50930eb88572446ee54ad961d98eede2246
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-11-07 14:29:04 -0800

    test the existing state

--- harness/tests/cli/test_requests.py
+++ harness/tests/cli/test_requests.py
@@ -9,7 +9,7 @@ import requests_mock
 import dataclasses
 import determined
 
-from determined.cli import experiment
+import determined.cli
 import determined.cli.cli as cli
 import determined.cli.command as command
 from determined.common import constants, context
@@ -25,27 +25,43 @@ class CliArgs:
     experiment_id: int
     user: str = "test"
     password: str = "test"
+    polling_interval: int = 1
 
 
 fixtures = Path(__file__).parent.parent / "fixtures"
 
 
 @pytest.fixture
-def experiment_details():
+def experiment_json():
     with open(fixtures / "experiment.json") as f:
-        return json.load(f)
+        return json.load(f)["experiment"]
 
 
-def test_transient_network(requests_mock: requests_mock.Mocker, experiment_details) -> None:
+def test_transient_network(requests_mock: requests_mock.Mocker, experiment_json) -> None:
     requests_mock.get("/info", status_code=200, json={"version": "1.0"})
     requests_mock.get(
         "/users/me", status_code=200, json={"username": constants.DEFAULT_DETERMINED_USER}
     )
     requests_mock.post("/login", status_code=200, json={"token": "fake-token"})
     args = CliArgs(master="http://localhost:8080", experiment_id=1)
+    exp = bindings.v1Experiment.from_json(experiment_json)
+
+    exp.state = bindings.determinedexperimentv1State.STATE_COMPLETED
     requests_mock.get(
         f"http://localhost:8080/api/v1/experiments/{args.experiment_id}",
         status_code=200,
-        json=experiment_details,
+        json={"experiment": exp.to_json()},
     )
-    experiment.wait(args)
+    with pytest.raises(SystemExit) as e:
+        determined.cli.experiment.wait(args)
+    assert e.value.code == 0
+
+    exp.state = bindings.determinedexperimentv1State.STATE_ERROR
+    requests_mock.get(
+        f"http://localhost:8080/api/v1/experiments/{args.experiment_id}",
+        status_code=200,
+        json={"experiment": exp.to_json()},
+    )
+    with pytest.raises(SystemExit) as e:
+        determined.cli.experiment.wait(args)
+    assert e.value.code == 1
