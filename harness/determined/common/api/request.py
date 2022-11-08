17aea233e0cdc1b1871599b7c1bbb4824ca0a592
Nikita <nikita.rajaneesh@hpe.com>
2022-11-08 14:11:44 -0600

    remove prints

--- harness/determined/common/api/request.py
+++ harness/determined/common/api/request.py
@@ -127,9 +127,9 @@ def do_request(
         data = det.util.json_encode(json)
 
     try:
-        print("method and path in do_request")
-        print(method)
-        print(path)
+        #print("method and path in do_request")
+        #print(method)
+        #print(path)
         r = determined.common.requests.request(
             method,
             make_url(host, path),
