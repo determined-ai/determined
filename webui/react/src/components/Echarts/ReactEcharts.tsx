e288325fadc387f84b11877eb1084b103e4da5dc
Keita Nonaka <keita.nonaka@hpe.com>
2022-11-07 16:38:14 -0800

    feat: PoC of line chart

--- webui/react/src/components/Echarts/ReactEcharts.tsx
+++ webui/react/src/components/Echarts/ReactEcharts.tsx
@@ -7,6 +7,7 @@ import useResize from 'hooks/useResize';
 
 export interface Props {
   loading?: boolean;
+  onClick?: (param) => void;
   option: EChartsOption;
   rendererType?: 'canvas' | 'svg';
   settings?: SetOptionOpts;
@@ -20,49 +21,54 @@ const ReactECharts: React.FC<Props> = ({
   settings,
   loading,
   theme,
-  rendererType,
+  rendererType = 'canvas',
+  onClick,
 }: Props) => {
   const chartRef = useRef<HTMLDivElement>(null);
   const resize = useResize(chartRef);
 
   useEffect(() => {
     // Initialize chart
-    let chart: ECharts | undefined;
+    const echart: ECharts | undefined = (() => {
+      if (chartRef.current) {
+        const chart = init(chartRef.current, theme, {
+          height: resize.height,
+          renderer: rendererType,
+          useDirtyRect: false,
+          width: resize.width,
+        });
+        chart.on('click', onClick ?? (() => undefined));
 
-    if (chartRef.current) {
-      chart = init(chartRef.current, theme, {
-        height: resize.height,
-        renderer: rendererType ?? 'canvas',
-        useDirtyRect: false,
-        width: resize.width,
-      });
-    }
+        return chart;
+      }
+      return undefined;
+    })();
 
     // Add chart resize listener
     const resizeChart = () => {
-      chart?.resize();
+      echart?.resize();
     };
     document.addEventListener('resize', resizeChart);
 
     return () => {
-      chart?.dispose();
+      echart?.dispose();
       document.removeEventListener('resize', resizeChart);
     };
-  }, [rendererType, resize.height, resize.width, theme]);
+  }, [onClick, rendererType, resize.height, resize.width, theme]);
 
   useEffect(() => {
     // Update chart
     if (chartRef.current) {
-      const chart = getInstanceByDom(chartRef.current);
-      chart?.setOption(option, settings);
+      const echart = getInstanceByDom(chartRef.current);
+      echart?.setOption(option, settings);
     }
   }, [option, settings, theme]);
 
   useEffect(() => {
     // Update chart
     if (chartRef.current) {
-      const chart = getInstanceByDom(chartRef.current);
-      loading === true ? chart?.showLoading() : chart?.hideLoading();
+      const echart = getInstanceByDom(chartRef.current);
+      loading === true ? echart?.showLoading() : echart?.hideLoading();
     }
   }, [loading, theme]);
 
@@ -70,7 +76,8 @@ const ReactECharts: React.FC<Props> = ({
     <div
       ref={chartRef}
       style={{
-        height: '100vh',
+        height: '50vh',
+        padding: '12px',
         position: 'relative',
         width: '100%',
         ...style,
--- webui/react/src/pages/Clusters/ClustersOverview.tsx
+++ webui/react/src/pages/Clusters/ClustersOverview.tsx
@@ -1,7 +1,5 @@
-import { EChartsOption } from 'echarts';
 import React, { useCallback, useEffect, useState } from 'react';
 
-import ReactECharts from 'components/Echarts/Echarts';
 import Grid, { GridMode } from 'components/Grid';
 import Link from 'components/Link';
 import ResourcePoolCard from 'components/ResourcePoolCard';
@@ -118,108 +116,6 @@ const ClusterOverview: React.FC = () => {
     return () => canceler.abort();
   }, [canceler, fetchAgents]);
 
-  const colors = ['#5470C6', '#91CC75', '#EE6666'];
-  const option: EChartsOption = {
-    color: colors,
-    grid: {
-      right: '20%',
-    },
-    legend: {
-      data: ['Evaporation', 'Precipitation', 'Temperature'],
-    },
-    series: [
-      {
-        data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
-        name: 'Evaporation',
-        type: 'bar',
-      },
-      {
-        data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
-        name: 'Precipitation',
-        type: 'bar',
-        yAxisIndex: 1,
-      },
-      {
-        data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2],
-        name: 'Temperature',
-        type: 'line',
-        yAxisIndex: 2,
-      },
-    ],
-    toolbox: {
-      feature: {
-        dataView: { readOnly: false, show: true },
-        restore: { show: true },
-        saveAsImage: { show: true },
-      },
-    },
-    tooltip: {
-      axisPointer: {
-        type: 'cross',
-      },
-      trigger: 'axis',
-    },
-    xAxis: [
-      {
-        axisTick: {
-          alignWithLabel: true,
-        },
-        // prettier-ignore
-        data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-
-        type: 'category',
-      },
-    ],
-    yAxis: [
-      {
-        alignTicks: true,
-        axisLabel: {
-          formatter: '{value} ml',
-        },
-        axisLine: {
-          lineStyle: {
-            color: colors[0],
-          },
-          show: true,
-        },
-        name: 'Evaporation',
-        position: 'right',
-        type: 'value',
-      },
-      {
-        alignTicks: true,
-        axisLabel: {
-          formatter: '{value} ml',
-        },
-        axisLine: {
-          lineStyle: {
-            color: colors[1],
-          },
-          show: true,
-        },
-        name: 'Precipitation',
-        offset: 80,
-        position: 'right',
-        type: 'value',
-      },
-      {
-        alignTicks: true,
-        axisLabel: {
-          formatter: '{value} °C',
-        },
-        axisLine: {
-          lineStyle: {
-            color: colors[2],
-          },
-          show: true,
-        },
-        name: '温度',
-        position: 'left',
-        type: 'value',
-      },
-    ],
-  };
-
   return (
     <div className={css.base}>
       <ClusterOverallStats />
@@ -236,7 +132,6 @@ const ClusterOverview: React.FC = () => {
       {!!rpDetail && (
         <ResourcePoolDetails finally={hideModal} resourcePool={rpDetail} visible={!!rpDetail} />
       )}
-      <ReactECharts option={option} theme={'dark'} />
     </div>
   );
 };
