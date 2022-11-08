f588f58e15c17a1756208e6e4fb4f03188b96612
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-11-07 16:00:49 -0800

    test retries

--- harness/tests/cli/test_requests.py
+++ harness/tests/cli/test_requests.py
@@ -1,13 +1,15 @@
 import os
 import tempfile
+from typing import Optional
 import uuid
 from pathlib import Path
 
 import pytest
 import requests
-import requests_mock
+import requests_mock as mock
 import dataclasses
 import determined
+from determined import common
 
 import determined.cli
 import determined.cli.cli as cli
@@ -37,7 +39,54 @@ def experiment_json():
         return json.load(f)["experiment"]
 
 
-def test_transient_network(requests_mock: requests_mock.Mocker, experiment_json) -> None:
+def test_transient_network(requests_mock: mock.Mocker, experiment_json) -> None:
+    requests_mock.get("/info", status_code=200, json={"version": "1.0"})
+    requests_mock.get(
+        "/users/me", status_code=200, json={"username": constants.DEFAULT_DETERMINED_USER}
+    )
+    requests_mock.post("/login", status_code=200, json={"token": "fake-token"})
+    args = CliArgs(master="http://localhost:8080", experiment_id=1)
+    exp = bindings.v1Experiment.from_json(experiment_json)
+    exp.state = bindings.determinedexperimentv1State.STATE_COMPLETED
+
+    def make_callback(fail_count: int):
+        """
+        Make a callback that will fail the first `fail_count` times it is called, and then succeed.
+        """
+        calls = 0
+
+        def callback(request, context):
+            nonlocal calls
+            if calls < fail_count:
+                calls += 1
+                context.status_code = 504
+                return ""
+            else:
+                context.status_code = 200
+                return json.dumps({"experiment": exp.to_json()})
+
+        return callback
+
+    requests_mock.register_uri(
+        "GET",
+        f"http://localhost:8080/api/v1/experiments/{args.experiment_id}",
+        text=make_callback(0),
+    )
+    with pytest.raises(SystemExit) as e:
+        determined.cli.experiment.wait(args)
+    assert e.value.code == 0
+
+    requests_mock.register_uri(
+        "GET",
+        f"http://localhost:8080/api/v1/experiments/{args.experiment_id}",
+        text=make_callback(5),
+    )
+    with pytest.raises(SystemExit) as e:
+        determined.cli.experiment.wait(args)
+    assert e.value.code == 0
+
+
+def test_stable_network(requests_mock: mock.Mocker, experiment_json) -> None:
     requests_mock.get("/info", status_code=200, json={"version": "1.0"})
     requests_mock.get(
         "/users/me", status_code=200, json={"username": constants.DEFAULT_DETERMINED_USER}
