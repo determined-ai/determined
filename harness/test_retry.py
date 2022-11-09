d6d27ea484610d9ff37c6ff2bbcd6738dde1f196
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-11-09 11:09:06 -0800

    setup mock adapter?

--- harness/test_retry.py
+++ harness/test_retry.py
@@ -4,6 +4,7 @@ import pytest
 import requests.adapters
 import requests.sessions
 import requests
+import requests_mock as mock
 import urllib3
 
 retry = urllib3.util.retry.Retry(
@@ -14,9 +15,12 @@ retry = urllib3.util.retry.Retry(
 adapter = requests.adapters.HTTPAdapter(max_retries=retry)
 
 
-def test_direct_retry(requests_mock):
+def test_direct_retry(requests_mock: mock.Mocker):
     url = "http://localhost:8080/test"
     requests_mock.get(url, status_code=503)
+    # with mock.Mocker(adapter=mock.Adapter(max_retries=retry)) as m:
+    # mock_adapter = mock.get_adapter(url)
+    # mock_adapter.max_retries = retry
 
     with requests.Session() as session:
         session.mount("http://", adapter)
