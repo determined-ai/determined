1d344f49fd17dc83b355d72eba0885fe7195bfb6
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-11-10 16:04:58 -0800

    test flaky endpoint

---
+++ harness/tests/common/test_api_server.py
@@ -0,0 +1,14 @@
+import requests
+
+from tests.common.api_server import run_api_server
+
+
+def test_flaky_endpoint():
+    for _ in range(2):  # no state is shared between runs
+        with run_api_server() as master_url:
+            with requests.Session() as session:
+                for _ in range(2):  # end point FAILS_FOR 2 times
+                    response = session.get(master_url + "/api/v1/experiments/1", verify=False)
+                    assert response.status_code == 504
+                response = session.get(master_url + "/api/v1/experiments/1", verify=False)
+                assert response.status_code == 200
