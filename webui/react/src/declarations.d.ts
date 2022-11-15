3234bd7ba0fe61085ed0ac9daa0d697e57b9358b
Keita Nonaka <keita.nonaka@hpe.com>
2022-11-15 14:52:50 -0800

    chore: add `vendor/notebook` in d.ts

--- webui/react/src/declarations.d.ts
+++ webui/react/src/declarations.d.ts
@@ -1,2 +1,3 @@
 // We need to tell TypeScript that when we write "import styles from './styles.scss' we mean to load a module (to look for a './styles.scss.d.ts').
 declare module '*.scss';
+declare module 'vendor/notebook';
