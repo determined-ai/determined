6503f61c70786cdc4507fecbde6e39c511f395f2
gt2345 <gt2345@columbia.edu>
2022-11-28 10:37:44 -0600

    fix: increase experiment icon font weight

--- webui/react/src/components/ExperimentIcons/index.tsx
+++ webui/react/src/components/ExperimentIcons/index.tsx
@@ -1,5 +1,5 @@
 import { Tooltip } from 'antd';
-import React, { useMemo } from 'react';
+import React, { CSSProperties, useMemo } from 'react';
 
 import { runStateToLabel } from 'constants/states';
 import Icon from 'shared/components/Icon/Icon';
@@ -15,6 +15,7 @@ interface Props {
 
 const ExperimentIcons: React.FC<Props> = ({ state }) => {
   const icon = useMemo(() => {
+    const IconStyle: CSSProperties = { fontWeight: 900 };
     switch (state) {
       case RunState.Queued:
         return <Queue />;
@@ -26,17 +27,19 @@ const ExperimentIcons: React.FC<Props> = ({ state }) => {
       case RunState.Paused:
         return <Icon name="pause" style={{ color: 'var(--theme-ix-cancel)' }} />;
       case RunState.Completed:
-        return <Icon name="checkmark" style={{ color: 'var(--theme-status-success)' }} />;
+        return (
+          <Icon name="checkmark" style={{ ...IconStyle, color: 'var(--theme-status-success)' }} />
+        );
       case RunState.Error:
       case RunState.Deleted:
       case RunState.Deleting:
       case RunState.DeleteFailed:
-        return <Icon name="error" style={{ color: 'var(--theme-status-error)' }} />;
+        return <Icon name="error" style={{ ...IconStyle, color: 'var(--theme-status-error)' }} />;
       case RunState.Active:
       case RunState.Unspecified:
         return <Active />;
       default:
-        return <Icon name="cancelled" style={{ color: 'var(--theme-ix-cancel)' }} />;
+        return <Icon name="cancelled" style={{ ...IconStyle, color: 'var(--theme-ix-cancel)' }} />;
     }
   }, [state]);
   return (
