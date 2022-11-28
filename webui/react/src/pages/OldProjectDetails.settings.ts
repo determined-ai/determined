TrellixVulnTeam/determined_A068

Rename groups and some frontend

webui/react/src/pages/OldProjectDetails.settings.ts
@@ -40,7 +40,7 @@ export const DEFAULT_COLUMN_WIDTHS: Record<ExperimentColumnName, number> = {
   description: 148,
   duration: 96,
   forkedFrom: 128,
-  groupName: 100,
+  groupName: 150,
   id: 60,
   name: 150,
   numTrials: 74,
@@ -56,6 +56,7 @@ export const DEFAULT_COLUMN_WIDTHS: Record<ExperimentColumnName, number> = {
 export interface ProjectDetailsSettings extends InteractiveTableSettings {
   archived?: boolean;
   columns: ExperimentColumnName[];
+  group?: string[];
   label?: string[];
   pinned: Record<number, number[]>; // key is `projectId`, value is array of experimentId
   row?: number[];
@@ -104,6 +105,14 @@ const config: SettingsConfig = {
     {
       key: 'label',
       storageKey: 'label',
+      type: {
+        baseType: BaseType.String,
+        isArray: true,
+      },
+    },
+    {
+      key: 'group',
+      storageKey: 'group',
       type: {
         baseType: BaseType.String,
         isArray: true,

determined-ai/determined

webui/react/src/pages/OldProjectDetails.settings.ts
@@ -51,7 +51,7 @@ export const DEFAULT_COLUMN_WIDTHS: Record<ExperimentColumnName, number> = {
   description: 148,
   duration: 96,
   forkedFrom: 128,
-  groupName: 100,
+  groupName: 150,
   id: 60,
   name: 150,
   numTrials: 74,
@@ -67,6 +67,7 @@ export const DEFAULT_COLUMN_WIDTHS: Record<ExperimentColumnName, number> = {
 export interface ProjectDetailsSettings extends InteractiveTableSettings {
   archived?: boolean;
   columns: ExperimentColumnName[];
+  group?: string[];
   label?: string[];
   pinned: Record<number, number[]>; // key is `projectId`, value is array of experimentId
   row?: number[];
@@ -126,8 +127,8 @@ const config: SettingsConfig<ProjectDetailsSettings> = {
       storageKey: 'pinned',
       type: record(number, array(number)),
     },
-    row: {
-      defaultValue: undefined,
+    {
+      key: 'row',
       storageKey: 'row',
       type: union([undefinedType, array(number)]),
     },