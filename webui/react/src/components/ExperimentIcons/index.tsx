602816400e219c406e4f9006925cba8aa0513e1f
Keita Nonaka <keita.nonaka@hpe.com>
2022-12-01 16:00:59 -0800

    feat: add icons in experiment and trial detail pages (#5512)
    * chore: add icons in experiment and trial detail pages
    
    * chore: add animation
    
    * fix: minor fixes
    
    * fix: make queue icon resizable
    
    * fix: style
    
    * fix: queue style

--- webui/react/src/components/ExperimentIcons/index.tsx
+++ webui/react/src/components/ExperimentIcons/index.tsx
@@ -1,51 +1,77 @@
 import { Tooltip } from 'antd';
 import React, { CSSProperties, useMemo } from 'react';
 
-import { runStateToLabel } from 'constants/states';
+import { stateToLabel } from 'constants/states';
 import Icon from 'shared/components/Icon/Icon';
-import { RunState } from 'types';
+import { CompoundRunState, JobState, RunState } from 'types';
 
 import Active from './Active';
 import Queue from './Queue';
 import Spinner from './Spinner';
 
 interface Props {
-  state: RunState;
+  isTooltipVisible?: boolean;
+  state: CompoundRunState;
+  style?: CSSProperties;
 }
 
-const ExperimentIcons: React.FC<Props> = ({ state }) => {
+const ExperimentIcons: React.FC<Props> = ({ state, style, isTooltipVisible = true }) => {
   const icon = useMemo(() => {
     const IconStyle: CSSProperties = { fontWeight: 900 };
     switch (state) {
+      case JobState.SCHEDULED:
+      case JobState.SCHEDULEDBACKFILLED:
+      case JobState.QUEUED:
       case RunState.Queued:
-        return <Queue />;
+        return <Queue style={style} />;
       case RunState.Starting:
       case RunState.Pulling:
-        return <Spinner type="bowtie" />;
+        return <Spinner style={style} type="bowtie" />;
       case RunState.Running:
-        return <Spinner type="shadow" />;
+        return <Spinner style={style} type="shadow" />;
       case RunState.Paused:
-        return <Icon name="pause" style={{ color: 'var(--theme-ix-cancel)' }} />;
+        return <Icon name="pause" style={{ ...style, color: 'var(--theme-ix-cancel)' }} />;
       case RunState.Completed:
         return (
-          <Icon name="checkmark" style={{ ...IconStyle, color: 'var(--theme-status-success)' }} />
+          <Icon
+            name="checkmark"
+            style={{ ...style, ...IconStyle, color: 'var(--theme-status-success)' }}
+          />
         );
       case RunState.Error:
       case RunState.Deleted:
       case RunState.Deleting:
       case RunState.DeleteFailed:
-        return <Icon name="error" style={{ ...IconStyle, color: 'var(--theme-status-error)' }} />;
+        return (
+          <Icon
+            name="error"
+            style={{ ...style, ...IconStyle, color: 'var(--theme-status-error)' }}
+          />
+        );
       case RunState.Active:
       case RunState.Unspecified:
+      case JobState.UNSPECIFIED:
         return <Active />;
       default:
-        return <Icon name="cancelled" style={{ ...IconStyle, color: 'var(--theme-ix-cancel)' }} />;
+        return (
+          <Icon
+            name="cancelled"
+            style={{ ...style, ...IconStyle, color: 'var(--theme-ix-cancel)' }}
+          />
+        );
     }
-  }, [state]);
+  }, [state, style]);
+
   return (
-    <Tooltip title={runStateToLabel[state]}>
-      <span>{icon}</span>
-    </Tooltip>
+    <>
+      {isTooltipVisible ? (
+        <Tooltip placement="bottom" title={stateToLabel(state)}>
+          <div style={{ display: 'flex' }}>{icon}</div>
+        </Tooltip>
+      ) : (
+        <div style={{ display: 'flex' }}>{icon}</div>
+      )}
+    </>
   );
 };
 
