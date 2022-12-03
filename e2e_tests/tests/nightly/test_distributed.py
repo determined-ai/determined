1893a6bbb6a93486a7f2fb1bbee60435776d9e95
liam <liam@determined.ai>
2022-12-02 16:55:46 -0800

    fix typo in e2e test

--- e2e_tests/tests/nightly/test_distributed.py
+++ e2e_tests/tests/nightly/test_distributed.py
@@ -206,7 +206,7 @@ def test_deepspeed_pipeline_parallel() -> None:
 @pytest.mark.deepspeed
 @pytest.mark.gpu_required
 def test_gpt_neox_zero1() -> None:
-    config = conf.load_config(conf.deepspeed_examples_path("gpt_neox/zero3.yaml"))
+    config = conf.load_config(conf.deepspeed_examples_path("gpt_neox/zero1.yaml"))
     config = conf.set_max_length(config, {"batches": 100})
     config = conf.set_min_validation_period(config, {"batches": 100})
 
