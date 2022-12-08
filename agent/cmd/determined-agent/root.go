TrellixVulnTeam/determined_A068

feat: add structured option for agent logs

agent/cmd/determined-agent/root.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	log "github.com/sirupsen/logrus"
+	"errors"
+
+	"github.com/determined-ai/determined/master/pkg/logger"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 type options struct {
+	logger.Config
+
 	logLevel string
 	noColor  bool
 }
@@ -22,23 +27,44 @@ func newRootCmd() *cobra.Command {
 			if err := bindEnv("DET_", cmd); err != nil {
 				return err
 			}
-			level, err := log.ParseLevel(opts.logLevel)
-			if err != nil {
-				return err
+
+			// Case hell for backwards compatability - remove whenever, after 0.20 series.
+			var usedDeprecatedLevel bool
+			switch {
+			case opts.logLevel != "" && opts.Level != "":
+				return errors.New("cannot use `--log-level` and `--level`")
+			case opts.logLevel != "" && opts.Level == "":
+				usedDeprecatedLevel = true
+				opts.Level = opts.logLevel
+			case opts.logLevel == "" && opts.Level == "":
+				opts.Level = "info"
 			}
-			log.SetLevel(level)
-			log.SetFormatter(&log.TextFormatter{
-				FullTimestamp: true,
-				ForceColors:   true,
-				DisableColors: opts.noColor,
-			})
+
+			var usedDeprecatedColor bool
+			if opts.noColor && opts.Color {
+				usedDeprecatedColor = true
+				opts.Color = !opts.noColor
+			}
+
+			logger.SetLogrus(opts.Config)
+
+			switch {
+			case usedDeprecatedLevel:
+				logrus.Warn("use of flag deprecated flag `--log-level`, please upgrade to `--level`")
+			case usedDeprecatedColor:
+				logrus.Warn("use of flag deprecated flag `--no-color`, please upgrade to `--color`")
+			}
 			return nil
 		},
 	}
 
-	cmd.PersistentFlags().StringVarP(&opts.logLevel, "log-level", "l", "info",
+	cmd.PersistentFlags().StringVarP(&opts.logLevel, "log-level", "l", "",
+		"set the logging level (can be one of: debug, info, warn, error, or fatal)")
+	cmd.PersistentFlags().StringVar(&opts.Level, "level", "",
 		"set the logging level (can be one of: debug, info, warn, error, or fatal)")
 	cmd.PersistentFlags().BoolVar(&opts.noColor, "no-color", false, "disable colored output")
+	cmd.PersistentFlags().BoolVar(&opts.Color, "color", true, "enable colored output")
+	cmd.PersistentFlags().BoolVar(&opts.Structured, "structured", true, "enable structured logging")
 
 	cmd.AddCommand(newCompletionCmd())
 	cmd.AddCommand(newVersionCmd())

determined-ai/determined

agent/cmd/determined-agent/root.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	log "github.com/sirupsen/logrus"
+	"fmt"
+
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+
+	"github.com/determined-ai/determined/master/pkg/logger"
 )
 
 type options struct {
+	logger.Config
+
 	logLevel string
 	noColor  bool
 }
@@ -22,23 +28,44 @@ func newRootCmd() *cobra.Command {
 			if err := bindEnv("DET_", cmd); err != nil {
 				return err
 			}
-			level, err := log.ParseLevel(opts.logLevel)
-			if err != nil {
-				return err
+
+			// Case hell for backwards compatibility - remove whenever, after 0.20 series.
+			var usedDeprecatedLevel bool
+			switch {
+			case opts.logLevel != "" && opts.Level != "":
+				return fmt.Errorf("cannot use `--log-level` and `--level`")
+			case opts.logLevel != "" && opts.Level == "":
+				usedDeprecatedLevel = true
+				opts.Level = opts.logLevel
+			case opts.logLevel == "" && opts.Level == "":
+				opts.Level = "info"
 			}
-			log.SetLevel(level)
-			log.SetFormatter(&log.TextFormatter{
-				FullTimestamp: true,
-				ForceColors:   true,
-				DisableColors: opts.noColor,
-			})
+
+			var usedDeprecatedColor bool
+			if opts.noColor && opts.Color {
+				usedDeprecatedColor = true
+				opts.Color = !opts.noColor
+			}
+
+			logger.SetLogrus(opts.Config)
+
+			switch {
+			case usedDeprecatedLevel:
+				logrus.Warn("use of flag deprecated flag `--log-level`, please upgrade to `--level`")
+			case usedDeprecatedColor:
+				logrus.Warn("use of flag deprecated flag `--no-color`, please upgrade to `--color`")
+			}
 			return nil
 		},
 	}
 
-	cmd.PersistentFlags().StringVarP(&opts.logLevel, "log-level", "l", "info",
+	cmd.PersistentFlags().StringVarP(&opts.logLevel, "log-level", "l", "",
+		"set the logging level (can be one of: debug, info, warn, error, or fatal)")
+	cmd.PersistentFlags().StringVar(&opts.Level, "level", "",
 		"set the logging level (can be one of: debug, info, warn, error, or fatal)")
 	cmd.PersistentFlags().BoolVar(&opts.noColor, "no-color", false, "disable colored output")
+	cmd.PersistentFlags().BoolVar(&opts.Color, "color", true, "enable colored output")
+	cmd.PersistentFlags().BoolVar(&opts.Structured, "structured", true, "enable structured logging")
 
 	cmd.AddCommand(newCompletionCmd())
 	cmd.AddCommand(newVersionCmd())