09845ee27dfedf244492da22375ed5314ef88c86
Eric Liu <eric.liu@hpe.com>
2022-12-16 16:22:37 -0800

    modify tensorboard manager to filter by workspace ID

--- master/internal/command/tensorboard_manager.go
+++ master/internal/command/tensorboard_manager.go
@@ -43,7 +43,8 @@ func (t *tensorboardManager) Receive(ctx *actor.Context) error {
 		}
 		for _, tensorboard := range ctx.AskAll(&tensorboardv1.Tensorboard{}, ctx.Children()...).GetAll() {
 			typed := tensorboard.(*tensorboardv1.Tensorboard)
-			if (len(users) == 0 && len(userIds) == 0) || users[typed.Username] || userIds[typed.UserId] {
+			if ((len(users) == 0 && len(userIds) == 0) || users[typed.Username] || userIds[typed.UserId]) &&
+				(msg.WorkspaceId == typed.WorkspaceId || msg.WorkspaceId == 0){
 				resp.Tensorboards = append(resp.Tensorboards, typed)
 			}
 		}
