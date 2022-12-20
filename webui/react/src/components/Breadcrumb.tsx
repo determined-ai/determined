89a64d00bd42016bb55faedd0397043eff0acfa8
John Kim <john.kim@hpe.com>
2022-12-20 10:35:07 -0800

    Breadcrumb component

---
+++ webui/react/src/components/Breadcrumb.tsx
@@ -0,0 +1,38 @@
+import { Breadcrumb } from 'antd';
+import React, { ReactNode } from 'react';
+
+interface BreadcrumbProps {
+  children?: ReactNode;
+  className?: string;
+  separator?: ReactNode;
+}
+
+interface BreadcrumbItemProps {
+  children?: ReactNode
+}
+
+type BreadcrumbItem = React.FC;
+type BreadcrumbSeparator = React.FC;
+type BreadcrumbComponent = React.FC<BreadcrumbProps> & { Item: typeof BreadcrumbItem, Separator: typeof BreadcrumbSeparator };
+
+const BreadcrumbDet: BreadcrumbComponent = (props: BreadcrumbProps): JSX.Element => {
+  return (
+    <Breadcrumb separator="" {...props} />);
+};
+
+const BreadcrumbItem: BreadcrumbItem = (props: BreadcrumbItemProps) => {
+  return (
+    <Breadcrumb.Item {...props} />
+  );
+};
+
+const BreadcrumbSeparator: BreadcrumbSeparator = () => {
+  return (
+    <Breadcrumb.Separator />
+  );
+};
+
+BreadcrumbDet.Item = BreadcrumbItem;
+BreadcrumbDet.Separator = BreadcrumbSeparator;
+
+export default BreadcrumbDet;
