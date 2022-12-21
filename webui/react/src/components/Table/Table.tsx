64709e1051e818eb1c57e6139d6e0efeddac0310
gt2345 <gt2345@columbia.edu>
2022-12-21 16:22:13 -0600

    fix

--- webui/react/src/components/Table/Table.tsx
+++ webui/react/src/components/Table/Table.tsx
@@ -16,7 +16,6 @@ import Icon from 'shared/components/Icon/Icon';
 import Spinner from 'shared/components/Spinner';
 import { Pagination } from 'shared/types';
 import { getDuration } from 'shared/utils/datetime';
-import { useUsers } from 'stores/users';
 import { StateOfUnion } from 'themes';
 import {
   CommandTask,
@@ -31,7 +30,6 @@ import {
   TrialItem,
   Workspace,
 } from 'types';
-import { Loadable } from 'utils/loadable';
 import { canBeOpened } from 'utils/task';
 import { openCommand } from 'utils/wait';
 
@@ -110,16 +108,6 @@ export const tooltipRenderer: Renderer = (text) => (
 );
 
 export const userRenderer: React.FC<DetailedUser | undefined> = (user) => {
-  return (
-    <div className={`${css.centerVertically} ${css.centerHorizontally}`}>
-      {user ? <UserAvatar user={user} /> : <Spinner />}
-    </div>
-  );
-};
-
-export const UserRenderer: Renderer<{ userId?: number }> = (_, record) => {
-  const users = Loadable.getOrElse([], useUsers());
-  const user = users.find((user) => user.id === record.userId);
   return (
     <div className={`${css.centerVertically} ${css.centerHorizontally}`}>
       {user ? <UserAvatar user={user} /> : <Spinner />}
