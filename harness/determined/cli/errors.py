2be9992381f329bb8bd78913c03bb9df2117e347
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-12-27 10:38:17 -0800

    define some expected types

--- harness/determined/cli/errors.py
+++ harness/determined/cli/errors.py
@@ -1,7 +1,10 @@
 from typing import Callable, Any
 import sys
+from requests import RequestException
 from termcolor import colored
 import argparse
+
+from harness.determined.common.api.bindings import APIHttpError
 
 
 class FeatureFlagDisabled(Exception):
@@ -25,15 +28,17 @@ def report_cli_errors(func: Callable[[argparse.Namespace], Any]) -> Callable[...
     def wrapper(args: argparse.Namespace) -> Any:
         try:
             return func(args)
-        except Exception as e:
+        except CliError as e:
             """
-            DISCUSS: is this a good pattern in Python? if so we could do this at argparse.Cmd
+            DISCUSS: is this a reasonable pattern in Python? if so we could do this at argparse.Cmd
             level there are some expected exceptions that we rather not individually handle
             and early break in cli commands. and for there we assume that we don't want to
             show the call stack to the end user. maybe the callstack can be controlled w/
             a flag. (dev, prod)
             """
-            # TODO: define expected types.
+            print(colored(f"Error: {e}", "red"), file=sys.stderr)
+            sys.exit(1)
+        except (APIHttpError, RequestException, ConnectionError) as e:
             print(colored(f"Error: {e}", "red"), file=sys.stderr)
             sys.exit(1)
 
