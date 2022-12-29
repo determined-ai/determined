bc799bbd461d17ca4eee87efd40281683c6c6e60
Ilia Glazkov <ilia@hpe.com>
2022-12-29 13:24:39 -0800

    upgraded goreleaser, refined install script.

--- master/get-deps.sh
+++ master/get-deps.sh
@@ -2,7 +2,7 @@ go install mvdan.cc/gofumpt@v0.3.1
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.45.2
 go install github.com/bufbuild/buf/cmd/buf@v0.42.1
 go install golang.org/x/tools/cmd/goimports@v0.1.5
-go install github.com/goreleaser/goreleaser@v1.13.1
+go install github.com/goreleaser/goreleaser@v1.14.1
 go install github.com/ryanbressler/CloudForest/growforest@v0.0.0-20161201194407-d014dc32840a
 go install github.com/swaggo/swag/cmd/swag@v1.7.0
 go install github.com/vektra/mockery/v2@v2.13.1
