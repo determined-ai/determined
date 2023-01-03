3f208f8183c3b05a141e92e1a7e6640db2c07f5a
Eric Liu <eric.liu@hpe.com>
2023-01-03 13:55:44 -0800

    migrating tensorboard rbac interface to nsc

--- master/internal/command/authz_basic_impl.go
+++ master/internal/command/authz_basic_impl.go
@@ -3,6 +3,9 @@ package command
 import (
 	"context"
 
+	"github.com/determined-ai/determined/proto/pkg/tensorboardv1"
+
+	"github.com/determined-ai/determined/master/internal/config"
 	"github.com/determined-ai/determined/master/pkg/model"
 )
 
@@ -38,6 +41,40 @@ func (a *NSCAuthZBasic) CanCreateNSC(
 // CanSetNSCsPriority always returns a nil error.
 func (a *NSCAuthZBasic) CanSetNSCsPriority(
 	ctx context.Context, curUser model.User, workspaceID model.AccessScopeID, priority int,
+) error {
+	return nil
+}
+
+// CanGetTensorboard returns true and nil error unless the developer master config option
+// security.authz._strict_ntsc_enabled is true then it returns a boolean if the user is
+// an admin or if the user owns the tensorboard and a nil error.
+func (a *NSCAuthZBasic) CanGetTensorboard(
+	ctx context.Context, curUser *model.User, tb *tensorboardv1.Tensorboard,
+) (canGetTensorboards bool, serverError error) {
+	if !config.GetMasterConfig().Security.AuthZ.StrictNTSCEnabled {
+		return true, nil
+	}
+
+	return curUser.Admin || int32(curUser.ID) == tb.UserId, nil
+}
+
+// FilterTensorboards always returns the same list.
+func (a *NSCAuthZBasic) FilterTensorboards(
+	ctx context.Context, curUser *model.User, tensorboards []*tensorboardv1.Tensorboard,
+) ([]*tensorboardv1.Tensorboard, error) {
+	return tensorboards, nil
+}
+
+// CanTerminateTensorboard always returns nil.
+func (a *NSCAuthZBasic) CanTerminateTensorboard(
+	ctx context.Context, curUser *model.User, tb *tensorboardv1.Tensorboard,
+) error {
+	return nil
+}
+
+// CanSetTensorboardPriority always returns nil.
+func (a *NSCAuthZBasic) CanSetTensorboardPriority(
+	ctx context.Context, curUser *model.User, tb *tensorboardv1.Tensorboard,
 ) error {
 	return nil
 }
--- master/internal/command/authz_iface.go
+++ master/internal/command/authz_iface.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"context"
+	"github.com/determined-ai/determined/proto/pkg/tensorboardv1"
 
 	"github.com/determined-ai/determined/master/internal/authz"
 	"github.com/determined-ai/determined/master/pkg/model"
@@ -9,6 +10,7 @@ import (
 
 // NSCAuthZ describes authz methods for Notebooks, Shells, and Commands.
 type NSCAuthZ interface {
+	// NSC functions
 	// GET /api/v1/NSCs/:nsc_id
 	// GET /tasks
 	CanGetNSC(
@@ -31,6 +33,27 @@ type NSCAuthZ interface {
 	// PATCH /NSCs/:nsc_id
 	CanSetNSCsPriority(
 		ctx context.Context, curUser model.User, workspaceID model.AccessScopeID, priority int,
+	) error
+
+	// Tensorboard functions
+	// GET /api/v1/tensorboards/:tb_id
+	CanGetTensorboard(
+		ctx context.Context, curUser *model.User, tb *tensorboardv1.Tensorboard,
+	) (canGetTensorboard bool, serverError error)
+
+	// GET /api/v1/tensorboards
+	FilterTensorboards(
+		ctx context.Context, curUser *model.User, tensorboards []*tensorboardv1.Tensorboard,
+	) ([]*tensorboardv1.Tensorboard, error)
+
+	// POST /api/v1/tensorboards/:tb_id/kill
+	CanTerminateTensorboard(
+		ctx context.Context, curUser *model.User, tb *tensorboardv1.Tensorboard,
+	) error
+
+	// POST /api/v1/tensorboards/:tb_id/set_priority
+	CanSetTensorboardPriority(
+		ctx context.Context, curUser *model.User, tb *tensorboardv1.Tensorboard,
 	) error
 }
 
