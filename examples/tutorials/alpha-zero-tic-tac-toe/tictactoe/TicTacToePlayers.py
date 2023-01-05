f595893e665a84f738e4aba89046817bc9d6953f
Erik Wilson <erik.wilson@hpe.com>
2023-01-05 09:18:36 -0700

    Cleanup comments

--- examples/tutorials/alpha-zero-tic-tac-toe/tictactoe/TicTacToePlayers.py
+++ examples/tutorials/alpha-zero-tic-tac-toe/tictactoe/TicTacToePlayers.py
@@ -34,11 +34,7 @@ class HumanTicTacToePlayer:
             if valid[i]:
                 print(int(i / self.game.n), int(i % self.game.n))
         while True:
-            # Python 3.x
             a = input()
-            # Python 2.x
-            # a = raw_input()
-
             x, y = [int(x) for x in a.split(" ")]
             a = self.game.n * x + y if x != -1 else self.game.n ** 2
             if valid[a]:
