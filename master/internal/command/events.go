71c43929b1d9f1429c60559b9d76333686b5c5f5
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-01-09 13:54:16 -0800

    feat: cover ntsc events and proxy path w/ rbac (#5666)
    https://determinedai.atlassian.net/browse/DET-8751

--- master/internal/command/events.go
+++ master/internal/command/events.go
@@ -134,24 +134,24 @@ func (e *eventManager) Receive(ctx *actor.Context) error {
 func canAccessCommandEvents(ctx *actor.Context, c echo.Context) error {
 	curUser := c.(*context.DetContext).MustGetUser()
 	taskID := model.TaskID(ctx.Self().Parent().Address().Local())
-
-	snapshot := CommandSnapshot{}
-
 	reqCtx := c.Request().Context()
-	if err := db.Bun().NewSelect().Model(&snapshot).
-		Relation("Task.Job").
-		Where("task.task_id = ?", taskID).
-		Scan(reqCtx); err != nil {
+	spec, err := db.GetCommandGenericSpec(reqCtx, taskID)
+	if err != nil {
 		return err
 	}
 
-	// TODO(DET-8751): separate NTSC types: tensorboards would need to consult ExperimentAuthZ.
-	if ok, err := AuthZProvider.Get().CanGetNSC(reqCtx, curUser, *snapshot.Task.Job.OwnerID,
-		snapshot.GenericCommandSpec.Metadata.WorkspaceID,
-	); err != nil {
+	var ok bool
+	if spec.TaskType == model.TaskTypeTensorboard {
+		ok, err = AuthZProvider.Get().CanGetTensorboard(
+			reqCtx, curUser, spec.Base.Owner.ID, spec.Metadata.WorkspaceID)
+	} else {
+		ok, err = AuthZProvider.Get().CanGetNSC(
+			reqCtx, curUser, spec.Base.Owner.ID, spec.Metadata.WorkspaceID)
+	}
+	if err != nil {
 		return err
 	} else if !ok {
-		return echo.NewHTTPError(http.StatusNotFound, "Not Found")
+		return echo.NewHTTPError(http.StatusNotFound, "service not found: "+taskID)
 	}
 	return nil
 }
