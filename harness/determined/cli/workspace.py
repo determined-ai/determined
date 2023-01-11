81748b20d6039606f9cdeb366009dbdde1d2dd0a
Eric Liu <eric.liu@hpe.com>
2023-01-11 13:31:53 -0800

    feat: add workspace filtering to tensorboards cli

--- harness/determined/cli/workspace.py
+++ harness/determined/cli/workspace.py
@@ -25,6 +25,20 @@ WORKSPACE_HEADERS = [
 workspace_arg: Arg = Arg("--workspace-name", type=str, help="workspace name")
 
 
+def get_workspace_id_from_args(args: Namespace) -> int:
+    workspace_id = None
+    if args.workspace_name:
+        workspace = cli.workspace.get_workspace_by_name(
+            cli.setup_session(args), args.workspace_name
+        )
+        if workspace is None:
+            return cli.report_cli_error(f'Workspace "{args.workspace_name}" not found')
+        if workspace.archived:
+            return cli.report_cli_error(f'Workspace "{args.workspace_name}" is archived')
+        workspace_id = workspace.id
+    return workspace_id
+
+
 def get_workspace_names(session: api.Session) -> Dict[int, str]:
     """Get a mapping of workspace IDs to workspace names."""
     resp = bindings.get_GetWorkspaces(session)
