fd2f6fc38a93046ef2938ef359c7cf9b4b5b263f
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-01-12 13:54:22 -0800

    avoid msging managers that are not updated yet.

--- master/internal/command/ntsc.go
+++ master/internal/command/ntsc.go
@@ -1,28 +1,29 @@
 package command
 
-import "github.com/determined-ai/determined/master/pkg/actor"
+import (
+	"fmt"
+
+	"github.com/determined-ai/determined/master/pkg/actor"
+)
 
 // MessageNTSC sends a msg to all ntsc managers.
 // CHECK: Tell(diff semantics?) or Ask
 func MessageNTSC(system *actor.System, msg interface{}) actor.Responses {
 	// CHAT: this could also accept an *actor.Context.
 	refs := []*actor.Ref{
-		system.Get(actor.Addr(CommandActorPath)),
+		// system.Get(actor.Addr(CommandActorPath)),
 		system.Get(actor.Addr(NotebookActorPath)),
-		system.Get(actor.Addr(TensorboardActorPath)),
-		system.Get(actor.Addr(ShellActorPath)),
+		// system.Get(actor.Addr(TensorboardActorPath)),
+		// system.Get(actor.Addr(ShellActorPath)),
 	}
 
 	// CHECK: can we message "notebooks/*" ?
-
-	// filter out nil refs
-	refs = refs[:0]
+	// FIXME: what if some of the actors aren't up? timeout?
 	for _, ref := range refs {
-		if ref != nil {
-			refs = append(refs, ref)
+		if ref == nil {
+			panic(fmt.Sprintf("ref is nil"))
 		}
 	}
 
-	// FIXME: what if some of the actors aren't up? timeout?
 	return system.AskAll(msg, refs...)
 }
