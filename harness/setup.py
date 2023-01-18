a5f5178496e5e3fb0c84d3b4707ef6b8c0fb18e3
Hanyu Cui <hanyu.cui@hpe.com>
2023-01-17 17:58:23 -0800

    chore: bump version: 0.19.10-dev0 -> 0.19.10-rc0

--- harness/setup.py
+++ harness/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="determined",
-    version="0.19.10-dev0",
+    version="0.19.10-rc0",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
