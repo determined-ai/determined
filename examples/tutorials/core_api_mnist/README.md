4a5a77d13815a9dddbfad7369db1ae99fd8b90f9
Isha Ghodgaonkar <isha.ghodgaonkar@hpe.com>
2023-01-19 09:43:53 -0800

    feat: add Core API MNIST tutorial

---
+++ examples/tutorials/core_api_mnist/README.md
@@ -0,0 +1,28 @@
+Core API MNIST Tutorial
+
+This tutorial is structured similarly to the [Integer Incrementing Core API Tutorial](https://github.com/determined-ai/determined/tree/master/examples/tutorials/core_api) and integrates the [PyTorch MNIST training example](https://github.com/pytorch/examples/blob/main/mnist/main.py) into Determined.
+
+## Files
+* **model_def.py**: The starting script (same as [main.py](https://github.com/pytorch/examples/blob/main/mnist/main.py) with no Determined integrations at all.
+* **const.yaml**: A barebones config to run the `model_def.py` script on
+  a Determined cluster.
+* **model_def_metrics.py**: Modify `model_def.py` to report metrics to Determined.
+* **metrics.yaml**: A config to run `model_def_metrics.py`
+* **model_def_checkpoints.py**: Modify `model_def_metrics.py` to save and load to and from
+  checkpoint storage.  This also introduces preemption support.
+* **checkpoints.yaml**: A config to run `model_def_checkpoints.py`.
+* **model_def_adaptive.py**: Modify `model_def_checkpoints.py` to participate in the
+  hyperparameter search offered by the Determined master.
+* **adaptive.yaml**: Use an adaptive ASHA search to optimize a hyperparameter.
+* **model_def_distributed.py**: Introduce distributed training to our script.
+* **distributed.yaml**: Run `model_def_distributed.py` across many nodes.
+
+## To Run
+If you have not yet installed Determined, installation instructions can be found
+under `docs/install-admin.html` or at https://docs.determined.ai/latest/index.html
+
+Run stage 0 with the following command:
+
+    det -m <master host:port> experiment create -f const.yaml .
+
+The other stages can be run by specifying the appropriate config file.
---
+++ examples/tutorials/core_api_mnist/model_def_distributed.py
@@ -0,0 +1,191 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+import torch 
+import determined as det
+import pathlib
+
+class Net(nn.Module):
+    def __init__(self, hparams):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, hparams["n_filters1"], 3, 1)
+        self.conv2 = nn.Conv2d(hparams["n_filters1"], hparams["n_filters2"], 3, 1)
+        self.dropout1 = nn.Dropout(hparams["dropout1"])
+        self.dropout2 = nn.Dropout(hparams["dropout2"])
+        self.fc1 = nn.Linear(144 * hparams["n_filters2"], 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+def train(args, model, device, train_loader, optimizer, epoch, core_context):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if (batch_idx + 1) % args.log_interval == 0:
+            
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            epoch, (batch_idx) * len(data), len(train_loader.dataset),
+            100. * (batch_idx) / len(train_loader), loss.item()))
+            
+            # NEW: report metrics only on rank 0
+            if core_context.distributed.rank == 0:
+                core_context.train.report_training_metrics(steps_completed=(batch_idx+1) + (epoch-1) * len(train_loader), \
+                 metrics={"train_loss":loss.item()})
+            
+            if args.dry_run:
+                break
+
+
+def test(args, model, device, test_loader, epoch, core_context, len_trainset):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+            if (batch_idx + 1) % args.log_interval == 0:
+                
+                # NEW: report metrics only on rank 0
+                if core_context.distributed.rank == 0:
+                    core_context.train.report_validation_metrics(steps_completed= (epoch) * len_trainset, \
+                    metrics={"test_loss": (test_loss /len(test_loader.dataset))})
+                    
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+def load_state(checkpoint_directory):
+    checkpoint_directory = pathlib.Path(checkpoint_directory)
+    with checkpoint_directory.joinpath("state").open("r") as f:
+        return torch.load(f)
+
+
+def main(core_context):
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=14, metavar='N',
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
+                        help='learning rate (default: 1.0)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--no-mps', action='store_true', default=True,
+                        help='disables macOS GPU training')
+    parser.add_argument('--dry-run', action='store_true', default=False,
+                        help='quickly check a single pass')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save-model', action='store_true', default=True,
+                        help='For Saving the current Model')
+  
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    use_mps = not args.no_mps and torch.backends.mps.is_available()
+
+    info = det.get_cluster_info()
+    assert info is not None, "this example only runs on-cluster"
+    latest_checkpoint = info.latest_checkpoint
+    if latest_checkpoint is not None: 
+        with core_context.checkpoint.restore_path(latest_checkpoint) as path:
+            model = load_state(path)
+
+    hparams = info.trial.hparams
+
+    torch.manual_seed(args.seed)
+
+    if use_cuda:
+        device = torch.device("cuda")
+    elif use_mps:
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+
+    train_kwargs = {'batch_size': args.batch_size}
+    test_kwargs = {'batch_size': args.test_batch_size}
+    if use_cuda:
+        cuda_kwargs = {'num_workers': 1,
+                       'pin_memory': True,
+                       'shuffle': True}
+        train_kwargs.update(cuda_kwargs)
+        test_kwargs.update(cuda_kwargs)
+
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+        ])
+    dataset1 = datasets.MNIST('../data', train=True, download=True,
+                       transform=transform)
+    dataset2 = datasets.MNIST('../data', train=False,
+                       transform=transform)
+    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
+    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+
+    model = Net(hparams).to(device)
+    optimizer = optim.Adadelta(model.parameters(), lr=hparams["learning_rate"])
+
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    for epoch in range(1, args.epochs + 1):
+        len_trainset = len(train_loader)
+        train(args, model, device, train_loader, optimizer, epoch, core_context)
+        test(args, model, device, test_loader, epoch, core_context, len_trainset)
+        scheduler.step()
+        
+        if core_context.distributed.rank == 0:
+            if args.save_model:
+                checkpoint_metadata_dict = {
+                    'steps_completed': len(train_loader),
+                }
+                
+            with core_context.checkpoint.store_path(checkpoint_metadata_dict) as (path, storage_id):
+                torch.save(model.state_dict(), str(path) + ('/checkpoint.pt'))
+
+            if core_context.preempt.should_preempt():
+                return
+    
+if __name__ == '__main__':
+
+    # NEW: initialize distributed context 
+    distributed = det.core.DistributedContext.from_torch_distributed()
+    with det.core.init(distributed=distributed) as core_context:
+        main(core_context=core_context)
