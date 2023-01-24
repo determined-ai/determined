2fb3c0f74e8874375790336244fa6bb114bb4497
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-01-24 10:22:06 -0800

    WIP adding push stage

--- pre-commit/check.py
+++ pre-commit/check.py
@@ -2,12 +2,68 @@
 
 from typing import List, Union, Dict, Set
 from pathlib import Path
+import argparse
 import os
 import sys
 
-exit(1)
 # TODO add option to run rules in parallel
 # TODO add option to pass in a git diff to check or git hash
+
+
+def get_git_commit_files(commit_hash: str) -> List[Path]:
+    output = os.popen(f"git diff-tree --no-commit-id --name-only -r {commit_hash}").read()
+    lines = output.split("\n")
+    lines = [x for x in lines if x]
+    files = [Path(x).absolute() for x in lines]
+    return files
+
+
+# get a list of paths to dirty and staged files from git
+def get_git_status() -> List[Path]:
+    git_status = os.popen("git status --porcelain").read()
+    git_status_list = git_status.split("\n")
+    git_status_list = [x for x in git_status_list if x]
+    files = [Path(x.split()[1]).absolute() for x in git_status_list]
+    return files
+
+
+# gets last changed files
+def get_changed_files() -> List[Path]:
+    files = get_git_status()
+    if len(files) != 0:
+        return files
+    files = get_git_commit_files("HEAD")
+    return files
+
+
+# check if path a child of another path
+def is_child(path: Path, parent: Path) -> bool:
+    assert path.is_absolute()
+    assert parent.is_absolute()
+    cur_path = path
+    while cur_path != parent and cur_path != root:
+        cur_path = cur_path.parent
+        if cur_path == parent:
+            return True
+    return False
+
+
+def unpushed_files() -> List[Path]:
+    """return all files that are committed but not pushed."""
+    # get upstream branch name
+    upstream_branch = (
+        os.popen("git rev-parse --abbrev-ref --symbolic-full-name @{u}").read().strip()
+    )
+    # get all commits that are on the current branch but not on the upstream branch
+    commits = os.popen(f"git log {upstream_branch}..HEAD --oneline").read().strip()
+    commits = [x.split()[0] for x in commits.split("\n")]
+    # get all files that are changed in the commits
+    files: List[Path] = []
+    for commit in commits:
+        files.extend(get_git_commit_files(commit))
+    # remove duplicates
+    return sorted(list(set(files)))
+
 
 # check if PRECOMMIT_ENABLE_SLOW is set to true
 if os.environ.get("PRE_COMMIT_ENABLE_SLOW", "false") != "true":
@@ -50,44 +106,6 @@ rules: Dict[Path, Union[str, List[str]]] = {
 }
 
 
-def get_git_commit_files(commit_hash: str) -> List[Path]:
-    output = os.popen(f"git diff-tree --no-commit-id --name-only -r {commit_hash}").read()
-    lines = output.split("\n")
-    lines = [x for x in lines if x]
-    files = [Path(x).absolute() for x in lines]
-    return files
-
-
-# get a list of paths to dirty and staged files from git
-def get_git_status() -> List[Path]:
-    git_status = os.popen("git status --porcelain").read()
-    git_status_list = git_status.split("\n")
-    git_status_list = [x for x in git_status_list if x]
-    files = [Path(x.split()[1]).absolute() for x in git_status_list]
-    return files
-
-
-# gets last changed files
-def get_changed_files() -> List[Path]:
-    files = get_git_status()
-    if len(files) != 0:
-        return files
-    files = get_git_commit_files("HEAD")
-    return files
-
-
-# check if path a child of another path
-def is_child(path: Path, parent: Path) -> bool:
-    assert path.is_absolute()
-    assert parent.is_absolute()
-    cur_path = path
-    while cur_path != parent and cur_path != root:
-        cur_path = cur_path.parent
-        if cur_path == parent:
-            return True
-    return False
-
-
 # check if path is the same or child of one of the rules and execute the rule as
 # subprocess
 def run_rule(rule_path: Path) -> int:
@@ -115,8 +133,24 @@ def find_rules(paths: List[Path]):
 
 
 def main():
+    # define an arg to get the stage
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        "--stage",
+        choices=["pre-commit", "pre-push"],
+        default="pre-commit",
+        help="The stage of the git hook to run",
+    )
+    args = argparser.parse_args()
+
     failed_rules: Set[Path] = set()
-    changed_files = get_changed_files()
+    changed_files: List[Path] = []
+    if args.stage == "pre-commit":
+        changed_files = get_changed_files()
+    elif args.stage == "pre-push":
+        changed_files = unpushed_files()
+    else:
+        raise ValueError(f"unknown stage {args.stage}")
     if len(changed_files) == 0:
         print("No changed files.")
         exit(0)
