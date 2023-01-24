bcba189bbe74662c79a816558cec195a410efd3c
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2023-01-24 14:19:34 -0500

    Revert "task logging fails"
    This reverts commit d91357a6af3aa1a2ad181353fc2662683fb4eac3.

--- master/static/srv/task-logging-teardown.sh
+++ master/static/srv/task-logging-teardown.sh
@@ -11,21 +11,15 @@ epoch_seconds() {
     printf '%(%s)T\n' -1
 }
 
-echo $(date -u) "DET_LOG_WAIT_COUNT $DET_LOG_WAIT_COUNT"
-
 # Wait for 30 seconds total for the logging to finish, otherwise just exit.
 waitfor="${DET_LOG_WAIT_TIME:-42}"
 deadline="$(($(epoch_seconds) + waitfor))"
 for ((i = 0; i < DET_LOG_WAIT_COUNT; i++)); do
     timeout="$((deadline - $(epoch_seconds)))"
-    echo $(date -u) "Timeout $timeout"    
     test "$timeout" -le 0 && break
-
-    echo $(date -u) "STARTING reading"        
     # read returns 1 on EOF or >128 with timeout, but it's a fifo so that is OK.
     # For read's -t timeout feature to work, we need to open the fifo for
     # reading and writing for some reason, which is what the `<>` is for.
     # See https://stackoverflow.com/a/6448737.
     read -N 1 -t "$timeout" <>"$DET_LOG_WAIT_FIFO" || true
-    echo $(date -u) "reading finished"            
 done
