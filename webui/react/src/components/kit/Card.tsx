6618a40af56d7481c307f26867ccc34c6940940c
Emily Bonar <emily.bonar@hpe.com>
2023-01-25 16:24:41 -0500

    Remove hint

--- webui/react/src/components/kit/Card.tsx
+++ webui/react/src/components/kit/Card.tsx
@@ -11,21 +11,13 @@ interface CardProps {
   actionMenu?: MenuProps;
   children?: React.ReactNode;
   footer?: React.ReactNode;
-  hint?: React.ReactNode;
   onClick?: () => void;
   title?: React.ReactNode;
 }
 
 const stopPropagation = (e: React.MouseEvent): void => e.stopPropagation();
 
-const Card: React.FC<CardProps> = ({
-  actionMenu,
-  children,
-  footer,
-  hint,
-  onClick,
-  title,
-}: CardProps) => {
+const Card: React.FC<CardProps> = ({ actionMenu, children, footer, onClick, title }: CardProps) => {
   return (
     <ConditionalWrapper
       condition={!!actionMenu}
@@ -35,23 +27,20 @@ const Card: React.FC<CardProps> = ({
         </Dropdown>
       )}>
       <div className={css.base} tabIndex={onClick ? 0 : -1} onClick={onClick}>
-        {(!!title || !!actionMenu || !!hint) && (
+        {(!!title || !!actionMenu) && (
           <div className={css.header}>
             <Typography.Title className={css.title} ellipsis={{ rows: 1, tooltip: true }} level={5}>
               {title}
             </Typography.Title>
-            <div className={css.extraArea}>
-              {!!actionMenu && (
-                <div className={css.action}>
-                  <Dropdown menu={actionMenu} placement="bottomRight" trigger={['click']}>
-                    <Button type="text" onClick={stopPropagation}>
-                      <Icon name="overflow-horizontal" />
-                    </Button>
-                  </Dropdown>
-                </div>
-              )}
-              {!!hint && <div className={css.hint}>{hint}</div>}
-            </div>
+            {!!actionMenu && (
+              <div className={css.action}>
+                <Dropdown menu={actionMenu} placement="bottomRight" trigger={['click']}>
+                  <Button type="text" onClick={stopPropagation}>
+                    <Icon name="overflow-horizontal" />
+                  </Button>
+                </Dropdown>
+              </div>
+            )}
           </div>
         )}
         <div className={css.content}>{children}</div>
