433388e28f89e0b69758d3f988f6c059002b5ee0
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-01-25 11:33:11 -0800

    nope, can't do that.
    Revert "keep the type updates"
    
    This reverts commit da48255719786d4b258cf2d73d1d66f277c21b43.

--- model_hub/model_hub/mmdetection/_data.py
+++ model_hub/model_hub/mmdetection/_data.py
@@ -5,7 +5,7 @@ import functools
 import logging
 import math
 import os
-from typing import Any, Iterator, List, Tuple
+from typing import Any, Iterator, Tuple
 
 import filelock
 import mmcv
@@ -59,12 +59,12 @@ class GroupSampler(torch.utils.data.Sampler):
         self.total_size = self.num_samples * self.num_replicas
 
     def __iter__(self) -> Iterator[Any]:
-        indices: List[np.ndarray] = []
+        indices = []
         for i, size in enumerate(self.group_sizes):
             if size > 0:
-                indice_nda = np.where(self.flag == i)[0]
-                assert len(indice_nda) == size
-                indice: list = indice_nda[list(torch.randperm(int(size)))].tolist()
+                indice = np.where(self.flag == i)[0]
+                assert len(indice) == size
+                indice = indice[list(torch.randperm(int(size)))].tolist()
                 extra = int(
                     math.ceil(size * 1.0 / self.samples_per_gpu / self.num_replicas)
                 ) * self.samples_per_gpu * self.num_replicas - len(indice)
