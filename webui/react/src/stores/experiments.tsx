26627ff41f7c48deb5d22cecdac34eefc5662550
Keita Nonaka <keita.nonaka@hpe.com>
2023-01-26 15:47:42 -0800

    chore: cache separately

--- webui/react/src/stores/experiments.tsx
+++ webui/react/src/stores/experiments.tsx
@@ -1,18 +1,25 @@
 import { Map } from 'immutable';
 
 import { getExperiments } from 'services/api';
+import { V1Pagination } from 'services/api-ts-sdk';
 import { GetExperimentsParams } from 'services/types';
-import { ExperimentPagination } from 'types';
+import { ExperimentItem, ExperimentPagination } from 'types';
 import handleError from 'utils/error';
 import { Loadable, Loaded, NotLoaded } from 'utils/loadable';
 import { Observable, observable, WritableObservable } from 'utils/observable';
 import { encodeParams } from 'utils/store';
 
+type ExperimentsCache = {
+  experimentIds: Readonly<number[]>;
+  pagination: Readonly<V1Pagination>;
+};
+
 // this is singleton
 export class ExperimentsService {
   static #isInternalConstructing = false;
   static #instance: ExperimentsService;
-  #experimentsCache: WritableObservable<Map<string, ExperimentPagination>> = observable(Map());
+  #experimentsCache: WritableObservable<Map<string, ExperimentsCache>> = observable(Map());
+  #experimentMap: WritableObservable<Map<number, ExperimentItem>> = observable(Map());
 
   private constructor() {
     if (!ExperimentsService.#isInternalConstructing) {
@@ -32,9 +39,24 @@ export class ExperimentsService {
   public experimentsByParams(
     params: Readonly<GetExperimentsParams>,
   ): Observable<Loadable<ExperimentPagination>> {
-    return this.#experimentsCache.select((exp) => {
-      const loadedVal = exp.get(encodeParams(params));
-      return loadedVal ? Loaded(loadedVal) : NotLoaded;
+    return this.#experimentsCache.select((map) => {
+      const cache = map.get(encodeParams(params));
+      if (!cache) {
+        return NotLoaded;
+      }
+      const experiments: ExperimentItem[] = [];
+      const expMap = this.#experimentMap.get();
+      for (const id of cache.experimentIds) {
+        const expItem = expMap.get(id);
+        if (expItem) {
+          experiments.push(expItem);
+        }
+      }
+      const expPagination: ExperimentPagination = {
+        experiments: experiments,
+        pagination: cache.pagination,
+      };
+      return Loaded(expPagination);
     });
   }
 
@@ -45,13 +67,33 @@ export class ExperimentsService {
     return async () => {
       try {
         const response = await getExperiments(params, { signal: canceler.signal });
-        this.#experimentsCache.update((prevState: Map<string, ExperimentPagination>) => {
-          const newState = prevState.set(encodeParams(params), response);
-          return newState;
-        });
+        this.#updateexperimentsCache(response, params);
+        this.#updateExperimentMap(response);
       } catch (e) {
         handleError(e);
       }
     };
+  }
+
+  #updateExperimentMap(expPagination: Readonly<ExperimentPagination>) {
+    this.#experimentMap.update((map) => {
+      let newMap: Map<number, ExperimentItem> = Map<number, ExperimentItem>();
+      for (const exp of expPagination.experiments) {
+        newMap = map.set(exp.id, exp);
+      }
+      return newMap;
+    });
+  }
+
+  #updateexperimentsCache(
+    expPagination: Readonly<ExperimentPagination>,
+    params: Readonly<GetExperimentsParams>,
+  ) {
+    this.#experimentsCache.update((prevState: Map<string, ExperimentsCache>) => {
+      const experimentIds: Readonly<number[]> = expPagination.experiments.map((exp) => exp.id);
+      const expCache: ExperimentsCache = { experimentIds, pagination: expPagination.pagination };
+      const newState = prevState.set(encodeParams(params), expCache);
+      return newState;
+    });
   }
 }
