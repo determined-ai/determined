300d2a0b5798156106dcefb6ebba14093ee20c82
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-01-25 17:50:07 -0800

    test: speed up user tests (#5783)
    add user and auth test utils

--- e2e_tests/tests/api_utils.py
+++ e2e_tests/tests/api_utils.py
@@ -1,9 +1,14 @@
+import uuid
 from typing import Optional
 
 from determined.common import api
-from determined.common.api import authentication, certs
+from determined.common.api import authentication, bindings, certs
 from tests import config as conf
 from tests.cluster import test_users
+
+
+def get_random_string() -> str:
+    return str(uuid.uuid4())
 
 
 def determined_test_session(
@@ -24,3 +29,25 @@ def determined_test_session(
         murl, requested_user=credentials.username, password=credentials.password
     )
     return api.Session(murl, credentials.username, authentication.cli_auth, certs.cli_cert)
+
+
+def create_test_user(
+    add_password: bool = False,
+    session: Optional[api.Session] = None,
+    user: Optional[bindings.v1User] = None,
+) -> authentication.Credentials:
+    session = session or determined_test_session(admin=True)
+    user = user or bindings.v1User(username=get_random_string(), admin=False, active=True)
+    password = get_random_string() if add_password else ""
+    bindings.post_PostUser(session, body=bindings.v1PostUserRequest(user=user, password=password))
+    return authentication.Credentials(user.username, password)
+
+
+def configure_token_store(credentials: authentication.Credentials) -> None:
+    """Authenticate the user for CLI usage with the given credentials."""
+    token_store = authentication.TokenStore(conf.make_master_url())
+    token = authentication.do_login(
+        conf.make_master_url(), credentials.username, credentials.password, certs.cli_cert
+    )
+    token_store.set_token(credentials.username, token)
+    token_store.set_active(credentials.username)
