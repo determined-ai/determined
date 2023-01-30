10e2227acb7cf3424e6316caf57247b794dfebf4
Jerry J. Harrow <84593277+jerryharrow@users.noreply.github.com>
2023-01-30 09:38:15 -0500

    chore: Add slurmcluster.sh support for aizn cluster (#648)
    The aizn/znode cluster is behind the hpcgate gateway host.
    Add support to configure the tunnel properly indirectly through
    hpcgate, and pull the auth token indirectly via hpcgate.
    
    OPT_LAUNCHERHOST is the value used in the master.yaml and was always configred
    as localhost, so do that automatically instead of requiring it per-node.
    Add OPT_LAUNCHERACTUALHOST which is the eventual hostname on which the launcher
    is executing to enable indirection via hpcgate.

--- tools/slurmcluster.sh
+++ tools/slurmcluster.sh
@@ -126,23 +126,29 @@ function mktunnel() {
 
 # Setup the inbound tunnel to enable access to the launcher
 function mkintunnel() {
-    MASTER_HOST=$1
-    MASTER_PORT=$2
+    LAUNCHER_HOST=$1
+    LAUNCHER_PORT=$2
     SSH_HOST=$3
-    ssh -NL ${MASTER_PORT}:${MASTER_HOST}:${MASTER_PORT} ${SSH_HOST}
+    ssh -NL ${LAUNCHER_PORT}:${LAUNCHER_HOST}:${LAUNCHER_PORT} ${SSH_HOST}
 }
 
 # Attempt to retrieve the auth token from the remote host
 # This requires that your account have sudo access to root
 # and will likely be prompted for a password.
-# Args: {hostname} {cluster}
+# If launcherActualHost is specified, hostname will be used as ssh -oProxyJump={hostname}
+# Args: {hostname} {clusterName} [launcherActualHost]
 function pull_auth_token() {
     HOST=$1
     CLUSTER=$2
+    ACTUAL=$3
+    if [[ -n $ACTUAL ]]; then
+        PROXY_OPT="-oProxyJump=$HOST"
+        HOST=$ACTUAL
+    fi
 
     echo "Attempting to access /opt/launcher/jetty/base/etc/.launcher.token from $HOST"
     rm -f ~/.token.log
-    ssh -t $HOST 'sudo cat /opt/launcher/jetty/base/etc/.launcher.token' | tee ~/.token.log
+    ssh $PROXY_OPT -t $HOST 'sudo cat /opt/launcher/jetty/base/etc/.launcher.token' | tee ~/.token.log
     # Token is the last line of the output (no newline)
     TOKEN=$(tail -n 1 ~/.token.log)
     if [[ ${TOKEN} != *" "* ]]; then
@@ -194,7 +200,6 @@ DEV_LAUNCHER_PORT=$(lookup "DEV_LAUNCHER_PORT_$USERNAME")
 
 # Configuration for atlas
 OPT_name_atlas=atlas.us.cray.com
-OPT_LAUNCHERHOST_atlas=localhost
 OPT_LAUNCHERPROTOCOL_atlas=http
 OPT_CHECKPOINTPATH_atlas=/lus/scratch/foundation-engineering/determined-cp
 OPT_MASTERHOST_atlas=atlas
@@ -203,9 +208,19 @@ OPT_TRESSUPPORTED_atlas=false
 OPT_GRESSUPPORTED_atlas=false
 OPT_PROTOCOL_atlas=http
 
+# Configuration for aizn-admin (via hpcgate proxy node)
+OPT_name_aizn=hpcgate.us.rdlabs.hpecorp.net
+OPT_LAUNCHERACTUALHOST_aizn=aizn-admin
+OPT_LAUNCHERPROTOCOL_aizn=http
+OPT_LAUNCHERPORT_aizn=8181
+OPT_CHECKPOINTPATH_aizn=/home/launcher/.launcher/checkpoints
+OPT_MASTERHOST_aizn=hpcgate
+OPT_MASTERPORT_aizn=$USERPORT
+OPT_PROTOCOL_aizn=http
+OPT_DEFAULTIMAGE_aizn=
+
 # Configuration for horizon
 OPT_name_horizon=horizon.us.cray.com
-OPT_LAUNCHERHOST_horizon=localhost
 OPT_LAUNCHERPORT_horizon=8181
 OPT_LAUNCHERPROTOCOL_horizon=http
 OPT_CHECKPOINTPATH_horizon=/lus/scratch/foundation_engineering/determined-cp
@@ -216,7 +231,6 @@ OPT_PROTOCOL_horizon=http
 
 # Configuration for casablanca-mgmt1 (uses suffix casablanca_mgmt1)
 OPT_name_casablanca_mgmt1=casablanca-mgmt1.us.cray.com
-OPT_LAUNCHERHOST_casablanca_mgmt1=localhost
 OPT_LAUNCHERPORT_casablanca_mgmt1=8181
 OPT_LAUNCHERPROTOCOL_casablanca_mgmt1=http
 OPT_CHECKPOINTPATH_casablanca_mgmt1=/mnt/lustre/foundation_engineering/determined-cp
@@ -226,7 +240,6 @@ OPT_TRESSUPPORTED_casablanca_mgmt1=true
 
 # Configuration for casablanca-login (uses suffix casablanca_login)
 OPT_name_casablanca_login=casablanca-login.us.cray.com
-OPT_LAUNCHERHOST_casablanca_login=localhost
 OPT_LAUNCHERPORT_casablanca_login=8443
 OPT_LAUNCHERPROTOCOL_casablanca_login=https
 OPT_CHECKPOINTPATH_casablanca_login=/mnt/lustre/foundation_engineering/determined-cp
@@ -236,7 +249,6 @@ OPT_TRESSUPPORTED_casablanca_login=true
 
 # Configuration for casablanca-login2 (uses suffix casablanca_login2)
 OPT_name_casablanca_login2=casablanca-login2.us.cray.com
-OPT_LAUNCHERHOST_casablanca_login2=localhost
 OPT_LAUNCHERPORT_casablanca_login2=8443
 OPT_LAUNCHERPROTOCOL_casablanca_login2=http
 OPT_CHECKPOINTPATH_casablanca_login2=/mnt/lustre/foundation_engineering/determined-cp
@@ -246,7 +258,6 @@ OPT_TRESSUPPORTED_casablanca_login2=false
 
 # Configuration for sawmill (10.100.97.101)
 OPT_name_sawmill=10.100.97.101
-OPT_LAUNCHERHOST_sawmill=localhost
 OPT_LAUNCHERPROTOCOL_sawmill=http
 OPT_CHECKPOINTPATH_sawmill=/scratch2/launcher/determined-cp
 OPT_MASTERHOST_sawmill=nid000001
@@ -274,7 +285,6 @@ EOF
 
 # Configuration for shuco
 OPT_name_shuco=shuco.us.cray.com
-OPT_LAUNCHERHOST_shuco=localhost
 OPT_LAUNCHERPORT_shuco=8181
 OPT_LAUNCHERPROTOCOL_shuco=http
 OPT_CHECKPOINTPATH_shuco=/home/launcher/determined-cp
@@ -286,7 +296,6 @@ OPT_RENDEVOUSIFACE_shuco=bond0
 
 # Configuration for mosaic
 OPT_name_mosaic=10.30.91.220
-OPT_LAUNCHERHOST_mosaic=localhost
 OPT_LAUNCHERPORT_mosaic=8181
 OPT_LAUNCHERPROTOCOL_mosaic=http
 OPT_CHECKPOINTPATH_mosaic=/home/launcher/determinedai/checkpoints
@@ -299,7 +308,6 @@ OPT_REMOTEUSER_mosaic=root@
 
 # Configuration for osprey
 OPT_name_osprey=osprey.us.cray.com
-OPT_LAUNCHERHOST_osprey=localhost
 OPT_LAUNCHERPROTOCOL_osprey=http
 OPT_CHECKPOINTPATH_osprey=/lus/scratch/foundation_engineering/determined-cp
 OPT_DEBUGLEVEL_osprey=debug
@@ -310,7 +318,6 @@ OPT_PROTOCOL_osprey=http
 
 # Configuration for swan
 OPT_name_swan=swan.hpcrb.rdlabs.ext.hpe.com
-OPT_LAUNCHERHOST_swan=localhost
 OPT_LAUNCHERPROTOCOL_swan=http
 OPT_CHECKPOINTPATH_swan=/lus/scratch/foundation_engineering/determined-cp
 OPT_MASTERHOST_swan=swan
@@ -320,7 +327,6 @@ OPT_PROTOCOL_swan=http
 
 # Configuration for raptor
 OPT_name_raptor=raptor.hpcrb.rdlabs.ext.hpe.com
-OPT_LAUNCHERHOST_raptor=localhost
 OPT_LAUNCHERPROTOCOL_raptor=http
 OPT_CHECKPOINTPATH_raptor=/lus/scratch/foundation_engineering/determined-cp
 OPT_MASTERHOST_raptor=raptor
@@ -338,7 +344,6 @@ OPT_PROTOCOL_raptor=http
 #  1 XL645d Gen10+ server with 2x AMD EPYC 7702 processors (64c/2.0GHz/200W), 512GB RAM DDR4-2666 2R, 1x 1.6TB NVMe disk, 1x IB EDR port, 4x NVIDIA A100/40GB PCIe GPUs (*)
 #
 OPT_name_o184i023=16.16.184.23
-OPT_LAUNCHERHOST_o184i023=localhost
 OPT_LAUNCHERPORT_o184i023=8181
 OPT_LAUNCHERPROTOCOL_o184i023=http
 OPT_CHECKPOINTPATH_o184i023=/cstor/harrow/determined-cp
@@ -365,6 +370,7 @@ EOF
 # section in devcluster-slurm.yaml.   See OPT_TASKCONTAINERDEFAULTS_sawmill as
 # an example of how to provide such multi-line values.
 export OPT_LAUNCHERHOST=$(lookup "OPT_LAUNCHERHOST_$CLUSTER")
+export OPT_LAUNCHERACTUALHOST=$(lookup "OPT_LAUNCHERACTUALHOST_$CLUSTER")
 export OPT_LAUNCHERPORT=$(lookup "OPT_LAUNCHERPORT_$CLUSTER")
 export OPT_LAUNCHERPROTOCOL=$(lookup "OPT_LAUNCHERPROTOCOL_$CLUSTER")
 export OPT_CHECKPOINTPATH=$(lookup "OPT_CHECKPOINTPATH_$CLUSTER")
@@ -412,12 +418,23 @@ if [[ -z $OPT_LAUNCHERPORT ]]; then
     exit 1
 fi
 
-if [[ -z $INTUNNEL ]]; then
-    OPT_LAUNCHERHOST=$SLURMCLUSTER
+if [[ -n $PULL_AUTH ]]; then
+    pull_auth_token ${OPT_REMOTEUSER}$SLURMCLUSTER $CLUSTER $OPT_LAUNCHERACTUALHOST
 fi
 
-if [[ -n $PULL_AUTH ]]; then
-    pull_auth_token ${OPT_REMOTEUSER}$SLURMCLUSTER $CLUSTER
+if [[ -z $OPT_LAUNCHERACTUALHOST ]]; then
+    # If not specified, the actual host of the launcher is the cluster master node
+    # we are simulating.  This is the actual hostname of the launcher and is used
+    # for the eventual destination of the tunnel.
+    OPT_LAUNCHERACTUALHOST=$SLURMCLUSTER
+fi
+
+if [[ -z $INTUNNEL ]]; then
+    # No tunnel, reference final actual host
+    OPT_LAUNCHERHOST=$OPT_LAUNCHERACTUALHOST
+else
+    # We are setting up a tunnel, so the master.yaml needs to reference localhost
+    OPT_LAUNCHERHOST=localhost
 fi
 
 export OPT_DEBUGLEVEL=$DEBUGLEVEL
@@ -451,7 +468,7 @@ if [[ -n $INTUNNEL || -n $TUNNEL ]]; then
     ssh $OPT_MASTERHOST pkill -u '$USER' -x -f '"^sshd: $USER[ ]*$"'
 fi
 if [[ -n $INTUNNEL ]]; then
-    mkintunnel $OPT_MASTERHOST $OPT_LAUNCHERPORT ${OPT_REMOTEUSER}$SLURMCLUSTER &
+    mkintunnel $OPT_LAUNCHERACTUALHOST $OPT_LAUNCHERPORT ${OPT_REMOTEUSER}$SLURMCLUSTER &
 fi
 if [[ -n $TUNNEL ]]; then
     mktunnel $OPT_MASTERHOST $OPT_MASTERPORT ${OPT_REMOTEUSER}$SLURMCLUSTER &
