963be57917d780ed6c545b055ce8f7627b65e1c3
Nick Doiron <nick.doiron@hpe.com>
2023-01-30 11:48:28 -0500

    fix: Profiler useSettings bug, dependency warning in chart [WEB-847] (#5856)
    * fix: Profiler useSettings bug, dependency warning in chart
    
    ---------
    
    Co-authored-by: thiagodallacqua-hpe <104855841+thiagodallacqua-hpe@users.noreply.github.com>

--- webui/react/src/pages/TrialDetails/Profiles/Charts/SystemMetricChart.tsx
+++ webui/react/src/pages/TrialDetails/Profiles/Charts/SystemMetricChart.tsx
@@ -18,7 +18,7 @@ export interface Settings {
   name?: string;
 }
 
-const config: SettingsConfig<Settings> = {
+const config = (trialId: number): SettingsConfig<Settings> => ({
   settings: {
     agentId: {
       defaultValue: undefined,
@@ -36,11 +36,11 @@ const config: SettingsConfig<Settings> = {
       type: union([undefinedType, string]),
     },
   },
-  storagePath: 'profiler-filters',
-};
+  storagePath: `profiler-filters-${trialId}`,
+});
 
 const SystemMetricChart: React.FC<ChartProps> = ({ getOptionsForMetrics, trial }) => {
-  const { settings, updateSettings } = useSettings<Settings>(config);
+  const { settings, updateSettings } = useSettings<Settings>(config(trial.id));
 
   const systemSeries = useFetchProfilerSeries(trial.id)[MetricType.System];
 
