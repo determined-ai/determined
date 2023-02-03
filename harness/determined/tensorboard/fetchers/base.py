4a4bd8f8a610be3cac87364c6b7a76e73967b025
Taylor Ritenour <taylor.ritenour@hpe.com>
2023-02-03 13:41:44 -0800

    feat: Support concurrent fetching for TensorBoard metric files [MLG-295]

--- harness/determined/tensorboard/fetchers/base.py
+++ harness/determined/tensorboard/fetchers/base.py
@@ -1,28 +1,64 @@
 import abc
-from typing import Any, Callable, Dict, List
+import datetime
+from typing import Any, Callable, Dict, Generator, List
 
 
 class Fetcher(metaclass=abc.ABCMeta):
-    """Abstract base class for Tensorflow fetchers.
+    """Abstract base class for TensorBoard fetchers.
 
-    Responsible for syncing tensorflow files from a list of storage paths to a local directory.
+    Syncs TensorBoard files from remote file blob stores.
     """
 
     storage_paths: List[str]
+    _file_records: Dict[str, datetime.datetime] = {}
 
     @abc.abstractmethod
     def __init__(self, storage_config: Dict[str, Any], storage_paths: List[str], local_dir: str):
         pass
 
     @abc.abstractmethod
-    def fetch_new(self, new_file_callback: Callable = lambda: None) -> int:
-        """Fetches changed files found in storage paths to local disk.
+    def _list(self, storage_path: str) -> Generator[str, None, None]:
+        """Iterates over the remote directory storage_path and yields any file that is new or
+        has an updated timestamp from when it was last fetched.
+
+        Arguments:
+            storage_path (str): Path at a remote location to iterate over
+            new_file_callback (Callable, optional): Callback function that
+                is fired each time a new file is fetched
+        """
+        pass
+
+    @abc.abstractmethod
+    def _fetch(self, filepath: str, new_file_callback: Callable) -> None:
+        """Performs actual file fetch from the remote filepath to the internal local_dir
+
+        Arguments:
+            filepath (str): Filepath as a string of the file's remote location
+            new_file_callback (Callable, optional): Callback function that
+                is fired each time a new file is fetched
+        """
+        pass
+
+    def list_all_generator(self) -> Generator[str, None, None]:
+        """Iterates over all files that need to be fetched"""
+        for storage_path in self.storage_paths:
+            for filepath in self._list(storage_path):
+                yield filepath
+
+    def fetch_all_serial(self, new_file_callback: Callable = lambda: None) -> int:
+        """Fetches all changed files found in storage paths in serial
 
         Arguments:
             new_file_callback (Callable, optional): Callback function that
                 is fired each time a new file is fetched
 
         Returns: count of new files fetched.
-
         """
-        pass
+        new_files = []
+        # Look at all files in our storage location.
+        for filepath in self.list_all_generator():
+            new_files.append(filepath)
+        # Download the new or updated files.
+        for filepath in new_files:
+            self._fetch(filepath, new_file_callback)
+        return len(new_files)
