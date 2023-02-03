adedad5bed4d6abd1b617d5a7fe5468768c80ee0
Isha Ghodgaonkar <isha.ghodgaonkar@hpe.com>
2023-02-03 11:40:14 -0800

    updated adaptive searcher to use epochs, follow correct pattern for training, report progress to master, etc.

--- examples/tutorials/core_api_mnist/model_def_adaptive.py
+++ examples/tutorials/core_api_mnist/model_def_adaptive.py
@@ -6,9 +6,10 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-import torch 
+import torch
 import determined as det
 import pathlib
+
 
 class Net(nn.Module):
     # NEW: add hparams to __init__
@@ -38,7 +39,8 @@ class Net(nn.Module):
         return output
 
 
-def train(args, model, device, train_loader, optimizer, epoch, core_context):
+# NEW: Modified function header to include op for reporting training progress to master
+def train(args, model, device, train_loader, optimizer, core_context, epoch, op):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
@@ -48,74 +50,120 @@ def train(args, model, device, train_loader, optimizer, epoch, core_context):
         loss.backward()
         optimizer.step()
         if (batch_idx + 1) % args.log_interval == 0:
-            
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-            epoch, (batch_idx) * len(data), len(train_loader.dataset),
-            100. * (batch_idx) / len(train_loader), loss.item()))
-            
-            core_context.train.report_training_metrics(steps_completed=(batch_idx + 1) + (epoch-1) * len(train_loader), \
-            metrics={"train_loss":loss.item()})
-        
+
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                    epoch,
+                    (batch_idx) * len(data),
+                    len(train_loader.dataset),
+                    100.0 * (batch_idx) / len(train_loader),
+                    loss.item(),
+                )
+            )
+
+            # NEW: report progress once in a while
+            op.report_progress(epoch)
+
+            core_context.train.report_training_metrics(
+                steps_completed=(batch_idx + 1) + (epoch - 1) * len(train_loader),
+                metrics={"train_loss": loss.item()},
+            )
+
             if args.dry_run:
                 break
 
 
-def test(args, model, device, test_loader, epoch, core_context, len_trainset):
+# NEW: Modified function header to include op for reporting training progress to master
+def test(args, model, device, test_loader, core_context, steps_completed, op):
+
     model.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
         for batch_idx, (data, target) in enumerate(test_loader):
-            
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += F.nll_loss(output, target, reduction="sum").item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-            if (batch_idx + 1) % args.log_interval == 0:
-                core_context.train.report_validation_metrics(steps_completed= (epoch) * len_trainset, \
-                 metrics={"test_loss": (test_loss /len(test_loader.dataset))})
-              
     test_loss /= len(test_loader.dataset)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    print(
+        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
+        )
+    )
+
+    core_context.train.report_validation_metrics(
+        steps_completed=steps_completed,
+        metrics={"test_loss": test_loss},
+    )
+
+    return test_loss
 
 
 def load_state(checkpoint_directory):
     checkpoint_directory = pathlib.Path(checkpoint_directory)
-    with checkpoint_directory.joinpath('checkpoint.pt').open("rb") as f:
+    with checkpoint_directory.joinpath("checkpoint.pt").open("rb") as f:
         return torch.load(f)
 
 
 def main(core_context):
     # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                        help='number of epochs to train (default: 14)')
-    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
-                        help='learning rate (default: 1.0)')
-    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
-                        help='Learning rate step gamma (default: 0.7)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--no-mps', action='store_true', default=True,
-                        help='disables macOS GPU training')
-    parser.add_argument('--dry-run', action='store_true', default=False,
-                        help='quickly check a single pass')                    
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=True,
-                        help='For Saving the current Model')
-  
+    parser = argparse.ArgumentParser(description="PyTorch MNIST Example")
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size for training (default: 64)",
+    )
+    parser.add_argument(
+        "--test-batch-size",
+        type=int,
+        default=1000,
+        metavar="N",
+        help="input batch size for testing (default: 1000)",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=10,
+        metavar="N",
+        help="number of epochs to train (default: 14)",
+    )
+    parser.add_argument(
+        "--lr", type=float, default=1.0, metavar="LR", help="learning rate (default: 1.0)"
+    )
+    parser.add_argument(
+        "--gamma",
+        type=float,
+        default=0.7,
+        metavar="M",
+        help="Learning rate step gamma (default: 0.7)",
+    )
+    parser.add_argument(
+        "--no-cuda", action="store_true", default=False, help="disables CUDA training"
+    )
+    parser.add_argument(
+        "--no-mps", action="store_true", default=True, help="disables macOS GPU training"
+    )
+    parser.add_argument(
+        "--dry-run", action="store_true", default=False, help="quickly check a single pass"
+    )
+    parser.add_argument("--seed", type=int, default=1, metavar="S", help="random seed (default: 1)")
+    parser.add_argument(
+        "--log-interval",
+        type=int,
+        default=10,
+        metavar="N",
+        help="how many batches to wait before logging training status",
+    )
+    parser.add_argument(
+        "--save-model", action="store_true", default=True, help="For Saving the current Model"
+    )
+
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
     use_mps = not args.no_mps and torch.backends.mps.is_available()
@@ -123,12 +171,9 @@ def main(core_context):
     info = det.get_cluster_info()
     assert info is not None, "this example only runs on-cluster"
     latest_checkpoint = info.latest_checkpoint
-    if latest_checkpoint is not None: 
+    if latest_checkpoint is not None:
         with core_context.checkpoint.restore_path(latest_checkpoint) as path:
             model = load_state(path)
-
-    # NEW: get hparams from config
-    hparams = info.trial.hparams
 
     torch.manual_seed(args.seed)
 
@@ -139,50 +184,64 @@ def main(core_context):
     else:
         device = torch.device("cpu")
 
-    train_kwargs = {'batch_size': args.batch_size}
-    test_kwargs = {'batch_size': args.test_batch_size}
+    train_kwargs = {"batch_size": args.batch_size}
+    test_kwargs = {"batch_size": args.test_batch_size}
     if use_cuda:
-        cuda_kwargs = {'num_workers': 1,
-                       'pin_memory': True,
-                       'shuffle': True}
+        cuda_kwargs = {"num_workers": 1, "pin_memory": True, "shuffle": True}
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
 
-    transform=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-        ])
-    dataset1 = datasets.MNIST('../data', train=True, download=True,
-                       transform=transform)
-    dataset2 = datasets.MNIST('../data', train=False,
-                       transform=transform)
-    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+    )
+    dataset1 = datasets.MNIST("../data", train=True, download=True, transform=transform)
+    dataset2 = datasets.MNIST("../data", train=False, transform=transform)
+    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
-    # NEW: pass relevant hparams to model and optimizer
+    # NEW: Get hparams chosen for this trial from cluster info object
+    hparams = info.trial.hparams
+
+    # NEW: Pass relevant hparams to model and optimizer
     model = Net(hparams).to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=hparams["learning_rate"])
-
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    for epoch in range(1, args.epochs + 1):
-        len_trainset = len(train_loader)
-        train(args, model, device, train_loader, optimizer, epoch, core_context)
-        test(args, model, device, test_loader, epoch, core_context, len_trainset)
-        scheduler.step()
-        
-        if args.save_model:
-            
-            checkpoint_metadata_dict = {
-                'steps_completed': len(train_loader),
-            }
-            
-        with core_context.checkpoint.store_path(checkpoint_metadata_dict) as (path, storage_id):
-            torch.save(model.state_dict(), str(path) + ('/checkpoint.pt'))
 
-        if core_context.preempt.should_preempt():
-            return
+    # NEW: Iterate through the core_context.searcher.operations() to decide how long to train for.
+    starting_epoch = 0
+    epoch = starting_epoch
+    last_checkpoint_batch = None
 
-if __name__ == '__main__':
+    for op in core_context.searcher.operations():
+
+        # NEW: Use a while loop for easier accounting of absolute lengths.
+        while epoch < op.length:
+            steps_completed = epoch * len(train_loader)
+
+            # NEW: Pass op into train and test functions
+            train(args, model, device, train_loader, optimizer, core_context, epoch, op)
+            test_loss = test(args, model, device, test_loader, core_context, steps_completed, op)
+
+            scheduler.step()
+            if args.save_model:
+
+                checkpoint_metadata_dict = {
+                    "steps_completed": steps_completed,
+                }
+
+            epoch += 1
+
+            with core_context.checkpoint.store_path(checkpoint_metadata_dict) as (path, storage_id):
+                torch.save(model.state_dict(), str(path) + ("/checkpoint.pt"))
+
+            if core_context.preempt.should_preempt():
+                return
+
+        # NEW: After training for each op, validate and report the searcher metric to the master.
+        op.report_completed(test_loss)
+
+
+if __name__ == "__main__":
 
     with det.core.init() as core_context:
         main(core_context=core_context)
