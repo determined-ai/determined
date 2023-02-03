1d56fe62058e13a9fd741461e942ea520aca2b09
Thiago Dallacqua <thiago.menezes-dallacqua@hpe.com>
2023-02-03 14:56:12 -0300

    feat: add required field for workspace

--- webui/react/src/hooks/useModal/Model/useModalModelCreate.tsx
+++ webui/react/src/hooks/useModal/Model/useModalModelCreate.tsx
@@ -1,7 +1,9 @@
-import { Button, Dropdown, MenuProps, ModalFuncProps } from 'antd';
-import React, { useCallback, useEffect, useState } from 'react';
 import { DownOutlined } from '@ant-design/icons';
+import { Button, Dropdown, MenuProps, ModalFuncProps } from 'antd';
+import { SelectInfo } from 'rc-menu/lib/interface';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
+import DynamicIcon from 'components/DynamicIcon';
 import Form from 'components/kit/Form';
 import Input from 'components/kit/Input';
 import Link from 'components/Link';
@@ -13,16 +15,14 @@ import useModal, { ModalHooks as Hooks, ModalCloseReason } from 'shared/hooks/us
 import usePrevious from 'shared/hooks/usePrevious';
 import { clone, isEqual } from 'shared/utils/data';
 import { DetError, ErrorType } from 'shared/utils/error';
+import { useWorkspaces } from 'stores/workspaces';
 import { Metadata } from 'types';
 import { notification } from 'utils/dialogApi';
 import handleError from 'utils/error';
+import { Loadable } from 'utils/loadable';
 
 import css from './useModalModelCreate.module.scss';
-// import { useDeterminedInfo, useUpdateDeterminedInfo } from 'stores/determinedInfo'; 
-import { Loadable } from 'utils/loadable';
-import { useWorkspaces } from 'stores/workspaces';
-import DynamicIcon from 'components/DynamicIcon';
-import { FixedSizeList } from 'react-window';
+// import { useDeterminedInfo, useUpdateDeterminedInfo } from 'stores/determinedInfo';
 
 const FORM_ID = 'create-model-form';
 
@@ -33,6 +33,7 @@ interface Props {
 interface FormInputs {
   description?: string;
   modelName: string;
+  workspace: string;
 }
 
 interface OpenProps {
@@ -60,6 +61,7 @@ const DEFAULT_MODAL_STATE = {
   modelName: '',
   tags: [],
   visible: false,
+  workspace: '',
 };
 
 const useModalModelCreate = ({ onClose }: Props = {}): ModalHooks => {
@@ -73,10 +75,11 @@ const useModalModelCreate = ({ onClose }: Props = {}): ModalHooks => {
     Loaded: (ws) => ws,
     NotLoaded: () => [],
   });
+  const [selectedWorkspace, setSelectedWorkspace] = useState('');
   const [modalState, setModalState] = useState<ModalState>(DEFAULT_MODAL_STATE);
   const prevModalState = usePrevious(modalState, undefined);
   const [form] = Form.useForm<FormInputs>();
-  const modelName = Form.useWatch('modelName', form);
+  const modelName = Form.useWatch(['modelName', 'workspace'], form);
 
   const handleOnClose = useCallback(
     (reason?: ModalCloseReason) => {
@@ -173,16 +176,32 @@ const useModalModelCreate = ({ onClose }: Props = {}): ModalHooks => {
     setModalState((prev) => ({ ...prev, modelDescription: e.target.value }));
   }, []);
 
-  const workspaceItems: MenuProps['items'] = workspaces.map((ws) => ({
-    label: (
-      <div className={css.workspaceFilterItem}>
-        <DynamicIcon name={ws.name} size={24} />
-        <span className={css.workspaceFilterName}>{ws.name}</span>
-      </div>
-    ),
-    key: ws.id,
-    value: ws.id,
-  }));
+  const onSelect = useCallback(
+    (info: SelectInfo) => {
+      const ws = workspaces.find(({ id }) => String(id) === info.key);
+
+      if (ws) {
+        setModalState((prev) => ({ ...prev, workspace: info.key }));
+        setSelectedWorkspace(ws.name);
+      }
+    },
+    [workspaces],
+  );
+
+  const workspaceItems: MenuProps['items'] = useMemo(
+    () =>
+      workspaces.map((ws) => ({
+        key: ws.id,
+        label: (
+          <div className={css.workspaceFilterItem}>
+            <DynamicIcon name={ws.name} size={24} />
+            <span className={css.workspaceFilterName}>{ws.name}</span>
+          </div>
+        ),
+        value: ws.id,
+      })),
+    [workspaces],
+  );
 
   const getModalContent = useCallback(
     (state: ModalState): React.ReactNode => {
@@ -198,30 +217,40 @@ const useModalModelCreate = ({ onClose }: Props = {}): ModalHooks => {
             <Form.Item
               label="Model name"
               name="modelName"
+              required
               rules={[{ message: 'Model name is required ', required: true }]}>
               <Input onChange={handleNameChange} />
             </Form.Item>
-            <Form.Item rules={[{ required: true, message: 'Please select a workspace!' }]} name="workspaces">
+            <Form.Item
+              label="Workspaces"
+              name="workspace"
+              required
+              rules={[{ message: 'Please select a workspace!', required: true }]}>
               <Dropdown
                 arrow
-                // placement='bottomRight'
+                className={css.workspacDropdown}
                 disabled={!workspaces.length}
-                className={css.workspaceButton}
-                overlayClassName={css.dropDownRoot}
-                getPopupContainer={(triggerNode) => triggerNode}
                 dropdownRender={(menu) =>
-                  React.cloneElement(menu as React.ReactElement, { style: { maxWidth: 'fit-content', maxHeight: '200px', overflowY: 'scroll' } })
+                  React.cloneElement(menu as React.ReactElement, {
+                    style: { maxHeight: '200px', maxWidth: 'fit-content', overflowY: 'scroll' },
+                  })
                 }
+                getPopupContainer={(triggerNode) => triggerNode}
                 menu={{
                   items: workspaceItems,
+                  onSelect,
                   selectable: true,
                 }}
-                trigger={['click']}
-              >
+                trigger={['click']}>
                 <Button>
-                  <span className={css.workspaceButtonContainer}>
-                    Workspaces
-                    <DownOutlined />
+                  <span
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      minWidth: '175px',
+                    }}>
+                    {selectedWorkspace ? selectedWorkspace : 'Select a workspace'}
+                    <DownOutlined style={{ marginLeft: '10px' }} />
                   </span>
                 </Button>
               </Dropdown>
@@ -260,6 +289,10 @@ const useModalModelCreate = ({ onClose }: Props = {}): ModalHooks => {
     },
     [
       form,
+      selectedWorkspace,
+      workspaceItems,
+      workspaces,
+      onSelect,
       handleDescriptionChange,
       handleMetadataChange,
       handleNameChange,
@@ -276,13 +309,17 @@ const useModalModelCreate = ({ onClose }: Props = {}): ModalHooks => {
         content: getModalContent(state),
         icon: null,
         maskClosable: true,
-        okButtonProps: { disabled: !modelName, form: FORM_ID, htmlType: 'submit' },
+        okButtonProps: {
+          disabled: !modelName || !selectedWorkspace,
+          form: FORM_ID,
+          htmlType: 'submit',
+        },
         okText: 'Create Model',
         onOk: () => handleOk(state),
         title: 'Create Model',
       };
     },
-    [getModalContent, handleOk, modelName],
+    [getModalContent, handleOk, modelName, selectedWorkspace],
   );
 
   /**
