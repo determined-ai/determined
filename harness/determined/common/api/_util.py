04b481c2b9101e2e1e445256c96bc35e973742d3
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-02-08 14:51:04 -0500

    simplify what's brought over. stronger typing, and reduce the scope

--- harness/determined/common/api/_util.py
+++ harness/determined/common/api/_util.py
@@ -1,7 +1,8 @@
 import enum
-from typing import Callable, Iterator, TypeVar
+import time
+from typing import Callable, Iterator, Optional, Set, TypeVar, Union
 
-from determined.common.api import bindings
+from determined.common.api import Session, bindings
 
 
 class PageOpts(str, enum.Enum):
@@ -38,3 +39,55 @@ def read_paginated(
             break
         assert pagination.endIndex is not None
         offset = pagination.endIndex
+
+
+# Literal["notebook", "tensorboard", "shell", "command"]
+class NTSC_Kind(enum.Enum):
+    notebook = "notebook"
+    tensorboard = "tensorboard"
+    shell = "shell"
+    command = "command"
+
+
+all_ntsc: Set[NTSC_Kind] = {
+    NTSC_Kind.notebook,
+    NTSC_Kind.tensorboard,
+    NTSC_Kind.shell,
+    NTSC_Kind.command,
+}
+proxied_ntsc: Set[NTSC_Kind] = {NTSC_Kind.notebook, NTSC_Kind.tensorboard}
+
+AnyNTSC = Union[bindings.v1Notebook, bindings.v1Tensorboard, bindings.v1Shell, bindings.v1Command]
+
+
+def get_ntsc_details(session: Session, typ: NTSC_Kind, ntsc_id: str) -> AnyNTSC:
+    ntsc: Union[bindings.v1Notebook, bindings.v1Tensorboard, bindings.v1Shell, bindings.v1Command]
+    if typ == NTSC_Kind.notebook:
+        return bindings.get_GetNotebook(session, notebookId=ntsc_id).notebook
+    elif typ == NTSC_Kind.tensorboard:
+        return bindings.get_GetTensorboard(session, tensorboardId=ntsc_id).tensorboard
+    elif typ == NTSC_Kind.shell:
+        return bindings.get_GetShell(session, shellId=ntsc_id).shell
+    elif typ == NTSC_Kind.command:
+        return bindings.get_GetCommand(session, commandId=ntsc_id).command
+    else:
+        raise ValueError("unknown type")
+
+
+def wait_for_ntsc_state(
+    session: Session,
+    typ: NTSC_Kind,
+    ntsc_id: str,
+    predicate: Callable[[bindings.taskv1State], bool],
+    timeout: int = 10,  # seconds
+) -> Optional[bindings.taskv1State]:
+    """wait for ntsc to reach a state that satisfies the predicate"""
+    start = time.time()
+    last_state = None
+    while True:
+        if time.time() - start > timeout:
+            raise Exception(f"timed out waiting for state predicate to pass. reached {last_state}")
+        last_state = get_ntsc_details(session, typ, ntsc_id).state
+        if predicate(last_state):
+            return last_state
+        time.sleep(0.5)
