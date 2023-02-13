fb38690dff6f8faaf8beceeb03c8f9986269f7df
Dan Herrera <dan.herrera@hpe.com>
2023-02-13 15:00:28 -0500

    chore(cli): address B028 flake8+bugbear error

--- cli/determined_cli/__init__.py
+++ cli/determined_cli/__init__.py
@@ -4,5 +4,7 @@ from determined.cli import *  # noqa
 from .__version__ import __version__
 
 warnings.warn(
-    "determined_cli package is deprecated, please use determined.cli instead.", FutureWarning
+    message="determined_cli package is deprecated, please use determined.cli instead.",
+    category=FutureWarning,
+    stacklevel=2,
 )
