c8c257d719c0aa61847f4df14a76eb358ce6f67b
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-02-13 14:26:10 -0500

    check for existence of container_startup_script

--- agent/packaging/entrypoint.sh
+++ agent/packaging/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-chmod +x /usr/local/determined/container_startup_script
-/usr/local/determined/container_startup_script
-exit_code=$?
-if [ $exit_code -ne 0 ]; then
-    echo "container_startup_script failed with exit code $exit_code" >&2
-    exit 1
+container_startup_script="/usr/local/bin/container_startup.sh"
+
+if [ -f "$container_startup_script" ]; then
+    chmod +x $container_startup_script
+    $container_startup_script
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        echo "container_startup_script failed with exit code $exit_code" >&2
+        exit 1
+    fi
 fi
 
 exec /usr/bin/determined-agent "$@"
