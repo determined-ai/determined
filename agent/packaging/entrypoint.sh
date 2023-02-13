a45a691c93f797ad0183dde876a4a58211f37d37
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-02-13 10:35:57 -0500

    feat: fail on user startup_script fail

--- agent/packaging/entrypoint.sh
+++ agent/packaging/entrypoint.sh
@@ -2,5 +2,10 @@
 
 chmod +x /usr/local/determined/container_startup_script
 /usr/local/determined/container_startup_script
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+    echo "container_startup_script failed with exit code $exit_code" >&2
+    exit 1
+fi
 
 exec /usr/bin/determined-agent "$@"
