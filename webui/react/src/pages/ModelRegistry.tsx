0af001b49fc7983875237bc1431675f03e14b3f5
Nick Doiron <nick.doiron@hpe.com>
2023-02-21 15:43:36 -0500

    fix: Display model breadcrumbs even if no access to workspace [WEB-919] (#6032)
    * use NotLoaded/Loaded on workspaces
    * disabled link on inaccessible workspace

--- webui/react/src/pages/ModelRegistry.tsx
+++ webui/react/src/pages/ModelRegistry.tsx
@@ -338,6 +338,13 @@ const ModelRegistry: React.FC<Props> = ({ workspace }: Props) => {
   const workspaceRenderer = useCallback(
     (record: ModelItem): React.ReactNode => {
       const workspace = workspaces.find((u) => u.id === record.workspaceId);
+      if (!workspace) {
+        return (
+          <Link disabled>
+            <DynamicIcon name="-" size={24} />
+          </Link>
+        );
+      }
       const workspaceId = record.workspaceId;
       return (
         <Tooltip placement="top" title={workspace?.name}>
