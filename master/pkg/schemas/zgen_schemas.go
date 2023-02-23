f32b28a8d6e80f09c43feddf834203a4bac9c0d5
Anda <anda@determined.ai>
2023-02-23 14:32:00 -0800

    schema change

--- master/pkg/schemas/zgen_schemas.go
+++ master/pkg/schemas/zgen_schemas.go
@@ -171,29 +171,6 @@ var (
     }
 }
 `)
-	textCheckEpochNotUsedV0 = []byte(`{
-    "$schema": "http://json-schema.org/draft-07/schema#",
-    "$id": "http://determined.ai/schemas/expconf/v0/check-epoch-not-used.json",
-    "title": "CheckEpochNotUsed",
-    "additionalProperties": {
-        "$ref": "http://determined.ai/schemas/expconf/v0/check-epoch-not-used.json"
-    },
-    "items": {
-        "$ref": "http://determined.ai/schemas/expconf/v0/check-epoch-not-used.json"
-    },
-    "checks": {
-        "must specify the top-level records_per_epoch when this field is in terms of epochs": {
-            "properties": {
-                "epochs": {
-                    "not": {
-                        "type": "number"
-                    }
-                }
-            }
-        }
-    }
-}
-`)
 	textCheckGridHyperparameterV0 = []byte(`{
     "$schema": "http://json-schema.org/draft-07/schema#",
     "$id": "http://determined.ai/schemas/expconf/v0/check-grid-hyperparameter.json",
@@ -3062,8 +3039,6 @@ var (
 
 	schemaBindMountsConfigV0 interface{}
 
-	schemaCheckEpochNotUsedV0 interface{}
-
 	schemaCheckGridHyperparameterV0 interface{}
 
 	schemaCheckPositiveLengthV0 interface{}
@@ -3227,26 +3202,6 @@ func ParsedBindMountsConfigV0() interface{} {
 		panic("invalid embedded json for BindMountsConfigV0")
 	}
 	return schemaBindMountsConfigV0
-}
-
-func ParsedCheckEpochNotUsedV0() interface{} {
-	cacheLock.RLock()
-	if schemaCheckEpochNotUsedV0 != nil {
-		cacheLock.RUnlock()
-		return schemaCheckEpochNotUsedV0
-	}
-	cacheLock.RUnlock()
-
-	cacheLock.Lock()
-	defer cacheLock.Unlock()
-	if schemaCheckEpochNotUsedV0 != nil {
-		return schemaCheckEpochNotUsedV0
-	}
-	err := json.Unmarshal(textCheckEpochNotUsedV0, &schemaCheckEpochNotUsedV0)
-	if err != nil {
-		panic("invalid embedded json for CheckEpochNotUsedV0")
-	}
-	return schemaCheckEpochNotUsedV0
 }
 
 func ParsedCheckGridHyperparameterV0() interface{} {
@@ -4250,8 +4205,6 @@ func schemaBytesMap() map[string][]byte {
 	cachedSchemaBytesMap[url] = textBindMountV0
 	url = "http://determined.ai/schemas/expconf/v0/bind-mounts.json"
 	cachedSchemaBytesMap[url] = textBindMountsConfigV0
-	url = "http://determined.ai/schemas/expconf/v0/check-epoch-not-used.json"
-	cachedSchemaBytesMap[url] = textCheckEpochNotUsedV0
 	url = "http://determined.ai/schemas/expconf/v0/check-grid-hyperparameter.json"
 	cachedSchemaBytesMap[url] = textCheckGridHyperparameterV0
 	url = "http://determined.ai/schemas/expconf/v0/check-positive-length.json"
