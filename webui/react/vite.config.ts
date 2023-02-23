7e83a44aa3c318ce1b2765f33e3d61d9a5ea2996
Eric Liu <eric.liu@hpe.com>
2023-02-22 16:38:48 -0800

    update version

--- webui/react/vite.config.ts
+++ webui/react/vite.config.ts
@@ -99,7 +99,7 @@ export default defineConfig(({ mode }) => ({
     'process.env.IS_DEV': JSON.stringify(mode === 'development'),
     'process.env.PUBLIC_URL': JSON.stringify(publicUrl || ''),
     'process.env.SERVER_ADDRESS': JSON.stringify(process.env.SERVER_ADDRESS),
-    'process.env.VERSION': '"0.20.0-rc0"',
+    'process.env.VERSION': '"0.20.0-dev0"',
   },
   optimizeDeps: {
     include: ['notebook'],
