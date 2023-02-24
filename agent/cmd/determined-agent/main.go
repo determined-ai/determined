e87057e9345ccc7c33a5a5c2681bda417db4c871
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2023-02-24 14:42:24 -0500

    agent sigusr1

--- agent/cmd/determined-agent/main.go
+++ agent/cmd/determined-agent/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"os"
+	"os/signal"
+	"syscall"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -34,7 +36,22 @@ func main() {
 	rootCmd := newRootCmd()
 	maybeInjectRootAlias(rootCmd, "run")
 
-	if err := newRootCmd().Execute(); err != nil {
-		log.WithError(err).Fatal("fatal error running Determined agent")
+	sigusr1 := make(chan os.Signal)
+	signal.Notify(sigusr1, syscall.SIGUSR1)
+
+	exit := make(chan bool)
+	go func() {
+		if err := newRootCmd().Execute(); err != nil {
+			log.WithError(err).Fatal("fatal error running Determined agent")
+		}
+		exit <- true
+	}()
+
+	select {
+	case <-exit:
+		return
+	case <-sigusr1:
+		log.Info("Got a SIGUSR1 quiting gracefully")
+		os.Exit(198)
 	}
 }
