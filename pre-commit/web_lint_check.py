7cdd4a78ee4a601e291253a5a565643d453b328b
Ashton Galloway <ashton.galloway@hpe.com>
2023-02-27 09:52:51 -0500

    type file_paths argument

--- pre-commit/web_lint_check.py
+++ pre-commit/web_lint_check.py
@@ -4,17 +4,19 @@ import argparse
 import multiprocessing
 import os
 import subprocess
+from typing import List
 
 
 def web_lint_check():
     parser = argparse.ArgumentParser(description="Lint Check for Web")
     parser.add_argument("target", help="Either js, css, or misc", choices=["js", "css", "misc"])
-    parser.add_argument("file_paths", help="1 or more file paths", nargs="+")
+    parser.add_argument("file_paths", help="1 or more file paths", nargs="+", type=List[str])
     args = parser.parse_args()
     DIR = "webui/react/"
 
     target: str = args.target
-    file_paths: str = " ".join([os.path.relpath(file_path, DIR) for file_path in args.file_paths])
+    file_paths: List[str] = args.file_paths
+    rel_file_paths: str = " ".join([os.path.relpath(file_path, DIR) for file_path in file_paths])
     nproc: int = multiprocessing.cpu_count()
     run_command: list[str] = [
         "make",
@@ -22,19 +24,19 @@ def web_lint_check():
         "-C",
         DIR,
         "prettier",
-        f"PRE_ARGS=-- --write -c {file_paths}",
+        f"PRE_ARGS=-- --write -c {rel_file_paths}",
     ]
 
     # TODO: replace it with `match` if we support python v3.10
     if target == "js":
-        run_command += ["eslint", f"ES_ARGS=-- --fix {file_paths}"]
+        run_command += ["eslint", f"ES_ARGS=-- --fix {rel_file_paths}"]
     elif target == "css":
-        run_command += ["stylelint", f"ST_ARGS=-- --fix {file_paths}"]
+        run_command += ["stylelint", f"ST_ARGS=-- --fix {rel_file_paths}"]
     elif target == "misc":
         run_command += ["check-package-lock"]
 
     returncode: int = subprocess.call(run_command)
-    subprocess.call(["git", "add"] + args.file_paths)
+    subprocess.call(["git", "add"] + file_paths)
     exit(returncode)
 
 
