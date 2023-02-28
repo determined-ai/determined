5d205d559fad594be203d83116b4ef2695e9d2d9
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2023-02-28 10:47:58 -0500

    add unit test

---
+++ master/pkg/syncx/errgroupx/errgroupx_test.go
@@ -0,0 +1,52 @@
+package errgroupx
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestErrgroupxCancelByReturnError(t *testing.T) {
+	ctx := context.Background()
+	g := WithContext(ctx)
+
+	finished := make(chan bool, 1)
+	g.Go(func(ctx context.Context) error {
+		return fmt.Errorf("Non nil error")
+	})
+	g.Go(func(ctx context.Context) error {
+		<-ctx.Done()
+		finished <- true
+		return nil
+	})
+
+	require.Error(t, g.Wait())
+	require.True(t, <-finished)
+	require.NoError(t, ctx.Err()) // Original context not canceled.
+}
+
+func TestErrgroupxCancelingParentCancels(t *testing.T) {
+	for _, cancelGroup := range []bool{true, false} {
+		ctx, cancel := context.WithCancel(context.Background())
+		g := WithContext(ctx)
+
+		finished := make(chan bool, 1)
+		g.Go(func(ctx context.Context) error {
+			<-ctx.Done()
+			finished <- true
+			return nil
+		})
+
+		if cancelGroup {
+			g.cancel()
+			require.NoError(t, ctx.Err())
+		} else {
+			cancel()
+			require.Error(t, ctx.Err())
+		}
+		require.True(t, <-finished)
+		require.NoError(t, g.Wait())
+	}
+}
