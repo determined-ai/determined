a20bbbfb825f5e49062f26bf0800ff16ee6cc568
julian-determined-ai <julian.glover@hpe.com>
2023-03-01 15:16:21 -0600

    remove maxtag count from metricselect logic

--- webui/react/src/components/MetricSelect.tsx
+++ webui/react/src/components/MetricSelect.tsx
@@ -143,19 +143,19 @@ const MetricSelect: React.FC<Props> = ({
     );
   }, [totalNumMetrics, visibleMetrics]);
 
-  const [maxTagCount, selectorPlaceholder] = useMemo((): [0, string] => {
+  const [selectorPlaceholder] = useMemo((): string => {
     // This should never happen, but fall back to inoffensive empty placeholder
     if (metricValues === undefined) {
-      return [0, ''];
+      return '';
     }
     if (metricValues.length === 0) {
       // If we set maxTagCount=0 in this case, this placeholder will not be displayed.
-      return [0, 'None selected'];
+      return 'None selected';
     } else if (metricValues.length === totalNumMetrics) {
       // If we set maxTagCount=-1 in these cases, it will display tags instead of the placeholder.
-      return [0, `All ${totalNumMetrics} selected`];
+      return `All ${totalNumMetrics} selected`;
     } else {
-      return [0, `${metricValues.length} of ${totalNumMetrics} selected`];
+      return `${metricValues.length} of ${totalNumMetrics} selected`;
     }
   }, [metricValues, totalNumMetrics]);
 
@@ -164,7 +164,7 @@ const MetricSelect: React.FC<Props> = ({
       disableTags
       filterOption={handleFiltering}
       label={label}
-      maxTagCount={maxTagCount}
+      maxTagCount={0}
       maxTagPlaceholder={selectorPlaceholder}
       mode={multiple ? 'multiple' : undefined}
       ref={selectRef}
