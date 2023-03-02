8da27256c4dc1e3e329099ddd0a308d948cac49b
Max Russell <max.russell@hpe.com>
2023-03-01 19:16:55 -0800

    fix: skip linter warning for deprecation warning

--- cli/determined_cli/__init__.py
+++ cli/determined_cli/__init__.py
@@ -3,6 +3,6 @@ import warnings
 from determined.cli import *  # noqa
 from .__version__ import __version__
 
-warnings.warn(
+warnings.warn(  # noqa: B028
     "determined_cli package is deprecated, please use determined.cli instead.", FutureWarning
 )
