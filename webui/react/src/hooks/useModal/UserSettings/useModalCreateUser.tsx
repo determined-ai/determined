50387d170d613a06760f0cb3efd0bc6dd12dbe5e
Thiago Dallacqua <thiago.menezes-dallacqua@hpe.com>
2023-03-02 14:58:53 -0300

    chore: ran fmt

--- webui/react/src/hooks/useModal/UserSettings/useModalCreateUser.tsx
+++ webui/react/src/hooks/useModal/UserSettings/useModalCreateUser.tsx
@@ -107,16 +107,16 @@ const ModalForm: React.FC<Props> = ({ form, user, viewOnly, roles }) => {
               showSearch>
               {Loadable.isLoaded(knownRoles)
                 ? knownRoles.data.map((r: UserRole) => (
-                  <Select.Option
-                    disabled={
-                      roles?.find((ro) => ro.id === r.id)?.fromGroup?.length ||
-                      roles?.find((ro) => ro.id === r.id)?.fromWorkspace?.length
-                    }
-                    key={r.id}
-                    value={r.id}>
-                    {r.name}
-                  </Select.Option>
-                ))
+                    <Select.Option
+                      disabled={
+                        roles?.find((ro) => ro.id === r.id)?.fromGroup?.length ||
+                        roles?.find((ro) => ro.id === r.id)?.fromWorkspace?.length
+                      }
+                      key={r.id}
+                      value={r.id}>
+                      {r.name}
+                    </Select.Option>
+                  ))
                 : undefined}
             </Select>
           </Form.Item>
