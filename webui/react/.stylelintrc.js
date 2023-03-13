0786c9bfc581ac77567cd8a6259c95ada199302b
Keita Nonaka <keita.nonaka@hpe.com>
2023-03-13 14:45:06 -0700

    chore: update stylelint

--- webui/react/.stylelintrc.js
+++ webui/react/.stylelintrc.js
@@ -1,2 +1,42 @@
-const config = require('./src/shared/configs/.stylelintrc');
-module.exports = config;
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-standard-scss'],
+  plugins: ['stylelint-order', 'stylelint-scss'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'at-rule-semicolon-space-before': 'never',
+    'custom-property-empty-line-before': 'never',
+    'declaration-block-semicolon-newline-after': 'always-multi-line',
+    'declaration-block-semicolon-newline-before': 'never-multi-line',
+    'declaration-block-semicolon-space-before': 'never',
+    'declaration-block-trailing-semicolon': null,
+    'declaration-empty-line-before': 'never',
+    'declaration-property-value-no-unknown': true,
+    'function-name-case': 'lower',
+    'keyframes-name-pattern': null,
+    'no-eol-whitespace': [true, { ignore: ['empty-lines'] }],
+    'no-extra-semicolons': true,
+    'order/order': [
+      'custom-properties',
+      'dollar-variables',
+      'at-variables',
+      'declarations',
+      'rules',
+      'at-rules',
+      'less-mixins',
+    ],
+    'order/properties-alphabetical-order': true,
+    'property-no-vendor-prefix': null,
+    'rule-empty-line-before': [
+      'always',
+      {
+        except: ['after-rule', 'first-nested', 'inside-block-and-after-rule'],
+      },
+    ],
+    'scss/at-rule-no-unknown': true,
+    'selector-class-pattern': null,
+    'selector-not-notation': 'simple',
+    'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global'] }],
+    'string-quotes': 'single',
+    'value-keyword-case': null,
+  },
+};
