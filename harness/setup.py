a45369d33e5b2c66cc0864ffc4105048d7b965f2
gt2345 <gt2345@columbia.edu>
2023-03-13 17:34:35 -0500

    chore: bump version: 0.20.1-rc3 -> 0.20.1-rc4

--- harness/setup.py
+++ harness/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="determined",
-    version="0.20.1-rc3",
+    version="0.20.1-rc4",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
--- webui/react/vite.config.ts
+++ webui/react/vite.config.ts
@@ -99,7 +99,7 @@ export default defineConfig(({ mode }) => ({
     'process.env.IS_DEV': JSON.stringify(mode === 'development'),
     'process.env.PUBLIC_URL': JSON.stringify(publicUrl || ''),
     'process.env.SERVER_ADDRESS': JSON.stringify(process.env.SERVER_ADDRESS),
-    'process.env.VERSION': '"0.20.1-rc3"',
+    'process.env.VERSION': '"0.20.1-rc4"',
   },
   optimizeDeps: {
     include: ['notebook'],
