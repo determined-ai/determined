7fcb42c8918cda7f6c476b6323c6d637261a4679
gt2345 <gt2345@columbia.edu>
2023-03-15 16:23:50 -0500

    draft

--- master/static/migrations/20230315150937_add-hyperparameters-metrics-name-view.tx.up.sql
+++ master/static/migrations/20230315150937_add-hyperparameters-metrics-name-view.tx.up.sql
@@ -1,2 +1,9 @@
 CREATE MATERIALIZED VIEW hyperparameters_view AS
-    SELECT workspace_id,  project_id, experiments.id AS experiment_id, hyperparameters FROM
+    SELECT workspaces.id AS workspace_id,  experiments.project_id AS project_id, experiments.id AS experiment_id, json_build_array((experiments.config->'hyperparameters')) AS hyperparameters FROM workspaces, experiments, projects where workspaces.id = projects.workspace_id AND experiments.project_id = projects.id;
+
+CREATE INDEX ix_hyperparameters_workspace_id ON hyperparameters_view USING btree (workspace_id)
+CREATE INDEX ix_hyperparameters_project_id ON hyperparameters_view USING btree (project_id)
+
+
+CREATE MATERIALIZED VIEW metrics_name_view AS  
+    SELECT workspaces.id AS workspace_id,  experiments.project_id AS project_id, experiments.id AS experiment_id, 
