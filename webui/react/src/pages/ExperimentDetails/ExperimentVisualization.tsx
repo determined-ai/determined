7dd473ed52e8ccf03bcaef6ba27d64e7ddcd1ae9
Nick Doiron <nick.doiron@hpe.com>
2023-03-23 14:36:11 -0500

    spinner visible

--- webui/react/src/pages/ExperimentDetails/ExperimentVisualization.tsx
+++ webui/react/src/pages/ExperimentDetails/ExperimentVisualization.tsx
@@ -431,7 +431,7 @@ const ExperimentVisualization: React.FC<Props> = ({ basePath, experiment }: Prop
   } else if (pageError !== undefined) {
     return <Message title={PAGE_ERROR_MESSAGES[pageError]} type={MessageType.Alert} />;
   } else if (!hasLoaded && experiment.state !== RunState.Paused) {
-    return <Spinner tip="Fetching metrics..." />;
+    return <Spinner spinning tip="Fetching metrics..." />;
   } else if (!hasData) {
     return isExperimentTerminal || experiment.state === RunState.Paused ? (
       <Message title="No data to plot." type={MessageType.Empty} />
