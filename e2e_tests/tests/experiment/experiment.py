e649aaff396d36624dc20430b5de68d9fc24227a
Nick Doiron <nick.doiron@hpe.com>
2023-03-28 10:42:18 -0500

    pause experiments using filters

--- e2e_tests/tests/experiment/experiment.py
+++ e2e_tests/tests/experiment/experiment.py
@@ -58,8 +58,12 @@ def pause_experiment(experiment_id: int) -> None:
     subprocess.check_call(command)
 
 
-def pause_experiments(experiment_ids: List[int]) -> None:
-    body = bindings.v1PauseExperimentsRequest(experimentIds=experiment_ids)
+def pause_experiments(experiment_ids: List[int] = [], name: Optional[str] = None) -> None:
+    if name is None:
+        body = bindings.v1PauseExperimentsRequest(experimentIds=experiment_ids)
+    else:
+        filters = bindings.v1BulkExperimentFilters(name=name)
+        body = bindings.v1PauseExperimentsRequest(filters=filters)
     bindings.post_PauseExperiments(api_utils.determined_test_session(), body=body)
 
 
@@ -68,8 +72,12 @@ def activate_experiment(experiment_id: int) -> None:
     subprocess.check_call(command)
 
 
-def activate_experiments(experiment_ids: List[int]) -> None:
-    body = bindings.v1ActivateExperimentsRequest(experimentIds=experiment_ids)
+def activate_experiments(experiment_ids: List[int] = [], name: Optional[str] = None) -> None:
+    if name is None:
+        body = bindings.v1ActivateExperimentsRequest(experimentIds=experiment_ids)
+    else:
+        filters = bindings.v1BulkExperimentFilters(name=name)
+        body = bindings.v1ActivateExperimentsRequest(filters=filters)
     bindings.post_ActivateExperiments(api_utils.determined_test_session(), body=body)
 
 
@@ -78,8 +86,12 @@ def cancel_experiment(experiment_id: int) -> None:
     wait_for_experiment_state(experiment_id, experimentv1State.STATE_CANCELED)
 
 
-def cancel_experiments(experiment_ids: List[int]) -> None:
-    body = bindings.v1CancelExperimentsRequest(experimentIds=experiment_ids)
+def cancel_experiments(experiment_ids: List[int] = [], name: Optional[str] = None) -> None:
+    if name is None:
+        body = bindings.v1CancelExperimentsRequest(experimentIds=experiment_ids)
+    else:
+        filters = bindings.v1BulkExperimentFilters(name=name)
+        body = bindings.v1CancelExperimentsRequest(filters=filters)
     bindings.post_CancelExperiments(api_utils.determined_test_session(), body=body)
 
 
