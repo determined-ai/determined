6d03024d178642d0cafc3cfa0fc7f7af0f15533d
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-03-28 13:28:03 -0700

    chore: bumpenvs and bump master amis

--- harness/determined/deploy/gcp/constants.py
+++ harness/determined/deploy/gcp/constants.py
@@ -3,7 +3,7 @@ class defaults:
     AUX_AGENT_INSTANCE_TYPE = "n1-standard-4"
     COMPUTE_AGENT_INSTANCE_TYPE = "n1-standard-32"
     DB_PASSWORD = "postgres"
-    ENVIRONMENT_IMAGE = "det-environments-835d8b1"
+    ENVIRONMENT_IMAGE = "det-environments-9d07809"
     GPU_NUM = 4
     GPU_TYPE = "nvidia-tesla-t4"
     MASTER_INSTANCE_TYPE = "n1-standard-2"
