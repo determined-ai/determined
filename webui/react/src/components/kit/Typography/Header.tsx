ddd3e0a6d34a043bb352ca5932c24ff18ede66e2
Thiago Dallacqua <thiago.menezes-dallacqua@hpe.com>
2023-03-29 16:49:22 -0300

    feat: remove the classes prop from the components

--- webui/react/src/components/kit/Typography/Header.tsx
+++ webui/react/src/components/kit/Typography/Header.tsx
@@ -2,14 +2,8 @@ import React from 'react';
 
 import css from './index.module.scss';
 
-interface Props {
-  classes?: string;
-}
-
-const Header: React.FC<React.PropsWithChildren<Props>> = ({ classes = '', children }) => {
-  const style = [css.header, classes];
-
-  return <h1 className={style.join(' ')}>{children}</h1>;
+const Header: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return <h1 className={css.header}>{children}</h1>;
 };
 
 export default Header;
--- webui/react/src/components/kit/Typography/Paragraph.tsx
+++ webui/react/src/components/kit/Typography/Paragraph.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 
-import css from './index.module.scss';
-
-interface Props {
-  classes?: string;
-}
-
-const Paragraph: React.FC<React.PropsWithChildren<Props>> = ({ classes = '', children }) => {
-  const style = [css.base, classes];
-  return <p className={style.join(' ')}>{children}</p>;
+const Paragraph: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return <p>{children}</p>;
 };
 
 export default Paragraph;
