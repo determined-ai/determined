673844952f2e82f8c080851fddb9c2de8a0a3b54
Wes Turner <wesley.turner@hpe.com>
2023-03-29 15:32:36 -0700

    build: Updates Python used in CircleCI to 3.8.16
    [MLG-359]
    
    This PR consists of two changes:
    
    1. A couple patches in `model_hub` that set typing such that mypy==0.910
       passes under Python 3.8. This likely means `make check` will fail
       on these files under Python 3.7.
    
    2. A change to the docker image used in CircleCI making CI checks run
       using Python 3.8 instead of 3.7.
    
    Release will progress in the following steps:
    
    1. A new cimg-base will be built from
       https://github.com/determined-ai/determined-ci-images/pull/9. It will
       be pushed to dockerhub with the tag 'py38'.
    
    2. This build will run its CI tests using `determinedai/cimg-base:py38'
    
    3. Assuming pre-commit tests are green, this PR will be merged.
    
    4. If post-commit tests are green, a `cimg-base` with the tag `latest`
       will be built from its PR #9
    
    5. We'll create a new PR in `deterimend-ai/determined` that moves the
       default docker-image in CircleCI's config.yml back to `latest`.

--- model_hub/model_hub/mmdetection/_data.py
+++ model_hub/model_hub/mmdetection/_data.py
@@ -5,7 +5,7 @@ import functools
 import logging
 import math
 import os
-from typing import Any, Iterator, Tuple
+from typing import Any, Iterator, List, Tuple
 
 import filelock
 import mmcv
@@ -59,31 +59,33 @@ class GroupSampler(torch.utils.data.Sampler):
         self.total_size = self.num_samples * self.num_replicas
 
     def __iter__(self) -> Iterator[Any]:
-        indices = []
+        shuffled_indices: List[int] = []
         for i, size in enumerate(self.group_sizes):
             if size > 0:
-                indice = np.where(self.flag == i)[0]
-                assert len(indice) == size
-                indice = indice[list(torch.randperm(int(size)))].tolist()
+                group_indices = np.where(self.flag == i)[0]
+                assert len(group_indices) == size
+                shuffled_group_indices: List[int] = group_indices[
+                    list(torch.randperm(int(size)))
+                ].tolist()
                 extra = int(
                     math.ceil(size * 1.0 / self.samples_per_gpu / self.num_replicas)
-                ) * self.samples_per_gpu * self.num_replicas - len(indice)
-                # pad indice
-                tmp = indice.copy()
+                ) * self.samples_per_gpu * self.num_replicas - len(shuffled_group_indices)
+                # pad index
+                tmp = shuffled_group_indices.copy()
                 for _ in range(extra // size):
-                    indice.extend(tmp)
-                indice.extend(tmp[: extra % size])
-                indices.extend(indice)
+                    shuffled_group_indices.extend(tmp)
+                shuffled_group_indices.extend(tmp[: extra % size])
+                shuffled_indices.extend(shuffled_group_indices)
 
-        assert len(indices) == self.total_size
+        assert len(shuffled_indices) == self.total_size
 
-        indices = [
-            indices[j]
-            for i in list(torch.randperm(len(indices) // self.samples_per_gpu))
+        shuffled_indices = [
+            shuffled_indices[j]
+            for i in list(torch.randperm(len(shuffled_indices) // self.samples_per_gpu))
             for j in range(i * self.samples_per_gpu, (i + 1) * self.samples_per_gpu)
         ]
 
-        return iter(indices)
+        return iter(shuffled_indices)
 
     def __len__(self) -> int:
         return self.total_size
