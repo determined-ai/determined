d322a90c96c0e0ca0ae2434bfcff4ceef50edd93
Bradley Laney <bradley.laney@hpe.com>
2023-03-29 12:53:15 -0400

    chore: add better sync/map pkg for Go (#6299)

---
+++ master/pkg/syncx/mapx/mapx.go
@@ -0,0 +1,55 @@
+package mapx
+
+import "sync"
+
+// New creates an empty map.
+func New[K comparable, V any]() Map[K, V] {
+	return Map[K, V]{inner: make(map[K]V)}
+}
+
+// Map is a generic, thread-safe map to supersede usages of sync.Map.
+type Map[K comparable, V any] struct {
+	mu    sync.RWMutex
+	inner map[K]V
+}
+
+// Load the value corresponding to k.
+func (m *Map[K, V]) Load(k K) (V, bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	v, ok := m.inner[k]
+	return v, ok
+}
+
+// Delete the value corresponding to k, idempotently.
+func (m *Map[K, V]) Delete(k K) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	delete(m.inner, k)
+}
+
+// Store the (k,v) pair.
+func (m *Map[K, V]) Store(k K, v V) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.inner[k] = v
+}
+
+// Len of the map (number of stored pairs).
+func (m *Map[K, V]) Len() int {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	return len(m.inner)
+}
+
+// WithLock runs the given function on the underlying map with a write lock.
+func (m *Map[K, V]) WithLock(f func(m map[K]V)) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	f(m.inner)
+}
---
+++ master/pkg/syncx/mapx/mapx_test.go
@@ -0,0 +1,33 @@
+package mapx
+
+import (
+	"testing"
+
+	"golang.org/x/exp/maps"
+)
+
+// FuzzMap tests for bad concurrent access. Run the command below to test it, if you change the map.
+//
+//	go test -fuzz FuzzMap --parallel 64 github.com/determined-ai/determined/master/pkg/syncx/mapx
+func FuzzMap(f *testing.F) {
+	m := New[uint8, string]()
+
+	f.Add(uint8(0), uint8(0), "hello")
+	f.Fuzz(func(t *testing.T, op, k uint8, v string) {
+		switch op % 5 {
+		case 0:
+			m.Len()
+		case 1:
+			m.Store(k, v)
+		case 2:
+			_, _ = m.Load(k)
+		case 3:
+			m.Delete(k)
+		case 4:
+			var _ []uint8
+			m.WithLock(func(m map[uint8]string) {
+				_ = maps.Keys(m)
+			})
+		}
+	})
+}
