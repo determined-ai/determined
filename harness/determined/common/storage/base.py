3a09a229d19e6e413855f93d9dbf3ea17ec50a0c
Daniel R Hunter <daniel.robert.hunter@hpe.com>
2023-03-31 15:11:19 -0400

    More code cleanup

--- harness/determined/common/storage/base.py
+++ harness/determined/common/storage/base.py
@@ -3,7 +3,7 @@ import contextlib
 import os
 import pathlib
 import urllib
-from typing import Any, Callable, Dict, Iterator, Optional, Set, Union
+from typing import Any, Callable, Dict, Iterator, List, Optional, Set, Union
 
 from determined.common import storage
 
@@ -165,6 +165,9 @@ class StorageManager(metaclass=abc.ABCMeta):
 
 
 def from_string(shortcut: str) -> StorageManager:
+    def process_query(q: Dict[str, List[str]]) -> Dict[str, str]:
+        return {k: v[0] for k, v in q.items()}
+
     p: urllib.parse.ParseResult = urllib.parse.urlparse(shortcut)
     if p.scheme == "":
         base_path = p.path
@@ -172,38 +175,25 @@ def from_string(shortcut: str) -> StorageManager:
     elif p.scheme == "ms":
         container = p.netloc
         connection_string = p.fragment
-        kwargs = urllib.parse.parse_qs(p.query)
-        account_url = kwargs.get("account_url", [None])[0]
-        credential = kwargs.get("credential", [None])[0]
-        temp_dir = kwargs.get("temp_dir", [None])[0]
+        kwargs = process_query(urllib.parse.parse_qs(p.query))
         return storage.AzureStorageManager(
             container=container,
             connection_string=connection_string,
-            account_url=account_url,
-            credential=credential,
-            temp_dir=temp_dir,
+            **kwargs,
         )
     elif p.scheme == "gs":
         bucket = p.netloc
         prefix = p.path.lstrip("/")
-        kwargs = urllib.parse.parse_qs(p.query)
-        temp_dir = kwargs.get("temp_dir", [None])[0]
-        return storage.GCSStorageManager(bucket=bucket, prefix=prefix, temp_dir=temp_dir)
+        kwargs = process_query(urllib.parse.parse_qs(p.query))
+        return storage.GCSStorageManager(bucket=bucket, prefix=prefix, **kwargs)
     elif p.scheme == "s3":
         bucket = p.netloc
         prefix = p.path.lstrip("/")
-        kwargs = urllib.parse.parse_qs(p.query)
-        access_key = kwargs.get("access_key", [None])[0]
-        secret_key = kwargs.get("secret_key", [None])[0]
-        endpoint_url = kwargs.get("endpoint_url", [None])[0]
-        temp_dir = kwargs.get("temp_dir", [None])[0]
+        kwargs = process_query(urllib.parse.parse_qs(p.query))
         return storage.S3StorageManager(
             bucket=bucket,
             prefix=prefix,
-            access_key=access_key,
-            secret_key=secret_key,
-            endpoint_url=endpoint_url,
-            temp_dir=temp_dir,
+            **kwargs,
         )
     else:
         raise ValueError(
