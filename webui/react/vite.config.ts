8c4bba2645fee261352aa19a415333cd7539a6e2
Ashton Galloway <ashton.galloway@hpe.com>
2023-04-04 16:15:20 -0400

    redirect from /design to /design/

--- webui/react/vite.config.ts
+++ webui/react/vite.config.ts
@@ -32,6 +32,24 @@ const portableFetchFix = (): Plugin => ({
   },
 });
 
+const devServerRedirects = (redirects: Record<string, string>): Plugin => ({
+  name: 'dev-server-redirects',
+  configureServer(server) {
+    Object.entries(redirects).forEach(([from, to]) => {
+      server.middlewares.use(from, (req, res, next) => {
+        if (req.originalUrl === from) {
+          res.writeHead(302, {
+            Location: to
+          })
+          res.end()
+        } else {
+          next()
+        }
+      })
+    })
+  }
+})
+
 const publicUrlBaseHref = (): Plugin => {
   let config: UserConfig;
   return {
@@ -113,6 +131,9 @@ export default defineConfig(({ mode }) => ({
       checker({
         typescript: true,
       }),
+    devServerRedirects({
+      '/design': '/design/',
+    }),
     cspHtml({
       cspRules: {
         'frame-src': ["'self'", 'netlify.determined.ai'],
