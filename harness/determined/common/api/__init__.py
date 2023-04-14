da5ea75d8c7f3c66a509ffd2fda4a331bd056351
Carolina Calderon <carolina.calderon@hpe.com>
2023-04-14 17:54:11 -0400

    chore: add --json to det e|task|trial logs [DET-8982] (#6544)

--- harness/determined/common/api/__init__.py
+++ harness/determined/common/api/__init__.py
@@ -4,8 +4,8 @@ from determined.common.api import bindings
 from determined.common.api._util import PageOpts, read_paginated, WARNING_MESSAGE_MAP
 from determined.common.api.authentication import Authentication, salt_and_hash
 from determined.common.api.logs import (
-    pprint_trial_logs,
-    pprint_task_logs,
+    pprint_logs,
+    print_json_logs,
     trial_logs,
     task_logs,
 )
