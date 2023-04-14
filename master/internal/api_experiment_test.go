074d54706789f1653095e24d62de3eff82b76c09
julian-determined-ai <julian.glover@hpe.com>
2023-04-14 10:34:38 -0500

    update test cases

--- master/internal/api_experiment_test.go
+++ master/internal/api_experiment_test.go
@@ -37,12 +37,12 @@ func TestExperimentSearchApiFilterParsing(t *testing.T) {
 		{`-value:null`, `value IS NOT NULL`},
 		{`str~"like"`, `str LIKE '%like%'`},
 		{`(str~"like" AND -otherstr~"notlike")`, `(str LIKE '%like%' AND otherstr NOT LIKE '%notlike%')`},
-		{`(general_column.description~"experiment description" AND (-general_column.id:456 OR -general_column.resourcePool~"test\"s comma value\"s"))`, `(e.config->>'description' LIKE '%experiment description%' AND (e.id != 456 OR e.config->'resources'->>'resource_pool' NOT LIKE '%test\"s comma value\"s%'))`},
-		{`(general_column.forkedFrom:5 OR (-validation.error:1 AND hp.hyperparameter<=10))`, `(e.parent_id = 5 OR ((besttrials.best_validation->'metrics'->'avg_metrics'->>'error')::float8 != 1 AND (e.config->'hyperparameters'->'hyperparameter'->>'val')::float8<=10))`},
+		{`(experiment.description~"experiment description" AND (-experiment.id:456 OR -experiment.resourcePool~"test\"s comma value\"s"))`, `(e.config->>'description' LIKE '%experiment description%' AND (e.id != 456 OR e.config->'resources'->>'resource_pool' NOT LIKE '%test\"s comma value\"s%'))`},
+		{`(experiment.forkedFrom:5 OR (-validation.error:1 AND hp.hyperparameter<=10))`, `(e.parent_id = 5 OR ((besttrials.best_validation->'metrics'->'avg_metrics'->>'error')::float8 != 1 AND (e.config->'hyperparameters'->'hyperparameter'->>'val')::float8<=10))`},
 	}
 	for _, c := range validTestCases {
 		result, err := parseFilter(c[0])
 		require.NoError(t, err)
-		require.Equal(t, c[1], result)
+		require.Equal(t, c[1], *result)
 	}
 }
