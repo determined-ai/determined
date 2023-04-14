90fe108b863727aa7b7f42f7385bdb23f034e3a7
Phillip Gaisford <phillip.gaisford@hpe.com>
2023-04-14 16:56:38 -0400

    feat: agent support for podman

--- master/static/srv/singularity-entrypoint-wrapper.sh
+++ master/static/srv/singularity-entrypoint-wrapper.sh
@@ -5,7 +5,7 @@ set -eE
 trap 'echo >&2 "FATAL: Unexpected error terminated dispatcher-wrapper container initialization.  See error messages above."' ERR
 
 # Controls debug logging for this method
-DEBUG=0
+DEBUG=1
 
 # TODO(DET-9074): Turn this only iff DET_DEBUG == true.
 set -x
@@ -62,6 +62,24 @@ fi
 if [ $(ulimit -l) == 64 ]; then
     log_debug "DEBUG: Setting (max locked memory) ulimit -l unlimited"
     ulimit -l unlimited
+fi
+
+# When running under podman rootless, the default user on entry is root/uid=0 inside the
+# container, which maps to the launching user outside the container.  In this case,
+# rewrite the determined agent user uid/gid in /run/determined/etc/passwd to be
+# 0:0 to match.
+#
+# cat  /run/determined/etc/passwd
+#     username:x:1001:39::/run/determined/workdir:/bin/bash
+#
+# This maps the launching user into the same account when entering vis SSH
+# as the user what is executing the entry-point script here, and will
+# result in:
+# cat  /run/determined/etc/passwd
+#     username:x:0:0::/run/determined/workdir:/bin/bash
+if [ $(whoami) == "root" ] && [ -r /run/determined/etc/passwd ]; then
+    log_debug "DEBUG: Running as root inside container, changing agent user passwd entry to uid/gid 0/0."
+    sed -i "s/\([a-zA-Z0-9]\+\):x:[0-9]\+:[0-9]\+:/\1:x:0:0:/" /run/determined/etc/passwd
 fi
 
 log_debug "DEBUG: Will utilize slots DET_SLOT_IDS $DET_SLOT_IDS"
