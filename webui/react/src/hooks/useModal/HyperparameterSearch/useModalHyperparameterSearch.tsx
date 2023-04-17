89900ce5b49bb4557b4910afc1ed8312ee33f9f1
gt2345 <gt2345@columbia.edu>
2023-04-17 17:02:37 -0500

    fix: close modal properly

--- webui/react/src/hooks/useModal/HyperparameterSearch/useModalHyperparameterSearch.tsx
+++ webui/react/src/hooks/useModal/HyperparameterSearch/useModalHyperparameterSearch.tsx
@@ -122,6 +122,7 @@ const useModalHyperparameterSearch = ({
 
   useEffect(() => {
     const cancelerTemp = canceler.current;
+    clusterStore.poll();
     return () => {
       cancelerTemp.abort();
     };
