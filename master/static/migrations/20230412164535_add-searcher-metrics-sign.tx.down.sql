185c07a899ab4e2a241796ee4bd45ebb14bf5f08
julian-determined-ai <julian.glover@hpe.com>
2023-04-17 10:08:24 -0500

    update queries

--- master/static/migrations/20230412164535_add-searcher-metrics-sign.tx.down.sql
+++ master/static/migrations/20230412164535_add-searcher-metrics-sign.tx.down.sql
@@ -1,11 +1,7 @@
 ALTER TABLE trials DROP COLUMN searcher_metric_value_signed;
 DROP INDEX ix_trials_metric_value;
-ALTET TABLE trials DROP COLUMN latest_validation_id;
-DROP INDEX ix_trials_latest_validation;
-DROP INDEX ix_trials_best_validation;
 
 ALTER TABLE experiments DROP COLUMN best_trial_id;
-DROP INDEX ix_experiments_best_trial;
 ALTER TABLE experiments DROP COLUMN validation_metrics;
 DROP INDEX ix_experiments_validation_metrics;
 
