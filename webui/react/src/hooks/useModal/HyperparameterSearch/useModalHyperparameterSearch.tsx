1847e49bf5404f46b7b4758d8e8194092958e89e
Guangqing Tang <40620519+gt2345@users.noreply.github.com>
2023-04-18 20:57:59 -0500

    fix: close modal properly (#6574)

--- webui/react/src/hooks/useModal/HyperparameterSearch/useModalHyperparameterSearch.tsx
+++ webui/react/src/hooks/useModal/HyperparameterSearch/useModalHyperparameterSearch.tsx
@@ -122,6 +122,7 @@ const useModalHyperparameterSearch = ({
 
   useEffect(() => {
     const cancelerTemp = canceler.current;
+    clusterStore.fetchResourcePools(cancelerTemp.signal);
     return () => {
       cancelerTemp.abort();
     };
