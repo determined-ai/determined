6049002ab4506bf80eabdd649b2662ea9c204b96
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-04-19 13:35:17 -0400

    Revert "on conflict also update workspace_id"
    This reverts commit 603db6ea54bdc6c36bdfd2a6fb20690cdaca88cb.

--- master/static/srv/put_template.sql
+++ master/static/srv/put_template.sql
@@ -1,4 +1,4 @@
 INSERT INTO templates (name, config, workspace_id)
 VALUES ($1, $2, $3)
-ON CONFLICT (name) DO UPDATE SET config=$2, workspace_id=$3
+ON CONFLICT (name) DO UPDATE SET config=$2
 RETURNING name, config, workspace_id
