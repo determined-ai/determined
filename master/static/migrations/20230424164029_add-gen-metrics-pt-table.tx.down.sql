b67f7421fea617f6ca259da441fd2710ba9ffe16
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-04-24 16:42:37 -0400

    add migration to create and partition gen metrics table

---
+++ master/static/migrations/20230424164029_add-gen-metrics-pt-table.tx.down.sql
@@ -0,0 +1,16 @@
+ALTER TABLE metrics DETACH PARTITION raw_steps;
+ALTER TABLE metrics DETACH PARTITION raw_validations;
+ALTER TABLE metrics DETACH PARTITION generic_metrics;
+
+DROP table metrics;
+DROP table generic_metrics;
+
+DROP INDEX IF EXISTS steps_trial_id_total_batches_run_id_type_unique;
+CREATE UNIQUE INDEX steps_trial_id_total_batches_run_id_unique ON raw_steps (trial_id, total_batches, trial_run_id);
+ALTER TABLE raw_steps DROP COLUMN IF EXISTS type;
+
+DROP INDEX IF EXISTS validations_trial_id_total_batches_run_id_type_unique;
+CREATE UNIQUE INDEX validations_trial_id_total_batches_run_id_unique ON raw_validations (trial_id, total_batches, trial_run_id);
+ALTER TABLE raw_validations DROP COLUMN IF EXISTS type;
+
+DROP type metric_type;
