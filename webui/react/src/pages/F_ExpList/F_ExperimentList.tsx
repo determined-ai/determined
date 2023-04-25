473616de2ea01a7eb9b51b8fd3bd9745e7baef68
Ashton Galloway <ashton.galloway@hpe.com>
2023-04-25 11:39:50 -0400

    reset pagination when sort changes

--- webui/react/src/pages/F_ExpList/F_ExperimentList.tsx
+++ webui/react/src/pages/F_ExpList/F_ExperimentList.tsx
@@ -107,6 +107,19 @@ const F_ExperimentList: React.FC<Props> = ({ project }) => {
     [experimentFilters],
   );
 
+  const resetPagination = useCallback(() => {
+    setPage(0);
+    setExperiments([]);
+  }, []);
+
+  const onSortChange = useCallback(
+    (sorts: Sort[]) => {
+      setSorts(sorts);
+      resetPagination();
+    },
+    [resetPagination],
+  );
+
   const sortString = useMemo(() => makeSortString(sorts.filter(isValidSort)), [sorts]);
 
   const fetchExperiments = useCallback(async (): Promise<void> => {
@@ -263,7 +276,7 @@ const F_ExperimentList: React.FC<Props> = ({ project }) => {
               sorts={sorts}
               total={total}
               onAction={handleOnAction}
-              onSortChange={setSorts}
+              onSortChange={onSortChange}
             />
             <GlideTable
               clearSelectionTrigger={clearSelectionTrigger}
