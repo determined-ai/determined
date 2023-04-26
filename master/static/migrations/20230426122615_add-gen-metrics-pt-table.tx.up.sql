135a71c197eba453d9ec340afcb20aabc154b52c
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-04-26 13:17:45 -0400

    fmt migrations

--- master/static/migrations/20230426122615_add-gen-metrics-pt-table.tx.up.sql
+++ master/static/migrations/20230426122615_add-gen-metrics-pt-table.tx.up.sql
@@ -2,20 +2,26 @@ CREATE TYPE metric_type AS ENUM ('validation', 'training', 'generic');
 
 ALTER TABLE raw_validations ADD COLUMN type metric_type NOT NULL DEFAULT 'validation';
 ALTER INDEX validations_trial_id_total_batches_run_id_unique RENAME TO validations_trial_id_total_batches_run_id_unique_old;
-CREATE UNIQUE INDEX validations_trial_id_total_batches_run_id_type_unique ON raw_validations (trial_id, total_batches, trial_run_id, type);
+CREATE UNIQUE INDEX validations_trial_id_total_batches_run_id_type_unique ON raw_validations (
+    trial_id, total_batches, trial_run_id, type
+);
 DROP INDEX validations_trial_id_total_batches_run_id_unique_old;
 
 ALTER TABLE raw_steps ADD COLUMN type metric_type NOT NULL DEFAULT 'training';
 ALTER INDEX steps_trial_id_total_batches_run_id_unique RENAME TO steps_trial_id_total_batches_run_id_unique_old;
-CREATE UNIQUE INDEX steps_trial_id_total_batches_run_id_type_unique ON raw_steps (trial_id, total_batches, trial_run_id, type);
+CREATE UNIQUE INDEX steps_trial_id_total_batches_run_id_type_unique ON raw_steps (
+    trial_id, total_batches, trial_run_id, type
+);
 DROP INDEX steps_trial_id_total_batches_run_id_unique_old;
 
 CREATE TABLE generic_metrics (LIKE raw_steps INCLUDING ALL);
 ALTER TABLE generic_metrics ALTER COLUMN type SET DEFAULT 'generic';
 
 CREATE TABLE metrics (LIKE generic_metrics INCLUDING ALL)
-    PARTITION BY LIST (type);
+PARTITION BY LIST (type);
 
 ALTER TABLE metrics ATTACH PARTITION generic_metrics FOR VALUES IN ('generic');
-ALTER TABLE metrics ATTACH PARTITION raw_validations FOR VALUES IN ('validation');
+ALTER TABLE metrics ATTACH PARTITION raw_validations FOR VALUES IN (
+    'validation'
+);
 ALTER TABLE metrics ATTACH PARTITION raw_steps FOR VALUES IN ('training');
