8e6aa2a032153bed084ee4b14e75c8d74f3319cd
Dan Herrera <dan.herrera@hpe.com>
2023-04-27 19:42:26 -0400

    implement docsite promotion script

--- docs/deploy/promote.py
+++ docs/deploy/promote.py
@@ -1,15 +1,9 @@
 import argparse
-import html
-import io
-import json
 import os
 import pathlib
-import re
-import sys
-import traceback
-from xml.etree import ElementTree
+import time
 
-from algoliasearch import search_client
+import boto3
 
 HERE = pathlib.Path(__file__).parent
 
@@ -19,42 +13,123 @@ BUILD = str(HERE / ".." / "site" / "xml")
 
 
 if __name__ == "__main__":
-    # get version to promote
-    # get account creds
-    # verify version already uploaded and error if not
-    # opt: verify search index already populated for version
-    # copy over blobs to latest, tracking all files that were copied over
-    # find and delete all files that are in /latest but weren't just copied over
     parser = argparse.ArgumentParser()
-    parser.add_argument("--json", action="store_true", help="dump records to stdout")
-    parser.add_argument("--upload", action="store_true", help="upload to algolia")
-    parser.add_argument("--app-id", default="9H1PGK6NP7", help="algloia app id")
     parser.add_argument(
-        "--api-key", default=os.environ.get("ALGOLIA_API_KEY"), help="algloia admin key"
+        "--version",
+        type=str,
+        help="version to copy over to site root",
+    )
+    parser.add_argument(
+        "--root-prefix",
+        type=str,
+        default=os.environ.get("DOCSITE_ROOT_KEY", "latest/"),
+        help="object key prefix for site root",
+    )
+    parser.add_argument(
+        "--bucket-id",
+        type=str,
+        default="determined-ai-docs",
+        help="S3 bucket ID where doc pages are served from",
+    )
+    parser.add_argument(
+        "--bucket-region",
+        type=str,
+        default="us-west-2",
+        help="S3 bucket region where doc pages are served from",
+    )
+    parser.add_argument(
+        "--cf-distribution",
+        type=str,
+        default=os.environ.get("CF_DISTRIBUTION_ID", ""),
+        help="CloudFront distribution ID to create invalidation for",
+    )
+    parser.add_argument(
+        "--aws-access-key-id",
+        type=str,
+        default=os.environ.get("AWS_ACCESS_KEY_ID"),
+        help="AWS access key ID for uploading to S3",
+    )
+    parser.add_argument(
+        "--aws-secret-access-key",
+        type=str,
+        default=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+        help="AWS secret access key for uploading to S3",
     )
     args = parser.parse_args()
 
-    # Pick the correct version.
-    HERE = pathlib.Path(__file__).parent
-    with (HERE / ".." / ".." / "VERSION").open() as f:
-        version = f.read().strip()
-    if "-dev" in version:
-        # Dev builds search against a special dev index that is update with every push to master.
-        version = "dev"
-    elif "-rc" in version:
-        # Each release candidate publishes against the actual version without the "-rc" in the name.
-        version = version[: version.index("-rc")]
+    if "preview" in args.version:
+        print("only promote published doc versions, not previews!")
+        os.exit(1)
 
-    records = scrape_tree(BUILD, EXCLUDES)
+    source_prefix = args.version + "/"
+    target_prefix = args.root_prefix
+    if target_prefix[-1] != "/":
+        # for consistency of use, ensure the prefix ends with a /
+        target_prefix = target_prefix + "/"
 
-    if args.json:
-        json.dump(records, sys.stdout, indent="  ")
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(args.bucket_id)
+    # confirm the version given actually exists
+    source_objects = [o.key for o in bucket.objects.filter(Prefix=source_prefix)]
+    if len(source_objects) == 0:
+        print("version specified doesn't seem to be uploaded")
+        print("run upload.py for a build of that version before retrying")
+        os.exit(1)
 
-    if args.upload:
-        if not args.api_key:
-            print("--api-key or ALGOLIA_API_KEY required for upload", file=sys.stderr)
-            exit(1)
-        upload(args.app_id, args.api_key, records, version)
+    # TODO(danh): reference existing Algolia search indices for the version
+    # being promoted to ensure only indexed docsites are being promoted
 
-    if not args.upload and not args.json:
-        print("scrape was successful, try --json or --upload", file=sys.stderr)
+    # track current objects to know what needs to be cleaned up afterwards
+    current_objects = [o.key for o in bucket.objects.filter(Prefix=target_prefix)]
+    if len(current_objects) > 0:
+        print("{} already at site root".format(len(current_objects)))
+
+    # copy over blobs to latest, tracking all files that were copied over
+    copied_objects = []
+    for src in source_objects:
+        copy_source = {
+            "Bucket": bucket.name,
+            "Key": src,
+        }
+        src_name = str(pathlib.Path(src).relative_to(source_prefix))
+        target_key = target_prefix + src_name
+        bucket.copy(copy_source, target_key)
+        print("copied over {}".format(target_key))
+        copied_objects.append(target_key)
+
+    to_delete = set(current_objects).difference(copied_objects)
+    if len(to_delete) > 0:
+        print("deleting {} excess objects".format(len(to_delete)))
+        delete_request = {"Objects": [{"Key": key} for key in to_delete], "Quiet": True}
+        # TODO(danh): just uncomment this whenever the thought's not scary :D
+        # bucket.delete_objects(Delete=delete_request)
+
+    print(
+        "upload done, {} objects copied, {} objects deleted".format(
+            len(copied_objects), len(to_delete)
+        )
+    )
+
+    # create invalidation if distribution ID provided
+    if args.cf_distribution:
+        client = boto3.client("cloudfront")
+        timestamp = time.time_ns()
+        path = "/{}*".format(target_prefix)
+        client.create_invalidation(
+            DistributionId=args.cf_distribution,
+            InvalidationBatch={
+                "Paths": {
+                    "Quantity": 1,
+                    "Items": [
+                        path,
+                    ],
+                },
+                "CallerReference": str(timestamp),
+            },
+        )
+        print("invalidation {} created for {}".format(timestamp, path))
+
+    objects_url = "https://{}.s3.{}.amazonaws.com/{}index.html".format(
+        args.bucket_id, args.bucket_region, target_prefix
+    )
+    print("site root available at: " + objects_url)
