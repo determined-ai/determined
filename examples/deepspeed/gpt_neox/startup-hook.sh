6f3b3443cb8455f49364a688778920430ae49382
Daran He <daran.he@hpe.com>
2023-04-27 16:45:43 -0700

    fix: Fix GPT-NeoX example copy collision during container startup inspired by Liam As Liam investigated, there is a copy collision with the GPT-NeoX demo currently. This change mitigates that. We might still need to convert to a shared_fs approach in the future to take advantage of larger scale data.
    ghstack-source-id: f647c0ae034b1bcd2b15a1a2a7f4290e6f3323b4
    Pull Request resolved: https://github.com/determined-ai/determined/pull/6663

--- examples/deepspeed/gpt_neox/startup-hook.sh
+++ examples/deepspeed/gpt_neox/startup-hook.sh
@@ -1,10 +1,7 @@
 export PYTHONPATH=$PYTHONPATH:/gpt-neox
 
-# Copy dataset from docker image to shared filesystem
+# Use data in docker image and copy over needed configs.
 USER=$(whoami)
 mkdir /tmp/${USER}
-mkdir -p /run/determined/workdir/shared_fs/data
-cp -r -n /gpt-neox/data /run/determined/workdir/shared_fs/
-
 cd /run/determined/workdir
-cp gpt_neox_config/determined_cluster.yml /gpt-neox/configs
+cp gpt_neox_config/* /gpt-neox/configs
