90ac5675671654b61877016048b8c82e0651876b
Taylor Ritenour <taylor.ritenour@hpe.com>
2023-05-02 09:55:52 -0700

    chore: update deepspeed to 0.8.3 [MLG-399] (#6685)

--- ds_autotuning_prototype/startup-hook.sh
+++
@@ -1,4 +0,0 @@
-# Update deepspeed
-pip install "deepspeed[autotuning_ml]"==0.8.1
-# Hack for seeing DEBUG logs from deepspeed
-# sed -i 's/level=logging.INFO/level=logging.DEBUG/g' /opt/conda/lib/python3.8/site-packages/deepspeed/utils/logging.py
