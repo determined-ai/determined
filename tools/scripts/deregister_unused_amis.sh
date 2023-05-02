e22245b0f91eb12ecb01b3aadcf55b7c1fe1926d
Daniel R Hunter <daniel.robert.hunter@hpe.com>
2023-05-02 14:56:30 -0400

    Final form

--- tools/scripts/deregister_unused_amis.sh
+++ tools/scripts/deregister_unused_amis.sh
@@ -9,37 +9,36 @@ REGIONS=(
 	"us-east-1"
 	"us-east-2"
 )
-rm -f .amis
 for region in ${REGIONS[@]}; do
-	echo ${region}
-	aws ec2 describe-images --region ${region} --owners self --output json | jq ".Images[] | {ImageId, Name, CreationDate}" | jq -s "sort_by(.CreationDate)" | jq -r '.[] | "\(.ImageId) \(.Name) \(.CreationDate)"' >.amis_${region}
-	while read image; do
-		arr=($image)
-		if [[ "${arr[1]}" =~ "det-environments-".* ]]; then
-			echo "${region} ${image}" >>.amis
-		fi
-	done <.amis_${region}
-	rm .amis_${region}
-done
+	aws ec2 describe-images --region ${region} --output json --filters "Name=name,Values=det-environments-*" \
+        | jq ".Images[] | {ImageId, Name, CreationDate}" \
+        | jq -s "sort_by(.CreationDate)" \
+        | jq -r '.[] | "\(.ImageId) \(.Name) \(.CreationDate)"' \
+        | sed "s/\$/ ${region}/"
+done | sort -u > all_amis_info
 
-rm -f .amis_to_deregister
+cat all_amis_info | awk '{ print $1 }' > all_amis
 
-while read ami; do
-	amiarray=($ami)
-	commit=${amiarray[2]##det-environments-}
-	logsearch=$(git log --pretty=oneline -S${commit} -- ./bumpenvs.yaml)
-	if [[ "$logsearch" ]]; then
-		echo "Commit ${commit} appeared in bumpenvs.yaml here:"
-		echo "$logsearch"
-	else
-		echo "Commit ${commit} not used! Maybe delete ${amiarray[1]}"
-		echo "${amiarray[0]} ${amiarray[1]}" >>.amis_to_deregister
-	fi
-done <.amis
+while read tag; do
+    git -C $DET_PROJ show $tag:tools/scripts/bumpenvs.yaml \
+        | yq -r ".[].new" \
+        | sed -n "/ami/p"
+done < det_versions | sort -u > published_amis
 
-echo "AMIs from environments commits that did not appear in determined:"
-cat .amis_to_deregister
-wc -l .amis_to_deregister
+for region in ${REGIONS[@]}; do
+    aws ec2 describe-images --region ${region} --output json \
+        --filters "Name=name,Values=det-environments-*" \
+        --query 'Images[?CreationDate>`2023-02-02`]' \
+        | jq -r '.[].ImageId'
+done | sort -u > recent_amis
+
+cat all_amis published_amis published_amis recent_amis recent_amis \
+    | sort | uniq -u > deletable_amis
+
+grep -f deletable_amis all_amis_info > deletable_amis_info
+
+wc -l deletable_amis
+wc -l deletable_amis_info
 
 #while read ami; do
 #	ami=($ami)
