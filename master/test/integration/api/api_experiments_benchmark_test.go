fadccd5fe1d0f12fc6db28a108725bf5e883e54e
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2023-05-09 17:01:28 -0400

    progress on writing a good list exp benchmark

--- master/test/integration/api/api_experiments_benchmark_test.go
+++ master/test/integration/api/api_experiments_benchmark_test.go
@@ -4,15 +4,24 @@
 package api
 
 import (
+	"context"
 	"fmt"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/uptrace/bun"
+
 	//"github.com/determined-ai/determined/proto/pkg/commonv1"
 	//"github.com/determined-ai/determined/proto/pkg/trialv1"
 	//"github.com/determined-ai/determined/master/internal"
 	//"github.com/determined-ai/determined/master/internal/db"
 	//"github.com/determined-ai/determined/master/pkg/schemas/expconf"
 	"github.com/determined-ai/determined/proto/pkg/apiv1"
+
+	"github.com/determined-ai/determined/master/internal/db"
+
+	"github.com/determined-ai/determined/master/test/testutils"
 )
 
 // Thin wrapper over b.Run with an undertime that the test will fail.
@@ -36,27 +45,132 @@ func RunBenchmark(t *testing.T, fasterThanTime time.Duration, name string, f fun
 
 func TestBenchmarkGetExperiments(t *testing.T) {
 	fasterThan := 500 * time.Millisecond
+	maxWebLimit := int32(200)
 
-	/*
-		_, _, cl, creds, err := testutils.RunMaster(ctx, nil)
-		if err != nil {
-			panic(err) // TODO
-		}
-	*/
+	// TODO don't do this everytime.
+	ctx := context.Background()
+	_, _, api, ctx, err := testutils.RunMaster(ctx, nil)
+	require.NoError(t, err)
 
-	for intSort, stringSort := range apiv1.GetExperimentsRequest_SortBy_name {
-		for intOrder, stringOrder := range apiv1.OrderBy_name {
-			testName := fmt.Sprintf("GetExperiments SortBy=%s OrderBy=%s", stringSort, stringOrder)
-			RunBenchmark(t, fasterThan, testName, func(b *testing.B) {
-				time.Sleep(1 * time.Millisecond)
-				if intSort == 10 {
-					time.Sleep(1000 * time.Millisecond)
-				}
+	longBytesOfAs := make([]byte, 1024*1024)
+	for i := range longBytesOfAs {
+		longBytesOfAs[i] = 'a'
+	}
+	longStringOfAs := string(longBytesOfAs)
 
-				_, _ = intSort, intOrder
-			})
+	type expParams struct {
+		bun.BaseModel `bun:"table:experiments"`
+		Description   string
+		Name          string
+	}
+
+	{
+		for intSort, stringSort := range apiv1.GetExperimentsRequest_SortBy_name {
+			for intOrder, stringOrder := range apiv1.OrderBy_name {
+				testName := fmt.Sprintf("GetExperiments SortBy=%s OrderBy=%s", stringSort, stringOrder)
+				RunBenchmark(t, fasterThan, testName, func(b *testing.B) {
+					for i := 0; i < b.N; i++ {
+						_, err := api.GetExperiments(ctx, &apiv1.GetExperimentsRequest{
+							SortBy:  apiv1.GetExperimentsRequest_SortBy(intSort),
+							OrderBy: apiv1.OrderBy(intOrder),
+							Limit:   maxWebLimit,
+						})
+						require.NoError(t, err)
+					}
+				})
+			}
 		}
 	}
+
+	{
+		RunBenchmark(t, fasterThan, "GetExperiments Description=a", func(b *testing.B) {
+			_, err := api.GetExperiments(ctx, &apiv1.GetExperimentsRequest{
+				Description: "a",
+				Limit:       maxWebLimit,
+			})
+			require.NoError(t, err)
+		})
+
+		RunBenchmark(t, fasterThan, "GetExperiments Description=longStringOfAs", func(b *testing.B) {
+			_, err := api.GetExperiments(ctx, &apiv1.GetExperimentsRequest{
+				Description: string(longStringOfAs),
+				Limit:       maxWebLimit,
+			})
+			require.NoError(t, err)
+		})
+
+		longestPresentDescription := expParams{}
+		require.NoError(t, db.Bun().NewSelect().Model(&longestPresentDescription).
+			ColumnExpr("config->>'description' AS description").
+			OrderExpr("length(config->>'description') DESC").
+			Limit(1).
+			Scan(ctx))
+		RunBenchmark(t, fasterThan, "GetExperiments Description=longestPresent", func(b *testing.B) {
+			_, err := api.GetExperiments(ctx, &apiv1.GetExperimentsRequest{
+				Description: longestPresentDescription.Description,
+				Limit:       maxWebLimit,
+			})
+			require.NoError(t, err)
+		})
+	}
+
+	{
+		RunBenchmark(t, fasterThan, "GetExperiments Name=a", func(b *testing.B) {
+			_, err := api.GetExperiments(ctx, &apiv1.GetExperimentsRequest{
+				Name:  "a",
+				Limit: maxWebLimit,
+			})
+			require.NoError(t, err)
+		})
+
+		RunBenchmark(t, fasterThan, "GetExperiments Name=longStringOfAs", func(b *testing.B) {
+			_, err := api.GetExperiments(ctx, &apiv1.GetExperimentsRequest{
+				Name:  longStringOfAs,
+				Limit: maxWebLimit,
+			})
+			require.NoError(t, err)
+		})
+
+		longestNameDescription := expParams{}
+		require.NoError(t, db.Bun().NewSelect().Model(&longestNameDescription).
+			ColumnExpr("config->>'name' AS name").
+			OrderExpr("length(config->>'Name') DESC").
+			Limit(1).
+			Scan(ctx))
+		RunBenchmark(t, fasterThan, "GetExperiments Name=longestPresentName", func(b *testing.B) {
+			_, err := api.GetExperiments(ctx, &apiv1.GetExperimentsRequest{
+				Name:  longestNameDescription.Name,
+				Limit: maxWebLimit,
+			})
+			require.NoError(t, err)
+		})
+	}
+
+	RunBenchmark(t, fasterThan, "GetExperiments Labels=%s", func(b *testing.B) {
+	})
+
+	RunBenchmark(t, fasterThan, "GetExperiments Archived=true", func(b *testing.B) {
+	})
+	RunBenchmark(t, fasterThan, "GetExperiments Archived=false", func(b *testing.B) {
+	})
+
+	RunBenchmark(t, fasterThan, "GetExperiments States=%s", func(b *testing.B) {
+	})
+
+	RunBenchmark(t, fasterThan, "GetExperiments Users=%s", func(b *testing.B) {
+	})
+
+	RunBenchmark(t, fasterThan, "GetExperiments UserIds=%s", func(b *testing.B) {
+	})
+
+	RunBenchmark(t, fasterThan, "GetExperiments ProjectId=%s", func(b *testing.B) {
+	})
+
+	RunBenchmark(t, fasterThan, "GetExperiments ExperimentIdFilter=%s", func(b *testing.B) {
+	})
+
+	RunBenchmark(t, fasterThan, "GetExperiments ShowTrialData=%s", func(b *testing.B) {
+	})
 
 	// TODOs filters...
 
