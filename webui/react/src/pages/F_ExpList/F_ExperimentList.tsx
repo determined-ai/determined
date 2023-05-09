4a66b313393b82fe7e47be602fb224b791da74df
Guangqing Tang <40620519+gt2345@users.noreply.github.com>
2023-05-09 16:13:18 -0500

    fix: Exp list supports selection with shift/cmd (#6745)

--- webui/react/src/pages/F_ExpList/F_ExperimentList.tsx
+++ webui/react/src/pages/F_ExpList/F_ExperimentList.tsx
@@ -271,6 +271,20 @@ const F_ExperimentList: React.FC<Props> = ({ project }) => {
     [updateSettings],
   );
 
+  useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        setClearSelectionTrigger((prev) => prev + 1);
+        setSelectAll(false);
+      }
+    };
+    window.addEventListener('keydown', handleEsc);
+
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  }, []);
+
   return (
     <Page
       bodyNoPadding
@@ -311,7 +325,7 @@ const F_ExperimentList: React.FC<Props> = ({ project }) => {
             fetchExperiments={fetchExperiments}
             handleScroll={handleScroll}
             handleUpdateExperimentList={handleUpdateExperimentList}
-            height={wholePageHeight}
+            height={wholePageHeight - 140}
             page={page}
             project={project}
             projectColumns={projectColumns}
