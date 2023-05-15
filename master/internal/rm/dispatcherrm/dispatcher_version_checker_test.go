32ba14d4288204d7196f7dc474cd0797724feb01
Jerry J. Harrow <84593277+jerryharrow@users.noreply.github.com>
2023-05-15 16:24:16 -0400

    fix: Leverage assigned dispatchID in DispatchRM [DET-9474] (#847)
    Utilize the 3.2.9 launcher feature to choose the dispatchID at launch
    to simplify the detection of throttled job launches.  Use the
    allocationID as the dispatcheID, and switch to synchronous launch.

--- master/internal/rm/dispatcherrm/dispatcher_version_checker_test.go
+++ master/internal/rm/dispatcherrm/dispatcher_version_checker_test.go
@@ -10,7 +10,7 @@ import (
 func TestCheckLauncherVersion(t *testing.T) {
 	assert.Equal(t, checkLauncherVersion(semver.MustParse("4.1.0")), true)
 	assert.Equal(t, checkLauncherVersion(semver.MustParse("4.1.3-SNAPSHOT")), true)
-	assert.Equal(t, checkLauncherVersion(semver.MustParse("3.2.4")), true)
+	assert.Equal(t, checkLauncherVersion(semver.MustParse("3.2.9")), true)
 	assert.Equal(t, checkLauncherVersion(semver.MustParse("3.2.3")), false)
 	assert.Equal(t, checkLauncherVersion(semver.MustParse("3.2.0")), false)
 	assert.Equal(t, checkLauncherVersion(semver.MustParse("3.1.3")), false)
