60e81ccb191f4861567f99a75a4856d0906e1533
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-05-15 15:02:14 -0700

    take out webui references

--- webui/react/src/pages/Wait.tsx
+++ webui/react/src/pages/Wait.tsx
@@ -28,14 +28,13 @@ const Wait: React.FC = () => {
   const [searchParams] = useSearchParams();
   const { taskType } = useParams<Params>();
   const [waitStatus, setWaitStatus] = useState<WaitStatus>();
-  const eventUrl = searchParams.get('eventUrl');
   const serviceAddr = searchParams.get('serviceAddr');
 
   const capitalizedTaskType = capitalize(taskType ?? '');
   const isLoading = !waitStatus || !terminalCommandStates.has(waitStatus.state);
 
   let message = `Waiting for ${capitalizedTaskType} ...`;
-  if (!eventUrl || !serviceAddr) {
+  if (!serviceAddr) {
     message = 'Missing required parameters.';
   } else if (waitStatus && terminalCommandStates.has(waitStatus.state)) {
     message = `${capitalizedTaskType} has been terminated.`;
@@ -62,7 +61,7 @@ const Wait: React.FC = () => {
   };
 
   useEffect(() => {
-    if (!eventUrl || !serviceAddr) return;
+    if (!serviceAddr) return;
     const taskId = (serviceAddr.match(/[0-f-]+/) || ' ')[0];
     const ival = setInterval(async () => {
       try {
@@ -85,7 +84,7 @@ const Wait: React.FC = () => {
         handleTaskError(e as Error);
       }
     }, 1000);
-  }, [eventUrl, serviceAddr]);
+  }, [serviceAddr]);
 
   return (
     <PageMessage title={capitalizedTaskType}>
