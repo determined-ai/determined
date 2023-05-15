5e4dd397ad9aa5f58979aeb2af9f1bb36515b1b3
Ilia Glazkov <ilia@hpe.com>
2023-05-15 15:50:51 -0700

    add a test.

---
+++ e2e_tests/tests/experiment/test_unmanaged.py
@@ -0,0 +1,12 @@
+import os
+import pytest
+import subprocess
+from tests import config as conf
+
+@pytest.mark.e2e_cpu
+def test_unmanaged() -> None:
+    master_url = conf.make_master_url()
+    exp_path = conf.EXAMPLES_PATH / "features" / "unmanaged" / "unmanaged_2_hp_search.py"
+    env = os.environ.copy()
+    env["DET_MASTER"] = master_url
+    subprocess.run(["python", exp_path], env=env, check=True)
