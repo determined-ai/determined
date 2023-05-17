fa02084556a3186253795e58407e22b46c7276ba
Thiago Dallacqua <thiago.menezes-dallacqua@hpe.com>
2023-05-17 16:19:56 -0300

    feat: created enums for theme sizing

---
+++ webui/react/src/utils/themeSizing.ts
@@ -0,0 +1,38 @@
+export const Header = {
+  FontSizeDefault: '--header-size-default',
+  FontSizeL: '--header-size-L',
+  FontSizeS: '--header-size-S',
+  FontSizeXL: '--header-size-XL',
+  FontSizeXS: '--header-size-XS',
+  LineHeightDefault: '--header-ln-default',
+  LineHeightL: '--header-ln-L',
+  LineHeightS: '--header-ln-S',
+  LineHeightXL: '--header-ln-XL',
+  LineHeightXS: '--header-ln-XS',
+};
+
+export const MultiLine = {
+  FontSizeDefault: '--multi-line-size-default',
+  FontSizeL: '--multi-line-size-L',
+  FontSizeS: '--multi-line-size-S',
+  FontSizeXL: '--multi-line-size-XL',
+  FontSizeXS: '--multi-line-size-XS',
+  LineHeightDefault: '--multi-line-ln-default',
+  LineHeightL: '--multi-line-ln-L',
+  LineHeightS: '--multi-line-ln-S',
+  LineHeightXL: '--multi-line-ln-XL',
+  LineHeightXS: '--multi-line-ln-XS',
+};
+
+export const SingleLine = {
+  FontSizeDefault: '--single-line-size-default',
+  FontSizeL: '--single-line-size-L',
+  FontSizeS: '--single-line-size-S',
+  FontSizeXL: '--single-line-size-XL',
+  FontSizeXS: '--single-line-size-XS',
+  LineHeightDefault: '--single-line-ln-default',
+  LineHeightL: '--single-line-ln-L',
+  LineHeightS: '--single-line-ln-S',
+  LineHeightXL: '--single-line-ln-XL',
+  LineHeightXS: '--single-line-ln-XS',
+};
