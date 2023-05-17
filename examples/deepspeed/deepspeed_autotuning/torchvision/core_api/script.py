877e82197033155759b2d1b6a9347305fb3822bf
Garrett Goon <garrett.goon@hpe.com>
2023-05-17 16:27:26 -0400

    core api script cleanup

--- examples/deepspeed/deepspeed_autotuning/torchvision/core_api/script.py
+++ examples/deepspeed/deepspeed_autotuning/torchvision/core_api/script.py
@@ -13,6 +13,12 @@ from determined.pytorch import dsat
 
 
 class RandImageNetDataset(Dataset):
+    """
+    A fake, ImageNet-like dataset which only actually contains `num_actual_datapoints` independent
+    datapoints, but pretends to have the number reported in `__len__`. Used for speed and
+    simplicity. Replace with your own ImageNet-like dataset as desired.
+    """
+
     def __init__(self, num_actual_datapoints: int = 128) -> None:
         self.num_actual_datapoints = num_actual_datapoints
         self.imgs = torch.randn(self.num_actual_datapoints, 3, 224, 224)
@@ -33,75 +39,31 @@ def main(
 ) -> None:
     is_chief = core_context.distributed.rank == 0
     hparams = AttrDict(hparams)
-    # TODO: Remove hack for seeing actual used HPs after Web UI is fixed.
-    logging.info(f"HPs seen by trial: {hparams}")
-    # Hack for clashing 'type' key. Need to change config parsing behavior so that
-    # user scripts don't need to inject helper functions like this.
     ds_config = dsat.get_ds_config_from_hparams(hparams)
-    logging.info(f"ds_config: {ds_config}")
-
     deepspeed.init_distributed()
 
-    ########################################################################
-    # The output of torchvision datasets are PILImage images of range [0, 1].
-    # We transform them to Tensors of normalized range [-1, 1].
-    # .. note::
-    #     If running on Windows and you get a BrokenPipeError, try setting
-    #     the num_worker of torch.utils.data.DataLoader() to 0.
-
     trainset = RandImageNetDataset()
+    model = getattr(models, hparams.model_name)()
+    parameters = filter(lambda p: p.requires_grad, model.parameters())
 
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=True, num_workers=2)
-
-    model_dict = {
-        "resnet152": models.resnet152,
-        "wide_resnet101_2": models.wide_resnet101_2,
-        "vgg19": models.vgg19,
-        "regnet_x_32gf": models.regnet_x_32gf,
-    }
-
-    net = model_dict[hparams.model_name]()
-
-    parameters = filter(lambda p: p.requires_grad, net.parameters())
-
-    # Initialize DeepSpeed to use the following features
-    # 1) Distributed model
-    # 2) Distributed data loader
-    # 3) DeepSpeed optimizer
-    model_engine, optimizer, trainloader, __ = deepspeed.initialize(
-        model=net,
+    model_engine, _, trainloader, _ = deepspeed.initialize(
+        model=model,
         model_parameters=parameters,
         training_data=trainset,
         config=ds_config,
     )
 
     fp16 = model_engine.fp16_enabled()
-
-    ########################################################################
-    # 3. Define a Loss function and optimizer
-    # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    # Let's use a Classification Cross-Entropy loss and SGD with momentum.
-
     criterion = nn.CrossEntropyLoss()
-
-    ########################################################################
-    # 4. Train the network
-    # ^^^^^^^^^^^^^^^^^^^^
-    #
-    # This is when things start to get interesting.
-    # We simply have to loop over our data iterator, and feed the inputs to the
-    # network and optimize.
 
     steps_completed = 0
     for op in core_context.searcher.operations():
         while steps_completed < op.length:
             for data in trainloader:
                 with dsat.dsat_reporting_context(core_context, op):
-                    # get the inputs; data is a list of [inputs, labels]
                     inputs, labels = data[0].to(model_engine.local_rank), data[1].to(
                         model_engine.local_rank
                     )
-                    logging.info(f"ACTUAL BATCH SIZE: {inputs.shape[0]}")  # Sanity checking.
                     if fp16:
                         inputs = inputs.half()
                     outputs = model_engine(inputs)
@@ -115,7 +77,6 @@ def main(
                             steps_completed=steps_completed, metrics=metrics_dict
                         )
                 if model_engine.is_gradient_accumulation_boundary():
-                    logging.info("COMPLETED STEP")
                     if steps_completed == op.length:
                         break
                 if core_context.preempt.should_preempt():
@@ -128,6 +89,6 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format=det.LOG_FORMAT)
     info = det.get_cluster_info()
     hparams = info.trial.hparams
-    distributed = det.core.DistributedContext.from_torch_distributed()
+    distributed = det.core.DistributedContext.from_deepspeed()
     with det.core.init(distributed=distributed) as core_context:
         main(core_context, hparams)
