38c3009dce04e291724efc0c0a6d0f69bb7ece3e
Carolina Calderon <carolina.calderon@hpe.com>
2023-05-22 13:36:32 -0600

    finish fixing text

--- e2e_tests/tests/cluster/test_master_restart.py
+++ e2e_tests/tests/cluster/test_master_restart.py
@@ -115,7 +115,6 @@ def _test_master_restart_reattach_recover_experiment(
 
 @pytest.mark.managed_devcluster
 @pytest.mark.parametrize("wait_for_amnesia", [True, False])
-# TODO CAROLINA
 def test_master_restart_error_missing_docker_container(
     managed_cluster_restarts: ManagedCluster,
     wait_for_amnesia: bool,
@@ -148,12 +147,12 @@ def test_master_restart_error_missing_docker_container(
 
         expected_message = (
             (
-                "allocation terminated due to agent failure: agent failed while the "
+                "allocation terminated after agent failed while the "
                 + "container was running: agent closed with allocated containers"
             )
             if wait_for_amnesia
             else (
-                "allocation failed due to restore error: RM failed "
+                "allocation terminated after RM failed "
                 + "to restore the allocation: container is gone on reattachment"
             )
         )
