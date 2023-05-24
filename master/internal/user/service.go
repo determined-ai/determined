589e92cb24980cb164dec4a13cfbaa0a0b860e1b
Sean Mackrory <sean.mackrory@hpe.com>
2023-05-24 11:58:31 -0600

    Changing cookie name

--- master/internal/user/service.go
+++ master/internal/user/service.go
@@ -133,7 +133,7 @@ func (s *Service) extractToken(r *http.Request) (string, error) {
 			return "", echo.ErrUnauthorized
 		}
 		return strings.TrimPrefix(authRaw, "Bearer "), nil
-	} else if cookie, err := r.Cookie("jwt"); err == nil {
+	} else if cookie, err := r.Cookie("det_jwt"); err == nil {
 		return cookie.Value, nil
 	} else if cookie, err := r.Cookie("auth"); err == nil {
 		return cookie.Value, nil
