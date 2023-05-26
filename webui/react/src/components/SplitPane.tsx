9ff5e316b2d6aec8649d938e0678429046ccaee0
Emily <15078396+EmilyBonar@users.noreply.github.com>
2023-05-26 13:00:30 -0400

    feat: add comparison toggle to glide experiment list [WEB-989] (#6976)

---
+++ webui/react/src/components/SplitPane.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+import useResize from 'hooks/useResize';
+
+import css from './SplitPane.module.scss';
+
+interface Props {
+  children: [React.ReactElement, React.ReactElement];
+  initialWidth?: number;
+  minimumWidths?: [number, number];
+  onChange?: (width: number) => void;
+  open?: boolean;
+}
+
+const SplitPane: React.FC<Props> = ({
+  children,
+  initialWidth = 400,
+  minimumWidths = [200, 200],
+  onChange,
+  open = true,
+}: Props) => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [width, setWidth] = useState(initialWidth);
+  const container = useRef<HTMLDivElement>(null);
+  const handle = useRef<HTMLDivElement>(null);
+  const containerDimensions = useResize(container);
+
+  useEffect(() => {
+    const c = (e: MouseEvent) => {
+      e.preventDefault();
+      setIsDragging(true);
+    };
+    const handleRef = handle.current;
+    handleRef?.addEventListener('mousedown', c);
+
+    return () => handleRef?.removeEventListener('mousedown', c);
+  }, []);
+
+  useEffect(() => {
+    if (!isDragging) return;
+    const c = (e: MouseEvent) => {
+      e.preventDefault();
+
+      // Get x-coordinate of pointer relative to container
+      const pointerRelativeXpos = e.clientX - containerDimensions.x;
+
+      // * 8px is the left/right spacing between .handle and its inner pseudo-element
+      const newWidth = Math.min(
+        Math.max(minimumWidths[0], pointerRelativeXpos - 8),
+        containerDimensions.width - minimumWidths[1],
+      );
+
+      // Resize box A
+      setWidth(newWidth);
+    };
+    document.addEventListener('mousemove', c);
+
+    return () => document.removeEventListener('mousemove', c);
+  }, [containerDimensions.width, containerDimensions.x, isDragging, minimumWidths]);
+
+  useEffect(() => {
+    if (!isDragging) return;
+    const c = () => {
+      // Turn off dragging flag when user mouse is up
+      setIsDragging(false);
+      onChange?.(width);
+    };
+
+    document.addEventListener('mouseup', c);
+
+    return () => document.removeEventListener('mouseup', c);
+  }, [width, isDragging, onChange]);
+
+  const classnames = [css.base];
+  if (open) classnames.push(css.open);
+
+  return (
+    <div className={classnames.join(' ')} ref={container}>
+      <div style={{ width: open ? width : '100%' }}>{children?.[0]}</div>
+      <div className={css.handle} ref={handle} />
+      <div className={css.rightBox}>{children?.[1]}</div>
+    </div>
+  );
+};
+
+export default SplitPane;
