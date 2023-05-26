a2751dc6e68beed66a9b4c217e86940e6a00bb3e
Ashton Galloway <ashton.galloway@hpe.com>
2023-05-26 11:53:42 -0400

    stablize return order for searches with sort

--- master/internal/api_experiment.go
+++ master/internal/api_experiment.go
@@ -2055,6 +2055,7 @@ func sortExperiments(sortString *string, experimentQuery *bun.SelectQuery) error
 		"desc": "DESC NULLS LAST",
 	}
 	sortParams := strings.Split(*sortString, ",")
+	hasIdSort := false
 	for _, sortParam := range sortParams {
 		paramDetail := strings.Split(sortParam, "=")
 		if len(paramDetail) != 2 {
@@ -2078,9 +2079,13 @@ func sortExperiments(sortString *string, experimentQuery *bun.SelectQuery) error
 			if _, ok := orderColMap[paramDetail[0]]; !ok {
 				return status.Errorf(codes.InvalidArgument, "invalid sort col: %s", paramDetail[0])
 			}
+			hasIdSort = hasIdSort || paramDetail[0] == "id"
 			experimentQuery.OrderExpr(
 				fmt.Sprintf("%s %s", orderColMap[paramDetail[0]], sortDirection))
 		}
+	}
+	if !hasIdSort {
+		experimentQuery.OrderExpr("id ASC")
 	}
 	return nil
 }
