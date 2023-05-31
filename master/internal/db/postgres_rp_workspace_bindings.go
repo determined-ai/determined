ae4de96f247b2c538767a97d646a1e7f11b20128
Nikita <nikita.rajaneesh@hpe.com>
2023-05-31 16:12:40 -0500

    lint

--- master/internal/db/postgres_rp_workspace_bindings.go
+++ master/internal/db/postgres_rp_workspace_bindings.go
@@ -7,7 +7,8 @@ import (
 )
 
 // RemoveRPWorkspaceBindings removes the bindings between workspaceIds and poolName.
-func (db *PgDB) RemoveRPWorkspaceBindings(ctx context.Context, workspaceIds []int32, poolName string) error {
+func (db *PgDB) RemoveRPWorkspaceBindings(ctx context.Context,
+	workspaceIds []int32, poolName string) error {
 	_, err := Bun().NewDelete().Table("rp_workspace_bindings").Where("workspace_id IN (?)",
 		bun.In(workspaceIds)).Where("pool_name = ?", poolName).Exec(ctx)
 
