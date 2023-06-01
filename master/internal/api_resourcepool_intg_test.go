7cbf7cdcbaa62a49e80fcaf8bf6ca4496c91b041
Eric <31023784+eecsliu@users.noreply.github.com>
2023-06-01 14:22:27 -0700

    chore: add testing function stubs (#7037)

---
+++ master/internal/api_resourcepool_intg_test.go
@@ -0,0 +1,16 @@
+package internal
+
+import "testing"
+
+func TestAuthZRPModifyBindings(t *testing.T) {
+	// test all endpoints against RBAC
+	return
+}
+
+func TestAuthZListWorkspacesBindingRP(t *testing.T) {
+	return
+}
+
+func TestAuthZListWorkspaceRPBindings(t *testing.T) {
+	return
+}
