ef3abda314e23888106f24297970546e10a6d3ff
Eric <31023784+eecsliu@users.noreply.github.com>
2023-06-01 12:37:08 -0700

    feat: add bind endpoint (#7027)

--- master/internal/db/postgres_rp_workspace_bindings.go
+++ master/internal/db/postgres_rp_workspace_bindings.go
@@ -6,9 +6,34 @@ import (
 	"github.com/uptrace/bun"
 )
 
+// RPWorkspaceBinding is a struct reflecting the db table rp_workspace_bindings.
+type RPWorkspaceBinding struct {
+	bun.BaseModel `bun:"table:rp_workspace_bindings"`
+	WorkspaceID   int    `bun:"workspace_id"`
+	PoolName      string `bun:"pool_name"`
+	Validity      bool   `bun:"validity"`
+}
+
+// AddRPWorkspaceBindings inserts new bindings between workspaceIds and poolName.
+func (db *PgDB) AddRPWorkspaceBindings(ctx context.Context, workspaceIds []int32, poolName string,
+) error {
+	var bindings []RPWorkspaceBinding
+	for _, workspaceID := range workspaceIds {
+		bindings = append(bindings, RPWorkspaceBinding{
+			WorkspaceID: int(workspaceID),
+			PoolName:    poolName,
+			Validity:    true,
+		})
+	}
+
+	_, err := Bun().NewInsert().Model(&bindings).Exec(ctx)
+	return err
+}
+
 // RemoveRPWorkspaceBindings removes the bindings between workspaceIds and poolName.
 func (db *PgDB) RemoveRPWorkspaceBindings(ctx context.Context,
-	workspaceIds []int32, poolName string) error {
+	workspaceIds []int32, poolName string,
+) error {
 	_, err := Bun().NewDelete().Table("rp_workspace_bindings").Where("workspace_id IN (?)",
 		bun.In(workspaceIds)).Where("pool_name = ?", poolName).Exec(ctx)
 
