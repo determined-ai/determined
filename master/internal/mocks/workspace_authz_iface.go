c33db82481d34cdb0272da4ca94b987b329071b8
Nikita Rajaneesh <nikita.rajaneesh@hpe.com>
2023-06-02 10:34:12 -0500

    feat: overwrrite bindings endpoint [DET-9548] (#7036)

--- master/internal/mocks/workspace_authz_iface.go
+++ master/internal/mocks/workspace_authz_iface.go
@@ -33,20 +33,6 @@ func (_m *WorkspaceAuthZ) CanArchiveWorkspace(ctx context.Context, curUser model
 	return r0
 }
 
-// CanBindRPWorkspace provides a mock function with given fields: ctx, curUser, workspaceIDs
-func (_m *WorkspaceAuthZ) CanBindRPWorkspace(ctx context.Context, curUser model.User, workspaceIDs []int32) error {
-	ret := _m.Called(ctx, curUser, workspaceIDs)
-
-	var r0 error
-	if rf, ok := ret.Get(0).(func(context.Context, model.User, []int32) error); ok {
-		r0 = rf(ctx, curUser, workspaceIDs)
-	} else {
-		r0 = ret.Error(0)
-	}
-
-	return r0
-}
-
 // CanCreateWorkspace provides a mock function with given fields: ctx, curUser
 func (_m *WorkspaceAuthZ) CanCreateWorkspace(ctx context.Context, curUser model.User) error {
 	ret := _m.Called(ctx, curUser)
@@ -117,6 +103,20 @@ func (_m *WorkspaceAuthZ) CanGetWorkspace(ctx context.Context, curUser model.Use
 	return r0
 }
 
+// CanModifyRPWorkspaceBindings provides a mock function with given fields: ctx, curUser, workspaceIDs
+func (_m *WorkspaceAuthZ) CanModifyRPWorkspaceBindings(ctx context.Context, curUser model.User, workspaceIDs []int32) error {
+	ret := _m.Called(ctx, curUser, workspaceIDs)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, model.User, []int32) error); ok {
+		r0 = rf(ctx, curUser, workspaceIDs)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // CanPinWorkspace provides a mock function with given fields: ctx, curUser, _a2
 func (_m *WorkspaceAuthZ) CanPinWorkspace(ctx context.Context, curUser model.User, _a2 *workspacev1.Workspace) error {
 	ret := _m.Called(ctx, curUser, _a2)
@@ -166,20 +166,6 @@ func (_m *WorkspaceAuthZ) CanSetWorkspacesName(ctx context.Context, curUser mode
 	var r0 error
 	if rf, ok := ret.Get(0).(func(context.Context, model.User, *workspacev1.Workspace) error); ok {
 		r0 = rf(ctx, curUser, _a2)
-	} else {
-		r0 = ret.Error(0)
-	}
-
-	return r0
-}
-
-// CanUnBindRPWorkspace provides a mock function with given fields: ctx, curUser, workspaceIDs
-func (_m *WorkspaceAuthZ) CanUnBindRPWorkspace(ctx context.Context, curUser model.User, workspaceIDs []int32) error {
-	ret := _m.Called(ctx, curUser, workspaceIDs)
-
-	var r0 error
-	if rf, ok := ret.Get(0).(func(context.Context, model.User, []int32) error); ok {
-		r0 = rf(ctx, curUser, workspaceIDs)
 	} else {
 		r0 = ret.Error(0)
 	}
