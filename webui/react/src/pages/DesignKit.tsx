54fd791430c5d57ff19d42aee9b1b01d7084c47b
John Kim <john.kim@hpe.com>
2023-06-02 15:56:34 -0400

    fix: Button with icon styling

--- webui/react/src/pages/DesignKit.tsx
+++ webui/react/src/pages/DesignKit.tsx
@@ -246,8 +246,16 @@ const ButtonsSection: React.FC = () => {
         </Space>
         <hr />
         <strong>Default Button with icon</strong>
+        With Antd Icon
         <Space>
-          <Button icon={<PoweroffOutlined />}>ButtonWithIcon</Button>
+          <Button icon={<PoweroffOutlined />} />
+          <Button icon={<PoweroffOutlined />}>ButtonText</Button>
+        </Space>
+        With Font Icon
+        <Space>
+          <Button icon={<Icon name="play" size="large" title="Play" />} />
+          <Button icon={<Icon name="play" size="large" title="Play" />}>ButtonWithLargeFontIcon</Button>
+          <Button icon={<Icon name="play" size="tiny" title="Play" />}>ButtonWithTinyFontIcon</Button>
         </Space>
         <hr />
         <strong>Button with icon and text displayed in a column</strong>
