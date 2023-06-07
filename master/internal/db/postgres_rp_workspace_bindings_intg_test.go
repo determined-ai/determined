28b98be4c36dbd152f126893db7adb9c3b4d9eca
Nikita <nikita.rajaneesh@hpe.com>
2023-06-07 10:06:25 -0500

    fix: master config, overwrite and remove intg test

--- master/internal/db/postgres_rp_workspace_bindings_intg_test.go
+++ master/internal/db/postgres_rp_workspace_bindings_intg_test.go
@@ -3,57 +3,103 @@
 
 package db
 
-import "testing"
+import (
+    "context"
+    "require"
+    "testing"
 
+    "github.com/determined-ai/determined/master/internal/config"
+    "github.com/determined-ai/determined/master/pkg/etc"
+)
+
+func masterConfig() {
+    config := config.GetMasterConfig()
+    config.
+}
 func TestAddAndRemoveBindings(t *testing.T) {
-	// Test single insert/delete
-	// Test bulk insert/delete
-	return
+    // Test single insert/delete
+    // Test bulk insert/delete
+    return
 }
 
 func TestBindingFail(t *testing.T) {
-	// Test add the same binding multiple times - should fail
-	// Test add same binding among bulk transaction - should fail the entire transaction
-	// Test add workspace that doesn't exist
-	// Test add RP that doesn't exist
-	return
+    // Test add the same binding multiple times - should fail
+    // Test add same binding among bulk transaction - should fail the entire transaction
+    // Test add workspace that doesn't exist
+    // Test add RP that doesn't exist
+    return
 }
 
 func TestListWorkspacesBindingRP(t *testing.T) {
-	// pretty straightforward
-	// don't list bindings that are invalid
-	// if RP is unbound, return nothing
-	return
+    // pretty straightforward
+    // don't list bindings that are invalid
+    // if RP is unbound, return nothing
+    return
 }
 
 func TestListRPsBoundToWorkspace(t *testing.T) {
-	// pretty straightforward
-	// don't list binding that are invalid
-	// return unbound pools too (we pull from config)
-	return
+    // pretty straightforward
+    // don't list binding that are invalid
+    // return unbound pools too (we pull from config)
+    return
 }
 
 func TestListAllBindings(t *testing.T) {
-	// pretty straightforward
-	// list ALL bindings, even invalid ones
-	// make sure to return unbound pools too (we pull from config)
-	return
+    // pretty straightforward
+    // list ALL bindings, even invalid ones
+    // make sure to return unbound pools too (we pull from config)
+    return
 }
 
 func TestOverwriteBindings(t *testing.T) {
-	// Test overwrite bindings
-	// Test overwrite pool that's not bound to anything currently
-	return
+    require.NoError(t, etc.SetRootPath(RootFromDB))
+    db := MustResolveTestPostgres(t)
+    MustMigrateTestPostgres(t, db, MigrationsFromDB)
+    ctx := context.TODO()
+    user := RequireMockUser(t, db)
+
+    // Test overwrite bindings
+    poolName := "poolName1"
+    workspaceNames := []string{"test1", "test2", "test3"}
+    workspaceIDs, err := MockWorkspaces(workspaceNames, user.ID)
+    require.NoError(err)
+    AddRPWorkspaceBindings(ctx, workspaceIDs, poolName)
+    OverwriteRPWorkspaceBindings(ctx, workspaceIDs, poolName)
+    // call list bindings here to make sure it worked
+    // Test overwrite pool that's not bound to anything currently
+    poolName = "poolName2"
+    OverwriteRPWorkspaceBindings(ctx, workspaceIDs, poolName)
+    // call list bindings here to make sure it worked
+    CleanupMockWorkspace(workspaceIds)
+    return
 }
 
 func TestOverwriteFail(t *testing.T) {
-	// Test overwrite adding workspace that doesn't exist
-	// Test overwrite pool that doesn't exist
-	return
+    ctx := context.TODO()
+    // Test overwrite adding workspace that doesn't exist
+    workspaceIDs := []int32{4, 5, 6}
+    poolName := "poolName"
+    err := OverwriteRPWorkspaceBindings(ctx, workspaceIDs, poolName)
+    require.Error(t, err)
+    // Test overwrite pool that doesn't exist
+    err = OverwriteRPWorkspaceBindings(ctx, workspaceIDs, poolName)
+    require.Error(t, err)
+    return
 }
 
 func TestRemoveInvalidBinding(t *testing.T) {
-	// remove binding that doesn't exist
-	// bulk remove bindings that don't exist
-	return
+    // remove binding that doesn't exist
+    poolName := "poolName"
+    workspaceIDs := []int32{1}
+    err := RemoveRPWorkspaceBindings(ctx, workspaceIDs, poolName)
+    require.Error(t, err)
+    // bulk remove bindings that don't exist
+    poolName := "poolName"
+    workspaceIDS := []int32{1, 2, 3}
+    err := RemoveRPWorkspaceBindings(ctx, workspaceIDs, poolName)
+    require.Error(t, err)
+    return
 }
+
+
+
