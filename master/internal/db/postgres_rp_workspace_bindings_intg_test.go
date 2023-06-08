2fcdc36761abe1c806beb73d8a33edc99f51ca69
Nikita Rajaneesh <nikita.rajaneesh@hpe.com>
2023-06-08 12:14:39 -0500

    fix: master config, overwrite intg test, remove invalid intg test (#7089)

--- master/internal/db/postgres_rp_workspace_bindings_intg_test.go
+++ master/internal/db/postgres_rp_workspace_bindings_intg_test.go
@@ -3,7 +3,15 @@
 
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/determined-ai/determined/master/internal/config"
+	"github.com/determined-ai/determined/master/pkg/etc"
+)
 
 func TestAddAndRemoveBindings(t *testing.T) {
 	// Test single insert/delete
@@ -41,19 +49,65 @@ func TestListAllBindings(t *testing.T) {
 }
 
 func TestOverwriteBindings(t *testing.T) {
+	require.NoError(t, etc.SetRootPath(RootFromDB))
+	db := MustResolveTestPostgres(t)
+	MustMigrateTestPostgres(t, db, MigrationsFromDB)
+	ctx := context.Background()
+	user := RequireMockUser(t, db)
+	var existingPools []config.ResourcePoolConfig
+	pool := config.ResourcePoolConfig{PoolName: "poolName1"}
+	existingPools = append(existingPools, pool)
 	// Test overwrite bindings
+	poolName := "poolName1" //nolint:goconst
+	workspaceNames := []string{"test1", "test2", "test3"}
+	workspaceIDs, err := MockWorkspaces(workspaceNames, user.ID)
+	require.NoError(t, err)
+	err = AddRPWorkspaceBindings(ctx, workspaceIDs, poolName, existingPools)
+	require.NoError(t, err)
+	err = OverwriteRPWorkspaceBindings(ctx, workspaceIDs, poolName, existingPools)
+	require.NoError(t, err)
+	// TODO: call list bindings here to make sure it worked
 	// Test overwrite pool that's not bound to anything currently
+	pool = config.ResourcePoolConfig{PoolName: "poolName2"}
+	existingPools = append(existingPools, pool)
+	poolName = "poolName2"
+	err = OverwriteRPWorkspaceBindings(ctx, workspaceIDs, poolName, existingPools)
+	require.NoError(t, err)
+	// TODO: call list bindings here to make sure it worked
+	err = CleanupMockWorkspace(workspaceIDs)
+	require.NoError(t, err)
 	return
 }
 
 func TestOverwriteFail(t *testing.T) {
+	ctx := context.Background()
+	var existingPools []config.ResourcePoolConfig
+	pool := config.ResourcePoolConfig{PoolName: "poolName1"}
+	existingPools = append(existingPools, pool)
 	// Test overwrite adding workspace that doesn't exist
+	workspaceIDs := []int32{100, 102, 103}
+	poolName := "poolName1"
+	err := OverwriteRPWorkspaceBindings(ctx, workspaceIDs, poolName, existingPools)
+	// db Error that workspace doesn't exist
+	require.ErrorContains(t, err, "violates foreign key constraint")
 	// Test overwrite pool that doesn't exist
+	poolName = "poolNameDoesntExist"
+	err = OverwriteRPWorkspaceBindings(ctx, workspaceIDs, poolName, existingPools)
+	require.ErrorContains(t, err, "doesn't exist in config")
 	return
 }
 
 func TestRemoveInvalidBinding(t *testing.T) {
+	ctx := context.Background()
 	// remove binding that doesn't exist
+	poolName := "poolName" //nolint:goconst
+	workspaceIDs := []int32{1}
+	err := RemoveRPWorkspaceBindings(ctx, workspaceIDs, poolName)
+	require.ErrorContains(t, err, "binding doesn't exist")
 	// bulk remove bindings that don't exist
+	poolName = "poolName"
+	workspaceIDs = []int32{1, 2, 3}
+	err = RemoveRPWorkspaceBindings(ctx, workspaceIDs, poolName)
+	require.ErrorContains(t, err, " binding doesn't exist")
 	return
 }
