a9da28f20ed675ac9f810d1e535e9647c41a7c1a
Thiago Dallacqua <thiago.menezes-dallacqua@hpe.com>
2023-06-14 16:42:56 -0300

    fix: add svg check for the Icon component

--- webui/react/src/components/kit/Icon.test.tsx
+++ webui/react/src/components/kit/Icon.test.tsx
@@ -4,6 +4,8 @@ import userEvent from '@testing-library/user-event';
 import Icon, { IconNameArray, IconSizeArray } from './Icon';
 import type { Props } from './Icon';
 
+const svgIcons = ['columns', 'filter', 'options', 'panel', 'row'];
+
 const setup = (props?: Props) => {
   const user = userEvent.setup();
   const view = render(<Icon name="star" showTooltip title="Icon" {...props} />);
@@ -24,7 +26,11 @@ describe('Icon', () => {
     it.each(IconNameArray)('should display a %s icon', (name) => {
       const { view } = setup({ name, title: name });
       const firstChild = view.container.firstChild;
-      expect(firstChild).toHaveClass(...['base', `icon-${name}`, 'medium']);
+      if (!svgIcons.includes(name)) {
+        expect(firstChild).toHaveClass(...['base', `icon-${name}`, 'medium']);
+      } else {
+        expect(firstChild?.nodeName).toBe('svg');
+      }
     });
   });
 
