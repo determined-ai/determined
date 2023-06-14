dc5d432047d8ca60bb3612d23ce56f55afec670d
Saloni Gupta <saloni.gupta@hpe.com>
2023-06-14 11:57:31 -0700

    feat: added new list fuction, new delete subcommand and added the use of default gcs bucket if local tf state if not present in det deploy gcp

--- harness/determined/deploy/gcp/cli.py
+++ harness/determined/deploy/gcp/cli.py
@@ -2,13 +2,14 @@ import argparse
 import os
 import sys
 from pathlib import Path
-from typing import Callable, Tuple
+from typing import Any, Callable, Tuple
 
 import pkg_resources
 from termcolor import colored
 
 import determined.deploy
-from determined.common.declarative_argparse import Arg, ArgGroup, Cmd
+from determined.cli.errors import CliError
+from determined.common.declarative_argparse import Arg, ArgGroup, Cmd, Group
 from determined.deploy.errors import MasterTimeoutExpired
 from determined.deploy.gcp import constants, gcp
 
@@ -36,6 +37,21 @@ def deploy_gcp(command: str, args: argparse.Namespace) -> None:
         os.makedirs(args.local_state_path)
     os.chdir(args.local_state_path)
 
+    # Set default tf state gcs bucket as '$PROJECT_NAME-determined-deploy` if local tf
+    # state doesn't exist and user has not provided a gcs bucket.
+    if (
+        (not os.path.exists(os.path.join(args.local_state_path, "terraform.tfstate")))
+        and hasattr(args, "tf_state_gcs_bucket_name")
+        and not args.tf_state_gcs_bucket_name
+    ):
+        # if user has provided credentials using --keypath, use them
+        if hasattr(args, "keypath") and args.keypath:
+            gcp.check_or_create_gcsbucket(args.project_id, args.keypath)
+        else:
+            gcp.check_or_create_gcsbucket(args.project_id)
+
+        args.tf_state_gcs_bucket_name = args.project_id + "-determined-deploy"
+
     # tf_state_gcs_bucket_name argument is not necessary for `down` operation, because TF reads it
     # from local tf files.
     if hasattr(args, "tf_state_gcs_bucket_name") and args.tf_state_gcs_bucket_name:
@@ -54,8 +70,82 @@ def deploy_gcp(command: str, args: argparse.Namespace) -> None:
         if args_dict[arg] is not None:
             det_configs[arg] = args_dict[arg]
 
+    # Not all args will be passed to Terraform, list the ones that won't be
+    # TODO(ilia): Switch to filtering variables_to_include instead, i.e.
+    #             only pass the ones recognized by terraform.
+    variables_to_exclude = [
+        "command",
+        "dry_run",
+        "environment",
+        "local_state_path",
+        "master",
+        "user",
+        "no_preflight_checks",
+        "no_wait_for_master",
+        "yes",
+        "no_prompt",
+        "master_config_template_path",
+        "tf_state_gcs_bucket_name",
+        "func",
+        "add_label",
+        "_command",
+        "_subcommand",
+        "_subsubcommand",
+    ]
+
     # Handle down subcommand.
-    if command == "down":
+    if command == "down" and args.cluster_id:
+        if not args.project_id:
+            raise CliError(
+                "Error: --project-id not provided. Please provide both project id"
+                + " and cluster id to delete the cluster."
+            )
+
+        # TODO: Find a way to get config defaults using CLI Parser Pipeline.
+        det_configs = {
+            "no_preflight_checks": False,
+            "no_wait_for_master": False,
+            "image_repo_prefix": "determinedai",
+            "cluster_id": args.cluster_id,
+            "project_id": args.project_id,
+            "network": "det-default",
+            "filestore_address": "",
+            "no_filestore": False,
+            "det_version": determined.__version__,
+            "region": constants.defaults.REGION,
+            "disk_size": constants.defaults.BOOT_DISK_SIZE,
+            "disk_type": constants.defaults.BOOT_DISK_TYPE,
+            "environment_image": constants.defaults.ENVIRONMENT_IMAGE,
+            "preemptible": False,
+            "operation_timeout_period": constants.defaults.OPERATION_TIMEOUT_PERIOD,
+            "master_instance_type": constants.defaults.MASTER_INSTANCE_TYPE,
+            "compute_agent_instance_type": constants.defaults.COMPUTE_AGENT_INSTANCE_TYPE,
+            "aux_agent_instance_type": constants.defaults.AUX_AGENT_INSTANCE_TYPE,
+            "db_password": constants.defaults.AUX_AGENT_INSTANCE_TYPE,
+            "max_aux_containers_per_agent": constants.defaults.MAX_AUX_CONTAINERS_PER_AGENT,
+            "max_idle_agent_period": constants.defaults.MAX_IDLE_AGENT_PERIOD,
+            "max_agent_starting_period": constants.defaults.MAX_AGENT_STARTING_PERIOD,
+            "port": constants.defaults.PORT,
+            "gpu_type": constants.defaults.GPU_TYPE,
+            "gpu_num": constants.defaults.GPU_NUM,
+            "min_dynamic_agents": constants.defaults.MIN_DYNAMIC_AGENTS,
+            "max_dynamic_agents": constants.defaults.MAX_DYNAMIC_AGENTS,
+            "min_cpu_platform_master": constants.defaults.MIN_CPU_PLATFORM_MASTER,
+            "min_cpu_platform_agent": constants.defaults.MIN_CPU_PLATFORM_AGENT,
+            "scheduler_type": constants.defaults.SCHEDULER_TYPE,
+            "preemption_enabled": constants.defaults.PREEMPTION_ENABLED,
+            "cpu_env_image": "",
+            "gpu_env_image": "",
+            "labels": {},
+            "local_state_path": os.getcwd(),
+            "tf_state_gcs_bucket_name": args.project_id + "-determined-deploy",
+        }
+
+        gcp.dry_run(det_configs, env, variables_to_exclude)
+        gcp.delete(det_configs, env, args.yes)
+        print("Delete Successful")
+        return
+    elif command == "down":
         gcp.delete(det_configs, env, args.yes)
         print("Delete Successful")
         return
@@ -85,29 +175,6 @@ def deploy_gcp(command: str, args: argparse.Namespace) -> None:
             )
         with args.master_config_template_path.open("r") as fin:
             det_configs["master_config_template"] = fin.read()
-
-    # Not all args will be passed to Terraform, list the ones that won't be
-    # TODO(ilia): Switch to filtering variables_to_include instead, i.e.
-    #             only pass the ones recognized by terraform.
-    variables_to_exclude = [
-        "command",
-        "dry_run",
-        "environment",
-        "local_state_path",
-        "master",
-        "user",
-        "no_preflight_checks",
-        "no_wait_for_master",
-        "yes",
-        "no_prompt",
-        "master_config_template_path",
-        "tf_state_gcs_bucket_name",
-        "func",
-        "add_label",
-        "_command",
-        "_subcommand",
-        "_subsubcommand",
-    ]
 
     # Dry-run flag
     if args.dry_run:
@@ -139,6 +206,20 @@ def deploy_gcp(command: str, args: argparse.Namespace) -> None:
 
     if args.no_wait_for_master:
         print("Please allow 1-5 minutes for the master instance to be accessible via the web-ui\n")
+
+
+def handle_list(args: argparse.Namespace) -> Any:
+    if hasattr(args, "tf_state_gcs_bucket_name") and args.tf_state_gcs_bucket_name:
+        bucket_name = args.tf_state_gcs_bucket_name
+    else:
+        bucket_name = args.project_id + "-determined-deploy"
+
+    if args.json:
+        return gcp.list_clusters(bucket_name, "json")
+    elif args.yaml:
+        return gcp.list_clusters(bucket_name, "yaml")
+    else:
+        return gcp.list_clusters(bucket_name)
 
 
 def handle_down(args: argparse.Namespace) -> None:
@@ -177,6 +258,30 @@ args_description = Cmd(
     "GCP help",
     [
         Cmd(
+            "list",
+            handle_list,
+            "list gcp cluster",
+            [
+                Arg(
+                    "--project-id",
+                    type=str,
+                    default=None,
+                    required=True,
+                    help="project id to list clusters from",
+                ),
+                Arg(
+                    "--tf-state-gcs-bucket-name",
+                    type=str,
+                    help="use a particular GCS bucket to retreive clusters "
+                    "instead of the default GCS bucket",
+                ),
+                Group(
+                    Arg("--json", action="store_true", help="print as CSV"),
+                    Arg("--yaml", action="store_true", help="print as JSON"),
+                ),
+            ],
+        ),
+        Cmd(
             "down",
             handle_down,
             "delete gcp cluster",
@@ -185,6 +290,16 @@ args_description = Cmd(
                     "optional named arguments",
                     None,
                     [
+                        Arg(
+                            "--cluster-id",
+                            type=str,
+                            help="cluster id of the cluster to be deleted",
+                        ),
+                        Arg(
+                            "--project-id",
+                            type=str,
+                            help="project id that the cluster belongs to",
+                        ),
                         Arg(
                             "--local-state-path",
                             type=str,
@@ -227,7 +342,7 @@ args_description = Cmd(
                             type=str,
                             default=None,
                             required=True,
-                            help="project ID to create the cluster in",
+                            help="project id to create the cluster in",
                         ),
                     ],
                 ),
