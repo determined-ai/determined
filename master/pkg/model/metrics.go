760312e51bc1c56fe93c2f8188e649fef748c1a7
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-06-15 12:25:15 -0700

    sanitize metrictype

--- master/pkg/model/metrics.go
+++ master/pkg/model/metrics.go
@@ -1,6 +1,12 @@
 package model
 
-import "github.com/determined-ai/determined/proto/pkg/apiv1"
+import (
+	"strings"
+
+	"github.com/determined-ai/determined/proto/pkg/apiv1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
 
 const (
 	// ValidationMetricType designates metrics from validation runs.
@@ -27,4 +33,14 @@ func (t MetricType) ToProto() apiv1.MetricType {
 	default:
 		return apiv1.MetricType_METRIC_TYPE_UNSPECIFIED
 	}
+}
+
+func (t MetricType) Validate() error {
+	if len(t) == 0 {
+		return status.Errorf(codes.InvalidArgument, "metric type cannot be empty")
+	}
+	if strings.Contains(t.ToString(), ".") {
+		return status.Errorf(codes.InvalidArgument, "metric type cannot contain '.'")
+	}
+	return nil
 }
