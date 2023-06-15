7eaa763de172b1392e826c507fb473680982584b
Anda <anda@determined.ai>
2023-06-15 09:23:37 -0700

    fix test

--- harness/tests/common/api_server.py
+++ harness/tests/common/api_server.py
@@ -103,7 +103,7 @@ def run_api_server(
             """
             key = "get_experiment_longrunning_n_calls"
             n_calls = 2
-            with open(FIXTURES_DIR / "experiment_longrunning.json") as f:
+            with open(FIXTURES_DIR / "experiment.json") as f:
                 sample_experiment = bindings.v1GetExperimentResponse.from_json(json.load(f))
 
             with lock:
@@ -131,7 +131,7 @@ def run_api_server(
                 if state[key] <= fail_for:
                     self.send_error(504)
                     return {}
-            with open(FIXTURES_DIR / "experiment_flaky.json") as f:
+            with open(FIXTURES_DIR / "experiment.json") as f:
                 sample_experiment = bindings.v1GetExperimentResponse.from_json(json.load(f))
             return sample_experiment.to_json()
 
