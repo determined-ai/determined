8e9bb15943b71c17452991b8e0635b0743fb388a
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-06-16 12:02:58 -0700

    change make_url

--- harness/determined/common/api/request.py
+++ harness/determined/common/api/request.py
@@ -29,7 +29,10 @@ def parse_master_address(master_address: str) -> parse.ParseResult:
 
 def make_url(master_address: str, suffix: str) -> str:
     parsed = parse_master_address(master_address)
-    return parse.urljoin(parsed.geturl(), suffix)
+    master_url = parsed.geturl().rstrip("/")
+    suffix = suffix.lstrip("/")
+    separator = "/" if suffix or master_address.endswith("/") else ""
+    return "{}{}{}".format(master_url, separator, suffix)
 
 
 def maybe_upgrade_ws_scheme(master_address: str) -> str:
