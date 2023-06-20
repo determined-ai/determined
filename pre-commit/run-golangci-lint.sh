a53dbe4fd4dabde68f1b2c18a8386ed832048a2c
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-06-20 10:59:52 -0700

    bring over go pre-commit scripts from Bahjat/pre-commit-golang
    https://github.com/Bahjat/pre-commit-golang

---
+++ pre-commit/run-golangci-lint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+CONFIG_FILE="$1"
+
+if ! command -v golangci-lint 2>&1 /dev/null
+then
+    echo "golangci-lint could not be found"
+    exit
+fi
+
+ERRS=$(golangci-lint run --new-from-rev="$(git rev-parse HEAD)" "$CONFIG_FILE")
+
+if [ -n "${ERRS}" ]; then
+    echo "${ERRS}"
+    exit 1
+fi
+exit 0
