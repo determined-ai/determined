ba3876b5611cf6b640fb42c99ad64dbbcc23c59f
azhou-determined <83614683+azhou-determined@users.noreply.github.com>
2023-06-20 16:08:44 -0700

    chore: cache and reload in Python SDK [MLG-549]  (#6853)
    * chore: cache and reload in Python SDK

--- harness/determined/common/experimental/model.py
+++ harness/determined/common/experimental/model.py
@@ -61,6 +61,7 @@ class ModelVersion:
             self._session, body=req, modelName=self.model_name, modelVersionNum=self.model_version
         )
         self.name = name
+        self.reload()
 
     def set_notes(self, notes: str) -> None:
         """
@@ -74,6 +75,7 @@ class ModelVersion:
             self._session, body=req, modelName=self.model_name, modelVersionNum=self.model_version
         )
         self.notes = notes
+        self.reload()
 
     def delete(self) -> None:
         """
@@ -287,6 +289,8 @@ class Model:
         bindings.patch_PatchModel(self._session, body=req, modelName=self.name)
 
         self.metadata = updated_metadata
+        self.reload()
+        assert self.metadata is not None
 
     def remove_metadata(self, keys: List[str]) -> None:
         """
@@ -310,6 +314,8 @@ class Model:
         bindings.patch_PatchModel(self._session, body=req, modelName=self.name)
 
         self.metadata = updated_metadata
+        self.reload()
+        assert self.metadata is not None
 
     def move_to_workspace(self, workspace_name: str) -> None:
         req = bindings.v1PatchModel(workspaceName=workspace_name)
@@ -332,10 +338,12 @@ class Model:
         bindings.patch_PatchModel(self._session, body=req, modelName=self.name)
 
         self.labels = labels
+        self.reload()
 
     def set_description(self, description: str) -> None:
         req = bindings.v1PatchModel(description=description)
         bindings.patch_PatchModel(self._session, body=req, modelName=self.name)
+        self.reload()
 
         self.description = description
 
@@ -345,6 +353,7 @@ class Model:
         """
         bindings.post_ArchiveModel(self._session, modelName=self.name)
         self.archived = True
+        self.reload()
 
     def unarchive(self) -> None:
         """
@@ -352,6 +361,7 @@ class Model:
         """
         bindings.post_UnarchiveModel(self._session, modelName=self.name)
         self.archived = False
+        self.reload()
 
     def delete(self) -> None:
         """
