c5bd6294131b6f19a03aa2145677b249be999169
azhou-determined <83614683+azhou-determined@users.noreply.github.com>
2023-06-20 16:08:44 -0700

    chore: cache and reload in Python SDK [MLG-549]  (#6853)
    * chore: cache and reload in Python SDK

--- e2e_tests/tests/experiment/test_core.py
+++ e2e_tests/tests/experiment/test_core.py
@@ -212,6 +212,7 @@ def test_end_to_end_adaptive() -> None:
     db_check = d.get_checkpoint(checkpoint.uuid)
     # Make sure the checkpoint metadata is correct and correctly saved to the db.
     # Beginning with 0.18 the TrialController contributes a few items to the dict.
+    assert checkpoint.metadata
     assert checkpoint.metadata.get("testing") == "metadata"
     assert checkpoint.metadata.keys() == {
         "determined_version",
