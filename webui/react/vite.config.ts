074daa77caeb81b0a5c02d6e1cea02c12b88e6de
Anda <anda@determined.ai>
2023-06-20 16:15:16 -0700

    chore: bump version: 0.23.1-rc1 -> 0.23.1-rc2

--- webui/react/vite.config.ts
+++ webui/react/vite.config.ts
@@ -109,7 +109,7 @@ export default defineConfig(({ mode }) => ({
     'process.env.IS_DEV': JSON.stringify(mode === 'development'),
     'process.env.PUBLIC_URL': JSON.stringify((mode !== 'test' && publicUrl) || ''),
     'process.env.SERVER_ADDRESS': JSON.stringify(process.env.SERVER_ADDRESS),
-    'process.env.VERSION': '"0.23.1-rc1"',
+    'process.env.VERSION': '"0.23.1-rc2"',
   },
   optimizeDeps: {
     include: ['notebook'],
