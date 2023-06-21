85171b67fa452720c49ef9b0b62df2debe5d69c1
Wes Turner <wesley.turner@hpe.com>
2023-06-21 09:26:58 -0700

    Test: split checkpoint download test
    * Mock storage managers and test they're called in test_checkpoint
    
    * Move functional test of download to new `test_s3.py`

--- harness/tests/determined/common/experimental/test_checkpoint.py
+++ harness/tests/determined/common/experimental/test_checkpoint.py
@@ -1,14 +1,16 @@
 import os
+from typing import Dict, Optional, Union, cast
+from unittest import mock
 
-import boto3
-import moto
 import pytest
 
-from determined.common import api
+from determined.common import api, storage
 from determined.common.experimental import checkpoint
 from tests.fixtures import api_responses
 
 _MASTER = "http://localhost:8080"
+
+StorageConfig = Dict[str, Optional[Union[str, int]]]
 
 
 @pytest.fixture
@@ -22,39 +24,49 @@ def sample_checkpoint(standard_session: api.Session) -> checkpoint.Checkpoint:
     return checkpoint.Checkpoint._from_bindings(bindings_checkpoint, standard_session)
 
 
-# Typing directive can be removed when https://github.com/getmoto/moto/issues/4944 is resolved.
-@moto.mock_s3  # type: ignore
-def test_download_can_download_from_s3_directly(
-    sample_checkpoint: checkpoint.Checkpoint, tmp_path: os.PathLike
-) -> None:
-    """Tests one particular code path (of many existing) for downloading a checkpoint.
-
-    There are several options for checkpoint storage (with access keys, overridden URLs, etc.) this
-    test chooses a single, simple set of these options.
-
-    As stored on disk, checkpoints have a complex file structure. This test creates a vastly
-    simplified checkpoint consisting solely of a metadata.json file and tests that this file is
-    downloaded.
-    """
-    metadata_payload = "{'determined_version': '0.22.2-dev0'}"
+def storage_conf_of_checkpoint(
+    sample_checkpoint: checkpoint.Checkpoint,
+) -> StorageConfig:
     if sample_checkpoint.training is None or sample_checkpoint.training.experiment_config is None:
         raise ValueError(
             "Test depends on an existing experiment_config within the tested checkpoint."
         )
     storage_conf = sample_checkpoint.training.experiment_config["checkpoint_storage"]
-    storage_conf.update({"type": "s3", "secret_key": None, "endpoint_url": None, "prefix": None})
+    return cast(StorageConfig, storage_conf)
 
-    s3_client = boto3.client("s3")
-    s3_client.create_bucket(Bucket=storage_conf["bucket"])
-    s3_client.put_object(
-        Body=bytes(metadata_payload, "utf-8"),
-        Bucket=storage_conf["bucket"],
-        Key=f"{sample_checkpoint.uuid}/metadata.json",
-    )
 
-    sample_checkpoint.download(path=str(tmp_path), mode=checkpoint.DownloadMode.DIRECT)
+@mock.patch.object(storage.GCSStorageManager, "download")
+def test_download_calls_GCSStorageManager_download_in_direct_download_mode(
+    mock_download: mock.MagicMock,
+    sample_checkpoint: checkpoint.Checkpoint,
+    tmp_path: os.PathLike,
+) -> None:
+    # Patch to avoid making actual calls to GCS.
+    with mock.patch("google.cloud.storage.Client"):
+        storage_conf = storage_conf_of_checkpoint(sample_checkpoint)
+        storage_conf.update({"type": "gcs", "prefix": None})
+        del storage_conf["access_key"]
+        del storage_conf["secret_key"]
+        del storage_conf["endpoint_url"]
 
-    downloaded_metadata_path = os.path.join(tmp_path, "metadata.json")
-    assert os.path.exists(downloaded_metadata_path)
-    with open(downloaded_metadata_path, "r") as f:
-        assert f.read() == metadata_payload
+        sample_checkpoint.download(path=str(tmp_path), mode=checkpoint.DownloadMode.DIRECT)
+        assert mock_download.call_count == 1
+
+
+@mock.patch.object(storage.S3StorageManager, "download")
+def test_download_calls_S3StorageManager_download_in_direct_download_mode(
+    mock_download: mock.MagicMock,
+    sample_checkpoint: checkpoint.Checkpoint,
+    tmp_path: os.PathLike,
+) -> None:
+    # Patch boto methods to avoid making actual calls to S3.
+    with mock.patch(
+        "determined.common.storage.boto3_credential_manager.initialize_boto3_credential_providers"
+    ), mock.patch("boto3.resource"):
+        storage_conf = storage_conf_of_checkpoint(sample_checkpoint)
+        storage_conf.update(
+            {"type": "s3", "secret_key": None, "endpoint_url": None, "prefix": None}
+        )
+
+        sample_checkpoint.download(path=str(tmp_path), mode=checkpoint.DownloadMode.DIRECT)
+        assert mock_download.call_count == 1
