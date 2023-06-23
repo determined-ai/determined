e0b6120349a4378e2b434fa0aa033b17e1c6ccfa
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-06-23 12:07:33 -0700

    fmt

--- harness/tests/common/test_api.py
+++ harness/tests/common/test_api.py
@@ -1,15 +1,16 @@
-from typing import NamedTuple
+from typing import List, NamedTuple
 
 import pytest
 
 from determined.common.api import request
 
+Case = NamedTuple("Case", [("base", str), ("path", str), ("expected", str)])
 
-def gen_make_url_cases():
-    Case = NamedTuple("Case", [("base", str), ("path", str), ("expected", str)])
+
+def gen_make_url_cases() -> List[Case]:
     host = "http://localhost:8080"
     path = "api/v1/experiments"
-    cases = [
+    cases: List[Case] = [
         Case("http://localhost:8080/", "", f"{host}/"),
         Case("http://localhost:8080", "", f"{host}"),
         Case("http://localhost:8080", "/api/v1/experiments", f"{host}/{path}"),
@@ -27,5 +28,5 @@ def gen_make_url_cases():
 
 
 @pytest.mark.parametrize("base, path, expected", gen_make_url_cases())
-def test_make_url(base: str, path: str, expected: str):
+def test_make_url(base: str, path: str, expected: str) -> None:
     assert request.make_url(base, path) == expected, f"base: {base}, path: {path}"
