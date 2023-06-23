6906e51348fd920f70d7adb6eab42b6a6e3b4df0
Taylor Ritenour <taylor.ritenour@hpe.com>
2023-06-23 09:33:42 -0700

    wip, adding stuff to protobuf, supporting model_versions

--- master/static/migrations/20230620162256_trial_source_info.tx.down.sql
+++ master/static/migrations/20230620162256_trial_source_info.tx.down.sql
@@ -4,3 +4,5 @@ DROP TYPE public.trial_source_info_type;
 DROP INDEX IF EXISTS ix_trial_source_info_trial_id;
 DROP INDEX IF EXISTS ix_trial_source_info_checkpoint_uuid;
 DROP INDEX IF EXISTS ix_trial_source_info_source_trial_id;
+DROP INDEX IF EXISTS ix_trial_source_info_source_model_verion;
+DROP INDEX IF EXISTS ix_trial_source_info_source_model_version;
--- master/static/migrations/20230620162256_trial_source_info.tx.up.sql
+++ master/static/migrations/20230620162256_trial_source_info.tx.up.sql
@@ -3,6 +3,7 @@ CREATE TYPE public.trial_source_info_type AS ENUM (
     'FINE_TUNING'
 );
 
+-- Denotes a connection between a given trial and a checkpoint/source trial/model version
 CREATE TABLE public.trial_source_info (
     -- Inference/Fine Tuning trial
     trial_id int REFERENCES public.trials(id) ON DELETE CASCADE NOT NULL,
@@ -10,14 +11,27 @@ CREATE TABLE public.trial_source_info (
     checkpoint_uuid uuid REFERENCES public.checkpoints_v2(uuid) ON DELETE CASCADE NOT NULL,
     -- Original trial that created the checkpoint (may be null in some inference and fine tuning use cases)
     source_trial_id int REFERENCES public.trials(id) ON DELETE CASCADE NULL,
-    -- Source Trial's Model version
-    -- source_model_version_id int REFERENCES public.model_versions(id) ON DELETE CASCADE NULL,
+    -- Source Trial's `model_version` `id` field
+    source_model_version_id int,
+    -- Source Trial's `model_version` `version` field
+    source_model_version_version int, 
     -- Type of the `trial_source_info` (inference or fine tuning for now)
     trial_source_info_type trial_source_info_type NOT NULL,
+    -- User defined description text
     description text,
-    metadata jsonb
+    -- User defined metadata
+    metadata jsonb,
+
+    CONSTRAINT fk_model_versions FOREIGN KEY (source_model_version_id, source_model_version_version) REFERENCES public.model_versions (model_id, version),
+    -- `public.model_version` defines its primary key as the combination of these
+    -- two values. Make sure that either they are both present or both missing
+    CONSTRAINT check_model_version_valid CHECK (
+        (source_model_version_id IS NULL AND source_model_version_version IS NULL) OR
+        (source_model_version_id IS NOT NULL AND source_model_version_version IS NOT NULL)
+    )
 );
 
 CREATE INDEX ix_trial_source_info_trial_id ON public.trial_source_info USING btree (trial_id);
 CREATE INDEX ix_trial_source_info_checkpoint_uuid ON public.trial_source_info USING btree (checkpoint_uuid);
 CREATE INDEX ix_trial_source_info_source_trial_id ON public.trial_source_info USING btree (source_trial_id);
+CREATE INDEX ix_trial_source_info_source_model_version ON public.trial_source_info USING btree (source_model_version_id, source_model_version_version);
