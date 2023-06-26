56a6c33a5a6acf485634e64db226834eb3d88c5f
Eric <31023784+eecsliu@users.noreply.github.com>
2023-06-26 10:22:36 -0700

    chore: rp workspace mapping bind testing (#7067)

--- master/internal/db/postgres_rp_workspace_bindings_intg_test.go
+++ master/internal/db/postgres_rp_workspace_bindings_intg_test.go
@@ -7,16 +7,78 @@ import (
 	"context"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/determined-ai/determined/master/internal/config"
 	"github.com/determined-ai/determined/master/pkg/etc"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAddAndRemoveBindings(t *testing.T) {
+	ctx := context.Background()
+	pgDB := MustResolveTestPostgres(t)
+	MustMigrateTestPostgres(t, pgDB, MigrationsFromDB)
+
+	user := RequireMockUser(t, pgDB)
+	workspaceIDs, err := MockWorkspaces([]string{"test1", "test2", "test3", "test4"}, user.ID)
+	require.NoError(t, err, "failed creating workspaces: %t", err)
+	defer func() {
+		err = CleanupMockWorkspace(workspaceIDs)
+		if err != nil {
+			log.Errorf("error when cleaning up mock workspaces")
+		}
+	}()
+
+	var int32IDs []int32
+	for _, workspaceID := range workspaceIDs {
+		int32IDs = append(int32IDs, workspaceID)
+	}
 	// Test single insert/delete
 	// Test bulk insert/delete
-	return
+	testPoolName := "test pool"
+	testPool2Name := "test pool too"
+	err = AddRPWorkspaceBindings(ctx, []int32{int32IDs[0]}, testPoolName, []config.ResourcePoolConfig{
+		{PoolName: testPoolName}, {PoolName: testPool2Name},
+	})
+	require.NoError(t, err, "failed to add bindings: %t", err)
+
+	var values []RPWorkspaceBinding
+	count, err := Bun().NewSelect().Model(&values).ScanAndCount(ctx)
+	require.NoError(t, err, "error when scanning DB: %t", err)
+	require.Equal(t, 1, count, "expected 1 item in DB, found %d", count)
+	require.Equal(t, int(workspaceIDs[0]), values[0].WorkspaceID,
+		"expected workspaceID to be %d, but it is %d", workspaceIDs[0], values[0].WorkspaceID)
+	require.Equal(t, testPoolName, values[0].PoolName,
+		"expected pool name to be '%s', but got %s", testPoolName, values[0].PoolName)
+
+	err = RemoveRPWorkspaceBindings(ctx, []int32{int32IDs[0]}, testPoolName)
+	require.NoError(t, err, "failed to remove bindings: %t", err)
+
+	count, err = Bun().NewSelect().Model(&values).ScanAndCount(ctx)
+	require.NoError(t, err, "error when scanning DB: %t", err)
+	require.Equal(t, 0, count, "expected 0 items in DB, found %d", count)
+
+	err = AddRPWorkspaceBindings(ctx, int32IDs, testPool2Name, []config.ResourcePoolConfig{
+		{PoolName: testPoolName}, {PoolName: testPool2Name},
+	})
+	require.NoError(t, err, "failed to add bindings: %t", err)
+
+	count, err = Bun().NewSelect().Model(&values).Order("workspace_id ASC").ScanAndCount(ctx)
+	require.NoError(t, err, "error when scanning DB: %t", err)
+	require.Equal(t, 4, count, "expected 4 items in DB, found %d", count)
+	for i := 0; i < 4; i++ {
+		require.Equal(t, int(workspaceIDs[i]), values[i].WorkspaceID,
+			"expected workspaceID to be %d, but it is %d", i+1, values[i].WorkspaceID)
+		require.Equal(t, testPool2Name, values[i].PoolName,
+			"expected pool name to be '%s', but got %s", testPool2Name, values[i].PoolName)
+	}
+
+	err = RemoveRPWorkspaceBindings(ctx, int32IDs, testPool2Name)
+	require.NoError(t, err, "failed to remove bindings: %t", err)
+
+	count, err = Bun().NewSelect().Model(&values).ScanAndCount(ctx)
+	require.NoError(t, err, "error when scanning DB: %t", err)
+	require.Equal(t, 0, count, "expected 0 items in DB, found %d", count)
 }
 
 func TestBindingFail(t *testing.T) {
