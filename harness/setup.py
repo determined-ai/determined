680c13b850328f69342ecf02639a39a4f2477f5d
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2023-06-30 16:45:19 -0400

    chore: bump version: 0.23.2-rc1 -> 0.23.2-rc2

--- harness/setup.py
+++ harness/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="determined",
-    version="0.23.2-rc1",
+    version="0.23.2-rc2",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
