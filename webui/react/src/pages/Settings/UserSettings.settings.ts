ac6ff1fd25ef867c42475f8b9c24c74b3e7adae5
johnkim-det <97752292+johnkim-det@users.noreply.github.com>
2023-06-30 20:05:27 -0700

    feat: keyboard shortcut for JupyterLabModal (#7184)

--- webui/react/src/pages/Settings/UserSettings.settings.ts
+++ webui/react/src/pages/Settings/UserSettings.settings.ts
@@ -3,11 +3,24 @@ import { KeyboardShortcut } from 'utils/shortcut';
 
 export interface Settings {
   navbarCollapsed: KeyboardShortcut;
+  jupyterLab: KeyboardShortcut;
   omnibar: KeyboardShortcut;
 }
 
 const shortCutSettingsConfig: SettingsConfig<Settings> = {
   settings: {
+    jupyterLab: {
+      defaultValue: {
+        alt: false,
+        ctrl: false,
+        key: 'L',
+        meta: true,
+        shift: true,
+      },
+      skipUrlEncoding: true,
+      storageKey: 'jupyterLab',
+      type: KeyboardShortcut,
+    },
     navbarCollapsed: {
       defaultValue: {
         alt: false,
