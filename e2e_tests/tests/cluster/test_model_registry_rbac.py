6e23aa220fb46683c4dd0958325821322c42a6fc
Corban Beaird <corban.beaird@hpe.com>
2023-07-03 09:29:48 -0600

    chore: removed admin dependency from delete model/version (#912)
    * chore: add custom prefix to PermissionDeniedError
    
    [e2e_tests changes only]

--- e2e_tests/tests/cluster/test_model_registry_rbac.py
+++ e2e_tests/tests/cluster/test_model_registry_rbac.py
@@ -1,15 +1,19 @@
 import uuid
-from typing import Tuple
+from typing import Any, Dict, List, Tuple
 
 import pytest
 
+from determined import cli
 from determined.common import yaml
-from determined.common.api import bindings, errors
+from determined.common.api import Session, authentication, bindings, errors
+from determined.common.api.bindings import experimentv1State
 from determined.common.experimental import model
 from determined.experimental import Checkpoint, Determined
 from determined.experimental import client as _client
 from tests import api_utils
 from tests import config as conf
+from tests import experiment, utils
+from tests.cluster.test_rbac import create_workspaces_with_users
 from tests.cluster.test_users import ADMIN_CREDENTIALS, log_in_user_cli, logged_in_user
 
 from .test_groups import det_cmd
@@ -79,6 +83,44 @@ def user_with_view_perms_test(
     with pytest.raises(errors.ForbiddenException) as e:
         determined_obj.create_model(name=get_random_string(), workspace_name=workspace_name)
     assert "access denied" in str(e.value)
+
+
+def create_model_registry(session: Session, model_name: str, workspace_id: int) -> model.Model:
+    resp = bindings.post_PostModel(
+        session,
+        body=bindings.v1PostModelRequest(name=model_name, workspaceId=workspace_id),
+    )
+    assert resp.model is not None
+    return model.Model._from_bindings(resp.model, session)
+
+
+def register_model_version(
+    creds: authentication.Credentials, model_name: str, workspace_id: int
+) -> Tuple[model.Model, model.ModelVersion]:
+    m = None
+    model_version = None
+    session = api_utils.determined_test_session(creds)
+    with logged_in_user(creds):
+        pid = bindings.post_PostProject(
+            session,
+            body=bindings.v1PostProjectRequest(name=get_random_string(), workspaceId=workspace_id),
+            workspaceId=workspace_id,
+        ).project.id
+        m = create_model_registry(session, model_name, workspace_id)
+        experiment_id = experiment.create_experiment(
+            conf.fixtures_path("no_op/single.yaml"),
+            conf.fixtures_path("no_op"),
+            ["--project_id", str(pid)],
+        )
+        experiment.wait_for_experiment_state(
+            experiment_id, experimentv1State.COMPLETED, credentials=creds
+        )
+        checkpoint = bindings.get_GetExperimentCheckpoints(
+            id=experiment_id, session=session
+        ).checkpoints[0]
+        model_version = m.register_version(checkpoint.uuid)
+        assert model_version.model_version == 1
+    return m, model_version
 
 
 @pytest.mark.test_model_registry_rbac
@@ -288,3 +330,157 @@ def test_model_registry_rbac() -> None:
                 model.move_to_workspace(workspace_name=test_workspace.name)
             assert "access denied" in str(e.value)
             model.delete()
+
+
+@pytest.mark.test_model_registry_rbac
+def test_model_rbac_deletes() -> None:
+    with create_workspaces_with_users(
+        [
+            [
+                (0, ["Editor"]),
+            ],
+        ]
+    ) as (workspaces, creds):
+        workspace_id = workspaces[0].id
+        # create non-cluster admin user
+        editor_creds = creds[0]
+        editor_session = api_utils.determined_test_session(editor_creds)
+
+        # create cluster admin user
+        cluster_admin_creds = api_utils.create_test_user(
+            add_password=True,
+            user=bindings.v1User(username=get_random_string(), active=True, admin=False),
+        )
+        api_utils.configure_token_store(ADMIN_CREDENTIALS)
+        cli.rbac.assign_role(
+            utils.CliArgsMock(
+                username_to_assign=cluster_admin_creds.username,
+                role_name="ClusterAdmin",
+            )
+        )
+        cluster_admin_session = api_utils.determined_test_session(cluster_admin_creds)
+
+        # create non-cluster admin user with OSS admin flag
+        oss_admin_creds = api_utils.create_test_user(
+            add_password=True,
+            user=bindings.v1User(username=get_random_string(), active=True, admin=True),
+        )
+        oss_admin_session = api_utils.determined_test_session(oss_admin_creds)
+
+        model_num = 0
+        try:
+            # test deleting model registries
+            tests: List[Dict[str, Any]] = [
+                {
+                    "create_session": cluster_admin_session,
+                    "delete_session": cluster_admin_session,
+                    "should_error": False,
+                },
+                {
+                    "create_session": editor_session,
+                    "delete_session": cluster_admin_session,
+                    "should_error": False,
+                },
+                {
+                    "create_session": editor_session,
+                    "delete_session": editor_session,
+                    "should_error": False,
+                },
+                {
+                    "create_session": cluster_admin_session,
+                    "delete_session": editor_session,
+                    "should_error": True,
+                },
+                {
+                    "create_session": cluster_admin_session,
+                    "delete_session": oss_admin_session,
+                    "should_error": True,
+                },
+            ]
+            for t in tests:
+                create_session: Session = t["create_session"]
+                delete_session: Session = t["delete_session"]
+                should_error: bool = t["should_error"]
+
+                model_name = "model_" + str(model_num)
+                model_num += 1
+                create_model_registry(create_session, model_name, workspace_id)
+
+                if should_error:
+                    with pytest.raises(errors.ForbiddenException) as permErr:
+                        bindings.delete_DeleteModel(delete_session, modelName=model_name)
+                    assert "access denied" in str(permErr.value)
+                else:
+                    bindings.delete_DeleteModel(delete_session, modelName=model_name)
+                    with pytest.raises(errors.NotFoundException) as notFoundErr:
+                        bindings.get_GetModel(create_session, modelName=model_name)
+                    assert "not found" in str(notFoundErr.value)
+
+            # test deleting model versions
+            tests = [
+                {
+                    "create_creds": cluster_admin_creds,
+                    "delete_session": cluster_admin_session,
+                    "should_error": False,
+                },
+                {
+                    "create_creds": editor_creds,
+                    "delete_session": editor_session,
+                    "should_error": False,
+                },
+                {
+                    "create_creds": editor_creds,
+                    "delete_session": cluster_admin_session,
+                    "should_error": False,
+                },
+                {
+                    "create_creds": cluster_admin_creds,
+                    "delete_session": editor_session,
+                    "should_error": True,
+                },
+                {
+                    "create_creds": cluster_admin_creds,
+                    "delete_session": oss_admin_session,
+                    "should_error": True,
+                },
+            ]
+
+            for t in tests:
+                create_creds: authentication.Credentials = t["create_creds"]
+                delete_session = t["delete_session"]
+                should_error = t["should_error"]
+
+                model_name = "model_" + str(model_num)
+                model_num += 1
+                m, ca_model_version = register_model_version(
+                    creds=create_creds, model_name=model_name, workspace_id=workspace_id
+                )
+                model_version_num = ca_model_version.model_version
+
+                if should_error:
+                    with pytest.raises(errors.ForbiddenException) as permErr:
+                        bindings.delete_DeleteModelVersion(
+                            delete_session,
+                            modelName=model_name,
+                            modelVersionNum=model_version_num,
+                        )
+                    assert "access denied" in str(permErr.value)
+                else:
+                    bindings.delete_DeleteModelVersion(
+                        delete_session, modelName=model_name, modelVersionNum=model_version_num
+                    )
+                    with pytest.raises(errors.NotFoundException) as notFoundErr:
+                        bindings.get_GetModelVersion(
+                            api_utils.determined_test_session(create_creds),
+                            modelName=model_name,
+                            modelVersionNum=model_version_num,
+                        )
+                    assert "not found" in str(notFoundErr.value)
+        finally:
+            for i in range(model_num):
+                admin_session = api_utils.determined_test_session(ADMIN_CREDENTIALS)
+                try:
+                    bindings.delete_DeleteModel(admin_session, modelName="model_" + str(i))
+                # model is has already been cleaned up
+                except errors.NotFoundException:
+                    continue
