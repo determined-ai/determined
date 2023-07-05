5ab55a1b593343520f557ef69cde62e45ced2137
azhou-determined <83614683+azhou-determined@users.noreply.github.com>
2023-07-05 13:15:31 -0700

    feat: add summary metrics to SDK and CLI [MLG-648] (#7255)
    * feat: add trial summary metrics for SDK and CLI

--- harness/tests/fixtures/api_responses.py
+++ harness/tests/fixtures/api_responses.py
@@ -44,6 +44,14 @@ def sample_get_experiment_trials() -> bindings.v1GetExperimentTrialsResponse:
         return resp
 
 
+def sample_get_trial(**kwargs: Any) -> bindings.v1GetTrialResponse:
+    with open(FIXTURES_DIR / "trial.json") as f:
+        resp = bindings.v1GetTrialResponse.from_json(json.load(f))
+        for k, v in kwargs.items():
+            setattr(resp.trial, k, v)
+        return resp
+
+
 def sample_get_model() -> bindings.v1GetModelResponse:
     """Get a sample model from a fixture."""
     with open(FIXTURES_DIR / "model.json") as f:
