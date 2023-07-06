6f6f5683443ce640c230cdf6471e477223f15639
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-07-06 14:58:56 -0700

    chore: golangci check the package instead (#7226)
    Co-authored-by: Erik Wilson <erik.wilson@hpe.com>

--- pre-commit/run-golangci-lint.sh
+++ pre-commit/run-golangci-lint.sh
@@ -1,16 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-CONFIG_FILE="$1"
-
-if ! command -v golangci-lint /dev/null 2>&1; then
-    echo "golangci-lint could not be found"
-    exit
-fi
-
-ERRS=$(golangci-lint run --new-from-rev="$(git rev-parse HEAD)" "$CONFIG_FILE")
-
-if [ -n "${ERRS}" ]; then
-    echo "${ERRS}"
+if ! command -v golangci-lint >/dev/null; then
+    echo "golangci-lint could not be found (try ./master/get-deps.sh)" >&2
     exit 1
 fi
-exit 0
+set -xeo pipefail
+
+CHANGED_DIRS=$(xargs -n1 dirname <<<"$*" | sort -u)
+
+golint() {
+    dirs=$(grep "^$1/" <<<"$CHANGED_DIRS" | sed -e "s|^$1/||g")
+    if [ -z "$dirs" ]; then
+        echo "No files to lint in $1, skipping."
+        return
+    fi
+    pushd $1
+    golangci-lint --build-tags integration run --timeout 30s $dirs
+    popd
+}
+
+golint master
+golint agent
