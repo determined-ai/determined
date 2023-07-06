5ed915c3943b28591f1f0f51dffd0d92595eacd0
Erik Wilson <erik.wilson@hpe.com>
2023-07-06 11:05:26 -0700

    chore: fix golangci-lint pre-commit hook

--- pre-commit/run-golangci-lint.sh
+++ pre-commit/run-golangci-lint.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-CHANGES="$*"
-
 if ! command -v golangci-lint >/dev/null; then
-    echo "golangci-lint could not be found"
-    exit
+    echo "golangci-lint could not be found (try ./master/get-deps.sh)" >&2
+    exit 1
 fi
+set -xeo pipefail
 
-CHANGED_DIRS=$(echo "$CHANGES" | xargs -n1 dirname | sort -u)
+CHANGED_DIRS=$(xargs -n1 dirname <<<"$*" | sort -u)
 
-for DIR in $CHANGED_DIRS; do
-    golangci-lint --build-tags integration run --timeout 10s "$DIR"
-done
+filter() {
+    grep "$1" <<<"$2" | sed -e "s|$1||g"
+}
 
-exit 0
+golint() {
+    dirs=$(filter "^$1/" "$CHANGED_DIRS")
+    if [ -z "$dirs" ]; then
+        echo "No files to lint in $1, skipping."
+        return
+    fi
+    pushd $1
+    golangci-lint --build-tags integration run --timeout 30s $dirs
+    popd
+}
+
+golint master
+golint agent
