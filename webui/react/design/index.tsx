c64a41bc5238058be53de8f9f25a3acc08603178
Ashton Galloway <ashton.galloway@hpe.com>
2023-07-07 12:47:23 -0400

    fix: ensure design kit landing page is typechecked
    This ensures the design kit landing page is included in typechecking.

--- webui/react/design/index.tsx
+++ webui/react/design/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Map } from 'immutable';
 import { observable } from 'micro-observables';
 import { createRoot } from 'react-dom/client';
@@ -6,43 +5,44 @@ import { HelmetProvider } from 'react-helmet-async';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'uplot/dist/uPlot.min.css';
 
+import { Loaded } from 'utils/loadable';
+
 import css from '../src/App.module.scss';
 import { ConfirmationProvider } from '../src/components/kit/useConfirm';
 import ThemeProvider from '../src/components/ThemeProvider';
 import { Settings, UserSettings } from '../src/hooks/useSettingsProvider';
 import DesignKit from '../src/pages/DesignKit';
 import { StoreProvider as UIProvider } from '../src/stores/contexts/UI';
-import { Loaded } from '../src/utils/loadable';
 
 import 'antd/dist/reset.css';
 
 const fakeSettingsContext = {
   clearQuerySettings: () => undefined,
   isLoading: false,
-  querySettings: '',
+  querySettings: new URLSearchParams(),
   state: observable(Loaded(Map<string, Settings>())),
 };
 
 const router = createBrowserRouter([
   {
-    path: "*",
-    element: <HelmetProvider>
-      <UIProvider>
-        <UserSettings.Provider value={fakeSettingsContext}>
-          <ThemeProvider>
-            <ConfirmationProvider>
-              <div className={css.base}>
-                <DesignKit />
-              </div>
-            </ConfirmationProvider>
-          </ThemeProvider>
-        </UserSettings.Provider>
-      </UIProvider>
-    </HelmetProvider>,
+    element: (
+      <HelmetProvider>
+        <UIProvider>
+          <UserSettings.Provider value={fakeSettingsContext}>
+            <ThemeProvider>
+              <ConfirmationProvider>
+                <div className={css.base}>
+                  <DesignKit />
+                </div>
+              </ConfirmationProvider>
+            </ThemeProvider>
+          </UserSettings.Provider>
+        </UIProvider>
+      </HelmetProvider>
+    ),
+    path: '*',
   },
 ]);
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
-);
+createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
