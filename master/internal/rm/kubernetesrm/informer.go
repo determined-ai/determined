196705774e3b102f22009de756f367ed174e1610
Carolina Calderon <carolina.calderon@hpe.com>
2023-07-12 10:05:06 -0400

    chore: k8s pod & node informer refactor

--- master/internal/rm/kubernetesrm/informer.go
+++ master/internal/rm/kubernetesrm/informer.go
@@ -2,107 +2,79 @@ package kubernetesrm
 
 import (
 	"context"
-	"time"
 
-	"k8s.io/apimachinery/pkg/watch"
-	"k8s.io/client-go/tools/cache"
-	watchtools "k8s.io/client-go/tools/watch"
-
-	"github.com/determined-ai/determined/master/pkg/actor/actors"
-
+	"github.com/sirupsen/logrus"
 	k8sV1 "k8s.io/api/core/v1"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
 	typedV1 "k8s.io/client-go/kubernetes/typed/core/v1"
-
-	"github.com/determined-ai/determined/master/pkg/actor"
+	"k8s.io/client-go/tools/cache"
+	watchtools "k8s.io/client-go/tools/watch"
 )
 
-const defaultInformerBackoff = 5 * time.Second
-
-// messages that are sent to the informer.
-type (
-	startInformer struct{}
-)
-
-// messages that are sent by the informer.
-type (
-	podStatusUpdate struct {
-		updatedPod *k8sV1.Pod
-	}
-)
+type podCallbackFunc func(*k8sV1.Pod)
 
 type informer struct {
-	podInterface typedV1.PodInterface
-	podsHandler  *actor.Ref
+	cb         podCallbackFunc
+	syslog     *logrus.Entry
+	resultChan <-chan watch.Event
 }
 
 func newInformer(
+	ctx context.Context,
+	namespace string,
 	podInterface typedV1.PodInterface,
-	podsHandler *actor.Ref,
-) *informer {
-	return &informer{
-		podInterface: podInterface,
-		podsHandler:  podsHandler,
-	}
-}
-
-// Receive implements the actor interface.
-func (i *informer) Receive(ctx *actor.Context) error {
-	switch msg := ctx.Message().(type) {
-	case actor.PreStart:
-		ctx.Tell(ctx.Self(), startInformer{})
-
-	case startInformer:
-		i.startInformer(ctx)
-
-	case actor.PostStop:
-
-	default:
-		ctx.Log().Errorf("unexpected message %T", msg)
-		return actor.ErrUnexpectedMessage(ctx)
-	}
-
-	return nil
-}
-
-func (i *informer) startInformer(ctx *actor.Context) {
-	pods, err := i.podInterface.List(
-		context.TODO(), metaV1.ListOptions{LabelSelector: determinedLabel})
+	cb podCallbackFunc,
+) (*informer, error) {
+	pods, err := podInterface.List(ctx, metaV1.ListOptions{LabelSelector: determinedLabel})
 	if err != nil {
-		ctx.Log().WithError(err).Warnf("error retrieving internal resource version")
-		actors.NotifyAfter(ctx, defaultInformerBackoff, startInformer{})
-		return
+		return nil, err
 	}
 
 	rw, err := watchtools.NewRetryWatcher(pods.ResourceVersion, &cache.ListWatch{
 		WatchFunc: func(options metaV1.ListOptions) (watch.Interface, error) {
-			return i.podInterface.Watch(
-				context.TODO(), metaV1.ListOptions{LabelSelector: determinedLabel})
+			options.LabelSelector = determinedLabel
+			return podInterface.Watch(ctx, options)
 		},
 	})
 	if err != nil {
-		ctx.Log().WithError(err).Warnf("error initializing pod retry watcher")
-		actors.NotifyAfter(ctx, defaultInformerBackoff, startInformer{})
-		return
+		return nil, err
 	}
 
-	ctx.Log().Info("pod informer is starting")
-	for event := range rw.ResultChan() {
+	// Log when pods are first added to the informer (at start-up).
+	syslog := logrus.WithFields(logrus.Fields{
+		"component": "podInformer",
+		"namespace": namespace,
+	})
+	for i := range pods.Items {
+		syslog.Debugf("informer added pod: %s", pods.Items[i].Name)
+		cb(&pods.Items[i])
+	}
+
+	return &informer{
+		cb:         cb,
+		syslog:     syslog,
+		resultChan: rw.ResultChan(),
+	}, nil
+}
+
+// startInformer returns the updated pod, if any.
+func (i *informer) run() {
+	i.syslog.Debug("pod informer is starting")
+	for event := range i.resultChan {
 		if event.Type == watch.Error {
-			ctx.Log().Warnf("pod informer emitted error %+v", event)
+			i.syslog.Warnf("pod informer emitted error %+v", event)
 			continue
 		}
 
 		pod, ok := event.Object.(*k8sV1.Pod)
 		if !ok {
-			ctx.Log().Warnf("error converting event of type %T to *k8sV1.Pod: %+v", event, event)
+			i.syslog.Warnf("error converting event of type %T to *k8sV1.Pod: %+v", event, event)
 			continue
 		}
 
-		ctx.Log().Debugf("informer got new pod event for pod: %s %s", pod.Name, pod.Status.Phase)
-		ctx.Tell(i.podsHandler, podStatusUpdate{updatedPod: pod})
+		i.syslog.Debugf("informer got new pod event for pod: %s %s", pod.Name, pod.Status.Phase)
+		i.cb(pod)
 	}
-
-	ctx.Log().Warn("pod informer stopped unexpectedly")
-	ctx.Tell(ctx.Self(), startInformer{})
+	panic("pod informer stopped unexpectedly")
 }
