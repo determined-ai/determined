84b3528c5cc2622cb15c6f19207b910a3f049194
Corey Staten <corey.staten@hpe.com>
2023-07-13 10:09:41 -0400

    Fix NUM_CLASSES reference.

--- examples/computer_vision/cifar10_tf_keras/cifar_model.py
+++ examples/computer_vision/cifar10_tf_keras/cifar_model.py
@@ -5,7 +5,6 @@ https://github.com/fchollet/keras/blob/master/examples/cifar10_cnn.py.
 
 import numpy as np
 import tensorflow as tf
-from data import NUM_CLASSES
 from packaging import version
 from tensorflow.keras.layers import Activation, Conv2D, Dense, Dropout, Flatten, MaxPooling2D
 from tensorflow.keras.losses import categorical_crossentropy
@@ -22,6 +21,7 @@ else:
 # Constants about the data set.
 IMAGE_SIZE = 32
 NUM_CHANNELS = 3
+NUM_CLASSES = 10
 
 
 def categorical_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
@@ -56,7 +56,7 @@ def build_model(layer1_dropout, layer2_dropout, layer3_dropout):
 
 
 def build_optimizer(learning_rate, learning_rate_decay):
-    return RMSprop(lr=learning_rate, decay=learning_rate_decay)
+    return RMSprop(learning_rate=learning_rate, decay=learning_rate_decay)
 
 
 def compile_model(model, optimizer):
