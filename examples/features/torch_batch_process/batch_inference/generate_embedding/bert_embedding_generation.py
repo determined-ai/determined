d62e1227ec1584379df3fe6f6828f8df52f2557c
Sze Wai Yuen <sze-wai.yuen@hpe.com>
2023-07-14 08:38:28 -0700

    format

--- examples/features/torch_batch_process/batch_inference/generate_embedding/bert_embedding_generation.py
+++ examples/features/torch_batch_process/batch_inference/generate_embedding/bert_embedding_generation.py
@@ -1,15 +1,16 @@
-import pinecone
+import logging
+import os
+import pathlib
+
+import chromadb
 import torch
 from datasets import load_dataset
 from transformers import BertModel, BertTokenizer
 
-from determined.pytorch.experimental import TorchBatchProcessor, torch_batch_process
-
-PINCONE_ENV = "<YOUR_PINECONE_ENV>"
-API_KEY = "<YOUR_PINECONE_API_KEY>"
+from determined.pytorch import experimental
 
 
-class BertEmbeddingProcessor(TorchBatchProcessor):
+class EmbeddingProcessor(experimental.TorchBatchProcessor):
     def __init__(self, context):
         self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
         self.model = BertModel.from_pretrained("bert-base-uncased", output_hidden_states=True)
@@ -21,8 +22,11 @@ class BertEmbeddingProcessor(TorchBatchProcessor):
         self.output = []
 
         self.context = context
+        self.last_index = 0
         self.rank = self.context.distributed.get_rank()
-        self.index = pinecone.Index("bert-embedding-example")
+
+        self.output_dir = "/tmp/data/embeddings"
+        os.makedirs(self.output_dir, exist_ok=True)
 
     def process_batch(self, batch, batch_idx) -> None:
         with torch.no_grad():
@@ -41,34 +45,46 @@ class BertEmbeddingProcessor(TorchBatchProcessor):
             outputs = self.model(inputs, masks)
             outputs = torch.mean(outputs["hidden_states"][-1], dim=1)
 
-            self.output.append({"embeddings": outputs, "id": batch["_id"]})
+            self.output.append({"embeddings": outputs, "id": batch["_id"], "text": batch["text"]})
+            self.last_index = batch_idx
 
     def on_checkpoint_start(self):
         if len(self.output) == 0:
             return
-        vector = []
-        for batch in self.output:
-            records = zip(batch["embeddings"], batch["id"])
-            for record in records:
-                embeddings = record[0].tolist()
-                id = record[1]
-                vector.append(
-                    (
-                        id,  # Vector ID
-                        embeddings,  # Dense vector values
-                    )
-                )
-
-        self.index.upsert(vectors=vector, namespace="scidocs")
-
+        file_name = f"bert_embedding_worker_{self.rank}_end_batch_{self.last_index}"
+        file_path = pathlib.Path(self.output_dir, file_name)
+        torch.save(self.output, file_path)
         self.output = []
+
+    def on_finish(self):
+        if self.rank == 0:
+            chroma_dir = "/tmp/chroma"
+            os.makedirs(chroma_dir, exist_ok=True)
+            chroma_client = chromadb.Client(chromadb.config.Settings(persist_directory=chroma_dir))
+            collection = chroma_client.create_collection(name="scidocs_embedding")
+
+            embeddings = []
+            documents = []
+            ids = []
+
+            for file in os.listdir(self.output_dir):
+                file_path = pathlib.Path(self.output_dir, file)
+                batches = torch.load(file_path)
+                for batch in batches:
+                    for embedding in batch["embeddings"]:
+                        embedding = embedding.tolist()
+                        embeddings.append(embedding)
+                    ids += batch["id"]
+                    documents += batch["text"]
+
+            collection.add(embeddings=embeddings, documents=documents, ids=ids)
+            logging.info(f"Embedding contains {collection.count()} entries")
 
 
 if __name__ == "__main__":
-    pinecone.init(api_key=API_KEY, environment=PINCONE_ENV)
     dataset = load_dataset("BeIR/scidocs", "corpus", split="corpus")
-    torch_batch_process(
-        BertEmbeddingProcessor,
+    experimental.torch_batch_process(
+        EmbeddingProcessor,
         dataset,
         batch_size=64,
         checkpoint_interval=10,
