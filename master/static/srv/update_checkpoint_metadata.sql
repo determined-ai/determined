ee68c3d479637f4a8fe97e3fce3fc7de845ae8de
NicholasBlaskey <nick.blaskey@hpe.com>
2023-07-19 12:01:40 -0400

    perf: migrate checkpoint v1 into v2 table (#7325)

--- master/static/srv/update_checkpoint_metadata.sql
+++ master/static/srv/update_checkpoint_metadata.sql
@@ -1,16 +1,3 @@
-WITH update_v1 AS (
-    UPDATE raw_checkpoints SET metadata = $2
-    WHERE uuid = $1
-    RETURNING metadata
-), update_v2 AS (
-    UPDATE checkpoints_v2 SET metadata = $2
-    WHERE uuid = $1
-    RETURNING metadata
-)
-SELECT metadata
-FROM update_v1
-WHERE metadata IS NOT NULL
-UNION ALL
-SELECT metadata
-FROM update_v2
-WHERE metadata IS NOT NULL
+UPDATE checkpoints_v2 SET metadata = $2
+WHERE uuid = $1
+RETURNING metadata
