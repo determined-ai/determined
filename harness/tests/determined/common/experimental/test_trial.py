0f2c35be80e49282cdd992dd0969ef0905760159
azhou-determined <83614683+azhou-determined@users.noreply.github.com>
2023-07-19 16:42:27 -0700

    chore: support microseconds in SDK log filter timestamps (#7425)
    SDK: trial.logs should support microsecond timestamps since protobuf does

--- harness/tests/determined/common/experimental/test_trial.py
+++ harness/tests/determined/common/experimental/test_trial.py
@@ -35,16 +35,8 @@ def test_trial_logs_converts_epoch_time(make_trialref: Callable[[int], trial.Tri
 
     timestamp_before = datetime.datetime.fromtimestamp(before_ts).isoformat("T") + "Z"
     timestamp_after = datetime.datetime.fromtimestamp(after_ts).isoformat("T") + "Z"
-    params = {
-        "timestampBefore": timestamp_before,
-        "timestampAfter": timestamp_after,
-    }
-    responses.get(
-        trial_logs_url,
-        stream=True,
-        body=trial_logs,
-        match=[responses.matchers.query_param_matcher(params, strict_match=False)],
-    )
+
+    responses.get(trial_logs_url, stream=True, body=trial_logs)
 
     list(trialref.logs(timestamp_before=before_ts, timestamp_after=after_ts))
 
@@ -70,3 +62,34 @@ def test_trial_logs_invalid_timestamps(
     with pytest.raises(ValueError):
         list(trialref.logs(timestamp_before=timestamp, timestamp_after=None))
         list(trialref.logs(timestamp_before=None, timestamp_after=timestamp))
+
+
+@pytest.mark.parametrize(
+    "name,valid_timestamp",
+    [
+        ("no_microseconds", "2021-10-26T23:17:12Z"),
+        ("microseconds", "2021-10-26T23:17:12.0000Z"),
+    ],
+)
+@responses.activate
+def test_trial_logs_accepts_valid_timestamps(
+    make_trialref: Callable[[int], trial.Trial], name: str, valid_timestamp: str
+) -> None:
+    trialref = make_trialref(1)
+    trial_logs = api_responses.sample_trial_logs()
+    trial_logs_url = f"{_MASTER}/api/v1/trials/{trialref.id}/logs"
+
+    responses.get(
+        trial_logs_url,
+        stream=True,
+        body=trial_logs,
+    )
+    list(trialref.logs(timestamp_before=valid_timestamp, timestamp_after=None))
+
+    responses.get(
+        trial_logs_url,
+        stream=True,
+        body=trial_logs,
+    )
+
+    list(trialref.logs(timestamp_before=None, timestamp_after=valid_timestamp))
