0da62be3f7dae0ffbfc2165c0c48e373f65173ef
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-07-21 12:31:38 -0700

    simplify cleanup

--- examples/tutorials/core_api/11_generic_metrics.py
+++ examples/tutorials/core_api/11_generic_metrics.py
@@ -8,7 +8,6 @@ import logging
 import random
 import time
 
-# NEW: import determined
 import determined as det
 from determined.common import util as det_util
 
@@ -21,12 +20,12 @@ metric_groups = [
 ]
 
 
-def main(core_context: det.core.Context, increment_by):
+def main(core_context: det.core.Context, increment_by: float):
     x = 0
-    steps_completed = 1
+    total_batches = 1
     for batch in range(100):
         x += increment_by
-        steps_completed = batch + 1
+        total_batches = batch + 1
         time.sleep(0.1)
         logging.info(f"x is now {x}")
         idx = batch % len(metric_groups)
@@ -34,21 +33,11 @@ def main(core_context: det.core.Context, increment_by):
         noise = random.random() * x
         metrics = {f"z{group}/metric_{i}": x * (i + 1) + noise for i in range(3)}
         core_context.train._report_trial_metrics(
-            group=group, total_batches=steps_completed, metrics=metrics
+            group=group, total_batches=total_batches, metrics=metrics
         )
 
 
 if __name__ == "__main__":
-    # NEW: enable logging, using the det.LOG_FORMAT.  Enabling
-    # logging enables useful log messages from the determined library,
-    # and det.LOG_FORMAT enables filter-by-level in the WebUI.
     logging.basicConfig(level=logging.DEBUG, format=det.LOG_FORMAT)
-    # Log at different levels to demonstrate filter-by-level in the WebUI.
-    logging.debug("debug-level message")
-    logging.info("info-level message")
-    logging.warning("warning-level message")
-    logging.error("error-level message")
-
-    # NEW: create a context, and pass it to the main function.
     with det.core.init() as core_context:
         main(core_context=core_context, increment_by=1)
