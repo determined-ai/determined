aedd69e9462dd300b56bb8a9ba54f468122587ea
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2023-07-25 12:14:06 -0400

    comment

--- master/static/migrations/20230718141429_migrate-checkpointv1.tx.up.sql
+++ master/static/migrations/20230718141429_migrate-checkpointv1.tx.up.sql
@@ -16,6 +16,7 @@ INSERT INTO public.checkpoints_v2 (
     size
 )
 SELECT
+    -- https://stackoverflow.com/questions/12505158/generating-a-uuid-in-postgres-for-insert-statement
     COALESCE(c.uuid, uuid_in(overlay(overlay(md5(random()::text || ':' || random()::text) placing '4' from 13) placing to_hex(floor(random()*(11-8+1) + 8)::int)::text from 17)::cstring)),
     t.task_id,
     CASE
