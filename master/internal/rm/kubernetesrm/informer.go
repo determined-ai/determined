62cdfbfc70e6a151516dc5287ac6b81faf347f38
Carolina Calderon <carolina.calderon@hpe.com>
2023-07-26 11:58:44 -0400

    reverted context.withCancel bc informers crash

--- master/internal/rm/kubernetesrm/informer.go
+++ master/internal/rm/kubernetesrm/informer.go
@@ -135,19 +135,13 @@ func newNodeInformer(
 }
 
 func (i *informer) run() {
-	ctx, cancel := context.WithCancel(context.TODO())
 	i.syslog.Debugf("%s informer is starting", i.name)
-	for {
-		select {
-		case event := <-i.resultChan:
-			if event.Type == watch.Error {
-				i.syslog.Warnf("%s informer emitted error %+v", i.name, event)
-				continue
-			}
-			i.cb(event)
-			cancel()
-		case <-ctx.Done():
-			panic(fmt.Sprintf("%s informer stopped unexpectedly: %s", i.name, ctx.Err()))
+	for event := range i.resultChan {
+		if event.Type == watch.Error {
+			i.syslog.Warnf("%s informer emitted error %+v", i.name, event)
+			continue
 		}
+		i.cb(event)
 	}
+	panic(fmt.Sprintf("%s informer stopped unexpectedly", i.name))
 }
