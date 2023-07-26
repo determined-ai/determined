a785114dd00718a2c22a038c6d4b18782d2c61b9
Emily Bonar <emily.bonar@hpe.com>
2023-07-26 15:29:28 -0400

    fix: only let cluster admins manage resource pool bindings [WEB-1476]

--- webui/react/src/pages/ResourcepoolDetail.tsx
+++ webui/react/src/pages/ResourcepoolDetail.tsx
@@ -13,12 +13,12 @@ import Section from 'components/Section';
 import Spinner from 'components/Spinner';
 import { V1SchedulerTypeToLabel } from 'constants/states';
 import useFeature from 'hooks/useFeature';
+import usePermissions from 'hooks/usePermissions';
 import { paths } from 'routes/utils';
 import { getJobQStats } from 'services/api';
 import { V1GetJobQueueStatsResponse, V1RPQueueStat, V1SchedulerType } from 'services/api-ts-sdk';
 import clusterStore from 'stores/cluster';
 import { maxPoolSlotCapacity } from 'stores/cluster';
-import determinedStore from 'stores/determinedInfo';
 import { ShirtSize } from 'themes';
 import { ValueOf } from 'types';
 import { JobState, ResourceState } from 'types';
@@ -54,7 +54,7 @@ export const DEFAULT_POOL_TAB_KEY = TabType.Active;
 const ResourcepoolDetailInner: React.FC = () => {
   const { poolname, tab } = useParams<Params>();
   const rpBindingFlagOn = useFeature().isOn('rp_binding');
-  const { rbacEnabled } = useObservable(determinedStore.info);
+  const { canManageResourcePoolBindings } = usePermissions();
   const agents = Loadable.getOrElse([], useObservable(clusterStore.agents));
   const resourcePools = useObservable(clusterStore.resourcePools);
 
@@ -178,7 +178,7 @@ const ResourcepoolDetailInner: React.FC = () => {
       },
     ];
 
-    if (rpBindingFlagOn && rbacEnabled) {
+    if (rpBindingFlagOn && canManageResourcePoolBindings) {
       tabItems.push({
         children: <ResourcePoolBindings poolName={pool.name} />,
         key: TabType.Bindings,
@@ -187,7 +187,7 @@ const ResourcepoolDetailInner: React.FC = () => {
     }
 
     return tabItems;
-  }, [pool, poolStats, rbacEnabled, renderPoolConfig, rpBindingFlagOn]);
+  }, [canManageResourcePoolBindings, pool, poolStats, renderPoolConfig, rpBindingFlagOn]);
 
   if (!pool || Loadable.isLoading(resourcePools)) return <Spinner center spinning />;
 
