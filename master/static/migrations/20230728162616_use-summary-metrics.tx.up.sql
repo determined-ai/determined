d6d18ea42ea2fda43826ebdc05d57feccd2d6b88
gt2345 <gt2345@columbia.edu>
2023-07-28 17:37:30 -0500

    calculate validation from summary metrics

---
+++ master/static/migrations/20230728162616_use-summary-metrics.tx.up.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION autoupdate_exp_best_trial_metrics() RETURNS trigger AS $$
+BEGIN
+    WITH bt AS (SELECT id, best_validation_id FROM trials WHERE experiment_id = NEW.experiment_id ORDER BY searcher_metric_value_signed LIMIT 1)
+    UPDATE experiments SET best_trial_id = bt.id, 
+    validation_metrics = 
+    (SELECT metrics -> 'validation_metrics' FROM validations v WHERE v.id = bt.best_validation_id) FROM bt
+    WHERE experiments.id = NEW.experiment_id;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
