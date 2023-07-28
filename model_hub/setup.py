65f76b2982b4becb56d80e74263324f68d01d345
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2023-07-28 11:31:53 -0400

    chore: bump version: 0.23.4-rc1 -> 0.23.4-rc2

--- model_hub/setup.py
+++ model_hub/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="model-hub",
-    version="0.23.4-rc1",
+    version="0.23.4-rc2",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
