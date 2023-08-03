8452b4842f0ad3f724f46f506c9cc32c4ba954f0
gt2345 <gt2345@columbia.edu>
2023-08-03 15:24:39 -0500

    fix migrations

--- master/static/migrations/20230728162616_use-summary-metrics.tx.down.sql
+++ master/static/migrations/20230728162616_use-summary-metrics.tx.down.sql
@@ -3,22 +3,14 @@ BEGIN
     WITH bt AS (SELECT id, best_validation_id FROM trials WHERE experiment_id = NEW.experiment_id ORDER BY searcher_metric_value_signed LIMIT 1)
     UPDATE experiments SET best_trial_id = bt.id, 
     validation_metrics = 
-    (
-        WITH metrics AS (
-            SELECT summary_metrics->'validation_metrics' AS jsonb, searcher_metric_value_signed=searcher_metric_value AS sign FROM trials WHERE id = 41575
-        ), 
-        metrics_values AS (
-            SELECT jsonb_object_keys(jsonb) AS metrics_key, sign, jsonb -> jsonb_object_keys(jsonb) ->> 'min' AS min, jsonb -> jsonb_object_keys(jsonb) ->> 'max' AS max, jsonb -> jsonb_object_keys(jsonb) ->> 'type' AS type FROM metrics),
-        result AS (
-        SELECT metrics_key, CASE sign when true then min else max end AS metrics_value, type FROM metrics_values WHERE type = 'number'
-        ) SELECT json_object_agg(metrics_key, metrics_value::float8) FROM result
-    )
+    (SELECT metrics -> 'validation_metrics' FROM validations v WHERE v.id = bt.best_validation_id) FROM bt
     WHERE experiments.id = NEW.experiment_id;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
-
-
+CREATE TRIGGER autoupdate_exp_best_trial_metrics
+AFTER UPDATE OF best_validation_id ON trials
+FOR EACH ROW EXECUTE PROCEDURE autoupdate_exp_best_trial_metrics();
 
 CREATE TABLE public.exp_metrics_name (
     id SERIAL PRIMARY KEY,
@@ -61,3 +53,7 @@ BEGIN
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER autoupdate_exp_validation_metrics_name
+AFTER INSERT ON raw_validations
+FOR EACH ROW EXECUTE PROCEDURE autoupdate_exp_validation_metrics_name();
--- master/static/migrations/20230728162616_use-summary-metrics.tx.up.sql
+++ master/static/migrations/20230728162616_use-summary-metrics.tx.up.sql
@@ -3,7 +3,16 @@ BEGIN
     WITH bt AS (SELECT id, best_validation_id FROM trials WHERE experiment_id = NEW.experiment_id ORDER BY searcher_metric_value_signed LIMIT 1)
     UPDATE experiments SET best_trial_id = bt.id, 
     validation_metrics = 
-    (SELECT metrics -> 'validation_metrics' FROM validations v WHERE v.id = bt.best_validation_id) FROM bt
+    (
+        WITH metrics AS (
+            SELECT summary_metrics->'validation_metrics' AS jsonb, searcher_metric_value_signed=searcher_metric_value AS sign FROM trials WHERE id = bt.id
+        ), 
+        metrics_values AS (
+            SELECT jsonb_object_keys(jsonb) AS metrics_key, sign, jsonb -> jsonb_object_keys(jsonb) ->> 'min' AS min, jsonb -> jsonb_object_keys(jsonb) ->> 'max' AS max, jsonb -> jsonb_object_keys(jsonb) ->> 'type' AS type FROM metrics),
+        result AS (
+        SELECT metrics_key, CASE sign when true then min else max end AS metrics_value, type FROM metrics_values WHERE type = 'number'
+        ) SELECT json_object_agg(metrics_key, metrics_value::float8) FROM result
+    )
     WHERE experiments.id = NEW.experiment_id;
     RETURN NEW;
 END;
