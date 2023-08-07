0a5c3adda9dcf937cb2c9651614c3a260b784d6c
Eric Liu <eric.liu@hpe.com>
2023-08-07 11:20:23 -0700

    add mocks and intg test stubs

---
+++ master/internal/api_resourcepool_intg_test.go
@@ -0,0 +1,29 @@
+//go:build integration
+// +build integration
+
+package internal
+
+import (
+	"testing"
+)
+
+// var rAuthZ *mocks.ResourceManagerAuthZ.
+func TestGetResourcePools(t *testing.T) {
+	return
+}
+
+func TestPostBinding(t *testing.T) {
+	return
+}
+
+func TestGetBindings(t *testing.T) {
+	return
+}
+
+func TestPatchBindings(t *testing.T) {
+	return
+}
+
+func TestDeleteBindings(t *testing.T) {
+	return
+}
