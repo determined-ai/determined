e815ec76962bc05da114eb9d03ba1ad08b23643a
Eric Liu <eric.liu@hpe.com>
2023-08-07 21:41:29 -0700

    add TestPostBindingSucceeds and cleanup functions

--- master/internal/api_resourcepool_intg_test.go
+++ master/internal/api_resourcepool_intg_test.go
@@ -6,10 +6,13 @@ package internal
 import (
 	"testing"
 
+	"github.com/uptrace/bun"
+
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
+	"github.com/determined-ai/determined/master/internal/db"
 	"github.com/determined-ai/determined/master/internal/mocks"
 	"github.com/determined-ai/determined/master/internal/sproto"
 	"github.com/determined-ai/determined/proto/pkg/apiv1"
@@ -21,7 +24,7 @@ func TestAuthZGetResourcePools(t *testing.T) {
 	return
 }
 
-func TestPostBinding(t *testing.T) {
+func TestPostBindingFailures(t *testing.T) {
 	api, _, ctx := setupAPITest(t, nil)
 	var mockRM mocks.ResourceManager
 	api.m.rm = &mockRM
@@ -36,6 +39,16 @@ func TestPostBinding(t *testing.T) {
 		logrus.Error("error posting workspace with name bindings_test_workspace_2 " +
 			"(workspace my already exist)")
 	}
+
+	defer func() {
+		workspaces := []string{"bindings_test_workspace_1", "bindings_test_workspace_2"}
+		_, err := db.Bun().NewDelete().Table("workspaces").
+			Where("name IN (?)", bun.In(workspaces)).
+			Exec(ctx)
+		if err != nil {
+			logrus.Errorf("Error deleting the following workspaces: %s", workspaces)
+		}
+	}()
 
 	// test resource pools on workspaces that do not exist
 	mockRM.On("GetDefaultComputeResourcePool", mock.Anything, mock.Anything).
@@ -88,6 +101,65 @@ func TestPostBinding(t *testing.T) {
 	})
 
 	require.ErrorContains(t, err, "default resource pool testRP cannot be bound to any workspace")
+
+	if err != nil {
+		return
+	}
+	return
+}
+
+func TestPostBindingSucceeds(t *testing.T) {
+	api, _, ctx := setupAPITest(t, nil)
+	var mockRM mocks.ResourceManager
+	api.m.rm = &mockRM
+
+	_, err := api.PostWorkspace(ctx, &apiv1.PostWorkspaceRequest{Name: "bindings_test_workspace_1"})
+	if err != nil {
+		logrus.Error("error posting workspace with name bindings_test_workspace_1 " +
+			"(workspace may already exist)")
+	}
+	_, err = api.PostWorkspace(ctx, &apiv1.PostWorkspaceRequest{Name: "bindings_test_workspace_2"})
+	if err != nil {
+		logrus.Error("error posting workspace with name bindings_test_workspace_2 " +
+			"(workspace my already exist)")
+	}
+
+	defer func() {
+		workspaces := []string{"bindings_test_workspace_1", "bindings_test_workspace_2"}
+		_, err := db.Bun().NewDelete().Table("workspaces").
+			Where("name IN (?)", bun.In(workspaces)).
+			Exec(ctx)
+		if err != nil {
+			logrus.Errorf("Error deleting the following workspaces: %s", workspaces)
+		}
+	}()
+
+	// test resource pool is a default resource pool
+	mockRM.On("GetDefaultComputeResourcePool", mock.Anything, mock.Anything).
+		Return(sproto.GetDefaultComputeResourcePoolResponse{}, nil).Twice()
+	mockRM.On("GetDefaultAuxResourcePool", mock.Anything, mock.Anything).
+		Return(sproto.GetDefaultAuxResourcePoolResponse{}, nil).Twice()
+	mockRM.On("GetResourcePools", mock.Anything, mock.Anything).
+		Return(&apiv1.GetResourcePoolsResponse{
+			ResourcePools: []*resourcepoolv1.ResourcePool{{Name: "testRP"}},
+		}, nil).Twice()
+
+	_, err = api.BindRPToWorkspace(ctx, &apiv1.BindRPToWorkspaceRequest{
+		ResourcePoolName: "testRP",
+		WorkspaceNames:   []string{"bindings_test_workspace_1"},
+	})
+	require.NoError(t, err)
+
+	// test no workspaces
+	mockRM.On("GetDefaultAuxResourcePool", mock.Anything, mock.Anything).
+		Return(sproto.GetDefaultAuxResourcePoolResponse{PoolName: "testRP"}, nil).Once()
+
+	_, err = api.BindRPToWorkspace(ctx, &apiv1.BindRPToWorkspaceRequest{
+		ResourcePoolName: "testRP",
+		WorkspaceNames:   []string{},
+	})
+
+	require.NoError(t, err)
 
 	if err != nil {
 		return
