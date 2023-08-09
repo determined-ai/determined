ab2415f7c9ab3d39252e550257e29cfaf261ad20
gt2345 <gt2345@columbia.edu>
2023-08-09 14:58:19 -0500

    allow unset default pool

--- master/internal/api_workspace.go
+++ master/internal/api_workspace.go
@@ -423,12 +423,12 @@ func (a *apiServer) PatchWorkspace(
 		insertColumns = append(insertColumns, "uid", "user_", "gid", "group_")
 	}
 
-	if req.Workspace.DefaultComputePool != "" {
-		updatedWorkspace.DefaultComputePool = req.Workspace.DefaultComputePool
+	if req.Workspace.DefaultComputePool != nil {
+		updatedWorkspace.DefaultComputePool = *req.Workspace.DefaultComputePool
 		insertColumns = append(insertColumns, "default_compute_pool")
 	}
-	if req.Workspace.DefaultAuxPool != "" {
-		updatedWorkspace.DefaultAuxPool = req.Workspace.DefaultAuxPool
+	if req.Workspace.DefaultAuxPool != nil {
+		updatedWorkspace.DefaultAuxPool = *req.Workspace.DefaultAuxPool
 		insertColumns = append(insertColumns, "default_aux_pool")
 	}
 
