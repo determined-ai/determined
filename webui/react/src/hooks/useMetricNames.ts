a104da8b34039a18a7b7f365a6b3274d3f0a3dd0
Anda <anda@determined.ai>
2023-08-14 17:09:49 -0700

    Revert "fix: Handle metric names finish loading, but still empty (#7634)"
    This reverts commit aafcff8f6d1440f3fe492153fd70d45c56bec097.

--- webui/react/src/hooks/useMetricNames.ts
+++ webui/react/src/hooks/useMetricNames.ts
@@ -58,37 +58,35 @@ const useMetricNames = (
           ...newValidationMetrics.map((name) => ({ name, type: MetricType.Validation })),
           ...newTrainingMetrics.map((name) => ({ name, type: MetricType.Training })),
         ];
-        setMetrics((prevMetrics) => {
-          if (newMetrics.length === 0) {
-            return Loadable.isLoaded(prevMetrics) ? prevMetrics : Loaded([]);
-          }
+        if (newMetrics.length > 0) {
+          setMetrics((prevMetrics) => {
+            /*
+             * Since we may intermittently receive a subset of all available
+             * metrics or an empty list of metrics we must merge the new and
+             * previous metrics to accurately determine if any new metrics have
+             * not been seen before.
+             */
+            const previousMetrics = Loadable.getOrElse([], prevMetrics);
 
-          /*
-           * Since we may intermittently receive a subset of all available
-           * metrics or an empty list of metrics we must merge the new and
-           * previous metrics to accurately determine if any new metrics have
-           * not been seen before.
-           */
-          const previousMetrics = Loadable.getOrElse([], prevMetrics);
+            const previousMetricsSet = Loadable.getOrElse([], prevMetrics).reduce(
+              (acc, cur) => acc.add(metricToKey(cur)),
+              new Set<string>(),
+            );
+            const updatedMetricsSet = [...newMetrics, ...previousMetrics].reduce(
+              (acc, cur) => acc.add(metricToKey(cur)),
+              new Set<string>(),
+            );
 
-          const previousMetricsSet = Loadable.getOrElse([], prevMetrics).reduce(
-            (acc, cur) => acc.add(metricToKey(cur)),
-            new Set<string>(),
-          );
-          const updatedMetricsSet = [...newMetrics, ...previousMetrics].reduce(
-            (acc, cur) => acc.add(metricToKey(cur)),
-            new Set<string>(),
-          );
+            if (isEqual(previousMetricsSet, updatedMetricsSet)) return prevMetrics;
 
-          if (_.isEqual(previousMetricsSet, updatedMetricsSet)) return prevMetrics;
-
-          return Loaded(
-            Array.from(updatedMetricsSet)
-              .map((metricKey) => metricKeyToMetric(metricKey))
-              .filter((metric): metric is Metric => !!metric)
-              .sort(metricSorter),
-          );
-        });
+            return Loaded(
+              Array.from(updatedMetricsSet)
+                .map((metricKey) => metricKeyToMetric(metricKey))
+                .filter((metric): metric is Metric => !!metric)
+                .sort(metricSorter),
+            );
+          });
+        }
       },
       errorHandler,
     );
