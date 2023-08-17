2625020cdddc9495819e56ce4c51f9d02b634c33
gt2345 <gt2345@columbia.edu>
2023-08-17 16:03:17 -0500

    fmt

--- master/internal/experiment_filter.go
+++ master/internal/experiment_filter.go
@@ -450,7 +450,8 @@ func (e experimentFilter) toSQL(q *bun.SelectQuery,
 // group_a.value.last -> group_a, value, last
 // group_b.value.a.last -> group_b, value.a, last .
 func parseMetricsName(str string) (string, string, string, error) {
-	template := regexp.MustCompile(`(?P<group>[[:print:]]+?)\.(?P<name>[[:print:]]+)\.(?P<qualifier>min|max|mean|last)`)
+	template := regexp.MustCompile(
+		`(?P<group>[[:print:]]+?)\.(?P<name>[[:print:]]+)\.(?P<qualifier>min|max|mean|last)`)
 
 	matches := template.FindStringSubmatch(str)
 	if len(matches) < 4 {
