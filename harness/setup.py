399ad9cfc45bfda0f1aab28f36dabab0fd0d2953
Anda <anda@determined.ai>
2023-08-18 08:22:43 -0700

    chore: bump version: 0.24.0-rc4 -> 0.24.0-rc5

--- harness/setup.py
+++ harness/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="determined",
-    version="0.24.0-rc4",
+    version="0.24.0-rc5",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
