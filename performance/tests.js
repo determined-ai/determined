3e9de5a66c5a2018cbc8f72815b2cd48752f8fb8
julian-determined-ai <julian.glover@hpe.com>
2023-08-21 12:57:00 -0500

    added some more example cases

--- performance/tests.js
+++ performance/tests.js
@@ -9,21 +9,68 @@ const scenarios = {
     smoke_test: {
         tags: { test_type: 'smoke' },
         executor: 'shared-iterations',
-        vus: 5,
-        iterations: 10
+        vus: 3,
+        maxDuration: "1m",
+        iterations: 5
     },
     average_load_test: {
-        tags: { test_type: 'smoke' },
+        tags: { test_type: 'average' },
         executor: 'ramping-vus',
         stages: [
-            { duration: '10m', target: 100 },
+            { duration: '10m', target: 50 },
+            { duration: '60m', target: 50 },
             { duration: '10m', target: 0 }
-        ]
+        ],
+        startTime: "1m"
     },
-    // more scenarios
+    stress_test: {
+        tags: { test_type: 'stress' },
+        executor: 'ramping-vus',
+        stages: [
+            { duration: '10m', target: 175 },
+            { duration: '20m', target: 175 },
+            { duration: '10m', target: 0 }
+        ],
+        startTime: "85m"
+    },
+    soak_test: {
+        tags: { test_type: 'soak' },
+        executor: 'ramping-vus',
+        stages: [
+            { duration: '5m', target: 50 },
+            { duration: '1h', target: 50 },
+            { duration: '10m', target: 0 }
+        ],
+        startTime: "130m"
+    },
+    spike_test: {
+        tags: { test_type: 'spike' },
+        executor: 'ramping-vus',
+        stages: [
+            { duration: '3m', target: 500 },
+            { duration: '1m', target: 0 },
+        ],
+        startTime: "210m"
+    },
+    breakpoint_test: {
+        tags: { test_type: 'breakpoint' },
+        executor: 'ramping-arrival-rate',
+        preAllocatedVUs: 0,
+        stages: [
+            { duration: '2h', target: 30000 },
+        ],
+        startTime: "215m",
+    },
 }
 
-export const options = { scenarios };
+export const options = {
+    scenarios, thresholds: {
+        'http_req_failed{test_type:breakpoint}': [{
+            threshold: 'rate<0.05',
+            abortOnFail: true,
+        },]
+    },
+};
 
 export default function () {
     const res = http.get(`${clusterUrl}${masterEndpoint}`);
