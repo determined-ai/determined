0a345296c4eed4df3c9b35dacb4b2bae6ca65159
Jagadeesh Madagundi <jagadeesh545@gmail.com>
2023-08-22 19:42:17 -0500

    fix: add unit test cases for mapx methods Values and Clear (#7699)

--- master/pkg/syncx/mapx/mapx_test.go
+++ master/pkg/syncx/mapx/mapx_test.go
@@ -1,8 +1,10 @@
 package mapx
 
 import (
+	"sort"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/exp/maps"
 )
 
@@ -14,7 +16,7 @@ func FuzzMap(f *testing.F) {
 
 	f.Add(uint8(0), uint8(0), "hello")
 	f.Fuzz(func(t *testing.T, op, k uint8, v string) {
-		switch op % 5 {
+		switch op % 7 {
 		case 0:
 			m.Len()
 		case 1:
@@ -28,6 +30,38 @@ func FuzzMap(f *testing.F) {
 			m.WithLock(func(m map[uint8]string) {
 				_ = maps.Keys(m)
 			})
+		case 5:
+			_ = m.Values()
+		case 6:
+			m.Clear()
 		}
 	})
+}
+
+func TestMapx(t *testing.T) {
+	tests := []struct {
+		k string
+		v string
+	}{
+		{"1234", "hi"},
+		{"1235", "hello"},
+		{"1236", "world"},
+	}
+	testMap := New[string, string]()
+	for _, tt := range tests {
+		testMap.Store(tt.k, tt.v)
+	}
+	assert.Equal(t, len(tests), testMap.Len())
+	value, _ := testMap.Load("1235")
+	assert.Equal(t, "hello", value)
+	testMap.Delete("1235")
+	expectedValueList := [...]string{"hi", "world"}
+	valueList := testMap.Values()
+	assert.Equal(t, 2, testMap.Len())
+	sort.Strings(valueList)
+	for i, v := range valueList {
+		assert.Equal(t, expectedValueList[i], v)
+	}
+	testMap.Clear()
+	assert.Equal(t, 0, testMap.Len())
 }
