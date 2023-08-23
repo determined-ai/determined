760a73818f8b1b8397fbcfcf4032f8603ad0b888
Jerry J. Harrow <84593277+jerryharrow@users.noreply.github.com>
2023-08-23 11:01:46 -0400

    test: Add pytorch2 distributed e2e tests on slurm [FE-168] (#1007)
    New test of HuggingFace examples/hf_trainer_api/hf_language_modeling on pytorch2.
    Enabled for slurm_gpu + distributed marks.

---
+++ e2e_tests/tests/nightly/test_pytorch2.py
@@ -0,0 +1,26 @@
+import pytest
+
+from tests import config as conf
+from tests import experiment as exp
+
+
+@pytest.mark.distributed
+@pytest.mark.gpu_required
+@pytest.mark.e2e_slurm_gpu
+def test_pytorch2_hf_language_modeling_distributed() -> None:
+    test_dir = "hf_language_modeling"
+
+    config = conf.load_config(conf.hf_trainer_examples_path(f"{test_dir}/distributed.yaml"))
+    config = conf.set_pt2_image(config)
+    config = conf.set_slots_per_trial(config, 4)
+    config["searcher"]["max_length"]["batches"] = 50
+
+    # Our hardware GPUs have only 16gb memory, lower memory use with smaller batches.
+    config = conf.set_entrypoint(
+        config,
+        config["entrypoint"]
+        .replace("--per_device_train_batch_size 8", "--per_device_train_batch_size 2")
+        .replace("--per_device_eval_batch_size 8", "--per_device_eval_batch_size 2"),
+    )
+
+    exp.run_basic_test_with_temp_config(config, conf.hf_trainer_examples_path(test_dir), 1)
