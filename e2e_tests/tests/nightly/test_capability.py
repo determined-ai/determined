dcba4d75c42f7e90e7860ef82ef076b2d250b19b
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-08-25 15:24:05 -0700

    remove nlp from ci

--- e2e_tests/tests/nightly/test_capability.py
+++ e2e_tests/tests/nightly/test_capability.py
@@ -4,13 +4,6 @@ from tests import config as conf
 from tests import experiment as exp
 
 
-@pytest.mark.nightly
-def test_bert_glue_const() -> None:
-    config = conf.load_config(conf.nlp_examples_path("bert_glue_pytorch/const.yaml"))
-    config = conf.set_max_length(config, {"batches": 200})
-
-    exp.run_basic_test_with_temp_config(config, conf.nlp_examples_path("bert_glue_pytorch"), 1)
-
 
 @pytest.mark.nightly
 def test_gaea_pytorch_const() -> None:
@@ -35,27 +28,6 @@ def test_pix2pix_facades_const() -> None:
     config = conf.set_max_length(config, {"batches": 200})
 
     exp.run_basic_test_with_temp_config(config, conf.gan_examples_path("pix2pix_tf_keras"), 1)
-
-
-@pytest.mark.nightly
-def test_word_language_transformer_const() -> None:
-    config = conf.load_config(conf.nlp_examples_path("word_language_model/const.yaml"))
-    config = conf.set_max_length(config, {"batches": 200})
-    config = config.copy()
-    config["hyperparameters"]["model_cls"] = "Transformer"
-
-    exp.run_basic_test_with_temp_config(config, conf.nlp_examples_path("word_language_model"), 1)
-
-
-@pytest.mark.nightly
-def test_word_language_lstm_const() -> None:
-    config = conf.load_config(conf.nlp_examples_path("word_language_model/const.yaml"))
-    config = conf.set_max_length(config, {"batches": 200})
-    config = config.copy()
-    config["hyperparameters"]["model_cls"] = "LSTM"
-    config["hyperparameters"]["tied"] = False
-
-    exp.run_basic_test_with_temp_config(config, conf.nlp_examples_path("word_language_model"), 1)
 
 
 @pytest.mark.nightly
