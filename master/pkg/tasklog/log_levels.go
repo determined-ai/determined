a6d0fba66bb2f6d4002b762e27088198e64cc17d
Phillip Gaisford <98362331+phillip-gaisford@users.noreply.github.com>
2023-08-30 14:01:31 -0400

    chore: rearrange log level constants (#7752)
    * chore: rearrange log level constants
    
    so that they may also be imported by pkg/archive
    
    * Update package name to tasklog

---
+++ master/pkg/tasklog/log_levels.go
@@ -0,0 +1,18 @@
+package tasklog
+
+const (
+	// LogLevelTrace is the trace task log level.
+	LogLevelTrace = "TRACE"
+	// LogLevelDebug is the debug task log level.
+	LogLevelDebug = "DEBUG"
+	// LogLevelInfo is the info task log level.
+	LogLevelInfo = "INFO"
+	// LogLevelWarning is the warn task log level.
+	LogLevelWarning = "WARNING"
+	// LogLevelError is the error task log level.
+	LogLevelError = "ERROR"
+	// LogLevelCritical is the critical task log level.
+	LogLevelCritical = "CRITICAL"
+	// LogLevelUnspecified is the unspecified task log level.
+	LogLevelUnspecified = "UNSPECIFIED"
+)
