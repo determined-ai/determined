e6c8faf09a4d88014b45fa3650ba7863abcb42cb
gt2345 <gt2345@columbia.edu>
2023-09-07 15:07:05 -0500

    reset canceler

--- webui/react/src/pages/TrialDetails/TrialDetailsLogs.tsx
+++ webui/react/src/pages/TrialDetails/TrialDetailsLogs.tsx
@@ -164,12 +164,16 @@ const TrialDetailsLogs: React.FC<Props> = ({ experiment, trial }: Props) => {
 
     const fieldCanceler = new AbortController();
 
+    const newCanceler = new AbortController();
+    canceler.current = newCanceler;
+
     readStream(
       detApi.StreamingExperiments.trialLogsFields(trial.id, true, { signal: fieldCanceler.signal }),
       (event) => setFilterOptions(event as Filters),
     );
 
     return () => {
+      canceler.current.abort();
       fieldCanceler.abort();
     };
   }, [trial?.id, ui.isPageHidden]);
