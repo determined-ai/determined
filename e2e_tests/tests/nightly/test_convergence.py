d59610b493ba48e64653a5d0f42394f756665a11
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-09-08 11:51:57 -0700

    remove tutorials from ci

--- e2e_tests/tests/nightly/test_convergence.py
+++ e2e_tests/tests/nightly/test_convergence.py
@@ -30,25 +30,6 @@ def test_mnist_pytorch_accuracy(client: _client.Determined) -> None:
 
 
 @pytest.mark.nightly
-def test_fashion_mnist_tf_keras(client: _client.Determined) -> None:
-    config = conf.load_config(conf.tutorials_path("fashion_mnist_tf_keras/const.yaml"))
-    config = conf.set_random_seed(config, 1591110586)
-    experiment_id = exp.run_basic_test_with_temp_config(
-        config, conf.tutorials_path("fashion_mnist_tf_keras"), 1
-    )
-
-    trials = exp.experiment_trials(experiment_id)
-    validations = _get_validation_metrics(client, trials[0].trial.id)
-    validation_accuracies = [v["val_accuracy"] for v in validations]
-
-    target_accuracy = 0.85
-    assert max(validation_accuracies) > target_accuracy, (
-        "fashion_mnist_tf_keras did not reach minimum target accuracy {}."
-        " full validation accuracy history: {}".format(target_accuracy, validation_accuracies)
-    )
-
-
-@pytest.mark.nightly
 def test_cifar10_pytorch_accuracy(client: _client.Determined) -> None:
     config = conf.load_config(conf.cv_examples_path("cifar10_pytorch/const.yaml"))
     experiment_id = exp.run_basic_test_with_temp_config(
--- e2e_tests/tests/nightly/test_distributed.py
+++ e2e_tests/tests/nightly/test_distributed.py
@@ -33,15 +33,6 @@ def test_mnist_pytorch_set_stop_requested_distributed() -> None:
     config = conf.load_config(conf.fixtures_path("mnist_pytorch/distributed-stop-requested.yaml"))
     exp.run_basic_test_with_temp_config(config, conf.fixtures_path("mnist_pytorch"), 1)
 
-
-@pytest.mark.distributed
-def test_fashion_mnist_tf_keras_distributed() -> None:
-    config = conf.load_config(conf.tutorials_path("fashion_mnist_tf_keras/distributed.yaml"))
-    config = conf.set_max_length(config, {"batches": 200})
-
-    exp.run_basic_test_with_temp_config(config, conf.tutorials_path("fashion_mnist_tf_keras"), 1)
-
-
 @pytest.mark.distributed
 def test_imagenet_pytorch_distributed() -> None:
     config = conf.load_config(conf.tutorials_path("imagenet_pytorch/distributed_cifar.yaml"))
