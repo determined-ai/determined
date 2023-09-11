5b346d39c6908d3b44deb993c67164cc7c12a80e
Corban Beaird <corban.beaird@hpe.com>
2023-09-11 10:22:05 -0600

    feat: added workspace_id to postgres triggers

--- master/addtrigger.sql
+++ master/addtrigger.sql
@@ -21,7 +21,7 @@ FOR EACH ROW EXECUTE PROCEDURE stream_trial_seq_modify();
 
 -- helper function to create trial jsonb object for streaming
 CREATE OR REPLACE FUNCTION stream_trial_notify(
-    before jsonb, beforeproj integer, after jsonb, afterproj integer
+    before jsonb, beforeproj integer, beforework integer, after jsonb, afterproj integer, afterwork integer
 ) RETURNS integer AS $$
 DECLARE
     output jsonb = NULL;
@@ -29,10 +29,12 @@ DECLARE
 BEGIN
     IF before IS NOT NULL THEN
         temp = before || jsonb_object_agg('project_id', beforeproj);
+        temp = temp || jsonb_object_agg('workspace_id', beforework);
         output = jsonb_object_agg('before', temp);
     END IF;
     IF after IS NOT NULL THEN
         temp = after || jsonb_object_agg('project_id', afterproj);
+        temp = temp || jsonb_object_agg('workspace_id', afterwork);
         IF output IS NULL THEN
             output = jsonb_object_agg('after', temp);
         ELSE
@@ -45,20 +47,24 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Trigger function to NOTIFY the master of changes, for INSERT/UPDATE/DETELE.
+-- Trigger function to NOTIFY the master of changes, for INSERT/UPDATE/DELETE.
 CREATE OR REPLACE FUNCTION stream_trial_change() RETURNS TRIGGER AS $$
 DECLARE
     proj integer;
+    work integer;
 BEGIN
     IF (TG_OP = 'INSERT') THEN
         proj = project_id from experiments where experiments.id = NEW.experiment_id;
-        PERFORM stream_trial_notify(NULL, NULL, to_jsonb(NEW), proj);
+        work = workspace_id from projects where projects.id = proj;
+        PERFORM stream_trial_notify(NULL, NULL, NULL, to_jsonb(NEW), proj, work);
     ELSEIF (TG_OP = 'UPDATE') THEN
         proj = project_id from experiments where experiments.id = NEW.experiment_id;
-        PERFORM stream_trial_notify(to_jsonb(OLD), proj, to_jsonb(NEW), proj);
+        work = workspace_id from projects where projects.id = proj;
+        PERFORM stream_trial_notify(to_jsonb(OLD), proj, work, to_jsonb(NEW), proj, work);
     ELSEIF (TG_OP = 'DELETE') THEN
         proj = project_id from experiments where experiments.id = OLD.experiment_id;
-        PERFORM stream_trial_notify(to_jsonb(OLD), proj, NULL, NULL);
+        work = workspace_id from projects where projects.id = proj;
+        PERFORM stream_trial_notify(to_jsonb(OLD), proj, work, NULL, NULL, NULL);
         -- DELETEs trigger BEFORE, and must return a non-NULL value.
         return OLD;
     END IF;
@@ -83,6 +89,8 @@ CREATE OR REPLACE FUNCTION stream_trial_ownership() RETURNS TRIGGER AS $$
 DECLARE
     trial RECORD;
     jtrial jsonb;
+    oldwork integer;
+    newwork integer;
 BEGIN
     FOR trial IN
         SELECT * from trials where experiment_id = NEW.id
@@ -90,14 +98,51 @@ BEGIN
         trial.seq = nextval('stream_trial_seq');
         UPDATE trials SET seq = trial.seq where id = trial.id;
         jtrial = to_jsonb(trial);
-        PERFORM stream_trial_notify(jtrial, OLD.project_id, jtrial, NEW.project_id);
+        oldwork = workspace_id from projects where projects.id = OLD.project_id;
+        newwork = workspace_id from projects where projects.id = NEW.project_id;
+        PERFORM stream_trial_notify(jtrial, OLD.project_id, oldwork, jtrial, NEW.project_id, newwork);
     END LOOP;
     -- return value for AFTER triggers is ignored
     return NULL;
 END;
+
 $$ LANGUAGE plpgsql;
 --
 DROP TRIGGER IF EXISTS stream_trial_trigger_ownership ON experiments;
 CREATE TRIGGER stream_trial_trigger_ownership
 AFTER UPDATE OF project_id ON EXPERIMENTS
 FOR EACH ROW EXECUTE PROCEDURE stream_trial_ownership();
+
+
+
+-- Trigger for detecting trial permission scope changes derived from projects.workspace_id.
+CREATE OR REPLACE FUNCTION stream_trial_permission() RETURNS TRIGGER AS $$
+DECLARE
+    trial RECORD;
+    jtrial jsonb;
+BEGIN
+    FOR trial IN
+        SELECT
+            t.*
+        FROM
+            experiments e
+            INNER JOIN 
+                trials t ON t.experiment_id = e.id 
+            WHERE
+                project_id = NEW.id
+    LOOP
+        trial.seq = nextval('stream_trial_seq');
+        UPDATE trials SET seq = trial.seq where id = trial.id;
+        jtrial = to_jsonb(trial);
+        PERFORM stream_trial_notify(jtrial, OLD.id, OLD.workspace_id, jtrial, NEW.id,  NEW.workspace_id);
+    END LOOP;
+    -- return value for AFTER triggers is ignored
+    return NULL;
+END;
+
+$$ LANGUAGE plpgsql;
+--
+DROP TRIGGER IF EXISTS stream_trial_trigger_permission ON projects;
+CREATE TRIGGER stream_trial_trigger_permission
+AFTER UPDATE OF workspace_id ON projects
+FOR EACH ROW EXECUTE PROCEDURE stream_trial_permission();
