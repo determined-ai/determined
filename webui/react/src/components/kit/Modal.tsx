1434d408e77982eb05ccbdd17c5025f74986583a
Keita Nonaka <keita.nonaka@hpe.com>
2023-09-12 12:30:17 -0700

    fix: support external link in modal

--- webui/react/src/components/kit/Modal.tsx
+++ webui/react/src/components/kit/Modal.tsx
@@ -20,6 +20,7 @@ import css from './Modal.module.scss';
 interface LinkParams {
   text: string;
   url: string;
+  external?: boolean;
 }
 
 export type ModalSize = 'small' | 'medium' | 'large';
@@ -120,7 +121,7 @@ export const Modal: React.FC<ModalProps> = ({
         <div className={css.footer}>
           <div className={css.footerLink}>
             {footerLink && (
-              <Link path={footerLink.url} popout>
+              <Link external={footerLink.external ?? false} path={footerLink.url} popout>
                 {footerLink.text}
               </Link>
             )}
@@ -160,7 +161,7 @@ export const Modal: React.FC<ModalProps> = ({
           <div className={css.headerTitle}>{title}</div>
           <div className={css.headerLink}>
             {headerLink && (
-              <Link path={headerLink.url} popout>
+              <Link external={headerLink.external ?? false} path={headerLink.url} popout>
                 {headerLink.text}
               </Link>
             )}
