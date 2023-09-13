653a93c8776e0742b05edb8219878c367b28f5b0
Carolina Calderon <carolina.calderon@hpe.com>
2023-09-13 17:53:01 -0600

    chore: bunify & tidy up internal/user

--- master/internal/experiment.go
+++ master/internal/experiment.go
@@ -194,7 +194,7 @@ func newExperiment(
 		telemetry.ReportExperimentCreated(expModel.ID, activeConfig)
 	}
 
-	agentUserGroup, err := user.GetAgentUserGroup(*expModel.OwnerID, workspaceID)
+	agentUserGroup, err := user.GetAgentUserGroup(context.TODO(), *expModel.OwnerID, workspaceID)
 	if err != nil {
 		return nil, launchWarnings, err
 	}
