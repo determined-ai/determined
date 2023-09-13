bc4f1ca1aa24dc35c4f528d1b9e0f065a3f15bde
julian-determined-ai <julian.glover@hpe.com>
2023-09-13 17:02:41 -0500

    feat: Add performance tests for endpoints used in the WebUI initial load

--- performance/src/api_performance_tests.ts
+++ performance/src/api_performance_tests.ts
@@ -10,18 +10,224 @@ const DEFAULT_CLUSTER_URL = 'http://localhost:8080';
 // Fallback to localhost if a cluster url is not supplied
 const clusterURL = __ENV.DET_MASTER ?? DEFAULT_CLUSTER_URL
 
-// List of tests
-const getloadTests = () => [
+interface TestConfiguration {
+    token: string
+}
 
-    // Query the master endpoint
-    test(
-        'visit master endpoint',
-        () => {
-            const res = http.get(generateEndpointUrl('/api/v1/master', clusterURL));
-            check(res, { '200 response': (r) => r.status == 200 });
-        }
-    ),
-]
+///"v2.public.eyJpZCI6MTIwODcsInVzZXJfaWQiOjEsImV4cGlyeSI6IjIwMjMtMDktMjBUMTI6MjM6MTcuODE4MS0wNTowMCJ9URiM9GD73UBazvfPNdmmbTJDV9A_pVesF1UdqVcRXQjTuxhHDu4YM-NGXFh1QMZohHc4Ef3xCxodT5iNsug6CA.bnVsbA"
+export const setup = () => {
+    const loginCredentials = {
+        username: "admin",
+        password: ""
+    }
+    const params = {
+        headers: { 'Content-Type': 'application/json' },
+    }
+    const requestBody = JSON.stringify(loginCredentials)
+    const authResponse = http.post(generateEndpointUrl('/api/v1/auth/login', clusterURL),
+        requestBody,
+        params)
+
+    const authResponseJson = authResponse.json() as JSONObject
+    const token = `Bearer ${authResponseJson.token}`;
+    return { token }
+}
+
+// List of tests
+const getloadTests = (testConfig?: TestConfiguration) => {
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `${testConfig?.token}`,
+        },
+    }
+
+    return [
+        // Query the master endpoint
+        test(
+            'get master configuration',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/master', clusterURL));
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get agents',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/agents', clusterURL), params);
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get workspaces',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/workspaces', clusterURL), params);
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get user settings',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/users/setting', clusterURL), params);
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get resource pools',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/resource-pools', clusterURL), params);
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get available workspace resource pools',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/workspaces/1/available-resource-pools', clusterURL), params);
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get users',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/users', clusterURL), params);
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get workspace bindings',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/resource-pools/default/workspace-bindings', clusterURL), params);
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'login',
+            () => {
+                const res = http.post(generateEndpointUrl('/api/v1/auth/login', clusterURL),
+                    JSON.stringify({
+                        username: "admin",
+                        password: ""
+                    }),
+                    {
+                        headers: { 'Content-Type': 'application/json' },
+                    })
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get workspace model labels',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/model/labels?workspaceId=1', clusterURL), params)
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        // test(
+        //     'get workspace projects',
+        //     () => {
+        //         const res = http.get(generateEndpointUrl('/api/v1/workspace/1/projects', clusterURL), params)
+        //         check(res, { '200 response': (r) => r.status == 200 });
+        //     }
+        // ),
+        test(
+            'get models',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/models', clusterURL), params)
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get telemetry',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/master/telemetry', clusterURL), params)
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get tensorboards',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/tensorboards', clusterURL), params)
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get shells',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/shells', clusterURL), params)
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get notebooks',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/notebooks', clusterURL), params)
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get commands',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/notebooks', clusterURL), params)
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get job queue stats',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/job-queues/stats', clusterURL), params)
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get v2 job queue',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/job-queues-2?resourcePool=default', clusterURL), params)
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get project',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/projects/1', clusterURL), params)
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        test(
+            'get user activity',
+            () => {
+                const res = http.get(generateEndpointUrl('/api/v1/users/activity', clusterURL), params)
+                check(res, { '200 response': (r) => r.status == 200 });
+            }
+        ),
+        // test(
+        //     'get permissions summary',
+        //     () => {
+        //         const res = http.get(generateEndpointUrl('/api/v1/permissions-summary', clusterURL), params);
+        //         check(res, { '200 response': (r) => r.status == 200 });
+        //     }
+        // ),
+        // test(
+        //     'search groups',
+        //     () => {
+        //         const res = http.get(generateEndpointUrl('/api/groups/search', clusterURL), params);
+        //         check(res, { '200 response': (r) => r.status == 200 });
+        //     }
+        // ),
+        // test(
+        //     'get workspace roles',
+        //     () => {
+        //         const res = http.get(generateEndpointUrl('/api/v1/roles/workspace/1', clusterURL), params);
+        //         check(res, { '200 response': (r) => r.status == 200 });
+        //     }
+        // ),
+        // test(
+        //     'get roles by assignability',
+        //     () => {
+        //         const res = http.get(generateEndpointUrl('/api/v1/roles/search/by-assignability', clusterURL), params);
+        //         check(res, { '200 response': (r) => r.status == 200 });
+        //     }
+        // ),
+    ]
+}
 
 
 const thresholds: { [name: string]: Threshold[] } = {
@@ -72,8 +278,8 @@ export const options: Options = {
     thresholds,
 };
 
-export default (): void => {
-    getloadTests().map(test => test.group())
+export default (config: TestConfiguration): void => {
+    getloadTests(config).map(test => test.group())
     sleep(1)
 }
 
