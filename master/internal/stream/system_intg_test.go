ec0099e04a3ab0d0832deb7b249cd4e52363e763
Eric Liu <eric.liu@hpe.com>
2023-09-13 12:56:01 -0700

    add more test cases

--- master/internal/stream/system_intg_test.go
+++ master/internal/stream/system_intg_test.go
@@ -30,7 +30,6 @@ func TestStartupTrial(t *testing.T) {
 		},
 		Subscribe: SubscriptionSpecSet{
 			Trials: &TrialSubscriptionSpec{
-				// TrialIds: []int{1, 2, 3},
 				ExperimentIds: []int{1}, // trials 1,2,3
 				Since:         0,
 			},
@@ -41,6 +40,46 @@ func TestStartupTrial(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, 1, len(deletions), "did not receive 1 deletion message")
 	require.Equal(t, "0", deletions[0], "expected deleted trials to be 0, not %s", deletions[0])
+	require.Equal(t, 0, len(trialMsgs), "received unexpected trial message")
+
+	// don't know about trial 3, and trial 4 doesn't exist
+	startupMessage = StartupMsg{
+		Known: KnownKeySet{
+			Trials: "1,2,4",
+		},
+		Subscribe: SubscriptionSpecSet{
+			Trials: &TrialSubscriptionSpec{
+				ExperimentIds: []int{1}, // trials 1,2,3
+				Since:         0,
+			},
+		},
+	}
+	messages = testStartup(t, startupMessage)
+	deletions, trialMsgs, err = splitDeletionsAndTrials(messages)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(deletions), "did not receive 1 deletion message")
+	require.Equal(t, "4", deletions[0], "expected deleted trials to be 4, not %s", deletions[0])
+	require.Equal(t, 1, len(trialMsgs), "received unexpected trial message")
+	require.Equal(t, 3, trialMsgs[0].ID, "expected trialMsg with ID 3, received ID %d",
+		trialMsgs[0].ID)
+
+	// Subscribe to all known trials, but 4 doesn't exist
+	startupMessage = StartupMsg{
+		Known: KnownKeySet{
+			Trials: "1,2,3,4",
+		},
+		Subscribe: SubscriptionSpecSet{
+			Trials: &TrialSubscriptionSpec{
+				TrialIds: []int{1, 2, 3, 4},
+				Since:    0,
+			},
+		},
+	}
+	messages = testStartup(t, startupMessage)
+	deletions, trialMsgs, err = splitDeletionsAndTrials(messages)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(deletions), "did not receive 1 deletion message")
+	require.Equal(t, "4", deletions[0], "expected deleted trials to be 4, not %s", deletions[0])
 	require.Equal(t, 0, len(trialMsgs), "received unexpected trial message")
 }
 
