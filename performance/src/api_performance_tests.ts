05b7e5d19b6b3d7073956f64ce3cf29c5ea7191c
julian-determined-ai <julian.glover@hpe.com>
2023-09-18 12:43:20 -0500

    more generator updates

--- performance/src/api_performance_tests.ts
+++ performance/src/api_performance_tests.ts
@@ -74,18 +74,17 @@ const getloadTests = (
         test("get resource pools", getRequest("/api/v1/resource-pools")),
         test(
             "get available workspace resource pools",
-            getRequest
-                (
-                    `/api/v1/workspaces/${sD?.workspace.id}/available-resource-pools`),
+            getRequest(
+                `/api/v1/workspaces/${sD?.workspace.id}/available-resource-pools`,
+            ),
 
             !!sD?.workspace.id,
         ),
         test("get users", getRequest("/api/v1/users")),
-        test("get workspace bindings", () =>
-            getRequest(
-                "/api/v1/resource-pools/default/workspace-bindings",
-
-            )),
+        test(
+            "get workspace bindings",
+            getRequest("/api/v1/resource-pools/default/workspace-bindings"),
+        ),
         test("login", () => {
             const res = http.post(
                 generateEndpointUrl("/api/v1/auth/login", CLUSTER_URL),
@@ -101,207 +100,139 @@ const getloadTests = (
         }),
         test(
             "get workspace model labels",
-            () =>
-                getRequest(
-                    `/api/v1/model/labels?workspaceId=${sD?.workspace.id}`,
-
-                ),
+            getRequest(`/api/v1/model/labels?workspaceId=${sD?.workspace.id}`),
             !!sD?.workspace.id,
         ),
-        test("get models", () =>
-            getRequest("/api/v1/models")),
-        test("get telemetry", () =>
-            getRequest("/api/v1/master/telemetry")),
-        test("get tensorboards", () =>
-            getRequest("/api/v1/tensorboards")),
-        test("get shells", () =>
-            getRequest("/api/v1/shells")),
-        test("get notebooks", () =>
-            getRequest("/api/v1/notebooks")),
-        test("get commands", () =>
-            getRequest("/api/v1/commands")),
-        test("get job queue stats", () =>
-            getRequest("/api/v1/job-queues/stats")),
-        test("get v2 job queue", () =>
-            getRequest(
-                "/api/v1/job-queues-v2?resourcePool=default",
-
-            )),
+        test("get models", getRequest("/api/v1/models")),
+        test("get telemetry", getRequest("/api/v1/master/telemetry")),
+        test("get tensorboards", getRequest("/api/v1/tensorboards")),
+        test("get shells", getRequest("/api/v1/shells")),
+        test("get notebooks", getRequest("/api/v1/notebooks")),
+        test("get commands", getRequest("/api/v1/commands")),
+        test("get job queue stats", getRequest("/api/v1/job-queues/stats")),
+        test(
+            "get v2 job queue",
+            getRequest("/api/v1/job-queues-v2?resourcePool=default"),
+        ),
         test(
             "get project",
-            () =>
-                getRequest(
-                    `/api/v1/projects/${sD?.workspace.projectId}`,
-
-                ),
+            getRequest(`/api/v1/projects/${sD?.workspace.projectId}`),
             !!sD?.workspace.projectId,
         ),
-        test("get user activity", () =>
-            getRequest(
-                "/api/v1/user/projects/activity",
-
-            )),
+        test("get user activity", getRequest("/api/v1/user/projects/activity")),
         test(
             "get workspace tensorboards",
-            () =>
-                getRequest(
-                    `/api/v1/tensorboards?sortBy=SORT_BY_UNSPECIFIED&orderBy=ORDER_BY_UNSPECIFIED&workspaceId=${sD?.workspace.id}`,
-
-                ),
+            getRequest(
+                `/api/v1/tensorboards?sortBy=SORT_BY_UNSPECIFIED&orderBy=ORDER_BY_UNSPECIFIED&workspaceId=${sD?.workspace.id}`,
+            ),
             !!sD?.workspace.id,
         ),
         test(
             "get workspace shells",
-            () =>
-                getRequest(
-                    `/api/v1/shells?sortBy=SORT_BY_UNSPECIFIED&orderBy=ORDER_BY_UNSPECIFIED&workspaceId=${sD?.workspace.id}`,
-
-                ),
+            getRequest(
+                `/api/v1/shells?sortBy=SORT_BY_UNSPECIFIED&orderBy=ORDER_BY_UNSPECIFIED&workspaceId=${sD?.workspace.id}`,
+            ),
             !!sD?.workspace.id,
         ),
         test(
             "get workspace notebooks",
-            () =>
-                getRequest(
-                    `/api/v1/notebooks?limit=1000&workspaceId=${sD?.workspace.id}`,
-
-                ),
+            getRequest(
+                `/api/v1/notebooks?limit=1000&workspaceId=${sD?.workspace.id}`,
+            ),
             !!sD?.workspace.id,
         ),
         test(
             "get workspace commands",
-            () =>
-                getRequest(
-                    `/api/v1/shells?limit=1000&workspaceId=${sD?.workspace.id}`,
-
-                ),
+            getRequest(`/api/v1/shells?limit=1000&workspaceId=${sD?.workspace.id}`),
             !!sD?.workspace.id,
         ),
         test(
             "get workspace projects",
-            () =>
-                getRequest(
-                    `/api/v1/workspaces/${sD?.workspace.id}/projects`,
-
-                ),
+            getRequest(`/api/v1/workspaces/${sD?.workspace.id}/projects`),
             !!sD?.workspace.id,
         ),
-        test("get webhooks", () =>
-            getRequest("/api/v1/webhooks")),
+        test("get webhooks", getRequest("/api/v1/webhooks")),
         test(
             "get project metric ranges",
-            () =>
-                getRequest(
-                    `/api/v1/projects/${sD?.workspace.projectId}/experiments/metric-ranges`,
-
-                ),
+            getRequest(
+                `/api/v1/projects/${sD?.workspace.projectId}/experiments/metric-ranges`,
+            ),
             !!sD?.workspace.projectId,
         ),
-        test("get project columns", () =>
-            getRequest(`/api/v1/projects/${sD?.workspace.projectId}/columns`)),
+        test(
+            "get project columns",
+            getRequest(`/api/v1/projects/${sD?.workspace.projectId}/columns`),
+        ),
         test(
             "search experiments",
-            () =>
-                getRequest(
-                    `/api/v1/experiments-search?projectId=${sD?.workspace.projectId}`,
-
-                ),
+            getRequest(
+                `/api/v1/experiments-search?projectId=${sD?.workspace.projectId}`,
+            ),
             !!sD?.workspace.projectId,
         ),
-        test("get model labels", () =>
-            getRequest("/api/v1/model/labels")),
+        test("get model labels", getRequest("/api/v1/model/labels")),
         test(
             "get model versions",
-            () =>
-                getRequest(
-                    `/api/v1/models/${sD?.model.name}/versions`,
-
-                ),
+            getRequest(`/api/v1/models/${sD?.model.name}/versions`),
             !!sD?.model.name,
         ),
         test(
             "get model version",
-            () =>
-                getRequest(
-                    `/api/v1/models/${sD?.model.name}/versions/${sD?.model.versionNum}`,
-
-                ),
+            getRequest(
+                `/api/v1/models/${sD?.model.name}/versions/${sD?.model.versionNum}`,
+            ),
             !!sD?.model.versionNum && !!sD?.model.name,
         ),
         test(
             "get trial",
-            () =>
-                getRequest(
-                    `/api/v1/trials/${sD?.trial.id}`,
-
-                ),
+            getRequest(`/api/v1/trials/${sD?.trial.id}`),
             !!sD?.trial.id,
         ),
         test(
             "get trial workloads",
-            () =>
-                getRequest(
-                    `/api/v1/trials/${sD?.trial.id}/workloads`,
-
-                ),
+            getRequest(`/api/v1/trials/${sD?.trial.id}/workloads`),
             !!sD?.trial.id,
         ),
         test(
             "get trial logs",
-            () =>
-                getRequest(
-                    `/api/v1/trials/${sD?.trial.id}/logs/fields?`,
-
-                ),
+            getRequest(`/api/v1/trials/${sD?.trial.id}/logs/fields?`),
             !!sD?.trial.id,
         ),
         test(
             "get experiment",
-            () =>
-                getRequest(
-                    `/api/v1/experiments/${sD?.experiment.id}`,
-
-                ),
+            getRequest(`/api/v1/experiments/${sD?.experiment.id}`),
             !!sD?.experiment.id,
         ),
         test(
             "get experiment metric names",
-            () =>
-                getRequest(
-                    `/api/v1/experiments/metrics-stream/metric-names?ids=${sD?.experiment.id}`,
-
-                ),
+            getRequest(
+                `/api/v1/experiments/metrics-stream/metric-names?ids=${sD?.experiment.id}`,
+            ),
             !!sD?.experiment.id,
         ),
         test(
             "get experiment metric batches",
-            () =>
-                getRequest(
-                    `/api/v1/experiments/${sD?.experiment.id}/metrics-stream/batches?metricName=${sD?.experiment.metricName}&metricType=${sD?.experiment.metricType}`,
-
-                ),
+            getRequest(
+                `/api/v1/experiments/${sD?.experiment.id}/metrics-stream/batches?metricName=${sD?.experiment.metricName}&metricType=${sD?.experiment.metricType}`,
+            ),
             !!sD?.experiment.id &&
             !!sD?.experiment.metricName &&
             !!sD?.experiment.metricType,
         ),
         test(
             "get experiment trials sample",
-            () =>
-                getRequest(
-                    `/api/v1/experiments/${sD?.experiment.id}/metrics-stream/trials-sample?metricName=${sD?.experiment.metricName}&metricType=${sD?.experiment.metricType}`,
-
-                ),
+            getRequest(
+                `/api/v1/experiments/${sD?.experiment.id}/metrics-stream/trials-sample?metricName=${sD?.experiment.metricName}&metricType=${sD?.experiment.metricType}`,
+            ),
             !!sD?.experiment.id &&
             !!sD?.experiment.metricName &&
             !!sD?.experiment.metricType,
         ),
         test(
             "get experiment trials snapshot",
-            () =>
-                getRequest(
-                    `/api/v1/experiments/${sD?.experiment.id}/metrics-stream/trials-snapshot?metricName=${sD?.experiment.metricName}&metricType=${sD?.experiment.metricType}&batchesProcessed=${sD?.experiment.batches}&batchesMargin=${sD?.experiment.batchesMargin}`,
-
-                ),
+            getRequest(
+                `/api/v1/experiments/${sD?.experiment.id}/metrics-stream/trials-snapshot?metricName=${sD?.experiment.metricName}&metricType=${sD?.experiment.metricType}&batchesProcessed=${sD?.experiment.batches}&batchesMargin=${sD?.experiment.batchesMargin}`,
+            ),
             !!sD?.experiment.id &&
             !!sD?.experiment.metricName &&
             !!sD?.experiment.metricType &&
@@ -310,127 +241,72 @@ const getloadTests = (
         ),
         test(
             "get experiment trials",
-            () =>
-                getRequest(
-                    `/api/v1/experiments/${sD?.experiment.id}/trials`,
-
-                ),
+            getRequest(`/api/v1/experiments/${sD?.experiment.id}/trials`),
             !!sD?.experiment.id,
         ),
         test(
             "get trials time series",
-            () =>
-                getRequest(
-                    `/api/v1/trials/time-series?trialIds=${sD?.trial.id}&startBatches=0&metricType=METRIC_TYPE_UNSPECIFIED`,
-
-                ),
+            getRequest(
+                `/api/v1/trials/time-series?trialIds=${sD?.trial.id}&startBatches=0&metricType=METRIC_TYPE_UNSPECIFIED`,
+            ),
             !!sD?.trial.id,
         ),
         test(
             "get experiment file tree",
-            () =>
-                getRequest(
-                    `/api/v1/experiments/${sD?.experiment.id}/file_tree`,
-
-                ),
+            getRequest(`/api/v1/experiments/${sD?.experiment.id}/file_tree`),
             !!sD?.experiment.id,
         ),
-        test("get experiments", () =>
-            getRequest(
-                `/api/v1/experiments?showTrialData=true`,
-
-            )),
-        test("get master logs", () =>
-            getRequest(
-                `/api/v1/master/logs?offset=-1&limit=0`,
-
-            )),
-        test("get resource allocations", () =>
+        test(
+            "get experiments",
+            getRequest(`/api/v1/experiments?showTrialData=true`),
+        ),
+        test(
+            "get master logs",
+            getRequest(`/api/v1/master/logs?offset=-1&limit=0`),
+        ),
+        test(
+            "get resource allocations",
             getRequest(
                 `/api/v1/resources/allocation/aggregated?startDate=2000-01-01&endDate=${new Date()
                     .toJSON()
                     .slice(0, 10)}&period=RESOURCE_ALLOCATION_AGGREGATION_PERIOD_DAILY`,
-
-            )),
-        test("get tasks", () =>
-            getRequest(`/api/v1/tasks`)),
-        test("get task count", () =>
-            getRequest(`/api/v1/tasks/count`)),
-        test(
-            "get task",
-            () =>
-                getRequest(`/api/v1/tasks/${sD?.task.id}`),
-            !!sD?.task.id,
+            ),
         ),
+        test("get tasks", getRequest(`/api/v1/tasks`)),
+        test("get task count", getRequest(`/api/v1/tasks/count`)),
+        test("get task", getRequest(`/api/v1/tasks/${sD?.task.id}`), !!sD?.task.id),
         test(
             "get task log fields",
-            () =>
-                getRequest(
-                    `/api/v1/tasks/${sD?.task.id}/logs/fields`,
-
-                ),
+            getRequest(`/api/v1/tasks/${sD?.task.id}/logs/fields`),
             !!sD?.task.id,
         ),
         test(
             "get task logs",
-            () =>
-                getRequest(
-                    `/api/v1/tasks/${sD?.task.id}/logs`,
-
-                ),
+            getRequest(`/api/v1/tasks/${sD?.task.id}/logs`),
             !!sD?.task.id,
         ),
         test(
             "get permissions summary",
-            () =>
-                getRequest("/api/v1/permissions-summary"),
+            getRequest("/api/v1/permissions-summary"),
             RBAC_ENABLED,
         ),
-        test(
-            "search groups",
-            () => getRequest("/api/groups/search"),
-            RBAC_ENABLED,
-        ),
+        test("search groups", getRequest("/api/groups/search"), RBAC_ENABLED),
         test(
             "get workspace roles",
-            () =>
-                getRequest(
-                    `/api/v1/roles/workspace/${sD?.workspace.id}`,
-
-                ),
+            getRequest(`/api/v1/roles/workspace/${sD?.workspace.id}`),
             RBAC_ENABLED && !!sD?.workspace.id,
         ),
         test(
             "get roles by assignability",
-            () =>
-                getRequest(
-                    "/api/v1/roles/search/by-assignability",
-
-                ),
+            getRequest("/api/v1/roles/search/by-assignability"),
             RBAC_ENABLED,
         ),
-        test(
-            "get group",
-            () => getRequest(`/v1/groups/{groupId}`),
-            RBAC_ENABLED,
-        ),
-        test(
-            "search groups",
-            () => getRequest("/api/v1/groups/search"),
-            RBAC_ENABLED,
-        ),
-        test(
-            "search roles",
-            () => getRequest(`/api/v1/roles-search`),
-            RBAC_ENABLED,
-        ),
+        test("get group", getRequest(`/v1/groups/{groupId}`), RBAC_ENABLED),
+        test("search groups", getRequest("/api/v1/groups/search"), RBAC_ENABLED),
+        test("search roles", getRequest(`/api/v1/roles-search`), RBAC_ENABLED),
         test(
             "search roles by group",
-            () =>
-                getRequest(
-                    `/api/v1/roles/search/by-group/{groupId}`,
-
-                ),
+            getRequest(`/api/v1/roles/search/by-group/{groupId}`),
             RBAC_ENABLED,
         ),
     ];
