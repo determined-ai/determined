995050f870abeed282b403b53d0407fa2a797c5c
Jerry J. Harrow <84593277+jerryharrow@users.noreply.github.com>
2023-09-19 15:00:18 -0400

    chore: Improve failure diagnostics in shell test [FE-216]
    We are getting spurious failures of this test and it provide
    no info when it fails.  On failure it also hangs until the
    shell times out since the shell is still executing and therefore
    doesn't close the stream we are reading.   Add an exit to force
    an EOF so the test fails quickly on failure.

--- e2e_tests/tests/command/test_shell.py
+++ e2e_tests/tests/command/test_shell.py
@@ -12,10 +12,16 @@ def test_start_and_write_to_shell(tmp_path: Path) -> None:
     with cmd.interactive_command("shell", "start") as shell:
         # Call our cli to ensure that PATH and PYTHONUSERBASE are properly set.
         shell.stdin.write(b"COLUMNS=80 det --version\n")
+        # Exit the shell, so we can read output below until EOF instead of timeout
+        shell.stdin.write(b"exit\n")
         shell.stdin.close()
 
+        lines = ""
         for line in shell.stdout:
             if str(det.__version__) in line:
                 break
+            lines += "OUTPUT:" + line
         else:
-            pytest.fail(f"Did not find expected input {det.__version__} in shell stdout.")
+            pytest.fail(
+                f"Did not find expected input {det.__version__} in shell stdout." + lines + "\n"
+            )
