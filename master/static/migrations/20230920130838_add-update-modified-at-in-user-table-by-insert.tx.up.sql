3a8805356997fe751c6a731b3d9b089703747876
Keita Nonaka <keita.nonaka@hpe.com>
2023-09-20 15:19:34 -0700

    fix: return value properly

--- master/static/migrations/20230920130838_add-update-modified-at-in-user-table-by-insert.tx.up.sql
+++ master/static/migrations/20230920130838_add-update-modified-at-in-user-table-by-insert.tx.up.sql
@@ -1,3 +1,16 @@
+CREATE OR REPLACE FUNCTION public.set_modified_time ()
+    RETURNS TRIGGER
+    AS $$
+BEGIN
+    IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE') THEN
+        NEW.modified_at := now();
+        RETURN NEW;
+    END IF;
+    RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
 DROP TRIGGER IF EXISTS autoupdate_users_modified_at on users;
 
 CREATE TRIGGER autoupdate_users_modified_at
