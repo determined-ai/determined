bd1cf0d3baa2df524f1be2d598932cf1a1216a35
Eric Liu <eric.liu@hpe.com>
2023-09-22 13:52:25 -0700

    add missing socketlike file

---
+++ master/internal/api/socketlike.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/gorilla/websocket"
+)
+
+type ReadWriter interface {
+	ReadJSON(interface{}) error
+	Write(interface{}) error
+}
+
+type WrappedWebsocket struct {
+	*websocket.Conn
+}
+
+func (w *WrappedWebsocket) Write(msg interface{}) error {
+	pm, ok := msg.(*websocket.PreparedMessage)
+	if !ok {
+		return fmt.Errorf("received message that is not a prepared message")
+	}
+	err := w.WritePreparedMessage(pm)
+	if err != nil {
+		return err
+	}
+	return nil
+}
