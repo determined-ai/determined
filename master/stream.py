8ad86593c1cb8ae67ce7dcbda82263f5972f9c64
Corban Beaird <corban.beaird@hpe.com>
2023-09-29 11:22:42 -0600

    feat: adds permission filter, online appearance/disappearance, and supervisor [DET-9774] (#7844)

--- master/stream.py
+++ master/stream.py
@@ -1,42 +1,57 @@
 import lomond
 from lomond import events
 
-url = "http://localhost:8080"
+from determined.experimental import client
+
+url = "localhost:8080"
+
 
 # steal determined token
-from determined.experimental import client
 client.login(url)
+print(f"Logged in as: {client._determined._session._auth.session.username}")
 token = client._determined._session._auth.session.token
 
-_state = 0
-def late_sub(ws):
-    global _state
-    _state += 1
-    if _state == 3:
-        print("------ updating subscription ------------")
-        ws.send_binary(
-            b'{"add": {"trials": {"trial_ids": [11]}}, "drop": {"trials": {"trial_ids": [1]}}}'
-        )
+trials = input("Submit array of trial_id values to subscribe to (default: [1]): ")
+if trials == "":
+    trials = "[1]"
 
-ws = lomond.WebSocket(f"{url}/stream")
-ws.add_header(b"Authorization", f"Bearer {token}".encode('utf8'))
-try:
-    for event in ws.connect():
-        if isinstance(event, events.Binary):
-            print(event.data.decode('utf8'))
-            late_sub(ws)
-        elif isinstance(event, events.Text):
-            print(event.text.strip())
-            late_sub(ws)
-        elif isinstance(event, events.Ready):
-            print("ready")
-            ws.send_binary(
-                b'{"subscribe": {"trials": {"trial_ids": [1], "since": 1000}},'
-                b' "known": {"trials": "1,99-110,1000-8000"}}'
-            )
-        elif isinstance(event, (events.ConnectFail, events.Rejected, events.ProtocolError)):
-            raise Exception(f"connection failed: {event}")
-        elif isinstance(event, (events.Closing, events.Disconnected)):
-            break
-except KeyboardInterrupt:
-    print("")
+
+def stream_loop():
+    max_retries = 5
+    num_retries = 0
+
+    ws = lomond.WebSocket(f"http://{url}/stream")
+    ws.add_header(b"Authorization", f"Bearer {token}".encode("utf8"))
+    try:
+        for event in ws.connect():
+            if isinstance(event, events.Binary):
+                print(event.data.decode("utf8"))
+            elif isinstance(event, events.Text):
+                print(event.text.strip())
+            elif isinstance(event, events.Ready):
+                print("ready")
+                payload = (
+                    '{"subscribe": {"trials": {"trial_ids": '
+                    + str(trials)
+                    + ', "since": 1000}}, "known": {"trials": "1,99-110,1000-8000"}}'
+                )
+                ws.send_binary(payload.encode())
+            elif isinstance(
+                event, (events.ConnectFail, events.Rejected, events.ProtocolError)
+            ):
+                print("Connection failed")
+                raise Exception(f"connection failed: {event}")
+            elif isinstance(event, (events.Closing, events.Disconnected)):
+                print("Connection closed...attempting reconnect")
+                num_retries += 1
+                if num_retries < max_retries:
+                    stream_loop()
+                else:
+                    print("exceeded max retries")
+    except KeyboardInterrupt:
+        print("")
+    except Exception as e:
+        print(e)
+
+
+stream_loop()
