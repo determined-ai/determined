d9b36c22d28ed7db959acc05678775878a0e4e5e
Jagadeesh Madagundi <jagadeesh545@gmail.com>
2023-09-29 12:25:56 -0500

    fix: Remove the unexposed GetJobQStats from RM interface and all RMs

--- master/internal/mocks/rm.go
+++ master/internal/mocks/rm.go
@@ -394,32 +394,6 @@ func (_m *ResourceManager) GetJobQ(_a0 actor.Messenger, _a1 sproto.GetJobQ) (map
 	return r0, r1
 }
 
-// GetJobQStats provides a mock function with given fields: _a0, _a1
-func (_m *ResourceManager) GetJobQStats(_a0 actor.Messenger, _a1 sproto.GetJobQStats) (*jobv1.QueueStats, error) {
-	ret := _m.Called(_a0, _a1)
-
-	var r0 *jobv1.QueueStats
-	var r1 error
-	if rf, ok := ret.Get(0).(func(actor.Messenger, sproto.GetJobQStats) (*jobv1.QueueStats, error)); ok {
-		return rf(_a0, _a1)
-	}
-	if rf, ok := ret.Get(0).(func(actor.Messenger, sproto.GetJobQStats) *jobv1.QueueStats); ok {
-		r0 = rf(_a0, _a1)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(*jobv1.QueueStats)
-		}
-	}
-
-	if rf, ok := ret.Get(1).(func(actor.Messenger, sproto.GetJobQStats) error); ok {
-		r1 = rf(_a0, _a1)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
 // GetJobQueueStatsRequest provides a mock function with given fields: _a0, _a1
 func (_m *ResourceManager) GetJobQueueStatsRequest(_a0 actor.Messenger, _a1 *apiv1.GetJobQueueStatsRequest) (*apiv1.GetJobQueueStatsResponse, error) {
 	ret := _m.Called(_a0, _a1)
