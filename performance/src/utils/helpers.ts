751f3db3fa71e66abc941cdadf2ffe5b923c9105
julian-determined-ai <julian.glover@hpe.com>
2023-10-02 12:42:26 -0500

    formatting

--- performance/src/utils/helpers.ts
+++ performance/src/utils/helpers.ts
@@ -7,113 +7,114 @@ import { Results, TestConfiguration, TestGroup } from "./types";
 // and then return the k6 group within the test 'default' method
 // the name is used to build the appropriate group thresholds.
 export const test = (
-    name: string,
-    test_function: () => void,
-    enabled: boolean = true,
+  name: string,
+  test_function: () => void,
+  enabled: boolean = true,
 ): TestGroup => {
-    return { name, group: () => group(name, test_function), enabled };
+  return { name, group: () => group(name, test_function), enabled };
 };
 
 // Return the correct cluster url for a given API endpoint
 export const generateEndpointUrl = (
-    endpoint: string,
-    clusterURL: string,
+  endpoint: string,
+  clusterURL: string,
 ): string => `${clusterURL}${endpoint}`;
 
 export const authenticateVU = (clusterURL: string): string => {
-    if (!__ENV.DET_ADMIN_USERNAME) {
-        throw new Error("Username is required");
-    }
-    if (!__ENV.DET_ADMIN_PASSWORD) {
-    }
-    const loginCredentials = {
-        username: __ENV.DET_ADMIN_USERNAME,
-        password: __ENV.DET_ADMIN_PASSWORD,
-    };
-    const params = {
-        headers: { "Content-Type": "application/json" },
-    };
-    const requestBody = JSON.stringify(loginCredentials);
-    const authResponse = http.post(
-        generateEndpointUrl("/api/v1/auth/login", clusterURL),
-        requestBody,
-        params,
-    );
+  if (!__ENV.DET_ADMIN_USERNAME) {
+    throw new Error("Username is required");
+  }
+  if (!__ENV.DET_ADMIN_PASSWORD) {
+  }
+  const loginCredentials = {
+    username: __ENV.DET_ADMIN_USERNAME,
+    password: __ENV.DET_ADMIN_PASSWORD,
+  };
+  const params = {
+    headers: { "Content-Type": "application/json" },
+  };
+  const requestBody = JSON.stringify(loginCredentials);
+  const authResponse = http.post(
+    generateEndpointUrl("/api/v1/auth/login", clusterURL),
+    requestBody,
+    params,
+  );
 
-    const authResponseJson = authResponse.json() as JSONObject;
-    const token = `Bearer ${authResponseJson.token}`;
-    return token;
+  const authResponseJson = authResponse.json() as JSONObject;
+  const token = `Bearer ${authResponseJson.token}`;
+  return token;
 };
 
 export const testGetRequestor = (
-    clusterURL: string,
-    testConfig?: TestConfiguration,
+  clusterURL: string,
+  testConfig?: TestConfiguration,
 ): ((url: string) => () => void) => {
-    return (url: string) => {
-        return () => {
-            const params = {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `${testConfig?.auth.token}`,
-                },
-            };
-            const res = http.get(generateEndpointUrl(url, clusterURL), params);
-            check(res, { "200 response": (r) => r.status == 200 });
-        };
+  return (url: string) => {
+    return () => {
+      const params = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${testConfig?.auth.token}`,
+        },
+      };
+      const res = http.get(generateEndpointUrl(url, clusterURL), params);
+      check(res, { "200 response": (r) => r.status == 200 });
     };
+  };
 };
 
 const getTestName = (resultName: string): string =>
-    "Test: " +
-    resultName.substring(
-        resultName.lastIndexOf(":") + 1,
-        resultName.indexOf("}"),
-    );
+  "Test: " +
+  resultName.substring(
+    resultName.lastIndexOf(":") + 1,
+    resultName.indexOf("}"),
+  );
 
 export const generateSlackResults = (results: Results): string => {
-    let successfulResultString = "";
-    let failureResultString = "";
-    let infoString = "";
-    let slackOutputString = "";
-    let testFailures = 0;
-    const failedRequests = `Failed HTTP Requests: ${results.metrics["http_req_failed"].values["passes"]} \n`;
-    const failedRequestsPercent = `Percent Failed HTTP Requests: ${Number(results.metrics["http_req_failed"].values["rate"]) * 100
-        }% \n`;
-    const statNames = ["avg", "min", "med", "max", "p(90)", "p(95)"];
-    const thresholdStatKey = "p(95)\u003c1000";
-    infoString = infoString.concat(failedRequests, failedRequestsPercent);
-    Object.keys(results.metrics)
-        .filter((key) => key.includes("group: ::"))
-        .forEach((key) => {
-            const testPassed = results.metrics[key].thresholds[thresholdStatKey]?.ok;
-            const stats = results.metrics[key].values;
-            const groupNameString = `${getTestName(key)} \n`;
-            const statsString = statNames.map(
-                (name) => `${name} = ${Number(stats[name]).toFixed(2)}ms   `,
-            );
-            if (!testPassed) {
-                testFailures++;
-                failureResultString = failureResultString.concat(
-                    groupNameString,
-                    ...statsString,
-                    "\n\n",
-                );
-            } else {
-                successfulResultString = successfulResultString.concat(
-                    groupNameString,
-                    ...statsString,
-                    "\n\n",
-                );
-            }
-        });
-    const failures = `Test Failures: ${testFailures} \n`;
-    infoString = infoString.concat(failures);
-    slackOutputString = slackOutputString.concat(
-        infoString,
-        "\n\nFailed Tests\n\n",
-        failureResultString,
-        "\n\nSuccessful Tests\n\n",
-        successfulResultString,
-    );
-    return slackOutputString;
+  let successfulResultString = "";
+  let failureResultString = "";
+  let infoString = "";
+  let slackOutputString = "";
+  let testFailures = 0;
+  const failedRequests = `Failed HTTP Requests: ${results.metrics["http_req_failed"].values["passes"]} \n`;
+  const failedRequestsPercent = `Percent Failed HTTP Requests: ${
+    Number(results.metrics["http_req_failed"].values["rate"]) * 100
+  }% \n`;
+  const statNames = ["avg", "min", "med", "max", "p(90)", "p(95)"];
+  const thresholdStatKey = "p(95)\u003c1000";
+  infoString = infoString.concat(failedRequests, failedRequestsPercent);
+  Object.keys(results.metrics)
+    .filter((key) => key.includes("group: ::"))
+    .forEach((key) => {
+      const testPassed = results.metrics[key].thresholds[thresholdStatKey]?.ok;
+      const stats = results.metrics[key].values;
+      const groupNameString = `${getTestName(key)} \n`;
+      const statsString = statNames.map(
+        (name) => `${name} = ${Number(stats[name]).toFixed(2)}ms   `,
+      );
+      if (!testPassed) {
+        testFailures++;
+        failureResultString = failureResultString.concat(
+          groupNameString,
+          ...statsString,
+          "\n\n",
+        );
+      } else {
+        successfulResultString = successfulResultString.concat(
+          groupNameString,
+          ...statsString,
+          "\n\n",
+        );
+      }
+    });
+  const failures = `Test Failures: ${testFailures} \n`;
+  infoString = infoString.concat(failures);
+  slackOutputString = slackOutputString.concat(
+    infoString,
+    "\n\nFailed Tests\n\n",
+    failureResultString,
+    "\n\nSuccessful Tests\n\n",
+    successfulResultString,
+  );
+  return slackOutputString;
 };
--- performance/src/utils/types.ts
+++ performance/src/utils/types.ts
@@ -1,98 +1,98 @@
 export interface Authorization {
-    token: string;
+  token: string;
 }
 
 export interface Experiment {
-    id: string;
-    metricName: string;
-    metricType: string;
-    batches: string;
-    batchesMargin: string;
+  id: string;
+  metricName: string;
+  metricType: string;
+  batches: string;
+  batchesMargin: string;
 }
 
 export interface Task {
-    id: string;
+  id: string;
 }
 
 export interface TestConfiguration {
-    auth: Authorization;
-    seededData: SeededData;
+  auth: Authorization;
+  seededData: SeededData;
 }
 
 export interface TestGroup {
-    name: string;
-    group: () => void;
-    enabled?: boolean;
+  name: string;
+  group: () => void;
+  enabled?: boolean;
 }
 
 interface Trial {
-    id: string;
+  id: string;
 }
 
 interface Model {
-    name: string;
-    versionNum: string;
+  name: string;
+  versionNum: string;
 }
 
 export interface SeededData {
-    model: Model;
-    task: Task;
-    trial: Trial;
-    experiment: Experiment;
-    workspace: Workspace;
+  model: Model;
+  task: Task;
+  trial: Trial;
+  experiment: Experiment;
+  workspace: Workspace;
 }
 
 interface Workspace {
-    id: string;
-    projectId: string;
+  id: string;
+  projectId: string;
 }
 
 interface Check {
-    name: string;
-    path: string;
-    id: string;
-    passes: number;
-    fails: number;
+  name: string;
+  path: string;
+  id: string;
+  passes: number;
+  fails: number;
 }
 export interface ResultGroup {
-    name: string;
-    path: string;
-    id: string;
-    group: ResultGroup[];
-    checks: Check[];
+  name: string;
+  path: string;
+  id: string;
+  group: ResultGroup[];
+  checks: Check[];
 }
 
 export interface MetricResults {
-    type: string;
-    contains: string;
-    values: Stats;
-    thresholds: Thresholds;
+  type: string;
+  contains: string;
+  values: Stats;
+  thresholds: Thresholds;
 }
 interface Metric {
-    [name: string]: MetricResults;
+  [name: string]: MetricResults;
 }
 
 interface Stat {
-    avg: number;
-    min: number;
-    med: number;
-    max: number;
-    "p(90)": number;
-    "p(95)": number;
+  avg: number;
+  min: number;
+  med: number;
+  max: number;
+  "p(90)": number;
+  "p(95)": number;
 }
 interface Stats {
-    [name: string]: Stat;
+  [name: string]: Stat;
 }
 
 interface ThresholdResults {
-    ok: boolean;
+  ok: boolean;
 }
 
 interface Thresholds {
-    [name: string]: ThresholdResults;
+  [name: string]: ThresholdResults;
 }
 
 export interface Results {
-    root_group: ResultGroup;
-    metrics: Metric;
+  root_group: ResultGroup;
+  metrics: Metric;
 }
