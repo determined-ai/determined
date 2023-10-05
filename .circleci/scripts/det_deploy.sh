d157513c74d100b18d33173ad13330a0786490bc
Erik Wilson <erik.wilson@hpe.com>
2023-10-05 11:57:43 -0700

    cleanup

--- .circleci/scripts/det_deploy.sh
+++ .circleci/scripts/det_deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 det deploy $@
-res=$?
-if [ $res -ne 0 ] && [ "$2" == "up" ] && [ "${CLUSTER_ID}" != "" ]; then
+status=$?
+# if we tried to bring up a cluster and it failed, recover by trying to bring the cluster down
+if [ $status -ne 0 ] && [ "$2" == "up" ] && [ "${CLUSTER_ID}" != "" ]; then
+    echo "CI error: det deploy $1 up failed, cleaning up cluster ${CLUSTER_ID}" >&2
     det deploy $1 down --cluster-id ${CLUSTER_ID} --yes
 fi
-exit $res
+exit $status
