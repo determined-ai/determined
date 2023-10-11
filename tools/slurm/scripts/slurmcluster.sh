4738abe5a9f9a188d6f94d444633dc13365e486a
CanmingCobble <107056780+CanmingCobble@users.noreply.github.com>
2023-10-11 10:18:37 -0500

    chore: skip copy .launcher.token for agent slurmcluster (#1083)

--- tools/slurm/scripts/slurmcluster.sh
+++ tools/slurm/scripts/slurmcluster.sh
@@ -142,10 +142,12 @@ trap 'kill $TUNNEL_PID' EXIT
 echo "Started bidirectional tunnels to $INSTANCE_NAME"
 
 # Grab launcher token.
-REMOTE_TOKEN_SOURCE=/opt/launcher/jetty/base/etc/.launcher.token
-LOCAL_TOKEN_DEST=$TEMPDIR/.launcher.token
-gcloud compute scp --quiet --zone "us-west1-b" --project "determined-ai" root@$INSTANCE_NAME:$REMOTE_TOKEN_SOURCE $LOCAL_TOKEN_DEST
-echo "Copied launcher token to $LOCAL_TOKEN_DEST"
+if [[ -z $DETERMINED_AGENT ]]; then
+    REMOTE_TOKEN_SOURCE=/opt/launcher/jetty/base/etc/.launcher.token
+    LOCAL_TOKEN_DEST=$TEMPDIR/.launcher.token
+    gcloud compute scp --quiet --zone "us-west1-b" --project "determined-ai" root@$INSTANCE_NAME:$REMOTE_TOKEN_SOURCE $LOCAL_TOKEN_DEST
+    echo "Copied launcher token to $LOCAL_TOKEN_DEST"
+fi
 
 # Build devcluster.yaml.
 gcloud_ssh() {
