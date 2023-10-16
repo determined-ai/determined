f6fcb699b1e646f2fb79f51c3bc3d86661bb73f6
Eric <31023784+eecsliu@users.noreply.github.com>
2023-10-16 15:11:57 -0700

    stream data startup test (#8123)

--- master/test/olddata/pre_remove_steps.go
+++ master/test/olddata/pre_remove_steps.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"github.com/determined-ai/determined/master/internal/db"
+	"github.com/determined-ai/determined/master/test/migrationutils"
 )
 
 //go:embed pre_remove_steps.sql
@@ -19,7 +20,7 @@ const migration001213 = 20200630141158
 
 // PreRemoveStepsExperimentsData holds the migration and useful constants for pre_remove_steps.sql.
 type PreRemoveStepsExperimentsData struct {
-	MustMigrate                  MustMigrateFn
+	MustMigrate                  migrationutils.MustMigrateFn
 	CompletedSingleExpID         int32
 	CompletedAdaptiveSimpleExpID int32
 	CompletedPBTExpID            int32
@@ -30,11 +31,11 @@ type PreRemoveStepsExperimentsData struct {
 // PreRemoveStepsExperiments returns a PreRemoveStepsExperimentsData.
 func PreRemoveStepsExperiments() PreRemoveStepsExperimentsData {
 	mustMigrate := func(t *testing.T, pgdb *db.PgDB, migrationsPath string) {
-		extra := migrationExtra{
+		extra := migrationutils.MigrationExtra{
 			When: migration001213,
 			SQL:  preRemoveStepsSQL,
 		}
-		mustMigrateWithExtras(t, pgdb, migrationsPath, extra)
+		migrationutils.MustMigrateWithExtras(t, pgdb, migrationsPath, extra)
 	}
 	return PreRemoveStepsExperimentsData{
 		MustMigrate:                  mustMigrate,
---
+++ master/test/streamdata/stream_data.go
@@ -0,0 +1,45 @@
+//go:build integration
+// +build integration
+
+package streamdata
+
+import (
+	// embed is only used in comments.
+	_ "embed"
+	"testing"
+
+	"github.com/determined-ai/determined/master/internal/db"
+	"github.com/determined-ai/determined/master/test/migrationutils"
+)
+
+//go:embed stream_trials.sql
+var streamTrialsSQL string
+
+const latestMigration = 20230830174810
+
+// StreamTrialsData holds the migration function and relevant information
+type StreamTrialsData struct {
+	MustMigrate migrationutils.MustMigrateFn
+	ExpID       int
+	JobID       string
+	TaskIDs     []string
+	TrialIDs    []int
+}
+
+// GenerateStreamTrials fills the database with dummy experiment, trials, jobs, and tasks.
+func GenerateStreamTrials() StreamTrialsData {
+	mustMigrate := func(t *testing.T, pgdb *db.PgDB, migrationsPath string) {
+		extra := migrationutils.MigrationExtra{
+			When: latestMigration,
+			SQL:  streamTrialsSQL,
+		}
+		migrationutils.MustMigrateWithExtras(t, pgdb, migrationsPath, extra)
+	}
+	return StreamTrialsData{
+		MustMigrate: mustMigrate,
+		ExpID:       0,
+		JobID:       "test_job",
+		TaskIDs:     []string{"1.1", "1.2", "1.3"},
+		TrialIDs:    []int{1, 2, 3},
+	}
+}
