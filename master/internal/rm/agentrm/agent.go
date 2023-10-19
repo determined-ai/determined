cacd0a5d97427f20f5fb76fcdc5e862a7838f0d3
Max Russell <max.russell@hpe.com>
2023-10-19 13:26:42 -0700

    Revert "Remove extraneous self-shutdown in agent actor when agent shutdown message is sent"
    This reverts commit ec22e6add1492d0fafe69fb62beb47e8a4144d41.

--- master/internal/rm/agentrm/agent.go
+++ master/internal/rm/agentrm/agent.go
@@ -542,6 +542,8 @@ func (a *agent) handleIncomingWSMessage(ctx *actor.Context, msg aproto.MasterMes
 						ErrMsg: aproto.ErrAgentMustReconnect.Error(),
 					},
 				}
+
+				ctx.Self().Stop()
 				return
 			}
 		} else {
