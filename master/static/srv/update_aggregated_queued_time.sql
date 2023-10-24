87c11fb7ea1c61cef7473b066a2cce06ef0c336e
Jerry G <124635598+jgongd@users.noreply.github.com>
2023-10-24 14:21:12 -0400

    fix: inaccurate task queue time [DET-9912] (#8225)
    * fix: inaccurate task queue time [DET-9912]
    
    (cherry picked from commit bb7020a404bedf4f210d2b794567a00ce1669ac7)

--- master/static/srv/update_aggregated_queued_time.sql
+++ master/static/srv/update_aggregated_queued_time.sql
@@ -17,6 +17,9 @@ day_agg AS (
     FROM task_stats, const, allocations
     WHERE
         allocations.allocation_id = task_stats.allocation_id
+        -- Exclude the rows with NULL start_time. When Bun sees StartTime is nil,
+        -- it saves it as 0001-01-01 00:00:00+00.
+        AND task_stats.start_time != '0001-01-01 00:00:00+00:00'::TIMESTAMPTZ
         AND task_stats.end_time >= const.target_date
         AND task_stats.end_time < (const.target_date + interval '1 day')
         AND event_type = 'QUEUED'
