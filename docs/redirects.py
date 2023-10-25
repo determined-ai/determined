381b193149a13b6f55817950be6a19452d205c08
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-10-25 14:58:25 -0700

    return after both checks happen

--- docs/redirects.py
+++ docs/redirects.py
@@ -290,13 +290,15 @@ if __name__ == "__main__":
         exit(1)
 
     if sys.argv[1] == "check":
+        return_code = 0
+
         #check for broken links
         errors = check_links(links)
         if errors:
             print("check failed, errors detected!", file=sys.stderr)
             for e in errors:
                 print(e, file=sys.stderr)
-            exit(1)
+            return_code = 1
 
         #check for dropped urls
         all_urls = set(l.src for l in links).union(all_urls_from_files())
@@ -307,9 +309,9 @@ if __name__ == "__main__":
                 "dropped and should be assigned redirects:\n    " + "\n    ".join(dropped),
                 file=sys.stderr,
             )
-            exit(1)
+            return_code = 1
 
-        exit(0)
+        exit(return_code)
 
     if sys.argv[1] == "publish":
         """
