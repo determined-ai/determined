cad848c7166d7389c39afb1f596d8eb918347a49
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-10-25 13:44:38 -0700

    docs: check for dropped urls in PRs

--- docs/redirects.py
+++ docs/redirects.py
@@ -290,12 +290,25 @@ if __name__ == "__main__":
         exit(1)
 
     if sys.argv[1] == "check":
+        #check for broken links
         errors = check_links(links)
         if errors:
             print("check failed, errors detected!", file=sys.stderr)
             for e in errors:
                 print(e, file=sys.stderr)
             exit(1)
+
+        #check for dropped urls
+        all_urls = set(l.src for l in links).union(all_urls_from_files())
+        dropped = published.difference(all_urls)
+        if dropped:
+            print(
+                "check failed; the following previously-published urls seem to have been "
+                "dropped and should be assigned redirects:\n    " + "\n    ".join(dropped),
+                file=sys.stderr,
+            )
+            exit(1)
+
         exit(0)
 
     if sys.argv[1] == "publish":
