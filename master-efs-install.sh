a3ce2c0607bc1f1bade16850035e53d92e7f3945
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-10-27 00:00:00 -0700

    install efs utils on master

--- master-efs-install.sh
+++ master-efs-install.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 
-# Exit on the first error
 set -e
-
-# Update the system package list and upgrade installed packages
 sudo apt-get update -y
-
-# Install necessary dependencies
 sudo apt-get install -y binutils git
-
-# Clone the efs-utils repository
 git clone https://github.com/aws/efs-utils
-
-# Change directory to efs-utils
-cd efs-utils
-
-# Build the Debian package
+pushd efs-utils
 ./build-deb.sh
-
-# Change the ownership of the .deb file to _apt
 sudo chown _apt:root ./build/amazon-efs-utils*deb
-
-# Install the generated Debian package
 sudo apt-get install -y ./build/amazon-efs-utils*deb
-
-# Change back to the previous directory
-cd ..
-
-# Remove the efs-utils directory
+popd ..
 rm -r efs-utils
