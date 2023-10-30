e4701b244bca8801492f23175129186351e38939
Thiago Dallacqua <104855841+thiagodallacqua-hpe@users.noreply.github.com>
2023-10-30 13:18:09 -0300

    fix: remove Topology from the ResourcePoolDetail page (#8274)

--- webui/react/src/pages/ResourcePool/ResourcepoolDetail.tsx
+++ webui/react/src/pages/ResourcePool/ResourcepoolDetail.tsx
@@ -17,7 +17,6 @@ import useFeature from 'hooks/useFeature';
 import usePermissions from 'hooks/usePermissions';
 import ClusterQueuedChart from 'pages/Cluster/ClusterQueuedChart';
 import JobQueue from 'pages/JobQueue/JobQueue';
-import Topology from 'pages/ResourcePool/Topology';
 import { paths } from 'routes/utils';
 import { getJobQStats } from 'services/api';
 import {
@@ -226,11 +225,6 @@ const ResourcepoolDetailInner: React.FC = () => {
             size={ShirtSize.Large}
           />
         </Section>
-        {!!agents.length && poolname && (
-          <Topology
-            nodes={agents.filter(({ resourcePools }) => resourcePools.includes(poolname))}
-          />
-        )}
         <Section>
           {pool.schedulerType === V1SchedulerType.ROUNDROBIN ? (
             <Section>
