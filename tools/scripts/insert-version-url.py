5dfda2bb64a8f9bb5051773b38c1b13164bf32da
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-11-03 15:21:03 -0700

    linting

--- tools/scripts/insert-version-url.py
+++ tools/scripts/insert-version-url.py
@@ -9,25 +9,25 @@ Usage: add_version_dropdown.py docs/_static/version-switcher/versions.json your.
 import json
 import sys
 
-def create_url_entry(version):
-    url=f"https://docs.determined.ai/{version}/"
 
-    entry= {
-        "version": version,
-        "url": url
-    }
+def create_url_entry(version):
+    url = f"https://docs.determined.ai/{version}/"
+
+    entry = {"version": version, "url": url}
 
     return entry
 
+
 def append_entry(json_file, entry, entry_position=1):
-    with open(json_file, 'rb') as f:
+    with open(json_file, "rb") as f:
         all_urls = json.load(f)
 
     all_urls.insert(entry_position, entry)
 
-    with open(json_file, 'w') as f:
-        json.dump(all_urls, f, indent=4*" ")
-        f.write('\n')
+    with open(json_file, "w") as f:
+        json.dump(all_urls, f, indent=4 * " ")
+        f.write("\n")
+
 
 def insert_url(json_file, version):
     new_entry = create_url_entry(version)
@@ -36,9 +36,8 @@ def insert_url(json_file, version):
     print(f"Added dropdown link for version {version}")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     if len(sys.argv) < 3:
         print(__doc__, file=sys.stderr)
         sys.exit(1)
     insert_url(sys.argv[1], sys.argv[2])
-
