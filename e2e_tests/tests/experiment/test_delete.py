37fc25332d2e7c4bdbc74858312d82d32eacb81f
AmanuelAaron <aaron.amanuel@hpe.com>
2023-11-06 13:23:35 -0500

    add sleep for test

--- e2e_tests/tests/experiment/test_delete.py
+++ e2e_tests/tests/experiment/test_delete.py
@@ -1,35 +1,26 @@
 import pathlib
 import subprocess
+from time import sleep
 import pytest
 
-from determined.common import util
 from tests import config as conf
 from tests import experiment as exp
 
+
 @pytest.mark.e2e_cpu
-def test_delete_experiment_removes_tensorboard_files(tmp_path: pathlib.Path) -> None:
-    config_obj = conf.load_config(conf.fixtures_path("no_op/single-one-short-step.yaml"))
-    config_obj["checkpoint_storage"] = {
-        "type": "directory",
-        "container_path": "/tmp/somepath",
-    }
-    tb_config = {}
-    tb_config["bind_mounts"] = config_obj["bind_mounts"] = [
-        {
-            "host_path": "/tmp/",
-            "container_path": "/tmp/somepath",
-        }
-    ]
-
-    tb_config_path = tmp_path / "tb.yaml"
-    with tb_config_path.open("w") as fout:
-        util.yaml_safe_dump(tb_config, fout)
-
-    experiment_id = exp.run_basic_test_with_temp_config(config_obj, conf.fixtures_path("no_op"), 1)
+def test_delete_experiment_removes_tensorboard_files() -> None:
+    """
+    Start a random experiment, delete the experiment and verify that TensorBoard files are deleted.
+    """
+    config_obj = conf.load_config(conf.tutorials_path("mnist_pytorch/const.yaml"))
+    experiment_id = exp.run_basic_test_with_temp_config(
+        config_obj, conf.tutorials_path("mnist_pytorch"), 1
+    )
 
     command = ["det", "e", "delete", str(experiment_id), "--yes"]
     subprocess.run(command, universal_newlines=True, stdout=subprocess.PIPE, check=True)
 
+    sleep(3)
     # Check if Tensorboard files are deleted
     tb_path = sorted(pathlib.Path("/tmp/determined-cp/").glob("*/tensorboard"))[0]
     tb_path = tb_path / "experiment" / str(experiment_id)
