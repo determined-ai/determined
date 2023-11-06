1fb561ff67ac1f80119c24b29da7d115bb869269
AmanuelAaron <aaron.amanuel@hpe.com>
2023-11-06 10:55:49 -0500

    move test to its own file

--- e2e_tests/tests/command/test_tensorboard.py
+++ e2e_tests/tests/command/test_tensorboard.py
@@ -279,32 +279,3 @@ def test_tensorboard_directory_storage(tmp_path: pathlib.Path) -> None:
         assert tensorboard.task_id is not None
         err = api.task_is_ready(determined_test_session(), tensorboard.task_id)
         assert err is None, err
-
-@pytest.mark.e2e_cpu
-def test_delete_experiment_removes_tensorboard_files(tmp_path: pathlib.Path) -> None:
-    config_obj = conf.load_config(conf.fixtures_path("no_op/single-one-short-step.yaml"))
-    config_obj["checkpoint_storage"] = {
-        "type": "directory",
-        "container_path": "/tmp/somepath",
-    }
-    tb_config = {}
-    tb_config["bind_mounts"] = config_obj["bind_mounts"] = [
-        {
-            "host_path": "/tmp/",
-            "container_path": "/tmp/somepath",
-        }
-    ]
-
-    tb_config_path = tmp_path / "tb.yaml"
-    with tb_config_path.open("w") as fout:
-        util.yaml_safe_dump(tb_config, fout)
-
-    experiment_id = exp.run_basic_test_with_temp_config(config_obj, conf.fixtures_path("no_op"), 1)
-
-    command = ["det", "e", "delete", str(experiment_id), "--yes"]
-    subprocess.run(command, universal_newlines=True, stdout=subprocess.PIPE, check=True)
-
-    # Check if Tensorboard files are deleted
-    tb_path = sorted(pathlib.Path("/tmp/determined-cp/").glob("*/tensorboard"))[0]
-    tb_path = tb_path / "experiment" / str(experiment_id)
-    assert not pathlib.Path(tb_path).exists()
