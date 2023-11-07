7c29928e4c799e0f84428a1aedad91d9c57c5055
Thiago Dallacqua <thiago.menezes-dallacqua@hpe.com>
2023-11-07 17:53:28 -0300

    feat: add text truncation

--- webui/react/src/pages/ResourcePool/Topology.tsx
+++ webui/react/src/pages/ResourcePool/Topology.tsx
@@ -1,4 +1,5 @@
-import React, { PropsWithChildren } from 'react';
+import Tooltip from 'hew/Tooltip';
+import React, { PropsWithChildren, useEffect, useRef, useState } from 'react';
 
 import Section from 'components/Section';
 import { Agent, Resource } from 'types';
@@ -15,6 +16,8 @@ interface Props {
 }
 
 const NodeElement: React.FC<PropsWithChildren<NodeElementProps>> = ({ name, slots }) => {
+  const [containerWidth, setContainerWidth] = useState(0);
+  const slotsContainer = useRef<HTMLSpanElement>(null);
   const singleSlot = slots.length === 1;
   const fewSlot = slots.length === 2;
   const styles = [css.nodeSlot];
@@ -22,10 +25,27 @@ const NodeElement: React.FC<PropsWithChildren<NodeElementProps>> = ({ name, slot
   if (singleSlot) styles.push(css.singleSlot);
   if (fewSlot) styles.push(css.fewSlot);
 
+  useEffect(() => {
+    setContainerWidth(slotsContainer.current?.getBoundingClientRect().width || 0);
+  }, []);
+
   return (
     <div className={css.node}>
-      <span className={css.nodeName}>{name}</span>
-      <span className={css.nodeCluster}>
+      <Tooltip content={name}>
+        {name.length > 5 ? (
+          <span className={css.nodeName}>
+            <span className={css.firstPart} style={{ maxWidth: `${containerWidth * 0.5}px` }}>
+              {name}
+            </span>
+            <span className={css.lastPart} style={{ maxWidth: `${containerWidth * 0.4}px` }}>
+              {name}
+            </span>
+          </span>
+        ) : (
+          <span className={css.nodeName}>{name}</span>
+        )}
+      </Tooltip>
+      <span className={css.nodeCluster} ref={slotsContainer}>
         {slots.map(({ enabled }, idx) => (
           <span className={`${styles.join(' ')} ${enabled ? css.active : ''}`} key={`slot${idx}`} />
         ))}
