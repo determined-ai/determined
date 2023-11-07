368d15df6d8b4d20dc7b7bad27d0254dd085e66b
Anda <anda@determined.ai>
2023-11-07 08:54:16 -0800

    fix pytorch_amp test

--- harness/tests/experiment/fixtures/pytorch_amp/apex_amp_model_def.py
+++ harness/tests/experiment/fixtures/pytorch_amp/apex_amp_model_def.py
@@ -2,6 +2,7 @@
 This example demonstrates how to modify a model to use PyTorch's support for
 NVIDIA APEX in Determined.
 """
+import typing
 
 from train import MNistTrial
 
@@ -9,8 +10,8 @@ from determined.pytorch import PyTorchTrialContext
 
 
 class MNistApexAMPTrial(MNistTrial):
-    def __init__(self, context: PyTorchTrialContext) -> None:
-        super().__init__(context)
+    def __init__(self, context: PyTorchTrialContext, hparams: typing.Optional[typing.Dict]) -> None:
+        super().__init__(context=context, hparams=hparams)
         self.model, self.optimizer = self.context.configure_apex_amp(
             models=self.model, optimizers=self.optimizer
         )
