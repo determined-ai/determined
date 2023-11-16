242ff97efecf3825cc95807e21640cb21ed875a6
Nick Doiron <nick.doiron@hpe.com>
2023-11-16 10:30:27 -0600

    fix: Wrap older modals in theme class [WEB-1824] (#8432)
    antd.Modal in all but one known cases
    
    (cherry picked from commit bf07e6146189c1991d3fc82325e97e3618496978)

--- webui/react/src/pages/JobQueue/ManageJob.tsx
+++ webui/react/src/pages/JobQueue/ManageJob.tsx
@@ -2,6 +2,7 @@ import { List, Modal, Typography } from 'antd';
 import Form from 'hew/Form';
 import Input from 'hew/Input';
 import Select, { Option } from 'hew/Select';
+import { useTheme } from 'hew/Theme';
 import { Loadable } from 'hew/utils/loadable';
 import React, { ReactNode, useCallback, useMemo, useState } from 'react';
 
@@ -86,6 +87,10 @@ const ManageJob: React.FC<Props> = ({
   const isOrderedQ = orderedSchedulers.has(schedulerType);
   const resourcePools = Loadable.getOrElse([], useObservable(clusterStore.resourcePools)); // TODO show spinner when this is loading
   const [selectedPoolName, setSelectedPoolName] = useState(initialPool);
+
+  const {
+    themeSettings: { className: themeClass },
+  } = useTheme();
 
   const details = useMemo(() => {
     interface Item {
@@ -178,6 +183,7 @@ const ManageJob: React.FC<Props> = ({
       mask
       open={true}
       title={'Manage Job ' + truncate(job.jobId, 6, '')}
+      wrapClassName={themeClass}
       onCancel={onFinish}
       onOk={onOk}>
       {isOrderedQ && (
