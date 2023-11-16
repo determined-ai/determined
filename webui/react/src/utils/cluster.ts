4b06ede658f29b2228ca0256892b4247f635bdf5
gt2345 <gt2345@columbia.edu>
2023-11-16 15:09:20 -0600

    fix deprecated k8s name

--- webui/react/src/utils/cluster.ts
+++ webui/react/src/utils/cluster.ts
@@ -5,14 +5,8 @@ export const getSlotContainerStates = (
   resourceType: ResourceType,
   resourcePoolName?: string,
 ): ResourceState[] => {
-  // agents of k8s clusters do not have resource pool name
-  // assume that k8s clusters only have 1 resource pool named 'kubernetes'
   const slotContainerStates = agents
-    .filter((agent) =>
-      resourcePoolName && resourcePoolName !== 'kubernetes'
-        ? agent.resourcePools?.includes(resourcePoolName)
-        : true,
-    )
+    .filter((agent) => (resourcePoolName ? agent.resourcePools?.includes(resourcePoolName) : true))
     .map((agent) => {
       return isDeviceType(resourceType)
         ? agent.resources.filter((res) => res.type === resourceType)
