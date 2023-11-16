99c4cea4b26f279e9fcbe3e9adff2ad4c0970ff1
Aaron Amanuel <amanuel.aaron1@gmail.com>
2023-11-16 16:44:17 -0500

    test: create a test for delete-tensorboards via `det e delete` (#8336)
    * add test for tb file delete w/ exp
    
    * move test to its own file
    
    * add sleep for test
    
    * lint
    
    * increase wait time for delettion
    
    * wait for experiment delete
    
    * change exp file and move assert
    
    * check if files are created in the first place
    
    * include import

---
+++ e2e_tests/tests/experiment/test_delete.py
@@ -0,0 +1,45 @@
+import pathlib
+import subprocess
+import time
+
+import pytest
+
+from determined.common import api
+from tests import config as conf
+from tests import experiment as exp
+
+
+@pytest.mark.e2e_cpu
+def test_delete_experiment_removes_tensorboard_files() -> None:
+    """
+    Start a random experiment, delete the experiment and verify that TensorBoard files are deleted.
+    """
+    config_obj = conf.load_config(conf.fixtures_path("no_op/single-medium-train-step.yaml"))
+    experiment_id = exp.run_basic_test_with_temp_config(config_obj, conf.fixtures_path("no_op"), 1)
+
+    # Check if Tensorboard files are created
+    path = (
+        config_obj["checkpoint_storage"]["host_path"]
+        + "/"
+        + config_obj["checkpoint_storage"]["storage_path"]
+    )
+    tb_path = sorted(pathlib.Path(path).glob("*/tensorboard"))[0]
+    tb_path = tb_path / "experiment" / str(experiment_id)
+    assert pathlib.Path(tb_path).exists()
+
+    command = ["det", "e", "delete", str(experiment_id), "--yes"]
+    subprocess.run(command, universal_newlines=True, stdout=subprocess.PIPE, check=True)
+
+    ticks = 60
+    for i in range(ticks):
+        try:
+            state = exp.experiment_state(experiment_id)
+            if i % 5 == 0:
+                print(f"experiment in state {state} waiting to be deleted")
+            time.sleep(1)
+        except api.errors.NotFoundException:
+            # Check if Tensorboard files are deleted
+            assert not pathlib.Path(tb_path).exists()
+            return
+
+    pytest.fail(f"experiment failed to be deleted after {ticks} seconds")
