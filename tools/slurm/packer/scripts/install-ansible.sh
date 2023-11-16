d080372620577efaa25678b251896c6bc93cc71e
Jerry J. Harrow <84593277+jerryharrow@users.noreply.github.com>
2023-11-16 17:45:47 -0500

    chore: Update GCP images with the latest task environment (#1135)
    To avoid pulling the task containers on every GCP test,
    recreate the latest GCP containers with the new default image.

--- tools/slurm/packer/scripts/install-ansible.sh
+++ tools/slurm/packer/scripts/install-ansible.sh
@@ -1,4 +1,5 @@
 sudo apt update -y
+sudo apt-get update -y --allow-releaseinfo-change
 sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
