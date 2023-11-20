28a8849f833ee3855037c9e40323aad14a3c088c
Ashton Galloway <ashton.galloway@hpe.com>
2023-11-20 17:03:05 -0500

    use fast-check

--- webui/react/src/utils/asValueObject.test.ts
+++ webui/react/src/utils/asValueObject.test.ts
@@ -1,22 +1,17 @@
+import fc, { Arbitrary } from 'fast-check';
 import { is, Map } from 'immutable';
 import * as t from 'io-ts';
+import { isEqual } from 'lodash';
 
 import { Json, JsonObject } from 'types';
 
-import { asValueObject } from './asValueObject';
-
-const jsonCodec: t.Type<Json> = t.recursion('Json', () =>
-  t.union([t.string, t.number, t.boolean, t.null, t.array(jsonCodec), jsonObjectCodec]),
-);
-const jsonObjectCodec: t.Type<JsonObject> = t.recursion('JsonObject', () =>
-  t.record(t.string, jsonCodec),
-);
+import { asValueObjectFactory } from './asValueObject';
 
 const codec = t.type({
   any: t.any,
   array: t.array(t.number),
   boolean: t.boolean,
-  deepObject: jsonObjectCodec,
+  deepObject: JsonObject,
   dictionary: t.record(t.string, t.string),
   nested: t.readonly(
     t.type({
@@ -28,78 +23,113 @@ const codec = t.type({
   string: t.string,
 });
 
-const testVal: t.TypeOf<typeof codec> = {
-  any: '+',
-  array: [1, 2, 3],
-  boolean: true,
-  deepObject: {
-    cool: {
-      beans: [{ bears: true }],
-    },
-    wow: 'woah',
-  },
-  dictionary: { foo: 'bar' },
-  nested: {
-    boolean: false,
-    literal: 'literal',
-  },
-  number: 100,
-  string: 'hewwo :3',
-};
+const asCodec = asValueObjectFactory(codec);
 
-const valueObj = asValueObject(codec, testVal);
-const cloneValueObj = asValueObject(codec, structuredClone(testVal));
-const other = asValueObject(codec, { ...testVal, deepObject: { number: 21 } });
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const otherExtra = asValueObject(codec, { ...testVal, extra: 2 } as any);
-const map = Map({ key: valueObj });
+const arb = fc.record({
+  any: fc.jsonValue({ maxDepth: 0 }),
+  array: fc.array(fc.float()),
+  boolean: fc.boolean(),
+  // weird type mismatch between json types, idk
+  deepObject: fc.dictionary(fc.string(), fc.jsonValue() as fc.Arbitrary<Json>),
+  dictionary: fc.dictionary(fc.string(), fc.string()),
+  nested: fc.record({
+    boolean: fc.boolean(),
+    literal: fc.constant('literal' as const),
+  }),
+  number: fc.float(),
+  string: fc.string(),
+});
+
+const extraArb = fc.record({
+  extra: fc.anything(),
+});
+
+const valueObjArb = arb.map(asCodec);
+
+const cloneValueObjArb = fc.clone(arb, 2).map(([f, s]) => [asCodec(f), asCodec(s)] as const);
+
+const differValueObjArb = fc
+  .tuple(arb, arb)
+  .filter((args) => !isEqual(...args))
+  .map(([f, s]) => [asCodec(f), asCodec(s)] as const);
+
+const extraValueObjArb = fc
+  .tuple(arb, extraArb)
+  .map(([testVal, extra]) => [asCodec(testVal), asCodec({ ...testVal, ...extra })] as const);
+
+const withMapArb = <T, V extends readonly [T, T]>(a: Arbitrary<V>) =>
+  a.map(([f, s]) => [Map({ key: f }), Map({ key: s })] as const);
 
 describe('asValueObject', () => {
   describe('ValueObject#hashcode', () => {
-    it('returns a number for the value object', () => {
-      expect(typeof valueObj.hashCode()).toBe('number');
+    it.concurrent('returns a number for the value object', () => {
+      fc.assert(
+        fc.property(valueObjArb, (valueObj) => {
+          expect(typeof valueObj.hashCode()).toBe('number');
+        }),
+      );
     });
 
-    it('returns different numbers for different value objects', () => {
-      expect(valueObj.hashCode()).not.toBe(other.hashCode());
+    it.concurrent('returns different numbers for different value objects', () => {
+      fc.assert(
+        fc.property(differValueObjArb, ([testVal, other]) => {
+          expect(testVal.hashCode()).not.toEqual(other.hashCode());
+        }),
+      );
     });
 
-    it('only includes values in the codec in the hash', () => {
-      expect(valueObj.hashCode()).toBe(otherExtra.hashCode());
+    it.concurrent('only includes values in the codec in the hash', () => {
+      fc.assert(
+        fc.property(extraValueObjArb, ([testObj, extraObj]) => {
+          expect(testObj.hashCode()).toEqual(extraObj.hashCode());
+        }),
+      );
     });
   });
+
   describe('ValueObject#equals', () => {
-    it('returns true if the objects are equal', () => {
-      expect(valueObj.equals(cloneValueObj)).toBe(true);
-      expect(cloneValueObj.equals(valueObj)).toBe(true);
+    it.concurrent('returns true if the objects are equal', () => {
+      fc.assert(
+        fc.property(cloneValueObjArb, ([valueObj, otherObj]) => {
+          expect(valueObj.equals(otherObj)).toBe(true);
+          expect(otherObj.equals(valueObj)).toBe(true);
+        }),
+      );
     });
 
-    it("returns false if the objects aren't equal", () => {
-      expect(valueObj.equals(other)).toBe(false);
-      expect(other.equals(valueObj)).toBe(false);
+    it.concurrent("returns false if the objects aren't equal", () => {
+      fc.assert(
+        fc.property(differValueObjArb, ([valueObj, otherObj]) => {
+          expect(valueObj.equals(otherObj)).toBe(false);
+          expect(otherObj.equals(valueObj)).toBe(false);
+        }),
+      );
     });
   });
 
   describe('in immutable', () => {
-    it('immutable.is returns true when setting to a clone', () => {
-      expect(is(map, map.set('key', cloneValueObj))).toBe(true);
+    it.concurrent('immutable.is returns true when setting to a clone', () => {
+      fc.assert(
+        fc.property(withMapArb(cloneValueObjArb), ([valueMap, otherMap]) => {
+          expect(is(valueMap, otherMap)).toBe(true);
+        }),
+      );
     });
 
-    it('immutable.is returns false when setting to another object', () => {
-      expect(is(map, map.set('key', other))).toBe(false);
+    it.concurrent('immutable.is returns false when setting to another object', () => {
+      fc.assert(
+        fc.property(withMapArb(differValueObjArb), ([valueMap, otherMap]) => {
+          expect(is(valueMap, otherMap)).toBe(false);
+        }),
+      );
     });
 
-    it('immutable.is returns true when setting to an object with extra props', () => {
-      expect(is(map, map.set('key', otherExtra))).toBe(true);
-    });
-
-    it('immutable.is falls back to value equality on fields where typechecking is disabled', () => {
-      const sameAny = asValueObject(codec, { ...testVal, any: '+' });
-      const newAny = asValueObject(codec, { ...testVal, any: {} });
-      const newAny2 = asValueObject(codec, { ...testVal, any: {} });
-
-      expect(is(map, map.set('key', sameAny))).toBe(true);
-      expect(is(map.set('key', newAny), map.set('key', newAny2))).toBe(false);
+    it.concurrent('immutable.is returns true when setting to an object with extra props', () => {
+      fc.assert(
+        fc.property(withMapArb(extraValueObjArb), ([valueMap, otherMap]) => {
+          expect(is(valueMap, otherMap)).toBe(true);
+        }),
+      );
     });
   });
 });
