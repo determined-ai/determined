06c2cdd7e9ddf77a4728d022514580e66cde7132
Ryan <rb@hpe.com>
2023-11-21 17:01:10 -0700

    ci: unit-test login scenarios, and others
    The login scenarios unit test replaces two e2e tests, and then provides
    even more test coverage beyond those.
    
    The intention of this test is more "clearly document the current
    behavior" rather than "enforce the current behavior forever".
    
    There are decisions we've made, especially regarding the default login
    credentials, that feel suspect to me.  I hope this test is useful for
    highlighting the effects of changing any of those decisions, so we can
    make those changes in an educated way.  I do not intend for this test
    to force us to keep the exact implementation we have forever.
    
    Additional tests here are focused on enabling future simplifications of
    e2e tests, which are currently providing an unhealthy amount of cli test
    coverage

--- e2e_tests/tests/command/test_shell.py
+++ e2e_tests/tests/command/test_shell.py
@@ -3,8 +3,6 @@ from pathlib import Path
 import pytest
 
 import determined as det
-import tests.config as conf
-from tests import api_utils
 from tests import command as cmd
 from tests.cluster import test_users
 
@@ -46,47 +44,4 @@ def test_open_shell() -> None:
         child.sendline("exit")
         child.read()
         child.wait()
-        assert child.exitstatus == 0
-
-
-@pytest.mark.e2e_cpu
-def test_user_flag_shell() -> None:
-    new_user_creds = api_utils.create_test_user(True)
-    new_user_username, _ = new_user_creds
-
-    with test_users.logged_in_user(new_user_creds):
-        with cmd.interactive_command("shell", "start", "--detach") as shell:
-            admin_username, admin_password = conf.ADMIN_CREDENTIALS
-            task_id = shell.task_id
-            assert task_id is not None
-
-            child = test_users.det_spawn(["shell", "open", task_id])
-            child.setecho(True)
-            child.expect(r".*Permanently added.+([0-9a-f-]{36}).+known hosts\.", timeout=180)
-
-            # Verify correct user credentials at the start of entering shell
-            child.sendline("det user whoami")
-            child.expect(f"You are logged in as user '{new_user_username}'")
-
-            # Use -u flag with logged in user and verify no password prompt.
-            child.sendline(f"det -u {new_user_username} user whoami")
-            child.expect(f"You are logged in as user '{new_user_username}'")
-
-            # Use the -u flag with logged out admin user and verify password prompt.
-            child.sendline(f"det -u {admin_username} user logout")
-            child.sendline(f"det -u {admin_username} user whoami")
-            child.expect(f"Password for user '{admin_username}'", timeout=10)
-            child.sendline(str(admin_password))
-            child.expect(f"You are logged in as user '{admin_username}'", timeout=10)
-
-            # Use -u flag with logged out user whose username and token are stored in environment
-            # and verify no password prompt.
-            child.sendline(f"det -u {new_user_username} user logout")
-            child.sendline(f"det -u {new_user_username} user whoami")
-            child.expect(f"You are logged in as user '{new_user_username}'")
-
-            child.sendline("exit")
-            child.read()
-            child.wait()
-
         assert child.exitstatus == 0
