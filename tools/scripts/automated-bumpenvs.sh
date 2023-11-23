0517e1119d79de088001b7c94d1ee300d54f5d69
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-11-22 16:35:00 -0800

    add optional args for update-bumpenvs-yaml

--- tools/scripts/automated-bumpenvs.sh
+++ tools/scripts/automated-bumpenvs.sh
@@ -1,15 +1,45 @@
 #!/bin/bash -ex
 
+# in case these are already set
+ENVS_HASH=''
+DEV_FLAG=''
+NO_CLOUD_IMAGES_FLAG=''
+
+while getopts "h:dn" opt; do
+  case $opt in
+    h)
+      ENVS_HASH="$OPTARG"
+      echo "using the following envs hash: $ENVS_HASH"
+    ;;
+    d)
+      echo "--dev flag will be used"
+      DEV_FLAG="--dev"
+    ;;
+    n)
+      echo "--no-cloud-images flag will be used"
+      NO_CLOUD_IMAGES_FLAG="--no-cloud-images"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+done
+
 # check for dirty changes
 if [[ -n "$(git status --porcelain)" ]]; then
     echo "untracked or dirty files are not allowed, cleanup before running insert-dropdown-url.sh"
     exit 1
 fi
 
-# get latest environments commit
-export ENVS_HASH="$(git ls-remote https://github.com/determined-ai/environments.git -h HEAD -q | cut -f1)"
+if [[ -z $ENVS_HASH ]]; then
+    # get latest environments commit
+    ENVS_HASH="$(git ls-remote https://github.com/determined-ai/environments.git -h HEAD -q | cut -f1)"
+    echo "nah"
+fi
+
 # update bumpenvs yaml with the given hash
-python3 tools/scripts/update-bumpenvs-yaml.py tools/scripts/bumpenvs.yaml ENVS_HASH
+python3 tools/scripts/update-bumpenvs-yaml.py tools/scripts/bumpenvs.yaml ENVS_HASH DEV_FLAG NO_CLOUD_IMAGES_FLAG
+
 # run the bumpenvs procedure
 python3 tools/scripts/bumpenvs.py tools/scripts/bumpenvs.yaml
 
