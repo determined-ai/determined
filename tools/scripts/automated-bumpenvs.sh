f0d86ba61504607e1e1e47c7f4a4e36160ae1b73
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-11-22 16:50:41 -0800

    add docstring

--- tools/scripts/automated-bumpenvs.sh
+++ tools/scripts/automated-bumpenvs.sh
@@ -1,5 +1,23 @@
 #!/bin/bash -ex
 
+: '
+This script wraps the entire bumpenvs procedure.
+It is primarily intended for release-party, but can be
+run by devs as well. This script will default to the latest
+main bumpenvs commit.
+
+Optional arguments:
+-h <string>
+  Commit hash from the bumpenvs repo
+-d <flag>
+  Pass --dev to update-bumpenvs-yaml
+  Must be true if the commit hash is not from environments main
+-n <flag>
+  Pass --no-cloud-images to update-bumpenvs-yaml
+  Must be true if publish-cloud-images was not run in the
+  associated environments dev branch
+'
+
 # in case these are already set
 ENVS_HASH=''
 DEV_FLAG=''
@@ -7,19 +25,20 @@ NO_CLOUD_IMAGES_FLAG=''
 
 while getopts "h:dn" opt; do
   case $opt in
-    h)
-      ENVS_HASH="$OPTARG"
-      echo "using the following envs hash: $ENVS_HASH"
+  h)
+    ENVS_HASH="$OPTARG"
+    echo "using the following envs hash: $ENVS_HASH"
     ;;
-    d)
-      echo "--dev flag will be used"
-      DEV_FLAG="--dev"
+  d)
+    echo "--dev flag will be used"
+    DEV_FLAG="--dev"
     ;;
-    n)
-      echo "--no-cloud-images flag will be used"
-      NO_CLOUD_IMAGES_FLAG="--no-cloud-images"
+  n)
+    echo "--no-cloud-images flag will be used"
+    NO_CLOUD_IMAGES_FLAG="--no-cloud-images"
     ;;
-    \?) echo "Invalid option -$OPTARG" >&2
+  \?)
+    echo "Invalid option -$OPTARG" >&2
     exit 1
     ;;
   esac
@@ -27,14 +46,14 @@ done
 
 # check for dirty changes
 if [[ -n "$(git status --porcelain)" ]]; then
-    echo "untracked or dirty files are not allowed, cleanup before running insert-dropdown-url.sh"
-    exit 1
+  echo "untracked or dirty files are not allowed, cleanup before running insert-dropdown-url.sh"
+  exit 1
 fi
 
 if [[ -z $ENVS_HASH ]]; then
-    # get latest environments commit
-    ENVS_HASH="$(git ls-remote https://github.com/determined-ai/environments.git -h HEAD -q | cut -f1)"
-    echo "using latest envs commit: $ENVS_HASH"
+  # get latest environments commit
+  ENVS_HASH="$(git ls-remote https://github.com/determined-ai/environments.git -h HEAD -q | cut -f1)"
+  echo "using latest envs commit: $ENVS_HASH"
 fi
 
 # update bumpenvs yaml with the given hash
@@ -45,8 +64,8 @@ python3 tools/scripts/bumpenvs.py tools/scripts/bumpenvs.yaml
 
 # check to see if bumpenvs.py published resulted in any file changes or not
 if [[ -z "$(git status --porcelain)" ]]; then
-    echo "no change to environment images found"
-    exit 0
+  echo "no change to environment images found"
+  exit 0
 fi
 git add --update
 git commit -m "chore: bumpenvs to environments commit $ENVS_HASH"
