017e361211d3aa0c56694f59f368030c83a234d0
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-11-22 17:01:41 -0800

    shfmt again

--- tools/scripts/automated-bumpenvs.sh
+++ tools/scripts/automated-bumpenvs.sh
@@ -24,36 +24,36 @@ DEV_FLAG=''
 NO_CLOUD_IMAGES_FLAG=''
 
 while getopts "h:dn" opt; do
-  case $opt in
-  h)
-    ENVS_HASH="$OPTARG"
-    echo "using the following envs hash: $ENVS_HASH"
-    ;;
-  d)
-    echo "--dev flag will be used"
-    DEV_FLAG="--dev"
-    ;;
-  n)
-    echo "--no-cloud-images flag will be used"
-    NO_CLOUD_IMAGES_FLAG="--no-cloud-images"
-    ;;
-  \?)
-    echo "Invalid option -$OPTARG" >&2
-    exit 1
-    ;;
-  esac
+    case $opt in
+        h)
+            ENVS_HASH="$OPTARG"
+            echo "using the following envs hash: $ENVS_HASH"
+            ;;
+        d)
+            echo "--dev flag will be used"
+            DEV_FLAG="--dev"
+            ;;
+        n)
+            echo "--no-cloud-images flag will be used"
+            NO_CLOUD_IMAGES_FLAG="--no-cloud-images"
+            ;;
+        \?)
+            echo "Invalid option -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
 done
 
 # check for dirty changes
 if [[ -n "$(git status --porcelain)" ]]; then
-  echo "untracked or dirty files are not allowed, cleanup before running insert-dropdown-url.sh"
-  exit 1
+    echo "untracked or dirty files are not allowed, cleanup before running insert-dropdown-url.sh"
+    exit 1
 fi
 
 if [[ -z $ENVS_HASH ]]; then
-  # get latest environments commit
-  ENVS_HASH="$(git ls-remote https://github.com/determined-ai/environments.git -h HEAD -q | cut -f1)"
-  echo "using latest envs commit: $ENVS_HASH"
+    # get latest environments commit
+    ENVS_HASH="$(git ls-remote https://github.com/determined-ai/environments.git -h HEAD -q | cut -f1)"
+    echo "using latest envs commit: $ENVS_HASH"
 fi
 
 # update bumpenvs yaml with the given hash
@@ -64,8 +64,8 @@ python3 tools/scripts/bumpenvs.py tools/scripts/bumpenvs.yaml
 
 # check to see if bumpenvs.py published resulted in any file changes or not
 if [[ -z "$(git status --porcelain)" ]]; then
-  echo "no change to environment images found"
-  exit 0
+    echo "no change to environment images found"
+    exit 0
 fi
 git add --update
 git commit -m "chore: bumpenvs to environments commit $ENVS_HASH"
