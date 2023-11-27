c794c5c8ba83c345a9aeb79adccc5f6d4959cf85
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2023-11-27 15:24:46 -0500

    chore: run migration moving trials to view and rename

---
+++ master/static/migrations/20231127110554_trial-to-run-view.tx.down.sql
@@ -0,0 +1,7 @@
+DROP VIEW trials;
+
+ALTER TABLE runs RENAME COLUMN restart_id TO run_id;
+ALTER TABLE runs RENAME COLUMN external_run_id TO external_trial_id;
+ALTER TABLE runs RENAME TO trials;
+
+DROP TABLE dummy;
