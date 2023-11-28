c6bcaa5731bbcd879e1eafc9c544060cf7e49ac8
Mikhail Kardash <mikhail.kardash@hpe.com>
2023-11-27 17:15:59 -0800

    run shfmt

--- tools/scripts/update-docker-tags.sh
+++ tools/scripts/update-docker-tags.sh
@@ -13,16 +13,15 @@ export OLD_VERSION="$1"
 export NEW_VERSION="$2"
 
 # get list of tags to replace via OLD_VERSION in bumpenvs.yaml
-export IMAGES=`grep -o -P "(?<=new: ).*(?=,)" tools/scripts/bumpenvs.yaml | grep $OLD_VERSION`
+export IMAGES=$(grep -o -P "(?<=new: ).*(?=,)" tools/scripts/bumpenvs.yaml | grep $OLD_VERSION)
 
 # update tags on dockerhub
-for OLD_TAG in $IMAGES
-do
-  NEW_TAG="`echo $OLD_TAG | grep -o '.*-'`$NEW_VERSION"
-  echo "Adding $OLD_TAG (clone of $NEW_TAG) to docker repo"
-  docker buildx create OLD_TAG --tag NEW_TAG
-  echo "Replacing $OLD_TAG to $NEW_TAG in bumpenvs.yaml"
-  sed -i -e "s@$OLD_TAG@$NEW_TAG@" tools/scripts/bumpenvs.yaml
+for OLD_TAG in $IMAGES; do
+    NEW_TAG="$(echo $OLD_TAG | grep -o '.*-')$NEW_VERSION"
+    echo "Adding $OLD_TAG (clone of $NEW_TAG) to docker repo"
+    docker buildx create OLD_TAG --tag NEW_TAG
+    echo "Replacing $OLD_TAG to $NEW_TAG in bumpenvs.yaml"
+    sed -i -e "s@$OLD_TAG@$NEW_TAG@" tools/scripts/bumpenvs.yaml
 done
 
 # check to see if redirects.py published resulted in any file changes or not
