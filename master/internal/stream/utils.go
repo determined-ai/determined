2a483c6cb42a46f4a2911a014c68e72ac838cfd3
Eric Liu <eric.liu@hpe.com>
2023-11-29 01:51:17 -0800

    add comment for clarity

--- master/internal/stream/utils.go
+++ master/internal/stream/utils.go
@@ -6,6 +6,8 @@ import (
 	"github.com/determined-ai/determined/master/pkg/model"
 )
 
+// permFilter adds a filter to the provided bun query to filter for workspaces the user has
+// access to.
 func permFilter(
 	q *bun.SelectQuery, accessMap model.AccessScopeSet, accessScopes []model.AccessScopeID,
 ) *bun.SelectQuery {
