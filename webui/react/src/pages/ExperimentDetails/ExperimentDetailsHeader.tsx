ad557160fad2eb619d5031d52e90df8bf646c0ff
Guangqing Tang <40620519+gt2345@users.noreply.github.com>
2023-12-01 11:45:27 -0600

    feat: Enable Retry for multi-trial exp with errored trials (#8518)

--- webui/react/src/pages/ExperimentDetails/ExperimentDetailsHeader.tsx
+++ webui/react/src/pages/ExperimentDetails/ExperimentDetailsHeader.tsx
@@ -33,10 +33,12 @@ import {
   activateExperiment,
   archiveExperiment,
   continueExperiment,
+  getExpTrials,
   openOrCreateTensorBoard,
   pauseExperiment,
   unarchiveExperiment,
 } from 'services/api';
+import { Experimentv1State } from 'services/api-ts-sdk';
 import {
   ExperimentAction as Action,
   CompoundRunState,
@@ -48,7 +50,12 @@ import {
 import { getStateColorThemeVar } from 'utils/color';
 import { getDuration } from 'utils/datetime';
 import handleError, { ErrorLevel, ErrorType } from 'utils/error';
-import { canActionExperiment, getActionsForExperiment } from 'utils/experiment';
+import {
+  canActionExperiment,
+  getActionsForExperiment,
+  isSingleTrialExperiment,
+} from 'utils/experiment';
+import { pluralizer } from 'utils/string';
 import { openCommandResponse } from 'utils/wait';
 
 import css from './ExperimentDetailsHeader.module.scss';
@@ -138,6 +145,8 @@ const ExperimentDetailsHeader: React.FC<Props> = ({
   const [isRunningDelete, setIsRunningDelete] = useState<boolean>(
     experiment.state === RunState.Deleting,
   );
+  const [erroredTrialCount, setErroredTrialCount] = useState<number>();
+  const [canceler] = useState(new AbortController());
   const confirm = useConfirm();
   const classes = [css.state];
 
@@ -232,6 +241,20 @@ const ExperimentDetailsHeader: React.FC<Props> = ({
     openModalHyperparameterSearch();
   }, [openModalHyperparameterSearch]);
 
+  const fetchErroredTrial = useCallback(async () => {
+    // No need to fetch errored trial count if it's single trial experiment or experiment is not completed.
+    if (isSingleTrialExperiment(experiment) || experiment.state !== RunState.Completed) return;
+    const res = await getExpTrials(
+      {
+        id: experiment.id,
+        limit: 1,
+        states: [Experimentv1State.ERROR],
+      },
+      { signal: canceler.signal },
+    );
+    setErroredTrialCount(res.pagination.total);
+  }, [experiment, canceler]);
+
   useEffect(() => {
     setIsRunningArchive(false);
     setIsRunningUnarchive(false);
@@ -240,6 +263,10 @@ const ExperimentDetailsHeader: React.FC<Props> = ({
   useEffect(() => {
     setIsRunningDelete(experiment.state === RunState.Deleting);
   }, [experiment.state]);
+
+  useEffect(() => {
+    fetchErroredTrial();
+  }, [fetchErroredTrial]);
 
   const headerOptions = useMemo(() => {
     const options: Partial<Record<Action, Option>> = {
@@ -295,11 +322,16 @@ const ExperimentDetailsHeader: React.FC<Props> = ({
         disabled: experiment.unmanaged,
         icon: <Icon decorative name="reset" />,
         key: 'retry',
-        label: 'Retry',
+        label: erroredTrialCount ?? 0 > 0 ? `Retry Errored (${erroredTrialCount})` : 'Retry',
         onClick: () => {
           confirm({
             content:
-              'Retry will resume the experiment from where it left off. Any previous progress will be retained.',
+              erroredTrialCount && erroredTrialCount > 0
+                ? `Retry will attempt to complete ${erroredTrialCount} errored ${pluralizer(
+                    erroredTrialCount,
+                    'trial',
+                  )} from their last available ${pluralizer(erroredTrialCount, 'checkpoint')}.`
+                : 'Retry will resume the experiment from where it left off. Any previous progress will be retained.',
             okText: 'Retry',
             onConfirm: async () => {
               await continueExperiment({ id: experiment.id });
@@ -367,7 +399,12 @@ const ExperimentDetailsHeader: React.FC<Props> = ({
       },
     };
 
-    const availableActions = getActionsForExperiment(experiment, headerActions, expPermissions);
+    const availableActions = getActionsForExperiment(
+      experiment,
+      headerActions,
+      expPermissions,
+      erroredTrialCount,
+    );
 
     return availableActions.map((action) => options[action]) as Option[];
   }, [
@@ -385,6 +422,7 @@ const ExperimentDetailsHeader: React.FC<Props> = ({
     experiment,
     fetchExperimentDetails,
     confirm,
+    erroredTrialCount,
   ]);
 
   const jobInfoLinkText = useMemo(() => {
