81c04885abbeda55c40fce07708086fab0324e05
AmanuelAaron <aaron.amanuel@hpe.com>
2023-12-06 13:20:44 -0500

    update cli call

--- harness/determined/cli/task.py
+++ harness/determined/cli/task.py
@@ -146,7 +146,7 @@ def create(args: Namespace) -> None:
 @authentication.required
 def config(args: Namespace) -> None:
     sess = cli.setup_session(args)
-    config_resp = bindings.get_GetTaskConfig(sess, taskId=args.task_id)
+    config_resp = bindings.get_GetGenericTaskConfig(sess, taskId=args.task_id)
     print(config_resp.config)
 
 
