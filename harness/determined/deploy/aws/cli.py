determined-ai/determined

commit c9ba474d8472daf29d18fb96d4b45f3a20536429

chore: add lore help and experimental disclaimer (#8563)
* add auto truncation for full commit hashes given to lore-version
https://hpe-aiatscale.atlassian.net/browse/MLG-1453
https://hpe-aiatscale.atlassian.net/browse/MLG-1416

harness/determined/deploy/aws/cli.py
@@ -30,6 +30,10 @@ def validate_spot_max_price() -> Callable:
     return validate
 
 
+def is_full_git_commit_hash(s: str) -> bool:
+    return bool(re.fullmatch(r"[0-9a-f]{40}", s))
+
+
 def parse_add_tag() -> Callable:
     def parse(s: str) -> Tuple[str, str]:
         try:
@@ -175,6 +179,23 @@ def deploy_aws(command: str, args: argparse.Namespace) -> None:
     if args.deployment_type != constants.deployment_types.LORE:
         if args.lore_version is not None:
             raise ValueError("--lore-version can only be specified for 'lore' deployments")
+    else:
+        print(
+            colored(
+                "Lore deployment type is experimental and not ready for production use.",
+                "yellow",
+            )
+        )
+        assert isinstance(args.lore_version, str)
+        if is_full_git_commit_hash(args.lore_version):
+            short_hash = args.lore_version[:7]
+            print(
+                colored(
+                    f"Lore tags are not full commit hashes. Using {short_hash} instead.",
+                    "yellow",
+                )
+            )
+            args.lore_version = short_hash
 
     if args.deployment_type not in {
         constants.deployment_types.EFS,
@@ -437,7 +458,7 @@ args_description = Cmd(
                     type=str,
                     choices=constants.deployment_types.DEPLOYMENT_TYPES,
                     default=constants.defaults.DEPLOYMENT_TYPE,
-                    help="deployment type",
+                    help="deployment type. Lore support is experimental.",
                 ),
                 Arg(
                     "--inbound-cidr",
@@ -639,7 +660,9 @@ args_description = Cmd(
                 Arg(
                     "--lore-version",
                     type=str,
-                    help=argparse.SUPPRESS,
+                    default="latest",
+                    help="Specifies the version of Lore to install. The value must be a valid"
+                    + " Lore tag available on Docker Hub.",
                 ),
             ],
         ),

commit 51f9f7126c1ccba7c6e52493d4e854d290ca179c

chore: fix bad arg validation for det deploy aws up (#8576)
fixes an issue that was introduced in #8563 wrt argument parsing
when deploying clusters of specific types

harness/determined/deploy/aws/cli.py
@@ -186,8 +186,7 @@ def deploy_aws(command: str, args: argparse.Namespace) -> None:
                 "yellow",
             )
         )
-        assert isinstance(args.lore_version, str)
-        if is_full_git_commit_hash(args.lore_version):
+        if args.lore_version is not None and is_full_git_commit_hash(args.lore_version):
             short_hash = args.lore_version[:7]
             print(
                 colored(
@@ -660,7 +659,6 @@ args_description = Cmd(
                 Arg(
                     "--lore-version",
                     type=str,
-                    default="latest",
                     help="Specifies the version of Lore to install. The value must be a valid"
                     + " Lore tag available on Docker Hub.",
                 ),