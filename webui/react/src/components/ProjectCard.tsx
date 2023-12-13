7884cf6e8e203a4571aafd87a23867ead6b15461
Nick Doiron <nick.doiron@hpe.com>
2023-12-13 14:19:08 -0600

    projectcard

--- webui/react/src/components/ProjectCard.tsx
+++ webui/react/src/components/ProjectCard.tsx
@@ -1,6 +1,8 @@
 import Avatar, { Size } from 'hew/Avatar';
 import Card from 'hew/Card';
+import Column from 'hew/Column';
 import Icon from 'hew/Icon';
+import Row from 'hew/Row';
 import Tooltip from 'hew/Tooltip';
 import { Title, TypographySize } from 'hew/Typography';
 import React from 'react';
@@ -48,40 +50,48 @@ const ProjectCard: React.FC<Props> = ({
       onClick={(e: AnyMouseEvent) => handlePath(e, { path: paths.projectDetails(project.id) })}
       onDropdown={onClick}>
       <div className={classnames.join(' ')}>
-        <Title size={TypographySize.XS} truncate={{ rows: 1, tooltip: true }}>
-          {project.name}
-        </Title>
-        <div className={css.workspaceContainer}>
-          {showWorkspace && project.workspaceId !== 1 && (
-            <Tooltip content={project.workspaceName}>
-              <div className={css.workspaceIcon}>
-                <Avatar palette="muted" size={Size.Small} square text={project.workspaceName} />
+        <Column>
+          <Row width={125}>
+            <Title size={TypographySize.XS} truncate={{ rows: 1, tooltip: true }}>
+              {project.name}
+            </Title>
+          </Row>
+          <Row>
+            <div className={css.workspaceContainer}>
+              {showWorkspace && project.workspaceId !== 1 && (
+                <Tooltip content={project.workspaceName}>
+                  <div className={css.workspaceIcon}>
+                    <Avatar palette="muted" size={Size.Small} square text={project.workspaceName} />
+                  </div>
+                </Tooltip>
+              )}
+            </div>
+          </Row>
+          <Row>
+            <div className={css.footerContainer}>
+              <div className={css.experiments}>
+                <Tooltip
+                  content={
+                    `${project.numExperiments.toLocaleString()}` +
+                    ` experiment${project.numExperiments === 1 ? '' : 's'}`
+                  }>
+                  <Icon name="experiment" size="small" title="Number of experiments" />
+                  <span>{nearestCardinalNumber(project.numExperiments)}</span>
+                </Tooltip>
               </div>
-            </Tooltip>
-          )}
-        </div>
-        <div className={css.footerContainer}>
-          <div className={css.experiments}>
-            <Tooltip
-              content={
-                `${project.numExperiments.toLocaleString()}` +
-                ` experiment${project.numExperiments === 1 ? '' : 's'}`
-              }>
-              <Icon name="experiment" size="small" title="Number of experiments" />
-              <span>{nearestCardinalNumber(project.numExperiments)}</span>
-            </Tooltip>
-          </div>
-          {project.archived ? (
-            <div className={css.archivedBadge}>Archived</div>
-          ) : (
-            project.lastExperimentStartedAt && (
-              <TimeAgo
-                datetime={project.lastExperimentStartedAt}
-                tooltipFormat="[Last experiment started: \n]MMM D, YYYY - h:mm a"
-              />
-            )
-          )}
-        </div>
+              {project.archived ? (
+                <div className={css.archivedBadge}>Archived</div>
+              ) : (
+                project.lastExperimentStartedAt && (
+                  <TimeAgo
+                    datetime={project.lastExperimentStartedAt}
+                    tooltipFormat="[Last experiment started: \n]MMM D, YYYY - h:mm a"
+                  />
+                )
+              )}
+            </div>
+          </Row>
+        </Column>
       </div>
       {contextHolders}
     </Card>
