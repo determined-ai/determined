13169e39ef64420e20506bde7a94a844d2c5dbaf
Sze Wai Yuen <sze-wai.yuen@hpe.com>
2023-12-13 11:48:02 -0800

    Change CLI and constants

--- harness/determined/deploy/aws/constants.py
+++ harness/determined/deploy/aws/constants.py
@@ -3,10 +3,10 @@ class deployment_types:
     SIMPLE_RDS = "simple-rds"
     SECURE = "secure"
     EFS = "efs"
-    LORE = "lore"
+    GENAI = "genai"
     FSX = "fsx"
     GOVCLOUD = "govcloud"
-    DEPLOYMENT_TYPES = [SIMPLE, SECURE, EFS, FSX, GOVCLOUD, SIMPLE_RDS, LORE]
+    DEPLOYMENT_TYPES = [SIMPLE, SECURE, EFS, FSX, GOVCLOUD, SIMPLE_RDS, GENAI]
     TYPE_TAG_KEY = "deployment-type"
 
 
