f7b560491540cc08a7155bb4cafa143bd57d15fe
gt2345 <gt2345@columbia.edu>
2024-01-02 12:10:37 -0600

    fix: use FixedSizeList for column picker

--- webui/react/src/pages/F_ExpList/glide-table/ColumnPickerMenu.tsx
+++ webui/react/src/pages/F_ExpList/glide-table/ColumnPickerMenu.tsx
@@ -9,6 +9,7 @@ import Pivot from 'hew/Pivot';
 import Spinner from 'hew/Spinner';
 import { Loadable } from 'hew/utils/loadable';
 import React, { ChangeEvent, useCallback, useMemo, useState } from 'react';
+import { FixedSizeList as List } from 'react-window';
 
 import { useSettings } from 'hooks/useSettings';
 import {
@@ -162,6 +163,25 @@ const ColumnPickerTab: React.FC<ColumnTabProps> = ({
     [setSearchString],
   );
 
+  const rows = useCallback(
+    ({ index, style }: { index: number; style: React.CSSProperties }) => {
+      const col = filteredColumns[index];
+      return (
+        <div>
+          <div key={col.column} style={style}>
+            <Checkbox
+              checked={checkedColumn.has(col.column)}
+              id={col.column}
+              onChange={handleColumnChange}>
+              {col.displayName || col.column}
+            </Checkbox>
+          </div>
+        </div>
+      );
+    },
+    [filteredColumns, checkedColumn, handleColumnChange],
+  );
+
   return (
     <div>
       <Input
@@ -174,15 +194,9 @@ const ColumnPickerTab: React.FC<ColumnTabProps> = ({
       {totalColumns.length !== 0 ? (
         <Space className={css.columnList} direction="vertical">
           {filteredColumns.length > 0 ? (
-            filteredColumns.map((col) => (
-              <Checkbox
-                checked={checkedColumn.has(col.column)}
-                id={col.column}
-                key={col.column}
-                onChange={handleColumnChange}>
-                {col.displayName || col.column}
-              </Checkbox>
-            ))
+            <List height={360} itemCount={filteredColumns.length} itemSize={30} width={360}>
+              {rows}
+            </List>
           ) : (
             <Message description="No results" icon="warning" />
           )}
