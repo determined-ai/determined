66991b1b9212d08bf2eb84d295877e6ee3073e59
Carolina Calderon <carolina.calderon@hpe.com>
2024-01-05 12:33:42 -0500

    reversing

--- master/static/migrations/20240105121220_drop-exp-columns-git-model-packages.tx.down.sql
+++ master/static/migrations/20240105121220_drop-exp-columns-git-model-packages.tx.down.sql
@@ -1,6 +1,6 @@
 ALTER TABLE public.experiments
-    DROP COLUMN git_remote,
-    DROP COLUMN git_commit,
-    DROP COLUMN git_committer,
-    DROP COLUMN git_commit_date,
-    DROP COLUMN model_packages;
+    ADD COLUMN git_remote character,
+    ADD COLUMN git_commit character,
+    ADD COLUMN git_commit character,
+    ADD COLUMN git_commit_date timestamp without time zone,
+    ADD COLUMN model_packages bytea;
--- master/static/migrations/20240105121220_drop-exp-columns-git-model-packages.tx.up.sql
+++ master/static/migrations/20240105121220_drop-exp-columns-git-model-packages.tx.up.sql
@@ -1,6 +1,6 @@
 ALTER TABLE public.experiments
-    ADD COLUMN git_remote character NULL,
-    ADD COLUMN git_commit character NULL,
-    ADD COLUMN git_commit character NULL,
-    ADD COLUMN git_commit_date timestamp without time zone NULL,
-    ADD COLUMN model_packages bytea NULL;
+    DROP COLUMN git_remote,
+    DROP COLUMN git_commit,
+    DROP COLUMN git_committer,
+    DROP COLUMN git_commit_date,
+    DROP COLUMN model_packages;
