c090181429159b91c87520c451883e74861803f2
Carolina Calderon <carolina.calderon@hpe.com>
2024-01-05 12:44:07 -0500

    add trailing whitespace

--- master/static/migrations/20240105121220_drop-exp-columns-git-model-packages.tx.down.sql
+++ master/static/migrations/20240105121220_drop-exp-columns-git-model-packages.tx.down.sql
@@ -4,3 +4,4 @@ ALTER TABLE public.experiments
     ADD COLUMN git_commit character,
     ADD COLUMN git_commit_date timestamp without time zone,
     ADD COLUMN model_packages bytea;
+    
--- master/static/migrations/20240105121220_drop-exp-columns-git-model-packages.tx.up.sql
+++ master/static/migrations/20240105121220_drop-exp-columns-git-model-packages.tx.up.sql
@@ -4,3 +4,4 @@ ALTER TABLE public.experiments
     DROP COLUMN git_committer,
     DROP COLUMN git_commit_date,
     DROP COLUMN model_packages;
+    
