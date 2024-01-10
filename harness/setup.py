eb0eae8af8daec32bc96442d6cc111de292b81cc
julian-determined-ai <julian.glover@hpe.com>
2024-01-10 09:18:45 -0600

    chore: bump version: 0.27.0-rc4 -> 0.27.0

--- harness/setup.py
+++ harness/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="determined",
-    version="0.27.0-rc4",
+    version="0.27.0",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://determined.ai/",
