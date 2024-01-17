065f11162a69b26bb08db7e3ebfcf2198b75db33
Mikhail Kardash <mikhail.kardash@hpe.com>
2024-01-17 07:56:38 -0800

    chore: bumpenvs 0.27.1

--- master/pkg/schemas/expconf/const.go
+++ master/pkg/schemas/expconf/const.go
@@ -8,7 +8,7 @@ const (
 
 // Default task environment docker image names.
 const (
-	CPUImage  = "determinedai/environments:py-3.9-pytorch-1.12-tf-2.11-cpu-622d512"
-	CUDAImage = "determinedai/environments:cuda-11.3-pytorch-1.12-tf-2.11-gpu-622d512"
+	CPUImage  = "determinedai/environments:py-3.9-pytorch-1.12-tf-2.11-cpu-f66cbce"
+	CUDAImage = "determinedai/environments:cuda-11.3-pytorch-1.12-tf-2.11-gpu-f66cbce"
 	ROCMImage = "determinedai/environments:rocm-5.0-pytorch-1.10-tf-2.7-rocm-622d512"
 )
