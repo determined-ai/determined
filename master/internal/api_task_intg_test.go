6a3a88cfbebca58c6d86947eb386e018858c7dde
AmanuelAaron <aaron.amanuel@hpe.com>
2024-01-18 18:00:20 -0500

    Create Child Task

--- master/internal/api_task_intg_test.go
+++ master/internal/api_task_intg_test.go
@@ -6,24 +6,25 @@ package internal
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/determined-ai/determined/master/pkg/model"
 	"github.com/determined-ai/determined/proto/pkg/apiv1"
-	"github.com/stretchr/testify/require"
 )
 
 func TestGetGenericTaskConfig(t *testing.T) {
 	api, _, ctx := setupAPITest(t, nil)
 
-	expected_config := "{\"test_config\": \"val\"}"
-	task_id := model.NewTaskID()
+	expectedConfig := "{\"test_config\": \"val\"}"
+	taskID := model.NewTaskID()
 
-	task := &model.Task{TaskType: model.TaskTypeGeneric, TaskID: task_id, Config: &expected_config}
+	task := &model.Task{TaskType: model.TaskTypeGeneric, TaskID: taskID, Config: &expectedConfig}
 	require.NoError(t, api.m.db.AddTask(task))
 
 	resp, err := api.GetGenericTaskConfig(ctx, &apiv1.GetGenericTaskConfigRequest{
-		TaskId: string(task_id),
+		TaskId: string(taskID),
 	})
 
 	require.NoError(t, err)
-	require.Equal(t, expected_config, resp.Config)
+	require.Equal(t, expectedConfig, resp.Config)
 }
