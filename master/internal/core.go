2f51abb8a521fc8f63df260aeb4b32f7d5fd09ea
Keita Nonaka <keita.nonaka@hpe.com>
2024-01-23 12:35:02 -0800

    chore: remove `Skipper` in `gzipConfig`

--- master/internal/core.go
+++ master/internal/core.go
@@ -966,17 +966,7 @@ func (m *Master) Run(ctx context.Context, gRPCLogInitDone chan struct{}) error {
 	// Other static files should only be cached for a short period of time
 	cacheFileShortTerm := regexp.MustCompile(`.(antd.\S+(.css)|ico|png|jpe*g|gif|svg)$`)
 
-	// API endpoints
-	apiRegex := regexp.MustCompile(`^/api/.+$`)
-
-	gzipConfig := middleware.GzipConfig{
-		Skipper: func(c echo.Context) bool {
-			reqPath := c.Request().URL.Path
-			return !cacheFileLongTerm.MatchString(reqPath) &&
-				!cacheFileShortTerm.MatchString(reqPath) &&
-				!apiRegex.MatchString(reqPath)
-		},
-	}
+	gzipConfig := middleware.GzipConfig{}
 	m.echo.Use(middleware.GzipWithConfig(gzipConfig))
 
 	m.echo.Use(middleware.AddTrailingSlashWithConfig(middleware.TrailingSlashConfig{
