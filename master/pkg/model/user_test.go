7e937f26197b9fbafdc678f1246f39fb045fad76
Bradley Laney <bradley.laney@hpe.com>
2024-01-24 13:59:17 -0500

    add checks for to proto exhaustiveness/correctness

--- master/pkg/model/user_test.go
+++ master/pkg/model/user_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -16,4 +17,17 @@ func TestUserNonNilLastAuthAtProto(t *testing.T) {
 	expectedTime := time.Now()
 	u := User{LastAuthAt: &expectedTime}
 	require.WithinDuration(t, expectedTime, u.Proto().LastAuthAt.AsTime(), time.Millisecond)
+}
+
+func TestUserWebSetting_Proto(t *testing.T) {
+	in := UserWebSetting{
+		UserID:      UserID(42),
+		Key:         uuid.NewString(),
+		Value:       uuid.NewString(),
+		StoragePath: uuid.NewString(),
+	}
+	out := in.Proto()
+	require.Equal(t, in.Key, out.Key)
+	require.Equal(t, in.Value, out.Value)
+	require.Equal(t, in.StoragePath, out.StoragePath)
 }
