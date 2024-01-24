c0e4a54312f8c9cd201d60e9ac39735ca899c016
Corban Beaird <corban.beaird@hpe.com>
2024-01-24 09:51:29 -0700

    chore: clean up project migrations

--- master/static/migrations/20240116155819_add-projects-trigger.tx.down.sql
+++ master/static/migrations/20240116155819_add-projects-trigger.tx.down.sql
@@ -1,6 +1,6 @@
 DROP TRIGGER IF EXISTS stream_project_trigger_seq ON projects
 DROP TRIGGER IF EXISTS stream_project_trigger_iu ON projects;
-DROP TRIGGER IF EXISTS stream_project_trigger_d ON experiments;
+DROP TRIGGER IF EXISTS stream_project_trigger_d ON projects;
 
 DROP FUNCTION IF EXISTS stream_project_seq_modify;
 DROP FUNCTION IF EXISTS stream_project_notify;
