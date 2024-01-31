38f48a8d3545a162ca229a22279f73eadebd6437
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2024-01-31 15:55:19 -0500

    updated migration

--- master/static/migrations/20240131154725_add-storage-config.tx.down.sql
+++ master/static/migrations/20240131154725_add-storage-config.tx.down.sql
@@ -11,21 +11,21 @@ CREATE OR REPLACE VIEW checkpoints_view AS
         c.state,
         c.resources,
         c.metadata,
-        t.id AS trial_id,
+        r.id AS trial_id,
         e.id AS experiment_id,
         e.config AS experiment_config,
-        t.hparams AS hparams,
+        r.hparams AS hparams,
         s.metrics AS training_metrics,
         v.metrics->'validation_metrics' AS validation_metrics,
         (v.metrics->'validation_metrics'->>(e.config->'searcher'->>'metric'))::float8 AS searcher_metric,
         CAST(c.metadata->>'steps_completed' AS int) as steps_completed,
         c.size
     FROM checkpoints_v2 AS c
-    LEFT JOIN trial_id_task_id AS task ON c.task_id = task.task_id
-    LEFT JOIN trials AS t on t.id = task.trial_id
-    LEFT JOIN experiments AS e on t.experiment_id = e.id
-    LEFT JOIN raw_validations AS v on CAST(c.metadata->>'steps_completed' AS int) = v.total_batches and t.id = v.trial_id AND NOT v.archived
-    LEFT JOIN raw_steps AS s on CAST(c.metadata->>'steps_completed' AS int) = s.total_batches and t.id = s.trial_id AND NOT s.archived;
+    LEFT JOIN run_checkpoints AS rc on rc.checkpoint_id = c.uuid
+    LEFT JOIN runs AS r on r.id = rc.run_id
+    LEFT JOIN experiments AS e on r.experiment_id = e.id
+    LEFT JOIN raw_validations AS v on CAST(c.metadata->>'steps_completed' AS int) = v.total_batches and r.id = v.trial_id AND NOT v.archived
+    LEFT JOIN raw_steps AS s on CAST(c.metadata->>'steps_completed' AS int) = s.total_batches and r.id = s.trial_id AND NOT s.archived;
 
 CREATE OR REPLACE VIEW proto_checkpoints_view AS
     SELECT
