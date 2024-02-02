9a278c3d2dbcda07de678baae37976f36cbcb940
Nicholas Blaskey <nick.blaskey@hpe.com>
2024-02-02 08:36:26 -0500

    perf: improve resource agg performance (#8735)

---
+++ master/internal/api_master_intg_test.go
@@ -0,0 +1,216 @@
+package internal
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/uptrace/bun"
+
+	"github.com/determined-ai/determined/master/internal/db"
+	"github.com/determined-ai/determined/master/pkg/etc"
+	"github.com/determined-ai/determined/proto/pkg/apiv1"
+	"github.com/determined-ai/determined/proto/pkg/masterv1"
+)
+
+// TODO(DET-10109) test and optimize the monthly version.
+func TestResourceAllocationAggregatedDaily(t *testing.T) {
+	require.NoError(t, etc.SetRootPath("../static/srv"))
+
+	pgDB, cleanup := db.MustResolveNewPostgresDatabase(t)
+	defer cleanup()
+	db.MustMigrateTestPostgres(t, pgDB, "file://../static/migrations")
+
+	api, _, ctx := setupAPITest(t, pgDB)
+
+	pgTable := []struct {
+		bun.BaseModel   `bun:"table:resource_aggregates"`
+		Date            string
+		AggregationType string
+		AggregationKey  string
+		Seconds         float64
+	}{
+		{bun.BaseModel{}, "2020-09-02", "total", "total", 1.25},
+		{bun.BaseModel{}, "2020-10-01", "total", "total", 2.25},
+		{bun.BaseModel{}, "2020-11-19", "total", "total", 3.25},
+
+		{bun.BaseModel{}, "2020-10-01", "username", "a", 1.5},
+		{bun.BaseModel{}, "2020-10-01", "username", "b", 2.5},
+		{bun.BaseModel{}, "2020-10-10", "username", "a", 3.5},
+		{bun.BaseModel{}, "2020-10-10", "username", "c", 4.5},
+		{bun.BaseModel{}, "2020-10-11", "username", "c", 5.5},
+		{bun.BaseModel{}, "2020-11-20", "username", "c", 6.5},
+
+		{bun.BaseModel{}, "2020-10-01", "experiment_label", "el_a", 1.75},
+		{bun.BaseModel{}, "2020-10-01", "experiment_label", "el_b", 2.75},
+		{bun.BaseModel{}, "2020-10-19", "experiment_label", "el_a", 3.75},
+		{bun.BaseModel{}, "2020-11-19", "experiment_label", "el_a", 4.75},
+		{bun.BaseModel{}, "2020-11-19", "experiment_label", "el_c", 5.75},
+
+		{bun.BaseModel{}, "2020-10-01", "resource_pool", "rp_a", 1.0},
+		{bun.BaseModel{}, "2020-10-01", "resource_pool", "rp_b", 2.0},
+		{bun.BaseModel{}, "2020-10-18", "resource_pool", "rp_a", 3.0},
+		{bun.BaseModel{}, "2020-11-19", "resource_pool", "rp_b", 4.0},
+	}
+	_, err := db.Bun().NewInsert().Model(&pgTable).Exec(ctx)
+	require.NoError(t, err)
+
+	cases := []struct {
+		name     string
+		start    string
+		end      string
+		expected string
+	}{
+		{"whole time", "2000-01-01", "2099-01-01", `[
+  {
+    "period_start": "2020-09-02",
+    "period": 1,
+    "seconds": 1.25
+  },
+  {
+    "period_start": "2020-10-01",
+    "period": 1,
+    "seconds": 2.25,
+    "by_username": {
+      "a": 1.5,
+      "b": 2.5
+    },
+    "by_experiment_label": {
+      "el_a": 1.75,
+      "el_b": 2.75
+    },
+    "by_resource_pool": {
+      "rp_a": 1,
+      "rp_b": 2
+    }
+  },
+  {
+    "period_start": "2020-10-10",
+    "period": 1,
+    "by_username": {
+      "a": 3.5,
+      "c": 4.5
+    }
+  },
+  {
+    "period_start": "2020-10-11",
+    "period": 1,
+    "by_username": {
+      "c": 5.5
+    }
+  },
+  {
+    "period_start": "2020-10-18",
+    "period": 1,
+    "by_resource_pool": {
+      "rp_a": 3
+    }
+  },
+  {
+    "period_start": "2020-10-19",
+    "period": 1,
+    "by_experiment_label": {
+      "el_a": 3.75
+    }
+  },
+  {
+    "period_start": "2020-11-19",
+    "period": 1,
+    "seconds": 3.25,
+    "by_experiment_label": {
+      "el_a": 4.75,
+      "el_c": 5.75
+    },
+    "by_resource_pool": {
+      "rp_b": 4
+    }
+  },
+  {
+    "period_start": "2020-11-20",
+    "period": 1,
+    "by_username": {
+      "c": 6.5
+    }
+  }
+]`},
+
+		{"just 10/01", "2020-10-01", "2020-10-01", `[
+  {
+    "period_start": "2020-10-01",
+    "period": 1,
+    "seconds": 2.25,
+    "by_username": {
+      "a": 1.5,
+      "b": 2.5
+    },
+    "by_experiment_label": {
+      "el_a": 1.75,
+      "el_b": 2.75
+    },
+    "by_resource_pool": {
+      "rp_a": 1,
+      "rp_b": 2
+    }
+  }
+]`},
+		{"10/01 to 10/10", "2020-10-01", "2020-10-10", `[
+  {
+    "period_start": "2020-10-01",
+    "period": 1,
+    "seconds": 2.25,
+    "by_username": {
+      "a": 1.5,
+      "b": 2.5
+    },
+    "by_experiment_label": {
+      "el_a": 1.75,
+      "el_b": 2.75
+    },
+    "by_resource_pool": {
+      "rp_a": 1,
+      "rp_b": 2
+    }
+  },
+  {
+    "period_start": "2020-10-10",
+    "period": 1,
+    "by_username": {
+      "a": 3.5,
+      "c": 4.5
+    }
+  }
+]`},
+		{"earlier than start", "2000-01-01", "2020-09-02", `[
+  {
+    "period_start": "2020-09-02",
+    "period": 1,
+    "seconds": 1.25
+  }
+]`},
+		{"later than end", "2020-11-20", "2024-01-01", `[
+  {
+    "period_start": "2020-11-20",
+    "period": 1,
+    "by_username": {
+      "c": 6.5
+    }
+  }
+]`},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			resp, err := api.ResourceAllocationAggregated(ctx, &apiv1.ResourceAllocationAggregatedRequest{
+				Period:    masterv1.ResourceAllocationAggregationPeriod_RESOURCE_ALLOCATION_AGGREGATION_PERIOD_DAILY,
+				StartDate: c.start,
+				EndDate:   c.end,
+			})
+			require.NoError(t, err)
+
+			actual, err := json.MarshalIndent(resp.ResourceEntries, "", "  ")
+			require.NoError(t, err)
+
+			require.Equal(t, c.expected, string(actual))
+		})
+	}
+}
--- master/static/srv/get_aggregated_allocation.sql
+++ master/static/srv/get_aggregated_allocation.sql
@@ -1,65 +1,37 @@
-WITH const AS (
-    SELECT daterange($1::date, $2::date, '[]') AS period
-),
-
-days AS (
-    SELECT
-        resource_aggregates.date::date AS period_start,
-        aggregation_type,
-        resource_aggregates.aggregation_key,
-        seconds
-    FROM
-        resource_aggregates,
-        const
-    WHERE
-        -- `@>` determines whether the range contains the time.
-        const.period @> resource_aggregates.date
-),
-
-starts AS (
-    SELECT DISTINCT period_start AS period_start
-    FROM
-        days
+WITH date_series AS (
+  SELECT generate_series(
+    GREATEST($1::date, (SELECT min(date) FROM resource_aggregates)),
+    LEAST($2::date, (SELECT max(date) FROM resource_aggregates)),
+    '1 day'::interval
+  ) AS period_start
 )
-
 SELECT
-    to_char(period_start, 'YYYY-MM-DD') AS period_start,
-    'RESOURCE_ALLOCATION_AGGREGATION_PERIOD_DAILY' AS period,
-    (
-        SELECT seconds
-        FROM
-            days
-        WHERE
-            aggregation_type = 'total'
-            AND days.period_start = starts.period_start
-        LIMIT
-            1
-    ) AS seconds,
-    (
-        SELECT jsonb_object_agg(aggregation_key, seconds)
-        FROM
-            days
-        WHERE
-            aggregation_type = 'username'
-            AND days.period_start = starts.period_start
-    ) AS by_username,
-    (
-        SELECT jsonb_object_agg(aggregation_key, seconds)
-        FROM
-            days
-        WHERE
-            aggregation_type = 'experiment_label'
-            AND days.period_start = starts.period_start
-    ) AS by_experiment_label,
-    (
-        SELECT jsonb_object_agg(aggregation_key, seconds)
-        FROM
-            days
-        WHERE
-            aggregation_type = 'resource_pool'
-            AND days.period_start = starts.period_start
-    ) AS by_resource_pool
+  to_char(ds.period_start, 'YYYY-MM-DD') AS period_start,
+  'RESOURCE_ALLOCATION_AGGREGATION_PERIOD_DAILY' AS period,
+
+  (SELECT seconds
+    FROM resource_aggregates
+    WHERE date = ds.period_start AND aggregation_type = 'total'
+    LIMIT 1
+  ) AS seconds,
+
+  (SELECT jsonb_object_agg(aggregation_key, seconds)
+    FROM resource_aggregates
+    WHERE date = ds.period_start AND aggregation_type = 'username'
+  ) AS by_username,
+
+  (SELECT jsonb_object_agg(aggregation_key, seconds)
+    FROM resource_aggregates
+    WHERE date = ds.period_start AND aggregation_type = 'experiment_label'
+  ) AS by_experiment_label,
+
+  (SELECT jsonb_object_agg(aggregation_key, seconds)
+    FROM resource_aggregates
+    WHERE date = ds.period_start AND aggregation_type = 'resource_pool'
+  ) AS by_resource_pool
 FROM
-    starts
+  date_series ds
+WHERE
+  ds.period_start IN (SELECT DISTINCT date FROM resource_aggregates)
 ORDER BY
-    period_start
+  ds.period_start
