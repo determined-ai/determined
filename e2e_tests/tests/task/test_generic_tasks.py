b9917df83f9202233f8944045a6caf20e6dd5b56
AmanuelAaron <aaron.amanuel@hpe.com>
2024-02-08 14:24:29 -0500

    add completion tests

--- e2e_tests/tests/task/test_generic_tasks.py
+++ e2e_tests/tests/task/test_generic_tasks.py
@@ -9,6 +9,19 @@ from determined.common.api import bindings
 from tests import api_utils
 from tests import config as conf
 
+import time
+
+
+def wait_for_task_state(test_session, task_id, expected_state, timeout) -> bool:
+    t = 0
+    found = False
+    while t < timeout and not found:
+        resp = bindings.get_GetTask(test_session, taskId=task_id)
+        found = expected_state == resp.task.taskState
+        t += 1
+        time.sleep(1)
+    return found
+
 
 @pytest.mark.e2e_cpu
 def test_create_generic_task() -> None:
@@ -27,6 +40,74 @@ def test_create_generic_task() -> None:
     ]
 
     subprocess.run(command, universal_newlines=True, stdout=subprocess.PIPE, check=True)
+
+
+@pytest.mark.e2e_cpu
+def test_generic_task_completion() -> None:
+    """
+    Start a simple task and check for task completion
+    """
+    test_session = api_utils.determined_test_session()
+
+    with open(conf.fixtures_path("generic_task/test_config.yaml"), "r") as config_file:
+        # Create task
+        config_text = config_file.read()
+        context_directory = context.read_v1_context(
+            pathlib.Path(conf.fixtures_path("generic_task")), []
+        )
+
+        req = bindings.v1CreateGenericTaskRequest(
+            config=config_text,
+            contextDirectory=context_directory,
+            projectId=None,
+            forkedFrom=None,
+            parentId=None,
+            inheritContext=False,
+            noPause=False,
+        )
+        task_resp = bindings.post_CreateGenericTask(test_session, body=req)
+
+        # Check for complete state
+        timeout = 30
+        is_valid_state = wait_for_task_state(
+            test_session, task_resp.taskId, bindings.v1GenericTaskState.COMPLETED, timeout
+        )
+        if not is_valid_state:
+            pytest.fail(f"task failed to complete after {timeout} seconds")
+
+
+@pytest.mark.e2e_cpu
+def test_create_generic_task_error() -> None:
+    """
+    Start a simple task that fails and check for error task state
+    """
+    test_session = api_utils.determined_test_session()
+
+    with open(conf.fixtures_path("generic_task/test_config_error.yaml"), "r") as config_file:
+        # Create task
+        config_text = config_file.read()
+        context_directory = context.read_v1_context(
+            pathlib.Path(conf.fixtures_path("generic_task")), []
+        )
+
+        req = bindings.v1CreateGenericTaskRequest(
+            config=config_text,
+            contextDirectory=context_directory,
+            projectId=None,
+            forkedFrom=None,
+            parentId=None,
+            inheritContext=False,
+            noPause=False,
+        )
+        task_resp = bindings.post_CreateGenericTask(test_session, body=req)
+
+        # Check for error state
+        timeout = 30
+        is_valid_state = wait_for_task_state(
+            test_session, task_resp.taskId, bindings.v1GenericTaskState.ERROR, timeout
+        )
+        if not is_valid_state:
+            pytest.fail(f"task failed to complete after {timeout} seconds")
 
 
 @pytest.mark.e2e_cpu
@@ -91,9 +172,7 @@ def test_generic_task_create_with_fork() -> None:
         task_resp = bindings.post_CreateGenericTask(test_session, body=req)
 
     # Create fork task
-    with open(
-        conf.fixtures_path("generic_task/test_config_fork.yaml"), "r"
-    ) as fork_config_file:
+    with open(conf.fixtures_path("generic_task/test_config_fork.yaml"), "r") as fork_config_file:
         config = ntsc.parse_config(fork_config_file, None, [], [])
         config_text = util.yaml_safe_dump(config)
         context_directory = context.read_v1_context(
@@ -114,9 +193,7 @@ def test_generic_task_create_with_fork() -> None:
         # Get fork task Config
         command = ["det", "-m", conf.make_master_url(), "task", "config", fork_task_resp.taskId]
 
-        res = subprocess.run(
-            command, universal_newlines=True, stdout=subprocess.PIPE, check=True
-        )
+        res = subprocess.run(command, universal_newlines=True, stdout=subprocess.PIPE, check=True)
         result_config = util.yaml_safe_load(res.stdout)
         expected_config = {"entrypoint": ["echo", "forked"]}
         assert expected_config == result_config
