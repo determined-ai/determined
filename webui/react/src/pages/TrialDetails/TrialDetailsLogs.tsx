696514425c2e154e2f53cabb461efb4c9d2f11cc
Emily Bonar <emilykbonar@gmail.com>
2024-02-08 11:06:15 -0500

    Fix string decoding

--- webui/react/src/pages/TrialDetails/TrialDetailsLogs.tsx
+++ webui/react/src/pages/TrialDetails/TrialDetailsLogs.tsx
@@ -8,7 +8,7 @@ import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
 import useUI from 'components/ThemeProvider';
 import { useSettings } from 'hooks/useSettings';
-import { DateString, decode } from 'ioTypes';
+import { DateString, decode, optional } from 'ioTypes';
 import { serverAddress } from 'routes/utils';
 import { detApi } from 'services/apiConfig';
 import { mapV1LogsResponse } from 'services/decoder';
@@ -129,8 +129,8 @@ const TrialDetailsLogs: React.FC<Props> = ({ experiment, trial }: Props) => {
         follow: false,
         limit: config.limit,
         orderBy: 'ORDER_BY_UNSPECIFIED',
-        timestampAfter: '',
-        timestampBefore: '',
+        timestampAfter: undefined as Date | string | undefined,
+        timestampBefore: undefined as Date | string | undefined,
       };
 
       if (type === FetchType.Initial) {
@@ -159,8 +159,8 @@ const TrialDetailsLogs: React.FC<Props> = ({ experiment, trial }: Props) => {
         settings.level,
         undefined,
         undefined,
-        decode(DateString, options.timestampBefore),
-        decode(DateString, options.timestampAfter),
+        decode(optional(DateString), options.timestampBefore),
+        decode(optional(DateString), options.timestampAfter),
         options.orderBy as OrderBy,
         settings.searchText,
         { signal },
