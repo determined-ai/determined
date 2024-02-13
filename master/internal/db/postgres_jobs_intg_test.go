caea9dbf892b01b19c27aa3b5c745945775d9bcc
Kristine Kunapuli <kristine.kunapuli@hpe.com>
2024-02-13 15:45:13 -0500

    chore: cover postgres_go

---
+++ master/internal/db/postgres_jobs_intg_test.go
@@ -0,0 +1,170 @@
+//go:build integration
+// +build integration
+
+package db
+
+import (
+	"testing"
+
+	"github.com/determined-ai/determined/master/pkg/etc"
+	"github.com/determined-ai/determined/master/pkg/model"
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/require"
+	"gotest.tools/assert"
+)
+
+func TestAddJob(t *testing.T) {
+	db := setupDbForTest(t)
+
+	t.Run("add job", func(t *testing.T) {
+		jobId := model.NewJobID()
+		j := model.Job{JobID: jobId, JobType: model.JobTypeCommand}
+		err := db.AddJob(&j)
+		require.NoError(t, err)
+	})
+
+	t.Run("add job with duplicate id", func(t *testing.T) {
+		jobId := model.NewJobID()
+		j := model.Job{JobID: jobId, JobType: model.JobTypeCommand}
+		err := db.AddJob(&j)
+		require.NoError(t, err)
+
+		// change job type and re-add job
+		j.JobType = model.JobTypeExperiment
+		err = db.AddJob(&j)
+		require.Error(t, err)
+	})
+
+	t.Run("add job with no job type", func(t *testing.T) {
+		err := db.AddJob(&model.Job{JobID: model.NewJobID()})
+		require.Error(t, err)
+	})
+}
+
+// JobByID retrieves a job by ID.
+func TestJobByID(t *testing.T) {
+	db := setupDbForTest(t)
+
+	t.Run("add and retrive job", func(t *testing.T) {
+		// create and send job
+		sendJob := model.Job{
+			JobID:   model.NewJobID(),
+			JobType: model.JobTypeExperiment,
+			QPos:    decimal.NewFromInt(10),
+		}
+		err := db.AddJob(&sendJob)
+		require.NoError(t, err)
+
+		// retrieve job and test for equality
+		recvJob, err := db.JobByID(sendJob.JobID)
+		require.NoError(t, err)
+		assert.Equal(t, sendJob.JobID, recvJob.JobID)
+		assert.Equal(t, sendJob.JobType, recvJob.JobType)
+		assert.Equal(t, sendJob.OwnerID, recvJob.OwnerID)
+		assert.Equal(t, decimal.NewFromInt(10).Equal(recvJob.QPos), true)
+	})
+
+	t.Run("retrieve non-existent job", func(t *testing.T) {
+		// retrieve job and test for equality
+		recvJob, err := db.JobByID(model.NewJobID())
+		require.Error(t, err)
+		require.Nil(t, recvJob)
+	})
+
+}
+
+// UpdateJobPosition propagates the new queue position to the job.
+func TestUpdateJobPosition(t *testing.T) {
+	db := setupDbForTest(t)
+
+	t.Run("update position", func(t *testing.T) {
+		// create and send job
+		origPos := decimal.NewFromInt(10)
+		newPos := decimal.NewFromInt(5)
+		sendJob := model.Job{
+			JobID:   model.NewJobID(),
+			JobType: model.JobTypeExperiment,
+			QPos:    origPos,
+		}
+		err := db.AddJob(&sendJob)
+		require.NoError(t, err)
+
+		// update job position
+		err = db.UpdateJobPosition(sendJob.JobID, newPos)
+		require.NoError(t, err)
+
+		// retrieve job and confirm pos update
+		recvJob, err := db.JobByID(sendJob.JobID)
+		require.NoError(t, err)
+		assert.Equal(t, newPos.Equal(recvJob.QPos), true)
+	})
+
+	t.Run("update position - negative value", func(t *testing.T) {
+		// create and send job
+		origPos := decimal.NewFromInt(10)
+		newPos := decimal.NewFromInt(-5)
+		sendJob := model.Job{
+			JobID:   model.NewJobID(),
+			JobType: model.JobTypeExperiment,
+			QPos:    origPos,
+		}
+		err := db.AddJob(&sendJob)
+		require.NoError(t, err)
+
+		// update job position
+		err = db.UpdateJobPosition(sendJob.JobID, newPos)
+		require.NoError(t, err)
+
+		// retrieve job and confirm pos update
+		recvJob, err := db.JobByID(sendJob.JobID)
+		require.NoError(t, err)
+		assert.Equal(t, newPos.Equal(recvJob.QPos), true)
+	})
+
+	t.Run("update position - empty ID", func(t *testing.T) {
+		// create and send job
+		origPos := decimal.NewFromInt(10)
+		newPos := decimal.NewFromInt(5)
+		sendJob := model.Job{
+			JobID:   model.NewJobID(),
+			JobType: model.JobTypeExperiment,
+			QPos:    origPos,
+		}
+		err := db.AddJob(&sendJob)
+		require.NoError(t, err)
+
+		// update job position
+		err = db.UpdateJobPosition(model.JobID(""), newPos)
+		require.Error(t, err)
+
+		// retrieve job and ensure queue pos not updated
+		recvJob, err := db.JobByID(sendJob.JobID)
+		require.NoError(t, err)
+		assert.Equal(t, origPos.Equal(recvJob.QPos), true)
+	})
+
+	t.Run("update position - ID does not exist", func(t *testing.T) {
+		// create and send job
+		origPos := decimal.NewFromInt(10)
+		newPos := decimal.NewFromInt(5)
+		sendJob := model.Job{
+			JobID:   model.NewJobID(),
+			JobType: model.JobTypeExperiment,
+			QPos:    origPos,
+		}
+		err := db.AddJob(&sendJob)
+		require.NoError(t, err)
+
+		// update job position for a job that doesn't exist
+		err = db.UpdateJobPosition(model.NewJobID(), newPos)
+		require.NoError(t, err)
+	})
+}
+
+func setupDbForTest(t *testing.T) *PgDB {
+	require.NoError(t, etc.SetRootPath(RootFromDB))
+
+	db := MustResolveTestPostgres(t)
+	MustMigrateTestPostgres(t, db, MigrationsFromDB)
+	return db
+}
