1fc1496a79cf398a593f2b0986ca9d4612897cc7
Aaron Amanuel <amanuel.aaron1@gmail.com>
2024-02-15 12:02:06 -0500

    fix: job queue test failures (#8843)
    * hotfix for test
    
    * ensure task completion
    
    * remove comment
    
    * update test helpers
    
    * default timeout
    
    (cherry picked from commit 08dfa43af5fcb750d4293b9492417c62d2fc1383)

--- e2e_tests/tests/task/task.py
+++ e2e_tests/tests/task/task.py
@@ -1,5 +1,7 @@
 import time
 
+import pytest
+
 from determined.common import api
 
 
@@ -7,26 +9,26 @@ def wait_for_task_state(
     test_session: api.Session,
     task_id: str,
     expected_state: api.bindings.v1GenericTaskState,
-    timeout: int,
-) -> bool:
+    timeout: int = 30,
+) -> None:
     deadline = time.time() + timeout
     while time.time() < deadline:
         resp = api.bindings.get_GetTask(test_session, taskId=task_id)
         if expected_state == resp.task.taskState:
-            return True
+            return
         time.sleep(0.1)
-    return False
+    pytest.fail(f"task failed to complete after {timeout} seconds")
 
 
 def wait_for_task_start(
     test_session: api.Session,
     task_id: str,
-    timeout: int,
-) -> bool:
+    timeout: int = 30,
+) -> None:
     deadline = time.time() + timeout
     while time.time() < deadline:
         resp = api.bindings.get_GetTask(test_session, taskId=task_id)
         if resp.task.allocations[0].state == api.bindings.taskv1State.RUNNING:
-            return True
+            return
         time.sleep(0.1)
-    return False
+    pytest.fail(f"task failed to start after {timeout} seconds")
