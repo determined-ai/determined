0a44bb404224864c2e378c5852bbdd7918c544cb
AmanuelAaron <aaron.amanuel@hpe.com>
2024-02-20 14:45:38 -0500

    add intg test

---
+++ master/internal/api_runs_intg_test.go
@@ -0,0 +1,63 @@
+//go:build integration
+// +build integration
+
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/determined-ai/determined/master/internal/db"
+	"github.com/determined-ai/determined/master/pkg/model"
+	"github.com/determined-ai/determined/master/pkg/ptrs"
+	"github.com/determined-ai/determined/proto/pkg/apiv1"
+)
+
+func TestSearchRuns(t *testing.T) {
+	api, curUser, ctx := setupAPITest(t, nil)
+	_, projectIDInt := createProjectAndWorkspace(ctx, t, api)
+	projectID := int32(projectIDInt)
+
+	// Empty response causes no errors.
+	req := &apiv1.SearchRunsRequest{
+		ProjectId: &projectID,
+		Sort:      ptrs.Ptr("id=asc"),
+	}
+	resp, err := api.SearchRuns(ctx, req)
+	require.NoError(t, err)
+	require.Len(t, resp.Runs, 0)
+
+	exp := createTestExpWithProjectID(t, api, curUser, projectIDInt)
+
+	task := &model.Task{TaskType: model.TaskTypeTrial, TaskID: model.NewTaskID()}
+	require.NoError(t, db.AddTask(ctx, task))
+	require.NoError(t, db.AddTrial(ctx, &model.Trial{
+		State:        model.PausedState,
+		ExperimentID: exp.ID,
+	}, task.TaskID))
+
+	resp, err = api.SearchRuns(ctx, req)
+	require.NoError(t, err)
+	require.Len(t, resp.Runs, 1)
+
+	// Add second experiment
+	exp2 := createTestExpWithProjectID(t, api, curUser, projectIDInt)
+
+	task2 := &model.Task{TaskType: model.TaskTypeTrial, TaskID: model.NewTaskID()}
+	require.NoError(t, db.AddTask(ctx, task2))
+	require.NoError(t, db.AddTrial(ctx, &model.Trial{
+		State:        model.PausedState,
+		ExperimentID: exp2.ID,
+	}, task2.TaskID))
+
+	// Sort by start time
+	resp, err = api.SearchRuns(ctx, &apiv1.SearchRunsRequest{
+		ProjectId: req.ProjectId,
+		Sort:      ptrs.Ptr("startTime=asc"),
+	})
+
+	require.NoError(t, err)
+	require.Equal(t, int32(exp.ID), resp.Runs[0].ExperimentId)
+	require.Equal(t, int32(exp2.ID), resp.Runs[1].ExperimentId)
+}
