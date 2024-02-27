a172fdedc9bdc8641e2f0dcd66d9007ff1690313
Kristine Kunapuli <49738148+kkunapuli@users.noreply.github.com>
2024-02-27 11:36:07 -0500

    chore: delete unused functions [RM-41] (#8888)

--- master/internal/db/database.go
+++ master/internal/db/database.go
@@ -18,8 +18,6 @@ type DB interface {
 	Migrate(migrationURL string, actions []string) (isNew bool, err error)
 	Close() error
 	GetOrCreateClusterID(telemetryID string) (string, error)
-	CheckExperimentExists(id int) (bool, error)
-	CheckTrialExists(id int) (bool, error)
 	TrialExperimentAndRequestID(id int) (int, model.RequestID, error)
 	AddExperiment(experiment *model.Experiment, modelDef []byte, activeConfig expconf.ExperimentConfig) error
 	ExperimentIDByTrialID(trialID int) (int, error)
