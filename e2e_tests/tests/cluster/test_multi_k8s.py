3659aaae58703e84fbecdd5c233bc21fd031d338
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2024-02-29 11:58:15 -0500

    progress

--- e2e_tests/tests/cluster/test_multi_k8s.py
+++ e2e_tests/tests/cluster/test_multi_k8s.py
@@ -5,6 +5,7 @@ from tests import api_utils
 from tests import config as conf
 from tests import experiment as exp
 
+
 @pytest.mark.e2e_multi_k8s
 def test_run_experiment_multi_k8s() -> None:
     sess = api_utils.user_session()
@@ -15,7 +16,10 @@ def test_run_experiment_multi_k8s() -> None:
         None,
     )
     exp.wait_for_experiment_state(
-        sess, exp_id, bindings.experimentv1State.COMPLETED, max_wait_secs=500,
+        sess,
+        exp_id,
+        bindings.experimentv1State.COMPLETED,
+        max_wait_secs=500,
     )
 
     # TODO test out like the whole submitting into resource pools and all.
