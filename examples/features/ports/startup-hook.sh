411e42ea01df609fcecd6e467cbef8370eba48fe
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2024-03-01 18:05:11 -0600

    take out version printouts
    Co-authored-by: Ilia Glazkov <ilia.glazkov@hpe.com>

--- examples/features/ports/startup-hook.sh
+++ examples/features/ports/startup-hook.sh
@@ -3,6 +3,3 @@
 pip install 'pydantic<2.0.0' # resolves 'pydantic.dataclasses.dataclass only supports init=False'
 pip install -U "ray[air]==2.9.3"
 pip install 'pandas==1.5.3'
-pip show pandas
-pip show pydantic
-pip show ray
