60436eac6c19295ac717ab6dd06772f78da7ed96
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2024-03-04 10:44:21 -0600

    chore: pin pandas and ray versions for ray tests (#8932)
    * avoid pinning pandas for other tests
    ---------
    
    Co-authored-by: Ilia Glazkov <ilia.glazkov@hpe.com>

--- examples/features/ports/startup-hook.sh
+++ examples/features/ports/startup-hook.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 pip install 'pydantic<2.0.0' # resolves 'pydantic.dataclasses.dataclass only supports init=False'
-pip install -U "ray[air]"
+pip install -U "ray[air]==2.9.3"
+pip install 'pandas==1.5.3'
