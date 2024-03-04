39ce1b3a0c9b84b033ca19dc9af1211ce07ea5bb
Wes Turner <wesley.turner@hpe.com>
2024-03-04 11:25:25 -0800

    test: refactor usage within test_local (#8913)
    Fixes `test-det-deploy-local` tests that were broken by a change in caching strategy in #8872.
    
    Changes to test_local.py:
    * changed interface to functions bringing up and down resources to be both more general and more explicit
    * resources in the tests themselves are brought up and down using a context manager. This ensures they clean up after themselves even when they fail.
    * deleted a couple functions that had been needed for the previously-deleted `test-stress`.
    
    Changes to api_utils.py:
    * `is_hpc` operates directly on a passed session.
    
    Changes to experiment.py & compute-stats.py
    * The session for a run experiment is passed to `api_utils.is_hpc` so that this function can query the now-live master to determine whether that master is running an HPC scheduler. Previous calls to `is_hpc` only accidentally worked because the very first time it was called it brought up a master that happened to run at the same default location as the session `is_hpc` generated by default.
    * Formatting

--- e2e_tests/tests/deploy/test_local.py
+++ e2e_tests/tests/deploy/test_local.py
@@ -1,7 +1,9 @@
+import contextlib
+import enum
 import os
 import pathlib
 import subprocess
-from typing import List
+from typing import Dict, Iterator, List, Optional
 
 import docker
 import pytest
@@ -13,14 +15,21 @@ from tests import detproc
 from tests import experiment as exp
 
 
+class Resource(enum.Enum):
+    def __str__(self) -> str:
+        return self.value
+
+    CLUSTER = "cluster"
+    MASTER = "master"
+    AGENT = "agent"
+
+
 def mksess(host: str, port: int, username: str = "determined", password: str = "") -> api.Session:
-    """
-    Since this file frequently creates new masters, always create a fresh Session.
-    """
+    """Since this file frequently creates new masters, always create a fresh Session."""
 
     master_url = api.canonicalize_master_url(f"http://{host}:{port}")
     utp = authentication.login(master_url, username=username, password=password)
-    return api.Session(master_url, utp, cert=None)
+    return api.Session(master_url, utp, cert=None, max_retries=0)
 
 
 def det_deploy(subcommand: List) -> None:
@@ -29,79 +38,78 @@ def det_deploy(subcommand: List) -> None:
         "deploy",
         "local",
     ] + subcommand
+    print(f"Running deployment: {' '.join(command)}")
     subprocess.run(command, check=True)
 
 
-def cluster_up(arguments: List, delete_db: bool = True) -> None:
-    command = ["cluster-up", "--no-gpu"]
-    if delete_db:
-        command += ["--delete-db"]
+def resource_up(
+    resource: Resource,
+    name: str,
+    kwflags: Optional[Dict[str, str]] = None,
+    flags: Optional[List[str]] = None,
+    positional_arguments: Optional[List[str]] = None,
+) -> None:
+    """Issue a `det deploy local` command to bring up a resource.
+
+    Ex:
+      det deploy local cluster-up --cluster-name test_cluster --det-version 0.15.0 --no-gpu
+
+    Arguments:
+        resource: The type of resource to bring up, e.g. "cluster", "master", "agent".
+        name: The name to give the resource.
+        kwflags: A dictionary of keyword flags (and their values) to pass to the command.
+        flags: A list of flags to pass to the command.
+        positional_arguments: A list of positional arguments to pass to the command.
+
+    This additionally sets a --det-version flag if DET_VERSION is set in the config.
+    """
+    command = [f"{resource}-up", f"--{resource}-name", name]
+    if kwflags:
+        for flag, val in kwflags.items():
+            command += [f"--{flag}", val]
+    if flags:
+        for flag in flags:
+            command += [f"--{flag}"]
+    if positional_arguments:
+        command += positional_arguments
+
     det_version = conf.DET_VERSION
     if det_version is not None:
         command += ["--det-version", det_version]
-    command += arguments
     det_deploy(command)
 
 
-def cluster_down(arguments: List) -> None:
-    command = ["cluster-down"]
-    command += arguments
+def resource_down(resource: Resource, name: str) -> None:
+    command = [f"{resource}-down", f"--{resource}-name", name]
     det_deploy(command)
 
 
-def master_up(arguments: List, delete_db: bool = True) -> None:
-    command = ["master-up"]
-    if delete_db:
-        command += ["--delete-db"]
-    det_version = conf.DET_VERSION
-    if det_version is not None:
-        command += ["--det-version", det_version]
-    command += arguments
-    det_deploy(command)
-
-
-def master_down(arguments: List) -> None:
-    command = ["master-down"]
-    command += arguments
-    det_deploy(command)
-
-
-def agent_up(arguments: List) -> None:
-    command = ["agent-up", conf.MASTER_IP, "--no-gpu"]
-    det_version = conf.DET_VERSION
-    if det_version is not None:
-        command += ["--det-version", det_version]
-    command += arguments
-    det_deploy(command)
-
-
-def agent_down(arguments: List) -> None:
-    command = ["agent-down"]
-    command += arguments
-    det_deploy(command)
-
-
-def agent_enable(sess: api.Session, arguments: List) -> None:
-    detproc.check_output(sess, ["det", "agent", "enable"] + arguments)
-
-
-def agent_disable(sess: api.Session, arguments: List) -> None:
-    detproc.check_output(sess, ["det", "agent", "disable"] + arguments)
+@contextlib.contextmanager
+def resource_manager(
+    resource: Resource,
+    name: str,
+    kwflags: Optional[Dict[str, str]] = None,
+    boolean_flags: Optional[List[str]] = None,
+    positional_arguments: Optional[List[str]] = None,
+) -> Iterator[None]:
+    """Context manager to bring resources up and down."""
+    resource_up(resource, name, kwflags, boolean_flags, positional_arguments)
+    try:
+        yield
+    finally:
+        resource_down(resource, name)
 
 
 @pytest.mark.det_deploy_local
 def test_cluster_down() -> None:
     name = "test_cluster_down"
 
-    cluster_up(["--cluster-name", name])
+    with resource_manager(Resource.CLUSTER, name, {}, ["no-gpu"]):
+        container_name = name + "_determined-master_1"
+        client = docker.from_env()
 
-    container_name = name + "_determined-master_1"
-    client = docker.from_env()
-
-    containers = client.containers.list(filters={"name": container_name})
-    assert len(containers) > 0
-
-    cluster_down(["--cluster-name", name])
+        containers = client.containers.list(filters={"name": container_name})
+        assert len(containers) > 0
 
     containers = client.containers.list(filters={"name": container_name})
     assert len(containers) == 0
@@ -112,47 +120,52 @@ def test_cluster_down() -> None:
 def test_custom_etc() -> None:
     name = "test_custom_etc"
     etc_path = str(pathlib.Path(__file__).parent.joinpath("etc/master.yaml").resolve())
-    cluster_up(["--master-config-path", etc_path, "--cluster-name", name])
-    sess = mksess("localhost", 8080)
-    exp.run_basic_test(
-        sess,
-        conf.fixtures_path("no_op/single-default-ckpt.yaml"),
-        conf.fixtures_path("no_op"),
-        1,
-    )
-    assert os.path.exists("/tmp/ckpt-test/")
-    cluster_down(["--cluster-name", name])
+    with resource_manager(Resource.CLUSTER, name, {"master-config-path": etc_path}, ["no-gpu"]):
+        sess = mksess("localhost", 8080)
+        exp.run_basic_test(
+            sess,
+            conf.fixtures_path("no_op/single-default-ckpt.yaml"),
+            conf.fixtures_path("no_op"),
+            1,
+        )
+        assert os.path.exists("/tmp/ckpt-test/")
 
 
 @pytest.mark.det_deploy_local
 def test_agent_config_path() -> None:
     cluster_name = "test_agent_config_path"
     master_name = f"{cluster_name}_determined-master_1"
-    master_up(["--master-name", master_name])
+    with resource_manager(Resource.MASTER, master_name):
+        # Config makes it unmodified.
+        etc_path = str(pathlib.Path(__file__).parent.joinpath("etc/agent.yaml").resolve())
+        agent_name = "test-path-agent"
+        with resource_manager(
+            Resource.AGENT,
+            agent_name,
+            {"agent-config-path": etc_path},
+            ["no-gpu"],
+            [conf.MASTER_IP],
+        ):
+            client = docker.from_env()
+            agent_container = client.containers.get(agent_name)
+            exit_code, out = agent_container.exec_run(["cat", "/etc/determined/agent.yaml"])
+            assert exit_code == 0
+            with open(etc_path) as f:
+                assert f.read() == out.decode("utf-8")
 
-    # Config makes it unmodified.
-    etc_path = str(pathlib.Path(__file__).parent.joinpath("etc/agent.yaml").resolve())
-    agent_name = "test-path-agent"
-    agent_up(["--agent-config-path", etc_path])
-
-    client = docker.from_env()
-    agent_container = client.containers.get(agent_name)
-    exit_code, out = agent_container.exec_run(["cat", "/etc/determined/agent.yaml"])
-    assert exit_code == 0
-    with open(etc_path) as f:
-        assert f.read() == out.decode("utf-8")
-    agent_down(["--agent-name", agent_name])
-
-    # Validate CLI flags overwrite config file options.
-    agent_name += "-2"
-    agent_up(["--agent-name", agent_name, "--agent-config-path", etc_path])
-    sess = mksess("localhost", 8080)
-    agent_list = detproc.check_json(sess, ["det", "a", "list", "--json"])
-    agent_list = [el for el in agent_list if el["id"] == agent_name]
-    assert len(agent_list) == 1
-    agent_down(["--agent-name", agent_name])
-
-    master_down(["--master-name", master_name])
+        # Validate CLI flags overwrite config file options.
+        agent_name += "-2"
+        with resource_manager(
+            Resource.AGENT,
+            agent_name,
+            {"agent-config-path": etc_path},
+            ["no-gpu"],
+            [conf.MASTER_IP],
+        ):
+            sess = mksess("localhost", 8080)
+            agent_list = detproc.check_json(sess, ["det", "a", "list", "--json"])
+            agent_list = [el for el in agent_list if el["id"] == agent_name]
+            assert len(agent_list) == 1
 
 
 @pytest.mark.det_deploy_local
@@ -160,44 +173,28 @@ def test_agent_config_path() -> None:
 def test_custom_port() -> None:
     name = "port_test"
     custom_port = 12321
-    arguments = [
-        "--cluster-name",
-        name,
-        "--master-port",
-        f"{custom_port}",
-    ]
-    cluster_up(arguments)
+    with resource_manager(Resource.CLUSTER, name, {"master-port": str(custom_port)}, ["no-gpu"]):
+        sess = mksess("localhost", custom_port)
 
-    sess = mksess("localhost", custom_port)
-
-    exp.run_basic_test(
-        sess,
-        conf.fixtures_path("no_op/single-one-short-step.yaml"),
-        conf.fixtures_path("no_op"),
-        1,
-    )
-    cluster_down(["--cluster-name", name])
+        exp.run_basic_test(
+            sess,
+            conf.fixtures_path("no_op/single-one-short-step.yaml"),
+            conf.fixtures_path("no_op"),
+            1,
+        )
 
 
 @pytest.mark.det_deploy_local
 def test_agents_made() -> None:
     name = "agents_test"
     num_agents = 2
-    arguments = [
-        "--cluster-name",
-        name,
-        "--agents",
-        f"{num_agents}",
-    ]
-    cluster_up(arguments)
-    container_names = [name + f"-agent-{i}" for i in range(0, num_agents)]
-    client = docker.from_env()
+    with resource_manager(Resource.CLUSTER, name, {"agents": str(num_agents)}, ["no-gpu"]):
+        container_names = [name + f"-agent-{i}" for i in range(0, num_agents)]
+        client = docker.from_env()
 
-    for container_name in container_names:
-        containers = client.containers.list(filters={"name": container_name})
-        assert len(containers) > 0
-
-    cluster_down(["--cluster-name", name])
+        for container_name in container_names:
+            containers = client.containers.list(filters={"name": container_name})
+            assert len(containers) > 0
 
 
 @pytest.mark.det_deploy_local
@@ -205,14 +202,11 @@ def test_master_up_down() -> None:
     cluster_name = "test_master_up_down"
     master_name = f"{cluster_name}_determined-master_1"
 
-    master_up(["--master-name", master_name])
+    with resource_manager(Resource.MASTER, master_name):
+        client = docker.from_env()
 
-    client = docker.from_env()
-
-    containers = client.containers.list(filters={"name": master_name})
-    assert len(containers) > 0
-
-    master_down(["--master-name", master_name])
+        containers = client.containers.list(filters={"name": master_name})
+        assert len(containers) > 0
 
     containers = client.containers.list(filters={"name": master_name})
     assert len(containers) == 0
@@ -224,15 +218,11 @@ def test_agent_up_down() -> None:
     cluster_name = "test_agent_up_down"
     master_name = f"{cluster_name}_determined-master_1"
 
-    master_up(["--master-name", master_name])
-    agent_up(["--agent-name", agent_name])
+    with resource_manager(Resource.MASTER, master_name):
+        with resource_manager(Resource.AGENT, agent_name, {}, ["no-gpu"], [conf.MASTER_IP]):
+            client = docker.from_env()
+            containers = client.containers.list(filters={"name": agent_name})
+            assert len(containers) > 0
 
-    client = docker.from_env()
-    containers = client.containers.list(filters={"name": agent_name})
-    assert len(containers) > 0
-
-    agent_down(["--agent-name", agent_name])
-    containers = client.containers.list(filters={"name": agent_name})
-    assert len(containers) == 0
-
-    master_down(["--master-name", master_name])
+        containers = client.containers.list(filters={"name": agent_name})
+        assert len(containers) == 0
