6239e0c603aec9b58285ec579e95c4ff73547739
gt2345 <gt2345@columbia.edu>
2024-03-04 12:28:20 -0600

    fix import

--- e2e_tests/tests/streaming/test_client.py
+++ e2e_tests/tests/streaming/test_client.py
@@ -4,33 +4,32 @@ import pytest
 
 from determined.common import streams
 from determined.common.api import bindings
-from determined.common.streams import _client
 from tests import api_utils
 
 
 @pytest.mark.e2e_cpu
 def test_client_connection() -> None:
     sess = api_utils.admin_session()
-    ws = _client.LomondStreamWebSocket(sess)
-    stream = _client.Stream(ws)
+    ws = streams._client.LomondStreamWebSocket(sess)
+    stream = streams._client.Stream(ws)
     syncId = "sync1"
-    stream.subscribe(sync_id=syncId, projects=_client.ProjectSpec(workspace_id=1))
+    stream.subscribe(sync_id=syncId, projects=streams._client.ProjectSpec(workspace_id=1))
     event = next(stream)
-    assert event == _client.Sync(syncId, False)
+    assert event == streams._client.Sync(syncId, False)
 
     event = next(stream)
     assert isinstance(event, streams.wire.ProjectMsg)
     assert event.id == 1
     assert event.immutable is True
     event = next(stream)
-    assert event == _client.Sync(syncId, True)
+    assert event == streams._client.Sync(syncId, True)
 
 
 @pytest.mark.e2e_cpu
 def test_client_subscribe() -> None:
     sess = api_utils.admin_session()
-    ws = _client.LomondStreamWebSocket(sess)
-    stream = _client.Stream(ws)
+    ws = streams._client.LomondStreamWebSocket(sess)
+    stream = streams._client.Stream(ws)
 
     syncId = "sync1"
     projectName = "streaming_project"
@@ -50,16 +49,16 @@ def test_client_subscribe() -> None:
     )
     p = resp_p.project
 
-    stream.subscribe(sync_id=syncId, projects=_client.ProjectSpec(workspace_id=w.id))
+    stream.subscribe(sync_id=syncId, projects=streams._client.ProjectSpec(workspace_id=w.id))
     event = next(stream)
-    assert event == _client.Sync(syncId, False)
+    assert event == streams._client.Sync(syncId, False)
     event = next(stream)
     assert isinstance(event, streams.wire.ProjectMsg)
     assert event.id == p.id
     assert event.name == projectName
     seq = event.seq
     event = next(stream)
-    assert event == _client.Sync(syncId, True)
+    assert event == streams._client.Sync(syncId, True)
 
     bindings.patch_PatchProject(sess, body=bindings.v1PatchProject(name=newProjectName), id=p.id)
     event = next(stream)
