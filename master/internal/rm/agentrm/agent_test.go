1632c06b6499a6d1e836651d4bf75f6b1613cd69
Bradley Laney <bradley.laney@hpe.com>
2024-03-05 18:03:11 -0500

    fixes

--- master/internal/rm/agentrm/agent_test.go
+++ master/internal/rm/agentrm/agent_test.go
@@ -8,14 +8,15 @@ import (
 	"testing"
 	"time"
 
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/require"
+
 	"github.com/determined-ai/determined/master/internal/config"
 	"github.com/determined-ai/determined/master/pkg/aproto"
 	"github.com/determined-ai/determined/master/pkg/model"
 	"github.com/determined-ai/determined/master/pkg/syncx/queue"
 	"github.com/determined-ai/determined/master/pkg/ws"
-	"github.com/gorilla/websocket"
-	"github.com/labstack/echo/v4"
-	"github.com/stretchr/testify/require"
 )
 
 func TestAgentFastFailAfterFirstConnect2(t *testing.T) {
@@ -39,13 +40,14 @@ func TestAgentFastFailAfterFirstConnect2(t *testing.T) {
 	// Connect a fake websocket.
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		a.HandleWebsocketConnection(webSocketRequest{echoCtx: c})
+		err := a.HandleWebsocketConnection(webSocketRequest{echoCtx: c})
+		require.NoError(t, err)
 		return nil
 	})
 	server := httptest.NewServer(e.Server.Handler)
 
 	var dialer websocket.Dialer
-	conn, _, err := dialer.Dial(fmt.Sprintf("ws://%s", strings.TrimLeft(server.URL, "http://")), nil)
+	conn, _, err := dialer.Dial(fmt.Sprintf("ws://%s", strings.TrimPrefix(server.URL, "http://")), nil)
 	require.NoError(t, err)
 	_, err = ws.Wrap[*aproto.MasterMessage, aproto.AgentMessage]("test", conn)
 	require.NoError(t, err)
