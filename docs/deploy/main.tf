f18dc47b1d50fdd2051704e72742e73528668488
Wes Turner <wesley.turner@hpe.com>
2024-03-06 14:10:28 -0800

    build: Update Terraform config to modern syntax

--- docs/deploy/main.tf
+++ docs/deploy/main.tf
@@ -6,13 +6,19 @@ terraform {
     key    = "terraform.tfstate"
     region = "us-west-2"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.19.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.1.2"
+    }
+  }
 }
 
 provider "aws" {
-  version = "~> 2.19.0"
-  region  = "us-west-2"
-}
-
-provider "null" {
-  version = "~> 2.1.2"
+  region = "us-west-2"
 }
