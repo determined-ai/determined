9762879cd768bf28c6625ecc9cb729424697f3e6
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2024-03-12 00:00:00 -0500

    Revert "fix"
    This reverts commit b3b1d81fa90a09b90871e90c4fdc8f8ec5a064fa.

--- master/internal/api_auth.go
+++ master/internal/api_auth.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 
 	"github.com/labstack/echo/v4"
@@ -165,49 +164,23 @@ func processProxyAuthentication(c echo.Context) (done bool, err error) {
 func processAuthWithRedirect(redirectPaths []string) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			echoErr := user.GetService().ProcessAuthentication(next)(c)
-			if echoErr == nil {
-				return nil
-			} else if httpErr, ok := echoErr.(*echo.HTTPError); !ok || httpErr.Code != http.StatusUnauthorized {
-				return echoErr
-			}
-
-			isProxiedPath := false
-			path := c.Request().RequestURI
-			for _, p := range redirectPaths {
-				if strings.HasPrefix(path, p) {
-					isProxiedPath = true
-				}
-			}
-			if !isProxiedPath {
-				// GRPC-backed routes are authenticated by grpcutil.*AuthInterceptor.
-				return echoErr
-			}
-
-			md := metadata.MD{}
-			for k, v := range c.Request().Header {
-				k = strings.TrimPrefix(k, grpcutil.GrpcMetadataPrefix)
-				md.Append(k, v...)
-			}
-			_, _, err := grpcutil.GetUser(metadata.NewIncomingContext(c.Request().Context(), md))
+			err := user.GetService().ProcessAuthentication(next)(c)
 			if err == nil {
-				return next(c)
-			} else {
-				errStatus := status.Convert(err)
-				if errStatus.Code() != codes.PermissionDenied && errStatus.Code() != codes.Unauthenticated {
-					return err
-				}
+				return nil
 			}
-
-			// TODO: reverse this logic to redirect only if accept is empty or specifies text/html.
 			// No web page redirects for programmatic requests.
 			for _, accept := range c.Request().Header["Accept"] {
 				if strings.Contains(accept, "application/json") {
-					return echoErr
+					return err
 				}
 			}
-
-			return redirectToLogin(c)
+			path := c.Request().RequestURI
+			for _, p := range redirectPaths {
+				if strings.HasPrefix(path, p) {
+					return redirectToLogin(c)
+				}
+			}
+			return err
 		}
 	}
 }
