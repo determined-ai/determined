6b63f35a31a5db2692ea1bfa5513bbff7783d08a
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2024-03-13 00:00:00 -0500

    modular

--- harness/determined/deploy/errors.py
+++ harness/determined/deploy/errors.py
@@ -1,6 +1,31 @@
+from typing import Optional
+
+from termcolor import colored
+
+import determined as det
+
+
 class PreflightFailure(Exception):
     pass
 
 
 class MasterTimeoutExpired(Exception):
     pass
+
+
+def warn_version_mistmatch(requestd_version: Optional[str]) -> None:
+    """
+    check for and warn about version compatibility between the `det` CLI and
+    the requested det version.
+    """
+    if not requestd_version:
+        return
+    print(
+        colored(
+            f"Warning: The specified --det-version ({requestd_version}) does not match the "
+            f"the current `det` CLI version ({det.__version__}), proceed with caution.",
+            "You should use a matching version of det CLI. ",
+            "https://docs.determined.ai/latest/tools/cli/cli-ug.html#determined-cli",
+            "yellow",
+        )
+    )
