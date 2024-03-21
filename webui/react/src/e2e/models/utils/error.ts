67dd40ba5b8f455dc3dde7f68808f5bf33251bc8
Justin Comins <justin.comins@hpe.com>
2024-03-21 16:54:10 -0700

    missed one file

---
+++ webui/react/src/e2e/models/utils/error.ts
@@ -0,0 +1,21 @@
+import { BaseComponent, NamedComponent, NamedComponentArgs } from 'e2e/models/BaseComponent';
+
+/**
+ * Returns a representation of the error util.
+ *
+ * @remarks
+ * This constructor represents the contents in src/utils/error.ts.
+ *
+ * @param {Object} obj
+ * @param {implementsGetLocator} obj.parent - The parent used to locate this error
+ * @param {string} [obj.selector] - Used instead of `defaultSelector`
+ */
+export class ErrorComponent extends NamedComponent({ defaultSelector: ".ant-notification"}) {
+  static readonly selectorTopRight = ".ant-notification-topRight"
+  static readonly selectorBottomRight = ".ant-notification-bottomRight"
+  constructor({ parent, selector }: NamedComponentArgs) {
+    super({ parent: parent, selector: selector || ErrorComponent.defaultSelector });
+  }
+  readonly message: BaseComponent = new BaseComponent({ parent: this, selector: ".ant-notification-notice-message" });
+  readonly description: BaseComponent = new BaseComponent({ parent: this, selector: ".ant-notification-notice-description" });
+}
