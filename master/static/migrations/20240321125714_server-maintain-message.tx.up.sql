26dde2c18eafd61822016c4b9913c7f20d438828
Max Russell <max.russell@hpe.com>
2024-03-21 13:47:10 -0700

    New schema to test

---
+++ master/static/migrations/20240321125714_server-maintain-message.tx.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE maintenance_messages (
+  message TEXT NOT NULL,
+  created_by INT REFERENCES public.users(id),
+  start_time TIMESTAMP with time zone NOT NULL DEFAULT NOW(),
+  end_time TIMESTAMP with time zone DEFAULT NULL,
+  created_time TIMESTAMP NOT NULL DEFAULT NOW()
+);
