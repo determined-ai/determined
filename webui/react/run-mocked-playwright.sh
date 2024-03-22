d68fbbad712085653b36bd7e05f3dba4de5420c7
Daniel Janicek <daniel.janicek@hpe.com>
2024-03-22 12:20:11 -0700

    WIP: passing with no backend, /workspaces api needs non-401 mock

--- webui/react/run-mocked-playwright.sh
+++ webui/react/run-mocked-playwright.sh
@@ -5,12 +5,15 @@ make mb-start
 sleep 3 # let MB start up. It's pretty quick.
 make mb-start-cached-imposters
 export DET_WEBPACK_PROXY_URL="http://localhost:4545"
-export DET_WEBSOCKET_PROXY_URL="ws://localhost:4546"
+# export DET_WEBSOCKET_PROXY_URL="ws://localhost:4546"
 npm run preview >/dev/null 2>&1 & PREVIEW_PID=$!
 echo "preview running on ${PREVIEW_PID} pid."
 PW_SERVER_ADDRESS="http://localhost:3001"  npm run e2e
 if [ $$? -ne 0 ]; 
 then 
+    make mb-stop
+    make mb-start
+    sleep 3
     make mb-start-record-imposters
     PW_SERVER_ADDRESS="http://localhost:3001"  npm run e2e 
     make mb-save-imposters
