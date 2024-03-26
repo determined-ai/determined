409acc1e8a93aff6728f590ba35506e1adad619c
Daniel Janicek <daniel.janicek@hpe.com>
2024-03-26 10:57:15 -0700

    predicate generation working

--- webui/react/src/e2e/mocks/predicate-generator.js
+++ webui/react/src/e2e/mocks/predicate-generator.js
@@ -1,20 +1,39 @@
 function generate(config) {
-    const predicate = {
-        equals: {
-            path: config.request.path
-        },
-        deepEquals: {
-            body: config.request.body,
-            query: config.request.query
-        },
+    config.logger.info('DNJ TODO making imposter: ', config.request);
+    const predicatePreview = {
         exists: {
             headers: {
-                'authorization': false
+                'authorization': false,
+                'cookie': false,
             }
+        },
+        equals: {
+            method: config.request.method,
+            path: config.request.path
         }
     };
-    if (config.request.headers['authorization']) {
-        predicate.exists.headers['authorization'] = true;
+    if (config.request.body) {
+        if (!predicatePreview.deepEquals) {
+            predicatePreview.deepEquals = {};
+        }
+        predicatePreview.deepEquals.body = config.request.body;
     }
+    if (config.request.query) {
+        if (!predicatePreview.deepEquals) {
+            predicatePreview.deepEquals = {};
+        }
+        predicatePreview.deepEquals.query = config.request.query;
+    }
+    if (config.request.headers['authorization']) {
+        predicatePreview.exists.headers['authorization'] = true;
+    }
+    if (config.request.headers['cookie']) {
+        predicatePreview.exists.headers['cookie'] = true;
+    }
+    const predicate = { and: [] };
+    for (const [operator, matchers] of Object.entries(predicatePreview)) {
+        predicate.and.push({ [operator]: matchers });
+    }
+    config.logger.info('DNJ TODO made imposter! ', config.request, 'Predicate: ', predicate);
     return [predicate];
 }
