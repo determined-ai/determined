5625aafbba53e83a83c22533b0dd718fc8504b18
Wes Turner <wesley.turner@hpe.com>
2024-03-26 16:47:44 -0700

    ci: more printing about state of master

--- .circleci/scripts/wait_for_perf_migration_upload_results.py
+++ .circleci/scripts/wait_for_perf_migration_upload_results.py
@@ -16,19 +16,23 @@ from determined.common.api import authentication, bindings, certs
 def _wait_for_master() -> None:
     print("Checking for master")
     cert = certs.Cert(noverify=True)
-    sess = api.UnauthSession("http://127.0.0.1:8080", cert)
+    sess = api.UnauthSession("http://127.0.0.1:8080", cert).with_retry(0)
 
     # 2 hours is the most a migration can take, with this setup.
     # If a migration takes longer than that we have hit an issue a customer will likely hit too.
-    for i in range(2 * 60 * 60):
+    time_start = time.time()
+    time_last_report = time_start
+    while time_start - time.time() < 2 * 60 * 60:
         try:
             r = sess.get("info")
             if r.status_code == requests.codes.ok:
+                print(f"Master up and available after {int(time.time() - time_start) / 60} minutes")
                 return
         except api.errors.MasterNotFoundException:
             pass
-        if i % 60 == 0:
-            print("Waiting for master to be available...")
+        if time.time() - time_last_report > 60:
+            time_last_report = time.time()
+            print(f"Waiting for master ({int(time_last_report - time_start) / 60} minutes elapsed)")
         time.sleep(1)
     raise ConnectionError("Timed out connecting to Master")
 
