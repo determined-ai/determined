303720ee46feeeaad908add9fc1d2526e1154916
Daniel Janicek <daniel.janicek@hpe.com>
2024-03-26 13:24:40 -0700

    draft: ci testing

--- webui/react/run-mocked-playwright.sh
+++ webui/react/run-mocked-playwright.sh
@@ -2,21 +2,38 @@
 set -euxo pipefail
 
 make mb-start
-sleep 3 # let MB start up. It's pretty quick.
+sleep 2 # let MB start up. It's pretty quick.
 make mb-start-saved-imposters
 export DET_WEBPACK_PROXY_URL="http://localhost:4545"
-# export DET_WEBSOCKET_PROXY_URL="ws://localhost:4546"
-npm run preview >/dev/null 2>&1 & PREVIEW_PID=$!
-echo "preview running on ${PREVIEW_PID} pid."
-PW_SERVER_ADDRESS="http://localhost:3001"  npm run e2e
-if [ $$? -ne 0 ]; 
-then 
+export DET_WEBSOCKET_PROXY_URL="ws://localhost:4546"
+export PW_SERVER_ADDRESS="http://localhost:3001"
+set +e
+npm run e2e
+test_result=$?
+set -e
+if [ $test_result -ne 0 ]; then
+    echo "Tests failed, re-running without mocks"
+    det deploy local cluster-up --no-gpu
     make mb-stop
     make mb-start
-    sleep 3
+    sleep 2
     make mb-record-imposters
-    PW_SERVER_ADDRESS="http://localhost:3001"  npm run e2e 
-    make mb-save-imposters
+    set +e
+    npm run e2e
+    test_result=$?
+    set -e
+    if [ $test_result -ne 0 ]; then
+        echo "Tests failed with a real backend service. This is likely a real bug or a test needs updates."
+        exit $test_result
+    else
+        test_result=1 # we still failed and need updates
+        echo "Tests failed with mocks, but passed with a real service. \
+        Mocks likely need updates. \
+        Copy 'webui/react/src/e2e/mocks/debug-saved-imposters.jsonn' from the artifacts, \
+        and replace mocks/saved-imposters.json to update the mocks."
+        make mb-save-imposters
+    fi
 fi
-kill "$PREVIEW_PID"
+
 make mb-stop
+exit $test_result
