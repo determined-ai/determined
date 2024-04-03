4d974dd5c158c68466ee511fc2ad8e2a09691b2f
Jerry Gong <jgong@hpe.com>
2024-04-03 18:27:09 -0400

    Address comments

--- harness/tests/cli/test_user.py
+++ harness/tests/cli/test_user.py
@@ -50,27 +50,33 @@ def test_user_edit_no_fields(mock_die: mock.MagicMock) -> None:
         )
 
 
-@mock.patch("getpass.getpass")
-@mock.patch("determined.cli.cli.die")
-def test_login_with_invalid_credentials_error_message(
-    mock_die: mock.MagicMock, mock_getpass: mock.MagicMock
-) -> None:
-    mock_getpass.side_effect = lambda *_: "newpass"
-    with responses.RequestsMock(
-        registry=responses.registries.OrderedRegistry, assert_all_requests_are_fired=True
-    ) as rsps:
-        util.expect_get_info(rsps)
-        rsps.post(
-            "http://localhost:8080/api/v1/auth/login",
-            status=401,
-        )
+@responses.activate(assert_all_requests_are_fired=True)
+@mock.patch("getpass.getpass", lambda *_: "newpass")
+def test_det_login_calls_login_endpoint() -> None:
+    util.expect_get_info()
+    userobj = bindings.v1User(active=True, admin=False, username="det-user", id=101)
+    login_resp = bindings.v1LoginResponse(token="test-token", user=userobj)
+    responses.post(
+        "http://localhost:8080/api/v1/auth/login", status=200, json=login_resp.to_json(True)
+    )
+    cli.main(["user", "login", "test-user"])
 
-        cli.main(["user", "login", "test-user"])
-        mock_die.assert_has_calls(
-            [
-                mock.call(
-                    "Failed to log in user: Invalid username/password combination. "
-                    "Please try again."
-                )
-            ]
-        )
+
+@responses.activate()
+@mock.patch("determined.common.api.authentication.TokenStore", util.MockTokenStore(strict=False))
+@mock.patch("getpass.getpass", lambda *_: "newpass")
+@mock.patch("determined.cli.cli.die")
+def test_login_returns_invalid_credentials_error_message(mock_die: mock.MagicMock) -> None:
+    util.expect_get_info()
+    responses.post(
+        "http://localhost:8080/api/v1/auth/login",
+        status=401,
+    )
+    cli.main(["user", "login", "test-user"])
+    mock_die.assert_has_calls(
+        [
+            mock.call(
+                "Failed to log in user: Invalid username/password combination. Please try again."
+            )
+        ]
+    )
