fa5ec3b2ffd2cbd72c265b23fa23c30662995a42
Jesse Amano <jesse.amano@hpe.com>
2024-04-03 12:19:22 -0700

    fix password generation for e2e tests?

--- e2e_tests/tests/cluster/test_users.py
+++ e2e_tests/tests/cluster/test_users.py
@@ -55,11 +55,12 @@ def extract_id_and_owner_from_exp_list(output: str) -> List[Tuple[int, str]]:
 @pytest.mark.e2e_cpu
 def test_post_user_api() -> None:
     new_username = api_utils.get_random_string()
+    new_password = api_utils.get_random_string() + "aA1"
 
     sess = api_utils.admin_session()
 
     user = bindings.v1User(active=True, admin=False, username=new_username)
-    body = bindings.v1PostUserRequest(password="", user=user)
+    body = bindings.v1PostUserRequest(password=new_password, user=user)
     resp = bindings.post_PostUser(sess, body=body)
     assert resp and resp.user
     assert resp.to_json()["user"]["username"] == new_username
@@ -73,7 +74,9 @@ def test_post_user_api() -> None:
             agentUid=1000, agentGid=1001, agentUser="username", agentGroup="groupname"
         ),
     )
-    resp = bindings.post_PostUser(sess, body=bindings.v1PostUserRequest(user=user))
+    resp = bindings.post_PostUser(
+        sess, body=bindings.v1PostUserRequest(user=user, password=new_password)
+    )
     assert resp and resp.user and resp.user.agentUserGroup
     assert resp.user.agentUserGroup.agentUser == "username"
     assert resp.user.agentUserGroup.agentGroup == "groupname"
@@ -91,7 +94,9 @@ def test_post_user_api() -> None:
     )
 
     with pytest.raises(errors.APIException):
-        bindings.post_PostUser(sess, body=bindings.v1PostUserRequest(user=user))
+        bindings.post_PostUser(
+            sess, body=bindings.v1PostUserRequest(user=user, password=new_password)
+        )
 
 
 @pytest.mark.e2e_cpu
