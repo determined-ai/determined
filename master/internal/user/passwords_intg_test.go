9263e324eed1c14f420a40a14070597cef97816a
Jesse Amano <jesse.amano@hpe.com>
2024-04-05 09:42:19 -0700

    test: add tests for password complexity checker

--- master/internal/user/passwords_intg_test.go
+++ master/internal/user/passwords_intg_test.go
@@ -14,11 +14,47 @@ import (
 
 func TestUpdateDefaultUserPasswords(t *testing.T) {
 	u := db.RequireMockUser(t, db.SingleDB())
-	err := SetUserPassword(context.Background(), u.Username, "abc")
+	err := SetUserPassword(context.Background(), u.Username, "abcDEF123!")
 	require.NoError(t, err)
 
 	nu, err := ByUsername(context.Background(), u.Username)
 	require.NoError(t, err)
-	ok := nu.ValidatePassword(ReplicateClientSideSaltAndHash("abc"))
+	ok := nu.ValidatePassword(ReplicateClientSideSaltAndHash("abcDEF123!"))
 	require.True(t, ok, "couldn't login after default changes")
+}
+
+func TestUpdateUserPasswordComplexityCheck(t *testing.T) {
+	u := db.RequireMockUser(t, db.SingleDB())
+
+	// empty passwords are grandfathered in, for now
+	nu, err := ByUsername(context.Background(), u.Username)
+	require.NoError(t, err)
+	ok := nu.ValidatePassword("")
+	require.True(t, ok, "couldn't login after changes")
+
+	// changing to a bad password is not allowed
+	var complexityErr PasswordComplexityErrors
+	err = SetUserPassword(context.Background(), u.Username, "")
+	require.ErrorAs(t, err, &complexityErr)
+	require.Len(t, complexityErr, 4)
+
+	err = SetUserPassword(context.Background(), u.Username, "abc")
+	require.ErrorAs(t, err, &complexityErr)
+	require.Len(t, complexityErr, 3)
+	require.Contains(t, complexityErr, errPasswordTooShort)
+	require.Contains(t, complexityErr, errPasswordRequiresUppercase)
+	require.Contains(t, complexityErr, errPasswordRequiresNumber)
+
+	err = SetUserPassword(
+		context.Background(),
+		u.Username,
+		"LEX LUTHOR TOOK 40 CAKES, THAT IS FOUR TENS, AND THAT'S TERRIBLE",
+	)
+	require.ErrorAs(t, err, &complexityErr)
+	require.Len(t, complexityErr, 1)
+	require.Contains(t, complexityErr, errPasswordRequiresLowercase)
+
+	// changing to a good password is allowed
+	err = SetUserPassword(context.Background(), u.Username, "Hunter2!")
+	require.NoError(t, err)
 }
