8d315b33b9f690a57cceed90457f4498f489dec2
Jesse Amano <jesse.amano@hpe.com>
2024-04-04 19:52:56 -0700

    test: python sdk and cli tests use passwords conforming to new requirements

--- harness/tests/cli/test_user.py
+++ harness/tests/cli/test_user.py
@@ -1,5 +1,6 @@
 from unittest import mock
 
+import pytest
 from responses import matchers
 
 from determined.cli import cli
@@ -10,7 +11,7 @@ from tests.cli import util
 
 @mock.patch("getpass.getpass")
 def test_user_change_password(mock_getpass: mock.MagicMock) -> None:
-    mock_getpass.side_effect = lambda *_: "newpass"
+    mock_getpass.side_effect = lambda *_: "ce93AA76-2f62-4f29-ab5d-c56a3375e702"
     with util.standard_cli_rsps() as rsps:
         userobj = bindings.v1User(active=True, admin=False, username="det-user", id=101)
         rsps.get(
@@ -19,7 +20,9 @@ def test_user_change_password(mock_getpass: mock.MagicMock) -> None:
             json={"user": userobj.to_json()},
         )
 
-        patchobj = bindings.v1PatchUser(isHashed=True, password=api.salt_and_hash("newpass"))
+        patchobj = bindings.v1PatchUser(
+            isHashed=True, password=api.salt_and_hash("ce93AA76-2f62-4f29-ab5d-c56a3375e702")
+        )
         rsps.patch(
             "http://localhost:8080/api/v1/users/101",
             status=200,
@@ -30,6 +33,28 @@ def test_user_change_password(mock_getpass: mock.MagicMock) -> None:
         )
 
         cli.main(["user", "change-password", "tgt-user"])
+
+    # cannot set password to blank
+    mock_getpass.side_effect = lambda *_: ""
+    with util.standard_cli_rsps() as rsps:
+        rsps.get(
+            "http://localhost:8080/api/v1/users/tgt-user/by-username",
+            status=200,
+            json={"user": userobj.to_json()},
+        )
+        with pytest.raises(SystemExit):
+            cli.main(["user", "change-password", "tgt-user"])
+
+    # cannot set password to something weak
+    mock_getpass.side_effect = lambda *_: "password"
+    with util.standard_cli_rsps() as rsps:
+        rsps.get(
+            "http://localhost:8080/api/v1/users/tgt-user/by-username",
+            status=200,
+            json={"user": userobj.to_json()},
+        )
+        with pytest.raises(SystemExit):
+            cli.main(["user", "change-password", "tgt-user"])
 
 
 @mock.patch("determined.cli.cli.die")
