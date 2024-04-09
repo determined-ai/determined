bb857e6e8dfde05f60b5af194165f6c7547e9117
Corban Beaird <corban.beaird@hpe.com>
2024-04-09 10:57:35 -0600

    chore: add custom project keys to project table and backfill

---
+++ master/static/migrations/20240409104254_add-custom-project-key.tx.up.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION function_generate_project_key(input_string TEXT)
+RETURNS TEXT AS $$
+DECLARE
+    prefix TEXT;
+    count_suffix TEXT;
+	count_value INT;
+BEGIN
+    -- Take the first 3 characters of the input string
+    prefix := LEFT(input_string, 3);
+	
+   	execute format('SELECT COUNT(*)+1 FROM projects WHERE key LIKE $1') 
+   		into count_value
+   		using prefix || '%';
+   	count_suffix := CAST(count_value as text);
+
+    -- Concatenate prefix and count suffix
+    RETURN prefix || count_suffix;
+END;
+$$ LANGUAGE plpgsql;
+
+ALTER TABLE projects ADD COLUMN key VARCHAR(5) UNIQUE;
+
+UPDATE projects SET key = function_generate_project_key(name) WHERE key IS NULL;
