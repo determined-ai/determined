033b013fd323780fb112db95aacfc60c6744ae11
Jerry Gong <jgong@hpe.com>
2024-04-15 11:53:52 -0400

    WIP

--- master/internal/stream/subscription.go
+++ master/internal/stream/subscription.go
@@ -3,8 +3,6 @@ package stream
 import (
 	"context"
 
-	"github.com/labstack/echo/v4"
-
 	"github.com/determined-ai/determined/master/pkg/model"
 	"github.com/determined-ai/determined/master/pkg/stream"
 )
@@ -62,6 +60,7 @@ func NewSubscriptionSet(
 				ps.Projects,
 				newPermFilter(ctx, user, ProjectMakePermissionFilter, &err),
 				newFilter(spec.Projects, ProjectMakeFilter, &err),
+				ProjectHydrateUpsertMsg(),
 			),
 			ProjectCollectStartupMsgs,
 		}
@@ -156,7 +155,7 @@ func startup[T stream.Msg, S any](
 	// Scan for historical msgs matching newly-added subscriptions.
 	newmsgs, err := state.CollectStartupMsgs(ctx, user, known, *spec)
 	if err != nil {
-		return echo.ErrCookieNotFound
+		return err
 	}
 	for _, msg := range newmsgs {
 		*msgs = append(*msgs, prepare(msg))
--- master/pkg/stream/stream.go
+++ master/pkg/stream/stream.go
@@ -1,12 +1,17 @@
 package stream
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"sync"
+
+	"github.com/pkg/errors"
 )
 
 // Msg is an object with a message and a sequence number and json marshal caching.
 type Msg interface {
+	GetID() int
 	SeqNum() int64
 	UpsertMsg() UpsertMsg
 	DeleteMsg() DeleteMsg
@@ -35,6 +40,7 @@ type UpsertMsg struct {
 
 // MarshalJSON returns a json marshaled UpsertMsg.
 func (u UpsertMsg) MarshalJSON() ([]byte, error) {
+	fmt.Printf("msg: %+v\n", u.Msg)
 	data := map[string]Msg{
 		u.JSONKey: u.Msg,
 	}
@@ -111,6 +117,8 @@ type Subscription[T Msg] struct {
 	permissionFilter func(T) bool
 	// wakeupID prevent duplicate wakeups if multiple events in a single Broadcast are relevant
 	wakeupID int64
+	// Hydrate an UpsertMsg.
+	hydrator func(int) (T, error)
 }
 
 // NewSubscription creates a new Subscription to messages of type T.
@@ -119,12 +127,14 @@ func NewSubscription[T Msg](
 	publisher *Publisher[T],
 	permFilter func(T) bool,
 	filterFn func(T) bool,
+	hydrator func(int) (T, error),
 ) Subscription[T] {
 	return Subscription[T]{
 		Streamer:         streamer,
 		Publisher:        publisher,
 		permissionFilter: permFilter,
 		filter:           filterFn,
+		hydrator:         hydrator,
 	}
 }
 
@@ -171,6 +181,34 @@ func (p *Publisher[T]) CloseAllStreamers() {
 	p.Subscriptions = nil
 }
 
+func (p *Publisher[T]) hydrateMsg(rawMsg T, idToSeq map[int]int64, sub *Subscription[T], ev Event[T]) interface{} {
+	var msg interface{}
+	// seq < rawMsg.SeqNum() and id doesn't exist in idToSeq
+	entityMsg, err := sub.hydrator(rawMsg.GetID())
+	if errors.Cause(err) == sql.ErrNoRows {
+		// This id has a delete event later
+		idToSeq[rawMsg.GetID()] = -1
+		return nil
+	}
+
+	upsertMsg := entityMsg.UpsertMsg()
+	fmt.Println("reach before filters")
+	// check filter again in case project move workspace that would be a fall out
+	if sub.filter(upsertMsg.Msg.(T)) && sub.permissionFilter(upsertMsg.Msg.(T)) {
+		fmt.Println("helper: check filter again")
+		ev.upsertCache = sub.Streamer.PrepareFn(upsertMsg)
+		// fmt.Println("helper: afterpreparemessage")
+		msg = ev.upsertCache
+		idToSeq[upsertMsg.Msg.GetID()] = upsertMsg.Msg.SeqNum()
+	} else {
+		// This id fall out later
+		fmt.Println("helper: fallout")
+		idToSeq[rawMsg.GetID()] = -1
+		return nil
+	}
+	return msg
+}
+
 // Broadcast receives a list of events, determines if they are
 // applicable to the publisher's subscriptions, and sends
 // appropriate messages to corresponding streamers.
@@ -181,25 +219,51 @@ func (p *Publisher[T]) Broadcast(events []Event[T]) {
 	// start with a fresh wakeupid
 	p.WakeupID++
 	wakeupID := p.WakeupID
+	idToSeq := map[int]int64{}
 
 	// check each event against each subscription
 	for sub := range p.Subscriptions {
+		// fmt.Println("NEW SUB")
 		func() {
 			for _, ev := range events {
+				// fmt.Printf("events idx: %v\n", i)
 				var msg interface{}
 				switch {
 				case ev.After != nil && sub.filter(*ev.After) && sub.permissionFilter(*ev.After):
-					// update, insert, or fallin: send the record to the client.
-					if ev.upsertCache == nil {
-						ev.upsertCache = sub.Streamer.PrepareFn((*ev.After).UpsertMsg())
+					rawMsg := *ev.After
+					// seq, ok := idToSeq[rawMsg.GetID()]
+					// fmt.Printf("events idx: %v, seq: %v, ok: %v\n", i, seq, ok)
+					if seq, ok := idToSeq[rawMsg.GetID()]; ok {
+						// update, insert, or fallin: send the record to the client.
+						if seq >= rawMsg.SeqNum() || seq == -1 {
+							// ignore this message
+							// fmt.Println("ignore this event. has cache.")
+							continue
+						} else {
+							// fmt.Println("upsert event seq > cacahed seq")
+							msg = p.hydrateMsg(rawMsg, idToSeq, sub, ev)
+							if msg == nil {
+								continue
+							}
+
+						}
+					} else {
+						fmt.Println("upsert no cached seq")
+						msg = p.hydrateMsg(rawMsg, idToSeq, sub, ev)
+						if msg == nil {
+							continue
+						}
+						fmt.Printf("idToSeq: %+v\n", idToSeq)
 					}
-					msg = ev.upsertCache
+
 				case ev.Before != nil && sub.filter(*ev.Before) && sub.permissionFilter(*ev.Before):
 					// deletion or fallout: tell the client the record is deleted.
+					rawMsg := *ev.Before
 					if ev.deleteCache == nil {
-						ev.deleteCache = sub.Streamer.PrepareFn((*ev.Before).DeleteMsg())
+						ev.deleteCache = sub.Streamer.PrepareFn(rawMsg.DeleteMsg())
 					}
 					msg = ev.deleteCache
+					// fmt.Println("delete msg")
 				default:
 					// ignore this message
 					continue
@@ -211,6 +275,7 @@ func (p *Publisher[T]) Broadcast(events []Event[T]) {
 					defer sub.Streamer.Cond.L.Unlock()
 					sub.Streamer.Cond.Signal()
 				}
+				fmt.Printf("broadcast msg: %#v\n", msg)
 				sub.Streamer.Msgs = append(sub.Streamer.Msgs, msg)
 			}
 		}()
