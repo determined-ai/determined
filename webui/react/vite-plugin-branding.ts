ff4bfc38d2cd06cad2ef863fe17e421b399cdc53
Eric <31023784+eecsliu@users.noreply.github.com>
2024-04-16 14:49:24 -0700

    feat: overwrite index.html description if EE (#9173)

---
+++ webui/react/vite-plugin-branding.ts
@@ -0,0 +1,18 @@
+import type { Plugin } from 'vite';
+
+export const brandHtml = (): Plugin => {
+  return {
+    name: 'brandHtml',
+    async transformIndexHtml(html: string) {
+      if (process.env.DET_BUILD_EE === 'true') {
+        const cheerio = await import('cheerio');
+        const $ = cheerio.load(html);
+        $('meta[name="description"]').each(function () {
+          $(this).attr('content', 'HPE Machine Learning Development Environment');
+        });
+        return $.html();
+      }
+      return html;
+    },
+  };
+};
