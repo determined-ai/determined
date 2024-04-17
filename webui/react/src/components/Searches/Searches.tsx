a743d8df3dd6e29bccd916486e39ddcb90b3a472
Ashton Galloway <ashton.galloway@hpe.com>
2024-04-17 16:54:55 -0400

    update searches

--- webui/react/src/components/Searches/Searches.tsx
+++ webui/react/src/components/Searches/Searches.tsx
@@ -847,9 +847,6 @@ const Searches: React.FC<Props> = ({ project }) => {
     <>
       <TableActionBar
         columnGroups={[V1LocationType.EXPERIMENT]}
-        excludedExperimentIds={excludedExperimentIds}
-        experiments={experiments}
-        filters={experimentFilters}
         formStore={formStore}
         initialVisibleColumns={columnsIfLoaded}
         isOpenFilter={isOpenFilter}
@@ -858,8 +855,7 @@ const Searches: React.FC<Props> = ({ project }) => {
         project={project}
         projectColumns={projectColumns}
         rowHeight={globalSettings.rowHeight}
-        selectAll={selectAll}
-        selectedExperimentIds={selectedExperimentIds}
+        selectedExperimentsMap={selectedExperimentIds}
         selection={settings.selection}
         sorts={sorts}
         tableViewMode={globalSettings.tableViewMode}
