1eaf24146ca6a1fd62220c23077c27a914f59e58
Anda <anda@determined.ai>
2024-04-25 10:31:26 -0700

    refactor test

--- harness/tests/common/api/test_session.py
+++ harness/tests/common/api/test_session.py
@@ -1,54 +1,49 @@
+import pytest
+
 from determined.common import api
 from determined.common.api import certs
 from tests.common import api_server
 
 
-def test_session_non_persistent_http_sessions() -> None:
-    with api_server.run_api_server(address=("localhost", 8080)) as master_url:
-        session = api.Session(
-            master=master_url, username="me", token="t1o2k3e4n5", cert=certs.Cert(noverify=True)
+class TestSession:
+    @staticmethod
+    @pytest.fixture(scope="class")
+    def test_master() -> str:
+        with api_server.run_api_server(address=("localhost", 8080)) as master_url:
+            yield master_url
+
+    @staticmethod
+    @pytest.fixture(scope="function")
+    def session(test_master: str) -> api.Session:
+        yield api.Session(
+            master=test_master, username="me", token="t1o2k3e4n5", cert=certs.Cert(noverify=True)
         )
 
-        # No persistent session was created, and we shouldn't have created any HTTP sessions.
-        assert session._http_session is None
-
-        resp_connections = []
-        num_requests = 5
-
-        # Make a few requests using the same session object.
-        for _ in range(num_requests):
+    def test_direct_instantiation_doesnt_reuse_requests_sessions(
+        self, session: api.Session
+    ) -> None:
+        # Make a few requests, and expect the request's session to be closed immediately after
+        # each request.
+        # To requests and underlying urllib3, this means the PoolManager should not contain any
+        # ConnectionPools.
+        for _ in range(3):
             resp = session.get(path="/info")
+            assert resp.connection  # for mypy
+            assert len(resp.connection.poolmanager.pools) == 0
 
-            # The underlying HTTP connection should be closed immediately after request.
-            # To requests and underlying urllib3, this means there are no pools in the poolmanager
-            assert len(resp.connection.poolmanager.pools) == 0  # type: ignore
-            resp_connections.append(resp.connection)  # type: ignore
+    def test_context_manager_reuses_requests_sessions(self, session: api.Session) -> None:
+        connection_pools = []
+        # Make a few requests across the same path and a different path and verify that the
+        # requests are made with the same requests session.
+        with session as sess:
+            for path in ["/info", "/info", "/users/me"]:
+                resp = sess.get(path=path)
 
-        # Expect each request to have been made with a different underlying HTTP connection.
-        assert len(set(resp_connections)) == len(resp_connections)
+                # urllib3 creates HTTP connections from the PoolManager's connection pool, so we
+                # assume that if there was one connection pool, it was the one the request used.
+                assert len(resp.connection.poolmanager.pools) == 1
 
-
-def test_session_persistent_http_sessions() -> None:
-    with api_server.run_api_server(address=("localhost", 8080)) as master_url:
-        session = api.Session(
-            master=master_url, username="me", token="t1o2k3e4n5", cert=certs.Cert(noverify=True)
-        )
-
-        resp_connections = []
-
-        # Within a session context, make a few requests.
-        with session as persistent_session:
-            # Expect a persistent HTTP session to be set.
-            assert persistent_session._http_session
-
-            num_requests = 5
-            for _ in range(num_requests):
-                resp = persistent_session.get(path="/info")
-
-                # There should always be one HTTP connection pool open.
-                assert len(resp.connection.poolmanager.pools) == 1  # type: ignore
-
-                resp_connections.append(resp.connection)  # type: ignore
-
-        # Expect each request to have been made with the same underlying HTTP connection.
-        assert len(set(resp_connections)) == 1
+                pool_key = resp.connection.poolmanager.pools.keys()[0]
+                connection_pool = resp.connection.poolmanager.pools.get(pool_key)
+                connection_pools.append(connection_pool)
+        assert len(set(connection_pools)) == 1
