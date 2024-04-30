3bda736f23de809ae5ea32e689dde50970eb86f0
Anda <anda@determined.ai>
2024-04-30 10:04:59 -0700

    mypy

--- harness/tests/common/api/test_session.py
+++ harness/tests/common/api/test_session.py
@@ -1,3 +1,5 @@
+from typing import Iterator
+
 import pytest
 
 from determined.common import api
@@ -8,13 +10,13 @@ from tests.common import api_server
 class TestSession:
     @staticmethod
     @pytest.fixture(scope="class")
-    def test_master() -> str:
+    def test_master() -> Iterator[str]:
         with api_server.run_api_server(address=("localhost", 8080)) as master_url:
             yield master_url
 
     @staticmethod
     @pytest.fixture(scope="function")
-    def session(test_master: str) -> api.Session:
+    def session(test_master: str) -> Iterator[api.Session]:
         yield api.Session(
             master=test_master, username="me", token="t1o2k3e4n5", cert=certs.Cert(noverify=True)
         )
@@ -28,8 +30,9 @@ class TestSession:
         # ConnectionPools.
         for _ in range(3):
             resp = session.get(path="/info")
-            assert resp.connection  # for mypy
-            assert len(resp.connection.poolmanager.pools) == 0
+            # mypy doesn't recognize attributes defined outside init.
+            resp_connection = getattr(resp, "connection", None)
+            assert len(resp_connection.poolmanager.pools) == 0
 
     def test_context_manager_reuses_requests_sessions(self, session: api.Session) -> None:
         connection_pools = []
@@ -41,9 +44,11 @@ class TestSession:
 
                 # urllib3 creates HTTP connections from the PoolManager's connection pool, so we
                 # assume that if there was one connection pool, it was the one the request used.
-                assert len(resp.connection.poolmanager.pools) == 1
+                # mypy doesn't recognize attributes defined outside init.
+                resp_connection = getattr(resp, "connection", None)
+                assert len(resp_connection.poolmanager.pools) == 1
 
-                pool_key = resp.connection.poolmanager.pools.keys()[0]
-                connection_pool = resp.connection.poolmanager.pools.get(pool_key)
+                pool_key = resp_connection.poolmanager.pools.keys()[0]
+                connection_pool = resp_connection.poolmanager.pools.get(pool_key)
                 connection_pools.append(connection_pool)
         assert len(set(connection_pools)) == 1
