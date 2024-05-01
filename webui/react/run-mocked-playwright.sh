4f3d14407f240d9f017f4e0b9c73762758f25a9f
Daniel Janicek <daniel.janicek@hpe.com>
2024-05-01 08:23:15 -0700

    mock update

--- webui/react/run-mocked-playwright.sh
+++ webui/react/run-mocked-playwright.sh
@@ -8,17 +8,20 @@ export DET_WEBSOCKET_PROXY_URL="ws://localhost:4546"
 export PW_SERVER_ADDRESS="http://localhost:3001"
 export PW_TEST_HTML_REPORT_OPEN='never'
 test_result=0
-if [[ "$PW_MOCK_RECORD_ONLY" != 'true' ]]; then
+if [[ "$PW_MOCK_RECORD_ONLY" != "true" ]]; then
     make mb-start-saved-imposters
     set +e
     PW_USER_NAME="admin" PW_PASSWORD="" \
     PW_SERVER_ADDRESS="http://localhost:3001"\
     DET_WEBSOCKET_PROXY_URL="ws://localhost:4546" DET_WEBPACK_PROXY_URL="http://localhost:4545" \
-    npm run e2e
+    npm run e2e -- userManagement.spec.ts -g "With a Test User" --project=mock-env
     test_result=$?
     set -e
+    if [[ "$PW_MOCK_ONLY" == "true" ]]; then 
+        exit $test_result
+    fi
 fi
-if [ "$PW_MOCK_RECORD_ONLY" == 'true' || $test_result -ne 0 ]; then
+if [[ "$PW_MOCK_RECORD_ONLY" == "true" || $test_result -ne 0 ]]; then
     echo "Running tests with mock recorder"
     make mb-stop
     make mb-start
@@ -27,10 +30,10 @@ if [ "$PW_MOCK_RECORD_ONLY" == 'true' || $test_result -ne 0 ]; then
     PW_USER_NAME="admin" PW_PASSWORD="" \
     PW_SERVER_ADDRESS="http://localhost:3001"\
     DET_WEBSOCKET_PROXY_URL="ws://localhost:4546" DET_WEBPACK_PROXY_URL="http://localhost:4545" \
-    npm run e2e
+    npm run e2e -- userManagement.spec.ts -g "With a Test User" --project=mock-env
     test_result=$?
     set -e
-    if [ $test_result -ne 0 ]; then
+    if [[ $test_result -ne 0 ]]; then
         echo "Tests failed with a real backend service. This is likely a real bug or a test needs updates."
         exit $test_result
     else
