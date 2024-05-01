98af70f2a9a75beabd1ab78e51d20d1d514e7d2d
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2024-05-01 00:00:00 -0500

    not need for further entrypoint changes

--- master/static/srv/notebook-entrypoint.sh
+++ master/static/srv/notebook-entrypoint.sh
@@ -24,6 +24,10 @@ set +x
 JUPYTER_LAB_LOG_FORMAT="%(levelname)s: [%(name)s] %(message)s"
 READINESS_REGEX='^.*Jupyter Server .* is running.*$'
 
+# f"det-{task_id[:8]}"
+ingress_prefix="det-${DET_TASK_ID:0:8}"
+# --ServerApp.base_url="/$ingress_prefix/proxy/${DET_TASK_ID}/" \
+
 jupyter lab --ServerApp.port=${NOTEBOOK_PORT} \
     --ServerApp.allow_origin="*" \
     --ServerApp.base_url="/proxy/${DET_TASK_ID}/" \
