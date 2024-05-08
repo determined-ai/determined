e6f6bb418643f2d45b796b2e6830997ace66ba21
Justin Comins <justin.comins@hpe.com>
2024-05-08 11:01:59 -0700

    comment the bash env code

--- webui/react/src/e2e/utils/detCLI.ts
+++ webui/react/src/e2e/utils/detCLI.ts
@@ -12,22 +12,6 @@ export function fullPath(relativePath: string): string {
   return path.join(process.cwd(), relativePath);
 }
 
-// export function detAuthenticate(): string {
-//   detExecSync('user logout');
-//   try {
-//     return execSync(
-//       `echo ${process.env.PW_PASSWORD} | ${detExecBase} user login ${process.env.PW_USER_NAME}`,
-//       { stdio: ['pipe', 'inherit', 'pipe'], timeout: 5_000 },
-//     ).toString();
-//   } catch (e: unknown) {
-//     if (typeof e === 'object' && e !== null && 'stderr' in e && typeof e.stderr === 'string') {
-//       throw new Error('detExecSync error: ' + e.stderr);
-//     } else {
-//       throw e;
-//     }
-//   }
-// }
-
 export function detExecSync(detCommand: string): string {
   try {
     return execSync(`${process.env.PW_DET_PATH} ${detCommand}`, {
