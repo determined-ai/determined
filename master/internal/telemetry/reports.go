86aa319861ce35d65d1a0dd538b069f398dbfa2f
Shreya <167355081+ShreyaLnuHpe@users.noreply.github.com>
2024-05-09 10:27:20 -0500

    chore: Bunify and add test coverage for `ExperimentTotalStepTime` and `ExperimentNumSteps` (#9333)

--- master/internal/telemetry/reports.go
+++ master/internal/telemetry/reports.go
@@ -172,8 +172,8 @@ func fetchNumTrials(db db.DB, experimentID int) *int64 {
 	return &result
 }
 
-func fetchNumSteps(db db.DB, experimentID int) *int64 {
-	result, err := db.ExperimentNumSteps(experimentID)
+func fetchNumSteps(experimentID int) *int64 {
+	result, err := db.ExperimentNumSteps(context.TODO(), experimentID)
 	if err != nil {
 		syslog.WithError(err).Warn("failed to fetch telemetry metrics")
 		return nil
@@ -181,8 +181,8 @@ func fetchNumSteps(db db.DB, experimentID int) *int64 {
 	return &result
 }
 
-func fetchTotalStepTime(db db.DB, experimentID int) *float64 {
-	result, err := db.ExperimentTotalStepTime(experimentID)
+func fetchTotalStepTime(experimentID int) *float64 {
+	result, err := db.ExperimentTotalStepTime(context.TODO(), experimentID)
 	if err != nil {
 		syslog.WithError(err).Warn("failed to fetch telemetry metrics")
 		return nil
@@ -200,8 +200,8 @@ func ReportExperimentStateChanged(db db.DB, e *model.Experiment) {
 		// Report additional metrics when an experiment reaches a terminal state.
 		// These metrics are null for non-terminal state transitions.
 		numTrials = fetchNumTrials(db, e.ID)
-		numSteps = fetchNumSteps(db, e.ID)
-		totalStepTime = fetchTotalStepTime(db, e.ID)
+		numSteps = fetchNumSteps(e.ID)
+		totalStepTime = fetchTotalStepTime(e.ID)
 	}
 
 	defaultTelemeter.track(
