7924b58a438d9285d1e599446439ac16a7032bd3
Corban Beaird <corban.beaird@hpe.com>
2024-05-20 15:57:12 -0600

    refactor: shift triggers & view work into new view_and_triggers directory

--- master/static/migrations/20240520133440_add-arbitrary-metadata-table.tx.up.sql
+++ master/static/migrations/20240520133440_add-arbitrary-metadata-table.tx.up.sql
@@ -11,7 +11,7 @@ CREATE TABLE runs_metadata_index (
     run_id INTEGER,
     flat_key VARCHAR,
     value TEXT,
-    data_type VARCHAR, -- 'string', 'number', 'boolean', 'timestamp', null
+    data_type VARCHAR, -- 'string', 'integer', 'float', 'boolean', 'timestamp'
     project_id INTEGER,
     FOREIGN KEY (run_id) REFERENCES runs(id) ON DELETE CASCADE,
     FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
@@ -19,19 +19,3 @@ CREATE TABLE runs_metadata_index (
 -- CREATE FILTER-OPTIMIZING INDEXES
 CREATE INDEX idx_flat_key ON runs_metadata_index (flat_key);
 CREATE INDEX idx_flat_key_value ON runs_metadata_index (flat_key, value);
-
--- Keep the run metadata indexes up to date with the project_id
-CREATE OR REPLACE FUNCTION func_update_run_metadata_index_project_id()
-RETURNS TRIGGER AS $$
-BEGIN
-UPDATE runs_metadata_index
-    SET project_id = NEW.project_id
-    WHERE run_id = NEW.id;
-RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-DROP TRIGGER IF EXISTS trigger_update_run_metadata_index_project_id ON runs;
-CREATE TRIGGER trigger_update_run_metadata_index_project_id
-AFTER UPDATE OF project_id ON runs
-FOR EACH ROW EXECUTE PROCEDURE func_update_run_metadata_index_project_id();
