7409561dc366accd03bd407e5f82b03b2d6feace
AmanuelAaron <aaron.amanuel@hpe.com>
2024-05-20 09:07:16 -0400

    recursive migration

--- master/static/migrations/20240508134054_add-run-hparam-table.tx.up.sql
+++ master/static/migrations/20240508134054_add-run-hparam-table.tx.up.sql
@@ -6,20 +6,20 @@ CREATE TABLE run_hparams (
     bool_val boolean NULL
 );
 
-INSERT INTO run_hparams(run_id, hparam, number_val, text_val, bool_val)
-SELECT s.id as run_id, s.key as hparam,
-CASE WHEN s.type='number' THEN s.value::text::float ELSE NULL END as number_val,
-CASE WHEN s.type='string' THEN s.value::text ELSE NULL END as text_val,
-CASE WHEN s.type='boolean' THEN s.value::text::boolean ELSE NULL END as bool_val
-FROM (SELECT r.id, h.key, h.value, jsonb_typeof(h.value) as type FROM runs as r, jsonb_each(r.hparams) as h WHERE r.hparams is not NULL AND r.hparams !='null') as s WHERE s.type!='object';
+WITH RECURSIVE flat (run_id, key, value, type) AS (
+    SELECT r.id, h.key, h.value, jsonb_typeof(h.value) as type
+    FROM runs as r, jsonb_each(r.hparams) as h WHERE r.hparams is not NULL AND r.hparams !='null' 
+	UNION
+	SELECT f.run_id,concat(f.key, '.', j.key) as key, j.value, jsonb_typeof(j.value) as type
+    FROM flat f, jsonb_each(f.value) j WHERE f.type = 'object'
+	)
 
 INSERT INTO run_hparams(run_id, hparam, number_val, text_val, bool_val)
-SELECT n.id as run_id, n.key as hparam,
-CASE WHEN n.type='number' THEN n.value::text::float ELSE NULL END as number_val,
-CASE WHEN n.type='string' THEN n.value::text ELSE NULL END as text_val,
-CASE WHEN n.type='boolean' THEN n.value::text::boolean ELSE NULL END as bool_val
-FROM (SELECT s.id, CONCAT(s.key, '.', nh.key) as key, nh.value,  jsonb_typeof(nh.value) as type FROM (SELECT r.id, h.key, h.value, jsonb_typeof(h.value) as type FROM runs as r, jsonb_each(r.hparams) as h WHERE r.hparams is not NULL AND r.hparams !='null') as s, jsonb_each(s.value) nh WHERE s.type='object') as n;
-
+SELECT run_id, key, 
+CASE WHEN type='number' THEN value::text::float ELSE NULL END as number_val,
+CASE WHEN type='string' THEN value::text ELSE NULL END as text_val,
+CASE WHEN type='boolean' THEN value::text::boolean ELSE NULL END as bool_val
+FROM flat WHERE type !='object'
 
 CREATE TABLE project_hparams (
     project_id int REFERENCES projects(id) ON DELETE CASCADE,
