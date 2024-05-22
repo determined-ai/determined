6573eea7947d85f3b84ffdba335dc781e5e783e1
Mikhail Kardash <mikhail.kardash@hpe.com>
2024-05-22 09:23:04 -0700

    different images

--- master/pkg/schemas/expconf/const.go
+++ master/pkg/schemas/expconf/const.go
@@ -8,7 +8,7 @@ const (
 
 // Default task environment docker image names.
 const (
-	CPUImage  = "determinedai/pytorch-ngc-dev:e960eae"
-	CUDAImage = "determinedai/pytorch-ngc-dev:e960eae"
+	CPUImage  = "determinedai/pytorch-ngc-hpc-dev:e960eae"
+	CUDAImage = "determinedai/pytorch-ngc-hpc-dev:e960eae"
 	ROCMImage = "determinedai/environments:rocm-5.0-pytorch-1.10-tf-2.7-rocm-622d512"
 )
