99d1e2af2e33f055318a35bb05c429383650fee3
Daniel Janicek <daniel.janicek@hpe.com>
2024-05-22 08:11:24 -0700

    lint

--- webui/react/src/e2e/fixtures/api.auth.fixture.ts
+++ webui/react/src/e2e/fixtures/api.auth.fixture.ts
@@ -1,10 +1,4 @@
-import {
-  APIRequest,
-  APIRequestContext,
-  Browser,
-  BrowserContext,
-  Page,
-} from '@playwright/test';
+import { APIRequest, APIRequestContext, Browser, BrowserContext, Page } from '@playwright/test';
 
 export class ApiAuthFixture {
   apiContext: APIRequestContext | undefined; // we can't get this until login, so may be undefined
