c2a2203ff85e462902b727ec807e6815100627ac
Keita Nonaka <keita.nonaka@hpe.com>
2024-05-23 16:31:46 -0700

    feat: add RBAC permissions for flatRuns

---
+++ webui/react/src/utils/flatRun.ts
@@ -0,0 +1,72 @@
+import { deletableRunStates, killableRunStates, terminalRunStates } from 'constants/states';
+import { PermissionsHook } from 'hooks/usePermissions';
+import { FlatRun, FlatRunAction } from 'types';
+
+type FlatRunChecker = (flatRun: FlatRun) => boolean;
+
+type FlatRunPermissionSet = Pick<
+  PermissionsHook,
+  'canCreateExperiment' | 'canDeleteFlatRun' | 'canModifyExperiment' | 'canMoveFlatRun'
+>;
+
+const flatRunCheckers: Record<FlatRunAction, FlatRunChecker> = {
+  [FlatRunAction.Archive]: (flatRun) =>
+    !flatRun.parentArchived && !flatRun.parentArchived && terminalRunStates.has(flatRun.state),
+
+  [FlatRunAction.Delete]: (flatRun) => deletableRunStates.has(flatRun.state),
+
+  [FlatRunAction.Kill]: (flatRun) => killableRunStates.includes(flatRun.state),
+
+  [FlatRunAction.Move]: (flatRun) => !flatRun?.parentArchived && !flatRun.parentArchived,
+
+  // [FlatRunAction.Pause]: (run) => pausableRunStates.has(run.state),
+
+  [FlatRunAction.Unarchive]: (flatRun) =>
+    terminalRunStates.has(flatRun.state) && flatRun.parentArchived,
+};
+
+export const canActionFlatRun = (action: FlatRunAction, flatRun: FlatRun): boolean => {
+  return flatRunCheckers[action](flatRun);
+};
+
+const getActionsForFlatRun = (
+  flatRun: FlatRun,
+  targets: FlatRunAction[],
+  permissions: FlatRunPermissionSet,
+): FlatRunAction[] => {
+  if (!flatRun) return []; // redundant, for clarity
+  const workspace = { id: flatRun.workspaceId };
+  return targets
+    .filter((action) => canActionFlatRun(action, flatRun))
+    .filter((action) => {
+      switch (action) {
+        case FlatRunAction.Delete:
+          return permissions.canDeleteFlatRun({ flatRun });
+
+        case FlatRunAction.Move:
+          return permissions.canMoveFlatRun({ flatRun });
+
+        case FlatRunAction.Archive:
+        case FlatRunAction.Unarchive:
+          return permissions.canModifyExperiment({ workspace });
+
+        case FlatRunAction.Kill:
+          return permissions.canModifyExperiment({ workspace }) && !flatRun.experiment?.unmanaged;
+
+        default:
+          return true;
+      }
+    });
+};
+
+export const getActionsForFlatRunsUnion = (
+  flatRun: FlatRun[],
+  targets: FlatRunAction[],
+  permissions: FlatRunPermissionSet,
+): FlatRunAction[] => {
+  if (!flatRun.length) return [];
+  const actionsForRuns = flatRun.map((run) => getActionsForFlatRun(run, targets, permissions));
+  return targets.filter((action) =>
+    actionsForRuns.some((runActions) => runActions.includes(action)),
+  );
+};
