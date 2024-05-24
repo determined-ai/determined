385c34a590f7a0eff5ea906737dab0a351900add
Keita Nonaka <keita.nonaka@hpe.com>
2024-05-24 11:09:46 -0700

    chore: minor changes

--- webui/react/src/pages/FlatRuns/FlatRuns.tsx
+++ webui/react/src/pages/FlatRuns/FlatRuns.tsx
@@ -52,7 +52,6 @@ import {
   rowHeightMap,
   settingsConfigGlobal,
 } from 'components/OptionsMenu.settings';
-import { BatchAction } from 'components/TableActionBar';
 import useUI from 'components/ThemeProvider';
 import { useAsync } from 'hooks/useAsync';
 import { useDebouncedSettings } from 'hooks/useDebouncedSettings';
@@ -707,14 +706,10 @@ const FlatRuns: React.FC<Props> = ({ projectId, searchId }) => {
     [rowRangeToIds, settings.selection, updateSettings],
   );
 
-  const onActionSuccess = useCallback(
-    // TODO: update this function
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    (_action: BatchAction, _successfulIds: number[]) => {
-      handleSelectionChange('remove-all');
-    },
-    [handleSelectionChange],
-  );
+  const onActionComplete = useCallback(async () => {
+    handleSelectionChange('remove-all');
+    await fetchRuns();
+  }, [fetchRuns, handleSelectionChange]);
 
   const handleContextMenuComplete: ContextMenuCompleteHandlerProps<ExperimentAction, FlatRun> =
     useCallback(() => {}, []);
@@ -1010,7 +1005,7 @@ const FlatRuns: React.FC<Props> = ({ projectId, searchId }) => {
               isMobile={isMobile}
               project={project}
               selectedRuns={selectedRuns}
-              onActionSuccess={onActionSuccess}
+              onActionComplete={onActionComplete}
             />
           </Row>
         </Column>
