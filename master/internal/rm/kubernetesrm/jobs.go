e8dc1a17d7103fba0bc34700946ff6c4075e42a6
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2024-06-04 16:14:07 -0400

    working

--- master/internal/rm/kubernetesrm/jobs.go
+++ master/internal/rm/kubernetesrm/jobs.go
@@ -175,7 +175,8 @@ func newJobsService(
 		syslog:                            logrus.WithField("namespace", namespace),
 		jobSchedulingStateCallback:        jobSchedulingStateCb,
 
-		kubeconfigPath: kubeconfigPath,
+		exposeProxyConfig: exposeProxyConfig,
+		kubeconfigPath:    kubeconfigPath,
 	}
 
 	if err := p.startClientSet(); err != nil {
