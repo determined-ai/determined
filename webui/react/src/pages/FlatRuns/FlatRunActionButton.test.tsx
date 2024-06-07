4e1a32509f93e8fd886e7074f800704e0de54efe
Keita Nonaka <keita.nonaka@hpe.com>
2024-06-07 01:03:31 -0700

    test: `FlatRunMoveModal`

--- webui/react/src/pages/FlatRuns/FlatRunActionButton.test.tsx
+++ webui/react/src/pages/FlatRuns/FlatRunActionButton.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import dayjs from 'dayjs';
 import UIProvider, { DefaultTheme } from 'hew/Theme';
 
@@ -7,6 +8,8 @@ import FlatRunActionButton from 'pages/FlatRuns/FlatRunActionButton';
 import { FlatRun, RunState } from 'types';
 
 const setup = (selectedFlatRuns: ReadonlyArray<Readonly<FlatRun>>) => {
+  const user = userEvent.setup();
+
   render(
     <UIProvider theme={DefaultTheme.Light}>
       <FlatRunActionButton
@@ -22,34 +25,50 @@ const setup = (selectedFlatRuns: ReadonlyArray<Readonly<FlatRun>>) => {
       />
     </UIProvider>,
   );
+
+  return {
+    user,
+  };
 };
 
 describe('canActionFlatRun function', () => {
   describe('Flat Run Action Button Visibility', () => {
+    const flatRuns: ReadonlyArray<Readonly<FlatRun>> = [
+      {
+        archived: false,
+        checkpointCount: 0,
+        checkpointSize: 0,
+        id: 1,
+        parentArchived: false,
+        projectId: 1,
+        projectName: 'test',
+        startTime: dayjs('2024-05-24T23:03:45.415603Z').toDate(),
+        state: RunState.Active,
+        workspaceId: 10,
+        workspaceName: 'test',
+      },
+    ];
+
     it('should not be appeard without selected flat runs', () => {
       setup([]);
       expect(screen.queryByText('Actions')).not.toBeInTheDocument();
     });
 
     it('should be appeard with selected flat runs', async () => {
-      const flatRuns: ReadonlyArray<Readonly<FlatRun>> = [
-        {
-          archived: false,
-          checkpointCount: 0,
-          checkpointSize: 0,
-          id: 1,
-          parentArchived: false,
-          projectId: 1,
-          projectName: 'test',
-          startTime: dayjs('2024-05-24T23:03:45.415603Z').toDate(),
-          state: RunState.Active,
-          workspaceId: 10,
-          workspaceName: 'test',
-        },
-      ];
-
       setup(flatRuns);
       expect(await screen.findByText('Actions')).toBeInTheDocument();
+    });
+
+    it('should show action list', async () => {
+      const { user } = setup(flatRuns);
+
+      const actionButton = await screen.findByText('Actions');
+      await user.click(actionButton);
+      expect(await screen.findByText('Move')).toBeInTheDocument();
+      expect(await screen.findByText('Archive')).toBeInTheDocument();
+      expect(await screen.findByText('Unarchive')).toBeInTheDocument();
+      expect(await screen.findByText('Delete')).toBeInTheDocument();
+      expect(await screen.findByText('Kill')).toBeInTheDocument();
     });
   });
 });
