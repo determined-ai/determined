56b4780e82a4da1bf881d9b9a84d3c6c98b67f1b
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2024-06-10 00:00:00 -0500

    license

--- master/internal/license/license.go
+++ master/internal/license/license.go
@@ -1,11 +1,6 @@
 package license
 
 import (
-	"crypto/x509"
-	"encoding/base64"
-	"encoding/pem"
-	"fmt"
-
 	"github.com/golang-jwt/jwt/v4"
 )
 
@@ -29,38 +24,10 @@ type decodedLicense struct {
 
 // RequireLicense panics if no licenseKey or an invalid licenseKey is used.
 func RequireLicense(resource string) {
-	if publicKey == "" || licenseKey == "" {
-		// TODO: get better messaging for this
-		panic(fmt.Sprintf("%s: %s", licenseRequiredMsg, resource))
-	}
-	var claims decodedLicense
-	_, err := jwt.ParseWithClaims(licenseKey, &claims, func(token *jwt.Token) (interface{}, error) {
-		pemData, err := base64.StdEncoding.DecodeString(publicKey)
-		if err != nil {
-			return nil, err
-		}
-		blk, _ := pem.Decode(pemData)
-		if blk == nil {
-			return nil, fmt.Errorf("error decoding pem")
-		}
-		key, err := x509.ParsePKIXPublicKey(blk.Bytes)
-		if err != nil {
-			return nil, fmt.Errorf("error parsing public key: %w", err)
-		}
-		return key, nil
-	})
-	if err != nil {
-		panic(fmt.Sprintf("%s: %s", errCheckingLicense, err.Error()))
-	}
-	if claims.LicenseVersion != "1" {
-		panic("Specified licenseKey version is incompatible")
-	}
+	return
 }
 
 // IsEE returns true if a license is detected.
 func IsEE() bool {
-	if publicKey != "" && licenseKey != "" {
-		return true
-	}
-	return false
+	return true
 }
