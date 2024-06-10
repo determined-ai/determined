971e08e1cc50b3abe756a0d8149c9e1899b706c7
Keita Nonaka <keita.nonaka@hpe.com>
2024-06-10 15:16:19 -0700

    test: test

--- webui/react/src/services/decoder.test.ts
+++ webui/react/src/services/decoder.test.ts
@@ -1,6 +1,8 @@
 import hparams from 'fixtures/hyperparameter-configs.json';
 import experimentResps from 'fixtures/responses/experiment-details/set-a.json';
 import * as ioTypes from 'ioTypes';
+import { V1ExperimentActionResult, V1RunActionResult } from 'services/api-ts-sdk';
+import * as decoder from 'services/decoder';
 
 type FailReport<T = unknown> = { error: Error; sample: T };
 
@@ -39,5 +41,81 @@ describe('Decoder', () => {
       },
     );
     expect(fails).toHaveLength(0);
+  });
+
+  describe('mapV1ActionResults', () => {
+    it('should work with Sdk.V1ExperimentActionResult[] input', () => {
+      const result: V1ExperimentActionResult[] = [
+        { error: '', id: 1 },
+        { error: '', id: 2 },
+        { error: 'error', id: 3 },
+      ];
+
+      const expected = decoder.mapV1ActionResults(result);
+      expect(expected).toStrictEqual({
+        failed: [{ error: 'error', id: 3 }],
+        successful: [1, 2],
+      });
+    });
+
+    it('should work with Sdk.V1RunActionResult[] input', () => {
+      const result: V1RunActionResult[] = [
+        { error: '', id: 1 },
+        { error: '', id: 2 },
+        { error: 'error', id: 3 },
+      ];
+
+      const expected = decoder.mapV1ActionResults(result);
+      expect(expected).toStrictEqual({
+        failed: [{ error: 'error', id: 3 }],
+        successful: [1, 2],
+      });
+    });
+
+    it('should work with empty input', () => {
+      const expected = decoder.mapV1ActionResults([]);
+      expect(expected).toStrictEqual({
+        failed: [],
+        successful: [],
+      });
+    });
+
+    it('should work with all successful input', () => {
+      const result: V1RunActionResult[] = [
+        { error: '', id: 1 },
+        { error: '', id: 2 },
+        { error: '', id: 3 },
+        { error: '', id: 4 },
+        { error: '', id: 5 },
+      ];
+
+      const expected = decoder.mapV1ActionResults(result);
+      expect(expected).toStrictEqual({
+        failed: [],
+        successful: [1, 2, 3, 4, 5],
+      });
+    });
+
+    it('should work with all failed input', () => {
+      const result: V1RunActionResult[] = [
+        { error: 'oh no', id: 1 },
+        { error: 'yare yare', id: 2 },
+        { error: 'error', id: 3 },
+        { error: 'a', id: 4 },
+        { error: 'エラー', id: 5 },
+      ];
+
+      const expected = decoder.mapV1ActionResults(result);
+      expect(expected).toStrictEqual({
+        failed: [
+          { error: 'oh no', id: 1 },
+          { error: 'yare yare', id: 2 },
+          { error: 'error', id: 3 },
+          { error: 'a', id: 4 },
+          { error: 'エラー', id: 5 },
+        ],
+        successful: [],
+      });
+    });
   });
 });
