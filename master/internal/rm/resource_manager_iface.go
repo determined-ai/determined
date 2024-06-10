b61d688e78d4d91ca95261f12eb814c4c362ca59
Amanda Vialva <144278621+amandavialva01@users.noreply.github.com>
2024-06-10 10:34:44 -0400

    chore: cleanup auto-created namespaces during workspace deletion (#9329)

--- master/internal/rm/resource_manager_iface.go
+++ master/internal/rm/resource_manager_iface.go
@@ -58,6 +58,7 @@ type ResourceManager interface {
 	// Kubernetes Namespaces.
 	DefaultNamespace(string) (*string, error)
 	VerifyNamespaceExists(string, string) error
+	DeleteNamespace(string) error
 }
 
 // ResourcePoolName holds the name of the resource pool, and describes the input/output
