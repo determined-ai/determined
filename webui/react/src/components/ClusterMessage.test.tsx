1a2341d5ff129f004a6070d1590c86bd7b6c8a1a
Max Russell <max.russell@hpe.com>
2024-06-10 21:47:02 -0700

    ClusterMessage component test fixup

--- webui/react/src/components/ClusterMessage.test.tsx
+++ webui/react/src/components/ClusterMessage.test.tsx
@@ -4,29 +4,30 @@ import { render, screen } from '@testing-library/react';
 import { ClusterMessage } from 'stores/determinedInfo';
 import ClusterMessageBanner from './ClusterMessage';
 
-const setup = (msg?: ClusterMessage) => {
+const setUp = (msg?: ClusterMessage) => {
   render(<ClusterMessageBanner message={msg} />); // render arbitrary components
 };
 
 const msg = 'this is a test msg for use in the ClusterMessageBanner test';
 
 describe('ClusterMessageBanner', () => {
-  it('should have a banner with cluster message text', () => {
+  it('should have a banner with cluster message text when there is a cluster message', () => {
     const time = new Date();
     const testMsg = { createdTime: time, endTime: time, message: msg, startTime: time };
 
-    setup(testMsg);
+    setUp(testMsg);
     // make sure these components exist.
     expect(screen.getByTestId('admin-msg')).toBeInTheDocument();
     expect(screen.getByTestId('cluster-msg')).toBeInTheDocument();
 
     // make sure the cluster message is visible, but the admin msg is not.
     expect(screen.getByText(msg)).toBeInTheDocument();
-    expect(screen.queryByText('Message from Admin')).not.toBeInTheDocument();
+    expect(screen.getByTestId('admin-msg')).toBeInTheDocument();
+    expect(screen.getByTestId('cluster-msg').textContent).toEqual(msg);
   });
 
-  it('should not have a banner', () => {
-    setup();
+  it('should not have a banner when there is no cluster message', () => {
+    setUp();
 
     // setting a null message means nothing should show up in the ui.
     expect(screen.queryByTestId('admin-msg')).not.toBeInTheDocument();
