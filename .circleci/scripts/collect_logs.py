4b15b312a3e4df04b2390054502dcb08edb2491f
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2024-06-12 00:00:00 -0500

    simplifications

--- .circleci/scripts/collect_logs.py
+++ .circleci/scripts/collect_logs.py
@@ -7,15 +7,15 @@ from typing import List, Union
 import requests
 
 from determined.common import api
+from determined.common.api import bindings as b
 from determined.common.api import certs
 
-Task = Union["b.v1Command", "b.v1Notebook", "b.v1Shell", "b.v1Tensorboard"]
+Task = Union[b.v1Command, b.v1Notebook, b.v1Shell, b.v1Tensorboard]
 
 
 def obtain_token(username: str, password: str, master_address: str) -> str:
     """
     Gets a Determined token without using a Session.
-    TODO: There is a replacement somewhere.
     """
     response = requests.post(
         f"{master_address}/api/v1/auth/login",
@@ -23,9 +23,7 @@ def obtain_token(username: str, password: str, master_address: str) -> str:
         verify=False,
     )
     response.raise_for_status()
-    token = response.json()["token"]
-    assert isinstance(token, str)
-    return token
+    return response.json()["token"]
 
 
 class CliBase:
@@ -37,15 +35,12 @@ class CliBase:
         self.username = username
         self.password = password
         self.mlde_host = mlde_host
-        token = obtain_token(username, password, master_address=mlde_host)
-        self.token = token
+        self.token = obtain_token(username, password, master_address=mlde_host)
         cert = certs.Cert(noverify=True)
-        session = api.Session(mlde_host, username=username, token=token, cert=cert)
-        self.session = session
+        self.session = api.Session(mlde_host, username=username, token=self.token, cert=cert)
 
 
 warnings.filterwarnings("ignore", category=FutureWarning, module="determined.*")
-from determined.common.api import bindings as b
 
 
 class Cli(CliBase):
@@ -57,38 +52,32 @@ class Cli(CliBase):
         ]
         states = non_terminal_states if just_active else None
         resp = b.get_GetExperiments(self.session, archived=False, states=states)
-        experiments = [exp for exp in resp.experiments]
-        return experiments
+        return resp.experiments
 
     def get_experiments_ids(self, just_active: bool = True) -> List[int]:
-        experiments = self.get_experiments(just_active)
-        return [c.id for c in experiments]
+        return [exp.id for exp in self.get_experiments(just_active)]
 
     def get_trial_logs(self, trial_id: int):
-        logs = b.get_TrialLogs(self.session, trialId=trial_id)
-        for log in logs:
-            yield log.message
+        return (log.message for log in b.get_TrialLogs(self.session, trialId=trial_id))
 
     def get_single_trial_exp_logs(self, exp_id: int):
         """
         Get logs for a single trial experiment.
         """
         trials = b.get_GetExperimentTrials(self.session, experimentId=exp_id).trials
-        t_id = trials[0].id
-        return self.get_trial_logs(t_id)
+        return self.get_trial_logs(trials[0].id)
 
     def save_single_trial_experiment_logs(self, output_dir: str, just_active: bool = False):
         """
         Save logs from first trial of each experiment to a given directory.
         """
-        experiments = self.get_experiments(just_active)
         output_path = Path(output_dir)
         output_path.mkdir(parents=True, exist_ok=True)
-        for c in experiments:
-            output_file = output_path / f"{c.id}-{c.name}.log"
+        for exp in self.get_experiments(just_active):
+            output_file = output_path / f"{exp.id}-{exp.name}.log"
             print(f"Saving logs {output_file}")
             with output_file.open("w") as f:
-                for log in self.get_single_trial_exp_logs(c.id):
+                for log in self.get_single_trial_exp_logs(exp.id):
                     f.write(log)
 
     def get_tasks(self) -> List[Task]:
@@ -101,29 +90,26 @@ class Cli(CliBase):
 
     def clean_os_path(self, s: str) -> str:
         """
-        remove some characters and replace spaces with underscores
+        Remove some characters and replace spaces with underscores.
         """
         return "".join([c if (c.isalnum() or c in "/-_.") else "_" for c in s])
 
     def save_all_logs(self, output_dir: str, just_active: bool = False):
         """
-        Save all task logs to a given directory
+        Save all task logs to a given directory.
         """
-        experiments = self.get_experiments(just_active)
         output_path = Path(output_dir)
         output_path.mkdir(parents=True, exist_ok=True)
-        for exp in experiments:
-            trials = b.get_GetExperimentTrials(self.session, experimentId=exp.id).trials
-            for t in trials:
+        for exp in self.get_experiments(just_active):
+            for trial in b.get_GetExperimentTrials(self.session, experimentId=exp.id).trials:
                 output_file = output_path / self.clean_os_path(
-                    f"exp{exp.id}-trial{t.id}-{exp.name}.log"
+                    f"exp{exp.id}-trial{trial.id}-{exp.name}.log"
                 )
                 print(f"Saving {output_file}")
                 with output_file.open("w") as f:
-                    for log in self.get_trial_logs(t.id):
+                    for log in self.get_trial_logs(trial.id):
                         f.write(log)
-        tasks = self.get_tasks()
-        for task in tasks:
+        for task in self.get_tasks():
             output_file = output_path / self.clean_os_path(f"task-{task.id}-{task.description}.log")
             print(f"Saving {output_file}")
             with output_file.open("w") as f:
