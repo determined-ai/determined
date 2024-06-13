4c8ea8465195428aace50175e7bfc55467c9bf9b
AmanuelAaron <aaron.amanuel@hpe.com>
2024-06-13 12:57:25 -0400

    add run_id index

--- master/static/migrations/20240610092648_add-run-hparam-index.tx.up.sql
+++ master/static/migrations/20240610092648_add-run-hparam-index.tx.up.sql
@@ -1,3 +1,4 @@
+CREATE INDEX run_hparam_id ON run_hparams(run_id);
 CREATE INDEX run_hparam_number_val ON run_hparams(hparam, number_val);
 CREATE INDEX run_hparam_text_val ON run_hparams(hparam, text_val);
 CREATE INDEX run_hparam_bool_val ON run_hparams(hparam, bool_val);
