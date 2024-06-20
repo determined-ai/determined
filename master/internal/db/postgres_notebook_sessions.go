553521e84e6144f696edb205182b589b77b651b4
Anda Zhou <83614683+azhou-determined@users.noreply.github.com>
2024-06-20 08:41:29 -0700

    feat: enable token auth for Jupyter notebooks [MD-404] (#9452)
    Adds notebook sessions table to persist and fetch Jupyter tokens

---
+++ master/internal/db/postgres_notebook_sessions.go
@@ -0,0 +1,63 @@
+package db
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/o1egl/paseto"
+
+	"github.com/determined-ai/determined/master/pkg/model"
+)
+
+// StartNotebookSession persists a new notebook session row into the database.
+func StartNotebookSession(
+	ctx context.Context,
+	userSessionID model.SessionID,
+	taskID model.TaskID,
+	token *string,
+) error {
+	notebookSession := &model.NotebookSession{
+		UserSessionID: userSessionID,
+		TaskID:        taskID,
+		Token:         token,
+	}
+
+	if _, err := Bun().NewInsert().Model(notebookSession).
+		Returning("id").Exec(ctx, &notebookSession.ID); err != nil {
+		return fmt.Errorf("failed to create notebook session for task (%s): %w", taskID, err)
+	}
+
+	return nil
+}
+
+// GenerateNotebookSessionToken generates a token for a notebook session.
+func GenerateNotebookSessionToken(
+	userSessionID model.SessionID,
+	taskID model.TaskID,
+) (string, error) {
+	notebookSession := &model.NotebookSession{
+		UserSessionID: userSessionID,
+		TaskID:        taskID,
+	}
+
+	v2 := paseto.NewV2()
+	token, err := v2.Sign(GetTokenKeys().PrivateKey, notebookSession, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate task authentication token: %w", err)
+	}
+	return token, nil
+}
+
+// DeleteNotebookSessionByTask deletes the notebook session associated with the task.
+func DeleteNotebookSessionByTask(
+	ctx context.Context,
+	taskID model.TaskID,
+) error {
+	if _, err := Bun().NewDelete().
+		Table("notebook_sessions").
+		Where("task_id = ?", taskID).
+		Exec(ctx); err != nil {
+		return fmt.Errorf("failed to delete notebook session for task (%s): %w", taskID, err)
+	}
+	return nil
+}
