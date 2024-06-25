ead223225c870044214ed5005af65c260a305c79
Aaron Amanuel <amanuel.aaron1@gmail.com>
2024-06-25 12:37:39 -0400

    fix: return workspace name for breadcrumb in Project Details page (#9564)
    (cherry picked from commit 2331e5b5e71a29661b2cc454b8817cc8ec4b3bb4)

--- master/internal/api_project_intg_test.go
+++ master/internal/api_project_intg_test.go
@@ -641,3 +641,65 @@ func TestPatchProjectWithConcurrent(t *testing.T) {
 	}
 	require.NoError(t, errgrp.Wait())
 }
+
+func TestPatchProjectWithInvalidProjectKey(t *testing.T) {
+	api, _, ctx := setupAPITest(t, nil)
+	wresp, werr := api.PostWorkspace(ctx, &apiv1.PostWorkspaceRequest{Name: uuid.New().String()})
+	require.NoError(t, werr)
+
+	projectName := "test-project" + uuid.New().String()
+	resp, err := api.PostProject(ctx, &apiv1.PostProjectRequest{
+		Name: projectName, WorkspaceId: wresp.Workspace.Id,
+	})
+	require.NoError(t, err)
+
+	type TestCase struct {
+		Description string
+		Key         string
+		Err         string
+	}
+	testCases := []TestCase{
+		{
+			Description: "empty key",
+			Key:         "",
+			Err:         "project key cannot be empty",
+		},
+		{
+			Description: "key with special characters",
+			Key:         "!@#$%",
+			Err:         "project key can only contain alphanumeric characters",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Description, func(t *testing.T) {
+			_, err := api.PatchProject(ctx, &apiv1.PatchProjectRequest{
+				Id: resp.Project.Id,
+				Project: &projectv1.PatchProject{
+					Key: wrapperspb.String(tc.Key),
+				},
+			})
+			require.Error(t, err)
+			require.ErrorContains(t, err, tc.Err)
+		})
+	}
+}
+
+func TestGetProjectByID(t *testing.T) {
+	api, curUser, ctx := setupAPITest(t, nil)
+	wresp, werr := api.PostWorkspace(ctx, &apiv1.PostWorkspaceRequest{Name: uuid.New().String()})
+	require.NoError(t, werr)
+
+	projectName := "test-project" + uuid.New().String()
+	resp, err := api.PostProject(ctx, &apiv1.PostProjectRequest{
+		Name: projectName, WorkspaceId: wresp.Workspace.Id,
+	})
+	require.NoError(t, err)
+
+	project, err := api.GetProjectByID(ctx, resp.Project.Id, curUser)
+	require.NoError(t, err)
+	require.Equal(t, wresp.Workspace.Name, project.WorkspaceName)
+	require.Equal(t, wresp.Workspace.Id, project.WorkspaceId)
+	require.Equal(t, projectName, project.Name)
+	require.Equal(t, resp.Project.Id, project.Id)
+}
