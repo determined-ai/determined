94e88b5106d8c94168ca5de21cfc36d30d5de711
Keita Nonaka <keita.nonaka@hpe.com>
2024-06-27 11:01:35 -0700

    fix: feedback

--- webui/react/src/components/RunMoveWarningModalComponent.tsx
+++ webui/react/src/components/RunMoveWarningModalComponent.tsx
@@ -12,12 +12,12 @@ type RunMoveWarningProps = {
 const RunMoveWarningCopy = ({ onClose }: RunMoveWarningProps) => (
   <Modal
     cancel
+    size="small"
     submit={{ handleError, handler: () => onClose('ok'), text: 'Move runs and searches' }}
     title="Move Run Dependency Alert"
     onClose={() => onClose('cancel')}>
-    Some runs you are trying to move belong to a Hyperparameter Search and cannot be moved
-    independently without breaking their contextual relationships. These runs will be moved with
-    their parent search.
+    {`Some of the runs you're trying to move are part of a hyperparameter search. To preserve their
+    contextual relationships, the associated search(es) will be moved along with the selected runs.`}
   </Modal>
 );
 
