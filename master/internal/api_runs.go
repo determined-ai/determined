f1421460b26465bc826a79569c97930932b3ed06
AmanuelAaron <aaron.amanuel@hpe.com>
2024-06-28 09:01:37 -0400

    cast style

--- master/internal/api_runs.go
+++ master/internal/api_runs.go
@@ -140,7 +140,7 @@ func getRunsColumns(q *bun.SelectQuery) *bun.SelectQuery {
 		Column("r.external_run_id").
 		Column("r.project_id").
 		Column("r.searcher_metric_value").
-		ColumnExpr("CONCAT(p.key, '-' ,CAST(r.local_id as text)) as local_id").
+		ColumnExpr("CONCAT(p.key, '-' , r.local_id::text) as local_id").
 		ColumnExpr("extract(epoch FROM coalesce(r.end_time, now()) - r.start_time)::int AS duration").
 		ColumnExpr("CASE WHEN r.hparams='null' THEN NULL ELSE r.hparams END AS hyperparameters").
 		ColumnExpr("r.summary_metrics AS summary_metrics").
