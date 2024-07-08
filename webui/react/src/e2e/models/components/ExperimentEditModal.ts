abcccc71422f95082392f5bf6a56262d37569650
John Kim <john.kim@hpe.com>
2024-07-08 16:41:43 -0400

    comment

--- webui/react/src/e2e/models/components/ExperimentEditModal.ts
+++ webui/react/src/e2e/models/components/ExperimentEditModal.ts
@@ -1,6 +1,9 @@
 import { BaseComponent } from 'e2e/models/common/base/BaseComponent';
 import { Modal } from 'e2e/models/common/hew/Modal';
 
+/**
+ * Represents the ExperimentEditModal component in src/components/ExperimentEditModal.tsx
+ */
 export default class ExperimentEditModal extends Modal {
   readonly nameInput = new BaseComponent({
     parent: this,
