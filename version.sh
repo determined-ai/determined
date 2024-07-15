6bf28556bc5ff9b98e63fa5e9d6360c3fe34dd5c
David Fluck <david.fluck@hpe.com>
2024-07-15 14:41:18 -0400

    Expand version.sh
    * Modify version.sh to return the version from the most recent previous
      tag, less 'v', with the current short commit hash appended, if run
      with VERSION unset. This is necessary to return correct versions if
      building locally. This helps resolve issues with using strings of the
      form '0.0.0-SHA', which Pachyderm uses. Specifically, the model_hub
      Python package depends on a version of determined>=0.13.11, which
      breaks with versions like '0.0.0', because it is semantically "less"
      (even if we're building a version that isn't actually earlier).

--- version.sh
+++ version.sh
@@ -1,16 +1,73 @@
 #!/bin/bash
 
-# Safety first.
-# -e: exit at first non-zero error code
-# -x: print commands as we execute them
-# -o pipefail: exit if one part of a pipe fails
-set -exo pipefail
+# This script dynamically determines an appropriate version string for the
+# currently checked-out commit. As tag versions will typically be provided by CI
+# for releases, this script is primarily to support local builds that work as
+# one would expect.
+#
+# Consider the following git DAG representing a hypothetical Determined git
+# tree:
+#
+#                  I---J (feat-behind-latest-tag)
+#                /
+#...A---B---C---D---E---F---M---N---O
+#    \                   \
+#     G---H (1.1.x)       K---L (1.2.x)
+#         |                   |
+#       1.1.0               1.2.0
+#
+# The newest version is 1.2.0, as tagged on the release branch 1.2.x, at L. The
+# previous version is 1.1.0, on release branch 1.1.x, at H. Our feature branch
+# is feat-behind-latest-tag, with HEAD at J. The goal is to output the nearest
+# tag on the release branch behind wherever we are. At first, I tried to finagle
+# git-rev-list to make this happen, but I was unable to get a working
+# solution. So instead, I used a combination of git-describe and git-tag.
+#
+# git-describe will give us a tag if we're currently on a release branch, or any
+# branch with tags, which is unlikely, but possible. If we're on a feature
+# branch, which is much more likely, we need to use git-tag and git-merge-base
+# to work backward: we find the merge-base of HEAD and main, then search for all
+# tags that don't contain that commit (i.e. all tags created before that
+# commit). From there, we just sort and filter the tag list, and grab the top
+# element, which is the most recent, previous tag.
+#
+# So, in our diagram, if run from B, C, D, E, F, I, J, or K, the script will return
+# 1.1.0. If run from L, M, N, or O, it will return 1.2.0. And so on.
+
+set -x
 
 # If VERSION is unset or the empty string, ""
 if [ -z ${VERSION} ]; then
-	# Grab version from git.
-	echo -n "$(git describe --tags --always 2>/dev/null)" | sed -e 's/-/./g' | sed -e 's/v//g'
+    # Check if this branch has any tags (typically, only release branches will
+    # have tags).
+    MAYBE_TAG=$(git describe --tags --abbrev=0 2>/dev/null)
+    SHA=$(git rev-parse --short HEAD)
+
+    # No tag on current branch.
+    if [ -z ${MAYBE_TAG} ]; then
+        # Use git to find the merge base between the current branch and main,
+        # and then find the closest tag behind that, using --no-contains. Then,
+        # use grep to remove some special cases, namely: old Determined version
+        # tags beginning with 'v', and all tags that end in '-ee'. Then, use
+        # head to grab the first one, since the list is sorted in descending
+        # order, handling -rc tags correctly courtesy of
+        # versionsort.suffix. Finally, use sed to remove the 'v' prefix to get
+        # the bare version string.
+        MAYBE_TAG=$(git \
+                        -c versionsort.suffix='-rc' \
+                        tag \
+                        --sort='-v:refname:short' \
+                        --format='%(refname:short)' \
+                        --no-contains=$(git merge-base HEAD main) | \
+                    grep -E -v 'v0.12|-ee' | \
+                    head -n 1 | \
+                    sed -e 's/v//g'
+                  )
+    fi
+
+    # Munge the tag into the form we want.
+    echo -n "${MAYBE_TAG}-${SHA}"
 else
-	# Use existing VERSION.
-	echo -n "${VERSION}"
+    # Use existing VERSION, which is much easier.
+    echo -n "${VERSION}"
 fi
