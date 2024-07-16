0887a97bc79b7f2e0f2f86baeb0c7eae8b23699d
Amanda Vialva <amanda.vialva@hpe.com>
2024-07-16 14:30:21 -0400

    fix: job queue

--- master/internal/rm/kubernetesrm/resource_pool.go
+++ master/internal/rm/kubernetesrm/resource_pool.go
@@ -111,6 +111,7 @@ func (k *kubernetesResourcePool) JobSchedulingStateChanged(msg jobSchedulingStat
 	defer k.mu.Unlock()
 	k.tryAdmitPendingTasks = true
 
+	fmt.Printf("Called: %s \n \n \n ", msg.AllocationID)
 	for it := k.reqList.Iterator(); it.Next(); {
 		req := it.Value()
 		if req.AllocationID == msg.AllocationID {
