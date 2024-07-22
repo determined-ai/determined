8eedd99f5494d6233b2bd0982a313c6bec72da2c
Mikhail Kardash <mikhail.kardash@hpe.com>
2024-07-22 14:42:56 -0700

    fixes

--- tools/scripts/release-party-bumpenvs.sh
+++ tools/scripts/release-party-bumpenvs.sh
@@ -1,25 +1,25 @@
 #!/bin/bash -ex
 
+if [ "$#" -gt 1 ] || [ -n "$1" -a "$1" != "--release" ]; then
+    echo "usage: $0 [--release]" >&2
+    exit 1
+fi
+
 # check for dirty changes
 if [[ -n "$(git status --porcelain)" ]]; then
     echo "untracked or dirty files are not allowed, cleanup before running release-party-bumpenvs.sh"
     exit 1
 fi
 
-if [ "$#" -gt 1 ] || [ -n "$1" && "$1" != "--release" ]; then
-    echo "usage: $0 [--release]" >&2
-    exit 1
-fi
-
-if [ "$1" == "--release"]; then
-    python3 tools/scripts/retag-bumpenvs-yaml.py tools/scripts/bumpenvs.yaml \
-    $(cat VERSION) $(cat tools/scripts/environments-target.txt) --release
+if [ "$1" == "--release" ]; then
+    python tools/scripts/retag-bumpenvs-yaml.py tools/scripts/bumpenvs.yaml \
+    "$(cat tools/scripts/environments-target.txt)" "$(cat VERSION)" --release
 else
-    python3 tools/scripts/retag-bumpenvs-yaml.py tools/scripts/bumpenvs.yaml \
-    $(cat VERSION) $(cat tools/scripts/environments-target.txt)
+    python tools/scripts/retag-bumpenvs-yaml.py tools/scripts/bumpenvs.yaml \
+    "$(cat tools/scripts/environments-target.txt)" "$(cat VERSION)"
 fi
 
-python3 tools/scripts/bumpenvs.py tools/scripts/bumpenvs.yaml
+python tools/scripts/bumpenvs.py tools/scripts/bumpenvs.yaml
 
 
 git add --update
