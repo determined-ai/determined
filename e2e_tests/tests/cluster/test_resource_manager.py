0dbb10887ae21c5db911dc390f4319312ddf37eb
Jerry Gong <jgong@hpe.com>
2024-07-24 12:43:41 -0400

    address comment

--- e2e_tests/tests/cluster/test_resource_manager.py
+++ e2e_tests/tests/cluster/test_resource_manager.py
@@ -37,7 +37,6 @@ def test_allocation_resources_incremental_release() -> None:
         ) as config_file:
             # Launch an experiment that has one resource (docker container) that exits immediately.
             config_obj = conf.load_config(conf.fixtures_path("no_op/single.yaml"))
-            config_obj["resources"] = {**config_obj.get("resources", {})}
             config_obj["hyperparameters"] = {
                 **config_obj.get("hyperparameters", {}),
                 **{"non_chief_exit_immediately": True},
