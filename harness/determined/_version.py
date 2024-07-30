007a4dd99531e75d358f0912a5f6a397f4dcf1c4
David Fluck <david.fluck@hpe.com>
2024-07-30 11:25:10 -0400

    * Remove version.sh invocation from _version.py (called by determined/__init__.py). The relative pathing breaks imports further down, and I can't believe I was foolish enough to think otherwise.

--- harness/determined/_version.py
+++ harness/determined/_version.py
@@ -1,10 +1,7 @@
 import importlib.metadata
 
-import os
-import subprocess
-
 def version(name):
-    version = None
+    version = "1!0.0.0+unknown"
 
     try:
         # Attempt to get the version from the package metadata. This should
@@ -12,26 +9,6 @@ def version(name):
         # includes pip editable installations.
         version = importlib.metadata.version(name)
     except importlib.metadata.PackageNotFoundError:
-        # In editable mode, or otherwise not running from a distribution. Try
-        # calling version.sh next.
         pass
-    else:
-        return version
-
-    try:
-        # This feels more disgusting than it is. Numpy does something similar,
-        # although they generate a version.py file from their Meson build file
-        # that returns a static version string. I'm not thrilled about calling a
-        # shell script during Determined's __init__.py (i.e. on import), but it
-        # should only run for things that use the Python source directly, like
-        # pytest.
-        output = subprocess.run(["./version.sh"], capture_output=True)
-    except subprocess.CalledProcessError:
-        # version.sh failed for whatever reason. Return an unknown version with
-        # epoch set to 1 so at least pip dependency resolution should succeed.
-        version = "1!0.0.0+unknown"
-    else:
-        # version.sh succeeded. Collect the output.
-        version = output.stdout.decode("utf-8")
 
     return version
