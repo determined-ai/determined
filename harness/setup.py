54328e779434f66fa4efd1a9b43cd6ec96f6c304
David Fluck <david.fluck@hpe.com>
2024-07-31 14:02:17 -0400

    * Fix formatting for harness/setup.py, courtesy of black.

--- harness/setup.py
+++ harness/setup.py
@@ -8,6 +8,7 @@ def readme():
     with open("../README.md", "r") as fd:
         return fd.read()
 
+
 def version():
     version = os.environ.get("VERSION")
 
@@ -30,11 +31,12 @@ def version():
 
     return version
 
+
 setuptools.setup(
     # We can't seem to use pyproject.toml to include the Determined README
     # relative to pyproject.toml. But that's okay, because we can still keep
     # setup.py for this.
     long_description=readme(),
     long_description_content_type="text/markdown",
-    version=version()
+    version=version(),
 )
