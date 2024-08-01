bfe79123d3c85ef02cd356b83f4f54478021859d
Determined CI <automation@determined.ai>
2024-08-01 20:54:55 +0000

    chore: bump version: 0.35.0-rc12 -> 0.35.0-rc13

--- model_hub/setup.py
+++ model_hub/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="model-hub",
-    version="0.35.0-rc12",
+    version="0.35.0-rc13",
     author="Determined AI",
     author_email="ai-open-source@hpe.com",
     url="https://determined.ai/",
