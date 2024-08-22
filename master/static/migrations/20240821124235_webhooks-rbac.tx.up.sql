dc1b52e66a0afdb8d99ab4db370c35509bfd5647
gt2345 <gt2345@columbia.edu>
2024-08-22 16:44:20 -0500

    add webhook permission to EditorRestricted

--- master/static/migrations/20240821124235_webhooks-rbac.tx.up.sql
+++ master/static/migrations/20240821124235_webhooks-rbac.tx.up.sql
@@ -5,10 +5,12 @@ INSERT INTO permissions(id, name, global_only) VALUES (97002, 'view webhooks', f
 INSERT INTO permission_assignments(permission_id, role_id) VALUES 
     (97001, 2), -- Workspace admin can edit webhooks
     (97001, 5), -- Editor can edit webhooks
+    (97001, 7), -- EditorRestricted can edit webhooks
     (97001, 9), -- EditorProjectRestricted can edit webhooks
     (97002, 1), -- Cluster admin can view webhooks
     (97002, 2), -- Workspace admin can view webhooks
     (97002, 5), -- Editor can view webhooks
+    (97002, 7), -- EditorRestricted can view webhooks
     (97002, 9), -- EditorProjectRestricted can view webhooks
     (97002, 4)  -- Viewer can view webhooks
     ON CONFLICT DO NOTHING; 
