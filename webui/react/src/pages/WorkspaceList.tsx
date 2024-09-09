b58024d3d0fc782b60af4c607e99388b5375ed0e
John Kim <john.kim@hpe.com>
2024-09-09 12:55:03 -0400

    fix empty message assertion with hew update

--- webui/react/src/pages/WorkspaceList.tsx
+++ webui/react/src/pages/WorkspaceList.tsx
@@ -394,15 +394,15 @@ const WorkspaceList: React.FC = () => {
           workspacesList
         ) : settings.whose === WhoseWorkspaces.All && settings.archived && !isLoading ? (
           <Message
-            data-testid="noWorkspaces"
             description={`Create a workspace to keep track of related projects and ${f_flat_runs ? 'searches' : 'experiments'}.`}
             icon="workspaces"
+            testId="noWorkspaces"
             title="No Workspaces"
           />
         ) : (
           <Message
-            data-testid="noMatchingWorkspaces"
             icon="warning"
+            testId="noMatchingWorkspaces"
             title="No workspaces matching the current filters"
           />
         )}
