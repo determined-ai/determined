607f0e4a453b593ece2cf5e0ef776a068d6b7900
ShreyaLnuHpe <shreya.lnu@hpe.com>
2024-09-10 04:27:19 -0700

    remove llt table

--- master/static/migrations/20240828111940_add-long-lived-tokens-table.tx.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE public.long_lived_tokens (
-    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id integer NOT NULL,
-    expires_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY(user_id) REFERENCES public.users(id) ON DELETE CASCADE
-);
