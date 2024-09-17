6d9c3ff4c6cec27946a0fd15d14922a1daf50197
Corban Beaird <corban.beaird@hpe.com>
2024-09-17 11:29:16 -0600

    refactor: clean up migration for readability

--- master/static/migrations/20240916144113_add-token-rbac-permissions.tx.up.sql
+++ master/static/migrations/20240916144113_add-token-rbac-permissions.tx.up.sql
@@ -4,30 +4,39 @@ INSERT INTO permissions (id, name, global_only) VALUES
     (12003, 'create long lived token', true),
     (12004, 'create other long lived token', true),
     (12005, 'view long lived token', true),
-    (12006, 'view other long lived token', true),
+    (12006, 'view other long lived token', true);
 
-INSERT INTO permission_assignments (permission_id, role_id) VALUES
-    (12001, 1), --	ClusterAdmin -- revoke long lived token
-    (12001, 2), --	WorkspaceAdmin
-    (12001, 5), --	Editor
-    (12001, 7), --	EditorRestricted
-    (12001, 8), --	GenAIUser
-    (12001, 9), --	EditorProjectRestricted
-    (12002, 1), --	ClusterAdmin -- revoke other long lived token
-    (12003, 1), --	ClusterAdmin -- create long lived token
-    (12003, 2), --	WorkspaceAdmin
-    (12003, 5), --	Editor
-    (12003, 7), --	EditorRestricted
-    (12003, 8), --	GenAIUser
-    (12003, 9), --	EditorProjectRestricted
-    (12004, 1), --	ClusterAdmin -- create other long lived token
-    (12005, 1), --	ClusterAdmin -- view long lived token
-    (12005, 2), --	WorkspaceAdmin
-    (12005, 3), -- WorkspaceCreator
-    (12005, 4), -- Viewer
-    (12005, 5), --	Editor
-    (12005, 6), -- ModelRegistryViewer
-    (12005, 7), --	EditorRestricted
-    (12005, 8), --	GenAIUser
-    (12005, 9), --	EditorProjectRestricted
-    (12006, 1); --	ClusterAdmin -- view other long lived token
+-- ClusterAdmin
+INSERT INTO permission_assignments (permission_id, role_id) 
+SELECT p.id AS permission_id, 1 FROM permissions p WHERE p.name IN (
+    'revoke long lived token',
+    'revoke other long lived token',
+    'create long lived token',
+    'create other long lived token',
+    'view long lived token',
+    'view other long lived token'
+);
+
+-- WorkspaceAdmin, Editor, EditorRestricted, GenAIUser, EditorProjectRestricted
+INSERT INTO permission_assignments (permission_id, role_id)
+SELECT p.id AS permission_id, r.id FROM permissions p JOIN roles r ON r.role_name IN (
+    'WorkspaceAdmin',
+    'Editor',
+    'EditorRestricted',
+    'GenAIUser',
+    'EditorProjectRestricted'
+) WHERE p.name IN (
+    'revoke long lived token',
+    'create long lived token',
+    'view long lived token'
+);
+
+-- Viewer, ModelRegistryViewer, WorkspaceCreator
+INSERT INTO permission_assignments (permission_id, role_id)
+SELECT p.id AS permission_id, r.id FROM permissions p JOIN roles r ON r.role_name IN (
+    'Viewer',
+    'ModelRegistryViewer',
+    'WorkspaceCreator'
+) WHERE p.name IN (
+    'view long lived token'
+);
