12a7661664c60db907d1a8d5617c6b91e0af02b4
gt2345 <gt2345@columbia.edu>
2024-09-19 14:13:11 -0500

    fix

--- webui/react/src/pages/FlatRuns/columns.ts
+++ webui/react/src/pages/FlatRuns/columns.ts
@@ -470,7 +470,6 @@ export const getColumnDefs = ({
       data: {
         appTheme,
         kind: STATE_CELL,
-        logSignal: record.logSignal,
         state: getCellStateFromExperimentState(record.state),
       },
       kind: GridCellKind.Custom,
