8aec5f2b4dff3769d6444206fa61902f5657e841
Anda <anda@determined.ai>
2024-09-20 07:26:04 -0700

    wait for task state

--- e2e_tests/tests/task/test_generic_tasks.py
+++ e2e_tests/tests/task/test_generic_tasks.py
@@ -232,23 +232,12 @@ def test_pause_and_unpause_generic_task() -> None:
 
     detproc.check_call(sess, command)
 
-    # The task may still be PAUSING, retry a few times.
-    retries = 5
-    for i in range(retries):
-        pause_resp = bindings.get_GetTask(sess, taskId=task_resp.taskId)
-        if pause_resp.task.taskState == bindings.v1GenericTaskState.PAUSED:
-            break
-        time.sleep(1)
-    else:
-        pytest.fail(f"Task {task_resp.taskId} did not reach a PAUSED state after {retries} seconds.")
-
+    task.wait_for_task_state(sess, task_resp.taskId, bindings.v1GenericTaskState.PAUSED)
 
     # Unpause task
     command = ["det", "-m", conf.make_master_url(), "task", "unpause", task_resp.taskId]
 
     detproc.check_call(sess, command)
 
-    unpause_resp = bindings.get_GetTask(sess, taskId=task_resp.taskId)
-    assert unpause_resp.task.taskState == bindings.v1GenericTaskState.ACTIVE
-
+    task.wait_for_task_state(sess, task_resp.taskId, bindings.v1GenericTaskState.ACTIVE)
     task.wait_for_task_state(sess, task_resp.taskId, bindings.v1GenericTaskState.COMPLETED)
