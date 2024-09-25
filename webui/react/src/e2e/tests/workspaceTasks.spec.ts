5cc62c14fec2ecae12f5e503809140434ed08717
John Kim <john.kim@hpe.com>
2024-09-25 12:36:29 -0400

    feedback

--- webui/react/src/e2e/tests/workspaceTasks.spec.ts
+++ webui/react/src/e2e/tests/workspaceTasks.spec.ts
@@ -5,32 +5,37 @@ import { WorkspaceDetails } from 'e2e/models/pages/WorkspaceDetails';
 test.describe('Workspace Tasks', () => {
   test('JupyterLab', async ({ authedPage, newWorkspace, context }) => {
     const workspaceDetails = new WorkspaceDetails(authedPage);
+    const firstRow = workspaceDetails.taskList.table.table.rows.nth(0);
 
-    await workspaceDetails.gotoWorkspace(newWorkspace.response.workspace.id);
-    await workspaceDetails.tasksTab.pwLocator.click();
+    await test.step('Start task', async () => {
+      await workspaceDetails.gotoWorkspace(newWorkspace.response.workspace.id);
+      await workspaceDetails.tasksTab.pwLocator.click();
 
-    await workspaceDetails.taskList.jupyterLabButton.pwLocator.click();
-    await workspaceDetails.taskList.jupyterLabModal.pwLocator.waitFor();
-    await workspaceDetails.taskList.jupyterLabModal.footer.submit.pwLocator.click();
-    await workspaceDetails.taskList.jupyterLabModal.pwLocator.waitFor({ state: 'hidden' });
+      await workspaceDetails.taskList.jupyterLabButton.pwLocator.click();
+      await workspaceDetails.taskList.jupyterLabModal.pwLocator.waitFor();
+      await workspaceDetails.taskList.jupyterLabModal.footer.submit.pwLocator.click();
+      await workspaceDetails.taskList.jupyterLabModal.pwLocator.waitFor({ state: 'hidden' });
 
-    const jupyterLabPage = await context.waitForEvent('page', { timeout: 10_000 });
-    await jupyterLabPage.close();
+      const jupyterLabPage = await context.waitForEvent('page', { timeout: 10_000 });
+      await jupyterLabPage.close();
+    });
 
-    await workspaceDetails.taskList.table.pwLocator.waitFor();
-    const firstRow = await workspaceDetails.taskList.table.table.rows.nth(0);
-    await (await firstRow.actions.open()).kill.pwLocator.click();
+    await test.step('Kill task', async () => {
+      await (await firstRow.actions.open()).kill.pwLocator.click();
 
-    await workspaceDetails.taskList.taskKillModal.pwLocator.waitFor();
-    await workspaceDetails.taskList.taskKillModal.killButton.pwLocator.click();
+      await workspaceDetails.taskList.taskKillModal.pwLocator.waitFor();
+      await workspaceDetails.taskList.taskKillModal.killButton.pwLocator.click();
 
-    await expect(async () => {
-      expect(await firstRow.state.pwLocator.textContent()).toBe('Terminated');
-    }).toPass();
-    await (await firstRow.actions.open()).viewLogs.pwLocator.click();
+      await expect(await firstRow.state.pwLocator).toHaveText('Terminated');
+    });
 
-    const taskLogs = new TaskLogs(authedPage);
-    await taskLogs.logViewer.pwLocator.waitFor();
-    await taskLogs.logViewer.logEntry.nth(0).pwLocator.waitFor();
+    await test.step('View logs', async () => {
+      await (await firstRow.actions.open()).viewLogs.pwLocator.click();
+
+      const taskLogs = new TaskLogs(authedPage);
+
+      await taskLogs.logViewer.pwLocator.waitFor();
+      await taskLogs.logViewer.logEntry.nth(0).pwLocator.waitFor();
+    });
   });
 });
