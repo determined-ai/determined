e7369813814ada3dc45bd6beb351a8b977b401db
Anda <anda@determined.ai>
2024-10-09 08:57:06 -0700

    auto commit: wip

--- master/pkg/searcher/util_test.go
+++ master/pkg/searcher/util_test.go
@@ -1,6 +1,7 @@
 package searcher
 
 import (
+	"fmt"
 	"testing"
 
 	"gotest.tools/assert"
@@ -12,21 +13,51 @@ type TestSearchRunner struct {
 	config   expconf.SearcherConfig
 	searcher *Searcher
 	method   SearchMethod
-	runs     map[int32]testRun
+	runs     map[int32]*testRun
 	t        *testing.T
 }
 
 type testRun struct {
-	id           int32
-	hparams      HParamSample
-	searchRunner *TestSearchRunner
+	id        int32
+	hparams   HParamSample
+	stopped   bool
+	stoppedAt int
+	completed bool
+}
+
+func (tr testRun) String() string {
+	return fmt.Sprintf("testRun{id: %v, hparams: %v, stopped: %v, stoppedAt: %v}", tr.id, tr.hparams, tr.stopped, tr.stoppedAt)
+}
+
+func (sr *TestSearchRunner) trainLoop() {
+	startingMetric := 1.0
+	sr.start()
+	for i := 0; i < len(sr.runs); i++ {
+		run := sr.runs[int32(i)]
+		for j := 0; j <= 100; j += 10 {
+			startingMetric += 1
+			sr.reportValidationMetric(run.id, j, startingMetric)
+			//fmt.Printf("run=%v, step=%v, metric=%v, created=%v, stopping=%v\n", run.id, j, startingMetric, creates, stops)
+			if run.stopped {
+				run.stoppedAt = j
+				break
+			}
+		}
+		sr.closeRun(run.id)
+	}
 }
 
 func NewTestSearchRunner(t *testing.T, config expconf.SearcherConfig, hparams expconf.Hyperparameters) *TestSearchRunner {
 	expSeed := uint32(102932948)
 	method := NewSearchMethod(config)
 	searcher := NewSearcher(expSeed, method, hparams)
-	return &TestSearchRunner{t: t, config: config, searcher: searcher, method: method, runs: make(map[int32]testRun)}
+	return &TestSearchRunner{
+		t:        t,
+		config:   config,
+		searcher: searcher,
+		method:   method,
+		runs:     make(map[int32]*testRun),
+	}
 }
 
 func (sr *TestSearchRunner) start() ([]testRun, []testRun) {
@@ -58,6 +89,8 @@ func (sr *TestSearchRunner) reportValidationMetric(runID int32, stepNum int, met
 
 // closeRun simulates a run completing its train loop and exiting.
 func (sr *TestSearchRunner) closeRun(runID int32) ([]testRun, []testRun) {
+	run := sr.runs[runID]
+	run.completed = true
 	actions, err := sr.searcher.RunClosed(runID)
 	assert.NilError(sr.t, err, "error closing run")
 	return sr.handleActions(actions)
@@ -70,15 +103,16 @@ func (sr *TestSearchRunner) handleActions(actions []Action) ([]testRun, []testRu
 	for _, action := range actions {
 		switch action := action.(type) {
 		case Create:
-			run := testRun{id: int32(len(sr.searcher.state.RunsCreated)), hparams: action.Hparams, searchRunner: sr}
+			run := testRun{id: int32(len(sr.searcher.state.RunsCreated)), hparams: action.Hparams}
 			_, err := sr.searcher.RunCreated(run.id, action)
 			assert.NilError(sr.t, err, "error creating run")
 
-			sr.runs[run.id] = run
+			sr.runs[run.id] = &run
 			runsCreated = append(runsCreated, run)
 		case Stop:
 			run := sr.runs[action.RunID]
-			runsStopped = append(runsStopped, run)
+			run.stopped = true
+			runsStopped = append(runsStopped, *run)
 		}
 	}
 	return runsCreated, runsStopped
