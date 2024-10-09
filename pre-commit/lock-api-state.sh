9ae8f2f9454cb9a75997bb40c5fc927518b8b36d
David Fluck <david.fluck@hpe.com>
2024-10-09 12:48:15 -0400

    Remove debug statements, remove pass_filenames.

--- pre-commit/lock-api-state.sh
+++ pre-commit/lock-api-state.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 
-echo "TEST123"
-
-git status --porcelain
-
 # Regenerate proto/buf.image.bin
 make get-deps-proto
 make -C proto gen-buf-image
-
-echo "TEST456"
-
-git status --porcelain
 
 # If proto/buf.image.bin has been modified locally, then we have changes to
 # commit, and the status check returns a 1 and fails. Otherwise, it returns a 0
