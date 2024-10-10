298289f19998999a6be473758c6bdf6912716fc3
Thiago Dallacqua <thiago.menezes-dallacqua@hpe.com>
2024-10-10 11:50:19 -0300

    Merge `main` into `thiago/ET-757`

--- webui/react/src/pages/ConfigPoliciesPage.tsx
+++ webui/react/src/pages/ConfigPoliciesPage.tsx
@@ -24,3 +24,29 @@ const TemplatesPage: React.FC = () => {
 };
 
 export default TemplatesPage;
+import React, { useRef } from 'react';
+
+import ConfigPolicies from 'components/ConfigPolicies';
+import Page from 'components/Page';
+import { paths } from 'routes/utils';
+
+const TemplatesPage: React.FC = () => {
+  const pageRef = useRef<HTMLElement>(null);
+
+  return (
+    <Page
+      breadcrumb={[
+        {
+          breadcrumbName: 'Config Policies',
+          path: paths.configPolicies(),
+        },
+      ]}
+      containerRef={pageRef}
+      id="configPolicies"
+      title="Config Policies">
+      <ConfigPolicies global />
+    </Page>
+  );
+};
+
+export default TemplatesPage;
