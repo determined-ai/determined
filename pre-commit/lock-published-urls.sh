ef8f679f8d27822229ee15099dc022daace68cee
David Fluck <david.fluck@hpe.com>
2024-10-14 17:05:53 -0400

    Add lock-published-urls make target for docs. This cleans up the pre-commit script a bit.
    Clean up lock-published-urls pre-commit script to invoke the new make
    target.

--- pre-commit/lock-published-urls.sh
+++ pre-commit/lock-published-urls.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-# redirects.py inspects files to detect missing redirects, so generated files
-# must be generated before the publish step.
-make -C docs attributions.rst
-
-# Generate redirects.
-python3 docs/redirects.py publish
+make -C docs lock-published-urls
 
 # If docs/.redirects/all_published_urls_ever.json has been modified locally,
 # then `redirects.py publish` has made modifications that we need to commit.
