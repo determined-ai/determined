ac144279ef74fc57271fe2027d30f7a7922c56b0
Emily Bonar <emily.bonar@hpe.com>
2024-10-16 17:05:05 -0400

    Fix comparison view when selection is ALL_EXCEPT

--- webui/react/src/pages/FlatRuns/FlatRuns.tsx
+++ webui/react/src/pages/FlatRuns/FlatRuns.tsx
@@ -1183,6 +1183,8 @@ const FlatRuns: React.FC<Props> = ({ projectId, workspaceId, searchId }) => {
             open={settings.compare}
             projectId={projectId}
             runSelection={settings.selection}
+            searchId={searchId}
+            tableFilters={filtersString}
             onWidthChange={handleCompareWidthChange}>
             <DataGrid<FlatRun, FlatRunAction>
               columns={columns}
