efa27ec79e8a1dc21e01a5c1233c7ef198ecafbe
Jerry Gong <jgong@hpe.com>
2024-10-19 00:36:39 -0400

    clean up

--- master/pkg/schemas/expconf/const.go
+++ master/pkg/schemas/expconf/const.go
@@ -12,11 +12,3 @@ const (
 	CUDAImage = "determinedai/pytorch-ngc-dev:0736b6d"
 	ROCMImage = "determinedai/environments:rocm-5.6-pytorch-1.3-tf-2.10-rocm-mpich-0736b6d"
 )
-
-// Default log policy values.
-const (
-	ECCErrorPattern = ".*uncorrectable ECC error encountered.*"
-	ECCErrorSignal  = "ECC Error"
-	CUDAOOMPattern  = ".*CUDA out of memory.*"
-	CUDAOOMSignal   = "CUDA OOM"
-)
