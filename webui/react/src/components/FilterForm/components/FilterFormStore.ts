06ae198d4662c9f26c08e5e0f5762d21909f1ff8
Thiago Dallacqua <thiago.menezes-dallacqua@hpe.com>
2024-10-23 12:26:46 -0300

    chore: attend to PR comments

--- webui/react/src/components/FilterForm/components/FilterFormStore.ts
+++ webui/react/src/components/FilterForm/components/FilterFormStore.ts
@@ -242,8 +242,9 @@ export class FilterFormStore {
     col: Pick<V1ProjectColumn, 'location' | 'type' | 'column'>,
   ): void {
     return this.#updateField(id, (form) => {
-      if (form.columnName === col.column) {
-        if (form.type === col.type && form.location === col.location) return form;
+      const isSameColumn = form.columnName === col.column && form.type === col.type;
+      if (isSameColumn && form.location === col.location) {
+        return form;
       }
       return {
         ...form,
