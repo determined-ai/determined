7ce9fad21c03cb080c117db2c2a732d4c81fb1f4
Daniel Janicek <daniel.janicek@hpe.com>
2024-10-31 14:11:13 -0700

    more install tweaks

--- tools/slurm/scripts/slurmcluster.sh
+++ tools/slurm/scripts/slurmcluster.sh
@@ -211,7 +211,7 @@ LOCAL_CUDA_IMAGE_STRING="determinedai/pytorch-ngc-dev:e960eae"
 LOCAL_CUDA_IMAGE_SQSH=${LOCAL_CUDA_IMAGE_STRING//[\/:]/+}.sqsh
 if [[ -n $GPUS ]]; then
     # install nvidia drivers
-    gcloud_ssh "curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --no-tty --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+    gcloud_ssh "curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --no-tty --batch --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
     && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
         sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
         sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list"
