3ed3f52512a254178e460dd45447e32c2a1f69ad
Daniel Janicek <daniel.janicek@hpe.com>
2024-10-31 15:06:23 -0700

    finish install after reboot

--- tools/slurm/scripts/slurmcluster.sh
+++ tools/slurm/scripts/slurmcluster.sh
@@ -214,6 +214,8 @@ if [[ -n $GPUS ]]; then
     gcloud_ssh "curl -L https://github.com/GoogleCloudPlatform/compute-gpu-installation/releases/download/cuda-installer-v1.1.0/cuda_installer.pyz --output cuda_installer.pyz"
     gcloud_ssh "sudo python3 cuda_installer.pyz install_driver"
     gcloud_ssh "sudo python3 cuda_installer.pyz install_cuda"
+    sleep 120 # DNJ DEBUG TODO - wait until reboot done, then run again to finish install
+    gcloud_ssh "sudo python3 cuda_installer.pyz install_cuda"
     gcloud_ssh "sudo python3 cuda_installer.pyz verify_cuda"
 fi
 # Enroot container creation
