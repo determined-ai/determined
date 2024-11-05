dd6451208ed4c7fc738185a8a35a3f162c702e76
Jerry Gong <jgong@hpe.com>
2024-11-05 15:09:05 -0500

    chore: bump current environment image versions to 0.38.0

--- master/pkg/schemas/expconf/const.go
+++ master/pkg/schemas/expconf/const.go
@@ -8,8 +8,8 @@ const (
 
 // Default task environment docker image names.
 const (
-	CPUImage  = "determinedai/pytorch-ngc-dev:0736b6d"
-	CUDAImage = "determinedai/pytorch-ngc-dev:0736b6d"
+	CPUImage  = "determinedai/pytorch-ngc:0.38.0"
+	CUDAImage = "determinedai/pytorch-ngc:0.38.0"
 	ROCMImage = "determinedai/environments:rocm-5.6-pytorch-1.3-tf-2.10-rocm-mpich-0736b6d"
 )
 
